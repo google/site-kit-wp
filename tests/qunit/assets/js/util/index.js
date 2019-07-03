375e9cdc4b2d391bc98d3f0e1e12b04c6af90bf7
Ivan Kristianto <ivan.kristianto@gmail.com>
2019-07-03 17:41:44 +0700

    Remove typo for qunit test data

--- tests/qunit/assets/js/util/index.js
+++ tests/qunit/assets/js/util/index.js
@@ -261,7 +261,7 @@ var gtag = function( type, name, sendto, category, label, value ) {
 	};
 };
 
-var sendAnalyticsTrackingEventExpected = '{"type":"event","name":"name","sendto":{"event_category":"category","event_label":"label","event_value":"value","dimension1":"","dimension2":"true"}}no';
+var sendAnalyticsTrackingEventExpected = '{"type":"event","name":"name","sendto":{"event_category":"category","event_label":"label","event_value":"value","dimension1":"","dimension2":"true"}}';
 QUnit.test( 'sendAnalyticsTrackingEvent', function ( assert ) {
 	window.googlesitekit.admin.trackingOptin = true;
 	const value = JSON.stringify( testFunctions.sendAnalyticsTrackingEvent( 'category', 'name', 'label', 'value' ) );
