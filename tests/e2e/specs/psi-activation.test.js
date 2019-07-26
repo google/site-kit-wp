7bb52125b741122a60fdc6ed8fae341f268ecf65
Adam Silverstein <adam@10up.com>
2019-07-26 12:33:36 -0600

    PSI test work

--- tests/e2e/specs/psi-activation.test.js
+++ tests/e2e/specs/psi-activation.test.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { switchUserToAdmin, visitAdminPage } from '@wordpress/e2e-test-utils';
+import { switchUserToAdmin, visitAdminPage, activatePlugin, deactivatePlugin } from '@wordpress/e2e-test-utils';
 
 describe( 'PageSpeed Insights Activation', () => {
 	beforeEach( async() => {
@@ -11,32 +11,78 @@ describe( 'PageSpeed Insights Activation', () => {
 	} );
 
 	it( 'Dashboard should display Activate PageSpeed Insights CTA', async() => {
+
 		await switchUserToAdmin();
+		await activatePlugin( 'e2e-tests-reset-plugin' );
+		await deactivatePlugin( 'e2e-tests-reset-plugin' );
+		await activatePlugin( 'e2e-tests-auth-plugin' );
 		await visitAdminPage( 'admin.php', 'page=googlesitekit-dashboard' );
 		await page.waitForSelector( '.googlesitekit-cta__title' );
 		const welcomeTitle = await page.$x(
-			'//h3[contains(@class,"googlesitekit-cta__title") and contains(text(), "Activate PageSpeed Insights.")]'
+			'//h3 [ contains( @class,"googlesitekit-cta__title") and contains( text(), "Activate PageSpeed Insights.")]'
 		);
 		expect( welcomeTitle.length ).not.toEqual( 0 );
 
 	} );
 
-	it( 'Should lead you to the activation page', async() => {
+	it( 'Setup button should lead you to the activation page', async() => {
 		const setupButton = await page.$x(
-			'//a[contains(@class,"googlesitekit-cta-link")]'
+			'//a[contains(@class,"googlesitekit-cta-link" ) and contains( text(), "Activate PageSpeed Insights")]'
 		);
 
 		expect( setupButton.length ).not.toEqual( 0 );
 
-		await setupButton[1].click();
+		// PSI CTA is the second CTA on the screen, link index 1.
+		const buttonText = await page.evaluate( a => a.textContent, setupButton[0] );
+		expect( buttonText ).toEqual( 'Activate PageSpeed Insights' );
+
+		await setupButton[0].click();
 		await page.waitForSelector( '.googlesitekit-setup-module__title' );
 
 		const psiHeader = await page.$x(
-			'//h2[contains(@class,"googlesitekit-setup-module__title") and contains(text(), "PageSpeed Insights")]'
+			'//h2[ contains( @class,"googlesitekit-setup-module__title" ) and contains( text(), "PageSpeed Insights" ) ]'
 		);
 
 		expect( psiHeader.length ).not.toEqual( 0 );
 
+	} );
+
+	it ( 'Activating should submit the entered key.', async() => {
+		try {
+
+		// Enter the actication key.
+		await page.type( 'input.mdc-text-field__input', 'PSIKEYTOSUBMIT' );
+
+		const submit = await page.$x( '//button [contains( @class,"mdc-button" ) and contains( span, "Proceed" ) ]' );
+
+		expect( submit.length ).not.toEqual( 0 );
+
+		// Click the submit button.
+		await submit[0].click();
+
+		await page.waitForSelector( 'h3.googlesitekit-heading-3' );
+
+		// Check that the correct key is saved on the settings page.
+		await visitAdminPage( 'admin.php', 'page=googlesitekit-settings' );
+		const tabs = await page.$x(
+			'//button[contains(@class,"mdc-tab" ) ]'
+		);
+
+
+		// Open the Admin tab.
+		await tabs[2].click();
+
+		// Check the API Key text, verifying the submitted value has been stored.
+		const apiKeyTextField = await page.$x(
+			'//h5[contains(@class,"googlesitekit-settings-module__meta-item-data" ) ]'
+		);
+		const apiKeyText = await page.evaluate( h5 => h5.textContent, apiKeyTextField[2] );
+
+		expect( apiKeyText ).toEqual( 'PSIKEYTOSUBMIT' );
+		} catch ( e ) {
+			await jestPuppeteer.debug();
+			throw e;
+		}
 
 	} );
 } );
