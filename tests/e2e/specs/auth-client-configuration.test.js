b6b584eafca398e52bbf274ac23b755758212d49
Evan Mullins <evan.mullins@get10up.com>
2019-07-26 18:10:24 +0100

    use `toMatchElement` for consistency with error and step two title in e2e configuration test

--- tests/e2e/specs/auth-client-configuration.test.js
+++ tests/e2e/specs/auth-client-configuration.test.js
@@ -39,7 +39,7 @@ describe( 'Providing client configuration', () => {
 
 	} );
 
-	it( 'Should have enabled button valid value', async() => {
+	it( 'Should have enabled button with valid value', async() => {
 
 		await page.waitForSelector( '#client-configuration' );
 		page.click( '#client-configuration' );
@@ -58,10 +58,7 @@ describe( 'Providing client configuration', () => {
 		}`;
 		await page.keyboard.type( configJSON );
 
-		const errorMessage = await page.$x(
-			'//p[contains(@class,"googlesitekit-error-text") and contains(text(), "Unable to parse client configuration values.")]'
-		);
-		expect( errorMessage.length ).toEqual( 0 );
+		await expect( page ).not.toMatchElement( '.googlesitekit-error-text', { text: 'Unable to parse client configuration values' } );
 
 		expect( await page.$eval( '#wizard-step-one-proceed', ( el ) => el.matches( '[disabled]' ) ) ).toBe( false );
 
@@ -69,11 +66,7 @@ describe( 'Providing client configuration', () => {
 
 		await page.waitForSelector( '.googlesitekit-wizard-step--two' );
 
-		const stepTwoTitle = await page.$x(
-			'//h2[contains(@class,"googlesitekit-wizard-step__title") and contains(text(), "Authenticate with Google")]'
-		);
-
-		expect( stepTwoTitle.length ).not.toEqual( 0 );
+		await expect( page ).toMatchElement( '.googlesitekit-wizard-step__title', { text: 'Authenticate with Google' } );
 
 	} );
 
