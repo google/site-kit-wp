f273cf9f4d600c8b75d59044d3f9748b19f23c47
lucy <lucy@wptips.me>
2019-07-30 17:20:17 +0200

    fix cancel test clicks

--- tests/e2e/specs/plugin-reset.test.js
+++ tests/e2e/specs/plugin-reset.test.js
@@ -6,88 +6,42 @@ import { deactivatePlugin, activatePlugin, visitAdminPage } from '@wordpress/e2e
 describe( 'Reset plugin', () => {
 	beforeAll( async() => {
 		await activatePlugin( 'e2e-tests-auth-plugin' );
-		await activatePlugin( 'google-site-kit' );
 	} );
 
 	afterAll( async() => {
 		await deactivatePlugin( 'e2e-tests-auth-plugin' );
-		await deactivatePlugin( 'google-site-kit' );
 	} );
 
 	beforeEach( async() => {
 		await visitAdminPage( 'admin.php', 'page=googlesitekit-settings' );
-		await page.waitForSelector( '.mdc-tab-scroller__scroll-content' );
+		await expect( page ).toMatchElement( '.googlesitekit-page-header__title', { text: 'Settings' } );
+
+		await page.waitForSelector( 'button.mdc-tab' );
 
 		// Click on Admin Settings Tab.
-		await page.$$eval( '.mdc-tab-scroller__scroll-content button', tabs => {
-			Array.apply( null, tabs )
-				.find( element => 'Admin Settings' === element.textContent )
-				.click();
-		} );
-
-		// Click on Reset Site Kit.
-		await page.evaluate( () => {
-			document.evaluate(
-				'//button[contains(text(), "Reset Site Kit")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
-			).singleNodeValue.click();
-		} );
+		await expect( page ).toClick( 'button.mdc-tab', { text: 'Admin Settings' } );
+		await page.waitForSelector( '.googlesitekit-settings-module__footer' );
 	} );
 
-	it( 'On Reset Site Kit click, confirm dialog displays', async() => {
-		await page.waitForSelector( '.mdc-dialog[aria-hidden="false"]' );
+	it( 'On Reset Site Kit click, confirm dialog displays.', async() => {
 
-		const isHidden = await page.$eval( '.mdc-dialog[aria-hidden="false"]', dialog => {
-			if ( dialog && 'Reset' === dialog.querySelector( '.mdc-button__label' ).textContent ) {
-				return false;
-			}
+		await expect( page ).toClick( 'button.googlesitekit-cta-link', { text: 'Reset Site Kit' } );
+		await page.waitForSelector( '.mdc-dialog--open' );
 
-			return true;
-		} );
-
-		expect( isHidden ).toBe( false );
+		await expect( page ).toMatchElement( '.mdc-dialog.mdc-dialog--open .mdc-button', { text: 'Reset' } );
 	} );
 
 	it( 'On Reset Site Kit click, confirm dialog hides when cancel', async() => {
-		await page.waitForSelector( '.mdc-dialog[aria-hidden="false"]' );
+		await expect( page ).toClick( 'button.googlesitekit-cta-link', { text: 'Reset Site Kit' } );
+		await page.waitForSelector( '.mdc-dialog--open' );
 
-		const resetDialogFound = await page.$eval( '.mdc-dialog[aria-hidden="false"]', dialog => {
-			if ( dialog && 'Reset' === dialog.querySelector( '.mdc-button__label' ).textContent ) {
-				dialog.querySelector( '.mdc-dialog__cancel-button' ).click();
-			}
-
-			return dialog;
-		} );
-
-		expect( resetDialogFound ).not.toEqual( null );
-
-		const isHidden = await page.$$eval( '.mdc-dialog[aria-hidden="true"]', dialogs => {
-
-			// Check reset dialog is hidden.
-			const dialog = Array.apply( dialogs ).filter( dialog => {
-				const button = dialog.querySelector( '.mdc-button__label' );
-				return button && 'Reset' === button.textContent;
-			} );
-			if ( dialog ) {
-				return true;
-			}
-
-			return false;
-		} );
-
-		expect( isHidden ).toBe( true );
+		await expect( page ).toClick( '.mdc-dialog.mdc-dialog--open button', { text: 'Cancel' } );
 	} );
 
 	it( 'Reset dialog button disconnects site kit', async() => {
-
-		// Click Reset button.
-		await page.$$eval( '.mdc-dialog__actions button', buttons => {
-			Array.apply( null, buttons ).map( button => {
-				const label = button.querySelector( '.mdc-button__label' );
-				if ( label && 'Reset' === label.textContent  ) {
-					button.click();
-				}
-			} );
-		} );
+		await expect( page ).toClick( 'button.googlesitekit-cta-link', { text: 'Reset Site Kit' } );
+		await page.waitForSelector( '.mdc-dialog--open' );
+		await expect( page ).toClick( '.mdc-dialog.mdc-dialog--open .mdc-button', { text: 'Reset' } );
 
 		await visitAdminPage( 'admin.php', 'page=googlesitekit-dashboard' );
 
