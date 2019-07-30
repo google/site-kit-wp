47978d6373b99f8576ea1002ac63697bff30a9f2
Evan Mullins <evan.mullins@get10up.com>
2019-07-30 16:48:42 +0100

    clean code up for consistency with other e2e tests

--- tests/e2e/specs/admin-tracking.test.js
+++ tests/e2e/specs/admin-tracking.test.js
@@ -8,35 +8,36 @@ import { resetSiteKit } from '../utils';
 describe( 'Providing client configuration', () => {
 
 	beforeAll( async() => {
-		await activatePlugin( 'google-site-kit' );
 		await activatePlugin( 'e2e-tests-auth-plugin' );
 		await resetSiteKit();
 	} );
 
 	beforeEach( async() => {
 		await visitAdminPage( 'admin.php', 'page=googlesitekit-settings' );
-		await page.waitForSelector( '.mdc-tab-scroller__scroll-content' );
-		page.click( '.mdc-tab-scroller__scroll-content button:last-child' );
+		await page.waitForSelector( '.mdc-tab-bar' );
+
+		// Click on Admin Settings Tab.
+		await expect( page ).toClick( 'button.mdc-tab', { text: 'Admin Settings' } );
 	} );
 
 	afterAll( async() => {
 		await resetSiteKit();
-		await deactivatePlugin( 'google-site-kit' );
 		await deactivatePlugin( 'e2e-tests-auth-plugin' );
 	} );
 
-	it( 'Should select opt-in by default', async() => {
+	it( 'should select opt-in by default', async() => {
 
 		await page.waitForSelector( '#opt-in' );
 
-		expect( await page.$eval( '#opt-in', el => el.checked ) ).toBe( true );
+		await expect( page ).toMatchElement( '#opt-in[checked]' );
 
 	} );
-	it( 'Should have tracking code when opted in', async() => {
+
+	it( 'should have tracking code when opted in', async() => {
 
 		await page.waitForSelector( '#opt-in' );
 
-		expect( await page.$eval( '#opt-in', el => el.checked ) ).toBe( true );
+		await expect( page ).toMatchElement( '#opt-in[checked]' );
 
 		const analyticsScriptTag = await page.$x(
 			'//script[contains(@src,"https://www.google-analytics.com/analytics.js")]'
@@ -50,11 +51,12 @@ describe( 'Providing client configuration', () => {
 
 	} );
 
-	it( 'Should uncheck opt-in box when clicked', async() => {
+	it( 'should uncheck opt-in box when clicked', async() => {
 
 		await page.waitForSelector( '#opt-in' );
 
-		await page.$eval( '#opt-in', elem => elem.click() );
+		// await page.$eval( '#opt-in', elem => elem.click() );
+		await expect( page ).toClick( '#opt-in' );
 
 		await page.waitForResponse( res => {
 			const reqURL = new URL( res.url() );
@@ -63,15 +65,13 @@ describe( 'Providing client configuration', () => {
 		} );
 
 		await page.waitForSelector( '.mdc-checkbox:not(.mdc-checkbox--selected) #opt-in' );
-
 		const optinChecked = await page.$( '.mdc-checkbox--selected #opt-in' );
 		expect( optinChecked ).toBeNull();
-
 		const optinUnChecked = await page.$( '.mdc-checkbox:not(.mdc-checkbox--selected) #opt-in' );
 		expect( optinUnChecked.length ).not.toEqual( 0 );
 	} );
 
-	it( 'Should not have tracking code when not opted in', async() => {
+	it( 'should not have tracking code when not opted in', async() => {
 
 		await page.waitForSelector( '#opt-in' );
 
