f7748be5fd9d8abc446301876f87a5a15bcbedb9
Felix Arntz <felixarntz@google.com>
2019-08-07 12:38:01 +0200

    Ensure a verified site via domain verification is correctly detected.

--- includes/Modules/Search_Console.php
+++ includes/Modules/Search_Console.php
@@ -445,8 +445,8 @@ final class Search_Console extends Module implements Module_With_Screen, Module_
 							);
 						}
 						if ( 'INET_DOMAIN' === $site->getType() ) {
-							$host = str_replace( array( 'http://', 'https://' ), '', $site->getIdentifier() );
-							if ( ! empty( $host ) && false !== strpos( $site_url, (string) $host ) ) {
+							$host = str_replace( array( 'http://', 'https://' ), '', trailingslashit( $site->getIdentifier() ) );
+							if ( ! empty( $host ) && false !== strpos( trailingslashit( $current_url ), $host ) ) {
 								$response = array(
 									'identifier' => $site->getIdentifier(),
 									'type'       => $site->getType(),
