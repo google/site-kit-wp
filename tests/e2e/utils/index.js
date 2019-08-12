be0e75e45984fb0b8f73410a8d09f56cca9cf8fc
Evan Mattson <evan.mattson@10up.com>
2019-08-12 11:59:41 +0300

    add clear-session-storage utility
    not to be confused with clear-local-storage

--- tests/e2e/utils/index.js
+++ tests/e2e/utils/index.js
@@ -1,3 +1,4 @@
 export { deactivateAllOtherPlugins } from './deactivateAllOtherPlugins';
+export { clearSessionStorage } from './clear-session-storage';
 export { pasteText } from './paste-text';
 export { resetSiteKit } from './reset';
