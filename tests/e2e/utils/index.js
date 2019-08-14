803c30d36f2ff5096df58b81455d1865dc2ae888
Evan Mattson <evan.mattson@10up.com>
2019-08-14 14:26:47 +0300

    add use-request-interception utility module

--- tests/e2e/utils/index.js
+++ tests/e2e/utils/index.js
@@ -7,4 +7,5 @@ export { setClientConfig } from './set-client-config';
 export { setSiteVerification } from './set-site-verification';
 export { setSearchConsoleProperty } from './set-search-console-property';
 export { testClientConfig } from './test-client-config';
+export { useRequestInterception } from './use-request-interception';
 export { wpApiFetch } from './wp-api-fetch';
