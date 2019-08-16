f36b328645bb9f4ee6670f21527e35af045e9fe4
Evan Mattson <evan.mattson@10up.com>
2019-08-16 11:51:40 +0300

    e2e add spec for admin-2 auth-flow

---
+++ tests/e2e/specs/auth-flow-admin-2.test.js
@@ -0,0 +1,88 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	activatePlugin,
+	loginUser,
+	createURL,
+	visitAdminPage,
+} from '@wordpress/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import {
+	logoutUser,
+	setAuthToken,
+	setClientConfig,
+	setSearchConsoleProperty,
+	setSiteVerification,
+	useRequestInterception,
+	wpApiFetch,
+} from '../utils';
+
+describe( 'the set up flow for the second administrator', () => {
+
+	beforeAll( async() => {
+		await page.setRequestInterception( true );
+		useRequestInterception( request => {
+			if ( request.url().startsWith( 'https://accounts.google.com/o/oauth2/auth' ) ) {
+				request.respond( {
+					status: 302,
+					headers: {
+						location: createURL( '/', 'oauth2callback=1&code=valid-test-code' )
+					}
+				} );
+			} else {
+				request.continue();
+			}
+		} );
+	} );
+
+	beforeEach( async() => {
+		await activatePlugin( 'e2e-tests-oauth-callback-plugin' );
+		await activatePlugin( 'e2e-tests-site-verification-api-mock' );
+		await setClientConfig();
+		await setAuthToken();
+		await setSiteVerification();
+		await setSearchConsoleProperty();
+		await logoutUser();
+	} );
+
+	afterEach( async() => {
+		await logoutUser();
+
+		// Restore the default/admin user
+		// (switchToAdmin will not work as it is not aware of the current user)
+		await loginUser();
+	} );
+
+	it( 'admin 2', async() => {
+		await loginUser( 'admin-2', 'password' );
+
+		// Simulate that the user is already verified.
+		await wpApiFetch( {
+			path: 'google-site-kit/v1/e2e/verify-site',
+			method: 'post',
+		} );
+		await visitAdminPage( 'admin.php', 'page=googlesitekit-splash' );
+
+		await expect( page ).toMatchElement( '.googlesitekit-wizard-step__title', { text: /Authenticate with Google/i } );
+
+		await Promise.all( [
+			expect( page ).toClick( '.googlesitekit-wizard-step button', { text: /sign in with google/i } ),
+			page.waitForNavigation(),
+		] );
+
+		await expect( page ).toMatchElement( '.googlesitekit-wizard-step__title', { text: /congratulations!/i } );
+
+		await Promise.all( [
+			expect( page ).toClick( '.googlesitekit-wizard-step button', { text: /go to dashboard/i } ),
+			page.waitForNavigation(),
+		] );
+
+		await expect( page ).toMatchElement( '#js-googlesitekit-dashboard' );
+		await expect( page ).toMatchElement( '.googlesitekit-publisher-win__title', { text: /Congrats on completing the setup for Site Kit!/i } );
+	} );
+} );
+
