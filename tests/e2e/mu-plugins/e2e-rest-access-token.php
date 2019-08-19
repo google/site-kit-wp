b9714d58b6cbb59a64425ea72fda05d0e1a33bd6
Evan Mattson <evan.mattson@10up.com>
2019-08-19 18:35:57 +0300

    add missing license headers to utility plugins

--- tests/e2e/mu-plugins/e2e-rest-access-token.php
+++ tests/e2e/mu-plugins/e2e-rest-access-token.php
@@ -2,6 +2,11 @@
 /**
  * Plugin Name: E2E Access Token Endpoint
  * Description: REST Endpoint for setting the access token for Site Kit during E2E tests.
+ *
+ * @package   Google\Site_Kit
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
  */
 
 use Google\Site_Kit\Core\REST_API\REST_Routes;
--- tests/e2e/mu-plugins/e2e-rest-site-verification.php
+++ tests/e2e/mu-plugins/e2e-rest-site-verification.php
@@ -2,6 +2,11 @@
 /**
  * Plugin Name: E2E Site Verification Endpoint
  * Description: REST Endpoint for setting the site verification status of the current user during E2E tests.
+ *
+ * @package   Google\Site_Kit
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
  */
 
 use Google\Site_Kit\Core\REST_API\REST_Routes;
