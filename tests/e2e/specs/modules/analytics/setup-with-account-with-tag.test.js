618f60e27377f143a9c6310df7d88a689ff1263f
Evan Mattson <evan.mattson@10up.com>
2019-08-19 15:40:00 +0300

    add analytics setup with account with tag spec

---
+++ tests/e2e/specs/modules/analytics/setup-with-account-with-tag.test.js
@@ -0,0 +1,121 @@
+/**
+ * WordPress dependencies
+ */
+import { activatePlugin, visitAdminPage } from '@wordpress/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import {
+	deactivateAllOtherPlugins,
+	resetSiteKit,
+	useRequestInterception,
+	setAnalyticsExistingPropertyId,
+	setAuthToken,
+	setClientConfig,
+	setSearchConsoleProperty,
+	setSiteVerification,
+} from '../../../utils';
+
+async function proceedToSetUpAnalytics() {
+	await Promise.all( [
+		expect( page ).toClick( '.googlesitekit-cta-link', { text: /set up analytics/i } ),
+		page.waitForSelector( '.googlesitekit-setup-module--analytics' ),
+		page.waitForResponse( res => res.url().match( 'analytics/data' ) ),
+	] );
+}
+
+const EXISTING_PROPERTY_ID = 'UA-00000001-1';
+const EXISTING_NO_MATCH_PROPERTY_ID = 'UA-99999999-1';
+
+let getAccountsRequestHandler;
+let tagPermissionRequestHandler;
+
+describe( 'setting up the Analytics module with an existing account and existing tag', () => {
+	beforeAll( async() => {
+		await page.setRequestInterception( true );
+		useRequestInterception( request => {
+			if ( request.url().match( 'modules/analytics/data/get-accounts' ) ) {
+				getAccountsRequestHandler && getAccountsRequestHandler( request );
+			} else if ( request.url().match( 'modules/analytics/data/tag-permission' ) ) {
+				tagPermissionRequestHandler && tagPermissionRequestHandler( request );
+			} else if ( request.url().match( '/wp-json/google-site-kit/v1/data/' ) ) {
+				request.respond( {
+					status: 200
+				} );
+			}
+
+			if ( ! request._interceptionHandled ) {
+				request.continue();
+			}
+		} );
+	} );
+
+	beforeEach( async() => {
+		await activatePlugin( 'e2e-tests-auth-plugin' );
+		await activatePlugin( 'e2e-tests-analytics-existing-tag' );
+		await activatePlugin( 'e2e-tests-module-setup-analytics-api-mock' );
+
+		await setClientConfig();
+		await setAuthToken();
+		await setSiteVerification();
+		await setSearchConsoleProperty();
+
+		await visitAdminPage( 'admin.php', 'page=googlesitekit-settings' );
+		await page.waitForSelector( '.mdc-tab-bar' );
+		await expect( page ).toClick( '.mdc-tab', { text: /connect more services/i } );
+		await page.waitForSelector( '.googlesitekit-settings-connect-module--analytics' );
+	} );
+
+	afterEach( async() => {
+		await deactivateAllOtherPlugins();
+		await resetSiteKit();
+	} );
+
+	it( 'pre-selects account and property if an existing tag is found that matches one belonging to the user and prevents them from being changed', async() => {
+		tagPermissionRequestHandler = request => {
+			request.respond( {
+				status: 200,
+				body: 'true',
+			} );
+		};
+		await setAnalyticsExistingPropertyId( EXISTING_PROPERTY_ID );
+		await proceedToSetUpAnalytics();
+
+		await expect( page ).toMatchElement( '.googlesitekit-setup-module--analytics p', { text: new RegExp( `An existing analytics tag was found on your site with the id ${EXISTING_PROPERTY_ID}`, 'i' ) } );
+
+		await expect( page ).toMatchElement( '.mdc-select--disabled .mdc-select__selected-text', { text: /test account a/i } );
+		await expect( page ).toMatchElement( '.mdc-select--disabled .mdc-select__selected-text', { text: /test property x/i } );
+		await expect( page ).toMatchElement( '.mdc-select:not(.mdc-select--disabled) .mdc-select__selected-text', { text: /test profile x/i } );
+
+		// Ensure that Views are not disabled
+		await expect( page ).toClick( '.mdc-select', { text: /test profile x/i } );
+		await expect( page ).toClick( '.mdc-menu-surface--open .mdc-list-item', { text: /test profile x/i } ),
+
+		await Promise.all( [
+			expect( page ).toClick( 'button', { text: /configure analytics/i } ),
+			page.waitForSelector( '.googlesitekit-publisher-win__title' ),
+		] );
+
+		await expect( page ).toMatchElement( '.googlesitekit-publisher-win__title', { text: /Congrats on completing the setup for Analytics!/i } );
+	} );
+
+	it( 'does not allow Analytics to be set up with an existing tag that does not match a property of the user', async() => {
+		getAccountsRequestHandler = request => {
+			request.respond( {
+				status: 500,
+				body: JSON.stringify( {
+					code: 'google_analytics_existing_tag_permission',
+					message: 'google_analytics_existing_tag_permission',
+				} )
+			} );
+		};
+
+		await setAnalyticsExistingPropertyId( EXISTING_NO_MATCH_PROPERTY_ID );
+		await proceedToSetUpAnalytics();
+
+		await expect( page ).toMatchElement( '.googlesitekit-setup-module--analytics p', { text: /google_analytics_existing_tag_permission/i } );
+		await expect( page ).toMatchElement( '.googlesitekit-setup-module--analytics button', { text: /create an account/i } );
+		await expect( page ).toMatchElement( '.googlesitekit-setup-module--analytics button', { text: /re-fetch my account/i } );
+	} );
+} );
