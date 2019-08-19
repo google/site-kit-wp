f5251169ad49915d6cddb1bdd20446aa513b4e6d
Evan Mattson <evan.mattson@10up.com>
2019-08-19 18:33:37 +0300

    add set-analytics-existing-property-id utility

---
+++ tests/e2e/mu-plugins/e2e-rest-analytics-existing-property-id.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: E2E Access Token Endpoint
+ * Description: REST Endpoint for setting the access token for Site Kit during E2E tests.
+ *
+ * @package   Google\Site_Kit
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+use Google\Site_Kit\Core\REST_API\REST_Routes;
+
+add_action( 'rest_api_init', function () {
+	if ( ! defined( 'GOOGLESITEKIT_PLUGIN_MAIN_FILE' ) ) {
+		return;
+	}
+
+	register_rest_route(
+		REST_Routes::REST_ROOT,
+		'e2e/analytics/existing-property-id',
+		array(
+			'methods'  => WP_REST_Server::EDITABLE,
+			'callback' => function ( WP_REST_Request $request ) {
+				if ( $request['id'] ) {
+					update_option( 'googlesitekit_e2e_analytics_existing_property_id', $request['id'] );
+				} else {
+					delete_option( 'googlesitekit_e2e_analytics_existing_property_id' );
+				}
+
+				return array( 'success' => true, 'id' => $request['id'] );
+			}
+		)
+	);
+}, 0 );
+
