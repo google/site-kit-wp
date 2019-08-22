88164ec8504e1b0efcb01b166d3ac7fa43e43c15
Evan Mattson <evan.mattson@10up.com>
2019-08-22 18:57:20 +0300

    remove global declaration for built-in globals

--- assets/js/util/cache-data.js
+++ assets/js/util/cache-data.js
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* global require, process */
 
 const puppeteer = require( 'puppeteer' );
 const fs = require( 'fs' );
