a12937ff954b92923df29309240217b1920541d2
Evan Mattson <evan.mattson@10up.com>
2019-08-23 11:27:28 +0300

    fix jsDoc for setAnalyticsExistingPropertyId

--- tests/e2e/utils/set-analytics-existing-property-id.js
+++ tests/e2e/utils/set-analytics-existing-property-id.js
@@ -7,7 +7,7 @@ import { wpApiFetch } from './wp-api-fetch';
 /**
  * Set the property ID to render on the site, outside of Site Kit.
  *
- * @param {string} token Access token to set.
+ * @param {string} id Analytics property ID (e.g. UA-00000000-1).
  * @return {*} resolved value from apiFetch promise.
  */
 export async function setAnalyticsExistingPropertyId( id ) {
