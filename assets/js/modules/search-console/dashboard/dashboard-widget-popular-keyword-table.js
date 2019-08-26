08a2786b2eb30f99b9e1c42539777ef1799796eb
Evan Mattson <evan.mattson@10up.com>
2019-08-26 11:10:14 +0300

    add clicks column to popular keyword table
    - before impressions

--- assets/js/modules/search-console/dashboard/dashboard-widget-popular-keyword-table.js
+++ assets/js/modules/search-console/dashboard/dashboard-widget-popular-keyword-table.js
@@ -71,6 +71,10 @@ class DashboardWidgetPopularKeywordsTable extends Component {
 				tooltip: __( 'Most searched for keywords related to your content', 'google-site-kit' ),
 			},
 			{
+				title: __( 'Clicks', 'google-site-kit' ),
+				tooltip: __( 'Number of times users clicked on your content in search results', 'google-site-kit' ),
+			},
+			{
 				title: __( 'Impressions', 'google-site-kit' ),
 				tooltip: __( 'Counted each time your content appears in search results', 'google-site-kit' ),
 			},
