7e298025a0b00350d0494ffb34aa506f6727ed0e
lucy <lucy@wptips.me>
2019-08-28 21:48:00 +0200

    re ad errorCode check and set errorCode state to true if no accounts were found

--- assets/js/modules/analytics/setup.js
+++ assets/js/modules/analytics/setup.js
@@ -259,6 +259,12 @@ class AnalyticsSetup extends Component {
 
 			const responseData = await data.get( 'modules', 'analytics', 'get-accounts', queryArgs, false );
 			if ( 0 === responseData.accounts.length ) {
+				newState = {
+					...newState,
+					errorCode: true,
+					errorReason: 'noAccounts',
+				};
+
 				// clear the cache.
 				data.deleteCache( 'analytics', 'get-accounts' );
 			} else if ( ! selectedAccount ) {
@@ -859,6 +865,10 @@ class AnalyticsSetup extends Component {
 		const {
 			onSettingsPage,
 		} = this.props;
+
+		if ( ! errorCode ) {
+			return null;
+		}
 
 		let showErrorFormat = true; // default error message.
 		let message = errorMsg;
