6920a1e5eed72ec86c1b1f7774ff62352a07c9cd
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-09-09 13:09:17 +0100

    feat: Tweak header styling and update visual test

--- assets/js/components/dashboard/dashboard-app.js
+++ assets/js/components/dashboard/dashboard-app.js
@@ -48,13 +48,12 @@ class DashboardApp extends Component {
 								mdc-layout-grid__cell--span-10-desktop mdc-layout-grid__cell--span-6-tablet mdc-layout-grid__cell--span-2-phone
 							">
 								<PageHeader
-									className="googlesitekit-page-header__page-title"
 									title={ __( 'Site Overview', 'google-site-kit' ) }
 								/>
 							</div>
 							<div className="
 									mdc-layout-grid__cell
-									mdc-layout-grid__cell--span-2-desktop mdc-layout-grid__cell--span-2-tablet mdc-layout-grid__cell--span-2-phone mdc-layout-grid__cell--align-top mdc-layout-grid__cell--align-right
+									mdc-layout-grid__cell--span-2-desktop mdc-layout-grid__cell--span-2-tablet mdc-layout-grid__cell--span-2-phone mdc-layout-grid__cell--align-middle mdc-layout-grid__cell--align-right
 							">
 								<DateRangeSelector />
 							</div>
