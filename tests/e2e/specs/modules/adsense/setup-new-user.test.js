5cb4597443eaf3046a4ec1322d01cfdbba878d20
Evan Mattson <evan.mattson@10up.com>
2019-09-25 14:07:40 +0300

    move additional response wait into proceed helper

--- tests/e2e/specs/modules/adsense/setup-new-user.test.js
+++ tests/e2e/specs/modules/adsense/setup-new-user.test.js
@@ -138,7 +138,6 @@ describe( 'setting up the AdSense module', () => {
 
 		await proceedToAdsenseSetup();
 
-		await page.waitForResponse( ( res ) => res.url().match( 'modules/adsense/data/account-status' ) );
 		await expect( page ).toMatchElement( '.googlesitekit-setup-module__title', { text: /We’re getting your site ready for ads/i } );
 		await expect( page ).toMatchElement( '.googlesitekit-cta-link', { text: /Go to your AdSense account to check on your site’s status/i } );
 
@@ -197,7 +196,6 @@ describe( 'setting up the AdSense module', () => {
 
 		await proceedToAdsenseSetup();
 
-		await page.waitForResponse( ( res ) => res.url().match( 'modules/adsense/data/account-status' ) );
 		await expect( page ).toMatchElement( '.googlesitekit-setup-module__title', { text: /We’re getting your site ready for ads/i } );
 		await expect( page ).toMatchElement( '.googlesitekit-cta-link', { text: /Go to your AdSense account to check on your site’s status/i } );
 
@@ -270,7 +268,6 @@ describe( 'setting up the AdSense module', () => {
 
 		await proceedToAdsenseSetup();
 
-		await page.waitForResponse( ( res ) => res.url().match( 'modules/adsense/data/account-status' ) );
 		await expect( page ).toMatchElement( '.googlesitekit-setup-module__title', { text: /Create your AdSense account/i } );
 		await expect( page ).toMatchElement( '.googlesitekit-setup-module__action button', { text: /Create AdSense Account/i } );
 
