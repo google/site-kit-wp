6037fe754d3cc3373d99497d48cb5119dc30b9c1
Evan Mattson <evan.mattson@10up.com>
2019-09-25 11:56:33 +0300

    improve stability of switchDateRange

--- tests/e2e/utils/switch-date-range.js
+++ tests/e2e/utils/switch-date-range.js
@@ -7,6 +7,11 @@
  * @param {string} toRange The new date range to select.
  */
 export async function switchDateRange( fromRange, toRange ) {
-	await expect( page ).toClick( '.mdc-select__selected-text', { text: new RegExp( fromRange, 'i' ) } );
-	await expect( page ).toClick( '.mdc-menu-surface--open .mdc-list-item', { text: new RegExp( toRange, 'i' ) } );
+	await Promise.all( [
+		expect( page ).toClick( '.mdc-select__selected-text', { text: new RegExp( fromRange, 'i' ) } ),
+		page.waitForSelector( '.mdc-select.mdc-select--focused' ),
+		page.waitForSelector( '.mdc-menu-surface--open .mdc-list-item' ),
+	] );
+	// Intentionally left off the '--open' suffix here as it proved problematic for stability.
+	await expect( page ).toClick( '.mdc-menu-surface .mdc-list-item', { text: new RegExp( toRange, 'i' ) } );
 }
