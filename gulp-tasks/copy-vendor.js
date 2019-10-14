f57bc66b1f59c463c9f6b35f75c8bc54fceae1d0
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-10-14 22:26:21 +0100

    chore: Restore copy-vendor script

--- gulp-tasks/copy-vendor.js
+++ gulp-tasks/copy-vendor.js
@@ -6,15 +6,6 @@ import del from 'del';
 
 gulp.task( 'copy-vendor', () => {
 	del.sync( [ 'dist/assets/vendor/*' ] );
-	gulp.src( [
-		// Update React with the version installed in
-		// `node_modules/`/`package.json`.
-		'node_modules/react/umd/react.development.js',
-		'node_modules/react/umd/react.production.min.js',
-		'node_modules/react-dom/umd/react-dom.development.js',
-		'node_modules/react-dom/umd/react-dom.production.min.js',
-	] )
-		.pipe( gulp.dest( 'assets/vendor/' ) );
 	gulp.src( 'assets/vendor/*' )
 		.pipe( gulp.dest( 'dist/assets/vendor/' ) );
 } );
