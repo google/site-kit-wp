c905b9120af64682a01b672482153108eb04718a
Evan Mattson <evan.mattson@10up.com>
2019-10-29 13:40:56 +0200

    refactor site kit plugin activation

--- tests/e2e/specs/plugin-activation.test.js
+++ tests/e2e/specs/plugin-activation.test.js
@@ -3,29 +3,59 @@
  */
 import { deactivatePlugin, activatePlugin } from '@wordpress/e2e-test-utils';
 
+/**
+ * Activation helpers are only necessary pre-1.0 release.
+ * WordPress populates the `data-slug` attribute from the
+ * `slug` returned by the wp.org plugin API. For plugins
+ * that are not available on the .org repo, the slug will be
+ * generated from the plugin's name.
+ *
+ * For this reason, we fallback to the title-based slug if the
+ * expected "official" slug is not found.
+ *
+ * Once the plugin is publicly available, these helpers can be removed
+ * and consuming code can use `google-site-kit` variations in their places.
+ */
+
+const activateSiteKit = async () => {
+	try {
+		await activatePlugin( 'google-site-kit' );
+	} catch {
+		await activatePlugin( 'site-kit-by-google' );
+	}
+};
+
+const deactivateSiteKit = async () => {
+	try {
+		await deactivatePlugin( 'google-site-kit' );
+	} catch {
+		await deactivatePlugin( 'site-kit-by-google' );
+	}
+};
+
 describe( 'Plugin Activation Notice', () => {
 	beforeEach( async () => {
-		await deactivatePlugin( 'site-kit-by-google' );
+		await deactivateSiteKit();
 		await activatePlugin( 'e2e-tests-gcp-credentials-plugin' );
 	} );
 
 	afterEach( async () => {
 		await deactivatePlugin( 'e2e-tests-gcp-credentials-plugin' );
-		await activatePlugin( 'site-kit-by-google' );
+		await activateSiteKit();
 	} );
 
 	it( 'Should be displayed', async () => {
-		await activatePlugin( 'site-kit-by-google' );
+		await activateSiteKit();
 
 		await page.waitForSelector( '.googlesitekit-activation' );
 
 		await expect( page ).toMatchElement( 'h3.googlesitekit-activation__title', { text: 'Congratulations, the Site Kit plugin is now activated.' } );
 
-		await deactivatePlugin( 'site-kit-by-google' );
+		await deactivateSiteKit();
 	} );
 
 	it( 'Should lead you to the setup wizard', async () => {
-		await activatePlugin( 'site-kit-by-google' );
+		await activateSiteKit();
 
 		await page.waitForSelector( '.googlesitekit-activation' );
 
