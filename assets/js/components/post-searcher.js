10e16b77c6f4ede326a46dc798edf547497ccfbb
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-10-29 17:19:20 +0000

    fix: Do not import stripTags

--- assets/js/components/post-searcher.js
+++ assets/js/components/post-searcher.js
@@ -38,26 +38,50 @@ import {
  */
 import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { stripTags as WPstripTags } from '@wordpress/sanitize';
 
-let stripTags = WPstripTags;
 // Shim wp.sanitize for WordPress < 4.9 when it was introduced.
 // @todo remove this when the plugin drops support for WordPress < 4.9.
-if ( ! stripTags ) {
-	/**
+if ( ! wp.sanitize ) {
+	// Code directly from core.
+	wp.sanitize = {
+
+		/**
 		 * Strip HTML tags.
 		 *
 		 * @param {string} text Text to have the HTML tags striped out of.
 		 *
 		 * @return  Stripped text.
 		 */
-	stripTags = ( text ) => {
-		text = text || '';
+		stripTags( text ) {
+			text = text || '';
 
-		return text
-			.replace( /<!--[\s\S]*?(-->|$)/g, '' )
-			.replace( /<(script|style)[^>]*>[\s\S]*?(<\/\1>|$)/ig, '' )
-			.replace( /<\/?[a-z][\s\S]*?(>|$)/ig, '' );
+			return text
+				.replace( /<!--[\s\S]*?(-->|$)/g, '' )
+				.replace( /<(script|style)[^>]*>[\s\S]*?(<\/\1>|$)/ig, '' )
+				.replace( /<\/?[a-z][\s\S]*?(>|$)/ig, '' );
+		},
+
+		/**
+		 * Strip HTML tags and convert HTML entities.
+		 *
+		 * @param {string} text Text to strip tags and convert HTML entities.
+		 *
+		 * @return Sanitized text. False on failure.
+		 */
+		stripTagsAndEncodeText( text ) {
+			const textarea = document.createElement( 'textarea' );
+			let _text = wp.sanitize.stripTags( text );
+
+			try {
+				textarea.innerHTML = _text;
+				_text = wp.sanitize.stripTags( textarea.value );
+			} catch ( er ) {
+
+				// No-op.
+			}
+
+			return _text;
+		},
 	};
 }
 
