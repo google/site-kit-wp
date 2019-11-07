dd26c457ecbaa9fb02d3155e97c004e96c7fd6d5
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-11-07 13:17:14 +0000

    chore: Add proxy render test

---
+++ tests/e2e/specs/auth-flow-proxy.test.js
@@ -0,0 +1,33 @@
+/**
+ * WordPress dependencies
+ */
+import { activatePlugin, deactivatePlugin, createURL, visitAdminPage } from '@wordpress/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import {
+	deactivateUtilityPlugins,
+	resetSiteKit,
+	setSearchConsoleProperty,
+	useRequestInterception,
+	setAuthToken,
+	setSiteVerification,
+} from '../utils';
+
+describe( 'Site Kit set up flow for the first time', () => {
+	afterEach( async () => {
+		await resetSiteKit();
+	} );
+
+	it( 'renders a splash page for proxy set up when no GCP credentials are provided', async () => {
+		await deactivatePlugin( 'e2e-tests-gcp-credentials-plugin' );
+		await visitAdminPage( 'admin.php', 'page=googlesitekit-splash' );
+
+		await page.waitForSelector( '.googlesitekit-start-setup' );
+
+		await expect( page ).toMatchElement( '.googlesitekit-setup__title', { text: 'The Site Kit plugin is active but requires setup' } );
+		await expect( page ).toMatchElement( '.googlesitekit-start-setup', { text: 'Start setup' } );
+	} );
+} );
+
