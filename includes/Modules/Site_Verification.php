64cced05e289a81b043af80de199b76d1e204651
Evan Mattson <evan.mattson@10up.com>
2019-11-20 14:41:10 +0200

    use filter_input for retrieving query params

--- includes/Modules/Site_Verification.php
+++ includes/Modules/Site_Verification.php
@@ -385,18 +385,19 @@ final class Site_Verification extends Module implements Module_With_Scopes {
 		$authentication = $this->authentication;
 		$auth_client    = $authentication->get_oauth_client();
 
-		if ( ! $auth_client->using_proxy()
-			|| empty( $_GET['googlesitekit_verification_token'] )
-			|| empty( $_GET['googlesitekit_verification_nonce'] )
+		if (
+			! filter_input( INPUT_GET, 'googlesitekit_verification_token' )
+			|| ! filter_input( INPUT_GET, 'googlesitekit_verification_nonce' )
+			|| ! $auth_client->using_proxy()
 		) {
 			return;
 		}
 
-		if ( ! wp_verify_nonce( $_GET['googlesitekit_verification_nonce'], 'googlesitekit_verification' ) ) {
+		if ( ! wp_verify_nonce( filter_input( INPUT_GET, 'googlesitekit_verification_nonce' ), 'googlesitekit_verification' ) ) {
 			wp_die( esc_html__( 'Invalid nonce.', 'google-site-kit' ) );
 		}
 
-		$verification_type = isset( $_GET['googlesitekit_verification_token_type'] ) ? $_GET['googlesitekit_verification_token_type'] : self::VERIFICATION_TYPE_META;
+		$verification_type = filter_input( INPUT_GET, 'googlesitekit_verification_token_type' ) ?: self::VERIFICATION_TYPE_META;
 		switch ( $verification_type ) {
 			case self::VERIFICATION_TYPE_FILE:
 				$authentication->verification_file()->set( $_GET['googlesitekit_verification_token'] );
@@ -405,16 +406,17 @@ final class Site_Verification extends Module implements Module_With_Scopes {
 				$authentication->verification_meta()->set( $_GET['googlesitekit_verification_token'] );
 		}
 
-		$code = isset( $_GET['googlesitekit_code'] ) ? $_GET['googlesitekit_code'] : '';
-
-		// We need to pass the 'missing_verification' error code here so that the URL includes a verification nonce.
 		wp_safe_redirect(
 			add_query_arg(
 				array(
 					'verify'              => 'true',
 					'verification_method' => $verification_type,
 				),
-				$auth_client->get_proxy_setup_url( $code, 'missing_verification' )
+				// We need to pass the 'missing_verification' error code here so that the URL includes a verification nonce.
+				$auth_client->get_proxy_setup_url(
+					filter_input( INPUT_GET, 'googlesitekit_code' ),
+					'missing_verification'
+				)
 			)
 		);
 		exit;
