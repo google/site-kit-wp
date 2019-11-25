cc894c932141e7c6ffd69d90877e77757a5132dc
ThierryA <thierrymuller@google.com>
2019-11-25 16:15:43 +0700

    Make DB cleaner REGEX stricter to avoid conflicts.

--- uninstall.php
+++ uninstall.php
@@ -11,20 +11,25 @@
 namespace Google\Site_Kit;
 
 // Bail if not uninstalling or reseting the plugin.
-if ( ! defined( WP_UNINSTALL_PLUGIN ) && empty( $googlesitekit_reset ) ) {
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) && empty( $googlesitekit_reset ) ) {
 	return;
 }
 
+// Include Context class.
+require_once plugin_dir_path( __FILE__ ) . 'includes/Context.php';
+
 global $wpdb;
 
-$key = '%googlesitekit%';
+$context = new Context( __FILE__ );
+$prefix = 'googlesitekit_%';
+$user_prefix = $context->is_network_mode() ? $prefix : $wpdb->get_blog_prefix() . $prefix;
 
 // Delete options and transients.
 $wpdb->query(
-	$wpdb->prepare( "DELETE FROM $wpdb->usermeta WHERE meta_key LIKE %s OR meta_key LIKE %s", $key, 'sitekit_authentication' )
+	$wpdb->prepare( "DELETE FROM $wpdb->options WHERE option_name LIKE %s OR option_name LIKE %s OR option_name LIKE %s", $prefix, '_transient_' . $prefix, '_transient_timeout_' . $prefix )
 );
 
 // Delete user meta.
 $wpdb->query(
-	$wpdb->prepare( "DELETE FROM $wpdb->options WHERE option_name LIKE %s", $key )
+	$wpdb->prepare( "DELETE FROM $wpdb->usermeta WHERE meta_key LIKE %s", $user_prefix )
 );
