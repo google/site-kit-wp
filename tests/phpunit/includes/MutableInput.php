f9a33dd68c55f287f9eef68b8ca82acfe95a45e4
Evan Mattson <evan.mattson@10up.com>
2019-11-26 17:51:38 +0200

    Add MutableInput Input implementation.

---
+++ tests/phpunit/includes/MutableInput.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Mutable Input class.
+ *
+ * @package   Google\Site_Kit\Tests
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests;
+
+use Google\Site_Kit\Core\Util\Input;
+
+/**
+ * Class MutableInput
+ */
+class MutableInput extends Input {
+	/**
+	 * @inheritDoc
+	 */
+	public function filter( $type, $variable_name, $filter = FILTER_DEFAULT, $options = null ) {
+		$input_map = array(
+			INPUT_GET    => $_GET,
+			INPUT_POST   => $_POST,
+			INPUT_SERVER => $_SERVER,
+			INPUT_COOKIE => $_COOKIE,
+			INPUT_ENV    => $_ENV,
+		);
+
+		if ( ! isset( $input_map[ $type ][ $variable_name ] ) ) {
+			return null;
+		}
+
+		return filter_var( $input_map[ $type ][ $variable_name ], $filter, $options );
+	}
+}
