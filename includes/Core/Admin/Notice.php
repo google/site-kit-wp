b637cea680f08291cd20dd59695c3ce810719291
Ricardo Moraleida <ricardo.moraleida@10up.com>
2019-11-27 11:14:41 -0300

    remove old phpcs ignore comment

--- includes/Core/Admin/Notice.php
+++ includes/Core/Admin/Notice.php
@@ -125,7 +125,7 @@ final class Notice {
 
 		?>
 		<div id="<?php echo esc_attr( 'googlesitekit-notice-' . $this->slug ); ?>" class="<?php echo esc_attr( $class ); ?>">
-			<?php echo wp_kses_post( $content ); /* phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?>
+			<?php echo wp_kses_post( $content ); ?>
 		</div>
 		<?php
 	}
