2fe15f9f98f2fe06efb93a0c0675b65e2334d15e
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-12-12 16:24:53 +0000

    Fix whitespace in PHP.

--- tests/e2e/plugins/module-setup-analytics.php
+++ tests/e2e/plugins/module-setup-analytics.php
@@ -241,7 +241,7 @@ add_action( 'rest_api_init', function () {
 					'internalWebPropertyID' => $request['internalWebPropertyID'],
 					'profileID'             => $request['profileID'] ?: time(),  // fake a new profile ID if empty
 					'useSnippet'            => ! empty( $request['useSnippet'] ),
-					'anonymizeIP'    => isset( $request['anonymizeIP'] ) ? (bool) $request['anonymizeIP'] : true,
+					'anonymizeIP'           => isset( $request['anonymizeIP'] ) ? (bool) $request['anonymizeIP'] : true,
 					'ampClientIDOptIn'      => ! empty( $request['ampClientIDOptIn'] )
 				);
 				update_option( 'googlesitekit_analytics_settings', $option );
