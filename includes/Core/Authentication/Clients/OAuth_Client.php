fcb9edce801560cb7adb312a3f25132b1cd865cd
Felix Arntz <felixarntz@google.com>
2019-12-13 17:20:21 +0100

    Do not check for new refresh token when refreshing access token because there will never be one.

--- includes/Core/Authentication/Clients/OAuth_Client.php
+++ includes/Core/Authentication/Clients/OAuth_Client.php
@@ -306,9 +306,6 @@ final class OAuth_Client {
 			isset( $authentication_token['expires_in'] ) ? $authentication_token['expires_in'] : '',
 			isset( $authentication_token['created'] ) ? $authentication_token['created'] : 0
 		);
-
-		$refresh_token = $this->get_client()->getRefreshToken();
-		$this->set_refresh_token( $refresh_token );
 	}
 
 	/**
