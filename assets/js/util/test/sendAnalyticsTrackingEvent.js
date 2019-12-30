ba07bbe45fbcd07047b6c3b217401cfe37037d07
Felix Arntz <felixarntz@google.com>
2019-12-30 14:07:53 +0100

    Update failing Jest test to use new global.

--- assets/js/util/test/sendAnalyticsTrackingEvent.js
+++ assets/js/util/test/sendAnalyticsTrackingEvent.js
@@ -8,9 +8,14 @@ describe( 'sendAnalyticsTrackingEvent', () => {
 		// eslint-disable-next-line no-undef
 		global.googlesitekitTrackingEnabled = true;
 		// eslint-disable-next-line no-undef
-		global.googlesitekit.setup.isFirstAdmin = true;
+		global._googlesitekitBase = {
+			referenceSiteURL: 'https://www.example.com/',
+			userIDHash: 'a1b2c3',
+			trackingID: 'UA-12345678-1',
+			isFirstAdmin: true,
+		};
 
-		const expected = '{"type":"event","name":"name","sendto":{"event_category":"category","event_label":"label","event_value":"value","dimension1":"","dimension2":"true"}}';
+		const expected = '{"type":"event","name":"name","sendto":{"send_to":"UA-12345678-1","event_category":"category","event_label":"label","event_value":"value","dimension1":"https://www.example.com","dimension2":"true","dimension3":"a1b2c3"}}';
 		const actual = JSON.stringify( sendAnalyticsTrackingEvent( 'category', 'name', 'label', 'value' ) );
 
 		expect( actual ).toStrictEqual( expected );
