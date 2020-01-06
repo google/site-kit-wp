3981b23782710707b22a9e1d3772e8cc895a3e5f
Evan Mattson <evan.mattson@10up.com>
2020-01-06 18:23:09 +0200

    Require manage users scope for tagmanager.

--- includes/Modules/Tag_Manager.php
+++ includes/Modules/Tag_Manager.php
@@ -127,6 +127,7 @@ final class Tag_Manager extends Module implements Module_With_Scopes, Module_Wit
 			'https://www.googleapis.com/auth/tagmanager.readonly',
 			'https://www.googleapis.com/auth/tagmanager.edit.containers',
 			'https://www.googleapis.com/auth/tagmanager.manage.accounts',
+			'https://www.googleapis.com/auth/tagmanager.manage.users',
 		);
 	}
 
--- tests/phpunit/integration/Modules/Tag_ManagerTest.php
+++ tests/phpunit/integration/Modules/Tag_ManagerTest.php
@@ -61,6 +61,7 @@ class Tag_ManagerTest extends TestCase {
 				'https://www.googleapis.com/auth/tagmanager.readonly',
 				'https://www.googleapis.com/auth/tagmanager.edit.containers',
 				'https://www.googleapis.com/auth/tagmanager.manage.accounts',
+				'https://www.googleapis.com/auth/tagmanager.manage.users',
 			),
 			$tagmanager->get_scopes()
 		);
