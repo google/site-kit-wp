05911519f1649922a02dbf360a3df97b9ac27c02
Evan Mattson <evan.mattson@10up.com>
2020-01-23 19:44:48 +0200

    Add googlesitekit_opcache_reset.

--- google-site-kit.php
+++ google-site-kit.php
@@ -75,6 +75,26 @@ function googlesitekit_deactivate_plugin( $network_wide ) {
 
 register_deactivation_hook( __FILE__, 'googlesitekit_deactivate_plugin' );
 
+/**
+ * Resets opcache if possible.
+ *
+ * @access private
+ * @since n.e.x.t
+ */
+function googlesitekit_opcache_reset() {
+	if ( ! empty( ini_get( 'opcache.restrict_api' ) ) && strpos( __FILE__, ini_get( 'opcache.restrict_api' ) ) !== 0 ) {
+		return;
+	}
+
+	// `opcache_reset` is prohibited on the WordPress VIP platform due to memory corruption.
+	if ( function_exists( 'is_wpcom_vip' ) || defined( 'VIP_GO_APP_ENVIRONMENT' ) ) {
+		return;
+	}
+
+	opcache_reset(); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.opcache_opcache_reset
+}
+add_action( 'upgrader_process_complete', 'googlesitekit_opcache_reset' );
+
 if ( version_compare( PHP_VERSION, '5.4.0', '>=' ) ) {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/loader.php';
 }
