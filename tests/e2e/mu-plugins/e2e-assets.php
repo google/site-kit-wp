fa2bdb0c54f4faf50afc64c67b995664968e94dc
Evan Mattson <evan.mattson@10up.com>
2020-01-24 18:44:28 +0200

    Replace wp-api-fetch utility with e2e-assets mu-plugin.

---
+++ tests/e2e/mu-plugins/e2e-assets.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: E2E Assets
+ * Description: Enqueues assets needed for E2E tests.
+ *
+ * @package   Google\Site_Kit
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+add_action(
+	'init',
+	function () {
+		if ( ! defined( 'GOOGLESITEKIT_PLUGIN_MAIN_FILE' ) ) {
+			return;
+		}
+
+		wp_enqueue_script( 'googlesitekit-e2e', plugins_url( 'dist/assets/js/e2e-utilities.js', GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+
+		wp_add_inline_script(
+			'googlesitekit-e2e',
+			implode(
+				"\n",
+				array(
+					sprintf( 'window._apiFetchRootURL = "%s";', esc_url_raw( get_rest_url() ) ),
+					sprintf(
+						'window._apiFetchNonceMiddleware = "%s";',
+						( wp_installing() && ! is_multisite() ) ? '' : wp_create_nonce( 'wp_rest' )
+					),
+					sprintf( 'window._apiFetchNonceEndpoint = "%s";', admin_url( 'admin-ajax.php?action=rest-nonce' ) ),
+				)
+			),
+			'before'
+		);
+	}
+);
