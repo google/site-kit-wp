a22cef3288eea720a51cf99cc19ebb30c9da1ab7
Evan Mattson <evan.mattson@10up.com>
2020-01-28 13:13:31 +0200

    Update Verification as User_Setting.

--- includes/Core/Authentication/Verification.php
+++ includes/Core/Authentication/Verification.php
@@ -10,7 +10,7 @@
 
 namespace Google\Site_Kit\Core\Authentication;
 
-use Google\Site_Kit\Core\Storage\User_Options;
+use Google\Site_Kit\Core\Storage\User_Setting;
 
 /**
  * Class representing the status of whether a user is verified as an owner of the site.
@@ -19,42 +19,12 @@ use Google\Site_Kit\Core\Storage\User_Options;
  * @access private
  * @ignore
  */
-final class Verification {
+final class Verification extends User_Setting {
 
 	/**
 	 * User option key.
 	 */
 	const OPTION = 'googlesitekit_site_verified_meta';
-
-	/**
-	 * User_Options object.
-	 *
-	 * @since 1.0.0
-	 * @var User_Options
-	 */
-	private $user_options;
-
-	/**
-	 * Constructor.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param User_Options $user_options User Options instance.
-	 */
-	public function __construct( User_Options $user_options ) {
-		$this->user_options = $user_options;
-	}
-
-	/**
-	 * Retrieves the user verification tag.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return bool True if the user is verified, or false otherwise.
-	 */
-	public function get() {
-		return (bool) $this->user_options->get( self::OPTION );
-	}
 
 	/**
 	 * Flags the user as verified or unverified.
@@ -66,21 +36,9 @@ final class Verification {
 	 */
 	public function set( $verified ) {
 		if ( ! $verified ) {
-			return $this->user_options->delete( self::OPTION );
+			return $this->delete();
 		}
 
-		return $this->user_options->set( self::OPTION, 'verified' );
-	}
-
-	/**
-	 * Checks whether the user is verified.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return bool True if verified, false otherwise.
-	 */
-	public function has() {
-		// Kind of redundant, but here for consistency.
-		return $this->get();
+		return parent::set( 'verified' );
 	}
 }
