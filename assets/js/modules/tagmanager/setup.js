8007948b9e484827fa115d99b0b4a634f2468453
Evan Mattson <evan.mattson@10up.com>
2020-02-03 19:14:30 +0200

    Add new create account dialog when selected.

--- assets/js/modules/tagmanager/setup.js
+++ assets/js/modules/tagmanager/setup.js
@@ -440,6 +440,14 @@ class TagmanagerSetup extends Component {
 			return this.renderCreateAccount();
 		}
 
+		if ( '-1' === selectedAccount ) {
+			return <Fragment>
+				<p>{ __( 'To create a new account, click the button below which will open the Google Tag Manager account creation screen in a new window.', 'google-site-kit' ) }</p>
+				<p>{ __( 'Once completed, click the link below to re-fetch your accounts to continue.', 'google-site-kit' ) }</p>
+				{ this.renderCreateAccount() }
+			</Fragment>;
+		}
+
 		return (
 			<Fragment>
 				{ hasExistingTag && (
