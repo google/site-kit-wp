1cb42b21406f906308e2a8a1e4eec9ca674bf81f
Evan Mattson <evan.mattson@10up.com>
2020-02-04 11:05:45 +0200

    Update reference to special account value.

--- assets/js/modules/tagmanager/setup.js
+++ assets/js/modules/tagmanager/setup.js
@@ -466,7 +466,7 @@ class TagmanagerSetup extends Component {
 			return this.renderCreateAccount();
 		}
 
-		if ( '-1' === selectedAccount ) {
+		if ( ACCOUNT_CREATE === selectedAccount ) {
 			return <Fragment>
 				<p>{ __( 'To create a new account, click the button below which will open the Google Tag Manager account creation screen in a new window.', 'google-site-kit' ) }</p>
 				<p>{ __( 'Once completed, click the link below to re-fetch your accounts to continue.', 'google-site-kit' ) }</p>
