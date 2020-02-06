890776e1241da44a3e08ae0c727404a3d64aed06
Evan Mattson <evan.mattson@10up.com>
2020-02-06 12:35:42 +0200

    Add and update tests for tracking functions.

--- assets/js/util/tracking/index.test.js
+++ assets/js/util/tracking/index.test.js
@@ -1,10 +1,75 @@
 /**
  * Internal dependencies
  */
-import { bootstrapTracking, DATA_LAYER, trackEvent } from './index.private';
+import {
+	bootstrapTracking,
+	DATA_LAYER,
+	disableTracking,
+	enableTracking,
+	isTrackingEnabled,
+	trackEvent,
+} from './index.private';
+
+/**
+ * Note: The global document object is provided by Jest using JSDOM.
+ * This is necessary for `querySelector` and `createElement` calls to not raise errors.
+ */
+
+describe( 'bootstrapTracking', () => {
+	it( 'initializes tracking based on user preference', () => {
+		const _global = {
+			_googlesitekitBase: { trackingEnabled: false },
+			document: global.document,
+			[ DATA_LAYER ]: {
+				push: jest.fn(),
+			},
+		};
+		bootstrapTracking( _global );
+
+		expect( isTrackingEnabled() ).toStrictEqual( false );
+	} );
+} );
+
+describe( 'disableTracking and isTrackingEnabled', () => {
+	it( 'does not mutate global tracking settings when toggling active state', () => {
+		const _global = {
+			_googlesitekitBase: { trackingEnabled: true },
+			document: global.document,
+			[ DATA_LAYER ]: {
+				push: jest.fn(),
+			},
+		};
+		bootstrapTracking( _global );
+
+		expect( isTrackingEnabled() ).toStrictEqual( true );
+
+		disableTracking();
+
+		expect( isTrackingEnabled() ).toStrictEqual( false );
+	} );
+} );
+
+describe( 'enableTracking and isTrackingEnabled', () => {
+	it( 'does not mutate global tracking settings when toggling active state', () => {
+		const _global = {
+			_googlesitekitBase: { trackingEnabled: false },
+			document: global.document,
+			[ DATA_LAYER ]: {
+				push: jest.fn(),
+			},
+		};
+		bootstrapTracking( _global );
+
+		expect( isTrackingEnabled() ).toStrictEqual( false );
+
+		enableTracking();
+
+		expect( isTrackingEnabled() ).toStrictEqual( true );
+	} );
+} );
 
 describe( 'trackEvent', () => {
-	it( 'sends a tracking event', () => {
+	it( 'adds a tracking event to the dataLayer', () => {
 		const _googlesitekitBase = {
 			referenceSiteURL: 'https://www.example.com/',
 			userIDHash: 'a1b2c3',
@@ -12,17 +77,15 @@ describe( 'trackEvent', () => {
 			isFirstAdmin: true,
 			trackingEnabled: true,
 		};
-
-		bootstrapTracking( { _googlesitekitBase } );
-
 		const _global = {
 			_googlesitekitBase,
 			[ DATA_LAYER ]: {
 				push: jest.fn(),
 			},
+			document: global.document,
 		};
-
 		bootstrapTracking( _global );
+
 		trackEvent( 'category', 'name', 'label', 'value' );
 
 		expect( _global[ DATA_LAYER ].push ).toHaveBeenCalledWith( [
