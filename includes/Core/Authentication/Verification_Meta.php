79c98284eee2d652e3d81a10ae24dea51cb4183c
Evan Mattson <evan.mattson@10up.com>
2020-02-18 12:29:29 +0200

    Remove set override in Verification_Meta.

--- includes/Core/Authentication/Verification_Meta.php
+++ includes/Core/Authentication/Verification_Meta.php
@@ -25,22 +25,4 @@ final class Verification_Meta extends User_Setting {
 	 * User option key.
 	 */
 	const OPTION = 'googlesitekit_site_verification_meta';
-
-	/**
-	 * Sets the value of the setting with the given value.
-	 *
-	 * @since n.e.x.t
-	 *
-	 * @param mixed $value Setting value. Must be serializable if non-scalar.
-	 *
-	 * @return bool True on success, false on failure.
-	 */
-	public function set( $value ) {
-		/**
-		 * Triggers the meta tag cache to be cleared.
-		 */
-		do_action( 'googlesitekit_invalidate_verification_meta_cache' );
-
-		return parent::set( $value );
-	}
 }
