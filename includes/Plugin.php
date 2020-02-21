907c7e1763430788add5da3c78d040f9287e726d
Evan Mattson <evan.mattson@10up.com>
2020-02-21 22:38:25 +0200

    Bootstrap Debug_Data in Plugin.

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -179,6 +179,7 @@ final class Plugin {
 		( new Core\Util\Activation( $this->context, $options, $assets ) )->register();
 		( new Core\Util\Migration_1_3_0( $this->context, $options ) )->register();
 		( new Core\Util\Reset( $this->context ) )->register();
+		( new Core\Util\Debug_Data( $this->context ) )->register();
 		( new Core\Util\Developer_Plugin_Installer( $this->context ) )->register();
 	}
 
