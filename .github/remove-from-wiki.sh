768fa516441cd10ebdebbbd601d39474edf448e3
Adam Silverstein <adamjs@google.com>
2020-02-24 12:58:22 -0700

    clean up shell scripts

--- .github/remove-from-wiki.sh
+++ .github/remove-from-wiki.sh
@@ -1,48 +1,8 @@
 #!/bin/bash
 
-function debug() {
-    echo "::debug file=${BASH_SOURCE[0]},line=${BASH_LINENO[0]}::$1"
-}
-
-function warning() {
-    echo "::warning file=${BASH_SOURCE[0]},line=${BASH_LINENO[0]}::$1"
-}
-
-function error() {
-    echo "::error file=${BASH_SOURCE[0]},line=${BASH_LINENO[0]}::$1"
-}
-
-function add_mask() {
-    echo "::add-mask::$1"
-}
-
-debug "Wiki cleanup"
-
-if [ -z "$GITHUB_ACTOR" ]; then
-    error "GITHUB_ACTOR environment variable is not set"
-    exit 1
-fi
-
-if [ -z "$GITHUB_REPOSITORY" ]; then
-    error "GITHUB_REPOSITORY environment variable is not set"
-    exit 1
-fi
-
-if [ -z "$GITHUB_PERSONAL_ACCESS_TOKEN" ]; then
-    error "GITHUB_PERSONAL_ACCESS_TOKEN environment variable is not set"
-    exit 1
-fi
-
-add_mask "${GITHUB_PERSONAL_ACCESS_TOKEN}"
-
-if [ -z "${WIKI_COMMIT_MESSAGE:-}" ]; then
-    debug "WIKI_COMMIT_MESSAGE not set, using default"
-    WIKI_COMMIT_MESSAGE='Automatically publish wiki'
-fi
-
 GIT_REPOSITORY_URL="https://${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git"
 
-debug "Checking out wiki repository"
+echo "Checking out wiki repository"
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 (
     cd "$tmp_dir" || exit 1
@@ -52,10 +12,9 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
     git pull "$GIT_REPOSITORY_URL"
 )
 
-debug "Removing $1"
 rm -Rf "$tmp_dir/$1"
 
-debug "Committing and pushing changes"
+echo "Removing build files from $1"
 (
     cd "$tmp_dir" || exit 1
     git commit -m "$WIKI_COMMIT_MESSAGE"
