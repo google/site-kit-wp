e86765c73585b4c3bb56784b84bbc5cb401d0c6b
Evan Mattson <evan.mattson@10up.com>
2020-02-25 20:08:52 +0200

    Add testSiteNotification e2e util.

---
+++ tests/e2e/utils/test-site-notification.js
@@ -0,0 +1,7 @@
+export const testSiteNotification = {
+	slug: 'test-notification-slug',
+	title: 'Test notification title',
+	content: 'Test notification content',
+	dismissible: true,
+	dismiss_label: 'test dismiss site notification',
+};
