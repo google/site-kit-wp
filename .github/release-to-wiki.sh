30ed8e456cb385525e32132d7c04ed1973b2fc45
Adam Silverstein <adamjs@google.com>
2020-02-28 10:56:38 -0700

    Correct git commands.

--- .github/release-to-wiki.sh
+++ .github/release-to-wiki.sh
@@ -2,15 +2,17 @@
 
 GIT_REPOSITORY_URL="https://${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git"
 tmp_dir="/tmp/build/${GIT_REF}"
+mkdir -p "$tmp_dir"
 (
     cd "$tmp_dir" || exit 1
-    git clone "$GIT_REPOSITORY_URL"
+    git clone "$GIT_REPOSITORY_URL" .
     git config user.name "$GITHUB_ACTOR"
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 )
 
 mkdir -p "$tmp_dir/${GIT_REF}"
-cp "${GIT_REF}/*.zip" "$tmp_dir/${GIT_REF}"
+ls "${GIT_REF}"
+cp "${GIT_REF}/"* "$tmp_dir/${GIT_REF}"
 
 echo "Publishing build files for ${GIT_REF}"
 (
--- .github/remove-from-wiki.sh
+++ .github/remove-from-wiki.sh
@@ -4,9 +4,10 @@ GIT_REPOSITORY_URL="https://${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_R
 
 echo "Checking out wiki repository"
 tmp_dir="/tmp/build/${GIT_REF}"
+mkdir -p "$tmp_dir"
 (
     cd "$tmp_dir" || exit 1
-    git clone "$GIT_REPOSITORY_URL"
+    git clone "$GIT_REPOSITORY_URL" .
     git config user.name "$GITHUB_ACTOR"
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 )
