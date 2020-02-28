72c853e3d09f20b7fb83fccd876e8d3b9c7a2aa3
Adam Silverstein <adamjs@google.com>
2020-02-28 10:49:27 -0700

    Remove action: pass ref to bash script via env.

--- .github/remove-from-wiki.sh
+++ .github/remove-from-wiki.sh
@@ -3,18 +3,17 @@
 GIT_REPOSITORY_URL="https://${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git"
 
 echo "Checking out wiki repository"
-tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+tmp_dir="/tmp/build/${GIT_REF}"
 (
     cd "$tmp_dir" || exit 1
-    git init
+    git clone "$GIT_REPOSITORY_URL"
     git config user.name "$GITHUB_ACTOR"
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-    git pull "$GIT_REPOSITORY_URL"
 )
 
-rm -Rf "$tmp_dir/$1"
+rm -Rf "$tmp_dir/${GIT_REF}"
 
-echo "Removing build files from $1"
+echo "Removing build files from ${GIT_REF}"
 (
     cd "$tmp_dir" || exit 1
     git commit -m "$WIKI_COMMIT_MESSAGE"
