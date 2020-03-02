40138f244c80cff406ca2a395be3d587a3929dd5
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-03-02 20:59:02 +0000

    Add collect to data exports.

--- assets/js/googlesitekit-data.js
+++ assets/js/googlesitekit-data.js
@@ -7,18 +7,18 @@
  */
 import SiteKitRegistry from 'assets/js/googlesitekit/data';
 import { registerSiteKitStores } from 'assets/js/googlesitekit/data/store';
-import { collectReducers } from 'assets/js/googlesitekit/data/utils';
+import { collect, collectReducers } from 'assets/js/googlesitekit/data/utils';
 
 if ( typeof global.googlesitekit === 'undefined' ) {
 	global.googlesitekit = {};
 }
 
 if ( typeof global.googlesitekit.data === 'undefined' ) {
-	global.googlesitekit.data = { collectReducers, registerSiteKitStores, registry: SiteKitRegistry };
+	global.googlesitekit.data = { collect, collectReducers, registerSiteKitStores, registry: SiteKitRegistry };
 	// Register all stores on the default Site Kit registry.
 	registerSiteKitStores( global.googlesitekit.data.registry );
 }
 
-export { collectReducers };
+export { collect, collectReducers };
 
 export default SiteKitRegistry;
