618b7a1a2fc061560714a6403e1ca0f41f9ce937
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-03-04 20:41:39 +0000

    Register datastore in non-entrypoint file.

--- assets/js/googlesitekit-datastore-site.js
+++ assets/js/googlesitekit-datastore-site.js
@@ -5,8 +5,4 @@
 /**
  * Internal dependencies
  */
-import Data from 'googlesitekit-data';
-import store, { STORE_NAME } from 'assets/js/googlesitekit/datastore/site';
-
-// Register this store on the global registry.
-Data.registerStore( STORE_NAME, store );
+import 'assets/js/googlesitekit/datastore/site/register';
