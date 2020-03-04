600116181e55749244d7e9d372cc55c1965a1226
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-03-04 20:12:41 +0000

    Change registry variable name.

--- assets/js/googlesitekit/data/index.js
+++ assets/js/googlesitekit/data/index.js
@@ -19,16 +19,16 @@ import {
 	collectState,
 } from 'assets/js/googlesitekit/data/utils';
 
-const siteKitRegistry = createRegistry();
+const Data = createRegistry();
 
 // Attach some of our utility functions to the registry so third-party
 // developers can use them.
-siteKitRegistry.collectActions = collectActions;
-siteKitRegistry.collectControls = collectControls;
-siteKitRegistry.collectReducers = collectReducers;
-siteKitRegistry.collectResolvers = collectResolvers;
-siteKitRegistry.collectSelectors = collectSelectors;
-siteKitRegistry.collectState = collectState;
-siteKitRegistry.collectReducers = collectReducers;
+Data.collectActions = collectActions;
+Data.collectControls = collectControls;
+Data.collectReducers = collectReducers;
+Data.collectResolvers = collectResolvers;
+Data.collectSelectors = collectSelectors;
+Data.collectState = collectState;
+Data.collectReducers = collectReducers;
 
-export default siteKitRegistry;
+export default Data;
