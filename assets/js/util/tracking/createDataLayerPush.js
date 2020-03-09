779061977c9b0a346b9a403dede0b0e1e7f72d10
Evan Mattson <evan.mattson@10up.com>
2020-03-09 19:12:57 +0200

    Fix usage of @link.

--- assets/js/util/tracking/createDataLayerPush.js
+++ assets/js/util/tracking/createDataLayerPush.js
@@ -16,7 +16,7 @@ export default function createDataLayerPush( target ) {
 	 * Must push an instance of Arguments to the target.
 	 * Using an ES6 spread operator (i.e. `...args`) will cause tracking events to _silently_ fail.
 	 *
-	 * @link https://github.com/google/site-kit-wp/issues/1181
+	 * @see {@link https://github.com/google/site-kit-wp/issues/1181}
 	 */
 	return function dataLayerPush() {
 		target[ DATA_LAYER ] = target[ DATA_LAYER ] || [];
