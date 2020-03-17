d3aa0248d774880f7759af0d00bed78ad14b47c1
Felix Arntz <felixarntz@google.com>
2020-03-17 11:46:17 -0700

    Ensure Module and Module_With_Settings are implemented by parameter passed to Modules::update_module_settings.

--- includes/Core/Modules/Modules.php
+++ includes/Core/Modules/Modules.php
@@ -585,10 +585,10 @@ final class Modules {
 							} catch ( \Exception $e ) {
 								return new WP_Error( 'invalid_module_slug', __( 'Invalid module slug.', 'google-site-kit' ), array( 'status' => 404 ) );
 							}
+
 							if ( ! $module instanceof Module_With_Settings ) {
 								return new WP_Error( 'invalid_module_slug', __( 'Module does not support settings.', 'google-site-kit' ), array( 'status' => 400 ) );
 							}
-
 							return new WP_REST_Response( $module->get_settings()->get() );
 						},
 						'permission_callback' => $can_manage_options,
@@ -601,9 +601,6 @@ final class Modules {
 								$module = $this->get_module( $slug );
 							} catch ( \Exception $e ) {
 								return new WP_Error( 'invalid_module_slug', __( 'Invalid module slug.', 'google-site-kit' ), array( 'status' => 404 ) );
-							}
-							if ( ! $module instanceof Module_With_Settings ) {
-								return new WP_Error( 'invalid_module_slug', __( 'Module does not support settings.', 'google-site-kit' ), array( 'status' => 400 ) );
 							}
 
 							$data   = isset( $request['data'] ) ? (array) $request['data'] : array();
@@ -715,6 +712,10 @@ final class Modules {
 	 * @return bool|WP_Error True on success, or an error object on failure.
 	 */
 	private function update_module_settings( Module $module, array $data ) {
+		if ( ! $module instanceof Module_With_Settings ) {
+			return new WP_Error( 'invalid_module_slug', __( 'Module does not support settings.', 'google-site-kit' ), array( 'status' => 400 ) );
+		}
+
 		if ( in_array( 'settings', $module->get_datapoints(), true ) ) {
 			$result = $module->set_data( 'settings', $data );
 			if ( is_wp_error( $result ) && $result->get_error_code() !== 'invalid_datapoint' ) {
