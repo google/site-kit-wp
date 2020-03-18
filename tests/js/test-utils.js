8767f2b88c2de56a3d20a82cdad7bd96fcbfe72d
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-03-18 20:37:25 +0000

    Add a Provider/registry to test render method.

--- tests/js/test-utils.js
+++ tests/js/test-utils.js
@@ -7,27 +7,28 @@ import PropTypes from 'prop-types';
 /**
  * WordPress dependencies
  */
-import { Fragment } from '@wordpress/element';
+import { RegistryProvider } from '@wordpress/data';
 
-const TestProviders = ( { children } ) => {
-	return (
-		<Fragment>
-			{ children }
-		</Fragment>
-	);
-};
+/**
+ * Internal dependencies
+ */
+import { createTestRegistry } from 'tests/js/utils';
 
-TestProviders.defaultProps = {
-	children: undefined,
-};
-
-TestProviders.propTypes = {
-	children: PropTypes.node,
-};
-
-// custom render with the test provider component
-const customRender = ( ui, options ) => {
-	return render( ui, { wrapper: TestProviders, ...options } );
+// Override `@testing-library/react`'s render method with one that includes
+// our data store.
+const customRender = ( children, options = {} ) => {
+	const {
+		registry = createTestRegistry(),
+		...renderOptions
+	} = options;
+	return {
+		...render( (
+			<RegistryProvider value={ registry }>
+				{ children }
+			</RegistryProvider>
+		), renderOptions ),
+		registry,
+	};
 };
 
 // Export our own test utils from this file.
