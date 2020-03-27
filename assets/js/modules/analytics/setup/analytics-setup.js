e245875a1f38d4ef11b2f24bca1a8944b9ef9b50
Evan Mattson <evan.mattson@10up.com>
2020-03-27 12:32:44 +0200

    Scaffold initial Analytics setup + form components.

---
+++ assets/js/modules/analytics/setup/analytics-setup.js
@@ -0,0 +1,47 @@
+/**
+ * AnalyticsSetup component.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { _x } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import AnalyticsSetupForm from './setup-form';
+import { SvgIcon } from '../../../util';
+
+export default function AnalyticsSetup() {
+	return (
+		<div className="googlesitekit-setup-module googlesitekit-setup-module--analytics">
+
+			<div className="googlesitekit-setup-module__logo">
+				<SvgIcon id="analytics" width="33" height="33" />
+			</div>
+
+			<h2 className="googlesitekit-heading-3 googlesitekit-setup-module__title">
+				{ _x( 'Analytics', 'Service name', 'google-site-kit' ) }
+			</h2>
+
+			{ /* { this.renderErrorOrNotice() } */ }
+
+			<AnalyticsSetupForm />
+		</div>
+	);
+}
---
+++ assets/js/modules/analytics/setup/setup-form.js
@@ -0,0 +1,54 @@
+/**
+ * Analytics Setup form.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import AccountSelect from '../common/account-select';
+import PropertySelect from '../common/property-select';
+import ProfileSelect from '../common/profile-select';
+import { STORE_NAME } from '../datastore';
+
+export default function AnalyticsSetupForm() {
+	const accounts = useSelect( ( select ) => select( STORE_NAME ).getAccounts() ) || [];
+	const hasExistingTag = useSelect( ( select ) => select( STORE_NAME ).hasExistingTag() );
+
+	return (
+		<div className="googlesitekit-analytics-setup__form">
+			{ ( !! accounts.length && ! hasExistingTag ) && (
+				<p>
+					{ __( 'Please select the account information below. You can change this view later in your settings.', 'google-site-kit' ) }
+				</p>
+			) }
+
+			<div className="googlesitekit-setup-module__inputs">
+				<AccountSelect />
+
+				<PropertySelect />
+
+				<ProfileSelect />
+			</div>
+		</div>
+	);
+}
