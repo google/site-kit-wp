2c68b578bf0929ce0f1509f415186d81284a5d4c
Ryan Welcher <ryan.welcher@10up.com>
2020-03-30 14:52:40 -0400

    Adds CleanWepbackPlugin to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ const path = require( 'path' );
  */
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebpackBar = require( 'webpackbar' );
 const { ProvidePlugin } = require( 'webpack' );
 
@@ -127,6 +128,10 @@ const webpackConfig = ( mode ) => {
 					name: 'Module Entry Points',
 					color: '#fbbc05',
 				} ),
+				new CleanWebpackPlugin( {
+					// Stop this build from removing unrelated JS and CSS files.
+					cleanOnceBeforeBuildPatterns: [], 
+				} ),
 			],
 			optimization: {
 				minimizer: [
@@ -192,6 +197,10 @@ const webpackConfig = ( mode ) => {
 					name: 'Plugin CSS',
 					color: '#4285f4',
 				} ),
+				new CleanWebpackPlugin( {
+					// Stop this build from removing unrelated JS and CSS files.
+					cleanOnceBeforeBuildPatterns: [],
+				} ),
 			],
 		},
 	];
@@ -216,6 +225,10 @@ const testBundle = () => {
 			new WebpackBar( {
 				name: 'Test files',
 				color: '#34a853',
+			} ),
+			new CleanWebpackPlugin( {
+				// Stop this build from removing unrelated JS and CSS files.
+				cleanOnceBeforeBuildPatterns: [],
 			} ),
 		],
 		externals,
