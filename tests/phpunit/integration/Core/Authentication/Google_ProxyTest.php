d6153a1544428ae872707ec2797b19ba43e6ac29
Ryan Welcher <ryan.welcher@10up.com>
2020-03-30 09:17:41 -0400

    Adds new site fields to unit tests.

--- tests/phpunit/integration/Core/Authentication/Google_ProxyTest.php
+++ tests/phpunit/integration/Core/Authentication/Google_ProxyTest.php
@@ -26,11 +26,12 @@ class Google_ProxyTest extends TestCase {
 
 		$this->assertEqualSetsWithIndex(
 			array(
-				'url'        => home_url(),
-				'action_uri' => admin_url( 'index.php' ),
-				'name'       => get_bloginfo( 'name' ),
-				'return_uri' => $context->admin_url( 'splash' ),
-				'admin_root' => parse_url( admin_url( '/' ), PHP_URL_PATH ),
+				'url'                    => home_url(),
+				'action_uri'             => admin_url( 'index.php' ),
+				'name'                   => get_bloginfo( 'name' ),
+				'return_uri'             => $context->admin_url( 'splash' ),
+				'admin_root'             => parse_url( admin_url( '/' ), PHP_URL_PATH ),
+				'analytics_redirect_uri' => add_query_arg( 'gatoscallback', 1, home_url() ),
 			),
 			$google_proxy->get_site_fields()
 		);
@@ -76,6 +77,7 @@ class Google_ProxyTest extends TestCase {
 				'admin_root',
 				'return_uri',
 				'action_uri',
+				'analytics_redirect_uri',
 			),
 			array_keys( $args['body'] )
 		);
