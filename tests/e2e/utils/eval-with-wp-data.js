6e0fc93b97fc23d651775fd70e77add9cd247151
Evan Mattson <evan.mattson@10up.com>
2020-04-01 13:32:02 +0300

    Document safety of evalWithWPData.

--- tests/e2e/utils/eval-with-wp-data.js
+++ tests/e2e/utils/eval-with-wp-data.js
@@ -6,6 +6,9 @@ import { getWPVersion } from './get-wp-version';
 /**
  * Evaluates a function if wp.data is available.
  *
+ * For older versions of WP, the callback will not be invoked
+ * making this safe to use in all environments.
+ *
  * @param {Function} callback Function to call in browser context, if wp.data is available.
  * @param {...*} args Arguments to pass to the callback in browser context.
  */
