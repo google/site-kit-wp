eb519a2981691bab0e8009aa5f469ab028028580
Evan Mattson <evan.mattson@10up.com>
2020-04-10 12:41:35 +0300

    Remove legacy Analytics from VRT stories.

--- stories/settings.stories.js
+++ stories/settings.stories.js
@@ -14,9 +14,6 @@ import { addFilter } from '@wordpress/hooks';
 import SettingsModules from '../assets/js/components/settings/settings-modules';
 import Layout from '../assets/js/components/layout/layout';
 import { googlesitekit as settingsData } from '../.storybook/data/wp-admin-admin.php-page=googlesitekit-settings-googlesitekit.js';
-import { fillFilterWithComponent } from '../assets/js/util';
-import AnalyticsSetup from '../assets/js/modules/analytics/setup';
-import SearchConsoleSettingStatus from '../assets/js/modules/search-console/settings/search-console-settings-status';
 import SettingsAdmin from '../assets/js/components/settings/settings-admin';
 import Tab from '@material/react-tab';
 import TabBar from '@material/react-tab-bar';
@@ -24,135 +21,6 @@ import TabBar from '@material/react-tab-bar';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-
-const setupSettings = () => {
-	global.googlesitekit.modules.analytics[ 'accounts-properties-profiles' ] = {
-		accounts: [
-			{
-				created: '2011-03-25T21:41:26.980Z',
-				id: '12345678',
-				kind: 'analytics#account',
-				name: '10up',
-				selfLink: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678',
-				starred: null,
-				updated: '2015-08-10T19:09:14.708Z',
-				permissions: {
-					effective: [],
-				},
-				childLink: {
-					href: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678/webproperties',
-					type: 'analytics#webproperties',
-				},
-			},
-			{
-				created: '2018-09-17T18:32:09.258Z',
-				id: '125893658',
-				kind: 'analytics#account',
-				name: 'Test',
-				selfLink: 'https://www.googleapis.com/analytics/v3/management/accounts/125893658',
-				starred: null,
-				updated: '2018-09-17T18:32:09.258Z',
-				permissions: {
-					effective: [
-						'COLLABORATE',
-						'EDIT',
-						'MANAGE_USERS',
-						'READ_AND_ANALYZE',
-					],
-				},
-				childLink: {
-					href: 'https://www.googleapis.com/analytics/v3/management/accounts/125893658/webproperties',
-					type: 'analytics#webproperties',
-				},
-			},
-			{
-				id: '-1',
-				name: 'Setup a New Account',
-			},
-		],
-		properties: [
-			{
-				accountId: '12345678',
-				created: '2016-12-06T14:48:54.188Z',
-				dataRetentionResetOnNewActivity: true,
-				dataRetentionTtl: 'MONTHS_26',
-				defaultProfileId: '12345678',
-				id: 'UA-12345678-3',
-				industryVertical: 'INTERNET_AND_TELECOM',
-				internalWebPropertyId: '12345678',
-				kind: 'analytics#webproperty',
-				level: 'STANDARD',
-				name: 'testwebsite.com',
-				profileCount: 1,
-				selfLink: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678/webproperties/UA-12345678-3',
-				starred: null,
-				updated: '2016-12-06T14:53:41.965Z',
-				websiteUrl: 'https://www.testwebsite.com',
-				permissions: {
-					effective: [
-						'READ_AND_ANALYZE',
-					],
-				},
-				parentLink: {
-					href: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678',
-					type: 'analytics#account',
-				},
-				childLink: {
-					href: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678/webproperties/UA-12345678-3/profiles',
-					type: 'analytics#profiles',
-				},
-			},
-			{
-				id: 0,
-				name: 'Setup a New Property',
-			},
-		],
-		profiles: [
-			{
-				accountId: '12345678',
-				botFilteringEnabled: false,
-				created: '2016-12-06T14:48:54.188Z',
-				currency: 'USD',
-				defaultPage: null,
-				eCommerceTracking: false,
-				enhancedECommerceTracking: null,
-				excludeQueryParameters: null,
-				id: '12345678',
-				internalWebPropertyId: '12345678',
-				kind: 'analytics#profile',
-				name: 'All Web Site Data',
-				selfLink: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678/webproperties/UA-12345678-3/profiles/12345678',
-				siteSearchCategoryParameters: null,
-				siteSearchQueryParameters: 's',
-				starred: null,
-				stripSiteSearchCategoryParameters: null,
-				stripSiteSearchQueryParameters: false,
-				timezone: 'America/Los_Angeles',
-				type: 'WEB',
-				updated: '2019-01-25T20:15:27.426Z',
-				webPropertyId: 'UA-12345678-3',
-				websiteUrl: 'https://www.testwebsite.com',
-				permissions: {
-					effective: [
-						'READ_AND_ANALYZE',
-					],
-				},
-				parentLink: {
-					href: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678/webproperties/UA-12345678-3',
-					type: 'analytics#webproperty',
-				},
-				childLink: {
-					href: 'https://www.googleapis.com/analytics/v3/management/accounts/12345678/webproperties/UA-12345678-3/profiles/12345678/goals',
-					type: 'analytics#goals',
-				},
-			},
-			{
-				id: 0,
-				name: 'Setup a New Profile',
-			},
-		],
-	};
-};
 
 /**
  * Add components to the settings page.
@@ -187,20 +55,6 @@ storiesOf( 'Settings', module )
 		global.googlesitekit.setupComplete = true;
 		global.googlesitekit.modules.analytics.setupComplete = true;
 		global.googlesitekit.modules[ 'search-console' ].setupComplete = true;
-		// Load the datacache with data.
-		setTimeout( () => {
-			setupSettings();
-			addFilter( 'googlesitekit.ModuleSettingsDetails-analytics',
-				'googlesitekit.AnalyticsModuleSettingsDetails',
-				fillFilterWithComponent( AnalyticsSetup, {
-					onSettingsPage: true,
-				} ) );
-			addFilter( 'googlesitekit.ModuleSettingsDetails-search-console',
-				'googlesitekit.SearchConsoleModuleSettingsDetails',
-				fillFilterWithComponent( SearchConsoleSettingStatus, {
-					onSettingsPage: true,
-				} ) );
-		}, 2500 );
 
 		return (
 			<div className="mdc-layout-grid__inner">
@@ -210,39 +64,6 @@ storiesOf( 'Settings', module )
 	}, {
 		options: {
 			delay: 100, // Wait for screen to render.
-		},
-	} )
-	.add( 'VRT: Editing Settings Module', () => {
-		global.googlesitekit = settingsData;
-		global.googlesitekit.setupComplete = true;
-		global.googlesitekit.modules.analytics.setupComplete = true;
-		// Load the datacache with data.
-		setTimeout( () => {
-			setupSettings();
-			addFilter( 'googlesitekit.ModuleSettingsDetails-analytics',
-				'googlesitekit.AnalyticsModuleSettingsDetails',
-				fillFilterWithComponent( AnalyticsSetup, {
-					onSettingsPage: true,
-				} ) );
-			addFilter( 'googlesitekit.ModuleSettingsDetails-search-console',
-				'googlesitekit.SearchConsoleModuleSettingsDetails',
-				fillFilterWithComponent( SearchConsoleSettingStatus, {
-					onSettingsPage: true,
-				} ) );
-		}, 2500 );
-
-		return (
-			<div className="mdc-layout-grid__inner">
-				<SettingsModules activeTab={ 0 } />
-			</div>
-		);
-	}, {
-		options: {
-			delay: 2000, // Sometimes the click doesn't work, waiting for everything to load.
-			clickSelectors: [ '#googlesitekit-settings-module__header--analytics', '.googlesitekit-settings-module__edit-button' ],
-			hoverSelector: '.googlesitekit-settings-module__title', // Move mouse off "Learn more" link.
-			postInteractionWait: 3000, // Wait for overlay and selects to animate.
-			onReadyScript: 'mouse.js',
 		},
 	} )
 	.add( 'Connect More Services', () => {
@@ -256,9 +77,8 @@ storiesOf( 'Settings', module )
 	.add( 'Admin Settings', () => {
 		global.googlesitekit = settingsData;
 		global.googlesitekit.modules.analytics.setupComplete = false;
-		global.googlesitekit.admin.clientID = '26521001426-xxx1234ffghrrro6hofusq2b8.apps..com';
+		global.googlesitekit.admin.clientID = '123456789-xxx1234ffghrrro6hofusq2b8.apps..com';
 		global.googlesitekit.admin.clientSecret = '••••••••••••••••••••••••••••';
-		global.googlesitekit.admin.apikey = 'AIzaSyAi7c63e21001ESQsrtIfdIY3IcyQVyiw4o';
 
 		return (
 			<div className="mdc-layout-grid">
