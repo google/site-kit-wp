8f7c9df806820d977ff76899ea5f36c407999079
Adam Silverstein <adamjs@google.com>
2020-04-15 08:46:31 -0600

    Correct import.

--- assets/js/modules/analytics/setup.js
+++ assets/js/modules/analytics/setup.js
@@ -21,7 +21,7 @@
  */
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import AccountCreate from './account-create';
+import AccountCreate from './common/account-create';
 
 /**
  * WordPress dependencies
