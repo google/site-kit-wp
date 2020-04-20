0c4c796282d72590a1a73d81646dd8a944b7a020
Adam Silverstein <adamjs@google.com>
2020-04-20 13:58:19 -0600

    Switch caching mechanism to `useMemo`.

--- assets/js/modules/analytics/common/timzezone-select.js
+++ assets/js/modules/analytics/common/timzezone-select.js
@@ -20,7 +20,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useState } from '@wordpress/element';
+import { useState, useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -32,106 +32,95 @@ import {
 import { countries } from './countries';
 const { default: { country: allCountries } } = countries;
 
-// Cache the complicated timezone dropdown.
-let timezoneSelectorCache = false;
-
-// Store current selections.
-let timezoneSelection = false;
-let countrySelection = false;
-let selectedTimezoneString = false;
-
 const TimezoneSelect = ( { timezone, setTimezone } ) => {
 	const [ selectedCountry, setSelectedCountry ] = useState( timezone );
 	const [ selectedTimezoneID, setSelectedTimezoneID ] = useState( '' );
+	let multiTimezone,
+		selectedTimezoneDisplay = selectedTimezoneID;
 
-	// Return the cached selector until the selected timezone changes.
-	if ( timezoneSelectorCache && timezone === timezoneSelection && selectedCountry === countrySelection && selectedTimezoneString === selectedTimezoneID ) {
-		return timezoneSelectorCache;
-	}
-
-	timezoneSelection = timezone;
-	countrySelection = selectedCountry;
-	selectedTimezoneString = selectedTimezoneID;
-
-	// Track whether the selected country has more than one timezone.
-	let multiTimezone = false;
-
-	timezoneSelectorCache = (
-		<div >
-			<span style={ { minWidth: '240px', margin: '0 5px 0 0' } /*todo: move to css */ } >
-				<Select
-					className="googlesitekit-analytics__select-timezone"
-					name="country"
-					enhanced
-					value={ selectedCountry }
-					onEnhancedChange={ ( i, item ) => {
-						setTimezone( item.dataset.value );
-						setSelectedCountry( item.dataset.value );
-					} }
-					label={ __( 'Country', 'google-site-kit' ) }
-					outlined
-				>
-					{
-						allCountries && allCountries
-							.map( ( aCountry ) => {
-								// If the selected timezone is in this country, the country should be selected.
-								let value = aCountry.defaultTimeZoneId;
-								const timezoneMatch = aCountry.timeZone.find( ( tz ) => tz.timeZoneId === timezone );
-								if ( timezoneMatch ) {
-									value = timezoneMatch.timeZoneId;
-									if ( aCountry.timeZone.length > 1 ) {
-										multiTimezone = aCountry.timeZone;
-									} else {
-										setSelectedTimezoneID( aCountry.timeZone[ 0 ].displayName );
-									}
-									setSelectedCountry( timezoneMatch.timeZoneId );
-								}
-
-								return (
-									<Option
-										key={ aCountry.displayName }
-										value={ value }
-									>
-										{ aCountry.displayName }
-									</Option>
-								);
-							} ) }
-				</Select>
-			</span>
-			<span style={ { minWidth: '240px', margin: '5px 5px 0 0' } /*todo: move to css */ } >
-				{ multiTimezone
-					? <Select
+	const getTimezoneSelector = () => {
+		return (
+			<div >
+				<span style={ { minWidth: '240px', margin: '0 5px 0 0' } /*todo: move to css */ } >
+					<Select
 						className="googlesitekit-analytics__select-timezone"
-						name="timezone2"
-						style={ { minWidth: '240px' } /*todo: move to css */ }
+						name="country"
 						enhanced
-						value={ timezone }
+						value={ selectedCountry }
 						onEnhancedChange={ ( i, item ) => {
 							setTimezone( item.dataset.value );
+							setSelectedCountry( item.dataset.value );
 						} }
-						label={ __( 'Timezone', 'google-site-kit' ) }
+						label={ __( 'Country', 'google-site-kit' ) }
 						outlined
 					>
 						{
-							multiTimezone
-								.map( ( aTimezone ) => {
+							allCountries && allCountries
+								.map( ( aCountry ) => {
+									// If the selected timezone is in this country, the country should be selected.
+									let value = aCountry.defaultTimeZoneId;
+									const timezoneMatch = aCountry.timeZone.find( ( tz ) => tz.timeZoneId === timezone );
+									if ( timezoneMatch ) {
+										value = timezoneMatch.timeZoneId;
+										if ( aCountry.timeZone.length > 1 ) {
+											multiTimezone = aCountry.timeZone;
+										} else {
+											setSelectedTimezoneID( aCountry.timeZone[ 0 ].displayName );
+											selectedTimezoneDisplay = aCountry.timeZone[ 0 ].displayName;
+											multiTimezone = false;
+										}
+										setSelectedCountry( timezoneMatch.timeZoneId );
+									}
+
 									return (
 										<Option
-											key={ aTimezone.displayName }
-											value={ aTimezone.timeZoneId }
+											key={ aCountry.displayName }
+											value={ value }
 										>
-											{ aTimezone.displayName }
+											{ aCountry.displayName }
 										</Option>
 									);
-								} )
-						}
+								} ) }
 					</Select>
-					: selectedTimezoneID
-				}
-			</span>
-		</div>
-	);
-	return timezoneSelectorCache;
+				</span>
+				<span style={ { minWidth: '240px', margin: '5px 5px 0 0' } /*todo: move to css */ } >
+					{ multiTimezone
+						? <Select
+							className="googlesitekit-analytics__select-timezone"
+							name="timezone2"
+							style={ { minWidth: '240px' } /*todo: move to css */ }
+							enhanced
+							value={ timezone }
+							onEnhancedChange={ ( i, item ) => {
+								setTimezone( item.dataset.value );
+							} }
+							label={ __( 'Timezone', 'google-site-kit' ) }
+							outlined
+						>
+							{
+								multiTimezone
+									.map( ( aTimezone ) => {
+										return (
+											<Option
+												key={ aTimezone.displayName }
+												value={ aTimezone.timeZoneId }
+											>
+												{ aTimezone.displayName }
+											</Option>
+										);
+									} )
+							}
+						</Select>
+						: selectedTimezoneDisplay
+					}
+				</span>
+			</div>
+		);
+	};
+
+	const timezoneSelector = useMemo( () => getTimezoneSelector(), [ selectedCountry, timezone ] );
+
+	return timezoneSelector;
 };
 
 export default TimezoneSelect;
