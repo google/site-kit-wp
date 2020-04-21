eadc96bf973db7f8598fba5a779faf15ba5f3a78
Evan Mattson <evan.mattson@10up.com>
2020-04-21 12:41:45 +0300

    Assume accounts will always be an array.

--- assets/js/modules/analytics/common/account-select.js
+++ assets/js/modules/analytics/common/account-select.js
@@ -62,7 +62,7 @@ export default function AccountSelect() {
 			enhanced
 			outlined
 		>
-			{ ( accounts || [] )
+			{ accounts
 				.concat( {
 					id: ACCOUNT_CREATE,
 					name: __( 'Set up a new account', 'google-site-kit' ),
