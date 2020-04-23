d60d89ceda9cf82b75af953d794f4c1cffc6599c
Evan Herman <eherman@godaddy.com>
2020-04-23 12:10:13 -0400

    Refactor

--- includes/Core/Admin/Standalone.php
+++ includes/Core/Admin/Standalone.php
@@ -3,7 +3,7 @@
  * Class Google\Site_Kit\Core\Admin\Standalone
  *
  * @package   Google\Site_Kit
- * @copyright 2019 Google LLC
+ * @copyright 2020 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
@@ -14,7 +14,7 @@ use Google\Site_Kit\Context;
 use Google\Site_Kit\Core\Assets\Stylesheet;
 
 /**
- * Class managing standlone mode.
+ * Class managing standalone mode.
  *
  * @since 1.0.0
  * @access private
@@ -23,91 +23,77 @@ use Google\Site_Kit\Core\Assets\Stylesheet;
 final class Standalone {
 
 	/**
-	 * Plugin context.
-	 *
-	 * @since NEXT
-	 *
-	 * @var Context
-	 */
-	private $context;
-
-	/**
-	 * Constructor.
-	 *
-	 * @since NEXT
-	 *
-	 * @param Context $context Plugin context.
-	 */
-	public function __construct( Context $context ) {
-
-		$this->context = $context;
-
-		$this->standalone_mode();
-
-	}
-
-	/**
 	 * Standalone mode
 	 *
-	 * @since NEXT
+	 * @since n.e.x.t
 	 */
-	public function standalone_mode() {
-
+	public function register() {
 		if ( ! $this->is_standalone() ) {
-
 			return;
-
 		}
 
-		add_filter( 'admin_body_class', array( $this, 'admin_body_classes' ) );
+		/**
+		 * Appends the standalone admin body class.
+		 *
+		 * @since n.e.x.t
+		 *
+		 * @param string $admin_body_classes Admin body classes.
+		 *
+		 * @return array Filtered array of query arguments.
+		 */
+		add_filter(
+			'admin_body_class',
+			function( $admin_body_classes ) {
+				return "{$admin_body_classes} googlesitekit-standalone";
+			}
+		);
 
 		remove_action( 'in_admin_header', 'wp_admin_bar_render', 0 );
 
 		add_filter( 'admin_footer_text', '__return_empty_string', PHP_INT_MAX );
 		add_filter( 'update_footer', '__return_empty_string', PHP_INT_MAX );
 
-		$styles = new Stylesheet(
-			'googlesitekit-standalone-css',
-			array(
-				'src' => $this->context->url( 'dist/assets/' ) . 'css/standalone.css',
-			)
-		);
-
-		$styles->register();
-		$styles->enqueue();
-
+		add_action( 'admin_head', array( $this, 'standalone_styles' ) );
 	}
 
 	/**
-	 * Append the standalone admin body class.
+	 * Detects if we are in Google Site Kit standalone mode
 	 *
-	 * @since NEXT
-	 *
-	 * @param string $admin_body_classes Admin body classes.
-	 *
-	 * @return array Filtered array of query arguments.
-	 */
-	public function admin_body_classes( $admin_body_classes ) {
-
-		return "{$admin_body_classes} googlesitekit-standalone";
-
-	}
-
-	/**
-	 * Detect if we are in Google Site Kit standalone mode
-	 *
-	 * @since NEXT
+	 * @since n.e.x.t
 	 *
 	 * @return boolean True when in standalone mode, else false.
 	 */
 	public function is_standalone() {
-
 		global $pagenow;
 
 		$page       = filter_input( INPUT_GET, 'page', FILTER_SANITIZE_STRING );
 		$standalone = filter_input( INPUT_GET, 'googlesitekit-standalone', FILTER_VALIDATE_BOOLEAN );
 
-		return ( 'admin.php' === $pagenow && ( '' !== $page && false !== strpos( $page, 'googlesitekit' ) ) && $standalone );
+		return ( 'admin.php' === $pagenow && false !== strpos( $page, 'googlesitekit' ) && $standalone );
+	}
 
+	/**
+	 * Enqueues styles for standalone mode.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return void
+	 */
+	public function standalone_styles() {
+		?>
+		<style type="text/css">
+		html {
+			padding-top: 0 !important;
+		}
+
+		body.site-kit_page_googlesitekit-splash.googlesitekit-standalone #adminmenumain {
+			display: none;
+		}
+
+		body.site-kit_page_googlesitekit-splash.googlesitekit-standalone #wpcontent {
+			margin-left: 0;
+		}
+		</style>
+		<?php
 	}
 }
