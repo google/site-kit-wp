2598559c0b67257949c56de832d267c18304f50a
Ryan Welcher <ryan.welcher@10up.com>
2020-05-05 12:24:15 -0400

    Further changes as per CR.

--- assets/js/googlesitekit/datastore/user/disconnect.js
+++ assets/js/googlesitekit/datastore/user/disconnect.js
@@ -29,7 +29,7 @@ import API from 'googlesitekit-api';
 // Actions
 const START_FETCH_DISCONNECT = 'START_FETCH_DISCONNECT';
 const FETCH_DISCONNECT = 'FETCH_DISCONNECT';
-const FINISH_DISCONNECT = 'FINISH_DISCONNET';
+const FINISH_FETCH_DISCONNECT = 'FINISH_DISCONNET';
 const CATCH_FETCH_DISCONNECT = 'CATCH_FETCH_DISCONNECT';
 const RECEIVE_DISCONNECT = 'RECEIVE_DISCONNECT';
 
@@ -52,11 +52,11 @@ export const actions = {
 				type: FETCH_DISCONNECT,
 			};
 
-			yield actions.receiveDisconnect( response );
+			yield actions.receiveDisconnected( response );
 
 			yield {
 				payload: {},
-				type: FINISH_DISCONNECT,
+				type: FINISH_FETCH_DISCONNECT,
 			};
 		} catch ( e ) {
 			error = e;
@@ -77,7 +77,7 @@ export const actions = {
 	 * @param {Object} disconnect Disconnection info from the API.
 	 * @return {Object} Redux-style action.
 	 */
-	receiveDisconnect( disconnect ) {
+	receiveDisconnected( disconnect ) {
 		invariant( disconnect, 'disconnect is required.' );
 
 		return {
@@ -103,7 +103,7 @@ export const reducer = ( state, { type, payload } ) => {
 		}
 
 		case RECEIVE_DISCONNECT: {
-			const { disconnect: disconnected } = payload;
+			const { disconnected } = payload;
 
 			return {
 				...state,
@@ -111,7 +111,7 @@ export const reducer = ( state, { type, payload } ) => {
 			};
 		}
 
-		case FINISH_DISCONNECT: {
+		case FINISH_FETCH_DISCONNECT: {
 			return {
 				...state,
 				isFetchingDisconnect: false,
@@ -142,8 +142,8 @@ export const selectors = {
 	 * @return {boolean} Is a disconnect ocurring or not.
 	 */
 	isDoingDisconnect( state ) {
-		const { isDoingDisconnect } = state;
-		return isDoingDisconnect;
+		const { isFetchingDisconnect } = state;
+		return isFetchingDisconnect;
 	},
 };
 
