dd4c372eb72696fb2bf5cdb30691f8d277753408
Adam Silverstein <adamjs@google.com>
2020-05-05 14:31:51 -0600

    Add missing use for Permissions.

--- includes/Modules/Analytics.php
+++ includes/Modules/Analytics.php
@@ -26,6 +26,7 @@ use Google\Site_Kit\Core\Authentication\Google_Proxy;
 use Google\Site_Kit\Core\Assets\Asset;
 use Google\Site_Kit\Core\Assets\Script;
 use Google\Site_Kit\Core\Authentication\Clients\Google_Site_Kit_Client;
+use Google\Site_Kit\Core\Permissions\Permissions;
 use Google\Site_Kit\Core\REST_API\Data_Request;
 use Google\Site_Kit\Core\Util\Debug_Data;
 use Google\Site_Kit\Modules\Analytics\Settings;
