fdf2a55203ec8aadf7ffb9fa48fcd46f50f8ba94
Shahid Qureshi <shah.qureshi@get10up.com>
2020-05-07 16:41:42 +0100

    Added new index file.

---
+++ assets/js/modules/search-console/datastore/index.js
@@ -0,0 +1,79 @@
+/**
+ * modules/search-console data store
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import Data from 'googlesitekit-data';
+import Modules from 'googlesitekit-modules';
+// import profiles from './profiles';
+import { STORE_NAME } from './constants';
+export { STORE_NAME };
+
+const baseModuleStore = Modules.createModuleStore( 'search-console', {
+	storeName: STORE_NAME,
+	settingSlugs: [
+		// 'profileID',
+	],
+} );
+
+export const INITIAL_STATE = Data.collectState(
+	baseModuleStore.INITIAL_STATE,
+	// profiles.INITIAL_STATE,
+);
+
+export const actions = Data.collectActions(
+	baseModuleStore.actions,
+	// profiles.actions,
+);
+
+export const controls = Data.collectControls(
+	baseModuleStore.controls,
+	// profiles.controls,
+);
+
+export const reducer = Data.addInitializeReducer(
+	INITIAL_STATE,
+	Data.collectReducers(
+		baseModuleStore.reducer,
+		// profiles.reducer,
+	)
+);
+
+export const resolvers = Data.collectResolvers(
+	baseModuleStore.resolvers,
+	// profiles.resolvers,
+);
+
+export const selectors = Data.collectSelectors(
+	baseModuleStore.selectors,
+	// profiles.selectors,
+);
+
+const store = {
+	actions,
+	controls,
+	reducer,
+	resolvers,
+	selectors,
+};
+
+// Register this store on the global registry.
+Data.registerStore( STORE_NAME, store );
+
+export default store;
