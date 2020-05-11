7c1af7fe5969266f21112287aff95f265c36e729
Evan Mattson <evan.mattson@10up.com>
2020-05-11 17:13:28 +0300

    Add tests for getContainers.

--- assets/js/modules/tagmanager/datastore/containers.test.js
+++ assets/js/modules/tagmanager/datastore/containers.test.js
@@ -20,13 +20,14 @@
  * Internal dependencies
  */
 import API from 'googlesitekit-api';
-import { STORE_NAME } from './constants';
+import { STORE_NAME, CONTEXT_WEB, CONTEXT_AMP } from './constants';
 import {
 	createTestRegistry,
 	muteConsole,
 	subscribeUntil,
 	unsubscribeFromAll,
 } from '../../../../../tests/js/utils';
+import * as factories from './__factories__';
 import * as fixtures from './__fixtures__';
 
 describe( 'modules/tagmanager containers', () => {
@@ -137,6 +138,119 @@ describe( 'modules/tagmanager containers', () => {
 	} );
 
 	describe( 'selectors', () => {
+		describe( 'getContainers', () => {
+			it( 'uses a resolver to make a network request', async () => {
+				const { account, containers } = factories.buildAccountWithContainers();
+				const accountID = account.accountId;
 
+				fetch
+					.doMockOnceIf(
+						/^\/google-site-kit\/v1\/modules\/tagmanager\/data\/containers/
+					)
+					.mockResponseOnce(
+						JSON.stringify( containers ),
+						{ status: 200 }
+					);
+
+				const initialContainers = registry.select( STORE_NAME ).getContainers( accountID );
+
+				// Ensure the proper parameters were sent.
+				expect( fetch.mock.calls[ 0 ][ 0 ] ).toMatchQueryParameters(
+					{
+						accountID,
+					}
+				);
+
+				expect( initialContainers ).toEqual( undefined );
+
+				await subscribeUntil( registry,
+					() => registry.select( STORE_NAME ).hasFinishedResolution( 'getContainers', [ accountID ] )
+				);
+
+				const resolvedContainers = registry.select( STORE_NAME ).getContainers( accountID );
+
+				expect( fetch ).toHaveBeenCalledTimes( 1 );
+				expect( resolvedContainers ).toEqual( containers );
+			} );
+
+			it( 'does not make a network request if containers for this account are already present', async () => {
+				const { account, containers } = factories.buildAccountWithContainers();
+				const accountID = account.accountId;
+
+				registry.dispatch( STORE_NAME ).receiveContainers( containers, { accountID } );
+
+				const resolvedContainers = registry.select( STORE_NAME ).getContainers( accountID );
+
+				await subscribeUntil( registry,
+					() => registry.select( STORE_NAME ).hasFinishedResolution( 'getContainers', [ accountID ] )
+				);
+
+				expect( fetch ).not.toHaveBeenCalled();
+				expect( resolvedContainers ).toEqual( containers );
+				expect( resolvedContainers ).toHaveLength( 1 );
+			} );
+
+			it( 'dispatches an error if the request fails', async () => {
+				const accountID = '123';
+				const response = {
+					code: 'internal_server_error',
+					message: 'Internal server error',
+					data: { status: 500 },
+				};
+				fetch
+					.doMockIf(
+						/^\/google-site-kit\/v1\/modules\/tagmanager\/data\/containers/
+					)
+					.mockResponse(
+						JSON.stringify( response ),
+						{ status: 500 }
+					);
+
+				muteConsole( 'error' );
+				registry.select( STORE_NAME ).getContainers( accountID );
+
+				await subscribeUntil( registry,
+					() => registry.select( STORE_NAME ).hasFinishedResolution( 'getContainers', [ accountID ] )
+				);
+
+				expect( fetch ).toHaveBeenCalledTimes( 1 );
+				const containers = registry.select( STORE_NAME ).getContainers( accountID );
+				expect( containers ).toEqual( undefined );
+				const error = registry.select( STORE_NAME ).getError();
+				expect( error ).toEqual( response );
+			} );
+
+			it( 'filters selected containers by the given usageContext if provided', () => {
+				const { account, containers: webContainers } = factories.buildAccountWithContainers( {
+					container: {
+						usageContext: [ CONTEXT_WEB ],
+					},
+					count: 2,
+				} );
+				const { containers: ampContainers } = factories.buildAccountWithContainers( {
+					account,
+					container: {
+						usageContext: [ CONTEXT_AMP ],
+					},
+					count: 3,
+				} );
+				const accountID = account.accountId;
+				registry.dispatch( STORE_NAME ).receiveContainers(
+					[ ...webContainers, ...ampContainers ],
+					{ accountID }
+				);
+
+				expect(
+					registry.select( STORE_NAME ).getContainers( accountID, CONTEXT_WEB )
+				).toEqual( webContainers );
+				expect(
+					registry.select( STORE_NAME ).getContainers( accountID, CONTEXT_AMP )
+				).toEqual( ampContainers );
+
+				expect(
+					registry.select( STORE_NAME ).getContainers( accountID )
+				).toEqual( [ ...webContainers, ...ampContainers ] );
+			} );
+		} );
 	} );
 } );
