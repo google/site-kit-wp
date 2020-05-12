e646f5dfd0887015590bb6e105bdc77f7bc47c19
Ryan Welcher <ryan.welcher@10up.com>
2020-05-12 14:32:36 -0400

    Add handling for URL fetch issues.

--- tests/e2e/matchers/to-have-valid-amp.js
+++ tests/e2e/matchers/to-have-valid-amp.js
@@ -4,17 +4,34 @@
 import amphtmlValidator from 'amphtml-validator';
 import fetch from 'node-fetch';
 
-export async function toHaveValidAMP( path = '/' ) {
+/**
+ * WordPress dependencies
+ */
+import {
+	createURL,
+} from '@wordpress/e2e-test-utils';
+
+/**
+ * Jest matcher for asserting the URL at the given path validates with AMP.
+ *
+ * @param {string} path The URL path of the current site to check.
+ */
+export async function toHaveValidAMP( path ) {
 	const result = {};
 	try {
-		const html = await fetch( 'http://localhost:9002' + path, { credentials: 'omit' } ).then( ( res ) => res.text() );
-		const scanStatus = '';
-		await amphtmlValidator.getInstance().then( ( validator ) => {
-			const { status } = validator.validateString( html );
-			result.pass = ( 'PASS' === status );
-			result.message = `AMP Status: ${ status }`;
-		} );
-		result.message = () => `AMP Status: ${ scanStatus }`;
+		const url = createURL( path );
+		const response = await fetch( url, { credentials: 'omit' } );
+		if ( 200 !== response.status ) {
+			result.pass = false;
+			result.message = () => `fetch() error: ${ url } returned a status of ${ response.statusText }`;
+		} else {
+			const html = await response.text();
+			await amphtmlValidator.getInstance().then( ( validator ) => {
+				const { status } = validator.validateString( html );
+				result.pass = ( 'PASS' === status );
+				result.message = () => `AMP Status: ${ status }`;
+			} );
+		}
 	} catch ( error ) {
 		result.pass = false;
 		result.message = () => error.message;
