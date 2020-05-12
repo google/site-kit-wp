a45a969d0a0fc59a3243da7acef2ce58dc9d7514
Jesse Lee <jqlee85@gmail.com>
2020-05-12 13:39:31 -0700

    Restore individiual exports for stores.

--- assets/js/googlesitekit/modules/datastore/index.js
+++ assets/js/googlesitekit/modules/datastore/index.js
@@ -30,6 +30,13 @@ const store = Data.combineStores(
 	modules,
 );
 
+export const INITIAL_STATE = store.INITIAL_STATE;
+export const actions = store.actions;
+export const controls = store.controls;
+export const reducer = store.reducer;
+export const resolvers = store.resolvers;
+export const selectors = store.selectors;
+
 // Register this store on the global registry.
 Data.registerStore( STORE_NAME, store );
 
