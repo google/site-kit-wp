4c8d0332a6e51fdcfa3ba543c5bc454d213443d6
Ryan Welcher <ryan.welcher@10up.com>
2020-05-14 13:18:15 -0400

    Update helper to not look for success message.

--- tests/e2e/utils/activate-amp-and-set-mode.js
+++ tests/e2e/utils/activate-amp-and-set-mode.js
@@ -31,11 +31,9 @@ export const activateAmpAndSetMode = async ( mode ) => {
  */
 export const setAMPMode = async ( mode ) => {
 	// Test to be sure that the passed mode is known.
-	expect( Object.keys( allowedAmpModes ) ).toContain( mode );
+	await expect( Object.keys( allowedAmpModes ) ).toContain( mode );
 	// Set the AMP mode
 	await visitAdminPage( 'admin.php', 'page=amp-options' );
 	await expect( page ).toClick( `#theme_support_${ allowedAmpModes[ mode ] }` );
 	await expect( page ).toClick( '#submit' );
-	await page.waitForSelector( '#setting-error-settings_updated' );
-	await expect( page ).toMatchElement( '#setting-error-settings_updated', { text: 'Settings saved.' } );
 };
