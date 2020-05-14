9c7eb6e5c1de6eae0b8931927e622174d023f8cd
Felix Arntz <felixarntz@google.com>
2020-05-14 11:11:51 -0700

    Fix broken Storybook stories.

--- stories/module-analytics-setup.stories.js
+++ stories/module-analytics-setup.stories.js
@@ -118,8 +118,8 @@ storiesOf( 'Analytics Module/Setup', module )
 		const setupRegistry = ( { dispatch } ) => {
 			dispatch( STORE_NAME ).receiveExistingTag( null );
 			dispatch( STORE_NAME ).receiveAccounts( accounts );
-			dispatch( STORE_NAME ).receiveProperties( properties );
-			dispatch( STORE_NAME ).receiveProfiles( profiles );
+			dispatch( STORE_NAME ).receiveProperties( properties, { accountID: properties[ 0 ].accountId } );
+			dispatch( STORE_NAME ).receiveProfiles( profiles, { propertyID: profiles[ 0 ].webPropertyId } );
 			dispatch( STORE_NAME ).setSettings( {
 				accountID: ACCOUNT_CREATE,
 			} );
