6526179c033e9496ebc53b77e148daff90e0bf7c
Ryan Welcher <ryan.welcher@10up.com>
2020-05-15 09:33:18 -0400

    Add option to custom matcher to test logged in status.

--- tests/e2e/matchers/to-have-valid-amp.js
+++ tests/e2e/matchers/to-have-valid-amp.js
@@ -15,26 +15,43 @@ import {
  * Jest matcher for asserting the URL at the given path validates with AMP.
  *
  * @param {string} path The URL path of the current site to check.
+ * @param {Object } options Settings object to control the matcher.
  */
-export async function toHaveValidAMP( path ) {
+export async function toHaveValidAMP( path, options = { loggedIn: false } ) {
+	const { loggedIn } = options;
 	const result = {};
-	try {
-		const url = createURL( path );
-		const response = await fetch( url, { credentials: 'omit' } );
-		if ( 200 !== response.status ) {
-			result.pass = false;
-			result.message = () => `fetch() error: ${ url } returned a status of ${ response.statusText }`;
-		} else {
-			const html = await response.text();
-			await amphtmlValidator.getInstance().then( ( validator ) => {
-				const { status } = validator.validateString( html );
-				result.pass = ( 'PASS' === status );
-				result.message = () => `AMP Status: ${ status }`;
-			} );
+	if ( true === loggedIn ) {
+		await Promise.all( [
+			page.goto( createURL( '/' ), { waitUntil: 'load' } ),
+			page.waitForSelector( '#amp-admin-bar-item-status-icon' ),
+		] );
+		try {
+			await expect( page ).toMatchElement( '#amp-admin-bar-item-status-icon', { text: '✅' } );
+			result.pass = true;
+			result.message = () => 'Expected logged-in user to have valid AMP';
+		} catch ( e ) {
+			result.pass = true;
+			result.message = () => 'Expected logged-in user not to have valid AMP';
 		}
-	} catch ( error ) {
-		result.pass = false;
-		result.message = () => error.message;
+	} else {
+		try {
+			const url = createURL( path );
+			const response = await fetch( url, { credentials: 'omit' } );
+			if ( 200 !== response.status ) {
+				result.pass = false;
+				result.message = () => `fetch() error: ${ url } returned a status of ${ response.statusText }`;
+			} else {
+				const html = await response.text();
+				await amphtmlValidator.getInstance().then( ( validator ) => {
+					const { status } = validator.validateString( html );
+					result.pass = ( 'PASS' === status );
+					result.message = () => `AMP Status: ${ status }`;
+				} );
+			}
+		} catch ( error ) {
+			result.pass = false;
+			result.message = () => error.message;
+		}
 	}
 
 	return result;
