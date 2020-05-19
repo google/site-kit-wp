6dd98600bab37466bbc5d4cebafcd0dd0fde9962
Felix Arntz <felixarntz@google.com>
2020-05-19 13:45:19 -0700

    Expand test coverage to ensure valid authenticated users are not modified.

--- tests/phpunit/integration/Core/Util/Migration_1_8_1Test.php
+++ tests/phpunit/integration/Core/Util/Migration_1_8_1Test.php
@@ -54,6 +54,7 @@ class Migration_1_8_1Test extends TestCase {
 	 */
 	protected $migration;
 
+	private $temp_api_request_issued;
 	private $temp_received_identifiers;
 
 	public function setUp() {
@@ -65,6 +66,7 @@ class Migration_1_8_1Test extends TestCase {
 		$this->authentication = new Authentication( $this->context, $this->options, $this->user_options );
 		$this->migration      = new Migration_1_8_1( $this->context, $this->options, $this->user_options, $this->authentication );
 
+		$this->temp_api_request_issued   = false;
 		$this->temp_received_identifiers = null;
 	}
 
@@ -94,17 +96,20 @@ class Migration_1_8_1Test extends TestCase {
 	}
 
 	public function test_migrate_without_problem_cases() {
-		$this->create_user( 'administrator', self::FILE_VERIFICATION );
+		$valid_authenticated_user = $this->create_user( 'administrator', self::FILE_VERIFICATION );
 		$this->create_user( 'subscriber', false );
 
 		$this->fake_proxy_site_connection();
+		$this->stub_mark_notifications_response();
 
 		$this->migration->migrate();
+		$this->assertNotIssuedAPIRequest();
+		$this->assertHasSiteKitUserOptions( $valid_authenticated_user->ID );
 		$this->assertEquals( Migration_1_8_1::DB_VERSION, $this->get_db_version() );
 	}
 
 	public function test_migrate_with_problem_cases() {
-		$this->create_user( 'administrator', self::FILE_VERIFICATION );
+		$valid_authenticated_user = $this->create_user( 'administrator', self::META_VERIFICATION );
 		$this->create_user( 'subscriber', false );
 		$this->create_user( 'subscriber', false );
 		$this->create_user( 'administrator', false );
@@ -135,9 +140,11 @@ class Migration_1_8_1Test extends TestCase {
 		$this->stub_mark_notifications_response();
 
 		$this->migration->migrate();
-		$this->assertReceivedIdentifiers( $expected_identifiers );
+		$this->assertIssuedAPIRequest();
+		$this->assertSentAPIRequestIdentifiers( $expected_identifiers );
+		$this->assertHasSiteKitUserOptions( $valid_authenticated_user->ID );
 		foreach ( $problem_cases as $user ) {
-			$this->assertNoSiteKitUserOptions( $user->ID );
+			$this->assertNotHasSiteKitUserOptions( $user->ID );
 		}
 		$this->assertEquals( Migration_1_8_1::DB_VERSION, $this->get_db_version() );
 	}
@@ -152,8 +159,9 @@ class Migration_1_8_1Test extends TestCase {
 		$this->stub_mark_notifications_response( 'invalid_notification_id' );
 
 		$this->migration->migrate();
+		$this->assertIssuedAPIRequest();
 		// Despite API error, users have been determined and cleared correctly.
-		$this->assertReceivedIdentifiers(
+		$this->assertSentAPIRequestIdentifiers(
 			array_map(
 				function( $user ) {
 					return $user->user_email;
@@ -162,21 +170,28 @@ class Migration_1_8_1Test extends TestCase {
 			)
 		);
 		foreach ( $problem_cases as $user ) {
-			$this->assertNoSiteKitUserOptions( $user->ID );
+			$this->assertNotHasSiteKitUserOptions( $user->ID );
 		}
 		$this->assertEquals( Migration_1_8_1::DB_VERSION, $this->get_db_version() );
 	}
 
 	private function create_user( $role, $verification_type = false ) {
 		$user = $this->factory()->user->create_and_get( array( 'role' => $role ) );
+
+		// Store original User_Options instance user to restore below.
+		$backup_user_id = $this->user_options->get_user_id();
+		$this->user_options->switch_user( (int) $user->ID );
+
+		// If user should be verified, set the respective token.
 		if ( ! empty( $verification_type ) ) {
-			$backup_user_id = $this->user_options->get_user_id();
-			$this->user_options->switch_user( (int) $user->ID );
 			$this->user_options->set( $verification_type, 'a1b2c3d4' );
-			// Set access token too to simulate user being fully authenticated.
+			// Also set access token to simulate user being fully authenticated.
 			$this->user_options->set( OAuth_Client::OPTION_ACCESS_TOKEN, 'test-access-token' );
-			$this->user_options->switch_user( $backup_user_id );
 		}
+
+		// Restore original user.
+		$this->user_options->switch_user( $backup_user_id );
+
 		return $user;
 	}
 
@@ -189,6 +204,7 @@ class Migration_1_8_1Test extends TestCase {
 					return $preempt;
 				}
 
+				$this->temp_api_request_issued   = true;
 				$this->temp_received_identifiers = isset( $args['body']['identifiers'] ) ? $args['body']['identifiers'] : null;
 
 				$response_code = 200;
@@ -216,7 +232,15 @@ class Migration_1_8_1Test extends TestCase {
 		);
 	}
 
-	private function assertReceivedIdentifiers( $expected ) {
+	private function assertIssuedAPIRequest() {
+		$this->assertTrue( $this->temp_api_request_issued, 'Failed asserting that API request was issued.' );
+	}
+
+	private function assertNotIssuedAPIRequest() {
+		$this->assertFalse( $this->temp_api_request_issued, 'Failed asserting that API request was issued.' );
+	}
+
+	private function assertSentAPIRequestIdentifiers( $expected ) {
 		$this->assertNotNull( $this->temp_received_identifiers, 'No user identifiers received.' );
 
 		if ( ! empty( $this->temp_received_identifiers ) ) {
@@ -232,7 +256,7 @@ class Migration_1_8_1Test extends TestCase {
 		$this->assertEquals( $expected, $actual, 'Failed asserting that received identifiers matched.' );
 	}
 
-	private function assertNoSiteKitUserOptions( $user_id ) {
+	private function assertHasSiteKitUserOptions( $user_id ) {
 		global $wpdb;
 
 		$prefix = $this->user_options->get_meta_key( 'googlesitekit\_%' );
@@ -240,7 +264,18 @@ class Migration_1_8_1Test extends TestCase {
 		$site_kit_user_options = $wpdb->get_col(
 			$wpdb->prepare( "SELECT meta_value FROM $wpdb->usermeta WHERE user_id = %d AND meta_key LIKE %s", $user_id, $prefix )
 		);
-		$this->assertEmpty( $site_kit_user_options, 'Failed asserting that user has no Site Kit options.' );
+		$this->assertNotEmpty( $site_kit_user_options, 'Failed asserting that user has Site Kit options.' );
+	}
+
+	private function assertNotHasSiteKitUserOptions( $user_id ) {
+		global $wpdb;
+
+		$prefix = $this->user_options->get_meta_key( 'googlesitekit\_%' );
+
+		$site_kit_user_options = $wpdb->get_col(
+			$wpdb->prepare( "SELECT meta_value FROM $wpdb->usermeta WHERE user_id = %d AND meta_key LIKE %s", $user_id, $prefix )
+		);
+		$this->assertEmpty( $site_kit_user_options, 'Failed asserting that user does not have Site Kit options.' );
 	}
 
 	private function get_db_version() {
