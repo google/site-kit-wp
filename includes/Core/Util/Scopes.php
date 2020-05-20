f2a64e4a5bc2d7e0cb1c7198ede08c223c51f0f3
Evan Mattson <evan.mattson@10up.com>
2020-05-20 13:35:38 +0300

    Add Scopes utility class and tests.

---
+++ includes/Core/Util/Scopes.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Class Google\Site_Kit\Core\Util\Scopes
+ *
+ * @package   Google\Site_Kit\Core\Util
+ * @copyright 2020 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Core\Util;
+
+/**
+ * Utility class for handling generic OAuth scope functions.
+ *
+ * @since n.e.x.t
+ * @access private
+ * @ignore
+ */
+class Scopes {
+
+	/**
+	 * Mapping of requested scope to satisfying scopes.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @var array
+	 */
+	protected static $map = array(
+		'https://www.googleapis.com/auth/adsense.readonly' => array(
+			'https://www.googleapis.com/auth/adsense',
+		),
+		'https://www.googleapis.com/auth/analytics.readonly' => array(
+			'requires_all' => true,
+			'https://www.googleapis.com/auth/analytics',
+			'https://www.googleapis.com/auth/analytics.edit',
+		),
+		'https://www.googleapis.com/auth/tagmanager.readonly' => array(
+			'https://www.googleapis.com/auth/tagmanager.edit.containers',
+		),
+		'https://www.googleapis.com/auth/webmasters.readonly' => array(
+			'https://www.googleapis.com/auth/webmasters',
+		),
+	);
+
+	/**
+	 * Tests if the given scope is satisfied by the given list of available scopes.
+	 *
+	 * @param string $scope            OAuth scope to test for.
+	 * @param array  $available_scopes Available OAuth scopes to test the individual scope against.
+	 * @return bool True if the given scope is satisfied, otherwise false.
+	 */
+	public static function is_satisfied_by( $scope, array $available_scopes ) {
+		if ( in_array( $scope, $available_scopes, true ) ) {
+			return true;
+		}
+
+		if ( empty( self::$map[ $scope ] ) ) {
+			return false;
+		}
+
+		$satisfying_scopes = self::$map[ $scope ];
+
+		if ( ! empty( $satisfying_scopes['requires_all'] ) ) {
+			unset( $satisfying_scopes['requires_all'] );
+
+			// Return true if all satisfying scopes are present, otherwise false.
+			return ! array_diff( $satisfying_scopes, $available_scopes );
+		}
+
+		// Return true if any of the scopes are present, otherwise false.
+		return (bool) array_intersect( $satisfying_scopes, $available_scopes );
+	}
+
+	/**
+	 * Tests if all the given scopes are satisfied by the list of available scopes.
+	 *
+	 * @param array $scopes           OAuth scopes to test.
+	 * @param array $available_scopes OAuth scopes to test $scopes against.
+	 * @return bool True if all given scopes are satisfied, otherwise false.
+	 */
+	public static function are_satisfied_by( array $scopes, array $available_scopes ) {
+		foreach ( $scopes as $scope ) {
+			if ( ! self::is_satisfied_by( $scope, $available_scopes ) ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
