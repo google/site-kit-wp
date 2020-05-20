e3baa214e0b79ed5bb900f92a9d45a98a73cece4
Evan Mattson <evan.mattson@10up.com>
2020-05-20 16:52:02 +0300

    Remove unused imports in OAuth_Client.

--- includes/Core/Authentication/Clients/OAuth_Client.php
+++ includes/Core/Authentication/Clients/OAuth_Client.php
@@ -15,14 +15,12 @@ use Google\Site_Kit\Context;
 use Google\Site_Kit\Core\Authentication\Credentials;
 use Google\Site_Kit\Core\Authentication\Google_Proxy;
 use Google\Site_Kit\Core\Authentication\Profile;
-use Google\Site_Kit\Core\Authentication\Verification;
 use Google\Site_Kit\Core\Authentication\Exception\Google_Proxy_Code_Exception;
 use Google\Site_Kit\Core\Storage\Encrypted_Options;
 use Google\Site_Kit\Core\Storage\Encrypted_User_Options;
 use Google\Site_Kit\Core\Storage\Options;
 use Google\Site_Kit\Core\Storage\User_Options;
 use Google\Site_Kit\Core\Util\Scopes;
-use Google\Site_Kit\Modules\Search_Console;
 use Google\Site_Kit_Dependencies\Google_Service_PeopleService;
 use WP_HTTP_Proxy;
 
