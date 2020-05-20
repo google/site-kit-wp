410ebabee8b7c5fc81c26c26ad3f1458fbc76361
Evan Mattson <evan.mattson@10up.com>
2020-05-20 21:05:30 +0300

    Conditionally annotate non-existent new datapoint.

--- includes/Modules/Tag_Manager.php
+++ includes/Modules/Tag_Manager.php
@@ -437,13 +437,16 @@ final class Tag_Manager extends Module implements Module_With_Scopes, Module_Wit
 	protected function get_datapoint_definitions() {
 		$map = parent::get_datapoint_definitions();
 
-		$map['POST:create-container'] = array_merge(
-			$map['POST:create-container'],
-			array(
-				'scopes'                 => array( 'https://www.googleapis.com/auth/tagmanager.edit.containers' ),
-				'request_scopes_message' => __( 'Additional permissions are required to create a new Tag Manager container.', 'google-site-kit' ),
-			)
-		);
+		// TODO: remove this once datapoint exists.
+		if ( isset( $map['POST:create-container'] ) ) {
+			$map['POST:create-container'] = array_merge(
+				$map['POST:create-container'],
+				array(
+					'scopes'                 => array( 'https://www.googleapis.com/auth/tagmanager.edit.containers' ),
+					'request_scopes_message' => __( 'Additional permissions are required to create a new Tag Manager container.', 'google-site-kit' ),
+				)
+			);
+		}
 
 		return $map;
 	}
