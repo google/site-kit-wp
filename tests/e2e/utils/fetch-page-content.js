dd9256fd0d4485b36441e92372b40931dc332a4a
Ryan Welcher <ryan.welcher@10up.com>
2020-05-27 09:08:55 -0400

    Whitespace change.

--- tests/e2e/utils/fetch-page-content.js
+++ tests/e2e/utils/fetch-page-content.js
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * Fetches markup for any given URL in the context of Puppeteer
  *
