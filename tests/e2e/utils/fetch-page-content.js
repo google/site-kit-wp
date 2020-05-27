c3787ac96e2824a5ce5d01b822b1fa622f729ebe
Ryan Welcher <ryan.welcher@10up.com>
2020-05-27 09:02:50 -0400

    Add file header.

--- tests/e2e/utils/fetch-page-content.js
+++ tests/e2e/utils/fetch-page-content.js
@@ -1,5 +1,22 @@
 /**
- * Fetch markup for any given URL in the context of Puppateer
+ * fetchPageContent utility function.
+ *
+ * Site Kit by Google, Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Fetches markup for any given URL in the context of Puppeteer
  *
  * @param {string} url     Page URI to retrieve the content for.
  * @param {Object} options Options object to be passed to fetch()
