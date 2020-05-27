8fe4d1963bb124856c67bef971c6e9e711c475a0
Ryan Welcher <ryan.welcher@10up.com>
2020-05-27 09:02:35 -0400

    Add headers and JS syntax updates.

--- tests/e2e/matchers/to-have-valid-amp-for-user.js
+++ tests/e2e/matchers/to-have-valid-amp-for-user.js
@@ -1,4 +1,22 @@
 /**
+ * toHaveValidAMPForUser matcher.
+ *
+ * Site Kit by Google, Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * External dependencies
  */
 import jsdom from 'jsdom';
@@ -30,8 +48,8 @@ export async function toHaveValidAMPForUser( path ) {
 	// Make sure we have a login cookie
 	expect( cookies.some( ( { name } ) => name.match( /^wordpress_logged_in/ ) ) ).toBeTruthy();
 
-	const html = await fetchPageContent( urlToFetch, );
-	const { document: jsDoc } = ( new JSDOM( html ) ).window;
+	const html = await fetchPageContent( urlToFetch );
+	const jsDoc = new JSDOM( html ).window.document;
 	try {
 		expect( jsDoc.querySelector( '#amp-admin-bar-item-status-icon' ).textContent ).toMatch( '✅' );
 		pass = true;
