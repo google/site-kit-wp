8a7b6ff96e90bbf4450acb1af8f6859ef212fa0e
Ryan Welcher <ryan.welcher@10up.com>
2020-06-01 12:04:27 -0400

    Remove unneeded helpers.

--- tests/e2e/specs/plugin-activation.test.js
+++ tests/e2e/specs/plugin-activation.test.js
@@ -17,46 +17,30 @@ import { deactivatePlugin, activatePlugin } from '@wordpress/e2e-test-utils';
  * and consuming code can use `google-site-kit` variations in their places.
  */
 
-const activateSiteKit = async () => {
-	try {
-		await activatePlugin( 'google-site-kit' );
-	} catch {
-		await activatePlugin( 'site-kit-by-google' );
-	}
-};
-
-const deactivateSiteKit = async () => {
-	try {
-		await deactivatePlugin( 'google-site-kit' );
-	} catch {
-		await deactivatePlugin( 'site-kit-by-google' );
-	}
-};
-
 describe( 'Plugin Activation Notice', () => {
 	describe( 'When Javascript is enabled', () => {
 		beforeEach( async () => {
-			await deactivateSiteKit();
+			await deactivatePlugin( 'google-site-kit' );
 			await activatePlugin( 'e2e-tests-gcp-credentials-plugin' );
 		} );
 
 		afterEach( async () => {
 			await deactivatePlugin( 'e2e-tests-gcp-credentials-plugin' );
-			await activateSiteKit();
+			await activatePlugin( 'google-site-kit' );
 		} );
 
 		it( 'Should be displayed', async () => {
-			await activateSiteKit();
+			await activatePlugin( 'google-site-kit' );
 
 			await page.waitForSelector( '.googlesitekit-activation__title' );
 
 			await expect( page ).toMatchElement( '.googlesitekit-activation__title', { text: /Congratulations, the Site Kit plugin is now activated./i } );
 
-			await deactivateSiteKit();
+			await deactivatePlugin( 'google-site-kit' );
 		} );
 
 		it( 'Should lead you to the setup wizard', async () => {
-			await activateSiteKit();
+			await activatePlugin( 'google-site-kit' );
 
 			await page.waitForSelector( '.googlesitekit-activation' );
 
@@ -68,25 +52,25 @@ describe( 'Plugin Activation Notice', () => {
 			// Ensure we're on the first step.
 			await expect( page ).toMatchElement( '.googlesitekit-wizard-progress-step__number--inprogress', { text: '1' } );
 
-			await deactivateSiteKit();
+			await deactivatePlugin( 'google-site-kit' );
 		} );
 
 		it( 'Should not display noscript notice', async () => {
-			await activateSiteKit();
+			await activatePlugin( 'google-site-kit' );
 
 			await expect( page ).not.toMatchElement( '.googlesitekit-noscript' );
 
-			await deactivateSiteKit();
+			await deactivatePlugin( 'google-site-kit' );
 		} );
 	} );
 
 	describe( 'When Javascript is disabled', () => {
 		beforeEach( async () => {
-			await deactivateSiteKit();
+			await deactivatePlugin( 'google-site-kit' );
 		} );
 
 		afterEach( async () => {
-			await activateSiteKit();
+			await activatePlugin( 'google-site-kit' );
 		} );
 
 		it( 'Should not display plugin html', async () => {
@@ -95,18 +79,18 @@ describe( 'Plugin Activation Notice', () => {
 			// `await page.setJavaScriptEnabled( false );` and
 			// `await page.setJavaScriptEnabled( true );` in the test itself.
 			await page.setJavaScriptEnabled( false );
-			await activateSiteKit();
+			await activatePlugin( 'google-site-kit' );
 
 			await expect( page ).toMatchElement( '[id^=js-googlesitekit-]', { visible: false } );
 			await expect( page ).not.toMatchElement( '.googlesitekit-activation__title' );
 
-			await deactivateSiteKit();
+			await deactivatePlugin( 'google-site-kit' );
 			await page.setJavaScriptEnabled( true );
 		} );
 
 		it( 'Should display noscript notice', async () => {
 			await page.setJavaScriptEnabled( false );
-			await activateSiteKit();
+			await activatePlugin( 'google-site-kit' );
 
 			await expect( page ).toMatchElement(
 				'.googlesitekit-noscript__text',
@@ -114,7 +98,7 @@ describe( 'Plugin Activation Notice', () => {
 				{ visible: true }
 			);
 
-			await deactivateSiteKit();
+			await deactivatePlugin( 'google-site-kit' );
 			await page.setJavaScriptEnabled( true );
 		} );
 	} );
