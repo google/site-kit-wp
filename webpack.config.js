8fd80305685fb691a6fbc8eca85432793c9dc1b1
Shahid Qureshi <shah.qureshi@get10up.com>
2020-06-03 09:10:20 +0100

    Renamed entry point for adsense.

--- webpack.config.js
+++ webpack.config.js
@@ -113,7 +113,7 @@ const webpackConfig = ( mode ) => {
 				'googlesitekit-admin': './assets/js/googlesitekit-admin.js',
 				'googlesitekit-module': './assets/js/googlesitekit-module.js',
 				// Needed to test if a browser extension blocks this by naming convention.
-				ads: './assets/js/adsense.js',
+				adsense: './assets/js/adsense.js',
 			},
 			externals,
 			output: {
