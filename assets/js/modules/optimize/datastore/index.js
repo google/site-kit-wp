2a569da6fd710cf9fbaca7549daebc88c57cd782
Shahid Qureshi <shah.qureshi@get10up.com>
2020-06-04 09:21:30 +0100

    Modified datastore index file.

--- assets/js/modules/optimize/datastore/index.js
+++ assets/js/modules/optimize/datastore/index.js
@@ -34,15 +34,9 @@ const baseModuleStore = Modules.createModuleStore( 'optimize', {
 } );
 
 const store = Data.combineStores(
+	Data.commonStore,
 	baseModuleStore,
 );
-
-export const INITIAL_STATE = store.INITIAL_STATE;
-export const actions = store.actions;
-export const controls = store.controls;
-export const reducer = store.reducer;
-export const resolvers = store.resolvers;
-export const selectors = store.selectors;
 
 // Register this store on the global registry.
 Data.registerStore( STORE_NAME, store );
