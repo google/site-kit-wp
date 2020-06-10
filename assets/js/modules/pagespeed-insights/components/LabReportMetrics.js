07e4903de73b27c730e0393ca20028dc4c18825a
Evan Mattson <evan.mattson@10up.com>
2020-06-10 23:11:37 +0300

    Add LabReportMetrics component.

---
+++ assets/js/modules/pagespeed-insights/components/LabReportMetrics.js
@@ -0,0 +1,66 @@
+/**
+ * PageSpeed Insights Lab Data report metrics component.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import ReportMetric from './ReportMetric';
+import { getScoreCategory } from '../dashboard/util';
+
+export default function LabReportMetrics( { data } ) {
+	const totalBlockingTime = data?.lighthouseResult?.audits?.[ 'total-blocking-time' ];
+	const largestContentfulPaint = data?.lighthouseResult?.audits?.[ 'largest-contentful-paint' ];
+	const cumulativeLayoutShift = data?.lighthouseResult?.audits?.[ 'cumulative-layout-shift' ];
+
+	return (
+		<div>
+			<ReportMetric
+				title={ __( 'Total Blocking Time', 'google-site-kit' ) }
+				description={ __( 'Sum of all time periods between FCP and Time to Interactive, when task length exceeded 50ms.', 'google-site-kit' ) }
+				displayValue={ totalBlockingTime.displayValue }
+				category={ getScoreCategory( totalBlockingTime.score ) }
+			/>
+			<ReportMetric
+				title={ __( 'Largest Contentful Paint', 'google-site-kit' ) }
+				description={ __( 'Marks the time at which the largest text or image is painted.', 'google-site-kit' ) }
+				displayValue={ largestContentfulPaint.displayValue }
+				category={ getScoreCategory( largestContentfulPaint.score ) }
+			/>
+			<ReportMetric
+				title={ __( 'Cumulative Layout Shift', 'google-site-kit' ) }
+				description={ __( 'Measures the movement of visible elements within the viewport.', 'google-site-kit' ) }
+				displayValue={ cumulativeLayoutShift.displayValue }
+				category={ getScoreCategory( cumulativeLayoutShift.score ) }
+			/>
+		</div>
+	);
+}
+
+LabReportMetrics.propTypes = {
+	data: PropTypes.object.isRequired,
+};
