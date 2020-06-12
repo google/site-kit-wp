47f745bb91349b01a45dfdccd96be4b54a3702f3
Evan Mattson <evan.mattson@10up.com>
2020-06-12 11:07:04 +0300

    Remove unused PSI dashboard utils.

--- assets/js/modules/pagespeed-insights/dashboard/util.js
+++ assets/js/modules/pagespeed-insights/dashboard/util.js
@@ -19,7 +19,6 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { CATEGORY_FAST, CATEGORY_AVERAGE, CATEGORY_SLOW } from '../components/constants';
 
 /**
@@ -40,80 +39,3 @@ export function getScoreCategory( score ) {
 
 	return CATEGORY_SLOW;
 }
-
-/**
- * Retrieve the score category label based on the given score.
- *
- * @param {number} score Score between 1.0 and 0.0.
- *
- * @return {string} Score category label.
- */
-export const getScoreCategoryLabel = ( score ) => {
-	const category = getScoreCategory( score );
-
-	if ( 'fast' === category ) {
-		return __( 'Fast', 'google-site-kit' );
-	}
-
-	if ( 'average' === category ) {
-		return __( 'Average', 'google-site-kit' );
-	}
-
-	return __( 'Slow', 'google-site-kit' );
-};
-
-export const PageSpeedReportScoreCategoryWrapper = ( props ) => {
-	const { score, children } = props;
-	const className = `googlesitekit-pagespeed-report__score-category-wrapper googlesitekit-pagespeed-report__score-category-wrapper--${ getScoreCategory( score ) }`;
-	const iconClassName = `googlesitekit-pagespeed-report__score-icon googlesitekit-pagespeed-report__score-icon--${ getScoreCategory( score ) }`;
-
-	return (
-		<span className={ className }>
-			{ children } <span className={ iconClassName }></span>
-		</span>
-	);
-};
-
-export const PageSpeedReportScoreGauge = ( props ) => {
-	const { score } = props;
-	const percentage = parseInt( score * 100, 10 );
-	const className = `
-		googlesitekit-percentage-circle
-		googlesitekit-percentage-circle--${ getScoreCategory( score ) }
-		googlesitekit-percentage-circle--percent-${ percentage }
-	`;
-
-	return (
-		<div className="googlesitekit-pagespeed-report__score-gauge">
-			<div className={ className }>
-				<div className="googlesitekit-percentage-circle__text">{ percentage }</div>
-				<div className="googlesitekit-percentage-circle__slice">
-					<div className="googlesitekit-percentage-circle__bar"></div>
-					<div className="googlesitekit-percentage-circle__fill"></div>
-				</div>
-			</div>
-			<span className="googlesitekit-pagespeed-report__score-gauge-label screen-reader-only">
-				{ __( 'Performance', 'google-site-kit' ) }
-			</span>
-		</div>
-	);
-};
-
-export const PageSpeedReportScale = () => {
-	return (
-		<div className="googlesitekit-pagespeed-report__scale">
-			<span>
-				{ __( 'Scale:', 'google-site-kit' ) }
-			</span>
-			<span className="googlesitekit-pagespeed-report__scale-range googlesitekit-pagespeed-report__scale-range--fast">
-				{ __( '90-100 (fast)', 'google-site-kit' ) }
-			</span>
-			<span className="googlesitekit-pagespeed-report__scale-range googlesitekit-pagespeed-report__scale-range--average">
-				{ __( '50-89 (average)', 'google-site-kit' ) }
-			</span>
-			<span className="googlesitekit-pagespeed-report__scale-range googlesitekit-pagespeed-report__scale-range--slow">
-				{ __( '0-49 (slow)', 'google-site-kit' ) }
-			</span>
-		</div>
-	);
-};
