2bdb6f61b9f0fea248146d71b81f53d2743e0f81
Felix Arntz <felixarntz@google.com>
2020-06-16 09:01:34 -0700

    Correct slightly inaccurate documentation in core/site store.

--- assets/js/googlesitekit/datastore/site/info.js
+++ assets/js/googlesitekit/datastore/site/info.js
@@ -233,7 +233,8 @@ export const selectors = {
 	 * @since 1.7.0
 	 *
 	 * @param {Object} state Data store's state.
-	 * @return {(number|undefined)} Current entity's ID.
+	 * @return {(number|null|undefined)} Current entity's ID, null if there is
+	 *                                   none, undefined if not loaded yet.
 	 */
 	getCurrentEntityID: createRegistrySelector( ( select ) => () => {
 		const { currentEntityID } = select( STORE_NAME ).getSiteInfo() || {};
@@ -247,7 +248,8 @@ export const selectors = {
 	 * @since 1.7.0
 	 *
 	 * @param {Object} state Data store's state.
-	 * @return {(string|undefined)} Current entity's title.
+	 * @return {(string|null|undefined)} Current entity's title, null if there
+	 *                                   is none, undefined if not loaded yet.
 	 */
 	getCurrentEntityTitle: createRegistrySelector( ( select ) => () => {
 		const { currentEntityTitle } = select( STORE_NAME ).getSiteInfo() || {};
@@ -261,7 +263,8 @@ export const selectors = {
 	 * @since 1.7.0
 	 *
 	 * @param {Object} state Data store's state.
-	 * @return {(string|undefined)} Current entity's type.
+	 * @return {(string|null|undefined)} Current entity's type, null if there
+	 *                                   is none, undefined if not loaded yet.
 	 */
 	getCurrentEntityType: createRegistrySelector( ( select ) => () => {
 		const { currentEntityType } = select( STORE_NAME ).getSiteInfo() || {};
@@ -275,7 +278,8 @@ export const selectors = {
 	 * @since 1.7.0
 	 *
 	 * @param {Object} state Data store's state.
-	 * @return {(string|undefined)} Current entity's reference URL.
+	 * @return {(string|null|undefined)} Current entity's URL, null if there is
+	 *                                   none, undefined if not loaded yet.
 	 */
 	getCurrentEntityURL: createRegistrySelector( ( select ) => () => {
 		const { currentEntityURL } = select( STORE_NAME ).getSiteInfo() || {};
