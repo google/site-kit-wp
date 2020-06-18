9649de4ab71b7690eadd72c86ee1f2684d98e2dd
Ryan Welcher <ryan.welcher@10up.com>
2020-06-18 08:43:01 -0400

    Import custom createPreloadingMiddleware.

--- assets/js/api-fetch-shim.js
+++ assets/js/api-fetch-shim.js
@@ -1,7 +1,30 @@
 /**
+ * API Fetch Shim.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * WordPress dependencies
  */
 import apiFetch from '@wordpress/api-fetch__non-shim';
+
+/**
+ * Internal dependencies
+ */
+import createPreloadingMiddleware from './googlesitekit/middleware/preloading.js';
 
 const {
 	nonce,
@@ -13,7 +36,7 @@ const {
 apiFetch.nonceEndpoint = nonceEndpoint;
 apiFetch.nonceMiddleware = apiFetch.createNonceMiddleware( nonce );
 apiFetch.rootURLMiddleware = apiFetch.createRootURLMiddleware( rootURL );
-apiFetch.preloadingMiddleware = apiFetch.createPreloadingMiddleware( preloadedData );
+apiFetch.preloadingMiddleware = createPreloadingMiddleware( preloadedData );
 
 apiFetch.use( apiFetch.nonceMiddleware );
 apiFetch.use( apiFetch.mediaUploadMiddleware );
