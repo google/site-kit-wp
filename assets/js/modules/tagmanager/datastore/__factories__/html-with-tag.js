dfbc24084ce5a0d2d96b83ae1a25c3490eafd1e0
Evan Mattson <evan.mattson@10up.com>
2020-06-22 11:35:37 +0300

    Update GTM HTML generator utilities.

--- assets/js/modules/tagmanager/datastore/__factories__/html-with-tag.js
+++ assets/js/modules/tagmanager/datastore/__factories__/html-with-tag.js
@@ -16,45 +16,58 @@
  * limitations under the License.
  */
 
-function tagHeadHTML( tag ) {
-	if ( ! tag ) {
-		return '';
-	}
+function tagHeadHTML( containerID ) {
 	return `
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${ tag }');</script>
+})(window,document,'script','dataLayer','${ containerID }');</script>
 <!-- End Google Tag Manager -->
 `;
 }
 
-function tagBodyOpenHTML( tag ) {
-	if ( ! tag ) {
-		return '';
-	}
+function tagBodyOpenHTML( containerID ) {
 	return `
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${ tag }"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${ containerID }"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 `;
 }
 
-export function generateHTMLWithTag( tag ) {
+/**
+ * Generates valid HTML with or without a GTM tag.
+ *
+ * @since n.e.x.t
+ *
+ * @param {string} [containerID] Optional. Container ID to generate tag with.
+ * @return {string} HTML.
+ */
+export function generateHTMLWithTag( containerID ) {
 	return `
 <!DOCTYPE html>
 <html>
 <head>
 <title>Test Title</title>
-${ tagHeadHTML( tag ) }
+${ containerID && tagHeadHTML( containerID ) }
 </head>
 <body>
-${ tagBodyOpenHTML( tag ) }
+${ containerID && tagBodyOpenHTML( containerID ) }
 <h1>Test Title</h1>
 </body>
 </html>
 `;
+}
+
+/**
+ * Generates valid HTML without a GTM tag.
+ *
+ * @since n.e.x.t
+ *
+ * @return {string} HTML.
+ */
+export function generateHTMLWithNoTag() {
+	return generateHTMLWithTag();
 }
