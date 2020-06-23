248f57435f4e9569808c377c979fd8f13768e1b6
Evan Mattson <evan.mattson@10up.com>
2020-06-23 22:51:03 +0300

    Scaffold AMPContainerSelect component, tests, and story.

--- stories/modules-tagmanager-components.stories.js
+++ stories/modules-tagmanager-components.stories.js
@@ -29,6 +29,7 @@ import { STORE_NAME } from '../assets/js/modules/tagmanager/datastore';
 import * as fixtures from '../assets/js/modules/tagmanager/datastore/__fixtures__';
 import AccountSelect from '../assets/js/modules/tagmanager/components/common/AccountSelect';
 import WebContainerSelect from '../assets/js/modules/tagmanager/components/common/WebContainerSelect';
+import AMPContainerSelect from '../assets/js/modules/tagmanager/components/common/AMPContainerSelect';
 
 storiesOf( 'Tag Manager Module/Components', module )
 	.add( 'AccountSelect', () => {
@@ -57,6 +58,22 @@ storiesOf( 'Tag Manager Module/Components', module )
 		return (
 			<WithTestRegistry callback={ setupRegistry }>
 				<WebContainerSelect />
+			</WithTestRegistry>
+		);
+	} )
+	.add( 'AMPContainerSelect', () => {
+		const setupRegistry = ( registry ) => {
+			const accountID = fixtures.getContainers.all[ 0 ].accountId;
+			registry.dispatch( STORE_NAME ).receiveGetSettings( {} );
+			registry.dispatch( STORE_NAME ).receiveGetAccounts( [] );
+			registry.dispatch( STORE_NAME ).setAccountID( accountID );
+			registry.dispatch( STORE_NAME ).receiveGetContainers( fixtures.getContainers.all, { accountID } );
+			registry.dispatch( STORE_NAME ).receiveGetExistingTag( null );
+		};
+
+		return (
+			<WithTestRegistry callback={ setupRegistry }>
+				<AMPContainerSelect />
 			</WithTestRegistry>
 		);
 	} )
