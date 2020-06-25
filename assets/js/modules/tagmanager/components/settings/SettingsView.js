e16c8b074adb05527fde00d36d26dcaeeb5a92ff
Evan Mattson <evan.mattson@10up.com>
2020-06-25 12:40:39 +0300

    Build out SettingsView and stories.

--- assets/js/modules/tagmanager/components/settings/SettingsView.js
+++ assets/js/modules/tagmanager/components/settings/SettingsView.js
@@ -16,6 +16,96 @@
  * limitations under the License.
  */
 
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import Data from 'googlesitekit-data';
+import DisplaySetting from '../../../../components/display-setting';
+import { STORE_NAME as CORE_SITE } from '../../../../googlesitekit/datastore/site';
+import { STORE_NAME } from '../../datastore';
+import {
+	ErrorNotice,
+	ExistingTagError,
+	ExistingTagNotice,
+} from '../common';
+const { useSelect } = Data;
+
 export default function SettingsView() {
-	return 'SettingsView';
+	const accountID = useSelect( ( select ) => select( STORE_NAME ).getAccountID() );
+	const containerID = useSelect( ( select ) => select( STORE_NAME ).getContainerID() );
+	const ampContainerID = useSelect( ( select ) => select( STORE_NAME ).getAMPContainerID() );
+	const useSnippet = useSelect( ( select ) => select( STORE_NAME ).getUseSnippet() );
+	const hasExistingTag = useSelect( ( select ) => select( STORE_NAME ).hasExistingTag() );
+	const hasExistingTagPermission = useSelect( ( select ) => select( STORE_NAME ).hasExistingTagPermission() );
+	const ampEnabled = useSelect( ( select ) => select( CORE_SITE ).isAMP() );
+	const isSecondaryAMP = useSelect( ( select ) => select( CORE_SITE ).isSecondaryAMP() );
+
+	return (
+		<div className="googlesitekit-setup-module googlesitekit-setup-module--tagmanager">
+
+			{ /* Prevent showing ExistingTagError and general ErrorNotice at the same time. */ }
+			{ ( ! hasExistingTag || hasExistingTagPermission ) && <ErrorNotice /> }
+			{ ( hasExistingTag && ! hasExistingTagPermission && hasExistingTagPermission !== undefined ) && <ExistingTagError /> }
+			{ ( hasExistingTag && hasExistingTagPermission && hasExistingTagPermission !== undefined ) && <ExistingTagNotice /> }
+
+			<div className="googlesitekit-settings-module__meta-items">
+				<div className="googlesitekit-settings-module__meta-item">
+					<h5 className="googlesitekit-settings-module__meta-item-type">
+						{ __( 'Account', 'google-site-kit' ) }
+					</h5>
+					<p className="googlesitekit-settings-module__meta-item-data">
+						<DisplaySetting value={ accountID } />
+					</p>
+				</div>
+
+				{ ( ! ampEnabled || isSecondaryAMP ) && (
+					<div className="googlesitekit-settings-module__meta-item">
+						<h5 className="googlesitekit-settings-module__meta-item-type">
+							{ isSecondaryAMP && __( 'Web Container ID', 'google-site-kit' ) }
+							{ ! ampEnabled && __( 'Container ID', 'google-site-kit' ) }
+						</h5>
+						<p className="googlesitekit-settings-module__meta-item-data">
+							<DisplaySetting value={ containerID } />
+						</p>
+					</div>
+				) }
+
+				{ ampEnabled && (
+					<div className="googlesitekit-settings-module__meta-item">
+						<h5 className="googlesitekit-settings-module__meta-item-type">
+							{ isSecondaryAMP && __( 'AMP Container ID', 'google-site-kit' ) }
+							{ ! isSecondaryAMP && __( 'Container ID', 'google-site-kit' ) }
+						</h5>
+						<p className="googlesitekit-settings-module__meta-item-data">
+							<DisplaySetting value={ ampContainerID } />
+						</p>
+					</div>
+				) }
+			</div>
+
+			<div className="googlesitekit-settings-module__meta-items">
+				<div className="googlesitekit-settings-module__meta-item">
+					<h5 className="googlesitekit-settings-module__meta-item-type">
+						{ __( 'Tag Manager Code Snippet', 'google-site-kit' ) }
+					</h5>
+
+					<p className="googlesitekit-settings-module__meta-item-data">
+						{ useSnippet && __( 'Snippet is inserted', 'google-site-kit' ) }
+						{ ! useSnippet && __( 'Snippet is not inserted', 'google-site-kit' ) }
+					</p>
+
+					{ hasExistingTag && (
+						<p>
+							{ __( 'Placing two tags at the same time is not recommended.', 'google-site-kit' ) }
+						</p>
+					) }
+				</div>
+			</div>
+		</div>
+	);
 }
---
+++ stories/modules-tagmanager-settings.stories.js
@@ -0,0 +1,85 @@
+/**
+ * Tag Manager Module Settings Stories.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { storiesOf } from '@storybook/react';
+
+/**
+ * Internal dependencies
+ */
+import { STORE_NAME as CORE_SITE, AMP_MODE_PRIMARY, AMP_MODE_SECONDARY } from '../assets/js/googlesitekit/datastore/site/constants';
+import { STORE_NAME as CORE_USER } from '../assets/js/googlesitekit/datastore/user';
+import { SettingsMain as TagManagerSettings } from '../assets/js/modules/tagmanager/components/settings';
+import * as fixtures from '../assets/js/modules/tagmanager/datastore/__fixtures__';
+import { STORE_NAME, ACCOUNT_CREATE } from '../assets/js/modules/tagmanager/datastore/constants';
+import { createTestRegistry, freezeFetch } from '../tests/js/utils';
+import createLegacySettingsWrapper from './utils/create-legacy-settings-wrapper';
+
+const Settings = createLegacySettingsWrapper( 'tagmanager', TagManagerSettings );
+
+storiesOf( 'Tag Manager Module/Settings', module )
+	.addDecorator( ( storyFn ) => {
+		const registry = createTestRegistry();
+		registry.dispatch( STORE_NAME ).setSettings( {} );
+		registry.dispatch( STORE_NAME ).receiveGetExistingTag( null );
+		registry.dispatch( CORE_USER ).receiveGetAuthentication( {} );
+
+		return storyFn( registry );
+	} )
+	.add( 'View, closed', ( registry ) => {
+		return <Settings isOpen={ false } registry={ registry } />;
+	} )
+	.add( 'View, open with all settings', ( registry ) => {
+		registry.dispatch( STORE_NAME ).receiveGetSettings( {
+			accountID: '123456789',
+			containerID: 'GTM-S1T3K1T',
+			internalContainerID: '54321',
+			useSnippet: true,
+		} );
+
+		return <Settings isOpen={ true } registry={ registry } />;
+	} )
+	.add( 'View, open with all settings, with existing tag (with access)', ( registry ) => {
+		const accountID = '123456789';
+		registry.dispatch( STORE_NAME ).receiveGetSettings( {
+			accountID,
+			containerID: 'GTM-S1T3K1T',
+			internalContainerID: '54321',
+			useSnippet: true,
+		} );
+		registry.dispatch( STORE_NAME ).receiveGetExistingTag( 'GTM-G000GL3' );
+		registry.dispatch( STORE_NAME ).receiveGetTagPermission( { accountID, permission: true }, { containerID: 'GTM-G000GL3' } );
+
+		return <Settings isOpen={ true } registry={ registry } />;
+	} )
+	.add( 'View, open with all settings, with existing tag (no access)', ( registry ) => {
+		const accountID = '123456789';
+		registry.dispatch( STORE_NAME ).receiveGetSettings( {
+			accountID,
+			containerID: 'GTM-S1T3K1T',
+			internalContainerID: '54321',
+			useSnippet: true,
+		} );
+		registry.dispatch( STORE_NAME ).receiveGetExistingTag( 'GTM-G000GL3' );
+		registry.dispatch( STORE_NAME ).receiveGetTagPermission( { accountID, permission: false }, { containerID: 'GTM-G000GL3' } );
+
+		return <Settings isOpen={ true } registry={ registry } />;
+	} )
+;
