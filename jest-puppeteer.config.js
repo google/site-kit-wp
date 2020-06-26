15b342d659e3e6b0c598c92464a72ba705bf79ed
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-06-26 15:01:45 +0300

    Update jest-puppeteer config.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,9 +1,20 @@
-exports.launch = {
-	args: [
-		'--disable-gpu',
-		'--no-sandbox',
-		'--lang=en-US',
-		'--disable-setuid-sandbox',
-		'--disable-dev-shm-usage',
-	],
+const config = require( '@wordpress/scripts/config/puppeteer.config.js' );
+
+// https://github.com/smooth-code/jest-puppeteer/blob/master/packages/jest-environment-puppeteer/README.md#jest-puppeteerconfigjs
+module.exports = {
+	launch: {
+		...( config.launch || {} ),
+		args: [
+			// https://peter.sh/experiments/chromium-command-line-switches/
+			'--disable-gpu',
+			'--no-sandbox',
+			'--lang=en-US',
+			'--disable-setuid-sandbox',
+			'--disable-dev-shm-usage',
+		],
+	},
+	// connect: {
+	// 	// https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerconnectoptions
+	// 	...( config.connect || {} ),
+	// },
 };
