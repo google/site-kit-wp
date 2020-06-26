ecf493431a3336b5f79026a230528f22e6b0c9bd
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-06-26 12:07:03 +0300

    Update jest-puppeteer config.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,10 @@
+// https://github.com/smooth-code/jest-puppeteer/blob/master/packages/jest-environment-puppeteer/README.md#jest-puppeteerconfigjs
+
 exports.launch = {
+	// https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerlaunchoptions
+	headless: process.env.HEADLESS !== 'no',
 	args: [
+		// https://peter.sh/experiments/chromium-command-line-switches/
 		'--disable-gpu',
 		'--no-sandbox',
 		'--lang=en-US',
@@ -7,3 +12,7 @@ exports.launch = {
 		'--disable-dev-shm-usage',
 	],
 };
+
+// exports.connect = {
+// 	// https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerconnectoptions
+// };
