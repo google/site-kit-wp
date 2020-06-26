18eb6d1f491d2cb62680cb53f2fbd6c7701f4a14
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-06-26 13:26:03 +0300

    Update jest-puppeteer config to make sure it extends a config from @wordpress/scripts.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,18 +1,20 @@
+const config = require( '@wordpress/scripts/config/puppeteer.config.js' );
+
 // https://github.com/smooth-code/jest-puppeteer/blob/master/packages/jest-environment-puppeteer/README.md#jest-puppeteerconfigjs
-
-exports.launch = {
-	// https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerlaunchoptions
-	headless: process.env.HEADLESS !== 'no',
-	args: [
-		// https://peter.sh/experiments/chromium-command-line-switches/
-		'--disable-gpu',
-		'--no-sandbox',
-		'--lang=en-US',
-		'--disable-setuid-sandbox',
-		'--disable-dev-shm-usage',
-	],
+module.exports = {
+	launch: {
+		...( config.launch || {} ),
+		args: [
+			// https://peter.sh/experiments/chromium-command-line-switches/
+			'--disable-gpu',
+			'--no-sandbox',
+			'--lang=en-US',
+			'--disable-setuid-sandbox',
+			'--disable-dev-shm-usage',
+		],
+	},
+	// connect: {
+	// 	// https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerconnectoptions
+	// 	...( config.connect || {} ),
+	// },
 };
-
-// exports.connect = {
-// 	// https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#puppeteerconnectoptions
-// };
