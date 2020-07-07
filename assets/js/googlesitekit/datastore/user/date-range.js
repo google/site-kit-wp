e5411f69a94f145d0859f8b2304f4887425ef17e
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-07-07 17:55:37 +0300

    Add bare date-range store to the core/user datastore.

---
+++ assets/js/googlesitekit/datastore/user/date-range.js
@@ -0,0 +1,46 @@
+/**
+ * core/user data store: date-range.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const INITIAL_STATE = {};
+
+export const actions = {};
+
+export const controls = {};
+
+export function reducer( state, { type } ) {
+	switch ( type ) {
+		default:
+			// do nothing
+			break;
+	}
+
+	return state;
+}
+
+export const resolvers = {};
+
+export const selectors = {};
+
+export default {
+	INITIAL_STATE,
+	actions,
+	controls,
+	reducer,
+	resolvers,
+	selectors,
+};
