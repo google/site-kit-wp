55e4ac165504d3e999cc60aa468de021aae69c6a
Yizhuo Du <michael03160525@gmail.com>
2020-07-08 21:50:27 +0000

    Modify the injected script from an external file to inline script.

--- includes/Modules/Analytics/Advanced_Tracking/Measurement_Code_Injector.php
+++ includes/Modules/Analytics/Advanced_Tracking/Measurement_Code_Injector.php
@@ -30,6 +30,28 @@ final class Measurement_Code_Injector {
 	private $event_configurations;
 
 	/**
+	 * The javascript code that is injected for event tracking.
+	 *
+	 * @since n.e.x.t.
+	 * @var string
+	 */
+	private static $inject_script = <<<INJECT_SCRIPT
+let config;
+for ( config of eventConfigurations ) {
+	const thisConfig = config;
+	document.addEventListener( config.on, function( e ) {
+		if ( e.target.matches( thisConfig.selector ) ) {
+			alert( 'Got an event called: '.concat( thisConfig.action ) );
+			//record event with gtag here
+		} else if ( e.target.matches( thisConfig.selector.concat( ' *' ) ) ) {
+			alert( 'Got an event called: '.concat( thisConfig.action ) );
+			//record event with gtag here
+		}
+	}, true );
+}
+INJECT_SCRIPT;
+
+	/**
 	 * Injector constructor.
 	 *
 	 * @since n.e.x.t.
@@ -46,17 +68,10 @@ final class Measurement_Code_Injector {
 	 * @since n.e.x.t.
 	 */
 	public function inject_event_tracking() {
-		$main_file_path = plugin_basename( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
-		$main_dir_path  = strstr( $main_file_path, '/', true );
-		wp_enqueue_script( // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
-			'shirshu_inject_event_tracking',
-			'/wp-content/plugins/' . $main_dir_path . '/assets/js/modules/analytics/advanced-tracking/measurement-event-tracking.js',
-			false,
-			null,
-			false
-		);
+		wp_add_inline_script('google_gtagjs', Measurement_Code_Injector::$inject_script);
+
 		wp_localize_script(
-			'shirshu_inject_event_tracking',
+			'google_gtagjs',
 			'eventConfigurations',
 			$this->event_configurations
 		);
