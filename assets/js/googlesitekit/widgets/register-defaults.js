64dcd91765ed2c81958f7d6cd6fda9d0f4713a46
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-07-09 17:54:07 +0300

    Rename widgetsApi to widgetsAPI.

--- assets/js/googlesitekit/widgets/register-defaults.js
+++ assets/js/googlesitekit/widgets/register-defaults.js
@@ -33,32 +33,32 @@ import {
  *
  * @since n.e.x.t
  *
- * @param {Object} widgetsApi Widgets API.
+ * @param {Object} widgetsAPI Widgets API.
  */
-export function registerDefaults( widgetsApi ) {
+export function registerDefaults( widgetsAPI ) {
 	const contexts = [ CONTEXT_DASHBOARD, CONTEXT_PAGE_DASHBOARD ];
 
-	widgetsApi.registerWidgetArea( AREA_ALL_TRAFFIC, {
+	widgetsAPI.registerWidgetArea( AREA_ALL_TRAFFIC, {
 		title: 'All Triffic',
 		subtitle: 'How people found your site.',
 	}, contexts );
 
-	widgetsApi.registerWidgetArea( AREA_SEARCH_FUNNEL, {
+	widgetsAPI.registerWidgetArea( AREA_SEARCH_FUNNEL, {
 		title: 'Search Funnel',
 		subtitle: 'How your site appeared in Search results and how many visitors you got from Search.',
 	}, contexts );
 
-	widgetsApi.registerWidgetArea( AREA_POPULARITY, {
+	widgetsAPI.registerWidgetArea( AREA_POPULARITY, {
 		title: 'Popularity',
 		subtitle: 'Your most popular pages and how people found them from Search.',
 	}, contexts );
 
-	widgetsApi.registerWidgetArea( AREA_SPEED, {
+	widgetsAPI.registerWidgetArea( AREA_SPEED, {
 		title: 'Page Speed and Experience',
 		subtitle: 'How fast your home page loads, how quickly people can interact with your content, and how stable your content is.',
 	}, contexts );
 
-	widgetsApi.registerWidgetArea( AREA_EARNINGS, {
+	widgetsAPI.registerWidgetArea( AREA_EARNINGS, {
 		title: 'Earnings',
 		subtitle: 'How much your site earns.',
 	}, contexts );
