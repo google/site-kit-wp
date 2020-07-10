3a5c7394a0af80539b391de42f070a850b989d3f
Jesse Lee <jqlee85@gmail.com>
2020-07-10 00:08:55 -0700

    Update Adsense components.

---
+++ assets/js/modules/adsense/components/settings/SettingsSetupIncomplete.js
@@ -0,0 +1,59 @@
+/**
+ * AdSense Settings Setup Incomplete component.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { Fragment } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import Data from 'googlesitekit-data';
+import Link from '../../../../components/link';
+import { STORE_NAME } from '../../datastore/constants';
+import { isPendingAccountStatus } from '../../util/status';
+import { AdBlockerWarning } from '../common';
+const { useSelect } = Data;
+
+export default function SettingsSetupIncomplete() {
+	const accountStatus = useSelect( ( select ) => select( STORE_NAME ).getAccountStatus() );
+	const isPendingStatus = isPendingAccountStatus( accountStatus );
+	const adminReauthURL = useSelect( ( select ) => select( STORE_NAME ).getAdminReauthURL() );
+
+	return (
+		<Fragment>
+			<AdBlockerWarning />
+
+			{ isPendingStatus && __( 'Site Kit has placed AdSense code on your site, ', 'google-site-kit' ) }
+			{ ! isPendingStatus && __( 'Setup incomplete: ', 'google-site-kit' ) }
+
+			<Link
+				className="googlesitekit-settings-module__edit-button"
+				href={
+					adminReauthURL
+				}
+				inherit
+			>
+				{ isPendingStatus && __( 'check module page', 'google-site-kit' ) }
+				{ ! isPendingStatus && __( 'continue module setup', 'google-site-kit' ) }
+			</Link>
+		</Fragment>
+	);
+}
---
+++ assets/js/modules/adsense/components/setup/SetupAccountNoClient.js
@@ -0,0 +1,60 @@
+/**
+ * AdSense Setup Account No Client component.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { Fragment } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import Link from '../../../../components/link';
+import { sanitizeHTML } from '../../../../util/sanitize';
+import { ErrorNotice } from '../common';
+
+export default function SetupAccountNoClient() {
+	return (
+		<Fragment>
+			<h3 className="googlesitekit-heading-4 googlesitekit-setup-module__title">
+				{ __( 'Looks like you have an AdMob account', 'google-site-kit' ) }
+			</h3>
+
+			<ErrorNotice />
+
+			<p>
+				{ __( 'To start using AdSense, you need to update your account so that you can connect your site to AdSense.', 'google-site-kit' ) }
+			</p>
+
+			<div className="googlesitekit-setup-module__action">
+				<Link
+					href="https://support.google.com/adsense/answer/6023158"
+					external
+					dangerouslySetInnerHTML={ sanitizeHTML(
+						__( 'Learn more<span class="screen-reader-text"> about updating your AdSense account</span>', 'google-site-kit' ),
+						{
+							ALLOWED_TAGS: [ 'span' ],
+							ALLOWED_ATTR: [ 'class' ],
+						}
+					) }
+				/>
+			</div>
+		</Fragment>
+	);
+}
