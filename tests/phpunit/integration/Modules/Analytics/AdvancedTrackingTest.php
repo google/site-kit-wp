3d5dc4f0f73de450a222cd2cd615c12f9c8834a2
Yizhuo Du <michael03160525@gmail.com>
2020-07-13 22:52:53 +0000

    Change the permission of the files in the subdirectory /bin/local-env/ to 100750.

--- tests/phpunit/integration/Modules/Analytics/AdvancedTrackingTest.php
+++ tests/phpunit/integration/Modules/Analytics/AdvancedTrackingTest.php
@@ -146,10 +146,14 @@ for ( config of eventConfigurations ) {
 	document.addEventListener( config.on, function( e ) {
 		if ( e.target.matches( thisConfig.selector ) ) {
 			alert( 'Got an event called: '.concat( thisConfig.action ) );
-			//record event with gtag here
+			gtag( 'event', thisConfig.action, {
+			 	'event_category': thisConfig.category
+			 });
 		} else if ( e.target.matches( thisConfig.selector.concat( ' *' ) ) ) {
 			alert( 'Got an event called: '.concat( thisConfig.action ) );
-			//record event with gtag here
+			gtag( 'event', thisConfig.action, {
+			 	'event_category': thisConfig.category
+			 });
 		}
 	}, true );
 }
