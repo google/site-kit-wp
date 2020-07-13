f556619ea0334c33b1296b228fb1e6fb514acfd7
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-07-13 15:22:13 +0300

    Update DashboardPageSpeedCTA component to use datastores.

--- assets/js/modules/pagespeed-insights/components/DashboardPageSpeedCTA.js
+++ assets/js/modules/pagespeed-insights/components/DashboardPageSpeedCTA.js
@@ -6,23 +6,22 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import Data from 'googlesitekit-data';
 import {
 	activateOrDeactivateModule,
 	getReAuthURL,
 	showErrorNotification,
-	getModulesData,
 } from '../../../util';
 import CTA from '../../../components/notifications/cta';
 import data from '../../../components/data';
 import GenericError from '../../../components/notifications/generic-error';
+import { STORE_NAME as MODULES_STORE } from '../../../googlesitekit/modules/datastore/constants';
+import { STORE_NAME as USER_STORE } from '../../../googlesitekit/datastore/user/constants';
+const { useSelect } = Data;
 
 function DashboardPageSpeedCTA() {
-	const {
-		active,
-		setupComplete,
-	} = getModulesData()[ 'pagespeed-insights' ];
-
-	const { canManageOptions } = global._googlesitekitLegacyData.permissions;
+	const { active, setupComplete } = useSelect( ( select ) => select( MODULES_STORE ).getModule( 'pagespeed-insights' ) );
+	const canManageOptions = useSelect( ( select ) => select( USER_STORE ).hasCapability( 'canManageOptions' ) );
 
 	if ( ! canManageOptions && ! setupComplete ) {
 		return null;
