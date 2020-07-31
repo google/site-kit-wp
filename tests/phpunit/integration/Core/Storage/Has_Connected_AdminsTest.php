522dc75cbf7cff8f33425f60c8a320d8c3ccec90
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-07-31 13:23:51 +0300

    Rework phpunit tests.

--- tests/phpunit/integration/Core/Storage/Has_Connected_AdminsTest.php
+++ tests/phpunit/integration/Core/Storage/Has_Connected_AdminsTest.php
@@ -11,6 +11,7 @@
 namespace Google\Site_Kit\Tests\Core\Storage;
 
 use Google\Site_Kit\Context;
+use Google\Site_Kit\Core\Authentication\Clients\OAuth_Client;
 use Google\Site_Kit\Core\Storage\Options;
 use Google\Site_Kit\Core\Storage\User_Options;
 use Google\Site_Kit\Core\Storage\Has_Connected_Admins;
@@ -44,26 +45,41 @@ class Has_Connected_AdminsTest extends TestCase {
 		$this->user_options = new User_Options( $this->context );
 	}
 
-	public function test_get_type() {
-		$setting = new FakeHas_Connected_Admins( $this->options, $this->user_options );
-		$this->assertEquals( 'boolean', $setting->get_type() );
+	public function tearDown() {
+		parent::tearDown();
+		delete_option( Has_Connected_Admins::OPTION );
 	}
 
 	public function test_get__without_option_value_yet() {
-		$setting = new FakeHas_Connected_Admins( $this->options, $this->user_options );
+		$setting = new Has_Connected_Admins( $this->options, $this->user_options );
 
-		$setting->set_query_connected_admins_return_value( array() );
 		delete_option( Has_Connected_Admins::OPTION );
 		$this->assertFalse( $setting->get() );
 
-		$setting->set_query_connected_admins_return_value( array( 1 ) );
+		$user_id = wp_insert_user(
+			array(
+				'user_login' => 'test_admin',
+				'user_email' => 'test_admin@example.com',
+				'user_pass'  => 'password',
+				'role'       => 'administrator',
+			)
+		);
+
+		add_user_meta(
+			$user_id,
+			$this->user_options->get_meta_key( OAuth_Client::OPTION_ACCESS_TOKEN ),
+			'xxxxx'
+		);
+
 		delete_option( Has_Connected_Admins::OPTION );
 		$this->assertTrue( $setting->get() );
 		$this->assertTrue( get_option( Has_Connected_Admins::OPTION ) );
+
+		wp_delete_user( $user_id );
 	}
 
 	public function test_get__with_option_value() {
-		$setting = new FakeHas_Connected_Admins( $this->options, $this->user_options );
+		$setting = new Has_Connected_Admins( $this->options, $this->user_options );
 		update_option( Has_Connected_Admins::OPTION, true );
 
 		$this->assertTrue( $setting->get() );
