6006e518449859c9de491e02ccba499e3f38e3a2
Ryan Welcher <ryan.welcher@10up.com>
2020-08-04 10:08:23 -0400

    Convert component to functional and use selector.

--- assets/js/modules/analytics/components/dashboard/AnalyticsAdSenseDashboardWidgetTopPagesTable.js
+++ assets/js/modules/analytics/components/dashboard/AnalyticsAdSenseDashboardWidgetTopPagesTable.js
@@ -25,11 +25,11 @@ import { map } from 'lodash';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
+import Data from 'googlesitekit-data';
 import { getTimeInSeconds, numberFormat, getModulesData } from '../../../../util';
 import withData from '../../../../components/higherorder/withdata';
 import { TYPE_MODULES } from '../../../../components/data';
@@ -39,99 +39,107 @@ import PreviewTable from '../../../../components/preview-table';
 import ctaWrapper from '../../../../components/notifications/cta-wrapper';
 import AdSenseLinkCTA from '../common/AdSenseLinkCTA';
 import { analyticsAdsenseReportDataDefaults, isDataZeroForReporting } from '../../util';
+import { STORE_NAME } from '../../datastore/constants';
+import { STORE_NAME as ADSENSE_STORE } from '../../../adsense/datastore/constants';
+import { STORE_NAME as SEARCH_CONSOLE_STORE } from '../../../search-console/datastore/constants';
 
-class AnalyticsAdSenseDashboardWidgetTopPagesTable extends Component {
-	static renderLayout( component ) {
-		const { accountURL } = getModulesData().adsense;
-		return (
-			<Layout
-				header
-				title={ __( 'Performance over previous 28 days', 'google-site-kit' ) }
-				headerCtaLabel={ __( 'Advanced Settings', 'google-site-kit' ) }
-				headerCtaLink={ accountURL }
-			>
-				{ component }
-			</Layout>
-		);
-	}
+const { useSelect } = Data;
 
-	render() {
-		const { data } = this.props;
-
-		// Do not return zero data callout here since it will already be
-		// present on the page from other sources.
-		if ( isDataZeroForReporting( data ) ) {
-			return null;
+const RenderLayout = ( { children } ) => {
+	const adSenseAccountID = useSelect( ( select ) => select( ADSENSE_STORE ).getAccountID() );
+	const propertyURL = useSelect( ( select ) => select( SEARCH_CONSOLE_STORE ).getPropertyID() );
+	const accountURL = useSelect( ( select ) => select( ADSENSE_STORE ).getServiceURL(
+		{
+			path: `/${ adSenseAccountID }/home`,
+			query: {
+				source: 'site-kit',
+				url: propertyURL,
+			},
 		}
+	) );
+	return (
+		<Layout
+			header
+			title={ __( 'Performance over previous 28 days', 'google-site-kit' ) }
+			headerCtaLabel={ __( 'Advanced Settings', 'google-site-kit' ) }
+			headerCtaLink={ accountURL }>
+			{ children }
+		</Layout>
+	);
+};
 
-		const headers = [
-			{
-				title: __( 'Page Title', 'google-site-kit' ),
-				tooltip: __( 'Page Title', 'google-site-kit' ),
-				primary: true,
-			},
-			{
-				title: __( 'Earnings', 'google-site-kit' ),
-				tooltip: __( 'Earnings', 'google-site-kit' ),
-			},
-			{
-				title: __( 'Page RPM', 'google-site-kit' ),
-				tooltip: __( 'Page RPM', 'google-site-kit' ),
-			},
-			{
-				title: __( 'Impressions', 'google-site-kit' ),
-				tooltip: __( 'Impressions', 'google-site-kit' ),
-			},
-		];
+const AnalyticsAdSenseDashboardWidgetTopPagesTable = ( { data } ) => {
+	const accountID = useSelect( ( select ) => select( STORE_NAME ).getAccountID() );
+	const profileID = useSelect( ( select ) => select( STORE_NAME ).getProfileID() );
+	const internalWebPropertyID = useSelect( ( select ) => select( STORE_NAME ).getInternalWebPropertyID() );
 
-		const dataMapped = map( data[ 0 ].data.rows, ( row ) => {
-			/**
-			 * dimensions[0] = ga:pageTitle
-			 * dimensions[1] = ga:pagePath
-			 *
-			 * metrics[0] = ga:adsenseECPM
-			 * metrics[1] = ga:adsensePageImpressions
-			 * metrics[2] = ga:adsenseRevenue
-			 */
-			return [
-				row.dimensions[ 0 ],
-				Number( row.metrics[ 0 ].values[ 0 ] ).toFixed( 2 ),
-				Number( row.metrics[ 0 ].values[ 1 ] ).toFixed( 2 ),
-				numberFormat( row.metrics[ 0 ].values[ 2 ] ),
-			];
-		} );
-
-		const {
-			accountID,
-			internalWebPropertyID,
-			profileID,
-		} = getModulesData().analytics.settings;
-
-		// Construct a deep link.
-		const adsenseDeepLink = `https://analytics.google.com/analytics/web/?pli=1#/report/content-pages/a${ accountID }w${ internalWebPropertyID }p${ profileID }/explorer-table.plotKeys=%5B%5D&_r.drilldown=analytics.pagePath:~2F`;
-
-		const linksMapped = map( data[ 0 ].data.rows, ( row ) => {
-			const pagePath = row.dimensions[ 1 ].replace( /\//g, '~2F' );
-			return adsenseDeepLink + pagePath;
-		} );
-
-		const options = {
-			hideHeader: false,
-			chartsEnabled: false,
-			links: linksMapped,
-		};
-
-		const dataTable = getDataTableFromData( dataMapped, headers, options );
-
-		return (
-			AnalyticsAdSenseDashboardWidgetTopPagesTable.renderLayout(
-				<TableOverflowContainer>
-					{ dataTable }
-				</TableOverflowContainer>
-			)
-		);
+	const adsenseDeepLink = useSelect( ( select ) => select( STORE_NAME ).getServiceURL(
+		{ path: `/report/content-pages/a${ accountID }w${ internalWebPropertyID }p${ profileID }/explorer-table.plotKeys=%5B%5D&_r.drilldown=analytics.pagePath:~2F` }
+	) );
+	// Do not return zero data callout here since it will already be
+	// present on the page from other sources.
+	if ( isDataZeroForReporting( data ) ) {
+		return null;
 	}
-}
+
+	const headers = [
+		{
+			title: __( 'Page Title', 'google-site-kit' ),
+			tooltip: __( 'Page Title', 'google-site-kit' ),
+			primary: true,
+		},
+		{
+			title: __( 'Earnings', 'google-site-kit' ),
+			tooltip: __( 'Earnings', 'google-site-kit' ),
+		},
+		{
+			title: __( 'Page RPM', 'google-site-kit' ),
+			tooltip: __( 'Page RPM', 'google-site-kit' ),
+		},
+		{
+			title: __( 'Impressions', 'google-site-kit' ),
+			tooltip: __( 'Impressions', 'google-site-kit' ),
+		},
+	];
+
+	const dataMapped = map( data[ 0 ].data.rows, ( row ) => {
+		/**
+		 * dimensions[0] = ga:pageTitle
+		 * dimensions[1] = ga:pagePath
+		 *
+		 * metrics[0] = ga:adsenseECPM
+		 * metrics[1] = ga:adsensePageImpressions
+		 * metrics[2] = ga:adsenseRevenue
+		 */
+		return [
+			row.dimensions[ 0 ],
+			Number( row.metrics[ 0 ].values[ 0 ] ).toFixed( 2 ),
+			Number( row.metrics[ 0 ].values[ 1 ] ).toFixed( 2 ),
+			numberFormat( row.metrics[ 0 ].values[ 2 ] ),
+		];
+	} );
+
+	const linksMapped = map( data[ 0 ].data.rows, ( row ) => {
+		const pagePath = row.dimensions[ 1 ].replace( /\//g, '~2F' );
+		return adsenseDeepLink + pagePath;
+	} );
+
+	const options = {
+		hideHeader: false,
+		chartsEnabled: false,
+		links: linksMapped,
+	};
+
+	const dataTable = getDataTableFromData( dataMapped, headers, options );
+
+	return (
+		<RenderLayout>
+			<TableOverflowContainer>
+				{ dataTable }
+			</TableOverflowContainer>
+		</RenderLayout>
+	);
+};
 
 /**
  * Check error data response, and handle the INVALID_ARGUMENT specifically.
@@ -141,16 +149,14 @@ class AnalyticsAdSenseDashboardWidgetTopPagesTable extends Component {
  * @return {(string|boolean|null)}  Returns a string with an error message if there is an error. Returns `false` when there is no data and no error message. Will return `null` when arguments are invalid.
  *                            string   data error message if it exists or unidentified error.
  *                            false    if no data and no error message
- *                            null     if invalid agument
+ *                            null     if invalid argument
  *
  */
 const getDataError = ( data ) => {
 	if ( data.code && data.message && data.data && data.data.status ) {
 		// Specifically looking for string "badRequest"
 		if ( 'badRequest' === data.data.reason ) {
-			return AnalyticsAdSenseDashboardWidgetTopPagesTable.renderLayout(
-				ctaWrapper( <AdSenseLinkCTA />, false, false, true )
-			);
+			return <RenderLayout>{ ctaWrapper( <AdSenseLinkCTA />, false, false, true ) }</RenderLayout>;
 		}
 
 		return data.message;
@@ -190,9 +196,7 @@ export default withData(
 			context: 'Single',
 		},
 	],
-	AnalyticsAdSenseDashboardWidgetTopPagesTable.renderLayout(
-		<PreviewTable padding />
-	),
+	<RenderLayout><PreviewTable padding /></RenderLayout>,
 	{ createGrid: true },
 	// Force isDataZero to false since it is handled within the component.
 	() => false,
