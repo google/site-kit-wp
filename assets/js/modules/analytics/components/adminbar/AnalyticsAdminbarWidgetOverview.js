d18442961610d3ce4507c40dc67ad82e873d24f3
Adam Silverstein <adamjs@google.com>
2020-08-05 07:23:05 -0600

    Update assets/js/modules/analytics/components/adminbar/AnalyticsAdminbarWidgetOverview.js
    Co-authored-by: Evan Mattson <evan.mattson@10up.com>

--- assets/js/modules/analytics/components/adminbar/AnalyticsAdminbarWidgetOverview.js
+++ assets/js/modules/analytics/components/adminbar/AnalyticsAdminbarWidgetOverview.js
@@ -157,7 +157,7 @@ export default withData(
 			maxAge: getTimeInSeconds( 'day' ),
 			context: 'Adminbar',
 			toState( state, { data } ) {
-				if ( ! state.totalUsers ) {
+				if ( false === state.totalUsers ) {
 					return parseTotalUsersData( data );
 				}
 			},
