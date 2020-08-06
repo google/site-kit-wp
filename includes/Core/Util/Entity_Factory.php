b67286f8d2607e7fb50edd25d24a2842f2879893
Felix Arntz <felixarntz@google.com>
2020-08-05 21:41:00 -0700

    Introduce Entity_Factory::url_to_query_args() helper method.

--- includes/Core/Util/Entity_Factory.php
+++ includes/Core/Util/Entity_Factory.php
@@ -12,6 +12,7 @@ namespace Google\Site_Kit\Core\Util;
 
 use Google\Site_Kit\Context;
 use WP_Post;
+use WP_Query;
 
 /**
  * Class providing access to entities.
@@ -68,6 +69,8 @@ final class Entity_Factory {
 	/**
 	 * Gets the entity for the given URL, if available.
 	 *
+	 * Calling this method is expensive, so it should only be used in certain admin contexts where this is acceptable.
+	 *
 	 * @since n.e.x.t
 	 *
 	 * @param string $url URL to determine the entity from.
@@ -103,6 +106,171 @@ final class Entity_Factory {
 		}
 
 		return null;
+	}
+
+	/**
+	 * Determines the WordPress query arguments to use for a given URL.
+	 *
+	 * This is an expensive function that works similarly to WordPress core's `url_to_postid()` function, however also
+	 * covering non-post URLs. It follows logic used in `WP::parse_request()` to cover the other kinds of URLs. The
+	 * majority of the code is a direct copy of certain parts of these functions.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $url URL to get WordPress query object for.
+	 * @return array|null Associative array of WP_Query arguments, or null if unable to parse query from URL.
+	 */
+	private static function url_to_query_args( $url ) {
+		global $wp, $wp_rewrite;
+
+		$url_host      = str_replace( 'www.', '', wp_parse_url( $url, PHP_URL_HOST ) );
+		$home_url_host = str_replace( 'www.', '', wp_parse_url( home_url(), PHP_URL_HOST ) );
+
+		// Bail early if the URL does not belong to this site.
+		if ( $url_host && $url_host !== $home_url_host ) {
+			return null;
+		}
+
+		// Strip 'index.php/' if we're not using path info permalinks.
+		if ( ! $wp_rewrite->using_index_permalinks() ) {
+			$url = str_replace( $wp_rewrite->index . '/', '', $url );
+		}
+
+		$url_path  = wp_parse_url( $url, PHP_URL_PATH );
+		$url_query = wp_parse_url( $url, PHP_URL_QUERY );
+
+		// Strip potential home URL path segment from URL path.
+		$home_path = untrailingslashit( wp_parse_url( home_url( '/' ), PHP_URL_PATH ) );
+		if ( ! empty( $home_path ) ) {
+			$url_path = substr( $url_path, strlen( $home_path ) );
+		}
+
+		// Strip leading and trailing slashes.
+		$url_path = trim( $url_path, '/' );
+
+		// These two variables will be used further down to determine actual WP_Query arguments.
+		$url_path_vars  = array();
+		$url_query_vars = array();
+
+		// Fetch the rewrite rules.
+		$rewrite = $wp_rewrite->wp_rewrite_rules();
+
+		// Match path against rewrite rules.
+		$matched_rule = '';
+		$query        = '';
+		$matches      = array();
+		if ( empty( $url_path ) || $url_path === $wp_rewrite->index ) {
+			if ( isset( $rewrite['$'] ) ) {
+				$matched_rule = '$';
+				$query        = $rewrite['$'];
+				$matches      = array( '' );
+			}
+		} else {
+			foreach ( (array) $rewrite as $match => $query ) {
+				if ( preg_match( "#^$match#", $url_path, $matches ) ) {
+					if ( $wp_rewrite->use_verbose_page_rules && preg_match( '/pagename=\$matches\[([0-9]+)\]/', $query, $varmatch ) ) {
+						// This is a verbose page match, let's check to be sure about it.
+						// We'll rely 100% on WP core functions here.
+						// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions
+						$page = get_page_by_path( $matches[ $varmatch[1] ] );
+						if ( ! $page ) {
+							continue;
+						}
+
+						$post_status_obj = get_post_status_object( $page->post_status );
+						if ( ! $post_status_obj->public && ! $post_status_obj->protected
+							&& ! $post_status_obj->private && $post_status_obj->exclude_from_search ) {
+							continue;
+						}
+					}
+
+					$matched_rule = $match;
+					break;
+				}
+			}
+		}
+
+		// If rewrite rules matched, populate $url_path_vars.
+		if ( $matched_rule ) {
+			// Trim the query of everything up to the '?'.
+			$query = preg_replace( '!^.+\?!', '', $query );
+
+			// Substitute the substring matches into the query.
+			$query = addslashes( \WP_MatchesMapRegex::apply( $query, $matches ) );
+
+			parse_str( $query, $url_path_vars );
+		}
+
+		// If there is a URL query string, populate $url_query_vars.
+		if ( $url_query ) {
+			parse_str( $url_query, $url_query_vars );
+		}
+
+		// Determine available post type query vars.
+		$post_type_query_vars = array();
+		foreach ( get_post_types( array(), 'objects' ) as $post_type => $post_type_obj ) {
+			if ( is_post_type_viewable( $post_type_obj ) && $post_type_obj->query_var ) {
+				$post_type_query_vars[ $post_type_obj->query_var ] = $post_type;
+			}
+		}
+
+		// Populate actual WP_Query arguments.
+		$query_args = array();
+		foreach ( $wp->public_query_vars as $wpvar ) {
+			if ( isset( $url_query_vars[ $wpvar ] ) ) {
+				$query_args[ $wpvar ] = $url_query_vars[ $wpvar ];
+			} elseif ( isset( $url_path_vars[ $wpvar ] ) ) {
+				$query_args[ $wpvar ] = $url_path_vars[ $wpvar ];
+			}
+
+			if ( ! empty( $query_args[ $wpvar ] ) ) {
+				if ( ! is_array( $query_args[ $wpvar ] ) ) {
+					$query_args[ $wpvar ] = (string) $query_args[ $wpvar ];
+				} else {
+					foreach ( $query_args[ $wpvar ] as $key => $value ) {
+						if ( is_scalar( $value ) ) {
+							$query_args[ $wpvar ][ $key ] = (string) $value;
+						}
+					}
+				}
+
+				if ( isset( $post_type_query_vars[ $wpvar ] ) ) {
+					$query_args['post_type'] = $post_type_query_vars[ $wpvar ];
+					$query_args['name']      = $query_args[ $wpvar ];
+				}
+			}
+		}
+
+		// Convert urldecoded spaces back into '+'.
+		foreach ( get_taxonomies( array(), 'objects' ) as $taxonomy => $taxonomy_obj ) {
+			if ( $taxonomy_obj->query_var && isset( $query_args[ $taxonomy_obj->query_var ] ) ) {
+				$query_args[ $taxonomy_obj->query_var ] = str_replace( ' ', '+', $query_args[ $taxonomy_obj->query_var ] );
+			}
+		}
+
+		// Don't allow non-publicly queryable taxonomies to be queried from the front end.
+		foreach ( get_taxonomies( array( 'publicly_queryable' => false ), 'objects' ) as $taxonomy => $t ) {
+			if ( isset( $query_args['taxonomy'] ) && $taxonomy === $query_args['taxonomy'] ) {
+				unset( $query_args['taxonomy'], $query_args['term'] );
+			}
+		}
+
+		// Limit publicly queried post_types to those that are 'publicly_queryable'.
+		if ( isset( $query_args['post_type'] ) ) {
+			$queryable_post_types = get_post_types( array( 'publicly_queryable' => true ) );
+			if ( ! is_array( $query_args['post_type'] ) ) {
+				if ( ! in_array( $query_args['post_type'], $queryable_post_types, true ) ) {
+					unset( $query_args['post_type'] );
+				}
+			} else {
+				$query_args['post_type'] = array_intersect( $query_args['post_type'], $queryable_post_types );
+			}
+		}
+
+		// Resolve conflicts between posts with numeric slugs and date archive queries.
+		$query_args = wp_resolve_numeric_slug_conflicts( $query_args );
+
+		return $query_args;
 	}
 
 	/**
---
+++ tests/phpunit/integration/Core/Util/Entity_FactoryTest.php
@@ -0,0 +1,429 @@
+<?php
+/**
+ * Class Google\Site_Kit\Tests\Core\Util\Entity_FactoryTest
+ *
+ * @package   Google\Site_Kit\Tests\Core\Util
+ * @copyright 2020 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests\Core\Util;
+
+use Google\Site_Kit\Core\Util\Entity_Factory;
+use Google\Site_Kit\Tests\TestCase;
+
+/**
+ * @group Util
+ */
+class Entity_FactoryTest extends TestCase {
+
+	/**
+	 * @dataProvider data_url_to_query_args
+	 *
+	 * @param string $url           URL to get query arguments.
+	 * @param array  $expected_args Expected query arguments.
+	 * @param array  $template_tags Map of template tag method names and whether they should be `true` or `false`.
+	 */
+	public function test_url_to_query_args( $url, $expected_args, $template_tags ) {
+		global $wp_rewrite;
+
+		add_filter(
+			'option_home',
+			function() {
+				return 'https://example.com';
+			}
+		);
+
+		$wp_rewrite->set_permalink_structure( '/%postname%/' );
+
+		$blog_id = self::factory()->post->create(
+			array(
+				'post_title' => 'Blog',
+				'post_name'  => 'blog',
+				'post_type'  => 'page',
+			)
+		);
+		$home_id = self::factory()->post->create(
+			array(
+				'post_title' => 'Home',
+				'post_name'  => 'home',
+				'post_type'  => 'page',
+			)
+		);
+		update_option( 'show_on_front', 'page' );
+		update_option( 'page_on_front', $home_id );
+		update_option( 'page_for_posts', $blog_id );
+
+		register_post_type(
+			'customposttype',
+			array(
+				'public'      => true,
+				'has_archive' => true,
+			)
+		);
+
+		register_taxonomy(
+			'customtaxonomy',
+			'post',
+			array(
+				'public' => true,
+			)
+		);
+
+		flush_rewrite_rules();
+
+		$url_to_query_args = new \ReflectionMethod( Entity_Factory::class, 'url_to_query_args' );
+		$url_to_query_args->setAccessible( true );
+
+		$query_args = $url_to_query_args->invoke( null, $url );
+
+		$this->assertEqualSetsWithIndex( $expected_args, $query_args );
+
+		$query = new \WP_Query();
+		$query->query( $query_args );
+
+		foreach ( $template_tags as $template_tag => $expected_result ) {
+			if ( $expected_result ) {
+				$this->assertTrue( $query->$template_tag(), "Failed asserting that $template_tag is true." );
+			} else {
+				$this->assertFalse( $query->$template_tag(), "Failed asserting that $template_tag is false." );
+			}
+		}
+	}
+
+	public function data_url_to_query_args() {
+		// Home URL is 'https://example.com'.
+		// Permalink structure is '/%postname%/'.
+		// Front page is a static page with slug 'home'.
+		// Posts page has the slug 'blog'.
+		// There is a custom post type called 'customposttype'.
+		// There is a custom taxonomy called 'customtaxonomy'.
+		return array(
+			'front page'                 => array(
+				'https://example.com/',
+				array(),
+				array(
+					'is_front_page' => true,
+					'is_page'       => true,
+					'is_home'       => false,
+					'is_singular'   => true,
+				),
+			),
+			'blog page'                  => array(
+				'https://example.com/blog/',
+				array(
+					'pagename' => 'blog',
+					'page'     => '',
+				),
+				array(
+					'is_front_page' => false,
+					'is_home'       => true,
+					'is_paged'      => false,
+					'is_singular'   => false,
+				),
+			),
+			'blog page, page 3'          => array(
+				'https://example.com/blog/page/3/',
+				array(
+					'pagename' => 'blog',
+					'paged'    => '3',
+				),
+				array(
+					'is_front_page' => false,
+					'is_home'       => true,
+					'is_paged'      => true,
+					'is_singular'   => false,
+				),
+			),
+			'single post'                => array(
+				'https://example.com/some-post/',
+				array(
+					'name' => 'some-post',
+					'page' => '',
+				),
+				array(
+					'is_single'   => true,
+					'is_singular' => true,
+				),
+			),
+			'author archives'            => array(
+				'https://example.com/author/johndoe/',
+				array(
+					'author_name' => 'johndoe',
+				),
+				array(
+					'is_author'   => true,
+					'is_paged'    => false,
+					'is_singular' => false,
+				),
+			),
+			'author archives, page 2'    => array(
+				'https://example.com/author/johndoe/page/2/',
+				array(
+					'author_name' => 'johndoe',
+					'paged'       => '2',
+				),
+				array(
+					'is_author'   => true,
+					'is_paged'    => true,
+					'is_singular' => false,
+				),
+			),
+			'year archives'              => array(
+				'https://example.com/2020/',
+				array(
+					'year' => '2020',
+				),
+				array(
+					'is_year'     => true,
+					'is_date'     => true,
+					'is_paged'    => false,
+					'is_singular' => false,
+				),
+			),
+			'year archives, page 3'      => array(
+				'https://example.com/2020/page/3/',
+				array(
+					'year'  => '2020',
+					'paged' => '3',
+				),
+				array(
+					'is_year'     => true,
+					'is_date'     => true,
+					'is_paged'    => true,
+					'is_singular' => false,
+				),
+			),
+			'month archives'             => array(
+				'https://example.com/2020/08/',
+				array(
+					'year'     => '2020',
+					'monthnum' => '08',
+				),
+				array(
+					'is_year'     => false,
+					'is_month'    => true,
+					'is_date'     => true,
+					'is_paged'    => false,
+					'is_singular' => false,
+				),
+			),
+			'month archives, page 3'     => array(
+				'https://example.com/2020/08/page/3/',
+				array(
+					'year'     => '2020',
+					'monthnum' => '08',
+					'paged'    => '3',
+				),
+				array(
+					'is_year'     => false,
+					'is_month'    => true,
+					'is_date'     => true,
+					'is_paged'    => true,
+					'is_singular' => false,
+				),
+			),
+			'day archives'               => array(
+				'https://example.com/2020/08/04/',
+				array(
+					'year'     => '2020',
+					'monthnum' => '08',
+					'day'      => '04',
+				),
+				array(
+					'is_year'     => false,
+					'is_month'    => false,
+					'is_day'      => true,
+					'is_date'     => true,
+					'is_paged'    => false,
+					'is_singular' => false,
+				),
+			),
+			'day archives, page 3'       => array(
+				'https://example.com/2020/08/04/page/3/',
+				array(
+					'year'     => '2020',
+					'monthnum' => '08',
+					'day'      => '04',
+					'paged'    => '3',
+				),
+				array(
+					'is_year'     => false,
+					'is_month'    => false,
+					'is_day'      => true,
+					'is_date'     => true,
+					'is_paged'    => true,
+					'is_singular' => false,
+				),
+			),
+			'custom post type post'      => array(
+				'https://example.com/customposttype/coffee/',
+				array(
+					'customposttype' => 'coffee',
+					'post_type'      => 'customposttype',
+					'name'           => 'coffee',
+					'page'           => '',
+				),
+				array(
+					'is_single'   => true,
+					'is_singular' => true,
+				),
+			),
+			'post type archives'         => array(
+				'https://example.com/customposttype/',
+				array(
+					'post_type' => 'customposttype',
+				),
+				array(
+					'is_post_type_archive' => true,
+					'is_paged'             => false,
+					'is_singular'          => false,
+				),
+			),
+			'post type archives, page 3' => array(
+				'https://example.com/customposttype/page/3/',
+				array(
+					'post_type' => 'customposttype',
+					'paged'     => '3',
+				),
+				array(
+					'is_post_type_archive' => true,
+					'is_paged'             => true,
+					'is_singular'          => false,
+				),
+			),
+			'taxonomy archives'          => array(
+				'https://example.com/customtaxonomy/coffee/',
+				array(
+					'customtaxonomy' => 'coffee',
+				),
+				array(
+					'is_tax'      => true,
+					'is_paged'    => false,
+					'is_singular' => false,
+				),
+			),
+			'taxonomy archives, page 3'  => array(
+				'https://example.com/customtaxonomy/coffee/page/3/',
+				array(
+					'customtaxonomy' => 'coffee',
+					'paged'          => '3',
+				),
+				array(
+					'is_tax'      => true,
+					'is_paged'    => true,
+					'is_singular' => false,
+				),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider data_url_to_query_args_homepage
+	 *
+	 * @param string $url           URL to get query arguments.
+	 * @param string $show_on_front Either 'posts' or 'page'.
+	 * @param array  $expected_args Expected query arguments.
+	 */
+	public function test_url_to_query_args_homepage( $url, $show_on_front, $expected_args ) {
+		global $wp_rewrite;
+
+		add_filter(
+			'option_home',
+			function() {
+				return 'https://example.com';
+			}
+		);
+
+		$wp_rewrite->set_permalink_structure( '/%postname%/' );
+
+		if ( 'page' === $show_on_front ) {
+			$home_id = self::factory()->post->create(
+				array(
+					'post_title' => 'Home',
+					'post_type'  => 'page',
+				)
+			);
+			update_option( 'show_on_front', 'page' );
+			update_option( 'page_on_front', $home_id );
+		} else {
+			update_option( 'show_on_front', 'posts' );
+		}
+
+		flush_rewrite_rules();
+
+		$url_to_query_args = new \ReflectionMethod( Entity_Factory::class, 'url_to_query_args' );
+		$url_to_query_args->setAccessible( true );
+
+		$query_args = $url_to_query_args->invoke( null, $url );
+
+		$this->assertEqualSetsWithIndex( $expected_args, $query_args );
+
+		$query = new \WP_Query();
+		$query->query( $query_args );
+
+		$this->assertTrue( $query->is_front_page() );
+		if ( 'page' === $show_on_front ) {
+			$this->assertTrue( $query->is_page() );
+			$this->assertFalse( $query->is_home() );
+		} else {
+			$this->assertTrue( $query->is_home() );
+			$this->assertFalse( $query->is_page() );
+		}
+	}
+
+	public function data_url_to_query_args_homepage() {
+		// Home URL is 'https://example.com'.
+		// Permalink structure is '/%postname%/'.
+		return array(
+			'posts'                       => array(
+				'https://example.com',
+				'posts',
+				array(),
+			),
+			'posts with trailing slash'   => array(
+				'https://example.com/',
+				'posts',
+				array(),
+			),
+			'posts with different scheme' => array(
+				'http://example.com',
+				'posts',
+				array(),
+			),
+			'posts with index.php'        => array(
+				'https://example.com/index.php',
+				'posts',
+				array(),
+			),
+			'posts, page 5'               => array(
+				'https://example.com/page/5',
+				'posts',
+				array(
+					'paged' => '5',
+				),
+			),
+			'page'                        => array(
+				'https://example.com',
+				'page',
+				array(),
+			),
+			'page with trailing slash'    => array(
+				'https://example.com/',
+				'page',
+				array(),
+			),
+			'page with different scheme'  => array(
+				'http://example.com',
+				'page',
+				array(),
+			),
+			'page with index.php'         => array(
+				'https://example.com/index.php',
+				'page',
+				array(),
+			),
+		);
+	}
+}
