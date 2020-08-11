7408105f287f26f862970375dab9e0b978c91205
Jesse Lee <jqlee85@gmail.com>
2020-08-11 12:11:02 -0700

    Update hook with force select logic.

--- assets/js/modules/analytics/hooks/useExistingTagEffect.js
+++ assets/js/modules/analytics/hooks/useExistingTagEffect.js
@@ -28,22 +28,34 @@ import { useEffect } from '@wordpress/element';
  */
 import Data from 'googlesitekit-data';
 import { STORE_NAME } from '../datastore/constants';
+import { STORE_NAME as CORE_MODULES } from '../../../googlesitekit/modules/datastore/constants';
+import { STORE_NAME as MODULES_TAGMANAGER } from '../../tagmanager/datastore/constants';
 const { useSelect, useDispatch } = Data;
 
 export default function useExistingTagEffect() {
+	// Check for existing Analytics tag.
 	const existingTag = useSelect( ( select ) => select( STORE_NAME ).getExistingTag() ) || {};
 	const existingTagPermission = useSelect( ( select ) => select( STORE_NAME ).getTagPermission( existingTag ) );
 	const hasExistingTag = useSelect( ( select ) => select( STORE_NAME ).hasExistingTag() );
 
+	// Check for existing Analytics tag added via Tagmanager.
+	const tagmanagerModuleActive = useSelect( ( select ) => select( CORE_MODULES ).isModuleActive( 'tagmanager' ) );
+	const gtmAnalyticsPropertyID = useSelect( ( select ) => select( MODULES_TAGMANAGER ).getSingleAnalyticsPropertyID() );
+	const gtmAnalyticsPropertyIDPermission = useSelect( ( select ) => select( STORE_NAME ).hasTagPermission( gtmAnalyticsPropertyID ) );
+
 	const { setAccountID, selectProperty } = useDispatch( STORE_NAME );
 	useEffect( () => {
-		if ( hasExistingTag && existingTagPermission ) {
-			const { accountID: existingTagAccountID } = existingTagPermission;
-			setAccountID( existingTagAccountID );
-			selectProperty( existingTag );
-		}
-
-		// Permission for a detected GTM property ID must also be checked if present with hasTagPermission( gtmAnalyticsPropertyID )
-		//An Analytics existing tag should take precedence over a detected property ID in GTM regarding the force-selected value(s)
+		( async () => {
+			// If there is an existing Analytics tag, select it.
+			if ( hasExistingTag && existingTagPermission ) {
+				const { accountID: existingTagAccountID } = existingTagPermission;
+				setAccountID( existingTagAccountID );
+				selectProperty( existingTag );
+			} else if ( tagmanagerModuleActive && gtmAnalyticsPropertyID && gtmAnalyticsPropertyIDPermission ) {
+				// If GTM container has GA tag and user has access to it, force select it.
+				await setAccountID( gtmAnalyticsPropertyIDPermission.accountID );
+				await selectProperty( gtmAnalyticsPropertyID );
+			}
+		} )();
 	}, [ hasExistingTag, existingTag, existingTagPermission ] );
 }
