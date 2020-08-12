34487a201f6076300daef08b8e0f7833ea7b8042
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-08-12 20:56:59 +0300

    Add license to the ErrorHandler component file.

--- assets/js/components/ErrorHandler/index.js
+++ assets/js/components/ErrorHandler/index.js
@@ -1,4 +1,22 @@
 /**
+ * ErrorHandler component.
+ *
+ * Google Site Kit, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * External dependencies
  */
 import PropTypes from 'prop-types';
@@ -56,7 +74,7 @@ class ErrorHandler extends Component {
 			</a>
 		);
 
-		const code = (
+		const errorInfo = (
 			// eslint-disable-next-line
 			<pre onClick={ this.errorClickHandler } style={ { overflow: 'auto' } }>
 				{ error.message + info.componentStack }
@@ -72,13 +90,11 @@ class ErrorHandler extends Component {
 				format="small"
 				type="win-error"
 			>
-				{ code }
+				{ errorInfo }
 			</Notification>
 		);
 	}
 }
-
-ErrorHandler.defaultProps = {};
 
 ErrorHandler.propTypes = {
 	/** @ignore */
