b5f6e5fa5e31e98be8fee9f03972ec2622c03e0c
Evan Mattson <evan.mattson@10up.com>
2020-08-24 18:42:58 +0300

    Add copyToClipboard utility.

---
+++ assets/js/util/copy-to-clipboard.js
@@ -0,0 +1,35 @@
+/**
+ * Copy to Clipboard utility.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Copies the given string of text to the clipboard.
+ *
+ * @since n.e.x.t
+ *
+ * @param {string} text Text to copy.
+ */
+export default function copyToClipboard( text ) {
+	const el = document.createElement( 'textarea' );
+	el.innerHTML = text;
+	document.body.appendChild( el );
+	el.select();
+
+	document.execCommand( 'copy' );
+
+	document.body.removeChild( el );
+}
