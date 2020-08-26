0c6649bbd2cb76cbf3e37930d88759d9d097c848
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-08-26 16:45:47 +0300

    Remove argument.

--- includes/Core/Storage/Setting_With_Owned_Keys_Trait.php
+++ includes/Core/Storage/Setting_With_Owned_Keys_Trait.php
@@ -34,17 +34,15 @@ trait Setting_With_Owned_Keys_Trait {
 	 * Registers hooks to determine an owner ID for a module.
 	 *
 	 * @since n.e.x.t
-	 *
-	 * @param string $owner_id_key Optional. The key to use for storing owner ID setting. Default 'ownerID'.
 	 */
-	protected function register_owned_keys( $owner_id_key = 'ownerID' ) {
+	protected function register_owned_keys() {
 		$keys = $this->get_owned_keys();
 
 		add_action(
 			'add_option_' . static::OPTION,
-			function ( $option, $value ) use ( $keys, $owner_id_key ) {
+			function ( $option, $value ) use ( $keys ) {
 				if ( is_array( $value ) && count( array_intersect( array_keys( $value ), $keys ) ) > 0 && current_user_can( Permissions::MANAGE_OPTIONS ) ) {
-					$this->merge( array( $owner_id_key => get_current_user_id() ) );
+					$this->merge( array( 'ownerID' => get_current_user_id() ) );
 				}
 			},
 			10,
@@ -53,11 +51,11 @@ trait Setting_With_Owned_Keys_Trait {
 
 		add_filter(
 			'pre_update_option_' . static::OPTION,
-			function( $value, $old_value ) use ( $keys, $owner_id_key ) {
+			function( $value, $old_value ) use ( $keys ) {
 				if ( is_array( $value ) && is_array( $old_value ) ) {
 					foreach ( $keys as $key ) {
 						if ( isset( $value[ $key ], $old_value[ $key ] ) && $value[ $key ] !== $old_value[ $key ] && current_user_can( Permissions::MANAGE_OPTIONS ) ) {
-							$value[ $owner_id_key ] = get_current_user_id();
+							$value['ownerID'] = get_current_user_id();
 							break;
 						}
 					}
