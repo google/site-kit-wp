97dd11e20d6fe4d2961e419acf790aeb00b0951f
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-08-26 21:11:17 +0300

    Update DashboardSummaryWidget widget.

--- assets/js/modules/adsense/components/dashboard/DashboardSummaryWidget.js
+++ assets/js/modules/adsense/components/dashboard/DashboardSummaryWidget.js
@@ -22,32 +22,67 @@
 import Data from 'googlesitekit-data';
 import { STORE_NAME } from '../../datastore/constants';
 import { STORE_NAME as CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
-import whenActive from '../../../../util/when-active';
+// import whenActive from '../../../../util/when-active';
 import ErrorText from '../../../../components/error-text';
+import PreviewBlock from '../../../../components/preview-block';
 const { useSelect } = Data;
 
 function DashboardSummaryWidget() {
-	const { error } = useSelect( ( select ) => {
+	const {
+		todayData,
+		todayError,
+		rangeData,
+		rangeError,
+		lastMonthData,
+		lastMonthError,
+	} = useSelect( ( select ) => {
 		const store = select( STORE_NAME );
-		const args = {
+
+		const now = new Date();
+		const today = `${ now.getFullYear() }-${ now.getMonth() + 1 }-${ now.getDate() }`;
+
+		now.setMonth( now.getMonth() - 1 );
+		const lastMonth = `${ now.getFullYear() }-${ now.getMonth() + 1 }-${ now.getDate() }`;
+
+		const todayArgs = {
+			startDate: today,
+			endDate: today,
+		};
+
+		const rangeArgs = {
 			dateRange: select( CORE_USER ).getDateRange(),
 		};
 
+		const lastMonthArgs = {
+			startDate: today,
+			endDate: lastMonth,
+			dimensions: [ 'DATE' ],
+		};
+
 		return {
-			report: store.getReport( args ),
-			error: store.getErrorForSelector( 'getReport', [ args ] ),
+			todayData: store.getReport( todayArgs ),
+			todayError: store.getErrorForSelector( 'getReport', [ todayArgs ] ),
+			rangeData: store.getReport( rangeArgs ),
+			rangeError: store.getErrorForSelector( 'getReport', [ rangeArgs ] ),
+			lastMonthData: store.getReport( lastMonthArgs ),
+			lastMonthError: store.getErrorForSelector( 'getReport', [ lastMonthArgs ] ),
 		};
 	} );
 
-	if ( error ) {
+	if ( todayError || rangeError || lastMonthError ) {
 		return (
 			<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-				<ErrorText message={ error.message } />
+				<ErrorText message={ ( todayError || rangeError || lastMonthError ).message } />
 			</div>
 		);
+	}
+
+	if ( ! todayData || ! rangeData || ! lastMonthData ) {
+		return <PreviewBlock width="100%" height="276px" />;
 	}
 
 	return 'DashboardSummaryWidget';
 }
 
-export default whenActive( { moduleName: 'adsence' } )( DashboardSummaryWidget );
+export default DashboardSummaryWidget;
+// export default whenActive( { moduleName: 'adsence' } )( DashboardSummaryWidget );
