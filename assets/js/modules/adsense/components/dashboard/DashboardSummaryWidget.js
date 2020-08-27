802ef8aae3ef0dcda14f35131214d7514195202d
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-08-27 18:55:47 +0300

    Implement summary widget itself.

--- assets/js/modules/adsense/components/dashboard/DashboardSummaryWidget.js
+++ assets/js/modules/adsense/components/dashboard/DashboardSummaryWidget.js
@@ -22,75 +22,159 @@
 import moment from 'moment';
 
 /**
+ * WordPress dependencies
+ */
+import { __, _x } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
 import { STORE_NAME } from '../../datastore/constants';
 import { STORE_NAME as CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
+import { reduceAdSenseData } from '../../util';
+import { readableLargeNumber, extractForSparkline, getSiteKitAdminURL } from '../../../../util';
 import whenActive from '../../../../util/when-active';
 import ErrorText from '../../../../components/error-text';
 import PreviewBlock from '../../../../components/preview-block';
+import Layout from '../../../../components/layout/layout';
+import DataBlock from '../../../../components/data-block';
+import Sparkline from '../../../../components/sparkline';
 const { useSelect } = Data;
 
 function DashboardSummaryWidget() {
 	const {
-		todayData,
+		today,
 		todayError,
-		rangeData,
-		rangeError,
-		lastMonthData,
-		lastMonthError,
+		period,
+		periodError,
+		daily,
+		dailyError,
 	} = useSelect( ( select ) => {
 		const store = select( STORE_NAME );
 		const metrics = [ 'EARNINGS', 'PAGE_VIEWS_RPM', 'IMPRESSIONS' ];
 
 		const format = 'YYYY-MM-DD';
-		const today = moment().format( format );
+		const now = moment().format( format );
 		const lastMonth = moment().subtract( 1, 'month' ).format( format );
 
-		global.console.log( today, lastMonth );
-
 		const todayArgs = {
-			startDate: today,
-			endDate: today,
+			startDate: now,
+			endDate: now,
 			metrics,
 		};
 
-		const rangeArgs = {
+		const periodArgs = {
 			dateRange: select( CORE_USER ).getDateRange(),
 			metrics,
 		};
 
-		const lastMonthArgs = {
+		const dailyArgs = {
 			startDate: lastMonth,
-			endDate: today,
-			dimensions: [ 'DATE' ],
+			endDate: now,
 			metrics,
+			dimensions: [ 'DATE' ],
 		};
 
 		return {
-			todayData: store.getReport( todayArgs ),
+			today: store.getReport( todayArgs ),
 			todayError: store.getErrorForSelector( 'getReport', [ todayArgs ] ),
-			rangeData: store.getReport( rangeArgs ),
-			rangeError: store.getErrorForSelector( 'getReport', [ rangeArgs ] ),
-			lastMonthData: store.getReport( lastMonthArgs ),
-			lastMonthError: store.getErrorForSelector( 'getReport', [ lastMonthArgs ] ),
+			period: store.getReport( periodArgs ),
+			periodError: store.getErrorForSelector( 'getReport', [ periodArgs ] ),
+			daily: store.getReport( dailyArgs ),
+			dailyError: store.getErrorForSelector( 'getReport', [ dailyArgs ] ),
 		};
 	} );
 
-	if ( todayError || rangeError || lastMonthError ) {
+	if ( todayError || periodError || dailyError ) {
 		return (
 			<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-				<ErrorText message={ ( todayError || rangeError || lastMonthError ).message } />
+				<ErrorText message={ ( todayError || periodError || dailyError ).message } />
 			</div>
 		);
 	}
 
-	if ( ! todayData || ! rangeData || ! lastMonthData ) {
+	if ( ! today || ! period || ! daily ) {
 		return <PreviewBlock width="100%" height="276px" />;
 	}
 
-	return 'DashboardSummaryWidget';
+	const processedData = reduceAdSenseData( daily.rows );
+	const href = getSiteKitAdminURL( 'googlesitekit-module-adsense', {} );
+
+	const currencyHeader = period.headers.find( ( header ) => null !== header.currency && 0 < header.currency.length );
+	const currencyCode = currencyHeader ? currencyHeader.currency : false;
+
+	return (
+		<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6-desktop mdc-layout-grid__cell--span-4-tablet">
+			<Layout className="googlesitekit-dashboard-adsense-stats" fill>
+				<div className="mdc-layout-grid">
+					<div className="mdc-layout-grid__inner">
+						<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+							<DataBlock
+								className="overview-adsense-rpm"
+								title={ __( 'RPM', 'google-site-kit' ) }
+								datapoint={ readableLargeNumber( period.totals[ 1 ], currencyCode ) }
+								source={ {
+									name: _x( 'AdSense', 'Service name', 'google-site-kit' ),
+									link: href,
+								} }
+								sparkline={ daily &&
+									<Sparkline
+										data={ extractForSparkline( processedData.dataMap, 2 ) }
+										change={ 1 }
+										loadSmall={ false }
+									/>
+								}
+								context="compact"
+							/>
+						</div>
+
+						<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+							<DataBlock
+								className="overview-adsense-earnings"
+								title={ __( 'Total Earnings', 'google-site-kit' ) }
+								datapoint={ readableLargeNumber( period.totals[ 0 ], currencyCode ) }
+								source={ {
+									name: _x( 'AdSense', 'Service name', 'google-site-kit' ),
+									link: href,
+								} }
+								change={ today.totals[ 0 ] }
+								changeDataUnit={ '$' }
+								sparkline={ daily &&
+									<Sparkline
+										data={ extractForSparkline( processedData.dataMap, 1 ) }
+										change={ 1 }
+										loadSmall={ false }
+									/>
+								}
+								context="compact"
+							/>
+						</div>
+
+						<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+							<DataBlock
+								className="overview-adsense-impressions"
+								title={ __( 'Ad Impressions', 'google-site-kit' ) }
+								datapoint={ readableLargeNumber( period.totals[ 2 ] ) }
+								source={ {
+									name: _x( 'AdSense', 'Service name', 'google-site-kit' ),
+									link: href,
+								} }
+								sparkline={ daily &&
+									<Sparkline
+										data={ extractForSparkline( processedData.dataMap, 3 ) }
+										change={ 1 }
+										loadSmall={ false }
+									/>
+								}
+								context="compact"
+							/>
+						</div>
+					</div>
+				</div>
+			</Layout>
+		</div>
+	);
 }
 
 export default whenActive( { moduleName: 'adsence' } )( DashboardSummaryWidget );
