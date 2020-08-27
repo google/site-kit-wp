c186ca35ff9158e61554794f2734741d9e5758fa
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-08-28 00:14:28 +0300

    Update assets/js/googlesitekit/modules/datastore/modules.js
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- assets/js/googlesitekit/modules/datastore/modules.js
+++ assets/js/googlesitekit/modules/datastore/modules.js
@@ -434,7 +434,7 @@ const baseSelectors = {
 	 *
 	 * @param {Object} state Data store's state.
 	 * @param {string} slug  Module slug.
-	 * @return {(boolean|undefined)} TRUE when the module exists, is actvie and connected, otherwise FALSE; `undefined` if state is still loading or `null` if said module doesn't exist.
+	 * @return {(boolean|null|undefined)} TRUE when the module exists, is active and connected, otherwise FALSE; `undefined` if state is still loading or `null` if said module doesn't exist.
 	 */
 	isModuleConnected: createRegistrySelector( ( select ) => ( state, slug ) => {
 		const module = select( STORE_NAME ).getModule( slug );
