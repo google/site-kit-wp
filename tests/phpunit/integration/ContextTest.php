aabffba954faec30fc88fb472a46a39878bb1d0c
Felix Arntz <felixarntz@users.noreply.github.com>
2020-09-02 07:42:41 -0700

    Turn assumption into assertion.
    Co-authored-by: Evan Mattson <evan.mattson@10up.com>

--- tests/phpunit/integration/ContextTest.php
+++ tests/phpunit/integration/ContextTest.php
@@ -191,7 +191,8 @@ class ContextTest extends TestCase {
 		set_current_screen( 'toplevel_page_googlesitekit-dashboard' );
 		$_GET['page'] = 'googlesitekit-dashboard';
 		$this->assertNull( $context->get_reference_entity() );
-		// We can probably safely assume that 987654 is not a valid post ID in the test environment.
+		// We can probably safely assume that 987654 is not a valid post ID in the test environment, but let's make sure.
+		$this->assertFalse( WP_Post::get_instance( 987654 ) );
 		$_GET['permaLink'] = add_query_arg( 'p', '987654', home_url( '/' ) );
 		$this->assertNull( $context->get_reference_entity() );
 		$_GET['permaLink'] = get_permalink( $public_post_id );
