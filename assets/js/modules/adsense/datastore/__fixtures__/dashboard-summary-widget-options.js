174f0b64c86b1b5da7654172fb95b244750c9dc1
Ryan Welcher <ryan.welcher@10up.com>
2020-09-11 12:03:33 -0400

    Add stories.

---
+++ assets/js/modules/adsense/datastore/__fixtures__/dashboard-summary-widget-options.js
@@ -0,0 +1,29 @@
+export const dashboardSummaryWidgetTodayOptions = {
+	startDate: '2020-09-11',
+	endDate: '2020-09-11',
+	metrics: [
+		'EARNINGS',
+		'PAGE_VIEWS_RPM',
+		'IMPRESSIONS',
+	],
+};
+export const dashboardSummaryWidgetPeriodOptions = {
+	dateRange: 'last-28-days',
+	metrics: [
+		'EARNINGS',
+		'PAGE_VIEWS_RPM',
+		'IMPRESSIONS',
+	],
+};
+export const dashboardSummaryWidget28DailyOptions = {
+	startDate: '2020-08-11',
+	endDate: '2020-09-11',
+	metrics: [
+		'EARNINGS',
+		'PAGE_VIEWS_RPM',
+		'IMPRESSIONS',
+	],
+	dimensions: [
+		'DATE',
+	],
+};
