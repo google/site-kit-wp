3fe9a75acce127eae012d63cd987cd4bf6abd89b
Ryan Welcher <ryan.welcher@10up.com>
2020-09-14 09:25:30 -0400

    Update export names.

--- assets/js/modules/adsense/components/dashboard/index.js
+++ assets/js/modules/adsense/components/dashboard/index.js
@@ -16,13 +16,13 @@
  * limitations under the License.
  */
 
-export { default as AdSenseDashboardMainSummary } from './AdSenseDashboardMainSummary';
 export { default as AdSenseDashboardWidget } from './AdSenseDashboardWidget';
 export { default as AdSenseEstimateEarningsWidget } from './AdSenseEstimateEarningsWidget';
 export { default as AdSensePerformanceWidget } from './AdSensePerformanceWidget';
-export { default as DashboardAdSenseTopEarningPagesSmall } from './DashboardAdSenseTopEarningPagesSmall';
 export { default as DashboardAdSenseTopPages } from './DashboardAdSenseTopPages';
-export { default as DashboardEarnings } from './DashboardEarnings';
 export { default as DashboardZeroData } from './DashboardZeroData';
 export { default as DashboardSummaryWidget } from './DashboardSummaryWidget';
 export { default as DashboardTopEarningPagesWidget } from './DashboardTopEarningPagesWidget';
+export { default as LegacyAdSenseDashboardMainSummaryAdSenseDashboardMainSummary } from './LegacyAdSenseDashboardMainSummary';
+export { default as LegacyDashboardAdSenseTopEarningPagesSmall } from './LegacyDashboardAdSenseTopEarningPagesSmall';
+export { default as LegacyDashboardEarnings } from './LegacyDashboardEarnings';
