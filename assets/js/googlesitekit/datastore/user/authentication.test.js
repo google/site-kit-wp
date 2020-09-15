58243c3262f5e674075e4bb1e207f7396f046458
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-09-15 18:38:34 +0300

    Add tests for getDisconnectedReason selector.

--- assets/js/googlesitekit/datastore/user/authentication.test.js
+++ assets/js/googlesitekit/datastore/user/authentication.test.js
@@ -36,6 +36,8 @@ describe( 'core/user authentication', () => {
 		requiredScopes: [],
 		grantedScopes: [],
 		unsatisfiedScopes: [],
+		needsReauthentication: true,
+		disconnectedReason: 'test-reason',
 	};
 	const coreUserDataEndpointRegExp = /^\/google-site-kit\/v1\/core\/user\/data\/authentication/;
 	let registry;
@@ -184,6 +186,7 @@ describe( 'core/user authentication', () => {
 			[ 'getRequiredScopes', 'requiredScopes' ],
 			[ 'getUnsatisfiedScopes', 'unsatisfiedScopes' ],
 			[ 'needsReauthentication', 'needsReauthentication' ],
+			[ 'getDisconnectedReason', 'disconnectedReason' ],
 		] )( '%s', ( selector, property ) => {
 			it( 'uses a resolver to load the authenticated value if not yet set.', async () => {
 				fetchMock.getOnce(
