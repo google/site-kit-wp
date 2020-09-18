5b8c1463d67e59090196be9785e9e3353c7b763b
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-09-18 21:28:41 +0300

    Implement remaining methods.

--- includes/Core/Storage/User_Transients.php
+++ includes/Core/Storage/User_Transients.php
@@ -157,7 +157,7 @@ class User_Transients implements User_Aware_Interface {
 	 * @return mixed Value set for the transient, or false if not set.
 	 */
 	private function get_from_cache( $transient ) {
-		$key = $this->get_transient_name_for_cache( $transient );
+		$key   = $this->get_transient_name_for_cache( $transient );
 		$group = $this->get_transient_group_for_cache();
 		return wp_cache_get( $key, $group );
 	}
@@ -173,9 +173,9 @@ class User_Transients implements User_Aware_Interface {
 	 * @return bool True on success, false on failure.
 	 */
 	private function set_in_cache( $transient, $value, $expiration ) {
-		$key = $this->get_transient_name_for_cache( $transient );
+		$key   = $this->get_transient_name_for_cache( $transient );
 		$group = $this->get_transient_group_for_cache();
-		return wp_cache_set( $key, $value, $group, $expiration );
+		return wp_cache_set( $key, $value, $group, (int) $expiration ); // phpcs:ignore WordPressVIPMinimum.Performance.LowExpiryCacheTime.LowCacheTime
 	}
 
 	/**
@@ -187,13 +187,33 @@ class User_Transients implements User_Aware_Interface {
 	 * @return bool True on success, false on failure.
 	 */
 	private function delete_from_cache( $transient ) {
-		$key = $this->get_transient_name_for_cache( $transient );
+		$key   = $this->get_transient_name_for_cache( $transient );
 		$group = $this->get_transient_group_for_cache();
 		return wp_cache_delete( $key, $group );
 	}
 
+	/**
+	 * Gets prefixed transient name.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $transient Transient name.
+	 * @return string Prefixed transient name.
+	 */
 	private function get_transient_name_for_user_options( $transient ) {
 		return "googlesitekit_transient_{$transient}";
+	}
+
+	/**
+	 * Gets prefixed transient timeout name.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $transient Transient name.
+	 * @return string Prefixed transient timeout name.
+	 */
+	private function get_transient_timeout_for_user_options( $transient ) {
+		return "googlesitekit_transient_timeout_{$transient}";
 	}
 
 	/**
@@ -205,17 +225,49 @@ class User_Transients implements User_Aware_Interface {
 	 * @return mixed Value set for the transient, or false if not set.
 	 */
 	private function get_from_user_options( $transient ) {
-		$key = $this->get_transient_name_for_user_options( $transient );
+		$prefixed_transient_timeout = $this->get_transient_timeout_for_user_options( $transient );
+		$timeout                    = $this->user_options->get( $prefixed_transient_timeout );
+		if ( false !== $timeout && $timeout < time() ) {
+			$this->delete( $transient );
+			return false;
+		}
+
+		$prefixed_transient = $this->get_transient_name_for_user_options( $transient );
+		return $this->user_options->get( $prefixed_transient );
 	}
 
+	/**
+	 * Sets the value for a transient.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $transient  Transient name.
+	 * @param mixed  $value      Transient value.
+	 * @param int    $expiration Optional. Time until expiration in seconds. Default 0 (no expiration).
+	 * @return bool True on success, false on failure.
+	 */
 	private function set_in_user_options( $transient, $value, $expiration ) {
-		$key = $this->get_transient_name_for_user_options( $transient );
+		$prefixed_transient_timeout = $this->get_transient_timeout_for_user_options( $transient );
+		$this->user_options->set( $prefixed_transient_timeout, (int) $expiration );
 
+		$prefixed_transient = $this->get_transient_name_for_user_options( $transient );
+		return $this->user_options->set( $prefixed_transient, $value );
 	}
 
+	/**
+	 * Deletes the given transient.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $transient Transient name.
+	 * @return bool True on success, false on failure.
+	 */
 	private function delete_from_user_options( $transient ) {
-		$key = $this->get_transient_name_for_user_options( $transient );
+		$prefixed_transient_timeout = $this->get_transient_timeout_for_user_options( $transient );
+		$this->user_options->delete( $prefixed_transient_timeout );
 
+		$prefixed_transient = $this->get_transient_name_for_user_options( $transient );
+		return $this->user_options->delete( $prefixed_transient );
 	}
 
 }
