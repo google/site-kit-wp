fb62406a7a3fa5ab201a68f0d756b9e446b44f93
Ryan Welcher <ryan.welcher@10up.com>
2020-09-28 11:13:17 -0400

    Set up and approve new stories.

---
+++ assets/js/modules/adsense/datastore/__fixtures__/ad-sense-performance-widget-data.js
@@ -0,0 +1,103 @@
+export const adSensePerformanceCurrentRangeData = {
+	averages: [
+		'0.25',
+		null,
+		'701',
+		null,
+	],
+	endDate: '2020-09-23',
+	kind: 'adsense#report',
+	rows: [
+		[
+			'0.25',
+			'0.10',
+			'701',
+			'0.0008',
+		],
+	],
+	startDate: '2020-08-27',
+	totalMatchedRows: '1',
+	totals: [
+		'0.25',
+		'0.10',
+		'701',
+		'0.0008',
+	],
+	warnings: [
+		'Some of the requested ad clients do not support the requested combination of dimensions.',
+	],
+	headers: [
+		{
+			currency: 'USD',
+			name: 'EARNINGS',
+			type: 'METRIC_CURRENCY',
+		},
+		{
+			currency: 'USD',
+			name: 'PAGE_VIEWS_RPM',
+			type: 'METRIC_CURRENCY',
+		},
+		{
+			currency: null,
+			name: 'IMPRESSIONS',
+			type: 'METRIC_TALLY',
+		},
+		{
+			currency: null,
+			name: 'PAGE_VIEWS_CTR',
+			type: 'METRIC_RATIO',
+		},
+	],
+};
+
+export const adSensePerformancePrevRangeData = {
+	averages: [
+		'0.84',
+		null,
+		'726',
+		null,
+	],
+	endDate: '2020-08-26',
+	kind: 'adsense#report',
+	rows: [
+		[
+			'0.84',
+			'0.32',
+			'726',
+			'0.0019',
+		],
+	],
+	startDate: '2020-07-30',
+	totalMatchedRows: '1',
+	totals: [
+		'0.84',
+		'0.32',
+		'726',
+		'0.0019',
+	],
+	warnings: [
+		'Some of the requested ad clients do not support the requested combination of dimensions.',
+	],
+	headers: [
+		{
+			currency: 'USD',
+			name: 'EARNINGS',
+			type: 'METRIC_CURRENCY',
+		},
+		{
+			currency: 'USD',
+			name: 'PAGE_VIEWS_RPM',
+			type: 'METRIC_CURRENCY',
+		},
+		{
+			currency: null,
+			name: 'IMPRESSIONS',
+			type: 'METRIC_TALLY',
+		},
+		{
+			currency: null,
+			name: 'PAGE_VIEWS_CTR',
+			type: 'METRIC_RATIO',
+		},
+	],
+};
---
+++ assets/js/modules/adsense/datastore/__fixtures__/ad-sense-performance-widget-options.js
@@ -0,0 +1,19 @@
+export const adSensePerformanceCurrentRangeOptions = {
+	dateRange: 'last-28-days',
+	metrics: [
+		'EARNINGS',
+		'PAGE_VIEWS_RPM',
+		'IMPRESSIONS',
+		'PAGE_VIEWS_CTR',
+	],
+};
+
+export const adSensePerformancePrevRangeOptions = {
+	dateRange: 'prev-28-days',
+	metrics: [
+		'EARNINGS',
+		'PAGE_VIEWS_RPM',
+		'IMPRESSIONS',
+		'PAGE_VIEWS_CTR',
+	],
+};
