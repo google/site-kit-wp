bad5d541b34534fe3dcf9dc4db40cd7ba04f4e14
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-09-30 21:38:37 +0300

    Update AccountCreate to use hasFinishedResolution.

--- assets/js/modules/analytics/components/common/AccountCreate/index.js
+++ assets/js/modules/analytics/components/common/AccountCreate/index.js
@@ -46,10 +46,16 @@ import Data from 'googlesitekit-data';
 const { useDispatch, useSelect } = Data;
 
 export default function AccountCreate() {
+	const {
+		accounts,
+		hasResolvedAccounts,
+	} = useSelect( ( select ) => ( {
+		accounts: select( STORE_NAME ).getAccounts(),
+		hasResolvedAccounts: select( STORE_NAME ).hasFinishedResolution( 'getAccounts' ),
+	} ) );
 	const accountTicketTermsOfServiceURL = useSelect( ( select ) => select( STORE_NAME ).getAccountTicketTermsOfServiceURL() );
 	const canSubmitAccountCreate = useSelect( ( select ) => select( STORE_NAME ).canSubmitAccountCreate() );
 	const isDoingCreateAccount = useSelect( ( select ) => select( STORE_NAME ).isDoingCreateAccount() );
-	const accounts = useSelect( ( select ) => select( STORE_NAME ).getAccounts() );
 	const hasProvisioningScope = useSelect( ( select ) => select( CORE_USER ).hasScope( PROVISIONING_SCOPE ) );
 	const hasAccountCreateForm = useSelect( ( select ) => select( CORE_FORMS ).hasForm( FORM_ACCOUNT_CREATE ) );
 	const autoSubmit = useSelect( ( select ) => select( CORE_FORMS ).getValue( FORM_ACCOUNT_CREATE, 'autoSubmit' ) );
@@ -125,7 +131,7 @@ export default function AccountCreate() {
 	const { rollbackSettings } = useDispatch( STORE_NAME );
 	const handleBack = useCallback( () => rollbackSettings() );
 
-	if ( isDoingCreateAccount || isNavigating || accounts === undefined || hasProvisioningScope === undefined ) {
+	if ( isDoingCreateAccount || isNavigating || ! hasResolvedAccounts || hasProvisioningScope === undefined ) {
 		return <ProgressBar />;
 	}
 
