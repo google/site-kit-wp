9f364b3e36404ec94ea6f0e6c477e79451fe20fc
Ryan Welcher <ryan.welcher@10up.com>
2020-10-01 08:18:17 -0400

    Add UserInput story for both boolen states for enabled.

--- stories/user-input.stories.js
+++ stories/user-input.stories.js
@@ -2,13 +2,30 @@
  * External dependencies
  */
 import { storiesOf } from '@storybook/react';
+import { set } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import UserInputApp from '../assets/js/components/user-input/UserInputApp';
+import { WithTestRegistry, createTestRegistry } from '../tests/js/utils';
 
 storiesOf( 'User Input', module )
 	.add( 'User Input Page', () => {
-		return <UserInputApp />;
+		// Set the featureFlag.
+		set( global, 'featureFlags.widgets.userInput.enabled', true );
+		return (
+			<WithTestRegistry callback={ createTestRegistry }>
+				<UserInputApp />
+			</WithTestRegistry>
+		);
+	} )
+	.add( 'No Access', () => {
+		// Set the featureFlag.
+		set( global, 'featureFlags.widgets.userInput.enabled', false );
+		return (
+			<WithTestRegistry callback={ createTestRegistry }>
+				<UserInputApp />
+			</WithTestRegistry>
+		);
 	} );
