c2108be461e229a2f0f5af43c56da37880c96e41
Felix Arntz <felixarntz@google.com>
2020-10-02 16:44:18 -0700

    Add PHPUnit test coverage for Analytics advanced tracking classes.

---
+++ tests/phpunit/includes/Modules/Dynamic_Blog_Post_Link_Event_List.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Class Google\Site_Kit\Tests\Modules\Dynamic_Blog_Post_Link_Event_List
+ *
+ * @package   Google\Site_Kit\Tests\Modules
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests\Modules;
+
+use Google\Site_Kit\Modules\Analytics\Advanced_Tracking\Event_List;
+use Google\Site_Kit\Modules\Analytics\Advanced_Tracking\Event;
+
+class Dynamic_Blog_Post_Link_Event_List extends Event_List {
+
+	/**
+	 * Registers an event for when someone views or edits a single post from within the blog.
+	 *
+	 * The events are registered dynamically, i.e. events are per post and include context data.
+	 */
+	public function register() {
+		if ( ! is_home() ) {
+			return;
+		}
+
+		add_action(
+			'the_post',
+			function( $post, $wp_query ) {
+				if ( ! $wp_query->is_main_query() ) {
+					return;
+				}
+
+				$this->add_event(
+					new Event(
+						array(
+							'action'   => 'view_blog_post',
+							'selector' => '.blog .post.post-' . $post->ID . ' a[rel="bookmark"]',
+							'on'       => 'click',
+							'metadata' => array(
+								'event_category' => 'engagement',
+								'event_label'    => (string) $post->ID,
+								// This could be a custom dimension in Google Analytics.
+								'post_title'     => $post->post_title,
+							),
+						)
+					)
+				);
+
+				$this->add_event(
+					new Event(
+						array(
+							'action'   => 'edit_blog_post',
+							'selector' => '.blog .post.post-' . $post->ID . ' a.post-edit-link',
+							'on'       => 'click',
+							'metadata' => array(
+								'event_category' => 'engagement',
+								'event_label'    => (string) $post->ID,
+								// This could be a custom dimension in Google Analytics.
+								'post_title'     => $post->post_title,
+							),
+						)
+					)
+				);
+			},
+			10,
+			2
+		);
+	}
+}
---
+++ tests/phpunit/includes/Modules/Fixed_Blog_Post_Link_Event_List.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Class Google\Site_Kit\Tests\Modules\Fixed_Blog_Post_Link_Event_List
+ *
+ * @package   Google\Site_Kit\Tests\Modules
+ * @copyright 2019 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests\Modules;
+
+use Google\Site_Kit\Modules\Analytics\Advanced_Tracking\Event_List;
+use Google\Site_Kit\Modules\Analytics\Advanced_Tracking\Event;
+
+class Fixed_Blog_Post_Link_Event_List extends Event_List {
+
+	/**
+	 * Registers an event for when someone views or edits a single post from within the blog.
+	 *
+	 * The events are registered non-dynamically, i.e. there is no context data provided.
+	 */
+	public function register() {
+		$this->add_event(
+			new Event(
+				array(
+					'action'   => 'view_blog_post',
+					'selector' => '.blog .post a[rel="bookmark"]',
+					'on'       => 'click',
+					'metadata' => array(
+						'event_category' => 'engagement',
+					),
+				)
+			)
+		);
+
+		$this->add_event(
+			new Event(
+				array(
+					'action'   => 'edit_blog_post',
+					'selector' => '.blog .post a.post-edit-link',
+					'on'       => 'click',
+					'metadata' => array(
+						'event_category' => 'engagement',
+					),
+				)
+			)
+		);
+	}
+}
