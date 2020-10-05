f96332d7e8beb3e05b6f60f49bc763dcb233b912
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-10-05 18:08:09 +0300

    Add a storybook for an existing entity.

--- stories/dashboard-details.stories.js
+++ stories/dashboard-details.stories.js
@@ -24,9 +24,47 @@ import { storiesOf } from '@storybook/react';
 /**
  * Internal dependencies
  */
+import { STORE_NAME as CORE_SITE } from '../assets/js/googlesitekit/datastore/site/constants';
+import { STORE_NAME as CORE_USER } from '../assets/js/googlesitekit/datastore/user/constants';
+import { WithTestRegistry } from '../tests/js/utils';
+import DashboardDetailsApp from '../assets/js/components/dashboard-details/dashboard-details-app';
 import DashboardDetailsEntityNotFoundView from '../assets/js/components/dashboard-details/DashboardDetailsEntityNotFoundView';
 
 storiesOf( 'Dashboard Details', module )
+	.add( 'Existing Entity', () => {
+		global.featureFlags = {
+			widgets: {
+				pageDashboard: {
+					enabled: false,
+				},
+			},
+		};
+
+		const setupRegistry = ( { dispatch } ) => {
+			dispatch( CORE_SITE ).receiveSiteInfo( {
+				usingProxy: true,
+				proxySetupURL: 'https://sitekit.withgoogle.com/site-management/setup/',
+				proxyPermissionsURL: 'https://sitekit.withgoogle.com/site-management/permissions/',
+				adminURL: 'http://example.com/wp-admin/',
+				referenceSiteURL: 'http://example.com',
+				currentEntityTitle: 'Test Page',
+				currentEntityURL: 'https://example.com/test-page',
+				siteName: 'My Site Name',
+			} );
+
+			dispatch( CORE_USER ).receiveGetAuthentication( {
+				authenticated: true,
+				requiredScopes: [],
+				grantedScopes: [],
+			} );
+		};
+
+		return (
+			<WithTestRegistry callback={ setupRegistry }>
+				<DashboardDetailsApp />
+			</WithTestRegistry>
+		);
+	} )
 	.add( 'Not Found Entity', () => (
 		<DashboardDetailsEntityNotFoundView permalink="https://example.com/test-page" />
 	) )
