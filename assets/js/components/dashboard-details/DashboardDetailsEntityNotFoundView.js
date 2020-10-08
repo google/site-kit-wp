1717eb9f9f7dd484e44c21a318f83ce8c49f5c2a
Felix Arntz <felixarntz@google.com>
2020-10-08 13:46:57 -0700

    Handle permaLink query parameter centralized via core/site datastore.

--- assets/js/components/dashboard-details/DashboardDetailsEntityNotFoundView.js
+++ assets/js/components/dashboard-details/DashboardDetailsEntityNotFoundView.js
@@ -17,29 +17,26 @@
  */
 
 /**
- * External dependencies
- */
-import PropTypes from 'prop-types';
-
-/**
  * WordPress dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
-import { getQueryArg } from '@wordpress/url';
 
 /**
  * Internal dependencies
  */
+import Data from 'googlesitekit-data';
 import { sanitizeHTML } from '../../util';
 import DashboardDetailsEntityHeaderContainer from './DashboardDetailsEntityHeaderContainer';
+import { STORE_NAME as CORE_SITE } from '../../googlesitekit/datastore/site/constants';
+const { useSelect } = Data;
 
-export default function DashboardDetailsEntityNotFoundView( { permalink } ) {
-	const currentEntityURL = permalink || getQueryArg( global.location.href, 'permaLink' );
+export default function DashboardDetailsEntityNotFoundView() {
+	const permaLink = useSelect( ( select ) => select( CORE_SITE ).getPermaLinkParam() );
 
 	const message = sprintf(
 		/* translators: %s: current entity URL */
 		__( 'It looks like the URL %s is not part of this site, therefore there is no data available to display.', 'google-site-kit' ),
-		`<strong>${ currentEntityURL }</strong>`
+		`<strong>${ permaLink }</strong>`
 	);
 
 	const sanitizeArgs = {
@@ -53,7 +50,3 @@ export default function DashboardDetailsEntityNotFoundView( { permalink } ) {
 		</DashboardDetailsEntityHeaderContainer>
 	);
 }
-
-DashboardDetailsEntityNotFoundView.propTypes = {
-	permalink: PropTypes.string,
-};
