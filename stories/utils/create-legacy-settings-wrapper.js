01195f5585a43acba3f586cb18153aa7543429cf
Evan Mattson <evan.mattson@10up.com>
2020-10-16 23:41:49 +0300

    Remove googlesitekit.ModuleSettingsDetails filter.

--- stories/utils/create-legacy-settings-wrapper.js
+++ stories/utils/create-legacy-settings-wrapper.js
@@ -17,29 +17,22 @@
  */
 
 /**
- * WordPress dependencies
- */
-import { useEffect, WPElement } from '@wordpress/element';
-import { removeAllFilters, addFilter } from '@wordpress/hooks';
-
-/**
  * Internal dependencies
  */
 import SettingsModule from '../../assets/js/components/settings/settings-module';
-import { fillFilterWithComponent } from '../../assets/js/util';
 import { WithTestRegistry } from '../../tests/js/utils';
 
 /**
  * Creates a legacy settings wrapper component for the given module.
  *
  * @since 1.12.0
+ * @since n.e.x.t Removed `moduleComponent` argument (now provided via module registration).
  * @private
  *
- * @param {string}    moduleSlug      The module's slug.
- * @param {WPElement} moduleComponent Module settings component to filter in.
+ * @param {string} moduleSlug The module's slug.
  * @return {Function} Legacy settings component.
  */
-export default function createLegacySettingsWrapper( moduleSlug, moduleComponent ) {
+export default function createLegacySettingsWrapper( moduleSlug ) {
 	return function SettingsLegacy( props ) {
 		const {
 			registry,
@@ -58,16 +51,6 @@ export default function createLegacySettingsWrapper( moduleSlug, moduleComponent
 			updateModulesList = global.console.log.bind( null, 'updateModulesList' ),
 			handleButtonAction = global.console.log.bind( null, 'handleButtonAction' ),
 		} = props;
-
-		useEffect( () => {
-			removeAllFilters( `googlesitekit.ModuleSettingsDetails-${ moduleSlug }` );
-			addFilter(
-				`googlesitekit.ModuleSettingsDetails-${ moduleSlug }`,
-				'googlesitekit.SettingsLegacy',
-				fillFilterWithComponent( moduleComponent )
-			);
-			return () => removeAllFilters( `googlesitekit.ModuleSettingsDetails-${ moduleSlug }` );
-		} );
 
 		const moduleKey = `${ moduleSlug }-module`;
 
