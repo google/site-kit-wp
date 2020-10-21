bab40d9b186362234aa73bd3b113b7a16d511bef
Felix Arntz <felixarntz@google.com>
2020-10-20 19:25:06 -0700

    Introduce GA4Notice component with Storybook.

---
+++ assets/js/modules/analytics/components/common/GA4Notice.js
@@ -0,0 +1,44 @@
+/**
+ * Analytics GA4 Notice component.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import SettingsNotice from '../../../../components/settings-notice';
+import Link from '../../../../components/link';
+
+export default function GA4Notice() {
+	return (
+		<SettingsNotice>
+			{ __( 'Dummy info about new Analytics.', 'google-site-kit' ) }
+			{ ' ' }
+			<Link
+				href="https://sitekit.withgoogle.com/documentation/dummy-new-analytics/"
+				external
+				inherit
+			>
+				{ __( 'Learn more about the new Analytics', 'google-site-kit' ) }
+			</Link>
+		</SettingsNotice>
+	);
+}
