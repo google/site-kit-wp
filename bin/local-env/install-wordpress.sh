e31e215b9df90b5f6d3aa13012dc04f55c7b9674
Ryan Welcher <ryan.welcher@10up.com>
2020-10-23 11:00:42 -0400

    Install the Yoast plugin.

--- bin/local-env/install-wordpress.sh
+++ bin/local-env/install-wordpress.sh
@@ -113,6 +113,14 @@ else
 	wp plugin update amp --minor --quiet
 fi
 
+status_message 'Installing the Yoast plugin...'
+if [[! -z "$YOAST_VERSION"]]; then
+	wp plugin install wordpress-seo --force --quiet --version="$YOAST_VERSION"
+else
+	# Install latest version of the plugin
+	wp plugin install wordpress-seo --force --quiet
+fi
+
 # Install a dummy favicon to avoid 404 errors.
 status_message "Installing a dummy favicon..."
 container touch /var/www/html/favicon.ico
