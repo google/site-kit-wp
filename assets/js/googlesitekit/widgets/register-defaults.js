5bd624ebc0c82e8d4555d0b8e2bd1052e5907278
Felix Arntz <felixarntz@google.com>
2020-10-29 14:04:38 -0700

    Fix search widget to only be shown in dashboard, realign widget area registration calls.

--- assets/js/googlesitekit/widgets/register-defaults.js
+++ assets/js/googlesitekit/widgets/register-defaults.js
@@ -26,7 +26,6 @@ import { __ } from '@wordpress/i18n';
  */
 import URLSearchWidget from '../../googlesitekit/widgets/components/URLSearchWidget';
 import { CONTEXT_DASHBOARD, CONTEXT_PAGE_DASHBOARD } from './default-contexts';
-import { WIDGET_URL_SEARCH } from './default-widgets';
 import {
 	AREA_DASHBOARD_ALL_TRAFFIC,
 	AREA_DASHBOARD_SEARCH_FUNNEL,
@@ -38,6 +37,7 @@ import {
 	AREA_PAGE_DASHBOARD_POPULARITY,
 	AREA_PAGE_DASHBOARD_SPEED,
 } from './default-areas';
+import { WIDGET_URL_SEARCH } from './default-widgets';
 import { WIDGET_AREA_STYLES, WIDGET_WIDTHS } from './datastore/constants';
 
 /**
@@ -48,73 +48,115 @@ import { WIDGET_AREA_STYLES, WIDGET_WIDTHS } from './datastore/constants';
  * @param {Object} widgetsAPI Widgets API.
  */
 export function registerDefaults( widgetsAPI ) {
-	widgetsAPI.registerWidgetArea( AREA_DASHBOARD_ALL_TRAFFIC, {
-		title: __( 'All Traffic', 'google-site-kit' ),
-		subtitle: __( 'How people found your site.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.BOXES,
-		priority: 1,
-	}, CONTEXT_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_DASHBOARD_ALL_TRAFFIC,
+		{
+			title: __( 'All Traffic', 'google-site-kit' ),
+			subtitle: __( 'How people found your site.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.BOXES,
+			priority: 1,
+		},
+		CONTEXT_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_DASHBOARD_SEARCH_FUNNEL, {
-		title: __( 'Search Funnel', 'google-site-kit' ),
-		subtitle: __( 'How your site appeared in Search results and how many visitors you got from Search.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.COMPOSITE,
-		priority: 2,
-	}, CONTEXT_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_DASHBOARD_SEARCH_FUNNEL,
+		{
+			title: __( 'Search Funnel', 'google-site-kit' ),
+			subtitle: __( 'How your site appeared in Search results and how many visitors you got from Search.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.COMPOSITE,
+			priority: 2,
+		},
+		CONTEXT_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_DASHBOARD_POPULARITY, {
-		title: __( 'Popularity', 'google-site-kit' ),
-		subtitle: __( 'Your most popular pages and how people found them from Search.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.BOXES,
-		priority: 3,
-	}, CONTEXT_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_DASHBOARD_POPULARITY,
+		{
+			title: __( 'Popularity', 'google-site-kit' ),
+			subtitle: __( 'Your most popular pages and how people found them from Search.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.BOXES,
+			priority: 3,
+		},
+		CONTEXT_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_DASHBOARD_SPEED, {
-		title: __( 'Page Speed and Experience', 'google-site-kit' ),
-		subtitle: __( 'How fast your home page loads, how quickly people can interact with your content, and how stable your content is.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.BOXES,
-		priority: 4,
-	}, CONTEXT_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_DASHBOARD_SPEED,
+		{
+			title: __( 'Page Speed and Experience', 'google-site-kit' ),
+			subtitle: __( 'How fast your home page loads, how quickly people can interact with your content, and how stable your content is.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.BOXES,
+			priority: 4,
+		},
+		CONTEXT_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_DASHBOARD_EARNINGS, {
-		title: __( 'Earnings', 'google-site-kit' ),
-		subtitle: __( 'How much you’re earning from your content through AdSense.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.BOXES,
-		priority: 5,
-	}, CONTEXT_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_DASHBOARD_EARNINGS,
+		{
+			title: __( 'Earnings', 'google-site-kit' ),
+			subtitle: __( 'How much you’re earning from your content through AdSense.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.BOXES,
+			priority: 5,
+		},
+		CONTEXT_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_PAGE_DASHBOARD_SEARCH_FUNNEL, {
-		title: __( 'Search Funnel', 'google-site-kit' ),
-		subtitle: __( 'How your site appeared in Search results and how many visitors you got from Search.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.COMPOSITE,
-		priority: 1,
-	}, CONTEXT_PAGE_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_PAGE_DASHBOARD_SEARCH_FUNNEL,
+		{
+			title: __( 'Search Funnel', 'google-site-kit' ),
+			subtitle: __( 'How your site appeared in Search results and how many visitors you got from Search.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.COMPOSITE,
+			priority: 1,
+		},
+		CONTEXT_PAGE_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_PAGE_DASHBOARD_ALL_TRAFFIC, {
-		title: __( 'All Traffic', 'google-site-kit' ),
-		subtitle: __( 'How people found your page.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.BOXES,
-		priority: 2,
-	}, CONTEXT_PAGE_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_PAGE_DASHBOARD_ALL_TRAFFIC,
+		{
+			title: __( 'All Traffic', 'google-site-kit' ),
+			subtitle: __( 'How people found your page.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.BOXES,
+			priority: 2,
+		},
+		CONTEXT_PAGE_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_PAGE_DASHBOARD_POPULARITY, {
-		title: __( 'Popularity', 'google-site-kit' ),
-		subtitle: __( 'What people searched for to find your page.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.BOXES,
-		priority: 3,
-	}, CONTEXT_PAGE_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_PAGE_DASHBOARD_POPULARITY,
+		{
+			title: __( 'Popularity', 'google-site-kit' ),
+			subtitle: __( 'What people searched for to find your page.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.BOXES,
+			priority: 3,
+		},
+		CONTEXT_PAGE_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidgetArea( AREA_PAGE_DASHBOARD_SPEED, {
-		title: __( 'Page Speed and Experience', 'google-site-kit' ),
-		subtitle: __( 'How fast your page loads, how quickly people can interact with your content, and how stable your content is.', 'google-site-kit' ),
-		style: WIDGET_AREA_STYLES.BOXES,
-		priority: 4,
-	}, CONTEXT_PAGE_DASHBOARD );
+	widgetsAPI.registerWidgetArea(
+		AREA_PAGE_DASHBOARD_SPEED,
+		{
+			title: __( 'Page Speed and Experience', 'google-site-kit' ),
+			subtitle: __( 'How fast your page loads, how quickly people can interact with your content, and how stable your content is.', 'google-site-kit' ),
+			style: WIDGET_AREA_STYLES.BOXES,
+			priority: 4,
+		},
+		CONTEXT_PAGE_DASHBOARD,
+	);
 
-	widgetsAPI.registerWidget( WIDGET_URL_SEARCH, {
-		priority: 100,
-		width: WIDGET_WIDTHS.HALF,
-		component: URLSearchWidget,
-		wrapWidget: false,
-	}, [ AREA_DASHBOARD_POPULARITY, AREA_PAGE_DASHBOARD_POPULARITY ] );
+	widgetsAPI.registerWidget(
+		WIDGET_URL_SEARCH,
+		{
+			priority: 100,
+			width: WIDGET_WIDTHS.HALF,
+			component: URLSearchWidget,
+			wrapWidget: false,
+		},
+		[
+			AREA_DASHBOARD_POPULARITY,
+		],
+	);
 }
