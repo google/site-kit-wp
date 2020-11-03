75a24f6a0f55da315822465f6638154eacc7e3d0
Ryan Welcher <ryan.welcher@10up.com>
2020-11-03 12:14:24 -0500

    Create sumObjectValueList and associated tests.

---
+++ assets/js/util/sum-object-list-value.js
@@ -0,0 +1,33 @@
+/**
+ * Utility function sumObjectListValue.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Returns the sum of a given fieldName is a list.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Array}  list      Array of objects or arrays.
+ * @param {string} fieldName The name of the field to be summed.
+ *
+ * @return {number} The sum.
+ */
+export default function sumObjectListValue( list, fieldName ) {
+	return list.reduce( ( acc, item ) => {
+		return item[ fieldName ] ? acc + item[ fieldName ] : acc;
+	}, 0 );
+}
