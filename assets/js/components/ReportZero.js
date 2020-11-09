b46ce9928c6a8412dfc88f4d17c7a658dc9ad7ce
Ryan Welcher <ryan.welcher@10up.com>
2020-11-09 15:35:18 -0500

    Create ReportZero component.

---
+++ assets/js/components/ReportZero.js
@@ -0,0 +1,52 @@
+/**
+ * ReportZero component.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __, sprintf } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import Data from 'googlesitekit-data';
+import { STORE_NAME as CORE_MODULES } from '../googlesitekit/modules/datastore/constants';
+import CTA from '../components/notifications/cta';
+
+const { useSelect } = Data;
+
+/**
+ * ReportZero component.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Object} props            Component props.
+ * @param {string} props.moduleSlug The module slug.
+ * @return {WPElement} CTA component with data error message.
+ */
+export default function ReportZero( { moduleSlug } ) {
+	const module = useSelect( ( select ) => select( CORE_MODULES ).getModule( moduleSlug ) );
+	return <CTA
+
+		/* translators: %s: Module name */
+		title={ sprintf( __( '%s Gathering Data', 'google-site-kit' ), module?.name ) }
+
+		/* translators: %s: Module name */
+		description={ sprintf( __( '%s data is not yet available, please check back later.', 'google-site-kit' ), module?.name ) }
+	/>;
+}
