669d224b051d8679f9461db69ac67839495fe4f2
Evan Mattson <evan.mattson@10up.com>
2020-11-18 19:24:36 +0200

    Update report-args utilities and add tests.

--- assets/js/modules/analytics/util/report-args.js
+++ assets/js/modules/analytics/util/report-args.js
@@ -27,6 +27,22 @@ const SLASH_ENCODED = encodeURIComponent( '/' );
 const SLASH_ARGS_ENCODED = SLASH_ENCODED.replace( '%', '~' );
 
 /**
+ * Encodes an individual value of a report args key/value pair.
+ *
+ * Uses string.replace internally with RegExp as string.replaceAll
+ * is not yet supported in Node and some browsers.
+ *
+ * @since n.e.x.t
+ *
+ * @param {*} value Raw argument value.
+ * @return {string} Encoded value.
+ */
+const encodeValue = ( value ) => {
+	return encodeURIComponent( value )
+		.replace( new RegExp( SLASH_ENCODED, 'g' ), SLASH_ARGS_ENCODED );
+};
+
+/**
  * Converts an object of report arguments into the special URL segment format.
  *
  * @since n.e.x.t
@@ -38,8 +54,6 @@ export const reportArgsToURLSegment = ( reportArgs ) => {
 	invariant( isPlainObject( reportArgs ), 'report args must be a plain object' );
 
 	return Object.entries( reportArgs )
-		.map( ( [ key, value ] ) => {
-			return `${ key }=${ encodeURIComponent( value ).replaceAll( SLASH_ENCODED, SLASH_ARGS_ENCODED ) }`;
-		} )
+		.map( ( [ key, value ] ) => `${ key }=${ encodeValue( value ) }` )
 		.join( '&' );
 };
