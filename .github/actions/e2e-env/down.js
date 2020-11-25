3b5c29fd70d004fcf2771c0698b03c3103c24fca
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-11-25 15:05:33 +0200

    Remove useless scripts.

--- .github/actions/e2e-env/down.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require( 'child_process' );
-const { resolve } = require( 'path' );
-
-execSync( resolve( __dirname, '../../../bin/local-env/stop.sh' ), {
-	shell: '/bin/bash',
-	stdio: 'inherit',
-} );
