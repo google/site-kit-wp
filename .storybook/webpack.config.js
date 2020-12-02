93f886fa11b698e0b48dc2baccb2cccff9c803ab
Evan Mattson <evan.mattson@10up.com>
2020-12-02 10:46:22 +0200

    Add file header and organize dependencies to Storybook webpack config.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,8 +1,37 @@
+/**
+ * Storybook Webpack config.
+ *
+ * Site Kit by Google, Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Node dependencies
+ */
 const path = require( 'path' );
+
+/**
+ * External dependencies
+ */
+const mapValues = require( 'lodash/mapValues' );
 const omitBy = require( 'lodash/omitBy' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+
+/**
+ * Internal dependencies
+ */
 const mainConfig = require( '../webpack.config' );
-const mapValues = require( 'lodash/mapValues' );
 
 module.exports = async ( { config } ) => {
 	const siteKitExternals = omitBy( mainConfig.siteKitExternals, ( value, key ) => key.startsWith( '@wordpress' ) );
