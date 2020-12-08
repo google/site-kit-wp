56b7503c12d633201be363670364b69f6ad3f5ce
Felix Arntz <felixarntz@google.com>
2020-12-08 14:29:54 -0800

    Combine two translation strings so that they have the same context.

--- assets/js/modules/adsense/util/site-stats-data.js
+++ assets/js/modules/adsense/util/site-stats-data.js
@@ -77,8 +77,8 @@ export function getSiteStatsDataForGoogleChart( current, previous, label, select
 			: 100; // if previous month has 0, we need to pretend it's 100% growth, thus the "difference" has to be 100
 
 		const dateRange = sprintf(
-			/* translators: %1$s: date for user stats, %2$s: previous date for user stats comparison */
-			_x( '%1$s vs %2$s', 'Date range for AdSense dashboard chart tooltip', 'google-site-kit' ),
+			/* translators: 1: date for user stats, 2: previous date for user stats comparison */
+			_x( '%1$s vs %2$s', 'Date range for chart tooltip', 'google-site-kit' ),
 			currentDate.toLocaleDateString( locale, localeDateOptions ),
 			previousDate.toLocaleDateString( locale, localeDateOptions ),
 		);
@@ -89,8 +89,8 @@ export function getSiteStatsDataForGoogleChart( current, previous, label, select
 		}
 
 		const statInfo = sprintf(
-			/* translators: %1$s: selected stat label, %2$s: numberic value of selected stat, %3$s: up or down arrow , %4$s: different change in percentage, %%: percent symbol */
-			_x( '%1$s: <strong>%2$s</strong> <em>%3$s %4$s%%</em>', 'Stat information for AdSense dashbaord chart tooltip', 'google-site-kit' ),
+			/* translators: 1: selected stat label, 2: numeric value of selected stat, 3: up or down arrow , 4: different change in percentage, %%: percent symbol */
+			_x( '%1$s: <strong>%2$s</strong> <em>%3$s %4$s%%</em>', 'Stat information for chart tooltip', 'google-site-kit' ),
 			label,
 			tooltipData,
 			`<svg width="9" height="9" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" class="${ classnames( 'googlesitekit-change-arrow', {
