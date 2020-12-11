ce866e904f5f2e87f6eb0a181925fb2cf8bc12ce
Asvin Balloo <asvin.balloo@10up.com>
2020-12-11 12:21:42 +0400

    Update chunks in filenames to be only in production mode.

--- webpack.config.js
+++ webpack.config.js
@@ -182,7 +182,7 @@ const webpackConfig = ( env, argv ) => {
 			output: {
 				filename: ( mode === 'production' ? '[name].[contenthash].js' : '[name].js' ),
 				path: path.join( __dirname, 'dist/assets/js' ),
-				chunkFilename: '[name].[chunkhash].js',
+				chunkFilename: ( mode === 'production' ? '[name].[chunkhash].js' : '[name].js' ),
 				publicPath: '',
 				/*
 					If multiple webpack runtimes (from different compilations) are used on the
