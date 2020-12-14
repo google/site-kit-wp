fad2fc5cb9ef19bd4892176cd5a057e41b9ae697
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-12-14 15:10:55 +0200

    Refactor blockable tags tests.

--- tests/phpunit/integration/Core/Modules/Tags/Module_AMP_TagTest.php
+++ tests/phpunit/integration/Core/Modules/Tags/Module_AMP_TagTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class Google\Site_Kit\Tests\Core\Modules\Module_With_Blockable_Tags_TraitTest
+ * Class Google\Site_Kit\Tests\Core\Modules\Tags\Module_AMP_TagTest
  *
  * @package   Google\Site_Kit\Tests\Core\Modules
  * @copyright 2020 Google LLC
@@ -8,46 +8,25 @@
  * @link      https://sitekit.withgoogle.com
  */
 
-namespace Google\Site_Kit\Tests\Core\Modules;
+namespace Google\Site_Kit\Tests\Core\Modules\Tags;
 
 use Google\Site_Kit\Tests\Core\Modules\Tags\FakeModule_AMP_Tag;
-use Google\Site_Kit\Tests\Core\Modules\Tags\FakeModule_Web_Tag;
 use Google\Site_Kit\Tests\TestCase;
 
-class Module_With_Blockable_Tags_TraitTest extends TestCase {
+class Module_AMP_TagTest extends TestCase {
 
 	private $amp_tag;
-
-	private $web_tag;
-
-	private $tag_id;
 
 	private $module_slug;
 
 	public function setUp() {
 		parent::setUp();
 
-		$this->tag_id      = 'test-tag-id';
 		$this->module_slug = 'fake-module';
-
-		$this->web_tag = new FakeModule_Web_Tag( $this->tag_id, $this->module_slug );
-		$this->amp_tag = new FakeModule_AMP_Tag( $this->tag_id, $this->module_slug );
+		$this->amp_tag     = new FakeModule_AMP_Tag( 'test-tag-id', $this->module_slug );
 	}
 
 	public function test_is_tag_blocked() {
-		// Tag is not blocked by default.
-		$this->assertFalse( $this->web_tag->is_tag_blocked() );
-
-		add_filter( "googlesitekit_{$this->module_slug}_tag_blocked", '__return_true' );
-
-		$this->assertTrue( $this->web_tag->is_tag_blocked() );
-
-		// The return value of the filter is cast to a boolean before returning.
-		add_filter( "googlesitekit_{$this->module_slug}_tag_blocked", '__return_empty_string' );
-		$this->assertFalse( $this->web_tag->is_tag_blocked() );
-	}
-
-	public function test_is_tag_blocked__amp() {
 		// Tag is not blocked by default.
 		$this->assertFalse( $this->amp_tag->is_tag_blocked() );
 
@@ -56,22 +35,12 @@ class Module_With_Blockable_Tags_TraitTest extends TestCase {
 		$this->assertTrue( $this->amp_tag->is_tag_blocked() );
 	}
 
-	public function test_get_tag_blocked_on_consent_attribute() {
-		// Returns empty string by default.
-		$this->assertEquals( '', $this->web_tag->get_tag_blocked_on_consent_attribute() );
-
-		// Returns attributes to prevent script loading if truthy.
-		add_filter( "googlesitekit_{$this->module_slug}_tag_block_on_consent", '__return_true' );
-
-		$this->assertEquals( ' type="text/plain" data-block-on-consent', $this->web_tag->get_tag_blocked_on_consent_attribute() );
-	}
-
 	/**
 	 * @dataProvider amp_block_on_consent_attribute_provider
 	 * @param mixed  $filter_value Value to return from filter callback.
 	 * @param string $expected     Expected return value from method.
 	 */
-	public function test_get_tag_amp_blocked_on_consent_attribute( $filter_value, $expected ) {
+	public function test_get_tag_blocked_on_consent_attribute( $filter_value, $expected ) {
 		// Returns empty string by default.
 		$this->assertEquals( '', $this->amp_tag->get_tag_blocked_on_consent_attribute() );
 
@@ -127,4 +96,5 @@ class Module_With_Blockable_Tags_TraitTest extends TestCase {
 			),
 		);
 	}
+
 }
