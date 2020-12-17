5a9f17571bcbeaea7e7b762049bd9916878f0c55
Evan Mattson <evan.mattson@10up.com>
2020-12-17 12:32:31 +0200

    Update alignment of inner args to create_analytics_site_data_request.

--- includes/Modules/Analytics.php
+++ includes/Modules/Analytics.php
@@ -1362,12 +1362,12 @@ final class Analytics extends Module
 	 * @param array $args {
 	 *     Optional. Additional arguments.
 	 *
-	 *     @type array  $dimensions        List of request dimensions. Default empty array.
-	 *     @type Google_Service_AnalyticsReporting_DimensionFilter[]  $dimension_filters List of dimension filter instances for the specified request dimensions. Default empty array.
-	 *     @type string $start_date        Start date in 'Y-m-d' format. Default empty string.
-	 *     @type string $end_date          End date in 'Y-m-d' format. Default empty string.
-	 *     @type string $page              Specific page URL to filter by. Default empty string.
-	 *     @type int    $row_limit         Limit of rows to return. Default 100.
+	 *     @type array                                               $dimensions        List of request dimensions. Default empty array.
+	 *     @type Google_Service_AnalyticsReporting_DimensionFilter[] $dimension_filters List of dimension filter instances for the specified request dimensions. Default empty array.
+	 *     @type string                                              $start_date        Start date in 'Y-m-d' format. Default empty string.
+	 *     @type string                                              $end_date          End date in 'Y-m-d' format. Default empty string.
+	 *     @type string                                              $page              Specific page URL to filter by. Default empty string.
+	 *     @type int                                                 $row_limit         Limit of rows to return. Default 100.
 	 * }
 	 * @return Google_Service_AnalyticsReporting_ReportRequest|WP_Error Analytics site request instance.
 	 */
