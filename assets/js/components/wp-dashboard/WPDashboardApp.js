ceeaa04cbcad236fb22bb8ea543d16444f0cb452
Felix Arntz <felixarntz@google.com>
2021-01-11 14:11:14 -0800

    Fix visual disparities with existing WP dashboard widget.

--- assets/js/components/wp-dashboard/WPDashboardApp.js
+++ assets/js/components/wp-dashboard/WPDashboardApp.js
@@ -17,6 +17,11 @@
  */
 
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
@@ -36,7 +41,6 @@ import AnalyticsInactiveCTA from '../AnalyticsInactiveCTA';
 import CompleteModuleActivationCTA from '../CompleteModuleActivationCTA';
 import { STORE_NAME as CORE_SITE } from '../../googlesitekit/datastore/site/constants';
 import { STORE_NAME as CORE_MODULES } from '../../googlesitekit/modules/datastore/constants';
-import { Cell } from '../../material-components/layout';
 const { useSelect } = Data;
 
 const WPDashboardApp = () => {
@@ -56,18 +60,10 @@ const WPDashboardApp = () => {
 				</Link>
 			</div>
 			{ featureFlags.widgets.wpDashboard.enabled && (
-				<div className="googlesitekit-wp-dashboard-stats googlesitekit-wp-dashboard-stats--fourup">
-					{ ( ! analyticsModuleConnected || ! analyticsModuleActive ) && (
-						<Cell mdSize={ 4 } lgSize={ 6 }>
-							{ ! analyticsModuleActive && (
-								<AnalyticsInactiveCTA />
-							) }
-
-							{ ( analyticsModuleActive && ! analyticsModuleConnected ) && (
-								<CompleteModuleActivationCTA slug="analytics" />
-							) }
-						</Cell>
-					) }
+				<div className={ classnames(
+					'googlesitekit-wp-dashboard-stats',
+					{ 'googlesitekit-wp-dashboard-stats--fourup': analyticsModuleActive && analyticsModuleConnected }
+				) }>
 					{ analyticsModuleActive && analyticsModuleConnected && (
 						<Fragment>
 							<WPDashboardUniqueVisitors />
@@ -76,6 +72,16 @@ const WPDashboardApp = () => {
 					) }
 					<WPDashboardImpressions />
 					<WPDashboardClicks />
+					{ ( ! analyticsModuleConnected || ! analyticsModuleActive ) && (
+						<div className="googlesitekit-wp-dashboard-stats__cta">
+							{ ! analyticsModuleActive && (
+								<AnalyticsInactiveCTA />
+							) }
+							{ analyticsModuleActive && (
+								<CompleteModuleActivationCTA slug="analytics" />
+							) }
+						</div>
+					) }
 				</div>
 			) }
 			<LegacyWPDashboardModules />
