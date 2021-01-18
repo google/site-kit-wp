cdf864d0a1c068013c2e05c11e073dfd27f5b13f
Eugene Manuilov <eugene.manuilov@gmail.com>
2021-01-18 17:20:22 +0200

    Rework helper plugin to reset options on activation and deactivation of the plugin.

--- tests/e2e/plugins/user-input.php
+++ tests/e2e/plugins/user-input.php
@@ -15,113 +15,118 @@
 use Google\Site_Kit\Context;
 use Google\Site_Kit\Core\Authentication\Google_Proxy;
 
-add_filter(
-	'pre_http_request',
-	function( $pre, $args, $url ) {
-		$google_proxy            = new Google_Proxy( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
-		$user_input_settings_url = $google_proxy->url( Google_Proxy::USER_INPUT_SETTINGS_URI );
-		if ( $url !== $user_input_settings_url ) {
-			return $pre;
-		}
+register_activation_hook( __FILE__, 'e2e_user_input_settings_reset' );
+register_deactivation_hook( __FILE__, 'e2e_user_input_settings_reset' );
 
-		$user_id  = get_current_user_id();
-		$defaults = array(
-			'role'          => array(
-				'values'     => array(),
-				'scope'      => 'user',
-				'answeredBy' => 0,
-			),
-			'postFrequency' => array(
-				'values'     => array(),
-				'scope'      => 'user',
-				'answeredBy' => 0,
-			),
-			'goals'         => array(
-				'values'     => array(),
-				'scope'      => 'site',
-				'answeredBy' => 0,
-			),
-			'helpNeeded'    => array(
-				'values'     => array(),
-				'scope'      => 'site',
-				'answeredBy' => 0,
-			),
-			'searchTerms'   => array(
-				'values'     => array(),
-				'scope'      => 'site',
-				'answeredBy' => 0,
-			),
-		);
+add_filter( 'pre_http_request', 'e2e_user_input_settings_proxy_handler', 10, 3 );
 
-		if ( ! empty( $args['body'] ) ) {
-			$body = json_decode( $args['body'], true );
-			if ( ! empty( $body ) ) {
-				$original_site_settings = get_option( 'googlesitekit_temp_userinput_sitewide', array() );
-				$user_settings          = array();
-				$site_settings          = array();
+function e2e_user_input_settings_reset() {
+	delete_option( 'googlesitekit_temp_userinput_sitewide' );
+	delete_option( 'googlesitekit_temp_userinput_' . get_current_user_id() );
+}
 
-				foreach ( $defaults as $key => $values ) {
-					if ( 'site' === $values['scope'] ) {
-						$new_values = ! empty( $body[ $key ] ) && is_array( $body[ $key ] )
-							? $body[ $key ]
-							: array();
+function e2e_user_input_settings_proxy_handler( $pre, $args, $url ) {
+	$google_proxy            = new Google_Proxy( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+	$user_input_settings_url = $google_proxy->url( Google_Proxy::USER_INPUT_SETTINGS_URI );
+	if ( $url !== $user_input_settings_url ) {
+		return $pre;
+	}
 
-						$original_values = ! empty( $original_site_settings[ $key ]['values'] ) && is_array( $original_site_settings[ $key ]['values'] )
-							? $original_site_settings[ $key ]['values']
-							: array();
+	$user_id  = get_current_user_id();
+	$defaults = array(
+		'role'          => array(
+			'values'     => array(),
+			'scope'      => 'user',
+			'answeredBy' => 0,
+		),
+		'postFrequency' => array(
+			'values'     => array(),
+			'scope'      => 'user',
+			'answeredBy' => 0,
+		),
+		'goals'         => array(
+			'values'     => array(),
+			'scope'      => 'site',
+			'answeredBy' => 0,
+		),
+		'helpNeeded'    => array(
+			'values'     => array(),
+			'scope'      => 'site',
+			'answeredBy' => 0,
+		),
+		'searchTerms'   => array(
+			'values'     => array(),
+			'scope'      => 'site',
+			'answeredBy' => 0,
+		),
+	);
 
-						$answered_by = ! empty( $original_site_settings[ $key ]['answeredBy'] )
-							? $original_site_settings[ $key ]['answeredBy']
-							: null;
+	if ( ! empty( $args['body'] ) ) {
+		$body = json_decode( $args['body'], true );
+		if ( ! empty( $body ) ) {
+			$original_site_settings = get_option( 'googlesitekit_temp_userinput_sitewide', array() );
+			$user_settings          = array();
+			$site_settings          = array();
 
-						if ( count( $new_values ) !== count( $original_values ) ) {
-							$answered_by = $user_id;
-						} else {
-							$intersection = array_intersect( $new_values, $original_values );
-							if ( count( $intersection ) !== count( $new_values ) ) {
-								$answered_by = $user_id;
-							}
-						}
+			foreach ( $defaults as $key => $values ) {
+				if ( 'site' === $values['scope'] ) {
+					$new_values = ! empty( $body[ $key ] ) && is_array( $body[ $key ] )
+						? $body[ $key ]
+						: array();
 
-						$site_settings[ $key ] = array(
-							'values'     => $new_values,
-							'scope'      => $values['scope'],
-							'answeredBy' => $answered_by,
-						);
+					$original_values = ! empty( $original_site_settings[ $key ]['values'] ) && is_array( $original_site_settings[ $key ]['values'] )
+						? $original_site_settings[ $key ]['values']
+						: array();
+
+					$answered_by = ! empty( $original_site_settings[ $key ]['answeredBy'] )
+						? $original_site_settings[ $key ]['answeredBy']
+						: null;
+
+					if ( count( $new_values ) !== count( $original_values ) ) {
+						$answered_by = $user_id;
 					} else {
-						$user_settings[ $key ] = array(
-							'values'     => ! empty( $body[ $key ] ) ? $body[ $key ] : array(),
-							'scope'      => $values['scope'],
-							'answeredBy' => $user_id,
-						);
+						$intersection = array_intersect( $new_values, $original_values );
+						if ( count( $intersection ) !== count( $new_values ) ) {
+							$answered_by = $user_id;
+						}
 					}
+
+					$site_settings[ $key ] = array(
+						'values'     => $new_values,
+						'scope'      => $values['scope'],
+						'answeredBy' => $answered_by,
+					);
+				} else {
+					$user_settings[ $key ] = array(
+						'values'     => ! empty( $body[ $key ] ) ? $body[ $key ] : array(),
+						'scope'      => $values['scope'],
+						'answeredBy' => $user_id,
+					);
 				}
-
-				update_option( 'googlesitekit_temp_userinput_sitewide', $site_settings, 'no' );
-				update_option( 'googlesitekit_temp_userinput_' . $user_id, $user_settings, 'no' );
 			}
+
+			update_option( 'googlesitekit_temp_userinput_sitewide', $site_settings, 'no' );
+			update_option( 'googlesitekit_temp_userinput_' . $user_id, $user_settings, 'no' );
 		}
+	}
 
-		$user_input    = array();
-		$user_settings = get_option( 'googlesitekit_temp_userinput_' . $user_id, array() );
-		$site_settings = get_option( 'googlesitekit_temp_userinput_sitewide', array() );
+	$user_input    = array();
+	$user_settings = get_option( 'googlesitekit_temp_userinput_' . $user_id, array() );
+	$site_settings = get_option( 'googlesitekit_temp_userinput_sitewide', array() );
 
-		foreach ( $defaults as $key => $values ) {
-			if ( isset( $user_settings[ $key ] ) ) {
-				$user_input[ $key ] = $user_settings[ $key ];
-			} elseif ( isset( $site_settings[ $key ] ) ) {
-				$user_input[ $key ] = $site_settings[ $key ];
-			} else {
-				$user_input[ $key ] = $values;
-			}
+	foreach ( $defaults as $key => $values ) {
+		if ( isset( $user_settings[ $key ] ) ) {
+			$user_input[ $key ] = $user_settings[ $key ];
+		} elseif ( isset( $site_settings[ $key ] ) ) {
+			$user_input[ $key ] = $site_settings[ $key ];
+		} else {
+			$user_input[ $key ] = $values;
 		}
+	}
 
-		return array(
-			'headers'  => array(),
-			'body'     => wp_json_encode( $user_input ),
-			'response' => array( 'code' => 200 ),
-		);
-	},
-	10,
-	3
-);
+	return array(
+		'headers'  => array(),
+		'body'     => wp_json_encode( $user_input ),
+		'response' => array( 'code' => 200 ),
+	);
+}
