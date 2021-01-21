fc24239eba354e6d773dff47d2bd3d52da3ed378
Felix Arntz <felixarntz@google.com>
2021-01-20 16:52:19 -0800

    Check for correct Site Kit OAuth redirect URL in condition for whether to show UnsatisfiedScopesAlert.

--- assets/js/components/notifications/UnsatisfiedScopesAlert.js
+++ assets/js/components/notifications/UnsatisfiedScopesAlert.js
@@ -59,7 +59,7 @@ function mapScopesToModuleNames( scopes ) {
 }
 
 export default function UnsatisfiedScopesAlert() {
-	const isNavigating = useSelect( ( select ) => select( CORE_LOCATION ).isNavigatingTo( /\/o\/oauth2/i ) );
+	const isNavigating = useSelect( ( select ) => select( CORE_LOCATION ).isNavigatingTo( /(\/o\/oauth2)|(googlesitekit_connect=1)/i ) );
 	const unsatisfiedScopes = useSelect( ( select ) => select( CORE_USER ).getUnsatisfiedScopes() );
 	const connectURL = useSelect( ( select ) => select( CORE_USER ).getConnectURL( {
 		redirectURL: global.location.href,
