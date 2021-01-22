232625413149a3e733d29084c7606edb37575e03
Ben Bowler <ben.bowler@10up.com>
2021-01-22 15:40:50 +0000

    Remove references to fontName: 'Roboto' in UserDimensionsPieChart to provent rendering issues.

--- assets/js/modules/analytics/components/dashboard/DashboardAllTrafficWidget/UserDimensionsPieChart.js
+++ assets/js/modules/analytics/components/dashboard/DashboardAllTrafficWidget/UserDimensionsPieChart.js
@@ -246,7 +246,6 @@ UserDimensionsPieChart.chartOptions = {
 		width: '100%',
 	},
 	backgroundColor: 'transparent',
-	// fontName: 'Roboto',
 	fontSize: 12,
 	height: 410,
 	legend: {
@@ -260,7 +259,6 @@ UserDimensionsPieChart.chartOptions = {
 	pieHole: 0.6,
 	pieSliceTextStyle: {
 		color: 'black',
-		// fontName: 'Roboto',
 		fontSize: 12,
 	},
 	slices: {
