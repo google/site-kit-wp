aeeefc03ae180b1a72b5cfa54cdc87f8f7c7be77
Evan Mattson <evan.mattson@10up.com>
2021-01-28 12:25:05 +0200

    Add missing import.

--- includes/Modules/Optimize.php
+++ includes/Modules/Optimize.php
@@ -10,6 +10,7 @@
 
 namespace Google\Site_Kit\Modules;
 
+use Google\Site_Kit\Core\Assets\Asset;
 use Google\Site_Kit\Core\Assets\Script;
 use Google\Site_Kit\Core\Modules\Module;
 use Google\Site_Kit\Core\Modules\Module_Settings;
