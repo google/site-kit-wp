193c21dbd19512b2c7e99b5c043c8241e3debf01
John Phillips <johnbasraphillips@gmail.com>
2021-02-01 16:57:02 +0000

    Refactor AdSenseDashboardWidget to functional / hook-based style.

--- assets/js/modules/adsense/components/dashboard/AdSenseDashboardWidget.js
+++ assets/js/modules/adsense/components/dashboard/AdSenseDashboardWidget.js
@@ -20,7 +20,11 @@
  * WordPress dependencies
  */
 import { withFilters } from '@wordpress/components';
-import { Component, Fragment } from '@wordpress/element';
+import {
+	// Component,
+	Fragment,
+	useState,
+} from '@wordpress/element';
 import { __, _x, _n, sprintf } from '@wordpress/i18n';
 
 /**
@@ -35,7 +39,7 @@ import getNoDataComponent from '../../../../components/legacy-notifications/noda
 import getDataErrorComponent from '../../../../components/legacy-notifications/data-error';
 import ProgressBar from '../../../../components/ProgressBar';
 import ModuleSettingsWarning from '../../../../components/legacy-notifications/module-settings-warning';
-import { getModulesData } from '../../../../util';
+//import { getModulesData } from '../../../../util';
 import DateRangeSelector from '../../../../components/DateRangeSelector';
 import HelpLink from '../../../../components/HelpLink';
 import Header from '../../../../components/Header';
@@ -45,29 +49,39 @@ import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
 import { Cell, Grid, Row } from '../../../../material-components';
 import { getCurrentDateRangeDayCount } from '../../../../util/date-range';
 
-const { withSelect } = Data;
+const {
+	// withSelect,
+	useSelect,
+} = Data;
+
+import { CORE_MODULES } from '../../../../googlesitekit/modules/datastore/constants';
+import { MODULES_ADSENSE, DATE_RANGE_OFFSET } from '../../../adsense/datastore/constants';
+import { generateDateRangeArgs } from '../../../adsense/util/report-date-range-args';
 
 // Empty component to allow filtering in refactored version.
 const AdSenseDashboardZeroData = withFilters( 'googlesitekit.AdSenseDashboardZeroData' )( () => null );
 
-class AdSenseDashboardWidget extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			receivingData: true,
-			error: false,
-			zeroData: false,
-			loading: true,
-		};
-		this.handleDataError = this.handleDataError.bind( this );
-		this.handleDataSuccess = this.handleDataSuccess.bind( this );
-		this.handleZeroData = this.handleZeroData.bind( this );
-	}
+export default function AdSenseDashboardWidget() {
+	const [ receivingData, setReceivingData ] = useState( true );
+	const [ error, setError ] = useState( false );
+	const [ errorObj, setErrorObj ] = useState();
+	const [ loading, setLoading ] = useState( true );
+	const [ zeroData, setZeroData ] = useState( false );
+
+	const adSenseModuleConnected = useSelect( ( select ) => select( CORE_MODULES ).isModuleConnected( 'adsense' ) );
+	const dateRangeDates = useSelect( ( select ) => select( CORE_USER ).getDateRangeDates( {
+		offsetDays: DATE_RANGE_OFFSET,
+	} ) );
+	const accountSiteURL = useSelect( ( select ) => select( MODULES_ADSENSE ).getServiceAccountSiteURL(
+		{ ...generateDateRangeArgs( dateRangeDates ) }
+	) );
+	const dateRange = useSelect( ( select ) => select( CORE_USER ).getDateRange() );
+	const currentDayCount = getCurrentDateRangeDayCount( dateRange );
 
 	/**
-	 * Handle data errors from the contained AdSense component(s).
+	 * Handles data errors from the contained Analytics component(s).
 	 *
-	 * Currently handled in the AdSenseEstimateEarningsWidget component.
+	 * Currently handled in the AnalyticsDashboardWidget component.
 	 *
 	 * If this component's API data calls returns an error, the error message is passed to this callback, resulting in the display of an error Notification.
 	 *
@@ -76,163 +90,136 @@ class AdSenseDashboardWidget extends Component {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param {string} error    A potential error string.
-	 * @param {Object} errorObj Full error object.
+	 * @param {string} err    A potential error string.
+	 * @param {Object} errObj Full error object.
 	 */
-	handleDataError( error, errorObj ) {
-		this.setState( {
-			receivingData: false,
-			error,
-			errorObj,
-			loading: false,
-		} );
-	}
+	const handleDataError = ( err, errObj ) => {
+		setReceivingData( false );
+		setLoading( false );
+		setError( err );
+		setErrorObj( errObj );
+	};
 
 	/**
-	 * Loading is set to false until data starts to resolve.
+	 * Sets Loading to false until data starts to resolve.
 	 */
-	handleDataSuccess() {
-		this.setState( {
-			receivingData: true,
-			error: false,
-			loading: false,
-			zeroData: false,
-		} );
-	}
+	const handleDataSuccess = () => {
+		setReceivingData( true );
+		setLoading( false );
+		setZeroData( false );
+		setError( false );
+	};
 
 	/**
-	 * Show the "We're getting your site ready for ads. screen until we have data.".
+	 * Shows the "We're getting your site ready for ads. screen until we have data.".
 	 */
-	handleZeroData() {
-		this.setState( {
-			zeroData: true,
-			loading: false,
-		} );
-	}
+	const handleZeroData = () => {
+		setZeroData( true );
+		setLoading( false );
+	};
 
-	render() {
-		const modulesData = getModulesData();
+	// Hide AdSense data display when we don't have data.
+	const wrapperClass = ( loading || ! receivingData || zeroData ) ? 'googlesitekit-nodata' : '';
 
-		const {
-			receivingData,
-			error,
-			errorObj,
-			loading,
-			zeroData,
-		} = this.state;
-
-		const { dateRange } = this.props;
-		const { homepage } = modulesData.adsense;
-
-		// Hide AdSense data display when we don't have data.
-		const wrapperClass = ( loading || ! receivingData || zeroData ) ? 'googlesitekit-nodata' : '';
-		const currentDayCount = getCurrentDateRangeDayCount( dateRange );
-
-		let moduleStatus;
-		let moduleStatusText;
-		if ( ! error && modulesData.adsense.setupComplete ) {
-			moduleStatus = 'connected';
-			moduleStatusText = sprintf(
-				/* translators: %s: module name. */
-				__( '%s is connected', 'google-site-kit' ),
-				_x( 'AdSense', 'Service name', 'google-site-kit' )
-			);
-		} else {
-			moduleStatus = 'not-connected';
-			moduleStatusText = sprintf(
-				/* translators: %s: module name. */
-				__( '%s is not connected', 'google-site-kit' ),
-				_x( 'AdSense', 'Service name', 'google-site-kit' )
-			);
-		}
-
-		return (
-			<Fragment>
-				<Header>
-					{ moduleStatus === 'connected' && <DateRangeSelector /> }
-				</Header>
-				<div className={ wrapperClass }>
-					<Alert module="adsense" />
-				</div>
-
-				<div className="googlesitekit-module-page googlesitekit-module-page--adsense">
-					<Grid>
-						<Row>
-							<Cell size={ 12 }>
-								<PageHeader
-									title={ _x( 'AdSense', 'Service name', 'google-site-kit' ) }
-									icon={
-										<AdSenseIcon
-											className="googlesitekit-page-header__icon"
-											height="33"
-											width="33"
-										/>
-									}
-									status={ moduleStatus }
-									statusText={ moduleStatusText }
-								/>
-								{ loading && <ProgressBar /> }
-							</Cell>
-
-							{ /* Data issue: on error display a notification. On missing data: display a CTA. */ }
-							{ zeroData &&
-								<Cell size={ 12 }>
-									<Layout fill>
-										<AdSenseDashboardZeroData />
-									</Layout>
-								</Cell>
-							}
-
-							{ ! receivingData && (
-								error ? getDataErrorComponent( 'adsense', error, true, true, true, errorObj ) : getNoDataComponent( _x( 'AdSense', 'Service name', 'google-site-kit' ), true, true, true )
-							) }
-
-							<Cell className={ wrapperClass } size={ 12 }>
-								<ModuleSettingsWarning slug="adsense" />
-							</Cell>
-
-							<Cell className={ wrapperClass } size={ 12 }>
-								<Layout
-									header
-									title={ sprintf(
-										/* translators: %s: number of days */
-										_n( 'Performance over the last %s day', 'Performance over the last %s days', currentDayCount, 'google-site-kit' ),
-										currentDayCount
-									) }
-									headerCTALabel={ __( 'See full stats in AdSense', 'google-site-kit' ) }
-									headerCTALink={ homepage }
-								>
-									<AdSensePerformanceWidget
-										handleDataError={ ( err ) => {
-											// If there is no error, it is a zero data condition, otherwise call the error handler.
-											if ( ! err ) {
-												this.handleZeroData();
-											} else {
-												this.handleDataError();
-											}
-										} }
-										handleDataSuccess={ this.handleDataSuccess }
-									/>
-								</Layout>
-							</Cell>
-
-							<Cell className={ wrapperClass } size={ 12 }>
-								<DashboardAdSenseTopPages />
-							</Cell>
-
-							<Cell alignRight size={ 12 }>
-								<HelpLink />
-							</Cell>
-						</Row>
-					</Grid>
-				</div>
-			</Fragment>
+	let moduleStatus;
+	let moduleStatusText;
+	if ( ! error && adSenseModuleConnected ) {
+		moduleStatus = 'connected';
+		moduleStatusText = sprintf(
+			/* translators: %s: module name. */
+			__( '%s is connected', 'google-site-kit' ),
+			_x( 'AdSense', 'Service name', 'google-site-kit' )
+		);
+	} else {
+		moduleStatus = 'not-connected';
+		moduleStatusText = sprintf(
+			/* translators: %s: module name. */
+			__( '%s is not connected', 'google-site-kit' ),
+			_x( 'AdSense', 'Service name', 'google-site-kit' )
 		);
 	}
+
+	return (
+		<Fragment>
+			<Header>
+				{ moduleStatus === 'connected' && <DateRangeSelector /> }
+			</Header>
+			<div className={ wrapperClass }>
+				<Alert module="adsense" />
+			</div>
+
+			<div className="googlesitekit-module-page googlesitekit-module-page--adsense">
+				<Grid>
+					<Row>
+						<Cell size={ 12 }>
+							<PageHeader
+								title={ _x( 'AdSense', 'Service name', 'google-site-kit' ) }
+								icon={
+									<AdSenseIcon
+										className="googlesitekit-page-header__icon"
+										height="33"
+										width="33"
+									/>
+								}
+								status={ moduleStatus }
+								statusText={ moduleStatusText }
+							/>
+							{ loading && <ProgressBar /> }
+						</Cell>
+
+						{ /* Data issue: on error display a notification. On missing data: display a CTA. */ }
+						{ zeroData &&
+							<Cell size={ 12 }>
+								<Layout fill>
+									<AdSenseDashboardZeroData />
+								</Layout>
+							</Cell>
+						}
+
+						{ ! receivingData && (
+							error ? getDataErrorComponent( 'adsense', error, true, true, true, errorObj ) : getNoDataComponent( _x( 'AdSense', 'Service name', 'google-site-kit' ), true, true, true )
+						) }
+
+						<Cell className={ wrapperClass } size={ 12 }>
+							<ModuleSettingsWarning slug="adsense" />
+						</Cell>
+
+						<Cell className={ wrapperClass } size={ 12 }>
+							<Layout
+								header
+								title={ sprintf(
+									/* translators: %s: number of days */
+									_n( 'Performance over the last %s day', 'Performance over the last %s days', currentDayCount, 'google-site-kit' ),
+									currentDayCount
+								) }
+								headerCTALabel={ __( 'See full stats in AdSense', 'google-site-kit' ) }
+								headerCTALink={ accountSiteURL }
+							>
+								<AdSensePerformanceWidget
+									handleDataError={ ( err ) => {
+										// If there is no error, it is a zero data condition, otherwise call the error handler.
+										if ( ! err ) {
+											handleZeroData();
+										} else {
+											handleDataError();
+										}
+									} }
+									handleDataSuccess={ handleDataSuccess }
+								/>
+							</Layout>
+						</Cell>
+
+						<Cell className={ wrapperClass } size={ 12 }>
+							<DashboardAdSenseTopPages />
+						</Cell>
+
+						<Cell alignRight size={ 12 }>
+							<HelpLink />
+						</Cell>
+					</Row>
+				</Grid>
+			</div>
+		</Fragment>
+	);
 }
 
-export default withSelect( ( select ) => (
-	{
-		dateRange: select( CORE_USER ).getDateRange(),
-	} )
-)( AdSenseDashboardWidget );
