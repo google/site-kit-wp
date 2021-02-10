4c1ead526bf77ac71ba6ed8d166a24b522e1ddd6
Evan Mattson <evan.mattson@10up.com>
2021-02-10 22:36:11 +0200

    Refactor WidgetAreaRenderer with new selectors.

--- assets/js/googlesitekit/widgets/components/WidgetAreaRenderer.js
+++ assets/js/googlesitekit/widgets/components/WidgetAreaRenderer.js
@@ -30,29 +30,13 @@ import { STORE_NAME, WIDGET_AREA_STYLES } from '../datastore/constants';
 import WidgetRenderer from './WidgetRenderer';
 import { getWidgetLayout, combineWidgets } from '../util';
 import { Cell, Grid, Row } from '../../../material-components';
-import { separateNullWidgets } from '../util/separate-null-widgets';
 const { useSelect } = Data;
 
 export default function WidgetAreaRenderer( { slug, totalAreas } ) {
 	const widgetArea = useSelect( ( select ) => select( STORE_NAME ).getWidgetArea( slug ) );
-
-	const { widgets, widgetStates } = useSelect( ( select ) => {
-		const allWidgets = select( STORE_NAME ).getWidgets( slug );
-		const allWidgetStates = {};
-		allWidgets.forEach( ( widget ) => {
-			allWidgetStates[ widget.slug ] = select( STORE_NAME ).getWidgetState( widget.slug );
-		} );
-		return {
-			widgets: allWidgets,
-			widgetStates: allWidgetStates,
-		};
-	} );
-
-	const { activeWidgets } = separateNullWidgets( widgets, widgetStates );
-
-	if ( activeWidgets.length === 0 ) {
-		return null;
-	}
+	const widgets = useSelect( ( select ) => select( STORE_NAME ).getWidgets( slug ) );
+	const activeWidgets = useSelect( ( select ) => select( STORE_NAME ).getActiveWidgets( slug ) );
+	const widgetStates = useSelect( ( select ) => select( STORE_NAME ).getWidgetStates() );
 
 	// Compute the layout.
 	const {
@@ -86,7 +70,6 @@ export default function WidgetAreaRenderer( { slug, totalAreas } ) {
 			} : undefined }
 			key={ widget.slug }
 			slug={ widget.slug }
-			active={ activeWidgets.some( ( { slug: s } ) => s === widget.slug ) }
 		/>
 	) );
 
