a29fcfd1537a9b58eeb4525b527d77dd9b52e8bb
Ivan@Kruchkoff.com <ivan@kruchkoff.com>
2021-02-11 08:43:58 -0800

    Add test cases for Reset Persistent.

---
+++ tests/phpunit/integration/Core/Util/ResetPersistentTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * ResetPersistentTest
+ *
+ * @package   Google\Site_Kit\Tests\Core\Util
+ * @copyright 2021 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests\Core\Util;
+
+use Google\Site_Kit\Context;
+use Google\Site_Kit\Core\Util\Reset_Persistent;
+use Google\Site_Kit\Tests\TestCase;
+use Google\Site_Kit\Tests\OptionsTestTrait;
+use Google\Site_Kit\Tests\UserOptionsTestTrait;
+use Google\Site_Kit\Tests\TransientsTestTrait;
+
+/**
+ * @group Util
+ */
+class ResetPersistentTest extends TestCase {
+	use OptionsTestTrait, UserOptionsTestTrait, TransientsTestTrait;
+
+	public function test_all() {
+		$context = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
+		$this->assertFalse( $context->is_network_mode() );
+		update_option( 'googlesitekitpersistentkeep', 'keep' );
+		update_option( 'googlesitekitpersistent-keep', 'keep' );
+
+		$this->run_reset( $context );
+
+		// Ensure options that don't start with googlesitekitpersistent_ are not deleted.
+		$this->assertEquals( 'keep', get_option( 'googlesitekitpersistentkeep' ) );
+		$this->assertEquals( 'keep', get_option( 'googlesitekitpersistent-keep' ) );
+	}
+
+	/**
+	 * @group ms-required
+	 */
+	public function test_network_mode_all() {
+		$this->network_activate_site_kit();
+		$context = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
+		$this->assertTrue( $context->is_network_mode() );
+
+		$this->run_reset( $context );
+	}
+
+	protected function run_reset( Context $context ) {
+		wp_load_alloptions();
+		$this->assertNotFalse( wp_cache_get( 'alloptions', 'options' ) );
+
+		$user_id         = $this->factory()->user->create();
+		$reset           = new Reset_Persistent( $context );
+		$is_network_mode = $context->is_network_mode();
+
+		$this->init_option_values( $is_network_mode );
+		$this->init_user_option_values( $user_id, $is_network_mode );
+		$this->init_transient_values( $is_network_mode );
+
+		$reset->all();
+
+		// Ensure options cache is flushed (must check before accessing other options as this will re-prime the cache)
+		$this->assertFalse( wp_cache_get( 'alloptions', 'options' ) );
+		$this->assertOptionsDeleted( $is_network_mode );
+		$this->assertUserOptionsDeleted( $user_id, $is_network_mode );
+		$this->assertTransientsDeleted( $is_network_mode );
+	}
+}
