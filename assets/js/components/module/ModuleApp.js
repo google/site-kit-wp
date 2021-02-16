3f8bf656f7e97387b1aae9aeee2d7448436227a1
John Phillips <johnbasraphillips@gmail.com>
2021-02-16 12:14:21 +0000

    Add header and new ModuleFooter to ModuleApp.

--- assets/js/components/module/ModuleApp.js
+++ assets/js/components/module/ModuleApp.js
@@ -17,6 +17,11 @@
  */
 
 /**
+ * WordPress dependencies
+ */
+import { _x, sprintf, __ } from '@wordpress/i18n';
+
+/**
  * External dependencies
  */
 import PropTypes from 'prop-types';
@@ -28,19 +33,28 @@ import Data from 'googlesitekit-data';
 import { isFeatureEnabled } from '../../features';
 import Header from '../Header';
 import Alert from '../Alert';
+import PageHeader from '../PageHeader';
+import ModuleFooter from './ModuleFooter';
 import WidgetContextRenderer from '../../googlesitekit/widgets/components/WidgetContextRenderer';
 
 import DateRangeSelector from '../DateRangeSelector';
-
 import { CORE_MODULES } from '../../googlesitekit/modules/datastore/constants';
 
 const { useSelect } = Data;
 
 function ModuleApp( { moduleSlug } ) {
 	const screenWidgetContext = useSelect( ( select ) => select( CORE_MODULES ).getScreenWidgetContext( moduleSlug ) );
-	const moduleConnected = useSelect( ( select ) => select( CORE_MODULES ).isModuleConnected( 'analytics' ) );
 	const { slug } = moduleSlug;
+
+	const moduleConnected = useSelect( ( select ) => select( CORE_MODULES ).isModuleConnected( slug ) );
+	const ModuleIcon = useSelect( ( select ) => select( CORE_MODULES ).getModuleIcon( slug ) );
 	const shouldRenderWidget = isFeatureEnabled( 'widgets.moduleScreens' ) && screenWidgetContext;
+	const moduleStatusText = sprintf(
+		/* translators: %s: module name. */
+		__( '%s is connected', 'google-site-kit' ),
+		_x( 'AdSense', 'Service name', 'google-site-kit' )
+	);
+	const moduleStatus = moduleConnected ? 'connected' : 'not-connected';
 
 	return (
 		<div>
@@ -49,10 +63,28 @@ function ModuleApp( { moduleSlug } ) {
 			</Header>
 			<Alert module={ slug } />
 			{ shouldRenderWidget &&
-				<WidgetContextRenderer slug={ screenWidgetContext } />
+				<WidgetContextRenderer
+					slug={ screenWidgetContext }
+					className={ `googlesitekit-module-page googlesitekit-module-page--${ slug }` }
+					Header={ () => (
+						<PageHeader
+							title={ _x( 'AdSense', 'Service name', 'google-site-kit' ) }
+							icon={
+								<ModuleIcon
+									className="googlesitekit-page-header__icon"
+									height="33"
+									width="33"
+								/>
+							}
+							status={ moduleStatus }
+							statusText={ moduleStatusText }
+						/>
+					) }
+				/>
 			}
 			<div>screenWidgetContext: { screenWidgetContext && `${ screenWidgetContext }` }</div>
 			<div>{ isFeatureEnabled( 'widgets.moduleScreens' ) && 'enabled' }</div>
+			<ModuleFooter />
 		</div>
 	);
 }
