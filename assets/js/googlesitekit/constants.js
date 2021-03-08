a7b5180a0c50dfcf4e5a94c2816e9c911ba5f2ae
Evan Mattson <evan.mattson@10up.com>
2021-03-08 12:43:52 +0200

    Add constants for all screen contexts.

---
+++ assets/js/googlesitekit/constants.js
@@ -0,0 +1,27 @@
+/**
+ * Core constants.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const SCREEN_CONTEXT_ACTIVATION = 'activation';
+export const SCREEN_CONTEXT_ADMIN_BAR = 'adminBar';
+export const SCREEN_CONTEXT_DASHBOARD = 'dashboard';
+export const SCREEN_CONTEXT_PAGE_DASHBOARD = 'pageDashboard';
+export const SCREEN_CONTEXT_MODULE_DASHBOARD = 'moduleDashboard';
+export const SCREEN_CONTEXT_SETTINGS = 'settings';
+export const SCREEN_CONTEXT_SPLASH = 'splash';
+export const SCREEN_CONTEXT_USER_INPUT = 'userInput';
+export const SCREEN_CONTEXT_WP_DASHBOARD = 'wpDashboard';
