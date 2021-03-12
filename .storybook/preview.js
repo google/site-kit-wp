1b51ebf70cc9e05429f2efa7d891e45d617f7341
Ivan@Kruchkoff.com <ivan@kruchkoff.com>
2021-03-12 15:32:02 -0800

    Pull config from PR2273.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 /**
- * Storybook config.
+ * Storybook preview config.
  *
  * Site Kit by Google, Copyright 2021 Google LLC
  *
@@ -20,7 +20,7 @@
  * External dependencies
  */
 import cloneDeep from 'lodash/cloneDeep';
-import { addDecorator, configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
 
 /**
  * Internal dependencies
@@ -96,14 +96,6 @@ addDecorator( ( story ) => (
 		<div className="googlesitekit-plugin">{ story() }</div>
 	</div>
 ) );
-
-const req = require.context( '../stories', true, /\.stories\.js$/ );
-
-function loadStories() {
-	req.keys().forEach( ( filename ) => req( filename ) );
-}
-
-configure( loadStories, module );
 
 // TODO Would be nice if this wrote to a file. This logs our Storybook data to the browser console. Currently it gets put in .storybook/storybook-data and used in tests/backstop/scenarios.js.
 // eslint-disable-next-line no-console
