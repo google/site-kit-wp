ed572a57ab52266075734cbbccf574a036ab490e
John Phillips <johnbasraphillips@gmail.com>
2021-03-18 11:37:00 +0000

    Table renders nicely using fixture; added ModuleHeader component.

--- assets/js/modules/adsense/components/module/ModuleTopEarningPagesWidget.js
+++ assets/js/modules/adsense/components/module/ModuleTopEarningPagesWidget.js
@@ -19,7 +19,7 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _n, sprintf } from '@wordpress/i18n';
 
 /**
  * External dependencies
@@ -31,16 +31,19 @@ import PropTypes from 'prop-types';
  */
 import Data from 'googlesitekit-data';
 import { numFmt } from '../../../../util';
-import { getDataTableFromData } from '../../../../components/data-table';
+import ReportTable from '../../../../components/ReportTable';
 import Link from '../../../../components/Link';
 import PreviewTable from '../../../../components/PreviewTable';
 import TableOverflowContainer from '../../../../components/TableOverflowContainer';
-import AdSenseLinkCTA from '../../../analytics/components/common/AdSenseLinkCTA';
+// import AdSenseLinkCTA from '../../../analytics/components/common/AdSenseLinkCTA';
 import { STORE_NAME, DATE_RANGE_OFFSET } from '../../datastore/constants';
 import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
 import { MODULES_ANALYTICS } from '../../../analytics/datastore/constants';
 import { isZeroReport } from '../../../analytics/util/is-zero-report';
 import { isRestrictedMetricsError } from '../../../analytics/util/error';
+import { generateDateRangeArgs } from '../../../analytics/util/report-date-range-args';
+import { getCurrentDateRangeDayCount } from '../../../../util/date-range';
+import ModuleHeader from '../common/ModuleHeader';
 const { useSelect } = Data;
 
 const fixture = {
@@ -260,22 +263,22 @@ const fixture = {
 };
 
 function ModuleTopEarningPagesWidget( { Widget, WidgetReportZero, WidgetReportError } ) {
-	// Do not return zero data callout here since it will already be
-	// present on the page from other sources.
-	// if ( isDataZeroForReporting( data ) ) {
-	// 	return null;
-	// }
-
 	const {
-		isAdSenseLinked,
+	//	isAdSenseLinked,
 		data,
 		isLoading,
 		error,
-		serviceURL,
+		analyticsMainURL,
+		currentDayCount,
 	} = useSelect( ( select ) => {
-		const { startDate, endDate } = select( CORE_USER ).getDateRangeDates( {
+		const analyticsStore = select( MODULES_ANALYTICS );
+		const userStore = select( CORE_USER );
+
+		const dateRange = userStore.getDateRange();
+		const { startDate, endDate } = userStore.getDateRangeDates( {
 			offsetDays: DATE_RANGE_OFFSET,
 		} );
+
 		const reportArgs = {
 			startDate,
 			endDate,
@@ -292,21 +295,26 @@ function ModuleTopEarningPagesWidget( { Widget, WidgetReportZero, WidgetReportEr
 			limit: 10,
 		};
 
-		const href = '/';
-
 		return {
-			isAdSenseLinked: select( MODULES_ANALYTICS ).getAdsenseLinked(),
-			// data: select( MODULES_ANALYTICS ).getReport( reportArgs ),
+			isAdSenseLinked: analyticsStore.getAdsenseLinked(),
+			// data: analyticsStore.getReport( reportArgs ),
 			data: fixture.data,
-			error: select( MODULES_ANALYTICS ).getErrorForSelector( 'getReport', [ reportArgs ] ),
-			// isLoading: ! select( MODULES_ANALYTICS ).hasFinishedResolution( 'getReport', [ reportArgs ] ),
+			error: analyticsStore.getErrorForSelector( 'getReport', [ reportArgs ] ),
+			// isLoading: ! analyticsStore.hasFinishedResolution( 'getReport', [ reportArgs ] ),
 			isLoading: false,
-			// serviceURL: select( STORE_NAME ).getServiceReportURL( 'content-pages', {
-			// 	'explorer-table.plotKeys': '[]',
-			// 	'_r.drilldown': `analytics.pagePath:${ href }`,
-			// } ),
+			analyticsMainURL: analyticsStore.getServiceReportURL(
+				'content-publisher-overview',
+				generateDateRangeArgs( { startDate, endDate } )
+			),
+			currentDayCount: getCurrentDateRangeDayCount( dateRange ),
 		};
 	} );
+
+	// Do not return zero data callout here since it will already be
+	// present on the page from other sources.
+	// if ( isDataZeroForReporting( data ) ) {
+	// 	return null;
+	// }
 
 	// A restricted metrics error will cause this value to change in the resolver
 	// so this check should happen before an error, which is only relevant if they are linked.
@@ -325,73 +333,100 @@ function ModuleTopEarningPagesWidget( { Widget, WidgetReportZero, WidgetReportEr
 	if ( isZeroReport( data ) ) {
 		return <WidgetReportZero module="adsense" />;
 	}
+
 	const { rows } = data?.[ 0 ]?.data || {};
 
 	if ( ! Array.isArray( rows ) ) {
 		return null;
 	}
 
-	const headers = [
-		{
-			title: __( 'Page Title', 'google-site-kit' ),
-			tooltip: __( 'Page Title', 'google-site-kit' ),
-			primary: true,
-		},
-		{
-			title: __( 'Earnings', 'google-site-kit' ),
-			tooltip: __( 'Earnings', 'google-site-kit' ),
-		},
-		{
-			title: __( 'Page RPM', 'google-site-kit' ),
-			tooltip: __( 'Page RPM', 'google-site-kit' ),
-		},
-		{
-			title: __( 'Impressions', 'google-site-kit' ),
-			tooltip: __( 'Impressions', 'google-site-kit' ),
-		},
-	];
-
-	const dataMapped = rows.map( ( row ) => {
-		/**
-		 * The shape of the dimensions and metrics objects:
-		 *
-		 * ```
-		 * dimensions[0] = ga:pageTitle
-		 * dimensions[1] = ga:pagePath
-		 *
-		 * metrics[0] = ga:adsenseECPM
-		 * metrics[1] = ga:adsensePageImpressions
-		 * metrics[2] = ga:adsenseRevenue
-		 * ```
-		 */
-		return [
-			row.dimensions[ 0 ],
-			Number( row.metrics[ 0 ].values[ 0 ] ).toFixed( 2 ),
-			Number( row.metrics[ 0 ].values[ 1 ] ).toFixed( 2 ),
-			numFmt( row.metrics[ 0 ].values[ 2 ], { style: 'decimal' } ),
-		];
-	} );
-
-	const options = {
-		hideHeader: false,
-		chartsEnabled: false,
-		links: rows.map( ( row ) => row.dimensions[ 1 ] || '/' ),
-		PrimaryLink: () => <Link href="www.google.com" external />,
-	};
-
-	const dataTable = getDataTableFromData( dataMapped, headers, options );
-
 	return (
-		<TableOverflowContainer>
-			{ dataTable }
-		</TableOverflowContainer>
+		<Widget
+			noPadding
+			Header={ () => (
+				<ModuleHeader
+					title={
+						sprintf(
+							/* translators: %s: number of days */
+							_n( 'Performance by page over the last %s day', 'Performance by page over the last %s days', currentDayCount, 'google-site-kit', ),
+							currentDayCount,
+						)
+					}
+					ctaLink={ analyticsMainURL }
+					ctaLabel="See full stats on Analytics"
+				/>
+			) }
+		>
+			<TableOverflowContainer>
+				<ReportTable
+					rows={ data[ 0 ].data.rows }
+					columns={ tableColumns }
+				/>
+			</TableOverflowContainer>
+		</Widget>
 	);
 }
 
+const tableColumns = [
+	{
+		title: __( 'Page Title', 'google-site-kit' ),
+		description: __( 'Page Title', 'google-site-kit' ),
+		primary: true,
+		Component: ( { row } ) => {
+			const [ title, url ] = row.dimensions;
+			const serviceURL = useSelect( ( select ) => select( STORE_NAME ).getServiceReportURL( 'content-pages', {
+				'explorer-table.plotKeys': '[]',
+				'_r.drilldown': `analytics.pagePath:${ url }`,
+			} ) );
+			return (
+				<Link
+					href={ serviceURL }
+					external
+					inherit
+				>
+					{ title }
+				</Link>
+			);
+		},
+	},
+	{
+		title: __( 'Earnings', 'google-site-kit' ),
+		description: __( 'Earnings', 'google-site-kit' ),
+		field: 'metrics.0.values.0',
+		Component: ( { fieldValue } ) => numFmt(
+			fieldValue,
+			{
+				style: 'decimal',
+				minimumFractionDigits: 2,
+				maximumFractionDigits: 2,
+			}
+		),
+	},
+	{
+		title: __( 'Page RPM', 'google-site-kit' ),
+		description: __( 'Page RPM', 'google-site-kit' ),
+		field: 'metrics.0.values.1',
+		Component: ( { fieldValue } ) => numFmt(
+			fieldValue,
+			{
+				style: 'decimal',
+				minimumFractionDigits: 2,
+				maximumFractionDigits: 2,
+			}
+		),
+	},
+	{
+		title: __( 'Impressions', 'google-site-kit' ),
+		description: __( 'Impressions', 'google-site-kit' ),
+		field: 'metrics.0.values.2',
+		Component: ( { fieldValue } ) => numFmt( fieldValue, { style: 'decimal' } ),
+	},
+];
+
 ModuleTopEarningPagesWidget.propTypes = {
-	// Widget: PropTypes.element.isRequired,
-	// WidgetReportZero: PropTypes.element.isRequired,
-	// WidgetReportError: PropTypes.element.isRequired,
+	Widget: PropTypes.func.isRequired,
+	WidgetReportZero: PropTypes.func.isRequired,
+	WidgetReportError: PropTypes.func.isRequired,
 };
 
 export default ModuleTopEarningPagesWidget;
