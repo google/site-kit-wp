f8bdf7660d77648bc3d2f4e4cf7d920d9839392f
Chris <cfa@google.com>
2021-03-30 22:56:02 +0000

    Add frontend JS entry script.

--- assets/js/modules/subscribe-with-google/frontend/single-posts.js
+++ assets/js/modules/subscribe-with-google/frontend/single-posts.js
@@ -1,2 +1,16 @@
-// eslint-disable-next-line no-console
-console.log( 'Hi!' );
+import { handleSubscribeClicks, handleContributeClicks } from './buttons';
+import { handlePaymentResponse } from './payments';
+import { unlockPageMaybe } from './unlock';
+
+// Wait for SwG API to become available.
+( self.SWG = self.SWG || [] ).push( async ( swg ) => {
+	// Handle payment response.
+	handlePaymentResponse( swg );
+
+	// Handle button clicks.
+	handleSubscribeClicks( swg );
+	handleContributeClicks( swg );
+
+	// Unlock page if possible.
+	return unlockPageMaybe( swg );
+} );
