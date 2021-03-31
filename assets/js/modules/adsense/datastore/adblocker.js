873d8d61d0f87f59ee792caf59b80b64f753a09b
Evan Mattson <evan.mattson@10up.com>
2021-03-31 16:29:41 +0300

    Remove use of addQueryArgs.

--- assets/js/modules/adsense/datastore/adblocker.js
+++ assets/js/modules/adsense/datastore/adblocker.js
@@ -23,11 +23,6 @@ import invariant from 'invariant';
 import { detectAnyAdblocker } from 'just-detect-adblock';
 
 /**
- * WordPress dependencies
- */
-import { addQueryArgs } from '@wordpress/url';
-
-/**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
@@ -67,17 +62,16 @@ export const controls = {
 		// additional stuff in the query string to (hopefully) trigger a filter.
 		// If this throws, then the fetch request failed completely and we'll assume it was blocked.
 		try {
+			const params = [
+				// The name of the parameter here doesn't really matter
+				// since adblockers look at the URL as a whole.
+				// Note: this value must not be URL-encoded.
+				'google-site-kit=/adsense/pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+				// Add a timestamp for cache-busting.
+				`timestamp=${ Date.now() }`,
+			];
 			await fetch(
-				addQueryArgs(
-					'/favicon.ico',
-					{
-						// The name of the parameter here doesn't really matter
-						// since adblockers look at the URL as a whole.
-						'google-site-kit': '/adsense/pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-						// Add a timestamp for cache-busting.
-						timestamp: Date.now(),
-					}
-				),
+				`/favicon.ico?${ params.join( '&' ) }`,
 				{
 					credentials: 'omit',
 					// Don't follow any redirects; we only care about this request being blocked or not.
