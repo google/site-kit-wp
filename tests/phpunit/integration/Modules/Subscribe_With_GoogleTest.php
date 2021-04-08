3fe691397813501596515f07c7576cb45feddb61
Chris <cfa@google.com>
2021-04-08 21:31:23 +0000

    Remove extra tests.

--- tests/phpunit/integration/Modules/Subscribe_With_GoogleTest.php
+++ tests/phpunit/integration/Modules/Subscribe_With_GoogleTest.php
@@ -11,22 +11,15 @@
 namespace Google\Site_Kit\Tests\Modules;
 
 use Google\Site_Kit\Context;
-use Google\Site_Kit\Core\Modules\Module_With_Owner;
-use Google\Site_Kit\Core\Modules\Module_With_Scopes;
 use Google\Site_Kit\Core\Storage\Options;
-use Google\Site_Kit\Modules\Analytics\Settings as AnalyticsSettings;
 use Google\Site_Kit\Modules\Subscribe_With_Google;
 use Google\Site_Kit\Modules\Subscribe_With_Google\Settings;
-use Google\Site_Kit\Tests\Core\Modules\Module_With_Owner_ContractTests;
-use Google\Site_Kit\Tests\Core\Modules\Module_With_Scopes_ContractTests;
 use Google\Site_Kit\Tests\TestCase;
 
 /**
  * @group Modules
  */
 class Subscribe_With_GoogleTest extends TestCase {
-	use Module_With_Scopes_ContractTests;
-	use Module_With_Owner_ContractTests;
 
 	public function test_register() {
 		$subscribewithgoogle = new Subscribe_With_Google( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
@@ -60,19 +53,5 @@ class Subscribe_With_GoogleTest extends TestCase {
 		$subscribewithgoogle->on_deactivation();
 
 		$this->assertOptionNotExists( Settings::OPTION );
-	}
-
-	/**
-	 * @return Module_With_Scopes
-	 */
-	protected function get_module_with_scopes() {
-		return new Subscribe_With_Google( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
-	}
-
-	/**
-	 * @return Module_With_Owner
-	 */
-	protected function get_module_with_owner() {
-		return new Subscribe_With_Google( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
 	}
 }
