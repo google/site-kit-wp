8f808390220bc3a29851a5db1857bdfb90ff7bb5
Hazlitt Eastman <hazlitt@manta9.com>
2021-04-13 09:26:06 +0100

    Progress update.

---
+++ assets/js/components/Button.stories.js
@@ -0,0 +1,58 @@
+/**
+ * Button Component Stories.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+//import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Button from './Button';
+
+export default {
+	title: 'Components/Button',
+	component: Button,
+	parameters: {
+		hoverSelector: '.googlesitekit-button--hover',
+		postInteractionWait: 3000, // Wait for shadows to animate.
+		onReadyScript: 'mouse.js',
+	},
+};
+
+export const DefaultButton = () => <Button>Default Button</Button>;
+
+DefaultButton.storyName = 'Default Button';
+
+export const HoverButton = () => <Button className="googlesitekit-button--hover">VRT: Default Button Hover</Button>;
+
+HoverButton.storyName = 'Default Button Hover';
+
+export const LinkButton = () => <Button href="http://google.com">Default Button Link</Button>;
+
+LinkButton.storyName = 'Default Button Link';
+
+export const DangerButton = () => <Button href="http://google.com" danger>Danger Button</Button>;
+
+DangerButton.storyName = 'Danger Button';
+
+export const DisabledButton = () => <Button disabled>Disabled Button</Button>;
+
+DisabledButton.storyName = 'Diasbled Button';
