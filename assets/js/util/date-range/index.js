dcd268743ccfd4d863155bdef60aebede4f430d6
Daniel Gent <daniel.gent@get10up.com>
2021-04-21 14:10:48 +0200

    Move date utils into shared folder.

--- assets/js/util/date-range/index.js
+++ assets/js/util/date-range/index.js
@@ -77,3 +77,14 @@ export function getAvailableDateRanges() {
 		},
 	};
 }
+export {
+	INVALID_DATE_INSTANCE_ERROR,
+	INVALID_DATE_RANGE_ERROR,
+	INVALID_DATE_STRING_ERROR,
+} from './constants';
+export { getDateString } from './get-date-string';
+export { getPreviousDate } from './get-previous-date';
+export { getPreviousWeekDate } from './get-previous-week-date';
+export { isValidDateInstance } from './is-valid-date-instance';
+export { isValidDateRange } from './is-valid-date-range';
+export { isValidDateString } from './is-valid-date-string';
