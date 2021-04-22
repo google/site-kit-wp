57f5b3b88c096ba1754b1689b6353328f9b9b9b2
Asvin Balloo <asvin.balloo@gmail.com>
2021-04-22 13:37:46 +0400

    Do not cache array length.

--- assets/js/components/user-input/UserInputKeywords.js
+++ assets/js/components/user-input/UserInputKeywords.js
@@ -49,8 +49,7 @@ export default function UserInputKeywords( { slug, max, next, isActive } ) {
 	const { setUserInputSetting } = useDispatch( CORE_USER );
 
 	// Add an empty string if the values array is empty.
-	const valuesLength = values.length;
-	if ( valuesLength === 0 || valuesLength < max ) {
+	if ( values.length === 0 || values.length < max ) {
 		values.push( '' );
 	}
 
