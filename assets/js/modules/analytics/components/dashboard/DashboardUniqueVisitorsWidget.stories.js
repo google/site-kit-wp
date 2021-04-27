4291fc264db9d88913c4270578d250b1cb25e9c1
Asvin Balloo <asvin.balloo@gmail.com>
2021-04-27 20:03:13 +0400

    Remove extra closing tag.

--- assets/js/modules/analytics/components/dashboard/DashboardUniqueVisitorsWidget.stories.js
+++ assets/js/modules/analytics/components/dashboard/DashboardUniqueVisitorsWidget.stories.js
@@ -69,8 +69,7 @@ const optionsCompareEntityURL = {
 const WidgetAreaTemplate = ( args ) => {
 	return (
 		<WithRegistrySetup func={ args?.setupRegistry }>
-			<WidgetAreaRenderer slug={ areaName }>
-			</WidgetAreaRenderer>
+			<WidgetAreaRenderer slug={ areaName } />
 		</WithRegistrySetup>
 	);
 };
