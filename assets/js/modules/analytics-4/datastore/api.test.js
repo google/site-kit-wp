b0796461f5a5b912d5d447057bdf3f5dd5d37e77
Daniel Gent <daniel.gent@get10up.com>
2021-04-27 18:25:44 +0200

    Implement selector partially + tests.

--- assets/js/modules/analytics-4/datastore/api.test.js
+++ assets/js/modules/analytics-4/datastore/api.test.js
@@ -46,10 +46,61 @@ describe( 'modules/analytics-4 properties', () => {
 
 	describe( 'selectors', () => {
 		describe( 'isAdminAPIWorking', () => {
-			it( 'should do', async () => {
+			// TODO - better names for all tests
+			it( 'should return false TODO AS PER SPEC', async () => {
 				const isAdminAPIWorking = registry.select( STORE_NAME ).isAdminAPIWorking( );
 
 				expect( isAdminAPIWorking ).toBe( false );
+			} );
+
+			it( 'should return true if at least one property with a non-empty array of properties', async () => {
+				// NOTE - selector does not work for receiveGetProperty. assuming that IB knows this!
+				registry.dispatch( STORE_NAME ).receiveGetProperties( [
+					{
+						_id: '1000',
+						_accountID: '100',
+						name: 'properties/1000',
+						createTime: '2014-10-02T15:01:23Z',
+						updateTime: '2014-10-02T15:01:23Z',
+						parent: 'accounts/100',
+						displayName: 'Test GA4 Property',
+						industryCategory: 'TECHNOLOGY',
+						timeZone: 'America/Los_Angeles',
+						currencyCode: 'USD',
+						deleted: false,
+					},
+				],
+				{ accountID: 'foo-bar' },
+				);
+
+				const isAdminAPIWorking = registry.select( STORE_NAME ).isAdminAPIWorking( );
+
+				expect( isAdminAPIWorking ).toBe( true );
+			} );
+
+			it( 'should return true if has at least one property with a non-empty array of web datastreams', async () => {
+				registry.dispatch( STORE_NAME ).receiveGetWebDataStreams(
+					[
+						{
+							_id: '2000',
+							_propertyID: '1000',
+							name: 'properties/1000/webDataStreams/2000',
+							// eslint-disable-next-line sitekit/acronym-case
+							measurementId: '1A2BCD345E',
+							// eslint-disable-next-line sitekit/acronym-case
+							firebaseAppId: '',
+							createTime: '2014-10-02T15:01:23Z',
+							updateTime: '2014-10-02T15:01:23Z',
+							defaultUri: 'http://example.com',
+							displayName: 'Test GA4 WebDataStream',
+						},
+					],
+					{ propertyID: 'foo-bar' }
+				);
+
+				const isAdminAPIWorking = registry.select( STORE_NAME ).isAdminAPIWorking( );
+
+				expect( isAdminAPIWorking ).toBe( true );
 			} );
 		} );
 	} );
