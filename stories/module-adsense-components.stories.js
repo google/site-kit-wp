682c403705679e714a7e55399bf8bc488c53c657
John Phillips <johnbasraphillips@gmail.com>
2021-04-28 10:19:36 +0100

    Clarify report and add comment in Storybook.

--- stories/module-adsense-components.stories.js
+++ stories/module-adsense-components.stories.js
@@ -189,27 +189,24 @@ const topEarningPagesArgs = {
 	limit: 5,
 };
 
-const topEarningPagesAdSenseReport = {
+// These components make a simple AdSense report query to determine the
+// currency that should be displayed in the report table.
+const getCurrencyFromReport = {
 	kind: 'adsense#report',
 	totalMatchedRows: '1',
 	headers: [
 		{ name: 'EARNINGS', type: 'METRIC_CURRENCY', currency: 'USD' },
-		{ name: 'PAGE_VIEWS_RPM', type: 'METRIC_CURRENCY', currency: 'USD' },
-		{ name: 'IMPRESSIONS', type: 'METRIC_TALLY' },
 	],
 	rows: [
-		[ '2103.23', '1.89', '5693093' ],
+		[ '680.67' ],
 	],
-	totals: [
-		'2103.23', '1.89', '5693093' ],
-	averages: [
-		'2103.23', null, '5693093',
-	],
+	totals: [ '680.67' ],
+	averages: [ '680.67' ],
 	startDate: '2020-08-15',
 	endDate: '2020-09-11',
 };
 
-const adsenseReportOptions = {
+const getCurrencyFromReportOptions = {
 	startDate: '2020-08-15',
 	endDate: '2020-09-11',
 	metrics: 'EARNINGS',
@@ -225,8 +222,8 @@ generateReportBasedWidgetStories( {
 	setup: ( { dispatch }, variantName ) => {
 		dispatch( MODULES_ANALYTICS ).setAdsenseLinked( variantName !== 'AdSense Not Linked' );
 		dispatch( STORE_NAME ).receiveIsAdBlockerActive( variantName === 'Ad Blocker Active' );
-		dispatch( STORE_NAME ).receiveGetReport( topEarningPagesAdSenseReport, { options: adsenseReportOptions } );
-		dispatch( STORE_NAME ).finishResolution( 'getReport', [ adsenseReportOptions ] );
+		dispatch( STORE_NAME ).receiveGetReport( getCurrencyFromReport, { options: getCurrencyFromReportOptions } );
+		dispatch( STORE_NAME ).finishResolution( 'getReport', [ getCurrencyFromReportOptions ] );
 	},
 	Component: DashboardTopEarningPagesWidget,
 	wrapWidget: false,
@@ -252,8 +249,8 @@ generateReportBasedWidgetStories( {
 	datastore: MODULES_ANALYTICS,
 	setup: ( { dispatch }, variantName ) => {
 		dispatch( MODULES_ANALYTICS ).setAdsenseLinked( variantName !== 'AdSense Not Linked' );
-		dispatch( STORE_NAME ).receiveGetReport( topEarningPagesAdSenseReport, { options: adsenseReportOptions } );
-		dispatch( STORE_NAME ).finishResolution( 'getReport', [ adsenseReportOptions ] );
+		dispatch( STORE_NAME ).receiveGetReport( getCurrencyFromReport, { options: getCurrencyFromReportOptions } );
+		dispatch( STORE_NAME ).finishResolution( 'getReport', [ getCurrencyFromReportOptions ] );
 	},
 	group: 'AdSense Module/Components/Module/Top Earning Pages Widget',
 	referenceDate: '2020-09-12',
