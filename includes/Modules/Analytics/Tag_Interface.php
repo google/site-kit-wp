cebed500419934ebf77b07809047558430d5fac6
Evan Mattson <evan.mattson@10up.com>
2021-04-29 22:28:57 +0300

    Add Analytics tag interface.

---
+++ includes/Modules/Analytics/Tag_Interface.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Class Google\Site_Kit\Modules\Analytics\Tag_Interface
+ *
+ * @package   Google\Site_Kit\Modules\Analytics
+ * @copyright 2021 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Modules\Analytics;
+
+/**
+ * Interface for an Analytics tag.
+ *
+ * @since n.e.x.t
+ * @access private
+ * @ignore
+ */
+interface Tag_Interface {
+	/**
+	 * Sets the current home domain.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $domain Domain name.
+	 */
+	public function set_home_domain( $domain );
+
+	/**
+	 * Sets whether or not to anonymize IP addresses.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param bool $anonymize_ip Anonymize or not.
+	 */
+	public function set_anonymize_ip( $anonymize_ip );
+
+	/**
+	 * Sets the ads conversion ID.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $ads_id Ads ID.
+	 */
+	public function set_ads_id( $ads_id );
+}
