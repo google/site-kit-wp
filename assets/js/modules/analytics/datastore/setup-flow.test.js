04a01fadb0d5607b7444607a96c0d3bc1f4137b6
Daniel Gent <daniel.gent@get10up.com>
2021-04-29 16:20:46 +0200

    Create test boilerplate.

---
+++ assets/js/modules/analytics/datastore/setup-flow.test.js
@@ -0,0 +1,51 @@
+/**
+ * `modules/analytics` data store: setup-flow tests.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import API from 'googlesitekit-api';
+import { STORE_NAME } from './constants';
+import { createTestRegistry, unsubscribeFromAll } from 'tests/js/utils';
+
+describe( 'modules/analytics properties', () => {
+	let registry;
+
+	beforeAll( () => {
+		API.setUsingCache( false );
+	} );
+
+	beforeEach( () => {
+		registry = createTestRegistry();
+		// Receive empty settings to prevent unexpected fetch by resolver.
+		registry.dispatch( STORE_NAME ).receiveGetSettings( {} );
+	} );
+
+	afterAll( () => {
+		API.setUsingCache( true );
+	} );
+
+	afterEach( () => {
+		unsubscribeFromAll( registry );
+	} );
+
+	it( 'should be true', () => {
+		expect( true ).toBe( true );
+	} );
+} );
+
