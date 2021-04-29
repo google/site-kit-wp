913bc061e2d114fbcfc511d4b25c5b045dbbd7c7
Daniel Gent <daniel.gent@get10up.com>
2021-04-29 16:41:06 +0200

    Add first test.

--- assets/js/modules/analytics/datastore/setup-flow.test.js
+++ assets/js/modules/analytics/datastore/setup-flow.test.js
@@ -22,6 +22,10 @@
 import API from 'googlesitekit-api';
 import { STORE_NAME } from './constants';
 import { createTestRegistry, unsubscribeFromAll } from 'tests/js/utils';
+import * as modulesAnalytics from '../';
+// import * as fixtures from './__fixtures__';
+// TODO - should be in own section? check other test copied from
+import { createRegistry } from '@wordpress/data';
 
 describe( 'modules/analytics properties', () => {
 	let registry;
@@ -44,8 +48,35 @@ describe( 'modules/analytics properties', () => {
 		unsubscribeFromAll( registry );
 	} );
 
+	// TODO - in describe block like other tests for selector getSetupFlowMode
+
 	it( 'should be true', () => {
 		expect( true ).toBe( true );
 	} );
+
+	it( 'returns “legacy” if the modules/analytics-4 store is not available ', async () => {
+		// only register this module
+		const newRegistry = createRegistry();
+
+		[
+			modulesAnalytics,
+		].forEach( ( { registerStore } ) => registerStore?.( newRegistry ) );
+
+		registry = newRegistry;
+		// Receive empty settings to prevent unexpected fetch by resolver.
+		registry.dispatch( STORE_NAME ).receiveGetSettings( {} );
+
+		expect( registry.select( STORE_NAME ).getSetupFlowMode() ).toBe( 'legacy' );
+	} );
+
+	// If the modules/analytics-4 store selector isAdminAPIWorking() (see #3169) returns false, it should return “legacy”. If it returns undefined, it should also return undefined (indicating "not loaded yet").
+
+	// If there is no account selected, it should return “ua”.
+
+	// If there is an account selected for which the modules/analytics-4 store selector getProperties returns an empty array (i.e. no GA4 properties), it should return “ua”.
+
+	// If there is an account selected for which the modules/analytics store selector getProperties returns an empty array (i.e. no UA properties), it should return “ga4”.
+
+	// If there is an account selected for which both the modules/analytics and modules/analytics-4 selectors getProperties return a non-empty array, it should return “ga4-transitional”.
 } );
 
