bf7151f7a6712945b7c5aee5a517bad04cfe3f96
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2021-04-29 15:59:16 +0100

    Add VRTs for widget-based charts.

--- .storybook/storybook-data.js
+++ .storybook/storybook-data.js
@@ -196,6 +196,20 @@ module.exports = [
 		},
 	},
 	{
+		id: 'adsense-module-components-module-overview-widget--loaded',
+		kind: 'AdSense Module',
+		name: 'Overview Widget',
+		story: 'Loaded',
+		parameters: {
+			fileName: './stories/module-adsense-components.stories.js',
+			options: {
+				hierarchyRootSeparator: '|',
+				hierarchySeparator: {},
+				readySelector: '.googlesitekit-chart .googlesitekit-chart__inner',
+			},
+		},
+	},
+	{
 		id: 'analytics-module--audience-overview-chart',
 		kind: 'Analytics Module',
 		name: 'Audience Overview Chart',
@@ -210,12 +224,40 @@ module.exports = [
 		},
 	},
 	{
+		id: 'analytics-module-components-module-page-overview-widget--loaded',
+		kind: 'Analytics Module',
+		name: 'Overview Widget',
+		story: 'Loaded',
+		parameters: {
+			fileName: './stories/module-analytics-components.stories.js',
+			options: {
+				hierarchyRootSeparator: '|',
+				hierarchySeparator: {},
+				readySelector: '.googlesitekit-chart .googlesitekit-chart__inner',
+			},
+		},
+	},
+	{
 		id: 'analytics-module--top-acquisition-pie-chart',
 		kind: 'Analytics Module',
 		name: 'Top Acquisition Pie Chart',
 		story: 'Top Acquisition Pie Chart',
 		parameters: {
 			fileName: './stories/module-analytics.stories.js',
+			options: {
+				hierarchyRootSeparator: '|',
+				hierarchySeparator: {},
+				readySelector: '.googlesitekit-chart .googlesitekit-chart__inner',
+			},
+		},
+	},
+	{
+		id: 'search-console-module-components-module-page-overview-widget--loaded',
+		kind: 'Search Console Module',
+		name: 'Overview Widget',
+		story: 'Loaded',
+		parameters: {
+			fileName: './stories/module-search-console-components.stories.js',
 			options: {
 				hierarchyRootSeparator: '|',
 				hierarchySeparator: {},
