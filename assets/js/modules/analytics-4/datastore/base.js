abfca088bda72771e979690175bff7d2f868ff7f
Evan Mattson <evan.mattson@10up.com>
2021-04-30 09:25:48 +0300

    Convert adsConversionID setting for GA4 to placeholder.

--- assets/js/modules/analytics-4/datastore/base.js
+++ assets/js/modules/analytics-4/datastore/base.js
@@ -25,9 +25,9 @@ import { STORE_NAME } from './constants';
 const baseModuleStore = Modules.createModuleStore( 'analytics-4', {
 	storeName: STORE_NAME,
 	settingSlugs: [
-		// TODO: This can be uncommented when Analytics and Analytics 4 modules are officially separated.
+		// TODO: These can be uncommented when Analytics and Analytics 4 modules are officially separated.
 		// 'accountID',
-		'adsConversionID',
+		// 'adsConversionID',
 		'propertyID',
 		'webDataStreamID',
 		'measurementID',
