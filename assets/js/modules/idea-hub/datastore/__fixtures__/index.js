933aaa0261e30d6eae87f31a324e2390423661cf
Ivan@Kruchkoff.com <ivan@kruchkoff.com>
2021-05-03 11:06:16 -0700

    Add saved ideas.

--- assets/js/modules/idea-hub/datastore/__fixtures__/index.js
+++ assets/js/modules/idea-hub/datastore/__fixtures__/index.js
@@ -1,5 +1,5 @@
 /**
- * `modules/idea-hub` data store
+ * Idea Hub Datastore Fixtures.
  *
  * Site Kit by Google, Copyright 2021 Google LLC
  *
@@ -19,25 +19,8 @@
 /**
  * Internal dependencies
  */
-import Data from 'googlesitekit-data';
-import { STORE_NAME } from './constants';
-import baseModuleStore from './base';
-import savedIdeas from './saved-ideas';
+import { default as savedIdeas } from './saved-ideas';
 
-const store = Data.combineStores(
-	baseModuleStore,
+export {
 	savedIdeas,
-);
-
-export const initialState = store.initialState;
-export const actions = store.actions;
-export const controls = store.controls;
-export const reducer = store.reducer;
-export const resolvers = store.resolvers;
-export const selectors = store.selectors;
-
-export const registerStore = ( registry ) => {
-	registry.registerStore( STORE_NAME, store );
 };
-
-export default store;
