b7cc5cb9b71e00ebe37d2b06f4bc10b525204c52
Eugene Manuilov <eugene.manuilov@gmail.com>
2021-05-10 13:36:57 +0300

    Add validation function.

---
+++ assets/js/modules/analytics-4/datastore/settings.js
@@ -0,0 +1,59 @@
+/**
+ * `modules/analytics-4` data store: settings.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import invariant from 'invariant';
+
+/**
+ * Internal dependencies
+ */
+import { createStrictSelect } from '../../../googlesitekit/data/utils';
+import { isValidPropertySelection, isValidWebDataStreamID } from '../utils/validation';
+import { INVARIANT_DOING_SUBMIT_CHANGES, INVARIANT_SETTINGS_NOT_CHANGED } from '../../../googlesitekit/data/create-settings-store';
+import { STORE_NAME } from './constants';
+
+// Invariant error messages.
+export const INVARIANT_INVALID_PROPERTY_SELECTION = 'a valid propertyID is required to submit changes';
+export const INVARIANT_INVALID_WEBDATASTREAM_ID = 'a valid webDataStreamID is required to submit changes';
+
+export async function submitChanges( {} ) {
+
+}
+
+export function validateCanSubmitChanges( select ) {
+	if ( ! select( STORE_NAME ).isAdminAPIWorking() ) {
+		return;
+	}
+
+	const strictSelect = createStrictSelect( select );
+	const {
+		haveSettingsChanged,
+		isDoingSubmitChanges,
+		getPropertyID,
+		getWebDataStreamID,
+	} = strictSelect( STORE_NAME );
+
+	// Note: these error messages are referenced in test assertions.
+	invariant( haveSettingsChanged(), INVARIANT_SETTINGS_NOT_CHANGED );
+	invariant( ! isDoingSubmitChanges(), INVARIANT_DOING_SUBMIT_CHANGES );
+
+	invariant( isValidPropertySelection( getPropertyID() ), INVARIANT_INVALID_PROPERTY_SELECTION );
+	invariant( isValidWebDataStreamID( getWebDataStreamID() ), INVARIANT_INVALID_WEBDATASTREAM_ID );
+}
