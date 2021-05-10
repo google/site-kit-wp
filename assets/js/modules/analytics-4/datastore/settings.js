65130622cfae2f9ab514b430b36b1527b4fa747e
Eugene Manuilov <eugene.manuilov@gmail.com>
2021-05-10 14:07:28 +0300

    Implement submitChanges function.

--- assets/js/modules/analytics-4/datastore/settings.js
+++ assets/js/modules/analytics-4/datastore/settings.js
@@ -24,17 +24,54 @@ import invariant from 'invariant';
 /**
  * Internal dependencies
  */
+import API from 'googlesitekit-api';
 import { createStrictSelect } from '../../../googlesitekit/data/utils';
 import { isValidPropertySelection, isValidWebDataStreamID } from '../utils/validation';
+import { invalidateCacheGroup } from '../../../components/data/invalidate-cache-group';
 import { INVARIANT_DOING_SUBMIT_CHANGES, INVARIANT_SETTINGS_NOT_CHANGED } from '../../../googlesitekit/data/create-settings-store';
-import { STORE_NAME } from './constants';
+import { MODULES_ANALYTICS } from '../../analytics/datastore/constants';
+import { STORE_NAME, PROPERTY_CREATE } from './constants';
+import { TYPE_MODULES } from '../../../components/data';
 
 // Invariant error messages.
 export const INVARIANT_INVALID_PROPERTY_SELECTION = 'a valid propertyID is required to submit changes';
 export const INVARIANT_INVALID_WEBDATASTREAM_ID = 'a valid webDataStreamID is required to submit changes';
 
-export async function submitChanges( {} ) {
+export async function submitChanges( { select, dispatch } ) {
+	let propertyID = select( STORE_NAME ).getPropertyID();
+	if ( propertyID === PROPERTY_CREATE ) {
+		const accountID = select( MODULES_ANALYTICS ).getAccountID();
+		const { response: property, error } = await dispatch( STORE_NAME ).createProperty( accountID );
+		if ( error ) {
+			return { error };
+		}
 
+		propertyID = property._id;
+		await dispatch( STORE_NAME ).setPropertyID( propertyID );
+	}
+
+	const webDataStreamID = select( STORE_NAME ).getWebDataStreamID();
+	if ( ! isValidWebDataStreamID( webDataStreamID ) ) {
+		const { response: webdatastream, error } = await dispatch( STORE_NAME ).createWebDataStream( webDataStreamID );
+		if ( error ) {
+			return { error };
+		}
+
+		await dispatch( STORE_NAME ).setWebDataStreamID( webdatastream._id );
+	}
+
+	if ( select( STORE_NAME ).haveSettingsChanged() ) {
+		const { error } = await dispatch( STORE_NAME ).saveSettings();
+		if ( error ) {
+			return { error };
+		}
+	}
+
+	await API.invalidateCache( 'modules', 'analytics-4' );
+	// TODO: Remove once legacy dataAPI is no longer used.
+	invalidateCacheGroup( TYPE_MODULES, 'analytics-4' );
+
+	return {};
 }
 
 export function validateCanSubmitChanges( select ) {
