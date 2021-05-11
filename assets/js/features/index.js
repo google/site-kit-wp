74da923b26dac173da7588c0d3e5950acd0908d8
Evan Mattson <evan.mattson@10up.com>
2021-05-11 13:15:56 +0300

    Update enabledFeatures to use a Set.
    This also fixes a bug where enabled features could be mutated via the global value as it was not dereferenced before.

--- assets/js/features/index.js
+++ assets/js/features/index.js
@@ -16,17 +16,17 @@
  * limitations under the License.
  */
 
-export const enabledFeatures = global?._googlesitekitBaseData?.enabledFeatures || [];
+export const enabledFeatures = new Set( global?._googlesitekitBaseData?.enabledFeatures || [] );
 
 /**
  * Returns true if a feature is enabled; false otherwise.
  *
  * @since 1.25.0
  *
- * @param {string} feature          The name of the feature to check.
- * @param {?Array} _enabledFeatures Optional. The list of enabled features. Uses `enabledFeatures` set by the server in a global JS variable, by default.
+ * @param {string} feature            The name of the feature to check.
+ * @param {Set}    [_enabledFeatures] Optional. The set of enabled features. Uses `enabledFeatures` set by the server in a global JS variable, by default.
  * @return {boolean} `true` if a feature is enabled; `false` otherwise.
  */
 export const isFeatureEnabled = ( feature, _enabledFeatures = enabledFeatures ) => {
-	return _enabledFeatures.includes( feature );
+	return _enabledFeatures.has( feature );
 };
