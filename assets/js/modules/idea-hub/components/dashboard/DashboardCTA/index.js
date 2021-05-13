ebf9e8d432f94d938f0fa535dd42f44f1aae32ff
John Phillips <johnbasraphillips@gmail.com>
2021-05-13 16:58:11 +0100

    Add logic to component.

--- assets/js/modules/idea-hub/components/dashboard/DashboardCTA/index.js
+++ assets/js/modules/idea-hub/components/dashboard/DashboardCTA/index.js
@@ -19,24 +19,46 @@
 /**
  * WordPress dependencies
  */
-// import { useCallback } from '@wordpress/element';
+import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-// import Data from 'googlesitekit-data';
-// import { CORE_MODULES } from '../../../../../googlesitekit/modules/datastore/constants';
+import Data from 'googlesitekit-data';
+import {
+	showErrorNotification,
+} from '../../../../../util';
+import GenericError from '../../../../../components/legacy-notifications/generic-error';
+import { CORE_MODULES } from '../../../../../googlesitekit/modules/datastore/constants';
+import { CORE_LOCATION } from '../../../../../googlesitekit/datastore/location/constants';
 import Button from '../../../../../components/Button';
 import Link from '../../../../../components/Link';
 import IdeaHubIcon from '../../../../../../svg/idea-hub.svg';
-// const { useSelect, useDispatch } = Data;
+const { useSelect, useDispatch } = Data;
+
+const MODULE_SLUG = 'idea-hub';
 
 function DashboardCTA() {
-	// const { connected, active } = useSelect( ( select ) => select( CORE_MODULES ).getModule( 'idea-hub' ) );
-	// const { activateModule } = useDispatch( CORE_MODULES );
+	const { connected, active } = useSelect( ( select ) => select( CORE_MODULES ).getModule( MODULE_SLUG ) );
+	const { activateModule } = useDispatch( CORE_MODULES );
+	const { navigateTo } = useDispatch( CORE_LOCATION );
 
-	// const onClick = useCallback( async () => { } );
+	const onClick = useCallback( async () => {
+		const { error, response } = await activateModule( MODULE_SLUG );
+
+		if ( ! error ) {
+			navigateTo( response.moduleReauthURL );
+		} else {
+			showErrorNotification( GenericError, {
+				id: `${ MODULE_SLUG }-setup-error`,
+				title: __( 'Internal Server Error', 'google-site-kit' ),
+				description: error.message,
+				format: 'small',
+				type: 'win-error',
+			} );
+		}
+	}, [ activateModule, navigateTo ] );
 
 	return (
 		<div className="_googlesitekit-idea-hub-dashboardcta">
@@ -54,7 +76,12 @@ function DashboardCTA() {
 					</Link>
 				</p>
 
-				<Button>{ __( 'Set up', 'google-site-kit' ) }</Button>
+				<Button onClick={ onClick }>{
+					active && ! connected
+						? __( 'Complete set up', 'google-site-kit' )
+						: __( 'Set up', 'google-site-kit' )
+				}</Button>
+
 			</div>
 		</div>
 	);
