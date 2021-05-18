ce5c1ce3ea6e580561ec9fde1d48411d850ce949
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-05-18 20:01:53 +0300

    Update transitional form.

--- assets/js/modules/analytics/components/setup/SetupFormGA4Transitional.js
+++ assets/js/modules/analytics/components/setup/SetupFormGA4Transitional.js
@@ -19,21 +19,55 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { Fragment } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { AccountSelect } from '../common';
+import Data from 'googlesitekit-data';
+import { STORE_NAME, PROFILE_CREATE } from '../../datastore/constants';
+import StoreErrorNotices from '../../../../components/StoreErrorNotices';
+import {
+	AccountSelect,
+	ProfileSelect,
+	PropertySelect,
+	ProfileNameTextField,
+	GA4Notice,
+} from '../common';
+const { useSelect } = Data;
 
 export default function SetupFormGA4Transitional() {
+	const accounts = useSelect( ( select ) => select( STORE_NAME ).getAccounts() ) || [];
+	const hasExistingTag = useSelect( ( select ) => select( STORE_NAME ).hasExistingTag() );
+
+	// Needed to conditionally show the profile name field and surrounding container.
+	const profileID = useSelect( ( select ) => select( STORE_NAME ).getProfileID() );
+
 	return (
 		<Fragment>
+			<StoreErrorNotices moduleSlug="analytics" storeName={ STORE_NAME } />
+			<GA4Notice />
+
+			{ ( !! accounts.length && ! hasExistingTag ) && (
+				<p className="googlesitekit-margin-bottom-0">
+					{ __( 'Please select the account information below. You can change this view later in your settings.', 'google-site-kit' ) }
+				</p>
+			) }
+
 			<div className="googlesitekit-setup-module__inputs">
 				<AccountSelect />
+
+				<PropertySelect />
+
+				<ProfileSelect />
 			</div>
-			<div>SetupFormGA4Transitional</div>
+
+			{ profileID === PROFILE_CREATE && (
+				<div className="googlesitekit-setup-module__inputs googlesitekit-setup-module__inputs--multiline">
+					<ProfileNameTextField />
+				</div>
+			) }
 		</Fragment>
 	);
 }
-
