d2f27c4be6ae711b658037c1f227e57d7a81c427
Daniel Gent <daniel.gent@get10up.com>
2021-05-26 19:47:28 +0200

    Place component on App.

--- assets/js/components/surveys/SurveyViewTrigger.js
+++ assets/js/components/surveys/SurveyViewTrigger.js
@@ -11,20 +11,22 @@ import Data from 'googlesitekit-data';
 import { CORE_SITE } from '../../googlesitekit/datastore/site/constants';
 const { useSelect } = Data;
 
+// TODO - in execution: https://github.com/google/site-kit-wp/issues/3355
+const triggerSurvey = ( args ) => {
+	// eslint-disable-next-line no-console
+	console.log( ...args );
+};
+
 const SurveyViewTrigger = ( { triggerID, ttl } ) => {
 	const usingProxy = useSelect( ( select ) => select( CORE_SITE ).isUsingProxy() );
 
 	useMount( () => {
 		if ( usingProxy ) {
-			console.debug( triggerID, ttl );
+			triggerSurvey( { triggerID, ttl } );
 		}
 	} );
 
-	return (
-		<div>
-			SurveyViewTrigger
-		</div>
-	);
+	return null;
 };
 
 SurveyViewTrigger.propTypes = {
