b29325ae92add83289bf73d9ca832ab9d112f37b
Ivan <373931+ivankruchkoff@users.noreply.github.com>
2021-05-28 12:56:44 -0700

    Update assets/js/googlesitekit/datastore/user/surveys.js
    Co-authored-by: Evan Mattson <evan.mattson@10up.com>

--- assets/js/googlesitekit/datastore/user/surveys.js
+++ assets/js/googlesitekit/datastore/user/surveys.js
@@ -73,7 +73,7 @@ const baseActions = {
 			const { ttl = 0 } = options;
 			invariant( 'string' === typeof triggerID && triggerID.length, 'triggerID is required and must be a string' );
 			invariant( isPlainObject( options ), 'options must be an object' );
-			invariant( 'number' === typeof options.ttl, 'options.ttl must be a number' );
+			invariant( 'number' === typeof ttl, 'options.ttl must be a number' );
 		},
 		function* ( triggerID, { ttl = 0 } = {} ) {
 			const { select } = yield Data.commonActions.getRegistry();
