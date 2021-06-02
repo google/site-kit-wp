71289dc4cd5b81c0bd855d47ee7b8acd2fc8828e
John Phillips <johnbasraphillips@gmail.com>
2021-06-02 10:58:29 +0100

    Refactor SPECIAL_STATES array to use function equality.

--- assets/js/googlesitekit/widgets/util/constants.js
+++ assets/js/googlesitekit/widgets/util/constants.js
@@ -20,6 +20,9 @@
  * Internal dependencies
  */
 import { WIDGET_WIDTHS } from '../datastore/constants';
+import ReportZero from '../../../components/ReportZero';
+import CompleteModuleActivationCTA from '../../../components/CompleteModuleActivationCTA';
+import ActivateModuleCTA from '../../../components/ActivateModuleCTA';
 
 export const WIDTH_GRID_COUNTER_MAP = {
 	[ WIDGET_WIDTHS.QUARTER ]: 3,
@@ -29,7 +32,7 @@ export const WIDTH_GRID_COUNTER_MAP = {
 
 export const HIDDEN_CLASS = 'googlesitekit-hidden';
 export const SPECIAL_WIDGET_STATES = [
-	'ReportZero',
-	'ModuleActivateCTA',
-	'CompleteModuleActivationCTA',
+	ActivateModuleCTA,
+	CompleteModuleActivationCTA,
+	ReportZero,
 ];
