b2b11e5884ff0811811f9e156ee3f9fd904faebc
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-06-04 21:54:51 +0300

    Update selectAccount action.

--- assets/js/modules/analytics/datastore/accounts.js
+++ assets/js/modules/analytics/datastore/accounts.js
@@ -163,12 +163,17 @@ const baseActions = {
 
 			if ( ! uaProperty ) {
 				uaProperty = {
-					id: PROPERTY_CREATE,
+					id: uaProperties.length === 0 ? PROPERTY_CREATE : '', // Create a new property only if the selected account has no UA properties.
 					internalWebPropertyId: '', // eslint-disable-line sitekit/acronym-case
 				};
 			}
 
-			yield propertyActions.selectProperty( uaProperty?.id, uaProperty?.internalWebPropertyId ); // eslint-disable-line sitekit/acronym-case
+			if ( uaProperty?.id ) {
+				yield propertyActions.selectProperty( uaProperty?.id, uaProperty?.internalWebPropertyId ); // eslint-disable-line sitekit/acronym-case
+			} else {
+				registry.dispatch( STORE_NAME ).setPropertyID( '' );
+				registry.dispatch( STORE_NAME ).setProfileID( '' );
+			}
 
 			if ( ! isFeatureEnabled( 'ga4setup' ) ) {
 				return;
