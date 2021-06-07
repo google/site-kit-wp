a97c3d5d4d2a46e097e483f013f9e36218745ec4
Daniel Gent <daniel.gent@get10up.com>
2021-06-07 19:10:43 +0200

    Export directly.

--- assets/js/components/SettingsNotice/index.js
+++ assets/js/components/SettingsNotice/index.js
@@ -1,14 +1,4 @@
 import SettingsNotice from './SettingsNotice';
-import {
-	TYPE_WARNING,
-	TYPE_INFO,
-	TYPE_SUGGESTION,
-} from './utils';
+export { TYPE_WARNING, TYPE_INFO, TYPE_SUGGESTION } from './utils';
 
 export default SettingsNotice;
-
-export {
-	TYPE_WARNING,
-	TYPE_INFO,
-	TYPE_SUGGESTION,
-};
