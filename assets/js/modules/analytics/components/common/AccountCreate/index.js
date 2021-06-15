1aff4a53f26f46cf8f375ea7a9f7bb2543a0cf07
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-06-15 15:18:11 +0300

    Fix permission scopes issue.

--- assets/js/modules/analytics/components/common/AccountCreate/index.js
+++ assets/js/modules/analytics/components/common/AccountCreate/index.js
@@ -26,7 +26,7 @@ import { useCallback, useState, useEffect } from '@wordpress/element';
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
-import { STORE_NAME, FORM_ACCOUNT_CREATE, PROVISIONING_SCOPE } from '../../../datastore/constants';
+import { STORE_NAME, FORM_ACCOUNT_CREATE, PROVISIONING_SCOPE, EDIT_SCOPE } from '../../../datastore/constants';
 import { CORE_SITE } from '../../../../../googlesitekit/datastore/site/constants';
 import { CORE_USER } from '../../../../../googlesitekit/datastore/user/constants';
 import { CORE_FORMS } from '../../../../../googlesitekit/datastore/forms/constants';
@@ -49,7 +49,9 @@ import CountrySelect from './CountrySelect';
 const { useDispatch, useSelect } = Data;
 
 export default function AccountCreate() {
+	const [ isNavigating, setIsNavigating ] = useState( false );
 	const isGA4enabled = useFeature( 'ga4setup' );
+
 	const { accounts, hasResolvedAccounts } = useSelect( ( select ) => ( {
 		accounts: select( STORE_NAME ).getAccounts(),
 		hasResolvedAccounts: select( STORE_NAME ).hasFinishedResolution( 'getAccounts' ),
@@ -58,16 +60,17 @@ export default function AccountCreate() {
 	const canSubmitAccountCreate = useSelect( ( select ) => select( STORE_NAME ).canSubmitAccountCreate() );
 	const isDoingCreateAccount = useSelect( ( select ) => select( STORE_NAME ).isDoingCreateAccount() );
 	const hasProvisioningScope = useSelect( ( select ) => select( CORE_USER ).hasScope( PROVISIONING_SCOPE ) );
+	const hasEditScope = useSelect( ( select ) => isGA4enabled ? select( CORE_USER ).hasScope( EDIT_SCOPE ) : undefined );
 	const hasAccountCreateForm = useSelect( ( select ) => select( CORE_FORMS ).hasForm( FORM_ACCOUNT_CREATE ) );
 	const autoSubmit = useSelect( ( select ) => select( CORE_FORMS ).getValue( FORM_ACCOUNT_CREATE, 'autoSubmit' ) );
 	const siteURL = useSelect( ( select ) => select( CORE_SITE ).getReferenceSiteURL() );
 	const siteName = useSelect( ( select ) => select( CORE_SITE ).getSiteName() );
 	const timezone = useSelect( ( select ) => select( CORE_SITE ).getTimezone() );
 
-	const [ isNavigating, setIsNavigating ] = useState( false );
-
 	const { setValues } = useDispatch( CORE_FORMS );
 	const { navigateTo } = useDispatch( CORE_LOCATION );
+	const { createAccount } = useDispatch( STORE_NAME );
+	const { setPermissionScopeError } = useDispatch( CORE_USER );
 
 	// Redirect if the accountTicketTermsOfServiceURL is set.
 	useEffect( () => {
@@ -89,38 +92,51 @@ export default function AccountCreate() {
 		}
 	}, [ hasAccountCreateForm, siteName, siteURL, timezone, setValues ] );
 
-	const { createAccount } = useDispatch( STORE_NAME );
-	const { setPermissionScopeError } = useDispatch( CORE_USER );
-	const handleSubmit = useCallback(
-		async () => {
-			// If scope not granted, trigger scope error right away. These are
-			// typically handled automatically based on API responses, but
-			// this particular case has some special handling to improve UX.
-			if ( ! hasProvisioningScope ) {
-				// When state is restored, auto-submit the request again.
-				setValues( FORM_ACCOUNT_CREATE, { autoSubmit: true } );
-				setPermissionScopeError( {
-					code: ERROR_CODE_MISSING_REQUIRED_SCOPE,
-					message: __( 'Additional permissions are required to create a new Analytics account.', 'google-site-kit' ),
-					data: {
-						status: 403,
-						scopes: [ PROVISIONING_SCOPE ],
-						skipModal: true,
-					},
-				} );
-				return;
-			}
+	const handleSubmit = useCallback( async () => {
+		const scopes = [];
 
-			setValues( FORM_ACCOUNT_CREATE, { autoSubmit: false } );
-			await trackEvent( 'analytics_setup', 'new_account_setup_clicked' );
-			const { error } = await createAccount();
+		if ( ! hasProvisioningScope ) {
+			scopes.push( PROVISIONING_SCOPE );
+		}
 
-			if ( ! error ) {
-				setIsNavigating( true );
-			}
-		},
-		[ createAccount, setIsNavigating, hasProvisioningScope, setPermissionScopeError, setValues ]
-	);
+		if ( isGA4enabled && ! hasEditScope ) {
+			scopes.push( EDIT_SCOPE );
+		}
+
+		// If scope not granted, trigger scope error right away. These are
+		// typically handled automatically based on API responses, but
+		// this particular case has some special handling to improve UX.
+		if ( scopes.length > 0 ) {
+			// When state is restored, auto-submit the request again.
+			setValues( FORM_ACCOUNT_CREATE, { autoSubmit: true } );
+			setPermissionScopeError( {
+				code: ERROR_CODE_MISSING_REQUIRED_SCOPE,
+				message: __( 'Additional permissions are required to create a new Analytics account.', 'google-site-kit' ),
+				data: {
+					status: 403,
+					scopes,
+					skipModal: true,
+				},
+			} );
+			return;
+		}
+
+		setValues( FORM_ACCOUNT_CREATE, { autoSubmit: false } );
+		await trackEvent( 'analytics_setup', 'new_account_setup_clicked' );
+
+		const { error } = await createAccount();
+		if ( ! error ) {
+			setIsNavigating( true );
+		}
+	}, [
+		createAccount,
+		setIsNavigating,
+		hasProvisioningScope,
+		hasEditScope,
+		setPermissionScopeError,
+		setValues,
+		isGA4enabled,
+	] );
 
 	// If the user ends up back on this component with the provisioning scope granted,
 	// and already submitted the form, trigger the submit again.
