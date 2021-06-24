b1992a906a1389d77bcc945f5ac28f79fbeba9af
John Phillips <johnbasraphillips@gmail.com>
2021-06-24 12:46:57 +0100

    Scaffold out SetupMain and SetupForm components.

---
+++ assets/js/modules/idea-hub/components/setup/SetupForm.js
@@ -0,0 +1,25 @@
+/**
+ * Idea Hub Setup Form component.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default function SetupForm() {
+	return (
+		<form className="googlesitekit-idea-hub-setup__form">
+			<p>Idea Hub description</p>
+		</form>
+	);
+}
