06d1ed16d5fd330ab859b310cfa69d656f5df7b5
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-06-28 22:26:54 +0300

    Add Dismissals class.

--- includes/Core/Dismissals/Dismissals.php
+++ includes/Core/Dismissals/Dismissals.php
@@ -1,63 +1,63 @@
 <?php
 /**
- * Class Google\Site_Kit\Core\Feature_Tours\Feature_Tours
+ * Class Google\Site_Kit\Core\Dismissals\Dismissals
  *
- * @package   Google\Site_Kit\Core\Feature_Tours
+ * @package   Google\Site_Kit\Core\Dismissals
  * @copyright 2021 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
 
-namespace Google\Site_Kit\Core\Feature_Tours;
+namespace Google\Site_Kit\Core\Dismissals;
 
 use Google\Site_Kit\Context;
 use Google\Site_Kit\Core\Storage\User_Options;
 
 /**
- * Class for handling feature tours.
+ * Class for handling dismissals.
  *
- * @since 1.27.0
+ * @since n.e.x.t
  * @access private
  * @ignore
  */
-class Feature_Tours {
+class Dismissals {
 
 	/**
-	 * Dismissed_Tours instance.
+	 * Dismissed_Items instance.
 	 *
-	 * @since 1.27.0
-	 * @var Dismissed_Tours
+	 * @since n.e.x.t
+	 * @var Dismissed_Items
 	 */
-	protected $dismissed_tours;
+	protected $dismissed_items;
 
 	/**
-	 * REST_Feature_Tours_Controller instance.
+	 * REST_Dismissals_Controller instance.
 	 *
-	 * @since 1.27.0
-	 * @var REST_Feature_Tours_Controller
+	 * @since n.e.x.t
+	 * @var REST_Dismissals_Controller
 	 */
 	protected $rest_controller;
 
 	/**
 	 * Constructor.
 	 *
-	 * @since 1.27.0
+	 * @since n.e.x.t
 	 *
 	 * @param Context      $context Plugin context.
 	 * @param User_Options $user_options Optional. User option API. Default is a new instance.
 	 */
 	public function __construct( Context $context, User_Options $user_options = null ) {
-		$this->dismissed_tours = new Dismissed_Tours( $user_options ?: new User_Options( $context ) );
-		$this->rest_controller = new REST_Feature_Tours_Controller( $this->dismissed_tours );
+		$this->dismissed_items = new Dismissed_Items( $user_options ?: new User_Options( $context ) );
+		$this->rest_controller = new REST_Dismissals_Controller( $this->dismissed_items );
 	}
 
 	/**
 	 * Registers functionality through WordPress hooks.
 	 *
-	 * @since 1.27.0
+	 * @since n.e.x.t
 	 */
 	public function register() {
-		$this->dismissed_tours->register();
+		$this->dismissed_items->register();
 		$this->rest_controller->register();
 	}
 
