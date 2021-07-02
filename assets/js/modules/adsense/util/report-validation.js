5539a4c653cbabcb08d17ab01ac7923840224736
Evan Mattson <evan.mattson@10up.com>
2021-07-02 11:44:00 +0300

    Add report validation utilities for metrics and dimensions.

---
+++ assets/js/modules/adsense/util/report-validation.js
@@ -0,0 +1,147 @@
+/**
+ * Reporting API validation utilities.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import invariant from 'invariant';
+
+/* eslint-disable-next-line */
+/** @see (@link https://developers.google.com/adsense/management/reference/rest/v2/Metric) */
+const VALID_METRICS = [
+	'METRIC_UNSPECIFIED',
+	'PAGE_VIEWS',
+	'AD_REQUESTS',
+	'MATCHED_AD_REQUESTS',
+	'TOTAL_IMPRESSIONS',
+	'IMPRESSIONS',
+	'INDIVIDUAL_AD_IMPRESSIONS',
+	'CLICKS',
+	'PAGE_VIEWS_SPAM_RATIO',
+	'AD_REQUESTS_SPAM_RATIO',
+	'MATCHED_AD_REQUESTS_SPAM_RATIO',
+	'IMPRESSIONS_SPAM_RATIO',
+	'INDIVIDUAL_AD_IMPRESSIONS_SPAM_RATIO',
+	'CLICKS_SPAM_RATIO',
+	'AD_REQUESTS_COVERAGE',
+	'PAGE_VIEWS_CTR',
+	'AD_REQUESTS_CTR',
+	'MATCHED_AD_REQUESTS_CTR',
+	'IMPRESSIONS_CTR',
+	'INDIVIDUAL_AD_IMPRESSIONS_CTR',
+	'ACTIVE_VIEW_MEASURABILITY',
+	'ACTIVE_VIEW_VIEWABILITY',
+	'ACTIVE_VIEW_TIME',
+	'ESTIMATED_EARNINGS',
+	'PAGE_VIEWS_RPM',
+	'AD_REQUESTS_RPM',
+	'MATCHED_AD_REQUESTS_RPM',
+	'IMPRESSIONS_RPM',
+	'INDIVIDUAL_AD_IMPRESSIONS_RPM',
+	'COST_PER_CLICK',
+	'ADS_PER_IMPRESSION',
+	'TOTAL_EARNINGS',
+	'WEBSEARCH_RESULT_PAGES',
+];
+
+/* eslint-disable-next-line */
+/** @see (@link https://developers.google.com/adsense/management/reference/rest/v2/Dimension) */
+const VALID_DIMENSIONS = [
+	'DIMENSION_UNSPECIFIED',
+	'DATE',
+	'WEEK',
+	'MONTH',
+	'ACCOUNT_NAME',
+	'AD_CLIENT_ID',
+	'PRODUCT_NAME',
+	'PRODUCT_CODE',
+	'AD_UNIT_NAME',
+	'AD_UNIT_ID',
+	'AD_UNIT_SIZE_NAME',
+	'AD_UNIT_SIZE_CODE',
+	'CUSTOM_CHANNEL_NAME',
+	'CUSTOM_CHANNEL_ID',
+	'OWNED_SITE_DOMAIN_NAME',
+	'OWNED_SITE_ID',
+	'URL_CHANNEL_NAME',
+	'URL_CHANNEL_ID',
+	'BUYER_NETWORK_NAME',
+	'BUYER_NETWORK_ID',
+	'BID_TYPE_NAME',
+	'BID_TYPE_CODE',
+	'CREATIVE_SIZE_NAME',
+	'CREATIVE_SIZE_CODE',
+	'DOMAIN_NAME',
+	'DOMAIN_CODE',
+	'COUNTRY_NAME',
+	'COUNTRY_CODE',
+	'PLATFORM_TYPE_NAME',
+	'PLATFORM_TYPE_CODE',
+	'TARGETING_TYPE_NAME',
+	'TARGETING_TYPE_CODE',
+	'CONTENT_PLATFORM_NAME',
+	'CONTENT_PLATFORM_CODE',
+	'AD_PLACEMENT_NAME',
+	'AD_PLACEMENT_CODE',
+	'REQUESTED_AD_TYPE_NAME',
+	'REQUESTED_AD_TYPE_CODE',
+	'SERVED_AD_TYPE_NAME',
+	'SERVED_AD_TYPE_CODE',
+	'AD_FORMAT_NAME',
+	'AD_FORMAT_CODE',
+	'CUSTOM_SEARCH_STYLE_NAME',
+	'CUSTOM_SEARCH_STYLE_ID',
+	'DOMAIN_REGISTRANT',
+	'WEBSEARCH_QUERY_STRING',
+];
+
+/**
+ * Validates the given metrics are valid to be used in a request.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Array} metrics Metrics to validate.
+ */
+export function validateMetrics( metrics ) {
+	invariant( Array.isArray( metrics ), 'metrics must be an array.' );
+	invariant( metrics.length, 'at least one metric is required.' );
+
+	const invalidMetrics = metrics.filter(
+		( metric ) => ! VALID_METRICS.includes( metric )
+	);
+
+	invariant( invalidMetrics.length === 0, `invalid AdSense metrics requested: ${ invalidMetrics.toString() }` );
+}
+
+/**
+ * Validates the given dimensions are valid to be used in a request.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Array} dimensions Dimensions to validate.
+ */
+export function validateDimensions( dimensions ) {
+	invariant( Array.isArray( dimensions ), 'dimensions must be an array.' );
+	invariant( dimensions.length, 'at least one dimension is required.' );
+
+	const invalidDimensions = dimensions.filter(
+		( metric ) => ! VALID_DIMENSIONS.includes( metric )
+	);
+
+	invariant( invalidDimensions.length === 0, `invalid AdSense dimensions requested: ${ invalidDimensions.toString() }` );
+}
