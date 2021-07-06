6dd73538b14ecfaae28b154a73dae18fb0e13386
Daniel Gent <daniel.gent@get10up.com>
2021-07-06 20:12:24 +0200

    Improve test for fetchCreateIdeaDraftPostStore reducer.

--- assets/js/modules/idea-hub/datastore/draft-ideas.js
+++ assets/js/modules/idea-hub/datastore/draft-ideas.js
@@ -40,9 +40,9 @@ const fetchCreateIdeaDraftPostStore = createFetchStore( {
 		return {
 			...state,
 			draftPostIdeas: [ ...( state.draftPostIdeas || [] ), ideaDraftPost ],
-			// Try to stop errors
-			newIdeas: ( state.newIdeas || [] ),
-			savedIdeas: ( state.savedIdeas || [] ),
+			// Try to stop errors.
+			// newIdeas: ( state.newIdeas || [] ),
+			// savedIdeas: ( state.savedIdeas || [] ),
 		};
 	},
 	argsToParams: ( idea ) => {
