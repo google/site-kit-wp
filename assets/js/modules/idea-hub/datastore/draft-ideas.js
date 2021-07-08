fb011f6181c4cd85c2a36c70e1ac5f1debbdebc9
Daniel Gent <daniel.gent@get10up.com>
2021-07-08 18:11:06 +0200

    Set correct text.

--- assets/js/modules/idea-hub/datastore/draft-ideas.js
+++ assets/js/modules/idea-hub/datastore/draft-ideas.js
@@ -44,9 +44,6 @@ const fetchCreateIdeaDraftPostStore = createFetchStore( {
 		return {
 			...state,
 			draftPostIdeas: [ ...( state.draftPostIdeas || [] ), ideaDraftPost ],
-			// Try to stop errors.
-			// newIdeas: ( state.newIdeas || [] ),
-			// savedIdeas: ( state.savedIdeas || [] ),
 		};
 	},
 	argsToParams: ( idea ) => {
