e8e3478cfe88b9e0da3b649b61a948c2e58b85a5
Daniel Gent <daniel.gent@get10up.com>
2021-07-19 19:20:05 +0200

    Revert default date settings.

--- assets/js/googlesitekit/datastore/user/date-range.js
+++ assets/js/googlesitekit/datastore/user/date-range.js
@@ -35,8 +35,7 @@ import {
 } from '../../../util/date-range';
 
 export const initialState = {
-	// dateRange: 'last-28-days',
-	dateRange: 'last-7-days',
+	dateRange: 'last-28-days',
 	referenceDate: getDateString( new Date() ),
 };
 
