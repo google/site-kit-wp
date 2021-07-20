b6d80df33f9d7864dde10c1be6292aec582fb0e8
Ivan@Kruchkoff.com <ivan@kruchkoff.com>
2021-07-20 11:18:56 +0300

    Add tests.

---
+++ tests/phpunit/integration/Core/Tags/Guards/Tag_Production_GuardTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tag_Production_GuardTest
+ *
+ * @package   Google\Site_Kit\Tests\Core\Tags\Guards
+ * @copyright 2021 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests\Core\Tags\Guards;
+
+use Google\Site_Kit\Core\Tags\Guards\Tag_Production_Guard;
+use Google\Site_Kit\Tests\TestCase;
+
+class Tag_Production_GuardTest extends TestCase {
+
+	public function test_can_activate_in_production() {
+		// Pre WP-5.5.0
+		if ( ! function_exists( 'wp_get_environment_type' ) ) {
+			$this->markTestSkipped( 'Missing wp_get_environment_type() function.' );
+		}
+		$tagproduction = new Tag_Production_Guard();
+		$this->assertTrue( $tagproduction->can_activate() );
+
+	}
+
+	public function test_can_activate_on_older_environments() {
+		// Post WP-5.5.0
+		if ( function_exists( 'wp_get_environment_type' ) ) {
+			$this->markTestSkipped( 'Testing legacy environments without wp_get_environment_type() function.' );
+		}
+		$tagproduction = new Tag_Production_Guard();
+		$this->assertTrue( $tagproduction->can_activate() );
+	}
+}
