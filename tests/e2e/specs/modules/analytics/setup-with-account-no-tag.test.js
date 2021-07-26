9577753b92ef538771f6a8a0fe0158131d35501a
Matthew Riley MacPherson <hi@tofumatt.com>
2021-07-26 11:09:02 +0100

    Update tests/e2e/specs/modules/analytics/setup-with-account-no-tag.test.js
    Co-authored-by: Eugene Manuilov <eugene.manuilov@gmail.com>

--- tests/e2e/specs/modules/analytics/setup-with-account-no-tag.test.js
+++ tests/e2e/specs/modules/analytics/setup-with-account-no-tag.test.js
@@ -83,7 +83,7 @@ describe( 'setting up the Analytics module with an existing account and no exist
 					status: 200,
 					body: JSON.stringify( { placeholder_response: true } ),
 				} );
-			} 	else if ( request.url().match( 'google-site-kit/v1/modules/analytics/data/goals' ) ) {
+			} else if ( request.url().match( 'google-site-kit/v1/modules/analytics/data/goals' ) ) {
 				request.respond( { status: 200, body: JSON.stringify( {} ) } );
 			} else {
 				request.continue();
