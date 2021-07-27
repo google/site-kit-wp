fdd01d502c1a888d5fb0d7bcfaee9ec0145d2b3b
John Phillips <johnbasraphillips@gmail.com>
2021-07-27 16:31:07 +0100

    Replace calls to STORE_NAME in googlesitekit/datastore/forms with CORE_FORMS.

--- assets/js/googlesitekit/datastore/forms/index.js
+++ assets/js/googlesitekit/datastore/forms/index.js
@@ -23,12 +23,12 @@ import Data from 'googlesitekit-data';
 import { createErrorStore } from '../../data/create-error-store';
 import { createSnapshotStore } from '../../data/create-snapshot-store';
 import forms from './forms';
-import { STORE_NAME } from './constants';
+import { CORE_FORMS } from './constants';
 
 const store = Data.combineStores(
 	Data.commonStore,
 	forms,
-	createSnapshotStore( STORE_NAME ),
+	createSnapshotStore( CORE_FORMS ),
 	createErrorStore(),
 );
 
@@ -40,7 +40,7 @@ export const resolvers = store.resolvers;
 export const selectors = store.selectors;
 
 export const registerStore = ( registry ) => {
-	registry.registerStore( STORE_NAME, store );
+	registry.registerStore( CORE_FORMS, store );
 };
 
 export default store;
