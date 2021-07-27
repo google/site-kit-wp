21413f96743c9b459e262afe55f255a5498cfb45
Daniel Gent <daniel.gent@get10up.com>
2021-07-27 16:03:28 +0200

    Add placeholder component.

---
+++ assets/js/components/surveys/SurveyQuestionOpenText.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SurveyQuestionOpenText = ( props ) => {
+	return (
+		<div>
+			SurveyQuestionOpenText
+		</div>
+	);
+};
+
+SurveyQuestionOpenText.propTypes = {
+
+};
+
+export default SurveyQuestionOpenText;
