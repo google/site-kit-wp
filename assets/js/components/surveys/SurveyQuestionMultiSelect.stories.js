d3793dd10124f2029f3adec8ef03094262ff91c0
Daniel Gent <daniel.gent@get10up.com>
2021-07-27 20:57:12 +0200

    Commit initial empty files.

---
+++ assets/js/components/surveys/SurveyQuestionMultiSelect.stories.js
@@ -0,0 +1,45 @@
+/**
+ * SurveyQuestionMultiSelect Component Stories.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import SurveyQuestionMultiSelect from './SurveyQuestionMultiSelect';
+
+const Template = ( args ) => (
+	<div className="googlesitekit-survey">
+		<SurveyQuestionMultiSelect { ...args } />
+	</div>
+);
+
+export const SurveyQuestionMultiSelectStory = Template.bind( {} );
+SurveyQuestionMultiSelectStory.storyName = 'SurveyQuestionMultiSelect';
+SurveyQuestionMultiSelectStory.args = {
+	question: 'Based on your experience so far, how satisfied are you with Site Kit?',
+	answerQuestion: ( answer ) => {
+		global.console.log( 'Clicked', answer );
+	},
+	dismissSurvey: () => {
+		global.console.log( 'Dismissed Survey' );
+	},
+};
+
+export default {
+	title: 'Components/Surveys',
+};
+
