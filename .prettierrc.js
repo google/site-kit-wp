7e3aab57305dddd969c9b63c8af63f05e7a5a171
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2021-07-27 11:06:30 +0100

    Add Prettier and configs.

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// This file is here, in part, so that users whose code editors are
+// set to automatically format files with Prettier have a config to detect.
+// Many users only run Prettier when a config is present, so this file makes
+// sure one can be detected, even though we aren't doing anything with it.
+module.exports = {
+	...require('@wordpress/prettier-config'),
+}
