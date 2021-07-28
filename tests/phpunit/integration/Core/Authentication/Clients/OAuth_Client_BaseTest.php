4aa4d45fd6edbe063660afa4da184f41fc35715f
Felix Arntz <felixarntz@google.com>
2021-07-28 13:40:58 -0700

    Replace OAuth_Client_Empty class with using PHPUnit abstract class mock.

--- tests/phpunit/integration/Core/Authentication/Clients/OAuth_Client_BaseTest.php
+++ tests/phpunit/integration/Core/Authentication/Clients/OAuth_Client_BaseTest.php
@@ -11,9 +11,9 @@
 namespace Google\Site_Kit\Tests\Core\Authentication\Clients;
 
 use Google\Site_Kit\Context;
+use Google\Site_Kit\Core\Authentication\Clients\OAuth_Client_Base;
 use Google\Site_Kit\Core\Authentication\Token;
 use Google\Site_Kit\Core\Storage\User_Options;
-use Google\Site_Kit\Tests\Core\Authentication\Clients\OAuth_Client_Empty;
 use Google\Site_Kit\Tests\TestCase;
 
 /**
@@ -22,7 +22,9 @@ use Google\Site_Kit\Tests\TestCase;
 class OAuth_Client_BaseTest extends TestCase {
 
 	public function test_get_client() {
-		$oauth_client = new OAuth_Client_Empty( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs( array( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) )
+			->getMockForAbstractClass();
 		$client       = $oauth_client->get_client();
 
 		$this->assertInstanceOf( 'Google\Site_Kit\Core\Authentication\Clients\Google_Site_Kit_Client', $client );
@@ -32,7 +34,9 @@ class OAuth_Client_BaseTest extends TestCase {
 	}
 
 	public function test_get_required_scopes() {
-		$client = new OAuth_Client_Empty( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs( array( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) )
+			->getMockForAbstractClass();
 		remove_all_filters( 'googlesitekit_auth_scopes' );
 
 		$this->assertEqualSets(
@@ -41,25 +45,29 @@ class OAuth_Client_BaseTest extends TestCase {
 				'https://www.googleapis.com/auth/userinfo.email',
 				'openid',
 			),
-			$client->get_required_scopes()
+			$oauth_client->get_required_scopes()
 		);
 	}
 
 	public function test_get_granted_scopes() {
 		$user_id = $this->factory()->user->create();
 		wp_set_current_user( $user_id );
-		$client = new OAuth_Client_Empty( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs( array( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) )
+			->getMockForAbstractClass();
 
 		$granted_scopes = array( 'test-scope' );
-		update_user_option( $user_id, OAuth_Client_Empty::OPTION_AUTH_SCOPES, $granted_scopes );
+		update_user_option( $user_id, OAuth_Client_Base::OPTION_AUTH_SCOPES, $granted_scopes );
 
-		$this->assertEquals( $granted_scopes, $client->get_granted_scopes() );
+		$this->assertEquals( $granted_scopes, $oauth_client->get_granted_scopes() );
 	}
 
 	public function test_set_granted_scopes() {
 		$user_id = $this->factory()->user->create();
 		wp_set_current_user( $user_id );
-		$client = new OAuth_Client_Empty( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs( array( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) )
+			->getMockForAbstractClass();
 
 		// Register a custom list of required scopes for this test.
 		add_filter(
@@ -69,31 +77,35 @@ class OAuth_Client_BaseTest extends TestCase {
 			}
 		);
 
-		$this->assertNotContains( 'test-scope', (array) get_user_option( OAuth_Client_Empty::OPTION_AUTH_SCOPES, $user_id ) );
+		$this->assertNotContains( 'test-scope', (array) get_user_option( OAuth_Client_Base::OPTION_AUTH_SCOPES, $user_id ) );
 
-		$client->set_granted_scopes( array( 'test-scope' ) );
-		$this->assertContains( 'test-scope', (array) get_user_option( OAuth_Client_Empty::OPTION_AUTH_SCOPES, $user_id ) );
+		$oauth_client->set_granted_scopes( array( 'test-scope' ) );
+		$this->assertContains( 'test-scope', (array) get_user_option( OAuth_Client_Base::OPTION_AUTH_SCOPES, $user_id ) );
 
 		// It ignores any scope that is not required.
-		$client->set_granted_scopes( array( 'test-scope', 'unsupported-scope' ) );
-		$this->assertContains( 'test-scope', (array) get_user_option( OAuth_Client_Empty::OPTION_AUTH_SCOPES, $user_id ) );
-		$this->assertNotContains( 'unsupported-scope', (array) get_user_option( OAuth_Client_Empty::OPTION_AUTH_SCOPES, $user_id ) );
+		$oauth_client->set_granted_scopes( array( 'test-scope', 'unsupported-scope' ) );
+		$this->assertContains( 'test-scope', (array) get_user_option( OAuth_Client_Base::OPTION_AUTH_SCOPES, $user_id ) );
+		$this->assertNotContains( 'unsupported-scope', (array) get_user_option( OAuth_Client_Base::OPTION_AUTH_SCOPES, $user_id ) );
 	}
 
 	public function test_get_token() {
 		$user_id = $this->factory()->user->create();
 		wp_set_current_user( $user_id );
-		$context = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
-		$token   = new Token( new User_Options( $context, $user_id ) );
-		$client  = new OAuth_Client_Empty(
-			new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ),
-			null,
-			null,
-			null,
-			null,
-			null,
-			$token
-		);
+		$context      = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
+		$token        = new Token( new User_Options( $context, $user_id ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs(
+				array(
+					$context,
+					null,
+					null,
+					null,
+					null,
+					null,
+					$token,
+				)
+			)
+			->getMockForAbstractClass();
 
 		$token_data = array(
 			'access_token' => 'test-access-token',
@@ -101,48 +113,56 @@ class OAuth_Client_BaseTest extends TestCase {
 			'created'      => 649724400,
 		);
 		$token->set( $token_data );
-		$this->assertEquals( $token_data, $client->get_token() );
+		$this->assertEquals( $token_data, $oauth_client->get_token() );
 	}
 
 	public function test_set_token() {
 		$user_id = $this->factory()->user->create();
 		wp_set_current_user( $user_id );
-		$context = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
-		$token   = new Token( new User_Options( $context, $user_id ) );
-		$client  = new OAuth_Client_Empty(
-			new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ),
-			null,
-			null,
-			null,
-			null,
-			null,
-			$token
-		);
+		$context      = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
+		$token        = new Token( new User_Options( $context, $user_id ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs(
+				array(
+					$context,
+					null,
+					null,
+					null,
+					null,
+					null,
+					$token,
+				)
+			)
+			->getMockForAbstractClass();
 
 		$token_data = array(
 			'access_token' => 'test-access-token',
 			'expires_in'   => 3600,
 			'created'      => 649724400,
 		);
-		$client->set_token( $token_data );
+		$oauth_client->set_token( $token_data );
 		$this->assertEquals( $token_data, $token->get() );
 	}
 
 	public function test_get_error_message_unknown() {
-		$client = new OAuth_Client_Empty( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs( array( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) )
+			->getMockForAbstractClass();
 
-		$this->assertContains( 'Unknown Error (code: unknown_code)', $client->get_error_message( 'unknown_code' ) );
-		$this->assertContains( 'Unknown Error (code: )', $client->get_error_message( '' ) );
-		$this->assertContains( 'Unknown Error (code: 123)', $client->get_error_message( 123 ) );
+		$this->assertContains( 'Unknown Error (code: unknown_code)', $oauth_client->get_error_message( 'unknown_code' ) );
+		$this->assertContains( 'Unknown Error (code: )', $oauth_client->get_error_message( '' ) );
+		$this->assertContains( 'Unknown Error (code: 123)', $oauth_client->get_error_message( 123 ) );
 	}
 
 	/**
 	 * @dataProvider error_message_provider
 	 */
 	public function test_get_error_message( $error_code ) {
-		$client = new OAuth_Client_Empty( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+		$oauth_client = $this->getMockBuilder( OAuth_Client_Base::class )
+			->setConstructorArgs( array( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) )
+			->getMockForAbstractClass();
 
-		$message = $client->get_error_message( $error_code );
+		$message = $oauth_client->get_error_message( $error_code );
 
 		$this->assertRegExp( '/unable|invalid|failed/i', $message );
 		$this->assertNotContains( 'Unknown Error', $message );
