038b4f53bdea2dcfdfd9452be85907852ddfc0e0
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2021-08-03 23:06:16 +0100

    Remove all v1/data API request interception calls in E2E tests.

--- tests/e2e/specs/modules/optimize/activation.test.js
+++ tests/e2e/specs/modules/optimize/activation.test.js
@@ -79,20 +79,7 @@ describe( 'Optimize Activation', () => {
 	beforeAll( async () => {
 		await page.setRequestInterception( true );
 		useRequestInterception( ( request ) => {
-			if ( request.url().match( '/google-site-kit/v1/data/' ) ) {
-				request.respond( { status: 200 } );
-			} else if (
-				request
-					.url()
-					.match(
-						'/wp-json/google-site-kit/v1/modules/analytics/data/report?'
-					)
-			) {
-				request.respond( {
-					status: 200,
-					body: JSON.stringify( { placeholder_response: true } ),
-				} );
-			} else if (
+			if (
 				request
 					.url()
 					.match(
