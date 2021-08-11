e01c7ee3d0f5296efd7158eb746abff343a09b1f
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-08-11 21:13:33 +0300

    Add Injection_Aware_Interface interface.

---
+++ includes/Core/DI/Injection_Aware_Interface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class Google\Site_Kit\Core\DI\Injection_Aware_Interface
+ *
+ * @package   Google\Site_Kit\Core\Dismissals
+ * @copyright 2021 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Core\DI;
+
+use Google\Site_Kit_Dependencies\Psr\Container\ContainerInterface;
+
+interface Injection_Aware_Interface {
+
+	/**
+	 * Gets DI container.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return ContainerInterface DI container.
+	 */
+	public function get_di();
+
+	/**
+	 * Sets DI container.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param ContainerInterface $di_container DI container.
+	 */
+	public function set_di( ContainerInterface $di_container );
+
+}
---
+++ includes/Core/DI/With_DI_Container_Trait.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class Google\Site_Kit\Core\DI\With_DI_Container_Trait
+ *
+ * @package   Google\Site_Kit\Core\Dismissals
+ * @copyright 2021 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Core\DI;
+
+/**
+ * Trait for injection aware classes.
+ *
+ * @since n.e.x.t
+ */
+trait With_DI_Container_Trait {
+
+}
