063591b9e924f5e4169d2ae18ce894d0aab79a62
John Phillips <johnbasraphillips@gmail.com>
2021-08-27 17:17:00 +0100

    Update Idea Hub module description.

--- includes/Modules/Idea_Hub.php
+++ includes/Modules/Idea_Hub.php
@@ -649,7 +649,7 @@ final class Idea_Hub extends Module
 		return array(
 			'slug'        => self::MODULE_SLUG,
 			'name'        => _x( 'Idea Hub', 'Service name', 'google-site-kit' ),
-			'description' => __( "Idea Hub suggests what you can write about next, based on searches that haven't been answered yet", 'google-site-kit' ),
+			'description' => __( 'Idea Hub suggests what you can write about next, from actual questions people asked on Google Search', 'google-site-kit' ),
 			'order'       => 7,
 		);
 	}
