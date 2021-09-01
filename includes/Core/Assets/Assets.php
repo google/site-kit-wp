a92a3d9c1abb84cde61748477e36b1465896358f
Arafat Zahan <arafat.zahan@10up.com>
2021-09-01 19:10:05 +0600

    Revert includes/Core/Assets/Assets.php changes.

--- includes/Core/Assets/Assets.php
+++ includes/Core/Assets/Assets.php
@@ -253,12 +253,13 @@ final class Assets {
 		add_action(
 			$action,
 			function() use ( $font_families ) {
-				$fonts_script = sprintf(
-					"
+				?>
+				<script>
+
 					WebFontConfig = {
-						google: { families: %s }
+						google: { families: [<?php echo "'" . implode( "','", $font_families ) . "'"; /* phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?>] }
 					};
-			
+
 					( function() {
 						var wf = document.createElement( 'script' );
 						wf.src = ( 'https:' === document.location.protocol ? 'https' : 'http' ) + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
@@ -267,10 +268,9 @@ final class Assets {
 						var s = document.getElementsByTagName( 'script' )[0];
 						s.parentNode.insertBefore( wf, s );
 					} )();
-					",
-					wp_json_encode( $font_families )
-				);
-				BC_Functions::wp_print_inline_script_tag( $fonts_script );
+
+				</script>
+				<?php
 			}
 		);
 	}
