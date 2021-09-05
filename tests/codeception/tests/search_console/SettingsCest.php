fb0bbc4c022e71e22f5d47484e9842e6a44a7f91
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-09-06 00:12:01 +0300

    Add a new empty plugin to mock Google Services.

--- tests/codeception/tests/search_console/SettingsCest.php
+++ tests/codeception/tests/search_console/SettingsCest.php
@@ -4,6 +4,15 @@ class SettingsCest {
 
 	public function _before( Search_ConsoleTester $I ) { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
 		$I->login( 'admin', 'password' );
+		$I->activate_plugins(
+			'google-site-kit-test-plugins/google-services-mock.php'
+		);
+	}
+
+	public function _after( Search_ConsoleTester $I ) { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+		$I->deactivate_plugins(
+			'google-site-kit-test-plugins/google-services-mock.php'
+		);
 	}
 
 	public function tryToTest( Search_ConsoleTester $I ) {
