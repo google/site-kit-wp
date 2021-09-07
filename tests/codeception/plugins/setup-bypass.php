a3b6a3b1d10fdf4b171e151ada0a20526ee15413
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-09-07 15:44:01 +0300

    Update settings test to correctly bypass authentication.

--- tests/codeception/plugins/setup-bypass.php
+++ tests/codeception/plugins/setup-bypass.php
@@ -11,3 +11,32 @@
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
+
+use Google\Site_Kit\Modules\Search_Console\Settings;
+
+add_filter(
+	'googlesitekit_proxy_setup_url_params',
+	function( $params ) {
+		add_filter(
+			'wp_redirect',
+			function() {
+				$url = add_query_arg(
+					array(
+						'oauth2callback'        => '1',
+						'code'                  => 'valid-test-code',
+						'e2e-site-verification' => '1',
+					),
+					admin_url( 'index.php' )
+				);
+
+				$settings               = get_option( Settings::OPTION );
+				$settings['propertyID'] = 'http://localhost:9002/';
+				update_option( Settings::OPTION, $settings );
+
+				return $url;
+			}
+		);
+
+		return $params;
+	}
+);
--- tests/codeception/tests/search_console/SettingsCest.php
+++ tests/codeception/tests/search_console/SettingsCest.php
@@ -5,12 +5,16 @@ class SettingsCest {
 	public function _before( Search_ConsoleTester $I ) { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
 		$I->login( 'admin', 'password' );
 		$I->activatePlugins(
+			'codeception-plugins/setup-bypass.php',
 			'google-site-kit-test-plugins/oauth-callback.php'
 		);
 	}
 
 	public function _after( Search_ConsoleTester $I ) { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
 		$I->deactivatePlugins(
+			'codeception-plugins/setup-bypass.php',
+			'google-site-kit-test-plugins/proxy-auth.php',
+			'google-site-kit-test-plugins/site-verification.php',
 			'google-site-kit-test-plugins/oauth-callback.php'
 		);
 	}
@@ -18,8 +22,8 @@ class SettingsCest {
 	public function tryToTestSettings( Search_ConsoleTester $I ) {
 		$I->amOnPage( '/wp-admin/admin.php?page=googlesitekit-splash' );
 		$I->click( '.googlesitekit-start-setup' );
-		$I->amOnPage( '/wp-admin/index.php?oauth2callback=1&code=valid-test-code&e2e-site-verification=1' );
 		$I->amOnPage( '/wp-admin/admin.php?page=googlesitekit-settings' );
+		$I->makeScreenshot();
 	}
 
 }
