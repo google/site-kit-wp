ca7d0bb310dc9c131e989211ab333b24443d2764
Ivan@Kruchkoff.com <ivan@kruchkoff.com>
2021-09-20 12:52:32 +0300

    Add manifest file.

---
+++ includes/Core/Assets/Manifest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Class Google\Site_Kit\Core\Assets\Manifest
+ *
+ * @package   GoogleSite_Kit
+ * @copyright 2021 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Core\Assets;
+
+/**
+ * Assets manifest.
+ *
+ * @since 1.15.0
+ * @access private
+ * @ignore
+ */
+class Manifest {
+
+	public static $assets = array(
+		"0"                                           => "0.js",
+		"googlesitekit-activation"                    => "googlesitekit-activation.js",
+		"googlesitekit-adminbar"                      => "googlesitekit-adminbar.js",
+		"googlesitekit-api"                           => "googlesitekit-api.js",
+		"googlesitekit-base"                          => "googlesitekit-base.js",
+		"googlesitekit-dashboard"                     => "googlesitekit-dashboard.js",
+		"googlesitekit-dashboard-details"             => "googlesitekit-dashboard-details.js",
+		"googlesitekit-dashboard-splash"              => "googlesitekit-dashboard-splash.js",
+		"googlesitekit-data"                          => "googlesitekit-data.js",
+		"googlesitekit-datastore-forms"               => "googlesitekit-datastore-forms.js",
+		"googlesitekit-datastore-location"            => "googlesitekit-datastore-location.js",
+		"googlesitekit-datastore-site"                => "googlesitekit-datastore-site.js",
+		"googlesitekit-datastore-ui"                  => "googlesitekit-datastore-ui.js",
+		"googlesitekit-datastore-user"                => "googlesitekit-datastore-user.js",
+		"googlesitekit-idea-hub-notice"               => "googlesitekit-idea-hub-notice.js",
+		"googlesitekit-idea-hub-post-list"            => "googlesitekit-idea-hub-post-list.js",
+		"googlesitekit-module"                        => "googlesitekit-module.js",
+		"googlesitekit-modules"                       => "googlesitekit-modules.js",
+		"googlesitekit-modules-adsense"               => "googlesitekit-modules-adsense.js",
+		"googlesitekit-modules-analytics"             => "googlesitekit-modules-analytics.js",
+		"googlesitekit-modules-analytics-4"           => "googlesitekit-modules-analytics-4.js",
+		"googlesitekit-modules-idea-hub"              => "googlesitekit-modules-idea-hub.js",
+		"googlesitekit-modules-optimize"              => "googlesitekit-modules-optimize.js",
+		"googlesitekit-modules-pagespeed-insights"    => "googlesitekit-modules-pagespeed-insights.js",
+		"googlesitekit-modules-search-console"        => "googlesitekit-modules-search-console.js",
+		"googlesitekit-modules-subscribe-with-google" => "googlesitekit-modules-subscribe-with-google.js",
+		"googlesitekit-modules-tagmanager"            => "googlesitekit-modules-tagmanager.js",
+		"googlesitekit-polyfills"                     => "googlesitekit-polyfills.js",
+		"googlesitekit-settings"                      => "googlesitekit-settings.js",
+		"googlesitekit-user-input"                    => "googlesitekit-user-input.js",
+		"googlesitekit-vendor"                        => "googlesitekit-vendor.js",
+		"googlesitekit-widgets"                       => "googlesitekit-widgets.js",
+		"googlesitekit-wp-dashboard"                  => "googlesitekit-wp-dashboard.js",
+		"runtime"                                     => "runtime.js",
+	);
+
+}
