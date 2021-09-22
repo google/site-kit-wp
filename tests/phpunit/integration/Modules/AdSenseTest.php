95272b22eda5c7dd075ccb2f88daee1ff8805153
Arafat Zahan <arafat.zahan@10up.com>
2021-09-22 23:00:12 +0600

    Add test for adsense platform meta tags.

--- tests/phpunit/integration/Modules/AdSenseTest.php
+++ tests/phpunit/integration/Modules/AdSenseTest.php
@@ -172,6 +172,58 @@ class AdSenseTest extends TestCase {
 	}
 
 	/**
+	 * @dataProvider module_is_connected
+	 * @param bool $connected
+	 */
+	public function test_adsense_platform_tags( $connected ) {
+		$adsense = new AdSense( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+
+		remove_all_actions( 'template_redirect' );
+		remove_all_actions( 'wp_head' );
+
+		$adsense->register();
+
+		do_action( 'template_redirect' );
+
+		if ( $connected ) {
+			$options = new Options( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+			$options->set(
+				Settings::OPTION,
+				array(
+					'accountSetupComplete' => true,
+					'siteSetupComplete'    => true,
+				)
+			);
+		}
+
+		$output = $this->capture_action( 'wp_head' );
+
+		if ( $connected ) {
+			$this->assertTrue( $adsense->is_connected() );
+		} else {
+			$this->assertFalse( $adsense->is_connected() );
+		}
+
+		$this->assertContains( 'google-adsense-platform-account', $output );
+		$this->assertContains( 'ca-host-pub-2644536267352236', $output );
+
+		$this->assertContains( 'google-adsense-platform-domain', $output );
+		$this->assertContains( 'sitekit.withgoogle.com', $output );
+
+	}
+
+	public function module_is_connected() {
+		return array(
+			'Not Connected' => array(
+				false,
+			),
+			'Connected'     => array(
+				true,
+			),
+		);
+	}
+
+	/**
 	 * @dataProvider block_on_consent_provider
 	 * @param bool $enabled
 	 */
