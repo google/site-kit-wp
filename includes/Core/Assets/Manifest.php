81933bc9a67f35ec58f235043af01c6c623111cf
Ivan@Kruchkoff.com <ivan@kruchkoff.com>
2021-09-28 13:50:09 +0300

    Update manifest manually.

--- includes/Core/Assets/Manifest.php
+++ includes/Core/Assets/Manifest.php
@@ -10,6 +10,8 @@
 
 namespace Google\Site_Kit\Core\Assets;
 
+use Google\Site_Kit\Plugin;
+
 /**
  * Assets manifest.
  *
@@ -19,41 +21,36 @@ namespace Google\Site_Kit\Core\Assets;
  */
 class Manifest {
 
-	public static $assets = array(
-		"0"                                           => "0.js",
-		"googlesitekit-activation"                    => "googlesitekit-activation.js",
-		"googlesitekit-adminbar"                      => "googlesitekit-adminbar.js",
-		"googlesitekit-api"                           => "googlesitekit-api.js",
-		"googlesitekit-base"                          => "googlesitekit-base.js",
-		"googlesitekit-dashboard"                     => "googlesitekit-dashboard.js",
-		"googlesitekit-dashboard-details"             => "googlesitekit-dashboard-details.js",
-		"googlesitekit-dashboard-splash"              => "googlesitekit-dashboard-splash.js",
-		"googlesitekit-data"                          => "googlesitekit-data.js",
-		"googlesitekit-datastore-forms"               => "googlesitekit-datastore-forms.js",
-		"googlesitekit-datastore-location"            => "googlesitekit-datastore-location.js",
-		"googlesitekit-datastore-site"                => "googlesitekit-datastore-site.js",
-		"googlesitekit-datastore-ui"                  => "googlesitekit-datastore-ui.js",
-		"googlesitekit-datastore-user"                => "googlesitekit-datastore-user.js",
-		"googlesitekit-idea-hub-notice"               => "googlesitekit-idea-hub-notice.js",
-		"googlesitekit-idea-hub-post-list"            => "googlesitekit-idea-hub-post-list.js",
-		"googlesitekit-module"                        => "googlesitekit-module.js",
-		"googlesitekit-modules"                       => "googlesitekit-modules.js",
-		"googlesitekit-modules-adsense"               => "googlesitekit-modules-adsense.js",
-		"googlesitekit-modules-analytics"             => "googlesitekit-modules-analytics.js",
-		"googlesitekit-modules-analytics-4"           => "googlesitekit-modules-analytics-4.js",
-		"googlesitekit-modules-idea-hub"              => "googlesitekit-modules-idea-hub.js",
-		"googlesitekit-modules-optimize"              => "googlesitekit-modules-optimize.js",
-		"googlesitekit-modules-pagespeed-insights"    => "googlesitekit-modules-pagespeed-insights.js",
-		"googlesitekit-modules-search-console"        => "googlesitekit-modules-search-console.js",
-		"googlesitekit-modules-subscribe-with-google" => "googlesitekit-modules-subscribe-with-google.js",
-		"googlesitekit-modules-tagmanager"            => "googlesitekit-modules-tagmanager.js",
-		"googlesitekit-polyfills"                     => "googlesitekit-polyfills.js",
-		"googlesitekit-settings"                      => "googlesitekit-settings.js",
-		"googlesitekit-user-input"                    => "googlesitekit-user-input.js",
-		"googlesitekit-vendor"                        => "googlesitekit-vendor.js",
-		"googlesitekit-widgets"                       => "googlesitekit-widgets.js",
-		"googlesitekit-wp-dashboard"                  => "googlesitekit-wp-dashboard.js",
-		"runtime"                                     => "runtime.js",
-	);
+	/**
+	 * Files as $handle => $filename map.
+	 *
+	 * @since n.e.x.t
+	 * @var array|null
+	 */
+	private static $assets;
 
+	/**
+	 * Gets the filename for a given handle.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $handle  Script or stylesheet handle.
+	 */
+	public static function get_filename( $handle ) {
+		if ( is_null( self::$assets ) ) {
+			$path = Plugin::instance()->context()->path( "dist/manifest.php" );
+			if ( file_exists( $path ) ) {
+				self::$assets = include $path;
+			}
+		}
+		if ( isset( self::$assets[ $handle ] ) ) {
+			return self::$assets[ $handle ];
+		}
+		$handle = str_replace( 'googlesitekit-', '', $handle );
+		if ( isset( self::$assets[ $handle ] ) ) {
+			return self::$assets[ $handle ];
+		}
+
+		return null;
+	}
 }
