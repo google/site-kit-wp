8977a023da96f3f05bf00cd7a89b5f166b5de7b3
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-11-01 12:49:19 +0200

    Add GA4Notice back to the AccountCreate component.

--- assets/js/modules/analytics/components/common/AccountCreate/index.js
+++ assets/js/modules/analytics/components/common/AccountCreate/index.js
@@ -44,6 +44,7 @@ import Button from '../../../../../components/Button';
 import Link from '../../../../../components/Link';
 import ProgressBar from '../../../../../components/ProgressBar';
 import StoreErrorNotices from '../../../../../components/StoreErrorNotices';
+import GA4Notice from '../GA4Notice';
 import GA4PropertyNotice from '../GA4PropertyNotice';
 import TimezoneSelect from './TimezoneSelect';
 import AccountField from './AccountField';
@@ -195,6 +196,8 @@ export default function AccountCreate() {
 
 	return (
 		<div>
+			{ ! isGA4enabled && <GA4Notice /> }
+
 			<StoreErrorNotices
 				moduleSlug="analytics"
 				storeName={ MODULES_ANALYTICS }
