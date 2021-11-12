cdc5295739fae61488871e7611cc3fd7446b74af
Evan Mattson <emattson@google.com>
2021-11-12 11:18:10 +0200

    Update PSI force-active tour to only show if data.

--- assets/js/feature-tours/pagespeed-insights-force-active.js
+++ assets/js/feature-tours/pagespeed-insights-force-active.js
@@ -28,6 +28,12 @@ import {
 	VIEW_CONTEXT_DASHBOARD,
 	VIEW_CONTEXT_PAGE_DASHBOARD,
 } from '../googlesitekit/constants';
+import { CORE_SITE } from '../googlesitekit/datastore/site/constants';
+import {
+	MODULES_PAGESPEED_INSIGHTS,
+	STRATEGY_DESKTOP,
+	STRATEGY_MOBILE,
+} from '../modules/pagespeed-insights/datastore/constants';
 
 const pagespeedInsightsForceActive = {
 	slug: 'pagespeedInsightsForceActive',
@@ -35,8 +41,32 @@ const pagespeedInsightsForceActive = {
 	version: '1.46.0',
 	gaEventCategory: ( viewContext ) =>
 		`${ viewContext }_pagespeed-widget-force-active`,
-	checkRequirements: () =>
-		global._googlesitekitBaseData.showPSIForceActiveTour,
+	checkRequirements: ( {
+		select,
+		__experimentalResolveSelect: resolveSelect,
+	} ) => {
+		if ( ! global._googlesitekitBaseData.showPSIForceActiveTour ) {
+			return false;
+		}
+
+		const referenceURL = select( CORE_SITE ).getCurrentReferenceURL();
+		const { getReport: asyncGetReport } = resolveSelect(
+			MODULES_PAGESPEED_INSIGHTS
+		);
+
+		// Only show the tour if reports are available within .5 sec.
+		return Promise.race( [
+			// If both reports finish first, the tour will be shown.
+			Promise.all( [
+				asyncGetReport( referenceURL, STRATEGY_DESKTOP ),
+				asyncGetReport( referenceURL, STRATEGY_MOBILE ),
+			] ).then( () => true ),
+			// If the timeout resolves first, the tour will not be shown.
+			new Promise( ( resolve ) => setTimeout( resolve, 500 ) ).then(
+				() => false
+			),
+		] );
+	},
 	steps: [
 		{
 			target: '.googlesitekit-widget-area--dashboardSpeed',
