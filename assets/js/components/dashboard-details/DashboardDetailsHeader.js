d185299e7c1289328cf1cacc96239d5205704b29
Arafat Zahan <arafat.zahan@10up.com>
2021-11-23 16:33:30 +0600

    Add period to the full sensentences.

--- assets/js/components/dashboard-details/DashboardDetailsHeader.js
+++ assets/js/components/dashboard-details/DashboardDetailsHeader.js
@@ -96,7 +96,7 @@ export default function DashboardDetailsHeader() {
 										sprintf(
 											/* translators: %s: current entity URL. */
 											__(
-												'It looks like the URL %s is not part of this site or is not based on standard WordPress content types, therefore there is no data available to display. Visit our <link1>support forums</link1> or <link2><VisuallyHidden>Site Kit </VisuallyHidden>website</link2> for support or further information',
+												'It looks like the URL %s is not part of this site or is not based on standard WordPress content types, therefore there is no data available to display. Visit our <link1>support forums</link1> or <link2><VisuallyHidden>Site Kit </VisuallyHidden>website</link2> for support or further information.',
 												'google-site-kit'
 											),
 											`<strong>${ permaLink }</strong>`
