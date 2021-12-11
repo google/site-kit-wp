89e2f7c39701dcc8237a565aa7133593c432088b
Jimmy Madon <hello@jimmymadon.com>
2021-12-11 16:17:30 +0000

    Refactor DashboardNav scrolling to use new util.

--- assets/js/components/DashboardNavigation.js
+++ assets/js/components/DashboardNavigation.js
@@ -58,40 +58,9 @@ import NavTrafficIcon from '../../svg/nav-traffic-icon.svg';
 import NavContentIcon from '../../svg/nav-content-icon.svg';
 import NavSpeedIcon from '../../svg/nav-speed-icon.svg';
 import NavMonetizationIcon from '../../svg/nav-monetization-icon.svg';
+import { getContextScrollTop } from '../util/scroll';
 
 const { useSelect } = Data;
-
-/**
- * Gets the y coordinate to scroll to the top of a context element, taking the sticky admin bar, header and navigation height into account.
- *
- * @since 1.47.0
- *
- * @param {string} contextID  The ID of the context element to scroll to.
- * @param {string} breakpoint The current breakpoint.
- * @return {number} The offset to scroll to.
- */
-const getContextScrollTop = ( contextID, breakpoint ) => {
-	const contextElement = document.getElementById( contextID );
-	if ( contextID === ANCHOR_ID_TRAFFIC || ! contextElement ) {
-		return 0;
-	}
-
-	const contextTop = contextElement.getBoundingClientRect().top;
-
-	const header = document.querySelector( '.googlesitekit-header' );
-
-	const hasStickyAdminBar = breakpoint !== 'small';
-
-	const headerHeight = hasStickyAdminBar
-		? header.getBoundingClientRect().bottom
-		: header.offsetHeight;
-
-	const navigationHeight = document.querySelector(
-		'.googlesitekit-navigation'
-	).offsetHeight;
-
-	return contextTop + global.scrollY - headerHeight - navigationHeight;
-};
 
 export default function DashboardNavigation() {
 	const dashboardType = useDashboardType();
@@ -141,7 +110,7 @@ export default function DashboardNavigation() {
 				global.history.replaceState( {}, '', `#${ hash }` );
 
 				global.scrollTo( {
-					top: getContextScrollTop( hash, breakpoint ),
+					top: getContextScrollTop( `#${ hash }`, breakpoint ),
 					behavior: 'smooth',
 				} );
 			} else {
@@ -161,7 +130,7 @@ export default function DashboardNavigation() {
 			setTimeout( () => {
 				global.scrollTo( {
 					top: getContextScrollTop(
-						global.location.hash.substr( 1 ),
+						global.location.hash,
 						breakpoint
 					),
 					behavior: 'smooth',
