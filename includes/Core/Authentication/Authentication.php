cf9badad4e23c528719aea864e5d74bc5ca6c890
Evan Mattson <emattson@google.com>
2021-12-16 12:27:12 +0200

    Remove Previous_Connected_Proxy_URL.

--- includes/Core/Authentication/Authentication.php
+++ includes/Core/Authentication/Authentication.php
@@ -188,14 +188,6 @@ final class Authentication {
 	protected $connected_proxy_url;
 
 	/**
-	 * Previous_Connected_Proxy_URL instance.
-	 *
-	 * @since n.e.x.t
-	 * @var Previous_Connected_Proxy_URL
-	 */
-	protected $previous_connected_proxy_url;
-
-	/**
 	 * Disconnected_Reason instance.
 	 *
 	 * @since 1.17.0
@@ -242,26 +234,25 @@ final class Authentication {
 		User_Options $user_options = null,
 		Transients $transients = null
 	) {
-		$this->context                      = $context;
-		$this->options                      = $options ?: new Options( $this->context );
-		$this->user_options                 = $user_options ?: new User_Options( $this->context );
-		$this->transients                   = $transients ?: new Transients( $this->context );
-		$this->user_input_state             = new User_Input_State( $this->user_options );
-		$this->user_input_settings          = new User_Input_Settings( $context, $this, $transients );
-		$this->google_proxy                 = new Google_Proxy( $this->context );
-		$this->credentials                  = new Credentials( new Encrypted_Options( $this->options ) );
-		$this->verification                 = new Verification( $this->user_options );
-		$this->verification_meta            = new Verification_Meta( $this->user_options );
-		$this->verification_file            = new Verification_File( $this->user_options );
-		$this->profile                      = new Profile( $this->user_options );
-		$this->token                        = new Token( $this->user_options );
-		$this->owner_id                     = new Owner_ID( $this->options );
-		$this->has_connected_admins         = new Has_Connected_Admins( $this->options, $this->user_options );
-		$this->has_multiple_admins          = new Has_Multiple_Admins( $this->transients );
-		$this->connected_proxy_url          = new Connected_Proxy_URL( $this->options );
-		$this->previous_connected_proxy_url = new Previous_Connected_Proxy_URL( $this->options );
-		$this->disconnected_reason          = new Disconnected_Reason( $this->user_options );
-		$this->initial_version              = new Initial_Version( $this->user_options );
+		$this->context              = $context;
+		$this->options              = $options ?: new Options( $this->context );
+		$this->user_options         = $user_options ?: new User_Options( $this->context );
+		$this->transients           = $transients ?: new Transients( $this->context );
+		$this->user_input_state     = new User_Input_State( $this->user_options );
+		$this->user_input_settings  = new User_Input_Settings( $context, $this, $transients );
+		$this->google_proxy         = new Google_Proxy( $this->context );
+		$this->credentials          = new Credentials( new Encrypted_Options( $this->options ) );
+		$this->verification         = new Verification( $this->user_options );
+		$this->verification_meta    = new Verification_Meta( $this->user_options );
+		$this->verification_file    = new Verification_File( $this->user_options );
+		$this->profile              = new Profile( $this->user_options );
+		$this->token                = new Token( $this->user_options );
+		$this->owner_id             = new Owner_ID( $this->options );
+		$this->has_connected_admins = new Has_Connected_Admins( $this->options, $this->user_options );
+		$this->has_multiple_admins  = new Has_Multiple_Admins( $this->transients );
+		$this->connected_proxy_url  = new Connected_Proxy_URL( $this->options );
+		$this->disconnected_reason  = new Disconnected_Reason( $this->user_options );
+		$this->initial_version      = new Initial_Version( $this->user_options );
 	}
 
 	/**
@@ -277,7 +268,6 @@ final class Authentication {
 		$this->has_connected_admins->register();
 		$this->owner_id->register();
 		$this->connected_proxy_url->register();
-		$this->previous_connected_proxy_url->register();
 		$this->disconnected_reason->register();
 		$this->user_input_state->register();
 		$this->initial_version->register();
@@ -920,7 +910,6 @@ final class Authentication {
 								'needsReauthentication' => $oauth_client->needs_reauthentication(),
 								'disconnectedReason'    => $this->disconnected_reason->get(),
 								'connectedProxyURL'     => $this->connected_proxy_url->get(),
-								'previousConnectedProxyURL' => $this->previous_connected_proxy_url->get(),
 							);
 
 							return new WP_REST_Response( $data );
@@ -1211,7 +1200,6 @@ final class Authentication {
 
 		$this->disconnect();
 		$this->disconnected_reason->set( Disconnected_Reason::REASON_CONNECTED_URL_MISMATCH );
-		$this->previous_connected_proxy_url->set( $this->connected_proxy_url->get() );
 	}
 
 	/**
