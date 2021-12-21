3c4dafba1b697515b7964f07435b6dbd5776740e
Evan Mattson <emattson@google.com>
2021-12-21 20:21:54 +0200

    Remove .min from js extension in production.

--- webpack.config.js
+++ webpack.config.js
@@ -247,14 +247,10 @@ function* webpackConfig( env, argv ) {
 		externals,
 		output: {
 			filename:
-				mode === 'production'
-					? '[name]-[contenthash].min.js'
-					: '[name].js',
+				mode === 'production' ? '[name]-[contenthash].js' : '[name].js',
 			path: path.join( __dirname, 'dist/assets/js' ),
 			chunkFilename:
-				mode === 'production'
-					? '[name]-[chunkhash].min.js'
-					: '[name].js',
+				mode === 'production' ? '[name]-[chunkhash].js' : '[name].js',
 			publicPath: '',
 			/*
 				If multiple webpack runtimes (from different compilations) are used on the
@@ -343,7 +339,7 @@ function* webpackConfig( env, argv ) {
 						name: 'googlesitekit-vendor',
 						filename:
 							mode === 'production'
-								? 'googlesitekit-vendor-[contenthash].min.js'
+								? 'googlesitekit-vendor-[contenthash].js'
 								: 'googlesitekit-vendor.js',
 						enforce: true,
 						test: /[\\/]node_modules[\\/]/,
@@ -369,9 +365,7 @@ function* webpackConfig( env, argv ) {
 		externals,
 		output: {
 			filename:
-				mode === 'production'
-					? '[name]-[contenthash].min.js'
-					: '[name].js',
+				mode === 'production' ? '[name]-[contenthash].js' : '[name].js',
 			path: __dirname + '/dist/assets/js',
 			publicPath: '',
 		},
