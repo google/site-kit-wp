0dbc2333e1493d70c99ec4baf832056f16c0f97a
Crisoforo Gaspar <crisoforo.gasparhernandez@10up.com>
2022-01-11 12:41:02 -0600

    Update file description to include, package and license.

--- tests/phpunit/integration/Core/Permissions/PermissionsTest.php
+++ tests/phpunit/integration/Core/Permissions/PermissionsTest.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Class Google\Site_Kit\Tests\Core\Permissions\PermissionsTest
+ *
+ * @package   Google\Site_Kit\Tests\Core\Permissions
+ * @copyright 2022 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
 namespace Google\Site_Kit\Tests\Core\Permissions;
 
 use Google\Site_Kit\Context;
