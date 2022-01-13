1a1ab1910b1b5b00d04d2c287e2db053434e6fa9
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2022-01-13 13:54:19 +0000

    Simplify tests/e2e/script.js.

--- tests/e2e/script.js
+++ tests/e2e/script.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * This file was originally copied from the @wordpress/scripts package, version 12.0.0:
  * https://github.com/WordPress/gutenberg/blob/8e06f0d212f89adba9099106497117819adefc5a/packages/scripts/scripts/test-e2e.js
@@ -23,9 +25,6 @@ process.on( 'unhandledRejection', ( err ) => {
 /* eslint-disable-next-line jest/no-jest-import */
 const jest = require( 'jest' );
 const { sync: spawn } = require( 'cross-spawn' );
-const { existsSync, realpathSync } = require( 'fs' );
-const path = require( 'path' );
-const { sync: readPkgUp } = require( 'read-pkg-up' );
 
 // getArgsFromCLI inlined from @wordpress/scripts utils/process.js v12.0.0.
 // https://github.com/WordPress/gutenberg/blob/8e06f0d212f89adba9099106497117819adefc5a/packages/scripts/utils/process.js#L1-L11
@@ -42,31 +41,6 @@ const getArgsFromCLI = ( excludePrefixes ) => {
 	return args;
 };
 
-const getCurrentWorkingDirectory = process.cwd;
-
-// getPackagePath and hasPackageProp inlined from @wordpress/scripts utils/package.js v12.0.0.
-// https://github.com/WordPress/gutenberg/blob/8e06f0d212f89adba9099106497117819adefc5a/packages/scripts/utils/package.js#L12-L18
-
-const { pkg, path: pkgPath } = readPkgUp( {
-	cwd: realpathSync( getCurrentWorkingDirectory() ),
-} );
-
-const getPackagePath = () => pkgPath;
-
-const hasPackageProp = ( prop ) => pkg && pkg.hasOwnProperty( prop );
-
-// fromProjectRoot, hasProjectFile and fromConfigRoot inlined from @wordpress/scripts utils/file.js v12.0.0.
-// https://github.com/WordPress/gutenberg/blob/8e06f0d212f89adba9099106497117819adefc5a/packages/scripts/utils/file.js#L12-L19
-
-const fromProjectRoot = ( fileName ) =>
-	path.join( path.dirname( getPackagePath() ), fileName );
-
-const hasProjectFile = ( fileName ) =>
-	existsSync( fromProjectRoot( fileName ) );
-
-const fromConfigRoot = ( fileName ) =>
-	path.join( path.dirname( __dirname ), 'config', fileName );
-
 // getArgFromCLI and hasArgInCLI inlined from @wordpress/scripts utils/cli.js v12.0.0.
 // https://github.com/WordPress/gutenberg/blob/8e06f0d212f89adba9099106497117819adefc5a/packages/scripts/utils/cli.js#L13-L22
 
@@ -79,28 +53,6 @@ const getArgFromCLI = ( arg ) => {
 	}
 };
 
-// getJestOverrideConfigFile and hasJestConfig inlined from @wordpress/scripts utils/config.js v12.0.0.
-// https://github.com/WordPress/gutenberg/blob/8e06f0d212f89adba9099106497117819adefc5a/packages/scripts/utils/config.js#L41-L58
-
-function getJestOverrideConfigFile( suffix ) {
-	if ( hasArgInCLI( '-c' ) || hasArgInCLI( '--config' ) ) {
-		return;
-	}
-
-	if ( hasProjectFile( `jest-${ suffix }.config.js` ) ) {
-		return fromProjectRoot( `jest-${ suffix }.config.js` );
-	}
-
-	if ( ! hasJestConfig() ) {
-		return fromConfigRoot( `jest-${ suffix }.config.js` );
-	}
-}
-
-const hasJestConfig = () =>
-	hasProjectFile( 'jest.config.js' ) ||
-	hasProjectFile( 'jest.config.json' ) ||
-	hasPackageProp( 'jest' );
-
 const hasArgInCLI = ( arg ) => getArgFromCLI( arg ) !== undefined;
 
 const result = spawn( 'node', [ require.resolve( 'puppeteer/install' ) ], {
@@ -111,21 +63,7 @@ if ( result.status > 0 ) {
 	process.exit( result.status );
 }
 
-// Provides a default config path for Puppeteer when jest-puppeteer.config.js
-// wasn't found at the root of the project or a custom path wasn't defined
-// using JEST_PUPPETEER_CONFIG environment variable.
-if (
-	! hasProjectFile( 'jest-puppeteer.config.js' ) &&
-	! process.env.JEST_PUPPETEER_CONFIG
-) {
-	process.env.JEST_PUPPETEER_CONFIG = fromConfigRoot( 'puppeteer.config.js' );
-}
-
-const configFile = getJestOverrideConfigFile( 'e2e' );
-
-const config = configFile
-	? [ '--config', JSON.stringify( require( configFile ) ) ]
-	: [];
+const config = [ '--config=tests/e2e/jest.config.js' ];
 
 const hasRunInBand = hasArgInCLI( '--runInBand' ) || hasArgInCLI( '-i' );
 const runInBand = ! hasRunInBand ? [ '--runInBand' ] : [];
