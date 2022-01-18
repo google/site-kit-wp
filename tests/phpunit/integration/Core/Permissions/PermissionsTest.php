c82db72b6896204007b12eca063d6bec02370f6d
Evan Mattson <emattson@google.com>
2022-01-18 19:01:06 +0200

    Restore correct package in file header.

--- tests/phpunit/integration/Core/Permissions/PermissionsTest.php
+++ tests/phpunit/integration/Core/Permissions/PermissionsTest.php
@@ -2,7 +2,7 @@
 /**
  * Class Google\Site_Kit\Tests\Core\Permissions\PermissionsTest
  *
- * @package   Google\Site_Kit
+ * @package   Google\Site_Kit\Tests\Core\Permissions
  * @copyright 2022 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
