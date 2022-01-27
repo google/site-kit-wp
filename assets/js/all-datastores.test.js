0614aab39f047771f5974e3fb378e9d1f6c460ab
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-01-27 21:44:15 +0530

    Another testing.

--- assets/js/all-datastores.test.js
+++ assets/js/all-datastores.test.js
@@ -42,16 +42,6 @@ describe( 'all data stores', () => {
 
 				expect( listener ).not.toHaveBeenCalled();
 			} );
-			it( 'Sample testing 4577', () => {
-				const listener = jest.fn();
-				registry.subscribe( listener );
-
-				registry.stores[ storeName ].store.dispatch( {
-					type: '@@NON_EXISTENT_ACTION_TYPE@@',
-				} );
-
-				expect( listener ).not.toHaveBeenCalled();
-			} );
 		} );
 	} );
 } );
