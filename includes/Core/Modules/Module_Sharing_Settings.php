b187e959c6a75718f4a1060746d85c17699e9109
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-02-11 14:17:44 +0530

    Add get_shared_roles() method.

--- includes/Core/Modules/Module_Sharing_Settings.php
+++ includes/Core/Modules/Module_Sharing_Settings.php
@@ -138,4 +138,23 @@ class Module_Sharing_Settings extends Setting {
 			$this->set( $settings );
 		}
 	}
+
+	/**
+	 * Gets the shared roles for the given module slug.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $slug Module slug.
+	 *
+	 * @return array list of shared roles for the module, otherwise an empty list.
+	 */
+	public function get_shared_roles( $slug ) {
+		$settings = $this->get();
+
+		if ( isset( $settings[ $slug ]['sharedRoles'] ) ) {
+			return $settings[ $slug ]['sharedRoles'];
+		}
+
+		return array();
+	}
 }
