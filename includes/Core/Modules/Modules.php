ff62bf7b6c65f5aeed1adaa036a3afc09cb143cc
Asvin Balloo <asvin.balloo@gmail.com>
2022-02-24 16:48:13 +0400

    Remove uneeded condition.

--- includes/Core/Modules/Modules.php
+++ includes/Core/Modules/Modules.php
@@ -813,6 +813,47 @@ final class Modules {
 				)
 			),
 			new REST_Route(
+				'core/modules/data/check-access',
+				array(
+					array(
+						'methods'             => WP_REST_Server::EDITABLE,
+						'callback'            => function( WP_REST_Request $request ) {
+							$slug = $request['slug'];
+
+							try {
+								$module = $this->get_module( $slug );
+							} catch ( Exception $e ) {
+								return new WP_Error( 'invalid_module_slug', __( 'Invalid module slug.', 'google-site-kit' ), array( 'status' => 404 ) );
+							}
+
+							if ( ! $this->is_module_connected( $slug ) ) {
+								return new WP_Error( 'module_not_connected', __( 'Module is not connected.', 'google-site-kit' ), array( 'status' => 400 ) );
+							}
+
+							$access = $module->check_service_entity_access();
+
+							if ( is_wp_error( $access ) ) {
+								return $access;
+							}
+
+							return new WP_REST_Response(
+								array(
+									'access' => $access,
+								)
+							);
+						},
+						'permission_callback' => $can_setup,
+						'args'                => array(
+							'slug' => array(
+								'type'              => 'string',
+								'description'       => __( 'Identifier for the module.', 'google-site-kit' ),
+								'sanitize_callback' => 'sanitize_key',
+							),
+						),
+					),
+				)
+			),
+			new REST_Route(
 				'modules/(?P<slug>[a-z0-9\-]+)/data/notifications',
 				array(
 					array(
@@ -968,47 +1009,6 @@ final class Modules {
 							'type'              => 'string',
 							'description'       => __( 'Module data point to address.', 'google-site-kit' ),
 							'sanitize_callback' => 'sanitize_key',
-						),
-					),
-				)
-			),
-			new REST_Route(
-				'core/modules/data/check-access',
-				array(
-					array(
-						'methods'             => WP_REST_Server::EDITABLE,
-						'callback'            => function( WP_REST_Request $request ) {
-							$slug = $request['slug'];
-
-							try {
-								$module = $this->get_module( $slug );
-							} catch ( Exception $e ) {
-								return new WP_Error( 'invalid_module_slug', __( 'Invalid module slug.', 'google-site-kit' ), array( 'status' => 404 ) );
-							}
-
-							if ( ! $this->is_module_connected( $slug ) ) {
-								return new WP_Error( 'module_not_connected', __( 'Module is not connected.', 'google-site-kit' ), array( 'status' => 400 ) );
-							}
-
-							$access = $module->check_service_entity_access();
-
-							if ( is_wp_error( $access ) && ! is_bool( $access ) ) {
-								return $access;
-							}
-
-							return new WP_REST_Response(
-								array(
-									'access' => $access,
-								)
-							);
-						},
-						'permission_callback' => $can_setup,
-						'args'                => array(
-							'slug' => array(
-								'type'              => 'string',
-								'description'       => __( 'Identifier for the module.', 'google-site-kit' ),
-								'sanitize_callback' => 'sanitize_key',
-							),
 						),
 					),
 				)
