46eb034827804846247cf3fa19be5d2fb066f0a0
Jimmy Madon <hello@jimmymadon.com>
2022-03-18 03:08:48 +0000

    Add datastore partial to get AdSense site information.

---
+++ assets/js/modules/adsense/datastore/sites.test.js
@@ -0,0 +1,218 @@
+/**
+ * `modules/adsense` data store: sites tests.
+ *
+ * Site Kit by Google, Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import API from 'googlesitekit-api';
+import { MODULES_ADSENSE } from './constants';
+import { CORE_SITE } from '../../../googlesitekit/datastore/site/constants';
+import {
+	createTestRegistry,
+	subscribeUntil,
+	unsubscribeFromAll,
+} from '../../../../../tests/js/utils';
+import * as fixtures from './__fixtures__';
+
+describe( 'modules/adsense sites', () => {
+	let registry;
+
+	beforeAll( () => {
+		API.setUsingCache( false );
+	} );
+
+	beforeEach( () => {
+		registry = createTestRegistry();
+	} );
+
+	afterAll( () => {
+		API.setUsingCache( true );
+	} );
+
+	afterEach( () => {
+		unsubscribeFromAll( registry );
+	} );
+
+	describe( 'actions', () => {} );
+
+	describe( 'selectors', () => {
+		describe( 'getSites', () => {
+			it( 'uses a resolver to make a network request', async () => {
+				fetchMock.getOnce(
+					/^\/google-site-kit\/v1\/modules\/adsense\/data\/sites/,
+					{ body: fixtures.sites, status: 200 }
+				);
+
+				const accountID = fixtures.clients[ 0 ]._accountID;
+
+				const initialSites = registry
+					.select( MODULES_ADSENSE )
+					.getSites( accountID );
+
+				expect( initialSites ).toEqual( undefined );
+				await subscribeUntil(
+					registry,
+					() =>
+						registry
+							.select( MODULES_ADSENSE )
+							.getSites( accountID ) !== undefined
+				);
+
+				const sites = registry
+					.select( MODULES_ADSENSE )
+					.getSites( accountID );
+
+				expect( fetchMock ).toHaveFetchedTimes( 1 );
+				expect( sites ).toEqual( fixtures.sites );
+			} );
+
+			it( 'does not make a network request if sites for this account are already present', async () => {
+				const accountID = fixtures.clients[ 0 ]._accountID;
+
+				// Load data into this store so there are matches for the data we're about to select,
+				// even though the selector hasn't fulfilled yet.
+				registry
+					.dispatch( MODULES_ADSENSE )
+					.receiveGetSites( fixtures.sites, { accountID } );
+
+				const sites = registry
+					.select( MODULES_ADSENSE )
+					.getSites( accountID );
+
+				await subscribeUntil( registry, () =>
+					registry
+						.select( MODULES_ADSENSE )
+						.hasFinishedResolution( 'getSites', [ accountID ] )
+				);
+
+				expect( fetchMock ).not.toHaveFetched();
+				expect( sites ).toEqual( fixtures.sites );
+			} );
+
+			it( 'dispatches an error if the request fails', async () => {
+				const response = {
+					code: 'internal_server_error',
+					message: 'Internal server error',
+					data: { status: 500 },
+				};
+				fetchMock.getOnce(
+					/^\/google-site-kit\/v1\/modules\/adsense\/data\/sites/,
+					{ body: response, status: 500 }
+				);
+
+				const fakeAccountID = 'pub-777888999';
+				registry.select( MODULES_ADSENSE ).getSites( fakeAccountID );
+				await subscribeUntil(
+					registry,
+					() =>
+						registry
+							.select( MODULES_ADSENSE )
+							.isFetchingGetSites( fakeAccountID ) === false
+				);
+
+				expect( fetchMock ).toHaveFetchedTimes( 1 );
+
+				const sites = registry
+					.select( MODULES_ADSENSE )
+					.getSites( fakeAccountID );
+				expect( sites ).toEqual( undefined );
+				expect( console ).toHaveErrored();
+			} );
+		} );
+		describe( 'getSite', () => {
+			beforeEach( () => {
+				const accountID = fixtures.clients[ 0 ]._accountID;
+
+				// Load data into this store so there are matches for the data we're about to select,
+				// even though the selector hasn't fulfilled yet.
+				registry
+					.dispatch( MODULES_ADSENSE )
+					.receiveGetSites( fixtures.sites, { accountID } );
+			} );
+			it.each( [
+				[ 'www.example.com', fixtures.sites[ 0 ] ],
+				[ 'othersubdomain.example.com', fixtures.sites[ 0 ] ],
+				[ 'www.test-site.com', fixtures.sites[ 1 ] ],
+				[ 'some-other-tld.ie', fixtures.sites[ 2 ] ],
+			] )(
+				'finds the site in this account that matches the domain: %s',
+				async ( domain, expected ) => {
+					const accountID = fixtures.clients[ 0 ]._accountID;
+					const site = registry
+						.select( MODULES_ADSENSE )
+						.getSite( accountID, domain );
+					expect( site ).toEqual( expected );
+				}
+			);
+			it.each( [
+				[ 'non-existent-site.com' ],
+				[ 'some-other-tld.com' ],
+			] )(
+				'returns null when no site matches the given domain: %s',
+				async ( domain ) => {
+					const accountID = fixtures.clients[ 0 ]._accountID;
+					const site = registry
+						.select( MODULES_ADSENSE )
+						.getSite( accountID, domain );
+					expect( site ).toEqual( null );
+				}
+			);
+			it( 'returns null when no site matches the given domain', async () => {
+				const accountID = fixtures.clients[ 0 ]._accountID;
+
+				const site = registry
+					.select( MODULES_ADSENSE )
+					.getSite( accountID, 'www.non-existent-url.com' );
+				expect( site ).toEqual( null );
+			} );
+		} );
+		describe( 'getCurrentSite', () => {
+			beforeEach( () => {
+				const accountID = fixtures.clients[ 0 ]._accountID;
+
+				// Load data into this store so there are matches for the data we're about to select,
+				// even though the selector hasn't fulfilled yet.
+				registry
+					.dispatch( MODULES_ADSENSE )
+					.receiveGetSites( fixtures.sites, { accountID } );
+			} );
+			it( 'gets the AdSense site which matches the domain of the current site', async () => {
+				const accountID = fixtures.clients[ 0 ]._accountID;
+
+				await registry.dispatch( CORE_SITE ).receiveSiteInfo( {
+					referenceSiteURL: 'http://example.com',
+				} );
+				const site = registry
+					.select( MODULES_ADSENSE )
+					.getCurrentSite( accountID );
+				expect( site ).toEqual( fixtures.sites[ 0 ] );
+			} );
+			it( 'returns null when the current site domain does not matches any site', async () => {
+				const accountID = fixtures.clients[ 0 ]._accountID;
+
+				await registry.dispatch( CORE_SITE ).receiveSiteInfo( {
+					referenceSiteURL: 'http://non-existent-site.com',
+				} );
+				const site = registry
+					.select( MODULES_ADSENSE )
+					.getCurrentSite( accountID );
+				expect( site ).toEqual( null );
+			} );
+		} );
+	} );
+} );
