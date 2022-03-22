15575570d183fa3eb3301956211344145fbd3953
Matthew Riley MacPherson <hi@tofumatt.com>
2022-03-22 19:10:32 +0000

    Update assets/js/components/ActivateAnalyticsCTA.js

--- assets/js/components/ActivateAnalyticsCTA.js
+++ assets/js/components/ActivateAnalyticsCTA.js
@@ -32,9 +32,6 @@ import { __ } from '@wordpress/i18n';
  */
 import useActivateModuleCallback from '../hooks/useActivateModuleCallback';
 import Button from './Button';
-// import PreviewGraph from './PreviewGraph';
-// import VisitorsGraph from '../../svg/graphics/cta-graph-visitors.svg';
-// import GoalsGraph from '../../svg/graphics/cta-graph-goals.svg';
 
 export default function ActivateAnalyticsCTA( { children } ) {
 	const activateModuleCallback = useActivateModuleCallback( 'analytics' );
