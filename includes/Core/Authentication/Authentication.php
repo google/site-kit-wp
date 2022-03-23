d7aec2af7ad7154d93cb04219496fd6fa9668d53
Evan Mattson <emattson@google.com>
2022-03-23 15:53:06 -0400

    Fix guard for fetching remote features.

--- includes/Core/Authentication/Authentication.php
+++ includes/Core/Authentication/Authentication.php
@@ -1371,10 +1371,8 @@ final class Authentication {
 	 * @return boolean State flag from the proxy server if it is available, otherwise the original value.
 	 */
 	private function filter_features_via_proxy( $feature_enabled, $feature_name ) {
-		$remote_features_option       = 'googlesitekitpersistent_remote_features';
-		$service_setup_v2_option_name = 'googlesitekitpersistent_service_setup_v2_enabled';
-
-		$features = $this->options->get( $remote_features_option );
+		$remote_features_option = 'googlesitekitpersistent_remote_features';
+		$features               = $this->options->get( $remote_features_option );
 
 		if ( false === $features ) {
 			// The experimental features (ideaHubModule and swgModule) are checked within Modules::construct() which
@@ -1398,13 +1396,9 @@ final class Authentication {
 				}
 			}
 
+			// Don't attempt to fetch features if the site is not connected yet.
 			if ( ! $this->credentials->has() ) {
-				// Use a persistent option so that it remains active even if the
-				// site is not connected. This is crucial to provide a
-				// consistent setup flow experience.
-				if ( $this->options->get( $service_setup_v2_option_name ) ) {
-					return true;
-				}
+				return $feature_enabled;
 			}
 
 			$features = $this->fetch_remote_features();
