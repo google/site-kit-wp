0f36716c04edb54bd4b4964e384da0c2c9ef16ba
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2022-03-30 13:10:12 +0100

    Add priority to scannable-urls request intercept.

--- tests/e2e/utils/activate-amp-and-set-mode.js
+++ tests/e2e/utils/activate-amp-and-set-mode.js
@@ -62,19 +62,22 @@ export const setAMPMode = async ( mode ) => {
 	useRequestInterception( ( request ) => {
 		// Mock the scannable-urls request to avoid occasional failing requests resulting in console errors.
 		if ( request.url().match( 'scannable-urls' ) ) {
-			request.respond( {
-				status: 200,
-				body: JSON.stringify( [
-					{
-						url: 'http://localhost:9002/',
-						type: 'is_home',
-						label: 'Homepage',
-						amp_url: 'http://localhost:9002/?amp=1',
-						validation_errors: [],
-						stale: false,
-					},
-				] ),
-			} );
+			request.respond(
+				{
+					status: 200,
+					body: JSON.stringify( [
+						{
+							url: 'http://localhost:9002/',
+							type: 'is_home',
+							label: 'Homepage',
+							amp_url: 'http://localhost:9002/?amp=1',
+							validation_errors: [],
+							stale: false,
+						},
+					] ),
+				},
+				2
+			);
 		} else {
 			request.continue( {}, 1 );
 		}
