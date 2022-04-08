8e625dcf967c662cb082a234d15f6c0279510db6
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-04-08 15:34:01 +0530

    Render zero notification if either one of the modules is in that state.

--- assets/js/components/notifications/ZeroDataStateNotifications.js
+++ assets/js/components/notifications/ZeroDataStateNotifications.js
@@ -113,28 +113,26 @@ export default function ZeroDataStateNotifications() {
 				/>
 			) }
 
-			{ analyticsGatheringData === false &&
-				searchConsoleGatheringData === false &&
-				( analyticsHasZeroData || searchConsoleHasZeroData ) && (
-					<BannerNotification
-						id="zero-data-notification"
-						title={ __(
-							'Not enough traffic yet to display stats',
-							'google-site-kit'
-						) }
-						description={ __(
-							'Site Kit will start showing stats on the dashboard as soon as enough people have visited your site. Keep working on your site to attract more visitors.',
-							'google-site-kit'
-						) }
-						format="small"
-						learnMoreLabel={ __( 'Learn more', 'google-site-kit' ) }
-						learnMoreURL="https://sitekit.withgoogle.com/documentation/using-site-kit/dashboard-data-display/"
-						dismiss={ __( 'Remind me later', 'google-site-kit' ) }
-						isDismissible
-						dismissExpires={ getTimeInSeconds( 'day' ) }
-						SmallImageSVG={ ZeroStateIcon }
-					/>
-				) }
+			{ ( analyticsHasZeroData || searchConsoleHasZeroData ) && (
+				<BannerNotification
+					id="zero-data-notification"
+					title={ __(
+						'Not enough traffic yet to display stats',
+						'google-site-kit'
+					) }
+					description={ __(
+						'Site Kit will start showing stats on the dashboard as soon as enough people have visited your site. Keep working on your site to attract more visitors.',
+						'google-site-kit'
+					) }
+					format="small"
+					learnMoreLabel={ __( 'Learn more', 'google-site-kit' ) }
+					learnMoreURL="https://sitekit.withgoogle.com/documentation/using-site-kit/dashboard-data-display/"
+					dismiss={ __( 'Remind me later', 'google-site-kit' ) }
+					isDismissible
+					dismissExpires={ getTimeInSeconds( 'day' ) }
+					SmallImageSVG={ ZeroStateIcon }
+				/>
+			) }
 		</Fragment>
 	);
 }
