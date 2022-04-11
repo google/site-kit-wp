860847c81e9d309ac68706a91cf7dab5172efce2
Evan Mattson <emattson@google.com>
2022-04-11 16:10:53 -0400

    Update triggerSurvey to wait for authentication to be resolved before selecting.

--- assets/js/googlesitekit/datastore/user/surveys.test.js
+++ assets/js/googlesitekit/datastore/user/surveys.test.js
@@ -101,6 +101,30 @@ describe( 'core/user surveys', () => {
 				} );
 			} );
 
+			it( 'waits for authentication to be resolved before making a network request', async () => {
+				muteFetch( surveyTriggerEndpoint );
+				fetchMock.getOnce(
+					/^\/google-site-kit\/v1\/core\/user\/data\/authentication/,
+					{
+						authenticated: true,
+					}
+				);
+				const triggerSurveyPromise = registry
+					.dispatch( CORE_USER )
+					.triggerSurvey( 'optimizeSurvey' );
+
+				expect( fetchMock ).not.toHaveFetched( surveyTriggerEndpoint );
+
+				provideUserAuthentication( registry );
+
+				await triggerSurveyPromise;
+				expect( fetchMock ).toHaveFetched( surveyTriggerEndpoint, {
+					body: {
+						data: { triggerID: 'optimizeSurvey' },
+					},
+				} );
+			} );
+
 			it( 'makes network requests to survey endpoint', async () => {
 				provideUserAuthentication( registry );
 				muteFetch( surveyTriggerEndpoint );
