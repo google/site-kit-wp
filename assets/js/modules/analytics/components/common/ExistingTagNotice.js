c73ed8ae1bce841a59d2f5cad11609245a826218
Arafat Zahan <arafat.zahan@10up.com>
2022-04-14 06:24:09 +0600

    Remove ga4 existing tag checks on the `ExistingTagNotice`.

--- assets/js/modules/analytics/components/common/ExistingTagNotice.js
+++ assets/js/modules/analytics/components/common/ExistingTagNotice.js
@@ -26,7 +26,6 @@ import { sprintf, __ } from '@wordpress/i18n';
  */
 import Data from 'googlesitekit-data';
 import { MODULES_ANALYTICS } from '../../datastore/constants';
-import { MODULES_ANALYTICS_4 } from '../../../analytics-4/datastore/constants';
 import { MODULES_TAGMANAGER } from '../../../tagmanager/datastore/constants';
 const { useSelect } = Data;
 
@@ -35,78 +34,12 @@ export default function ExistingTagNotice() {
 		select( MODULES_ANALYTICS ).getPropertyID()
 	);
 
-	const hasGA4ExistingTag = useSelect( ( select ) =>
-		select( MODULES_ANALYTICS_4 ).hasExistingTag()
-	);
-
-	const ga4ExistingTag = useSelect( ( select ) =>
-		select( MODULES_ANALYTICS_4 ).getExistingTag()
-	);
-
-	const measurementID = useSelect( ( select ) =>
-		select( MODULES_ANALYTICS_4 ).getMeasurementID()
-	);
-
 	const gtmAnalyticsPropertyID = useSelect( ( select ) =>
 		select( MODULES_TAGMANAGER ).getSingleAnalyticsPropertyID()
 	);
 
-	const hasGTMAnalyticsProperty = !! gtmAnalyticsPropertyID;
-
-	if ( ! hasGTMAnalyticsProperty ) {
+	if ( ! gtmAnalyticsPropertyID ) {
 		return null;
-	}
-
-	function getNoticeForExistingGTMPropertyAndGA4Tag() {
-		if (
-			gtmAnalyticsPropertyID === propertyID &&
-			ga4ExistingTag === measurementID
-		) {
-			return sprintf(
-				/* translators: %1$s: GTM property ID, %2$s: Analytics 4 measurement ID */
-				__(
-					'An existing Google Tag Manager property with the ID %1$s and an existing Google Analytics 4 tag with the ID %2$s were found on your site. Since they refer to the same properties selected here, Site Kit will not place its own tags and rely on the existing ones. If later on you decide to remove this property and/or tag, Site Kit can place new tags for you.',
-					'google-site-kit'
-				),
-				gtmAnalyticsPropertyID,
-				ga4ExistingTag
-			);
-		} else if (
-			gtmAnalyticsPropertyID === propertyID &&
-			ga4ExistingTag !== measurementID
-		) {
-			return sprintf(
-				/* translators: %1$s: GTM property ID, %2$s: Analytics 4 measurement ID */
-				__(
-					'An existing Google Tag Manager property with the ID %1$s and an existing Google Analytics 4 tag with the ID %2$s were found on your site. Since the Google Tag Manager property refers to the same property selected here, Site Kit will not place its own tag and rely on the existing one.',
-					'google-site-kit'
-				),
-				gtmAnalyticsPropertyID,
-				ga4ExistingTag
-			);
-		} else if (
-			gtmAnalyticsPropertyID !== propertyID &&
-			ga4ExistingTag === measurementID
-		) {
-			return sprintf(
-				/* translators: %1$s: GTM property ID, %2$s: Analytics 4 measurement ID */
-				__(
-					'An existing Google Tag Manager property with the ID %1$s and an existing Google Analytics 4 tag with the ID %2$s were found on your site. Since the Google Analytics 4 tag refers to the same property selected here, Site Kit will not place its own tag and rely on the existing one. If later on you decide to remove this tag, Site Kit can place a new tag for you.',
-					'google-site-kit'
-				),
-				gtmAnalyticsPropertyID,
-				ga4ExistingTag
-			);
-		}
-		return sprintf(
-			/* translators: %1$s: GTM property ID, %2$s: Analytics 4 measurement ID */
-			__(
-				'An existing Google Tag Manager property with the ID %1$s and an existing Google Analytics 4 tag with the ID %2$s were found on your site.',
-				'google-site-kit'
-			),
-			gtmAnalyticsPropertyID,
-			ga4ExistingTag
-		);
 	}
 
 	function getNoticeForExistingGTMProperty() {
@@ -130,9 +63,5 @@ export default function ExistingTagNotice() {
 		);
 	}
 
-	const notice = hasGA4ExistingTag
-		? getNoticeForExistingGTMPropertyAndGA4Tag()
-		: getNoticeForExistingGTMProperty();
-
-	return <p>{ notice }</p>;
+	return <p>{ getNoticeForExistingGTMProperty() }</p>;
 }
