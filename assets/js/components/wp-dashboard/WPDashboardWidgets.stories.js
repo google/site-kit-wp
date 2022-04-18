bbc7755a9975a78c4b60afcaae9cf620bef59783
Jimmy Madon <hello@jimmymadon.com>
2022-04-18 13:08:22 +0200

    Refactor and add ZDS stories for WPDashboard.

--- assets/js/components/wp-dashboard/WPDashboardWidgets.stories.js
+++ assets/js/components/wp-dashboard/WPDashboardWidgets.stories.js
@@ -27,82 +27,13 @@ import {
 	provideModuleRegistrations,
 } from '../../../../tests/js/utils';
 import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
-import { provideSearchConsoleMockReport } from '../../modules/search-console/util/data-mock';
-import { provideAnalyticsMockReport } from '../../modules/analytics/util/data-mock';
-
-const searchConsoleArgs = {
-	startDate: '2021-08-18',
-	endDate: '2021-10-12',
-	dimensions: 'date',
-};
-
-const analyticsArgs = [
-	{
-		startDate: '2021-09-15',
-		endDate: '2021-10-12',
-		compareStartDate: '2021-08-18',
-		compareEndDate: '2021-09-14',
-		metrics: [
-			{
-				expression: 'ga:goalCompletionsAll',
-				alias: 'Goal Completions',
-			},
-			'ga:bounceRate',
-		],
-	},
-	{
-		startDate: '2021-09-15',
-		endDate: '2021-10-12',
-		compareStartDate: '2021-08-18',
-		compareEndDate: '2021-09-14',
-		dimensions: 'ga:date',
-		metrics: [
-			{
-				expression: 'ga:goalCompletionsAll',
-				alias: 'Goal Completions',
-			},
-			'ga:bounceRate',
-		],
-	},
-	{
-		startDate: '2021-09-15',
-		endDate: '2021-10-12',
-		compareStartDate: '2021-08-18',
-		compareEndDate: '2021-09-14',
-		metrics: [
-			{
-				expression: 'ga:users',
-				alias: 'Total Users',
-			},
-		],
-		dimensions: [ 'ga:channelGrouping' ],
-		dimensionFilters: { 'ga:channelGrouping': 'Organic Search' },
-	},
-	{
-		startDate: '2021-09-15',
-		endDate: '2021-10-12',
-		compareStartDate: '2021-08-18',
-		compareEndDate: '2021-09-14',
-		metrics: [
-			{
-				expression: 'ga:users',
-				alias: 'Total Users',
-			},
-		],
-		dimensions: [ 'ga:date' ],
-		dimensionFilters: { 'ga:channelGrouping': 'Organic Search' },
-	},
-	{
-		dimensions: [ 'ga:date' ],
-		metrics: [
-			{
-				expression: 'ga:users',
-			},
-		],
-		startDate: '2021-09-15',
-		endDate: '2021-10-12',
-	},
-];
+import {
+	setupSearchConsoleAnalyticsMockReports,
+	setupSearchConsoleMockReports,
+	setupSearchConsoleAnalyticsZeroData,
+	setupSearchConsoleGatheringData,
+	setupAnalyticsGatheringData,
+} from './common.stories';
 
 const Template = ( { setupRegistry } ) => (
 	<WithRegistrySetup func={ setupRegistry }>
@@ -119,14 +50,33 @@ const Template = ( { setupRegistry } ) => (
 export const Ready = Template.bind( {} );
 Ready.storyName = 'Ready';
 Ready.args = {
-	setupRegistry: ( registry ) => {
-		for ( const options of analyticsArgs ) {
-			provideAnalyticsMockReport( registry, options );
-		}
-
-		provideSearchConsoleMockReport( registry, searchConsoleArgs );
-	},
+	setupRegistry: setupSearchConsoleAnalyticsMockReports,
 };
+Ready.decorators = [
+	( Story ) => {
+		const setupRegistry = ( registry ) => {
+			// Set up the search console and analytics modules stores but provide no data.
+			provideModules( registry, [
+				{
+					slug: 'search-console',
+					active: true,
+					connected: true,
+				},
+				{
+					slug: 'analytics',
+					active: true,
+					connected: true,
+				},
+			] );
+		};
+
+		return (
+			<WithRegistrySetup func={ setupRegistry }>
+				<Story />
+			</WithRegistrySetup>
+		);
+	},
+];
 
 export const ReadyWithActivateModuleCTA = Template.bind( {} );
 ReadyWithActivateModuleCTA.storyName = 'Ready with Activate Module CTA';
@@ -140,7 +90,7 @@ ReadyWithActivateModuleCTA.args = {
 			},
 		] );
 
-		provideSearchConsoleMockReport( registry, searchConsoleArgs );
+		setupSearchConsoleMockReports( registry );
 	},
 };
 
@@ -156,7 +106,7 @@ ReadyWithActivateAnalyticsCTA.args = {
 			},
 		] );
 
-		provideSearchConsoleMockReport( registry, searchConsoleArgs );
+		setupSearchConsoleMockReports( registry );
 	},
 };
 ReadyWithActivateAnalyticsCTA.parameters = {
@@ -181,26 +131,75 @@ ReadyWithCompleteAnalyticsActivationCTA.args = {
 			},
 		] );
 		provideModuleRegistrations( registry );
-		provideSearchConsoleMockReport( registry, searchConsoleArgs );
+		setupSearchConsoleMockReports( registry );
 	},
 };
 ReadyWithCompleteAnalyticsActivationCTA.parameters = {
 	features: [ 'zeroDataStates' ],
 };
 
+export const GatheringData = Template.bind( {} );
+GatheringData.storyName = 'Gathering Data';
+GatheringData.args = {
+	setupRegistry: ( registry ) => {
+		provideModules( registry, [
+			{
+				slug: 'search-console',
+				active: true,
+				connected: true,
+			},
+			{
+				slug: 'analytics',
+				active: true,
+				connected: true,
+			},
+		] );
+		setupSearchConsoleGatheringData( registry );
+		setupAnalyticsGatheringData( registry );
+	},
+};
+GatheringData.parameters = {
+	features: [ 'zeroDataStates' ],
+};
+
+export const ZeroData = Template.bind( {} );
+ZeroData.storyName = 'Zero Data';
+ZeroData.args = {
+	setupRegistry: ( registry ) => {
+		provideModules( registry, [
+			{
+				slug: 'search-console',
+				active: true,
+				connected: true,
+			},
+			{
+				slug: 'analytics',
+				active: true,
+				connected: true,
+			},
+		] );
+		setupSearchConsoleAnalyticsZeroData( registry );
+	},
+};
+ZeroData.parameters = {
+	features: [ 'zeroDataStates' ],
+};
+
 export default {
 	title: 'Views/WPDashboardApp/WPDashboardWidgets',
+	// Do not use common decorator so as to activate/connect analytics in only certain scenarios.
 	decorators: [
 		( Story, { args } ) => {
 			const setupRegistry = ( registry ) => {
 				provideSiteInfo( registry );
-				registry.dispatch( CORE_USER ).setReferenceDate( '2021-10-13' );
 				registry.dispatch( CORE_USER ).receiveGetAuthentication( {
 					needsReauthentication: false,
 				} );
 
 				// Call story-specific setup.
-				args.setupRegistry( registry );
+				if ( typeof args?.setupRegistry === 'function' ) {
+					args.setupRegistry( registry );
+				}
 			};
 
 			return (
