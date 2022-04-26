25354d87f602aec4a476bcaf1a71e7fb171301a7
Asvin Balloo <asvin.balloo@gmail.com>
2022-04-26 21:05:26 +0400

    Finalize styles for DashboardSharingSettingsButton modal.

--- assets/js/components/dashboard-sharing/DashboardSharingSettingsButton.js
+++ assets/js/components/dashboard-sharing/DashboardSharingSettingsButton.js
@@ -25,36 +25,107 @@ import Dialog, { DialogContent, DialogFooter } from '@material/react-dialog';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useCallback, useState } from '@wordpress/element';
+import { Icon, arrowLeft } from '@wordpress/icons';
+import {
+	createInterpolateElement,
+	Fragment,
+	useCallback,
+	useState,
+} from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
+import Link from '../Link';
 import Button from '../Button';
 import Portal from '../Portal';
 import ShareIcon from '../../../svg/icons/share.svg';
-import Link from '../Link';
+import { BREAKPOINT_SMALL, useBreakpoint } from '../../hooks/useBreakpoint';
 
 export default function DashboardSharingSettingsButton() {
+	const breakpoint = useBreakpoint();
 	const [ dialogOpen, setDialogOpen ] = useState( false );
 
-	const handleButtonClick = useCallback( () => {
-		setDialogOpen( ! dialogOpen );
-	}, [ dialogOpen ] );
+	const openDialog = useCallback( () => {
+		setDialogOpen( true );
+	}, [] );
+
+	const closeDialog = useCallback( () => {
+		setDialogOpen( false );
+	}, [] );
 
 	return (
-		<Button onClick={ handleButtonClick }>
-			<ShareIcon width={ 20 } height={ 20 } />
+		<Fragment>
+			<Link
+				aria-label={ __( 'Open sharing settings', 'google-site-kit' ) }
+				onClick={ openDialog }
+			>
+				<ShareIcon width={ 21 } height={ 21 } />
+			</Link>
 
 			<Portal>
 				<Dialog open={ dialogOpen } className="googlesitekit-dialog">
-					<DialogContent></DialogContent>
-					<DialogFooter>
-						<Link>{ __( 'Cancel', 'google-site-kit' ) }</Link>
-						<Button>{ __( 'Apply', 'google-site-kit' ) }</Button>
+					<div
+						className="googlesitekit-dialog__back-wrapper"
+						aria-hidden={
+							breakpoint === BREAKPOINT_SMALL ? false : true
+						}
+					>
+						<Button
+							aria-label={ __( 'Back', 'google-site-kit' ) }
+							className="googlesitekit-dialog__back"
+							onClick={ closeDialog }
+						>
+							<Icon icon={ arrowLeft } />
+						</Button>
+					</div>
+
+					<DialogContent className="googlesitekit-dialog__content">
+						<div className="googlesitekit-dialog__titles">
+							<h2 className="googlesitekit-dialog__title">
+								{ __(
+									'Dashboard sharing & permissions',
+									'google-site-kit'
+								) }
+							</h2>
+
+							<p className="googlesitekit-dialog__subtitle">
+								{ createInterpolateElement(
+									__(
+										'Share a view-only version of your Site Kit dashboard with other WordPress roles. <a>Learn more</a>',
+										'google-site-kit'
+									),
+									{
+										a: (
+											<Link
+												aria-label={ __(
+													'Learn more about dashboard sharing',
+													'google-site-kit'
+												) }
+												href="https://sitekit.withgoogle.com/documentation/using-site-kit/dashboard-sharing/"
+												external
+												hideExternalIndicator
+												inherit
+											/>
+										),
+									}
+								) }
+							</p>
+						</div>
+					</DialogContent>
+
+					<DialogFooter className="googlesitekit-dialog__footer">
+						<Link onClick={ closeDialog }>
+							{ __( 'Cancel', 'google-site-kit' ) }
+						</Link>
+
+						{ /* @TODO: Update Apply button behaviour */ }
+						<Button onClick={ closeDialog }>
+							{ __( 'Apply', 'google-site-kit' ) }
+						</Button>
 					</DialogFooter>
 				</Dialog>
 			</Portal>
-		</Button>
+		</Fragment>
 	);
 }
