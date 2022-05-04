e8b289bfef951c8da3b042063b5554cfa58d6bcd
Arafat Zahan <arafat.zahan@10up.com>
2022-05-05 02:04:10 +0600

    Change variable name to be less confusing.

--- includes/Core/Authentication/Authentication.php
+++ includes/Core/Authentication/Authentication.php
@@ -946,7 +946,7 @@ final class Authentication {
 			return current_user_can( Permissions::SETUP );
 		};
 
-		$can_authenticate = function() {
+		$can_access_authentication = function() {
 			return current_user_can( Permissions::AUTHENTICATE ) || current_user_can( Permissions::VIEW_SHARED_DASHBOARD );
 		};
 
@@ -997,7 +997,7 @@ final class Authentication {
 
 							return new WP_REST_Response( $data );
 						},
-						'permission_callback' => $can_authenticate,
+						'permission_callback' => $can_access_authentication,
 					),
 				)
 			),
