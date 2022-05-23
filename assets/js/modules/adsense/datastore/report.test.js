ad6866df0df2830c59a77cc0603245562ded773d
nfmohit <nahid.mohit@10up.com>
2022-05-23 22:47:27 +0600

    Fix the last failing test.

--- assets/js/modules/adsense/datastore/report.test.js
+++ assets/js/modules/adsense/datastore/report.test.js
@@ -112,7 +112,7 @@ describe( 'modules/adsense report', () => {
 					data: { status: 500 },
 				};
 				fetchMock.getOnce(
-					/^\/google-site-kit\/v1\/modules\/adsense\/data\/earnings/,
+					/^\/google-site-kit\/v1\/modules\/adsense\/data\/report/,
 					{ body: response, status: 500 }
 				);
 
