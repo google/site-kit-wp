c1c9d980ffeb60d7eb7b053421a0fb9765b0dd90
nfmohit <nahid.mohit@10up.com>
2022-05-25 05:04:53 +0600

    Add unit test for tracking exclusions.

--- tests/phpunit/integration/Modules/Analytics/SettingsTest.php
+++ tests/phpunit/integration/Modules/Analytics/SettingsTest.php
@@ -300,6 +300,32 @@ class SettingsTest extends SettingsTestCase {
 		$this->assertTrue( $settings->get()['canUseSnippet'] );
 	}
 
+	public function test_tracking_disabled() {
+		$context  = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
+		$settings = new Settings( new Options( $context ) );
+		$settings->register();
+		$analytics = new Analytics( $context );
+
+		// Defaults to `[ 'loggedinUsers' ]`
+		$this->assertEqualSets( array( 'loggedinUsers' ), $settings->get()['trackingDisabled'] );
+
+		// Save with defaults.
+		$settings->merge( array() );
+		$this->assertEqualSets( array( 'loggedinUsers' ), $settings->get()['trackingDisabled'] );
+
+		// Save with loggedinUsers.
+		$settings->merge( array( 'trackingDisabled' => array( 'loggedinUsers' ) ) );
+		$this->assertEqualSets( array( 'loggedinUsers' ), $settings->get()['trackingDisabled'] );
+
+		// Save with contentCreators.
+		$settings->merge( array( 'trackingDisabled' => array( 'contentCreators' ) ) );
+		$this->assertEqualSets( array( 'contentCreators' ), $settings->get()['trackingDisabled'] );
+
+		// Save with multiple options including loggedinUsers.
+		$settings->merge( array( 'trackingDisabled' => array( 'loggedinUsers', 'contentCreators' ) ) );
+		$this->assertEqualSets( array( 'loggedinUsers' ), $settings->get()['trackingDisabled'] );
+	}
+
 	protected function get_testcase() {
 		return $this;
 	}
