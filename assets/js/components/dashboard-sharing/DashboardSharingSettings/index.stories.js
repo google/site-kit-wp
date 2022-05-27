a64c3672c96bc32bcc7d3a21a66b475589664859
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-05-27 19:50:01 +0530

    Add stories for DashboardSharingSettings component.

--- assets/js/components/dashboard-sharing/DashboardSharingSettings/index.stories.js
+++ assets/js/components/dashboard-sharing/DashboardSharingSettings/index.stories.js
@@ -19,14 +19,91 @@
 /**
  * Internal dependencies
  */
-import { CORE_MODULES } from '../../../googlesitekit/modules/datastore/constants';
-import { withConnected } from '../../../googlesitekit/modules/datastore/__fixtures__';
 import {
 	provideModuleRegistrations,
+	provideModules,
 	provideSiteConnection,
 } from '../../../../../tests/js/utils';
 import WithRegistrySetup from '../../../../../tests/js/WithRegistrySetup';
 import DashboardSharingSettings from './index';
+import { MODULES_PAGESPEED_INSIGHTS } from '../../../modules/pagespeed-insights/datastore/constants';
+import { MODULES_SEARCH_CONSOLE } from '../../../modules/search-console/datastore/constants';
+import { MODULES_ANALYTICS } from '../../../modules/analytics/datastore/constants';
+import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
+
+const settings = {
+	'search-console': {
+		sharedRoles: [ 'editor', 'subscriber' ],
+		management: 'owner',
+	},
+	analytics: {
+		sharedRoles: [ 'editor' ],
+		management: 'owner',
+	},
+	'pagespeed-insights': {
+		sharedRoles: [ 'editor' ],
+		management: 'all_admins',
+	},
+	adsense: {
+		sharedRoles: [],
+		management: 'all_admins',
+	},
+};
+const roles = [
+	{
+		id: 'administrator',
+		displayName: 'Administrator',
+	},
+	{
+		id: 'editor',
+		displayName: 'Editor',
+	},
+	{
+		id: 'author',
+		displayName: 'Author',
+	},
+	{
+		id: 'contributor',
+		displayName: 'Contributor',
+	},
+];
+
+const sharedOwnershipModules = [ 'pagespeed-insights' ];
+
+const modules = [
+	{
+		slug: 'search-console',
+		shareable: true,
+		owner: {
+			id: 1,
+			login: 'Admin 1',
+		},
+	},
+	{
+		slug: 'pagespeed-insights',
+		shareable: true,
+		owner: {
+			id: 1,
+			login: 'Admin 1',
+		},
+	},
+	{
+		slug: 'analytics',
+		shareable: true,
+		owner: {
+			id: 2,
+			login: 'Admin 2',
+		},
+	},
+	{
+		slug: 'adsense',
+		shareable: true,
+		owner: {
+			id: 2,
+			login: 'Admin 2',
+		},
+	},
+];
 
 const Template = ( { setupRegistry = () => {}, ...args } ) => (
 	<WithRegistrySetup func={ setupRegistry }>
@@ -34,46 +111,219 @@ const Template = ( { setupRegistry = () => {}, ...args } ) => (
 	</WithRegistrySetup>
 );
 
-export const DefaultDashboardSharingSettings = Template.bind( {} );
-DefaultDashboardSharingSettings.storyName = 'Default';
-DefaultDashboardSharingSettings.args = {
+export const SingleAdminDefault = Template.bind( {} );
+SingleAdminDefault.storyName = 'Single Admin Default';
+SingleAdminDefault.args = {
+	setupRegistry: () => {},
+};
+
+export const SingleAdminWithOwnedModules = Template.bind( {} );
+SingleAdminWithOwnedModules.storyName = 'Single Admin With Owned Modules';
+SingleAdminWithOwnedModules.args = {
 	setupRegistry: ( registry ) => {
-		registry
-			.dispatch( CORE_MODULES )
-			.receiveGetModules(
-				withConnected(
-					'adsense',
-					'analytics',
-					'pagespeed-insights',
-					'search-console'
-				)
-			);
-		provideModuleRegistrations( registry );
+		provideModules( registry, [
+			...modules,
+			{
+				slug: 'analytics',
+				shareable: true,
+				owner: {
+					id: 1,
+					login: 'Admin 1',
+				},
+			},
+			{
+				slug: 'adsense',
+				shareable: true,
+				owner: {
+					id: 1,
+					login: 'Admin 1',
+				},
+			},
+		] );
 	},
 };
 
-export const MultipleAdminsDashboardSharingSettings = Template.bind( {} );
-MultipleAdminsDashboardSharingSettings.storyName = 'Multiple Admins';
-MultipleAdminsDashboardSharingSettings.args = {
+export const SingleAdminWithNonOwnedModules = Template.bind( {} );
+SingleAdminWithNonOwnedModules.storyName =
+	'Single Admin With Non-Owned Modules';
+SingleAdminWithNonOwnedModules.args = {
 	setupRegistry: ( registry ) => {
-		registry
-			.dispatch( CORE_MODULES )
-			.receiveGetModules(
-				withConnected(
-					'adsense',
-					'analytics',
-					'pagespeed-insights',
-					'search-console'
-				)
-			);
-		provideModuleRegistrations( registry );
+		provideModules( registry, [
+			...modules,
+			{
+				slug: 'search-console',
+				shareable: true,
+				owner: {
+					id: 2,
+					login: 'Admin 2',
+				},
+			},
+			{
+				slug: 'pagespeed-insights',
+				shareable: true,
+				owner: {
+					id: 2,
+					login: 'Admin 2',
+				},
+			},
+		] );
+	},
+};
+
+export const MultiAdminsDefault = Template.bind( {} );
+MultiAdminsDefault.storyName = 'Multi Admins Default';
+MultiAdminsDefault.args = {
+	setupRegistry: ( registry ) => {
 		provideSiteConnection( registry, {
 			hasMultipleAdmins: true,
 		} );
 	},
 };
 
+export const MultiAdminsWithOwnedModules = Template.bind( {} );
+MultiAdminsWithOwnedModules.storyName = 'Multi Admins With Owned Modules';
+MultiAdminsWithOwnedModules.args = {
+	setupRegistry: ( registry ) => {
+		provideModules( registry, [
+			...modules,
+			{
+				slug: 'analytics',
+				shareable: true,
+				owner: {
+					id: 1,
+					login: 'Admin 1',
+				},
+			},
+			{
+				slug: 'adsense',
+				shareable: true,
+				owner: {
+					id: 1,
+					login: 'Admin 1',
+				},
+			},
+		] );
+
+		provideSiteConnection( registry, {
+			hasMultipleAdmins: true,
+		} );
+	},
+};
+
+export const MultiAdminsWithNonOwnedModules = Template.bind( {} );
+MultiAdminsWithNonOwnedModules.storyName =
+	'Multi Admins With Non-Owned Modules';
+MultiAdminsWithNonOwnedModules.args = {
+	setupRegistry: ( registry ) => {
+		provideModules( registry, [
+			...modules,
+			{
+				slug: 'search-console',
+				shareable: true,
+				owner: {
+					id: 2,
+					login: 'Admin 2',
+				},
+			},
+			{
+				slug: 'pagespeed-insights',
+				shareable: true,
+				owner: {
+					id: 2,
+					login: 'Admin 2',
+				},
+			},
+		] );
+
+		provideSiteConnection( registry, {
+			hasMultipleAdmins: true,
+		} );
+	},
+};
+
+export const MultiAdminsWithSharedOwnershipModules = Template.bind( {} );
+MultiAdminsWithSharedOwnershipModules.storyName =
+	'Multi Admins With Shared Ownership Modules';
+MultiAdminsWithSharedOwnershipModules.args = {
+	setupRegistry: ( registry ) => {
+		provideSiteConnection( registry, {
+			hasMultipleAdmins: true,
+		} );
+
+		provideModules( registry, [
+			...modules,
+			{
+				slug: 'analytics',
+				shareable: true,
+				owner: {
+					id: 1,
+					login: 'Admin 1',
+				},
+			},
+			{
+				slug: 'adsense',
+				shareable: true,
+				owner: {
+					id: 1,
+					login: 'Admin 1',
+				},
+			},
+		] );
+
+		global._googlesitekitDashboardSharingData = {
+			settings,
+			roles,
+			sharedOwnershipModules: [
+				'search-console',
+				'analytics',
+				'adsense',
+				'pagespeed-insights',
+			],
+		};
+	},
+};
+
 export default {
 	title: 'Components/DashboardSharingSettings',
-	component: DashboardSharingSettings,
+	decorators: [
+		( Story, { args } ) => {
+			const setupRegistry = ( registry ) => {
+				// Set global dashboard sharing variables
+				global._googlesitekitDashboardSharingData = {
+					settings,
+					roles,
+					sharedOwnershipModules,
+				};
+
+				provideModules( registry, modules );
+				provideModuleRegistrations( registry );
+
+				registry.dispatch( CORE_USER ).receiveUserInfo( {
+					id: 1,
+					email: 'admin@example.com',
+					name: 'admin',
+					picture: 'https://path/to/image',
+				} );
+
+				registry
+					.dispatch( MODULES_PAGESPEED_INSIGHTS )
+					.receiveGetSettings( { ownerID: 1 } );
+				registry
+					.dispatch( MODULES_SEARCH_CONSOLE )
+					.receiveGetSettings( { ownerID: 1 } );
+				registry
+					.dispatch( MODULES_ANALYTICS )
+					.receiveGetSettings( { ownerID: 1 } );
+
+				// Call story-specific setup.
+				args.setupRegistry( registry );
+			};
+
+			return (
+				<WithRegistrySetup func={ setupRegistry }>
+					<Story />
+				</WithRegistrySetup>
+			);
+		},
+	],
 };
