49b98af978524338c46bb29c84eda6e9f2953c2c
Evan Mattson <emattson@google.com>
2022-05-28 11:46:06 -0400

    Update condition for readability.

--- includes/Core/Assets/Assets.php
+++ includes/Core/Assets/Assets.php
@@ -102,9 +102,11 @@ final class Assets {
 		);
 
 		// All other asset-related general logic should only be active when the
-		// current user can actually use Site Kit (which only is so if they can
-		// authenticate or view shared dashboard).
-		if ( ! ( current_user_can( Permissions::VIEW_SPLASH ) || current_user_can( Permissions::VIEW_DASHBOARD ) ) ) {
+		// current user can actually use Site Kit.
+		if ( false === (
+				current_user_can( Permissions::VIEW_SPLASH ) || current_user_can( Permissions::VIEW_DASHBOARD )
+			)
+		) {
 			return;
 		}
 
