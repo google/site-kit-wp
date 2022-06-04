c65c22b3608a97f12825ba760d73110906c60c0c
merkys7 <merkys.maliukevicius@hostinger.com>
2022-06-04 13:46:12 +0100

    Cleanup wp_options table after plugin uninstall

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,8 @@
  * @link      https://sitekit.withgoogle.com
  */
 
+global $wpdb;
+
 // Prevent execution from directly accessing the file.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
@@ -18,3 +20,18 @@ require_once dirname( __FILE__ ) . '/google-site-kit.php';
 
 // Fire action to trigger uninstallation logic.
 do_action( 'googlesitekit_uninstallation' );
+
+// Remove options introduced by the plugin.
+delete_option( 'googlesitekit_analytics_settings' );
+delete_option( 'googlesitekit_analytics-4_settings' );
+delete_option( 'googlesitekit_owner_id' );
+delete_option( 'googlesitekit_search-console_settings' );
+delete_option( 'googlesitekit_connected_proxy_url' );
+delete_option( 'googlesitekitpersistent_remote_features' );
+delete_option( 'googlesitekit_credentials' );
+delete_option( 'googlesitekit_active_modules' );
+delete_option( 'googlesitekit_has_connected_admins' );
+delete_option( 'googlesitekit_db_version' );
+
+// Remove any transients which the plugin may have left behind.
+$wpdb->query( "DELETE FROM {$wpdb->options} WHERE `option_name` LIKE '_transient_%googlesitekit%'" );
