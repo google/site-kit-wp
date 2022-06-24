9185554f3e59650eeaab47b4edbd345129089a1e
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2022-06-24 11:40:02 +0100

    Add WidgetRecoverableModules component.

---
+++ assets/js/googlesitekit/widgets/components/WidgetRecoverableModules.js
@@ -0,0 +1,50 @@
+/**
+ * WidgetRecoverableModules component.
+ *
+ * Site Kit by Google, Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { useMemo } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import useWidgetStateEffect from '../hooks/useWidgetStateEffect';
+import RecoverableModules from '../../../components/RecoverableModules';
+
+// The supported props must match `RecoverableModules` (except `widgetSlug`).
+export default function WidgetRecoverableModules( {
+	widgetSlug,
+	moduleSlugs,
+	...props
+} ) {
+	const metadata = useMemo( () => ( { moduleSlugs } ), [ moduleSlugs ] );
+	useWidgetStateEffect( widgetSlug, RecoverableModules, metadata );
+
+	return <RecoverableModules moduleSlugs={ moduleSlugs } { ...props } />;
+}
+
+WidgetRecoverableModules.propTypes = {
+	widgetSlug: PropTypes.string.isRequired,
+	...RecoverableModules.propTypes,
+};
