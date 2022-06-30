0a7a2757c27ee3ab4509fd078fc12a2b2b412de3
Maciej <ostromaciej@gmail.com>
2022-06-30 09:20:56 +0200

    Change text in the view-only dashboard access screen.

--- assets/js/components/setup/SetupUsingProxyViewOnly.js
+++ assets/js/components/setup/SetupUsingProxyViewOnly.js
@@ -129,7 +129,7 @@ export default function SetupUsingProxyViewOnly() {
 												</p>
 												<p className="googlesitekit-setup__description">
 													{ __(
-														'Get insights about how people find and use your site, how to improve and monetize your content, directly in your WordPress dashboard',
+														'Get insights about how people find and use your site as well as how to improve and monetize your content, directly in your WordPress dashboard.',
 														'google-site-kit'
 													) }
 												</p>
