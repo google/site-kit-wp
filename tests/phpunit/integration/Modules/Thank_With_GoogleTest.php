844a1f2ce89360a6dc2ed48dec4575af6ab3bb0d
Arafat Zahan <arafat.zahan@10up.com>
2022-07-18 23:16:22 +0600

    Add test for TwG tag infrastructure.

--- tests/phpunit/integration/Modules/Thank_With_GoogleTest.php
+++ tests/phpunit/integration/Modules/Thank_With_GoogleTest.php
@@ -18,6 +18,7 @@ use Google\Site_Kit\Tests\TestCase;
 
 /**
  * @group Modules
+ * @group Thank_With_Google
  */
 class Thank_With_GoogleTest extends TestCase {
 
@@ -27,6 +28,13 @@ class Thank_With_GoogleTest extends TestCase {
 	 * @var Context
 	 */
 	private $context;
+
+	/**
+	 * Options instance.
+	 *
+	 * @var Options
+	 */
+	private $options;
 
 	/**
 	 * Thank_With_Google instance.
@@ -39,32 +47,122 @@ class Thank_With_GoogleTest extends TestCase {
 		parent::set_up();
 
 		$this->context           = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
-		$this->thank_with_google = new Thank_With_Google( $this->context );
-	}
+		$this->options           = new Options( $this->context );
+		$this->thank_with_google = new Thank_With_Google( $this->context, $this->options );
 
-	public function test_is_connected() {
-		$options           = new Options( $this->context );
-		$thank_with_google = new Thank_With_Google( $this->context, $options );
-
-		$options->set(
+		$this->options->set(
 			Settings::OPTION,
 			array(
 				'publicationID'   => '12345',
-				'colorTheme'      => 'light',
-				'buttonPlacement' => 'bottom',
+				'colorTheme'      => 'blue',
+				'buttonPlacement' => 'static_auto',
+				'buttonPostTypes' => array( 'post' ),
+			)
+		);
+	}
+
+	public function test_register_template_redirect() {
+		remove_all_actions( 'template_redirect' );
+
+		$this->thank_with_google->register();
+
+		remove_all_actions( 'wp_enqueue_scripts' );
+		remove_all_filters( 'the_content' );
+
+		$this->assertFalse( has_action( 'wp_enqueue_scripts' ) );
+		$this->assertFalse( has_filter( 'the_content' ) );
+
+		do_action( 'template_redirect' );
+		$this->assertTrue( has_action( 'wp_enqueue_scripts' ) );
+		$this->assertTrue( has_filter( 'the_content' ) );
+
+		remove_all_actions( 'wp_enqueue_scripts' );
+		remove_all_filters( 'the_content' );
+
+		$this->assertFalse( has_action( 'wp_enqueue_scripts' ) );
+		$this->assertFalse( has_filter( 'the_content' ) );
+
+		// Tag not hooked when blocked.
+		add_filter( 'googlesitekit_' . Thank_With_Google::MODULE_SLUG . '_tag_blocked', '__return_true' );
+		do_action( 'template_redirect' );
+
+		$this->assertFalse( has_action( 'wp_enqueue_scripts' ) );
+		$this->assertFalse( has_filter( 'the_content' ) );
+	}
+
+	public function test_footer_snippet() {
+		remove_all_actions( 'template_redirect' );
+		remove_all_actions( 'wp_enqueue_scripts' );
+
+		$this->thank_with_google->register();
+
+		// Hook `wp_print_footer_scripts` on placeholder action for capturing.
+		add_action( '__test_print_scripts', 'wp_print_footer_scripts' );
+
+		do_action( 'template_redirect' );
+		do_action( 'wp_enqueue_scripts' );
+
+		$output = $this->capture_action( '__test_print_scripts' );
+
+		// Snippet should not be present on non singular pages.
+		$this->assertEmpty( $output );
+
+		$post_ID = $this->factory()->post->create();
+		\WP_UnitTestCase_Base::go_to( get_permalink( $post_ID ) );
+
+		do_action( 'template_redirect' );
+		do_action( 'wp_enqueue_scripts' );
+
+		$output = $this->capture_action( '__test_print_scripts' );
+
+		$this->assertStringContainsString( 'Thank with Google snippet added by Site Kit', $output );
+		$this->assertStringContainsString( 'buttonPosition: \'inline\',', $output );
+		$this->assertStringContainsString( 'isPartOfProductId: \'12345:default\',', $output );
+	}
+
+	public function test_content_placeholder() {
+		remove_all_filters( 'the_content' );
+
+		$this->thank_with_google->register();
+		do_action( 'template_redirect' );
+
+		$mock_content = '<p>Hello World</p>';
+
+		$output = apply_filters( 'the_content', $mock_content );
+
+		// Snippet should not be present on non singular pages.
+		$this->assertEquals( $output, $mock_content );
+
+		$post_ID = $this->factory()->post->create();
+		\WP_UnitTestCase_Base::go_to( get_permalink( $post_ID ) );
+
+		$output = apply_filters( 'the_content', '<p>Hello World</p>' );
+
+		$this->assertStringContainsString( 'Thank with Google snippet added by Site Kit', $output );
+		$this->assertStringContainsString( '<button twg-button', $output );
+	}
+
+	public function test_is_connected() {
+		$this->options->delete( Settings::OPTION );
+		$this->assertFalse( $this->thank_with_google->is_connected() );
+
+		$this->options->set(
+			Settings::OPTION,
+			array(
+				'publicationID'   => '12345',
+				'colorTheme'      => 'blue',
+				'buttonPlacement' => 'static_auto',
 				'buttonPostTypes' => array( 'post' ),
 			)
 		);
 
-		$this->assertTrue( $thank_with_google->is_connected() );
+		$this->assertTrue( $this->thank_with_google->is_connected() );
 	}
 
 	public function test_on_deactivation() {
-		$thank_with_google = new Thank_With_Google( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
-		$options           = new Options( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
-		$options->set( Settings::OPTION, 'test-value' );
+		$this->assertOptionExists( Settings::OPTION );
 
-		$thank_with_google->on_deactivation();
+		$this->thank_with_google->on_deactivation();
 
 		$this->assertOptionNotExists( Settings::OPTION );
 	}
