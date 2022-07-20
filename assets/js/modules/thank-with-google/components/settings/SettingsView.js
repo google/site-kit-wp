6f2b141aadbc7453ea89a2aa536583768834ec0c
Eugene Manuilov <eugene.manuilov@get10up.com>
2022-07-20 21:18:09 +0300

    Add the color setting to the settings view.

--- assets/js/modules/thank-with-google/components/settings/SettingsView.js
+++ assets/js/modules/thank-with-google/components/settings/SettingsView.js
@@ -29,17 +29,26 @@ import Data from 'googlesitekit-data';
 import DisplaySetting from '../../../../components/DisplaySetting';
 import { MODULES_THANK_WITH_GOOGLE } from '../../datastore/constants';
 import StoreErrorNotices from '../../../../components/StoreErrorNotices';
+import { getColorThemes } from '../../util/settings';
 const { useSelect } = Data;
 
 export default function SettingsView() {
 	const publicationID = useSelect( ( select ) =>
 		select( MODULES_THANK_WITH_GOOGLE ).getPublicationID()
 	);
+	const colorTheme = useSelect( ( select ) =>
+		select( MODULES_THANK_WITH_GOOGLE ).getColorTheme()
+	);
 
 	// Bail if the values aren't ready.
 	if ( publicationID === undefined ) {
 		return null;
 	}
+
+	const { name: colorName } =
+		getColorThemes()?.find(
+			( { colorThemeID } ) => colorThemeID === colorTheme
+		) || {};
 
 	return (
 		<Fragment>
@@ -55,6 +64,14 @@ export default function SettingsView() {
 					</h5>
 					<p className="googlesitekit-settings-module__meta-item-data">
 						<DisplaySetting value={ publicationID } />
+					</p>
+				</div>
+				<div className="googlesitekit-settings-module__meta-item">
+					<h5 className="googlesitekit-settings-module__meta-item-type">
+						{ __( 'Color', 'google-site-kit' ) }
+					</h5>
+					<p className="googlesitekit-settings-module__meta-item-data">
+						<DisplaySetting value={ colorName } />
 					</p>
 				</div>
 			</div>
