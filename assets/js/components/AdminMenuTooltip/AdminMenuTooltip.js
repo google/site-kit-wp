80a9330a944bdc92fb5556affc7c5e4d1eb57abb
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2022-08-04 13:34:20 +0100

    Extract reusable AdminMenuTooltip component from AdSenseConnectCTAWidget.

---
+++ assets/js/components/AdminMenuTooltip/AdminMenuTooltip.js
@@ -0,0 +1,60 @@
+import { useCallback } from '@wordpress/element';
+
+import PropTypes from 'prop-types';
+
+import Data from 'googlesitekit-data';
+import Tooltip from '../Tooltip';
+import { CORE_UI } from '../../googlesitekit/datastore/ui/constants';
+
+import { useTooltipState } from './useTooltipState';
+
+const { useDispatch } = Data;
+
+export function AdminMenuTooltip( { onDismiss, tooltipStateKey, ...props } ) {
+	const { setValue } = useDispatch( CORE_UI );
+
+	const { rehideAdminMenu, rehideAdminSubMenu } = useTooltipState(
+		tooltipStateKey
+	);
+
+	const handleDismissTooltip = useCallback( async () => {
+		// If the WordPress admin menu was closed, re-close it.
+		if ( rehideAdminMenu ) {
+			const isAdminMenuOpen =
+				document.querySelector( '#adminmenu' ).offsetHeight > 0;
+
+			if ( isAdminMenuOpen ) {
+				document.getElementById( 'wp-admin-bar-menu-toggle' )?.click();
+			}
+		}
+
+		// If the Site Kit admin submenu was hidden, re-hide it.
+		if ( rehideAdminSubMenu ) {
+			// Click on the body to close the submenu.
+			document.querySelector( 'body' ).click();
+		}
+
+		await onDismiss?.();
+
+		setValue( tooltipStateKey, undefined );
+	}, [
+		onDismiss,
+		rehideAdminMenu,
+		rehideAdminSubMenu,
+		setValue,
+		tooltipStateKey,
+	] );
+
+	return <Tooltip onDismiss={ handleDismissTooltip } { ...props } />;
+}
+
+AdminMenuTooltip.propTypes = {
+	...Tooltip.propTypes,
+	target: PropTypes.string,
+	tooltipStateKey: PropTypes.string.isRequired,
+};
+
+AdminMenuTooltip.defaultProps = {
+	// Point to the Site Kit Settings menu item by default.
+	target: '#adminmenu [href*="page=googlesitekit-settings"]',
+};
