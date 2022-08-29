c07a22eb7788cdfd012e826564192d5141e77e4e
nfmohit <nahid.mohit@10up.com>
2022-08-29 14:48:19 +0600

    Add Storybook stories for SupporterWall.

---
+++ assets/js/modules/thank-with-google/components/common/SupporterWall.stories.js
@@ -0,0 +1,73 @@
+/**
+ * Site Kit by Google, Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import SupporterWall from './SupporterWall';
+import { MODULES_THANK_WITH_GOOGLE } from '../../datastore/constants';
+import { provideModules } from '../../../../../../tests/js/utils';
+import WithRegistrySetup from '../../../../../../tests/js/WithRegistrySetup';
+
+const Template = ( args ) => <SupporterWall { ...args } />;
+
+export const WidgetNotPlaced = Template.bind( {} );
+WidgetNotPlaced.storyName = 'No Widget Placed';
+WidgetNotPlaced.args = {
+	setupRegistry: ( registry ) => {
+		registry
+			.dispatch( MODULES_THANK_WITH_GOOGLE )
+			.receiveGetSupporterWallSidebars( [] );
+	},
+};
+
+export const WidgetPlaced = Template.bind( {} );
+WidgetPlaced.storyName = 'With Widget Placed';
+WidgetPlaced.args = {
+	setupRegistry: ( registry ) => {
+		registry
+			.dispatch( MODULES_THANK_WITH_GOOGLE )
+			.receiveGetSupporterWallSidebars( [ 'All sidebars' ] );
+	},
+};
+
+export default {
+	title: 'Modules/Thank with Google/Common/SupporterWall',
+	decorators: [
+		( Story, { args } ) => {
+			const setupRegistry = ( registry ) => {
+				provideModules( registry, [
+					{
+						slug: 'thank-with-google',
+						active: true,
+						connected: true,
+					},
+				] );
+
+				// Call story-specific setup.
+				if ( typeof args?.setupRegistry === 'function' ) {
+					args.setupRegistry( registry );
+				}
+			};
+
+			return (
+				<WithRegistrySetup func={ setupRegistry }>
+					<Story />
+				</WithRegistrySetup>
+			);
+		},
+	],
+};
