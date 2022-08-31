889bc8edd13fe33c08f70b027ddb64a016883831
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-08-31 15:58:39 +0530

    Replace apostrophe with the special character.

--- assets/js/modules/thank-with-google/components/setup/SetupMain.test.js
+++ assets/js/modules/thank-with-google/components/setup/SetupMain.test.js
@@ -140,7 +140,7 @@ describe( 'SetupMain', () => {
 		} );
 
 		expect( container ).toHaveTextContent(
-			"To get started, create an account. Currently available only in the US. If setup failed because you're outside the US, disconnect Thank with Google in your Settings."
+			'To get started, create an account. Currently available only in the US. If setup failed because you’re outside the US, disconnect Thank with Google in your Settings.'
 		);
 	} );
 
