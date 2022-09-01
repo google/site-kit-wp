3e8549a71376f40de5c29396dee6fb291c108d8e
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-09-01 15:14:26 +0530

    Remove CompleteModuleActivationCTA occurences.

--- assets/js/googlesitekit/widgets/util/constants.js
+++ assets/js/googlesitekit/widgets/util/constants.js
@@ -22,7 +22,6 @@
 import { WIDGET_WIDTHS } from '../datastore/constants';
 import ReportZero from '../../../components/ReportZero';
 import RecoverableModules from '../../../components/RecoverableModules';
-import CompleteModuleActivationCTA from '../../../components/CompleteModuleActivationCTA';
 
 export const WIDTH_GRID_COUNTER_MAP = {
 	[ WIDGET_WIDTHS.QUARTER ]: 3,
@@ -31,8 +30,4 @@ export const WIDTH_GRID_COUNTER_MAP = {
 };
 
 export const HIDDEN_CLASS = 'googlesitekit-hidden';
-export const SPECIAL_WIDGET_STATES = [
-	CompleteModuleActivationCTA,
-	ReportZero,
-	RecoverableModules,
-];
+export const SPECIAL_WIDGET_STATES = [ ReportZero, RecoverableModules ];
