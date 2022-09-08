6b7d18a3ed6d2663ff8fcecf051c489591849a9a
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-09-08 10:51:57 +0530

    Return the user to the same step after oauth flow.

--- assets/js/modules/analytics-4/components/dashboard/ActivationBanner/index.js
+++ assets/js/modules/analytics-4/components/dashboard/ActivationBanner/index.js
@@ -19,7 +19,7 @@
 /**
  * WordPress dependencies
  */
-import { useState } from '@wordpress/element';
+import { useState, useCallback, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -32,32 +32,57 @@ import {
 	ACTIVATION_STEP_REMINDER,
 	ACTIVATION_STEP_SETUP,
 	ACTIVATION_STEP_SUCCESS,
+	GA4_ACTIVATION_BANNER_STATE,
 } from '../../../constants';
 import { CORE_MODULES } from '../../../../../googlesitekit/modules/datastore/constants';
-const { useSelect } = Data;
+import { CORE_USER } from '../../../../../googlesitekit/datastore/user/constants';
+import { EDIT_SCOPE } from '../../../../analytics/datastore/constants';
+import { CORE_FORMS } from '../../../../../googlesitekit/datastore/forms/constants';
+const { useSelect, useDispatch, useRegistry } = Data;
 
 export default function ActivationBanner() {
 	const [ step, setStep ] = useState( ACTIVATION_STEP_REMINDER );
 
+	const registry = useRegistry();
+
 	const uaConnected = useSelect( ( select ) =>
 		select( CORE_MODULES ).isModuleConnected( 'analytics' )
 	);
-
 	const ga4Connected = useSelect( ( select ) =>
 		select( CORE_MODULES ).isModuleConnected( 'analytics-4' )
 	);
+	const hasEditScope = useSelect( ( select ) =>
+		select( CORE_USER ).hasScope( EDIT_SCOPE )
+	);
+	const returnToSetupStep = useSelect( ( select ) =>
+		select( CORE_FORMS ).getValue(
+			GA4_ACTIVATION_BANNER_STATE,
+			'returnToSetupStep'
+		)
+	);
 
-	const handleSubmit = () => {
+	const { setValues } = useDispatch( CORE_FORMS );
+
+	useEffect( () => {
+		if ( hasEditScope && returnToSetupStep ) {
+			setStep( ACTIVATION_STEP_SETUP );
+			setValues( GA4_ACTIVATION_BANNER_STATE, {
+				returnToSetupStep: false,
+			} );
+		}
+	}, [ hasEditScope, registry, returnToSetupStep, setValues ] );
+
+	const handleSubmit = useCallback( () => {
 		if ( step === ACTIVATION_STEP_REMINDER ) {
 			setStep( ACTIVATION_STEP_SETUP );
 		}
 
-		if ( step === ACTIVATION_STEP_SETUP ) {
+		if ( ! returnToSetupStep && step === ACTIVATION_STEP_SETUP ) {
 			setStep( ACTIVATION_STEP_SUCCESS );
 		}
 
 		return { dismissOnCTAClick: false };
-	};
+	}, [ returnToSetupStep, step ] );
 
 	if ( ! uaConnected || ga4Connected ) {
 		return null;
