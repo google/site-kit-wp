f2bbd894f57862cd912a126ee96601859dc9756b
Hussain Thajutheen <hussain.thajutheen@10up.com>
2022-09-13 19:57:59 +0530

    Update getServicePublicationURL selector.

--- assets/js/modules/thank-with-google/datastore/publications.js
+++ assets/js/modules/thank-with-google/datastore/publications.js
@@ -195,13 +195,17 @@ const baseSelectors = {
 	 * @param {string} publicationID The ID of the publication to get link for.
 	 * @return {string} Publication link.
 	 */
-	getServicePublicationURL: ( _state, publicationID ) => {
-		invariant( publicationID, 'A publicationID is required.' );
+	getServicePublicationURL: createRegistrySelector(
+		( select ) => ( _state, publicationID ) => {
+			invariant( publicationID, 'A publicationID is required.' );
 
-		return `${ PUBLISHER_CENTER_URL }/publications/${ encodeURIComponent(
-			publicationID
-		) }/overview`;
-	},
+			const url = `${ PUBLISHER_CENTER_URL }/${ encodeURIComponent(
+				publicationID
+			) }/home`;
+
+			return select( CORE_USER ).getAccountChooserURL( url );
+		}
+	),
 };
 
 const store = Data.combineStores( fetchGetPublicationsStore, {
