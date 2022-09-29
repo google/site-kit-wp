ff598337d9b7e9061c5cbfb32658767cbcb10c83
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2022-09-29 17:20:29 +0100

    Add tests for Checkbox controlled input behaviour, not yet working though.

--- assets/js/components/material-3/Checkbox.test.js
+++ assets/js/components/material-3/Checkbox.test.js
@@ -16,10 +16,12 @@
  * limitations under the License.
  */
 
+import { useState } from '@wordpress/element';
+
 /**
  * Internal dependencies
  */
-import { render } from '../../../../tests/js/test-utils';
+import { fireEvent, render } from '../../../../tests/js/test-utils';
 import Checkbox from './Checkbox';
 
 // import '@material/web/checkbox/checkbox';
@@ -34,7 +36,7 @@ const log = ( ...args ) => console.__proto__.log.call( console, ...args );
 describe( 'Checkbox', () => {
 	it( 'should render the checkbox', () => {
 		const { container } = render( <Checkbox /> );
-		log( 'container', container );
+		// log( 'container', container );
 		expect( 1 ).toBe( 1 );
 
 		// Snapshotting not working, produces the following error:
@@ -50,9 +52,84 @@ describe( 'Checkbox', () => {
 		// expect( container ).toMatchSnapshot();
 	} );
 
-	// it( 'should work as a controlled input', () => {
-	// 	const { container } = render(
-	// 		<Checkbox />
-	// 	);
-	// } );
+	describe( 'controlled input behaviour', () => {
+		it( 'should correctly invoke onChange and retain its checked state when clicked', () => {
+			const onChange = jest.fn();
+			const { container, getByRole } = render(
+				<Checkbox onChange={ onChange } />
+			);
+
+			// expect( container ).toMatchSnapshot();
+
+			// Confirm the checkbox is not checked.
+			// getByRole is not working, produces the same error as toMatchSnapshot:
+			// PrettyFormatPluginError: Invalid string lengthRangeError: Invalid string length
+			//   at printObjectProperties (node_modules/pretty-format/build/collections.js:172:47)
+			expect( getByRole( 'checkbox' ) ).not.toHaveAttribute( 'checked' );
+
+			fireEvent.click( getByRole( 'checkbox' ) );
+
+			// expect( container ).toMatchSnapshot();
+
+			// Confirm the click resulted in a change event with target.checked: true.
+			expect( onChange ).toHaveBeenCalledTimes( 1 );
+			const event = onChange.mock.calls[ 0 ][ 0 ];
+			expect( event.target.checked ).toBe( true );
+
+			// Confirm the checkbox remains unchecked as its state is controlled.
+			expect( getByRole( 'checkbox' ) ).not.toHaveAttribute( 'checked' );
+		} );
+
+		it( 'should allow updating of the checked state', () => {
+			function CheckableCheckbox( { onChange } ) {
+				const [ checked, setChecked ] = useState( false );
+
+				return (
+					<Checkbox
+						checked={ checked }
+						onChange={ ( event ) => {
+							onChange( event );
+							setChecked( event.target.checked );
+						} }
+					/>
+				);
+			}
+
+			const onChange = jest.fn();
+			const { container, getByRole } = render(
+				<CheckableCheckbox onChange={ onChange } />
+			);
+
+			// expect( container ).toMatchSnapshot();
+
+			// Confirm the checkbox is not checked.
+			expect( getByRole( 'checkbox' ) ).not.toHaveAttribute( 'checked' );
+
+			fireEvent.click( getByRole( 'checkbox' ) );
+
+			// expect( container ).toMatchSnapshot();
+
+			// Confirm the click resulted in a change event with target.checked: true.
+			expect( onChange ).toHaveBeenCalledTimes( 1 );
+			let event = onChange.mock.calls[ 0 ][ 0 ];
+			expect( event.target.checked ).toBe( true );
+
+			// Confirm the checkbox is now checked
+			expect( getByRole( 'checkbox' ) ).toHaveAttribute( 'checked' );
+
+			// Click again to uncheck
+			onChange.mockClear();
+			fireEvent.click( getByRole( 'checkbox' ) );
+
+			// expect( container ).toMatchSnapshot();
+
+			// Confirm the click resulted in a change event with target.checked: undefined.
+			expect( onChange ).toHaveBeenCalledTimes( 1 );
+			event = onChange.mock.calls[ 0 ][ 0 ];
+			expect( event.target ).toHaveProperty( 'checked', undefined );
+
+			// Confirm the checkbox is now unchecked
+			expect( getByRole( 'checkbox' ) ).not.toHaveAttribute( 'checked' );
+		} );
+	} );
 } );
