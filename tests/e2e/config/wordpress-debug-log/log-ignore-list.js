285170e45f1802b2d357e043ff9a0b02af934acb
nfmohit <nahid.mohit@10up.com>
2022-09-30 19:44:13 +0600

    Add debug.log ignores for 5.2.

--- tests/e2e/config/wordpress-debug-log/log-ignore-list.js
+++ tests/e2e/config/wordpress-debug-log/log-ignore-list.js
@@ -3,4 +3,15 @@
  *
  * @type {Object} WordPress debug log entries to ignore, keyed by WordPress version.
  */
-export const logIgnoreList = {};
+export const logIgnoreList = {
+	'5.2.16': [
+		// Deprecated syntax or function calls which are fixed in later WP versions.
+		'PHP Deprecated:  Function get_magic_quotes_gpc() is deprecated in /var/www/html/wp-includes/load.php on line 926',
+		'PHP Deprecated:  Function get_magic_quotes_gpc() is deprecated in /var/www/html/wp-includes/formatting.php on line 4803',
+
+		// These are guarded against in later WP versions.
+		'PHP Notice:  Trying to access array offset on value of type bool in /var/www/html/wp-admin/includes/update.php on line 673',
+		'PHP Notice:  Trying to access array offset on value of type null in /var/www/html/wp-includes/rest-api/class-wp-rest-request.php on line 337',
+		'PHP Notice:  Trying to access array offset on value of type bool in /var/www/html/wp-includes/theme.php on line 2360',
+	],
+};
