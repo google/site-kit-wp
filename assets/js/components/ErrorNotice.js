e15a6711dba3a0bcee1c29fbd9eb099e064cc49a
sashadoes <sasha.pawlyshyn@get10up.com>
2022-10-29 23:51:34 +0100

    Change Retry button UI in inline error notice.

--- assets/js/components/ErrorNotice.js
+++ assets/js/components/ErrorNotice.js
@@ -24,14 +24,14 @@ import PropTypes from 'prop-types';
 /**
  * WordPress dependencies
  */
-import { Fragment, useCallback } from '@wordpress/element';
+import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
-import { Button } from 'googlesitekit-components';
+import Link from './Link';
 import { isPermissionScopeError, isErrorRetryable } from '../util/errors';
 import ErrorText from './ErrorText';
 
@@ -72,17 +72,17 @@ export default function ErrorNotice( {
 	const shouldDisplayRetry = isErrorRetryable( error, selectorData );
 
 	return (
-		<Fragment>
+		<div className="googlesitekit-error-notice__wrapper">
 			<ErrorText
 				message={ message }
 				reconnectURL={ error.data?.reconnectURL }
 			/>
 			{ shouldDisplayRetry && (
-				<Button onClick={ handleRetry }>
+				<Link onClick={ handleRetry }>
 					{ __( 'Retry', 'google-site-kit' ) }
-				</Button>
+				</Link>
 			) }
-		</Fragment>
+		</div>
 	);
 }
 
