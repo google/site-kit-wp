dcda380498d0ec88c11092d9a6a0c8edc6e47d17
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2022-11-09 17:10:03 +0000

    Add PoC Checkbox implementation with stories.

--- assets/js/googlesitekit/components-gm3/Checkbox.js
+++ assets/js/googlesitekit/components-gm3/Checkbox.js
@@ -16,6 +16,130 @@
  * limitations under the License.
  */
 
-export default function Checkbox() {
-	return null;
+/**
+ * External dependencies
+ */
+import '@material/web/checkbox/checkbox';
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useRef, useState } from '@wordpress/element';
+
+// Avoid console.log in tests.
+const log = process?.stdout
+	? ( ...args ) =>
+			process.stdout.write(
+				args.map( JSON.stringify ).join( ' ' ) + '\n'
+			)
+	: global.console.log;
+
+export default function Checkbox( {
+	checked,
+	disabled,
+	name,
+	value,
+	onChange,
+	onKeyDown,
+} ) {
+	log( 'Checkbox render. checked:', checked );
+	const ref = useRef( null );
+
+	// Use this state in a key prop on the web component to force a rerender.
+	// This is a workaround for the fact the web component doesn't update visually when the checked property is changed programmatically. Seemingly this glitch only occurs once the rendered md-checkbox has been clicked.
+	// This workaround has a side effect of unfocusing the checkbox when changed via keyboard input which needs to be addressed.
+	const [ index, setIndex ] = useState( 0 );
+
+	useEffect( () => {
+		const { current } = ref;
+
+		const click = ( event ) => {
+			log( 'click', event );
+
+			// Prevent default and manually dispatch a change event, in order to retain checkbox state and use as a controlled input.
+			event.preventDefault();
+
+			if ( disabled ) {
+				return;
+			}
+
+			current.checked = current.checked ? undefined : true;
+			// current.checked = ! current.checked;
+
+			current.dispatchEvent(
+				new Event( 'change', {
+					bubbles: true,
+					composed: true,
+				} )
+			);
+		};
+
+		const change = ( event ) => {
+			log( 'change', event );
+
+			onChange?.( event );
+
+			setIndex( index + 1 );
+		};
+
+		// Keydown events work fine without any special logic.
+		const keydown = ( event ) => {
+			log( 'keydown', event );
+
+			onKeyDown?.( event );
+		};
+
+		// The click event is triggered by both mouse and keyboard entry (space key) on Chrome, need to confirm other browsers.
+		current?.addEventListener( 'click', click );
+		current?.addEventListener( 'change', change );
+		current?.addEventListener( 'keydown', keydown );
+
+		// The 'action' event is dispatched by the ActionElement base class of the Material checkbox. See ActionElement source for details.
+		// current?.addEventListener( 'action', cancelEvent );
+
+		return () => {
+			current?.removeEventListener( 'click', click );
+			current?.removeEventListener( 'change', change );
+			current?.removeEventListener( 'keydown', keydown );
+		};
+	}, [ checked, disabled, index, onChange, onKeyDown ] );
+
+	return (
+		<md-checkbox
+			key={ `checkbox-${ name }-${ index }` }
+			ref={ ref }
+			role="checkbox"
+			// Lit boolean attributes treat anything non-null|undefined as true. Coerce to undefined if false. TODO: Use null instead of undefined?
+			// See https://lit.dev/docs/v1/components/properties/#attributes
+			checked={ checked || undefined }
+			disabled={ disabled || undefined }
+			name={ name }
+			value={ value }
+		></md-checkbox>
+	);
 }
+
+/**
+ * Props determined by examining the Material 3 web component's source code.
+ *
+ * See @property definitions and event handlers in the source to identify which props are needed.
+ */
+
+Checkbox.propTypes = {
+	// Fundamental checkbox attributes.
+	checked: PropTypes.bool,
+	disabled: PropTypes.bool,
+	name: PropTypes.string,
+	value: PropTypes.string,
+
+	// Event handlers.
+	onChange: PropTypes.func,
+	onKeyDown: PropTypes.func,
+
+	// Accessibility attributes.
+	// ariaLabel: PropTypes.string,
+	// ariaLabelledBy: PropTypes.string,
+	// ariaDescribedBy: PropTypes.string,
+	// reducedTouchTarget: PropTypes.bool,
+};
---
+++ assets/js/googlesitekit/components-gm3/Checkbox.stories.js
@@ -0,0 +1,95 @@
+/**
+ * Checkbox Component Stories.
+ *
+ * Site Kit by Google, Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import Checkbox from './Checkbox';
+
+const Template = ( args ) => <Checkbox { ...args } />;
+
+export const DefaultCheckbox = Template.bind( {} );
+DefaultCheckbox.storyName = 'Default Checkbox';
+DefaultCheckbox.args = {
+	onChange: ( e ) => {
+		global.console.log( '[story] change', e.target.value );
+	},
+	onKeyDown: ( e ) => {
+		global.console.log( '[story] keydown', e.target.value );
+	},
+	value: 'value-1',
+};
+
+export const CheckedCheckbox = Template.bind( {} );
+CheckedCheckbox.storyName = 'Checked Checkbox';
+CheckedCheckbox.args = {
+	onChange: ( e ) => {
+		global.console.log( '[story] change', e.target.value );
+	},
+	onKeyDown: ( e ) => {
+		global.console.log( '[story] keydown', e.target.value );
+	},
+	value: 'value-1',
+	checked: true,
+};
+
+export const InteractiveCheckbox = () => {
+	const [ checked, setChecked ] = useState( false );
+	return (
+		<Checkbox
+			onChange={ ( e ) => {
+				global.console.log(
+					'[story] change: value, checked',
+					e.target.value,
+					e.target.checked
+				);
+				setChecked( e.target.checked );
+			} }
+			onKeyDown={ ( e ) => {
+				global.console.log(
+					'[story] keydown: value, checked',
+					e.target.value,
+					e.target.checked
+				);
+			} }
+			value={ 'value-1' }
+			checked={ checked }
+		/>
+	);
+};
+InteractiveCheckbox.storyName = 'Interactive Checkbox';
+
+export const DisabledCheckbox = Template.bind( {} );
+DisabledCheckbox.storyName = 'Disabled Checkbox';
+DisabledCheckbox.args = {
+	onChange: ( e ) => {
+		global.console.log( '[story] change', e.target.value );
+	},
+	onKeyDown: ( e ) => {
+		global.console.log( '[story] keydown', e.target.value );
+	},
+	value: 'value-1',
+	disabled: true,
+};
+
+export default {
+	title: 'Components/Material 3/Checkbox',
+	component: Checkbox,
+};
