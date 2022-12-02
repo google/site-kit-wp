bb38147d5ac2f6b0b6056698fdae5e9141b11c5c
nfmohit <nahid.mohit@10up.com>
2022-12-02 14:45:21 +0600

    Update test case.

--- tests/phpunit/integration/Core/Util/SortTest.php
+++ tests/phpunit/integration/Core/Util/SortTest.php
@@ -17,19 +17,74 @@ use Google\Site_Kit\Tests\TestCase;
  * @group Util
  */
 class SortTest extends TestCase {
-
 	/**
-	 * Asset arguments.
+	 * Sample unsorted array.
 	 * @var array
 	 */
-	protected $unsorted_array = array(
+	private static $unsorted_array = array(
+		array(
+			'firstname' => 'John',
+			'lastname'  => 'Doe',
+		),
+		array(
+			'firstname' => 'Harry',
+			'lastname'  => 'Smith',
+		),
+		array(
+			'firstname' => 'foo',
+			'lastname'  => 'bar',
+		),
+	);
+
+	/**
+	 * Array sorted, ordered by 'firstname` in 'ASC' order.
+	 * @var array
+	 */
+	private static $sorted_array_firstname_asc = array(
+		array(
+			'firstname' => 'foo',
+			'lastname'  => 'bar',
+		),
+		array(
+			'firstname' => 'Harry',
+			'lastname'  => 'Smith',
+		),
 		array(
 			'firstname' => 'John',
 			'lastname'  => 'Doe',
 		),
 	);
 
-	public function test_case_insensitive_list_sort() {
+	/**
+	 * Array sorted, ordered by 'lastname` in 'DESC' order.
+	 * @var array
+	 */
+	private static $sorted_array_lastname_desc = array(
+		array(
+			'firstname' => 'Harry',
+			'lastname'  => 'Smith',
+		),
+		array(
+			'firstname' => 'John',
+			'lastname'  => 'Doe',
+		),
+		array(
+			'firstname' => 'foo',
+			'lastname'  => 'bar',
+		),
+	);
 
+	public function test_case_insensitive_list_sort() {
+		// Sorts array correctly, ordered by 'firstname` in 'ASC' order.
+		$this->assertEquals(
+			Sort::case_insensitive_list_sort( static::$unsorted_array, 'firstname', 'ASC' ),
+			static::$sorted_array_firstname_asc
+		);
+
+		// Sorts array correctly, ordered by 'lastname` in 'DESC' order.
+		$this->assertEquals(
+			Sort::case_insensitive_list_sort( static::$unsorted_array, 'lastname', 'DESC' ),
+			static::$sorted_array_lastname_desc
+		);
 	}
 }
