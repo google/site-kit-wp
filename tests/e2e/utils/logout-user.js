cd9e5ee79496b8fc9fbf1f67a1ce353150d68f88
Julian Weiland <julian.weiland@10up.com>
2022-12-22 09:41:26 +0100

    Use `deleteAuthCookie` inside `logoutUser` e2e utility.

--- tests/e2e/utils/logout-user.js
+++ tests/e2e/utils/logout-user.js
@@ -2,15 +2,13 @@
  * WordPress dependencies
  */
 import { createURL, isCurrentURL } from '@wordpress/e2e-test-utils';
+import { deleteAuthCookie } from './delete-auth-cookie';
 
 /**
  * Deletes authentication cookies to sign out the current user.
  */
 export async function logoutUser() {
-	const cookies = ( await page.cookies() )
-		.filter( ( cookie ) => cookie.name.match( /^wordpress_/ ) )
-		.filter( ( cookie ) => cookie.name !== 'wordpress_test_cookie' );
-	await page.deleteCookie( ...cookies );
+	await deleteAuthCookie();
 
 	if ( ! isCurrentURL( 'wp-login.php' ) ) {
 		await page.goto( createURL( 'wp-login.php' ) );
