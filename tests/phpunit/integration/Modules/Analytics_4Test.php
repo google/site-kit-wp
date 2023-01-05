695a5eccd387a2bc21ebf13e0eb603b63778d87d
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2023-01-05 19:26:39 +0000

    Add PHPUnit test for GET:report.

--- tests/phpunit/integration/Modules/Analytics_4Test.php
+++ tests/phpunit/integration/Modules/Analytics_4Test.php
@@ -11,12 +11,14 @@
 namespace Google\Site_Kit\Tests\Modules;
 
 use Google\Site_Kit\Context;
+use Google\Site_Kit\Core\Authentication\Authentication;
 use Google\Site_Kit\Core\Modules\Module;
 use Google\Site_Kit\Core\Modules\Module_With_Owner;
 use Google\Site_Kit\Core\Modules\Module_With_Scopes;
 use Google\Site_Kit\Core\Modules\Module_With_Settings;
 use Google\Site_Kit\Core\Modules\Module_With_Service_Entity;
 use Google\Site_Kit\Core\Storage\Options;
+use Google\Site_Kit\Core\Storage\User_Options;
 use Google\Site_Kit\Modules\Analytics_4;
 use Google\Site_Kit\Modules\Analytics_4\Settings;
 use Google\Site_Kit\Tests\Core\Modules\Module_With_Owner_ContractTests;
@@ -29,6 +31,7 @@ use Google\Site_Kit_Dependencies\Google\Service\GoogleAnalyticsAdmin\GoogleAnaly
 use Google\Site_Kit_Dependencies\Google\Service\GoogleAnalyticsAdmin\GoogleAnalyticsAdminV1betaDataStreamWebStreamData;
 use Google\Site_Kit_Dependencies\GuzzleHttp\Message\Request;
 use Google\Site_Kit_Dependencies\GuzzleHttp\Message\Response;
+use Google\Site_Kit_Dependencies\GuzzleHttp\Psr7\Query;
 use Google\Site_Kit_Dependencies\GuzzleHttp\Stream\Stream;
 
 /**
@@ -211,6 +214,188 @@ class Analytics_4Test extends TestCase {
 				'webdatastreams-batch',
 			),
 			$this->analytics->get_datapoints()
+		);
+	}
+
+	public function test_get_report() {
+		$user_id        = $this->factory()->user->create();
+		$context        = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
+		$options        = new Options( $context );
+		$user_options   = new User_Options( $context, $user_id );
+		$authentication = new Authentication( $context, $options, $user_options );
+		$analytics      = new Analytics_4( $context, $options, $user_options, $authentication );
+		$authentication->get_oauth_client()->get_client()->setHttpClient(
+			new FakeHttpClient() // Returns 200 by default.
+		);
+
+		$invocations = array();
+
+		$http_client = new FakeHttpClient();
+		$http_client->set_request_handler(
+			function ( Request $request ) use ( &$invocations ) {
+				$url    = parse_url( $request->getUrl() );
+				$body   = Query::parse( $request->getBody() );
+				$params = json_decode( array_keys( $body )[0], true );
+
+				$invocations[] = array(
+					'url'    => $url,
+					'params' => $params,
+				);
+
+				if ( 'analyticsdata.googleapis.com' !== $url['host'] ) {
+					return new Response( 200 );
+				}
+
+				switch ( $url['path'] ) {
+					case '/v1beta/properties/123456789:batchRunReports':
+						return new Response(
+							200,
+							array(),
+							Stream::factory(
+								json_encode(
+									array(
+										'kind'    => 'analyticsData#batchRunReports',
+										'reports' => array(
+											array(
+												'rows' => array(
+													array(
+														'metricValues' => array(
+															array(
+																'value' => 'some-value',
+															),
+														),
+													),
+												),
+											),
+										),
+									)
+								)
+							)
+						);
+
+					default:
+						return new Response( 200 );
+				}
+			}
+		);
+
+		$analytics->get_client()->setHttpClient( $http_client );
+		$analytics->register();
+
+		// Grant scopes so request doesn't fail.
+		$authentication->get_oauth_client()->set_granted_scopes(
+			$analytics->get_scopes()
+		);
+
+		$data = $analytics->get_data(
+			'report',
+			array(
+				'startDate'        => '2022-11-02',
+				'endDate'          => '2022-11-04',
+				'compareStartDate' => '2022-11-01',
+				'compareEndDate'   => '2022-11-02',
+				'propertyID'       => '123456789',
+				'metrics'          => array(
+					array( 'name' => 'totalUsers' ),
+				),
+				'dimensions'       => array( 'date' ),
+				'dimensionFilters' => array(
+					'sessionDefaultChannelGrouping' => 'Organic Search',
+				),
+			)
+		);
+
+		$this->assertNotWPError( $data );
+
+		// TODO: Refactor the below tests to use a @dataProvider.
+
+		// Verify the reports are returned by checking a metric value.
+		$this->assertEquals( 'some-value', $data[0]['rows'][0][0]['value'] );
+
+		// Verify the request URL and body.
+		$this->assertCount( 1, $invocations );
+
+		$request_url = $invocations[0]['url'];
+
+		$this->assertEquals( 'analyticsdata.googleapis.com', $request_url['host'] );
+		$this->assertEquals( '/v1beta/properties/123456789:batchRunReports', $request_url['path'] );
+
+		$request_params = $invocations[0]['params']['requests'][0];
+
+		$this->assertEquals(
+			array(
+				array(
+					'startDate' => '2022-11-02',
+					'endDate'   => '2022-11-04',
+				),
+				array(
+					'startDate' => '2022-11-01',
+					'endDate'   => '2022-11-02',
+				),
+			),
+			$request_params['dateRanges']
+		);
+
+		$this->assertEquals(
+			array(
+				'andGroup' => array(
+					'expressions' => array(
+						array(
+							'filter' => array(
+								'fieldName'    => 'hostName',
+								'inListFilter' => array(
+									'values' => array(
+										'example.org',
+										'www.example.org',
+									),
+								),
+							),
+						),
+						array(
+							'filter' => array(
+								'fieldName'    => 'sessionDefaultChannelGrouping',
+								'stringFilter' => array(
+									'matchType' => 'EXACT',
+									'value'     => 'Organic Search',
+								),
+							),
+						),
+					),
+				),
+			),
+			$request_params['dimensionFilter']
+		);
+
+		$this->assertEquals(
+			array(
+				array(
+					'name' => 'date',
+				),
+			),
+			$request_params['dimensions']
+		);
+
+		$this->assertEquals(
+			array(
+				'TOTAL',
+				'MINIMUM',
+				'MAXIMUM',
+			),
+			$request_params['metricAggregations']
+		);
+
+		$this->assertEquals(
+			array(
+				array(
+					'name' => 'totalUsers',
+				),
+			),
+			$request_params['metrics']
+		);
+
+		$this->assertEquals(
+			'properties/123456789',
+			$request_params['property']
 		);
 	}
 
