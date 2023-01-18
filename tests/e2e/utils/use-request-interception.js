42bfc2fc637f8ea5e1bc09f0edc9bdf604e9ef50
Hussain Thajutheen <hussain.thajutheen@10up.com>
2023-01-18 18:17:34 +0530

    Add useSharedRequestInterception utility function.

--- tests/e2e/utils/use-request-interception.js
+++ tests/e2e/utils/use-request-interception.js
@@ -26,3 +26,44 @@ export function useRequestInterception( callback ) {
 		page.off( 'request', requestHandler );
 	};
 }
+
+/**
+ * Adds shared request handler for intercepting requests.
+ *
+ * Used intercept HTTP requests during tests with a custom handler function.
+ * Returns a function that, when called, stops further request
+ * handling/interception.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Array.<{isMatch: Function, getResponse: Function}>} requestCases An array of request cases to add to the shared request interception.
+ * @return {Object} Functions that can be called to remove the added handler function from the page and add new request cases.
+ */
+export function useSharedRequestInterception( requestCases ) {
+	const cases = [ ...requestCases ];
+	const requestHandler = ( request ) => {
+		// Prevent errors for requests that happen after interception is disabled.
+		if ( ! request._allowInterception ) {
+			return;
+		}
+
+		const requestCase = cases.find( ( { isMatch } ) => {
+			return isMatch( request );
+		} );
+
+		if ( requestCase ) {
+			request.respond( requestCase.getResponse() );
+		} else {
+			request.continue();
+		}
+	};
+
+	page.on( 'request', requestHandler );
+
+	return {
+		cleanup: () => page.off( 'request', requestHandler ),
+		addRequestCases: ( newCases ) => {
+			cases.push( ...newCases );
+		},
+	};
+}
