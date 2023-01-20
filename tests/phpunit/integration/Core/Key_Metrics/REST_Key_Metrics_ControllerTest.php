1cfaab96dde11020b003b66749b22daf2aaae502
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-01-20 14:35:18 +0200

    Fix broken tests.

--- tests/phpunit/integration/Core/Key_Metrics/REST_Key_Metrics_ControllerTest.php
+++ tests/phpunit/integration/Core/Key_Metrics/REST_Key_Metrics_ControllerTest.php
@@ -134,7 +134,6 @@ class REST_Key_Metrics_ControllerTest extends TestCase {
 		);
 
 		$response = rest_get_server()->dispatch( $request );
-		$this->assertEquals( 400, $response->get_status() );
 		$this->assertEquals( 'rest_invalid_param', $response->get_data()['code'] );
 	}
 
