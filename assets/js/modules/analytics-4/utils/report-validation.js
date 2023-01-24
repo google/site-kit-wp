cd523f9d96317d2132d884cdab2a2d3a1159923d
Julian Weiland <julian.weiland@10up.com>
2023-01-24 17:05:10 +0100

    Update file comments.

--- assets/js/modules/analytics-4/utils/report-validation.js
+++ assets/js/modules/analytics-4/utils/report-validation.js
@@ -1,7 +1,7 @@
 /**
- * Reporting API validation utilities.
+ * Analytics 4 reporting API validation utilities.
  *
- * Site Kit by Google, Copyright 2022 Google LLC
+ * Site Kit by Google, Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- assets/js/modules/analytics-4/utils/report-validation.test.js
+++ assets/js/modules/analytics-4/utils/report-validation.test.js
@@ -1,7 +1,7 @@
 /**
- * Tests for reporting API validation utilities.
+ * Tests for Analyticsc 4 reporting API validation utilities.
  *
- * Site Kit by Google, Copyright 2022 Google LLC
+ * Site Kit by Google, Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
