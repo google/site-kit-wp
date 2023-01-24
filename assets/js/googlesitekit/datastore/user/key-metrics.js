536fc51856270f2cb7999f5c8f98fc3fa79de287
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-01-24 19:11:08 +0200

    Add an action and a fetch store.

---
+++ assets/js/googlesitekit/datastore/user/key-metrics.js
@@ -0,0 +1,108 @@
+/**
+ * Site Kit by Google, Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import invariant from 'invariant';
+import isPlainObject from 'lodash/isPlainObject';
+
+/**
+ * Internal dependencies
+ */
+import API from 'googlesitekit-api';
+import Data from 'googlesitekit-data';
+import { CORE_USER } from './constants';
+import { createFetchStore } from '../../data/create-fetch-store';
+
+const baseInitialState = {
+	keyMetrics: undefined,
+};
+
+const fetchSaveKeyMetricsStore = createFetchStore( {
+	baseName: 'saveKeyMetrics',
+	controlCallback: ( settings ) =>
+		API.set( 'core', 'user', 'key-metrics', { settings } ),
+	reducerCallback: ( state, keyMetrics ) => ( { ...state, keyMetrics } ),
+	argsToParams: ( settings ) => settings,
+	validateParams: ( settings ) => {
+		invariant( isPlainObject( settings ), 'valid settings are required.' );
+	},
+} );
+
+const baseActions = {
+	/**
+	 * Saves key metrics settings.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return {Object} Object with `response` and `error`.
+	 */
+	*saveKeyMetrics() {
+		const registry = yield Data.commonActions.getRegistry();
+		const keyMetrics = registry.select( CORE_USER ).getKeyMetrics();
+
+		const { response, error } =
+			yield fetchSaveKeyMetricsStore.actions.fetchSaveKeyMetrics(
+				keyMetrics
+			);
+
+		return { response, error };
+	},
+};
+
+const baseControls = {};
+
+const baseReducer = ( state, { type } ) => {
+	switch ( type ) {
+		default:
+			return state;
+	}
+};
+
+const baseResolvers = {};
+
+const baseSelectors = {
+	/**
+	 * Gets key metrics selected by the user.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {Object} state Data store's state.
+	 * @return {(Object|undefined)} Key metrics settings.
+	 */
+	getKeyMetrics( state ) {
+		return state.keyMetrics;
+	},
+};
+
+const store = Data.combineStores( fetchSaveKeyMetricsStore, {
+	initialState: baseInitialState,
+	actions: baseActions,
+	controls: baseControls,
+	reducer: baseReducer,
+	resolvers: baseResolvers,
+	selectors: baseSelectors,
+} );
+
+export const initialState = store.initialState;
+export const actions = store.actions;
+export const controls = store.controls;
+export const reducer = store.reducer;
+export const resolvers = store.resolvers;
+export const selectors = store.selectors;
+
+export default store;
