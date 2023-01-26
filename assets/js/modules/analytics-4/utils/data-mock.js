73c3aeb4664857baf67ce11b68ae93590552e0a5
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2023-01-26 13:39:46 +0000

    Add a descriptive comment.

--- assets/js/modules/analytics-4/utils/data-mock.js
+++ assets/js/modules/analytics-4/utils/data-mock.js
@@ -186,6 +186,7 @@ function generateDateRange( startDate, endDate ) {
 	const end = stringToDate( endDate );
 
 	while ( currentDate.getTime() <= end.getTime() ) {
+		// Ensure the generated dates are the same regardless of local timezone.
 		const year = currentDate.getFullYear();
 		const month = String( currentDate.getMonth() + 1 ).padStart( 2, '0' );
 		const day = String( currentDate.getDate() ).padStart( 2, '0' );
