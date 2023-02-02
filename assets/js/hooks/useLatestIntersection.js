3f7271b2f6185380582e145e066c5854c457e9ff
Arafat Zahan <arafat.zahan@10up.com>
2023-02-03 00:07:23 +0600

    Add `useLatestIntersection` hook.

---
+++ assets/js/hooks/useLatestIntersection.js
@@ -0,0 +1,58 @@
+/**
+ * `useLatestIntersection` hook.
+ *
+ * Site Kit by Google, Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useState } from '@wordpress/element';
+
+/**
+ * Returns latest intersaction observer entry for a given element.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Object} ref     The ref object of html element.
+ * @param {Object} options Options for the intersaction observer.
+ * @return {IntersectionObserverEntry | null} The latest intersaction observer entry.
+ */
+const useLatestIntersection = ( ref, options ) => {
+	const [ intersectionObserverEntry, setIntersectionObserverEntry ] =
+		useState( null );
+
+	useEffect( () => {
+		if ( ref.current && typeof IntersectionObserver === 'function' ) {
+			const handler = ( entries ) => {
+				setIntersectionObserverEntry( entries[ entries.length - 1 ] );
+			};
+
+			const observer = new IntersectionObserver( handler, options );
+			observer.observe( ref.current );
+
+			return () => {
+				setIntersectionObserverEntry( null );
+				observer.disconnect();
+			};
+		}
+		return () => {};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ ref.current, options.threshold, options.root, options.rootMargin ] );
+
+	return intersectionObserverEntry;
+};
+
+export default useLatestIntersection;
