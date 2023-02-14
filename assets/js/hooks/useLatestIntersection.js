a8a4bdef6d778c3cb7cfb4aa44a95e5938341771
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2023-02-14 14:41:20 +0000

    Update JSDoc.

--- assets/js/hooks/useLatestIntersection.js
+++ assets/js/hooks/useLatestIntersection.js
@@ -24,9 +24,9 @@ import { useEffect, useState } from '@wordpress/element';
 /**
  * Returns latest intersaction observer entry for a given element.
  *
- * This hook is based on the `useIntersection` hook from `react-use` package. The difference
+ * This hook is based on the `useIntersection` hook from the `react-use` package. The difference
  * is that this hook returns the last intersection observer entry instead of the first one.
- *
+ * See https://github.com/streamich/react-use/blob/325f5bd69904346788ea981ec18bfc7397c611df/src/useIntersection.ts.
  * @since n.e.x.t
  *
  * @param {Object} ref     A ref object that points to the HTML element to observe.
