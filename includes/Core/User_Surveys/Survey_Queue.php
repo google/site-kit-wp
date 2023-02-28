d3347e813aea3ad2e2fabd2043f7a5832ecb0fcd
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-02-28 19:01:32 +0200

    Add the Survey_Queue class.

---
+++ includes/Core/User_Surveys/Survey_Queue.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class Google\Site_Kit\Core\User_Surveys\Survey_Queue
+ *
+ * @package   Google\Site_Kit\Core\User_Surveys
+ * @copyright 2023 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Core\User_Surveys;
+
+/**
+ * Class for handling surveys queue.
+ *
+ * @since n.e.x.t
+ * @access private
+ * @ignore
+ */
+class Survey_Queue {
+
+	/**
+	 * Registers functionality through WordPress hooks.
+	 *
+	 * @since n.e.x.t
+	 */
+	public function register() {
+	}
+
+}
