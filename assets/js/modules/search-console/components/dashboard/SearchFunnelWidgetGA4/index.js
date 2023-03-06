b57f64adb2c57a854076de47f604aa5f5a721d02
Hussain Thajutheen <hussain.thajutheen@10up.com>
2023-03-06 21:17:29 +0530

    Remove percentage in graph tooltip.

--- assets/js/modules/search-console/components/dashboard/SearchFunnelWidgetGA4/index.js
+++ assets/js/modules/search-console/components/dashboard/SearchFunnelWidgetGA4/index.js
@@ -39,7 +39,6 @@ import {
 import { DATE_RANGE_OFFSET as DATE_RANGE_OFFSET_ANALYTICS } from '../../../../analytics/datastore/constants';
 import { CORE_SITE } from '../../../../../googlesitekit/datastore/site/constants';
 import { CORE_USER } from '../../../../../googlesitekit/datastore/user/constants';
-import { numFmt } from '../../../../../util';
 import PreviewBlock from '../../../../../components/PreviewBlock';
 import Header from '../SearchFunnelWidget/Header';
 import Footer from '../SearchFunnelWidget/Footer';
@@ -468,12 +467,7 @@ const SearchFunnelWidgetGA4 = ( { Widget, WidgetReportError } ) => {
 						] }
 						dataFormats={ [
 							( x ) => parseFloat( x ).toLocaleString(),
-							( x ) =>
-								numFmt( x / 100, {
-									style: 'percent',
-									signDisplay: 'never',
-									maximumFractionDigits: 2,
-								} ),
+							( x ) => parseFloat( x ).toLocaleString(),
 						] }
 						statsColor={
 							SearchFunnelWidgetGA4.metrics[ selectedStats ].color
