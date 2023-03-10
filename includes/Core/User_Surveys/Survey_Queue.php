831edc1a2f239bf430c98936c16e5379f4101d56
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-03-10 11:58:53 +0200

    Apply suggestions from code review
    Co-authored-by: Evan Mattson <emattson@google.com>

--- includes/Core/User_Surveys/Survey_Queue.php
+++ includes/Core/User_Surveys/Survey_Queue.php
@@ -34,7 +34,7 @@ class Survey_Queue extends Array_Setting {
 	public function enqueue( $survey ) {
 		$surveys = $this->get();
 
-		// Do no add the survey if it is already in the queue.
+		// Do not add the survey if it is already in the queue.
 		foreach ( $surveys as $item ) {
 			if ( $item['survey_id'] === $survey['survey_id'] ) {
 				return false;
@@ -86,7 +86,7 @@ class Survey_Queue extends Array_Setting {
 	 */
 	public function front() {
 		$surveys = $this->get();
-		return count( $surveys ) > 0 ? $surveys[0] : null;
+		return reset( $surveys ) ?: null;
 	}
 
 	/**
