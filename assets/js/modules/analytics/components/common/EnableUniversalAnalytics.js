247141dc8c84483ef8ec8c34446e901daada8c07
nfmohit <nahid.mohit@10up.com>
2023-04-11 02:44:17 +0600

    Update `EnableUniversalAnalytics` to handle module access.

--- assets/js/modules/analytics/components/common/EnableUniversalAnalytics.js
+++ assets/js/modules/analytics/components/common/EnableUniversalAnalytics.js
@@ -25,8 +25,12 @@ import { useMount } from 'react-use';
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { Fragment, useCallback } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
+import {
+	Fragment,
+	useCallback,
+	createInterpolateElement,
+} from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -34,12 +38,19 @@ import { Fragment, useCallback } from '@wordpress/element';
 import Data from 'googlesitekit-data';
 import { Switch } from 'googlesitekit-components';
 import { CORE_FORMS } from '../../../../googlesitekit/datastore/forms/constants';
+import { CORE_MODULES } from '../../../../googlesitekit/modules/datastore/constants';
 import { MODULES_ANALYTICS, FORM_SETUP } from '../../datastore/constants';
+import { TYPE_INFO } from '../../../../components/SettingsNotice';
 import ProfileSelect from './ProfileSelect';
 import PropertySelect from './PropertySelect';
+import SettingsNotice from '../../../../components/SettingsNotice/SettingsNotice';
+import WarningIcon from '../../../../../../assets/svg/icons/warning-icon.svg';
 const { useSelect, useDispatch } = Data;
 
-export default function EnableUniversalAnalytics( { children } ) {
+export default function EnableUniversalAnalytics( {
+	children,
+	hasModuleAccess = true,
+} ) {
 	const accountID = useSelect( ( select ) =>
 		select( MODULES_ANALYTICS ).getAccountID()
 	);
@@ -53,14 +64,16 @@ export default function EnableUniversalAnalytics( { children } ) {
 	const propertyID = useSelect( ( select ) =>
 		select( MODULES_ANALYTICS ).getPropertyID()
 	);
+	const isUAEnabled = useSelect( ( select ) =>
+		select( CORE_FORMS ).getValue( FORM_SETUP, 'enableUA' )
+	);
+	const module = useSelect( ( select ) =>
+		select( CORE_MODULES ).getModule( 'analytics' )
+	);
 
 	const { setValues } = useDispatch( CORE_FORMS );
 	const { resetPropertyAndProfileIDs, revertPropertyAndProfileIDs } =
 		useDispatch( MODULES_ANALYTICS );
-
-	const isUAEnabled = useSelect( ( select ) =>
-		select( CORE_FORMS ).getValue( FORM_SETUP, 'enableUA' )
-	);
 
 	const onChange = useCallback( () => {
 		if ( isUAEnabled ) {
@@ -76,6 +89,10 @@ export default function EnableUniversalAnalytics( { children } ) {
 		resetPropertyAndProfileIDs,
 		revertPropertyAndProfileIDs,
 	] );
+
+	const formattedOwnerName = module?.owner?.login
+		? `<strong>${ module.owner.login }</strong>`
+		: __( 'Another admin', 'google-site-kit' );
 
 	useMount( () => {
 		if ( propertyID ) {
@@ -109,12 +126,33 @@ export default function EnableUniversalAnalytics( { children } ) {
 			{ isUAEnabled && (
 				<Fragment>
 					<div className="googlesitekit-setup-module__inputs">
-						<PropertySelect />
-						<ProfileSelect />
+						<PropertySelect hasModuleAccess={ hasModuleAccess } />
+						<ProfileSelect hasModuleAccess={ hasModuleAccess } />
 					</div>
 
 					{ /* Renders the SetupUseSnippetSwitch or SettingsUseSnippetSwitch */ }
 					{ children }
+
+					{ hasModuleAccess === false && (
+						<SettingsNotice
+							type={ TYPE_INFO }
+							Icon={ WarningIcon }
+							notice={ createInterpolateElement(
+								sprintf(
+									/* translators: 1: module owner's name, 2: module name */
+									__(
+										'%1$s configured %2$s and you don’t have access to its configured property. Contact them to share access or change the configured property.',
+										'google-site-kit'
+									),
+									formattedOwnerName,
+									module?.name
+								),
+								{
+									strong: <strong />,
+								}
+							) }
+						/>
+					) }
 				</Fragment>
 			) }
 		</Fragment>
@@ -123,4 +161,5 @@ export default function EnableUniversalAnalytics( { children } ) {
 
 EnableUniversalAnalytics.propTypes = {
 	children: PropTypes.node.isRequired,
+	hasModuleAccess: PropTypes.bool,
 };
