88a34502b856adee88800c97988b70a2916ffd94
nfmohit <nahid.mohit@10up.com>
2023-05-14 00:18:18 +0200

    Add Ad Blocker Detection settings to Adsense.

--- includes/Modules/AdSense/Settings.php
+++ includes/Modules/AdSense/Settings.php
@@ -14,6 +14,7 @@ use Google\Site_Kit\Core\Modules\Module_Settings;
 use Google\Site_Kit\Core\Storage\Setting_With_Legacy_Keys_Trait;
 use Google\Site_Kit\Core\Storage\Setting_With_Owned_Keys_Interface;
 use Google\Site_Kit\Core\Storage\Setting_With_Owned_Keys_Trait;
+use Google\Site_Kit\Core\Util\Feature_Flags;
 
 /**
  * Class for AdSense settings.
@@ -149,7 +150,7 @@ class Settings extends Module_Settings implements Setting_With_Owned_Keys_Interf
 	 * @return array
 	 */
 	protected function get_default() {
-		return array(
+		$settings = array(
 			'ownerID'              => 0,
 			'accountID'            => '',
 			'autoAdsDisabled'      => array(),
@@ -161,6 +162,14 @@ class Settings extends Module_Settings implements Setting_With_Owned_Keys_Interf
 			'useSnippet'           => true,
 			'webStoriesAdUnit'     => '',
 		);
+
+		if ( Feature_Flags::enabled( 'adBlockerDetection' ) ) {
+			$settings['useAdBlockerDetectionSnippet']      = false;
+			$settings['useAdBlockerDetectionErrorSnippet'] = false;
+			$settings['adBlockingRecoverySetupStatus']     = '';
+		}
+
+		return $settings;
 	}
 
 	/**
