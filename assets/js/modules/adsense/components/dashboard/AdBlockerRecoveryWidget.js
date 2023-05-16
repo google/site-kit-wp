c85bc6b6c35479f0f2b33072ad37ee00ba585062
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-05-16 17:09:16 +0300

    Split the BannerNotification component into a few more smaller components and updated the widget to use them.

--- assets/js/modules/adsense/components/dashboard/AdBlockerRecoveryWidget.js
+++ assets/js/modules/adsense/components/dashboard/AdBlockerRecoveryWidget.js
@@ -22,63 +22,73 @@ import PropTypes from 'prop-types';
 /**
  * WordPress dependencies
  */
-import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
-import { CORE_SITE } from '../../../../googlesitekit/datastore/site/constants';
 import GA4SuccessGreenSVG from '../../../../../svg/graphics/ga4-success-green.svg';
-import BannerNotification from '../../../../components/notifications/BannerNotification';
+import { CORE_SITE } from '../../../../googlesitekit/datastore/site/constants';
+import { Cell } from '../../../../material-components';
+import BannerTitle from '../../../../components/notifications/BannerNotification/BannerTitle';
+import BannerActions from '../../../../components/notifications/BannerNotification/BannerActions';
+import Banner from '../../../../components/notifications/BannerNotification/Banner';
 const { useSelect } = Data;
 
 export default function AdBlockerRecoveryWidget( { Widget } ) {
+	const learnMore = useSelect( ( select ) =>
+		select( CORE_SITE ).getGoogleSupportURL( '/adsense/answer/11576589' )
+	);
+
 	const pageURL = useSelect( ( select ) =>
 		select( CORE_SITE ).getAdminURL( 'googlesitekit-ad-blocking-recovery' )
 	);
 
 	return (
 		<Widget>
-			<BannerNotification
-				id="switched-ga4-dashboard-view"
-				title={ __(
-					'Recover revenue lost to ad blockers',
-					'google-site-kit'
-				) }
-				ctaLabel={ __( 'Set up now', 'google-site-kit' ) }
-				ctaLink={ pageURL }
-				onDismiss={ () => {} }
-				dismiss={ __( 'Maybe later', 'google-site-kit' ) }
-				WinImageSVG={ () => (
-					<Fragment>
-						<GA4SuccessGreenSVG />
-						<p>
-							{ __(
-								'* Average for publishers showing non-dismissible ad blocking recovery messages placed at the center of the page on desktop',
-								'google-site-kit'
-							) }
-						</p>
-					</Fragment>
-				) }
-			>
-				<p>
-					{ __(
-						'Display a message to give site visitors with an ad blocker the option to allow ads on your site. Site Kit will place an ad blocking recovery tag on your site.',
-						'google-site-kit'
-					) }{ ' ' }
-					<a href={ pageURL }>
-						{ __( 'Learn more', 'google-site-kit' ) }
-					</a>
-				</p>
-				<p>
-					{ __(
-						'Publishers see up to 1 in 5 users choose to allow ads once they encounter an ad blocking recovery message*',
-						'google-site-kit'
-					) }
-				</p>
-			</BannerNotification>
+			<Banner>
+				<Cell lgSize={ 8 }>
+					<BannerTitle
+						title={ __(
+							'Recover revenue lost to ad blockers',
+							'google-site-kit'
+						) }
+					/>
+
+					<p>
+						{ __(
+							'Display a message to give site visitors with an ad blocker the option to allow ads on your site. Site Kit will place an ad blocking recovery tag on your site.',
+							'google-site-kit'
+						) }{ ' ' }
+						<a href={ learnMore }>
+							{ __( 'Learn more', 'google-site-kit' ) }
+						</a>
+					</p>
+					<p>
+						{ __(
+							'Publishers see up to 1 in 5 users choose to allow ads once they encounter an ad blocking recovery message*',
+							'google-site-kit'
+						) }
+					</p>
+
+					<BannerActions
+						ctaLabel={ __( 'Set up now', 'google-site-kit' ) }
+						ctaLink={ pageURL }
+						dismissCallback={ () => {} }
+						dismissLabel={ __( 'Maybe later', 'google-site-kit' ) }
+					/>
+				</Cell>
+				<Cell lgSize={ 4 }>
+					<GA4SuccessGreenSVG />
+					<p>
+						{ __(
+							'* Average for publishers showing non-dismissible ad blocking recovery messages placed at the center of the page on desktop',
+							'google-site-kit'
+						) }
+					</p>
+				</Cell>
+			</Banner>
 		</Widget>
 	);
 }
