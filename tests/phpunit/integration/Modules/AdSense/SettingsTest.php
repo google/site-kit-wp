a63ede85bb0a0a20276e2d89801368fcbe0eb320
Arafat Zahan <arafat.zahan@10up.com>
2023-05-25 22:27:17 +0600

    Add test for existing setup.

--- tests/phpunit/integration/Modules/AdSense/SettingsTest.php
+++ tests/phpunit/integration/Modules/AdSense/SettingsTest.php
@@ -116,7 +116,7 @@ class SettingsTest extends SettingsTestCase {
 		$this->assertArrayNotHasKey( 'account_id', $option );
 	}
 
-	public function test_setup_completed_timestamp() {
+	public function test_setup_completed_timestamp__new_setup() {
 		$settings = new Settings( new Options( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) );
 		$settings->register();
 
@@ -150,7 +150,39 @@ class SettingsTest extends SettingsTestCase {
 		);
 
 		$this->assertTrue( gmdate( 'Ymd' ) === gmdate( 'Ymd', $settings->get()['setupCompletedTimestamp'] ) );
+	}
 
+	public function test_setup_completed_timestamp__existing_setup() {
+		$settings = new Settings( new Options( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) ) );
+		$settings->register();
+
+		$this->assertNull( $settings->get()['setupCompletedTimestamp'] );
+
+		remove_all_filters( 'pre_update_option_' . Settings::OPTION );
+
+		// Change both account and site status.
+		$settings->merge(
+			array(
+				'accountStatus' => 'ready',
+				'siteStatus'    => 'ready',
+			)
+		);
+		$settings->register();
+
+		$this->assertNull( $settings->get()['setupCompletedTimestamp'] );
+
+		// Change any AdSense setting.
+		$settings->merge(
+			array(
+				'useSnippet' => false,
+			)
+		);
+
+		$test_date            = (int) gmdate( 'Ymd', strtotime( '-1 month' ) );
+		$setup_completed_date = (int) gmdate( 'Ymd', $settings->get()['setupCompletedTimestamp'] );
+
+		// Test date can be greater than setup completed date if the `setupCompletedTimestamp` was saved on midnight.
+		$this->assertTrue( $test_date >= $setup_completed_date );
 	}
 
 	protected function get_testcase() {
