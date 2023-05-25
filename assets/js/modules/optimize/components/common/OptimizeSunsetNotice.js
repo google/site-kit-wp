f82f0e6467f9254a62d9bd6a049d3d2802547296
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-05-25 20:44:00 +0300

    Update the OptimizeSunsetNotice component.

--- assets/js/modules/optimize/components/common/OptimizeSunsetNotice.js
+++ assets/js/modules/optimize/components/common/OptimizeSunsetNotice.js
@@ -22,16 +22,23 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import SupportLink from '../../../../components/SupportLink';
 import SettingsNotice from '../../../../components/SettingsNotice';
 import { TYPE_WARNING } from '../../../../components/SettingsNotice/utils';
 
 export default function OptimizeSunsetNotice() {
 	return (
-		<SettingsNotice type={ TYPE_WARNING }>
-			{ __(
+		<SettingsNotice
+			type={ TYPE_WARNING }
+			notice={ __(
 				'Optimize will no longer work after September 30, 2023 and will be removed from subsequent versions of Site Kit after that date.',
 				'google-site-kit'
 			) }
-		</SettingsNotice>
+			LearnMore={ () => (
+				<SupportLink path="/optimize/answer/12979939" external>
+					{ __( 'Learn more', 'google-site-kit' ) }
+				</SupportLink>
+			) }
+		/>
 	);
 }
