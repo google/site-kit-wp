6e1213056e61e9c98b26510c43ca2cbb94b89b29
nfmohit <nahid.mohit@10up.com>
2023-05-27 12:24:04 +0100

    Track events in GoogleCharts DateMarkers.

---
+++ assets/js/components/GoogleChart/DateMarkers.js
@@ -0,0 +1,70 @@
+/**
+ * GoogleChart DateMarkers component.
+ *
+ * Site Kit by Google, Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { Fragment, useEffect } from '@wordpress/element';
+import { Icon, info } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import { Tooltip } from 'googlesitekit-components';
+import useViewContext from '../../hooks/useViewContext';
+import { trackEvent } from '../../util';
+
+export default function DateMarkers( { id, dateMarker } ) {
+	const viewContext = useViewContext();
+
+	const eventCategory = `${ viewContext }_ga4-data-collection-line`;
+
+	useEffect( () => {
+		trackEvent( eventCategory, 'chart_line_view' );
+	} );
+
+	return (
+		<Fragment>
+			<div
+				id={ `googlesitekit-chart__date-marker-line--${ id }` }
+				className="googlesitekit-chart__date-marker-line"
+			/>
+			{ dateMarker.text && (
+				<div
+					id={ `googlesitekit-chart__date-marker-tooltip--${ id }` }
+					className="googlesitekit-chart__date-marker-tooltip"
+				>
+					<Tooltip
+						title={ dateMarker.text }
+						onOpen={ () => {
+							trackEvent( eventCategory, 'chart_tooltip_view' );
+						} }
+					>
+						<span>
+							<Icon
+								fill="currentColor"
+								icon={ info }
+								size={ 18 }
+							/>
+						</span>
+					</Tooltip>
+				</div>
+			) }
+		</Fragment>
+	);
+}
