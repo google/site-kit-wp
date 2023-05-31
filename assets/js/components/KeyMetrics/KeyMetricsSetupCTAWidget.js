82a5dc26bc1b02c87039531569d39325a95a219e
hussain-t <hussain.thajutheen@10up.com>
2023-05-31 15:58:35 +0530

    Design KeyMetricsSetupCTAWidget component.

--- assets/js/components/KeyMetrics/KeyMetricsSetupCTAWidget.js
+++ assets/js/components/KeyMetrics/KeyMetricsSetupCTAWidget.js
@@ -22,12 +22,23 @@
 import PropTypes from 'prop-types';
 
 /**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
+import { Button } from 'googlesitekit-components';
+import { Cell, Grid, Row } from '../../material-components';
+import GhostCardsSVG from './GhostCardsSVG';
 import { CORE_MODULES } from '../../googlesitekit/modules/datastore/constants';
 import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
 import { useFeature } from '../../hooks/useFeature';
+import Link from '../Link';
+import { CORE_SITE } from '../../googlesitekit/datastore/site/constants';
+import { BREAKPOINT_SMALL, useBreakpoint } from '../../hooks/useBreakpoint';
 const { useSelect } = Data;
 
 function KeyMetricsSetupCTAWidget( { Widget, WidgetNull } ) {
@@ -36,13 +47,18 @@ function KeyMetricsSetupCTAWidget( { Widget, WidgetNull } ) {
 	const isUserInputCompleted = useSelect( ( select ) =>
 		select( CORE_USER ).isUserInputCompleted()
 	);
-
 	const searchConsoleModuleConnected = useSelect( ( select ) =>
 		select( CORE_MODULES ).isModuleConnected( 'search-console' )
 	);
 	const analyticsModuleConnected = useSelect( ( select ) =>
 		select( CORE_MODULES ).isModuleConnected( 'analytics-4' )
 	);
+	const ctaLink = useSelect( ( select ) =>
+		select( CORE_SITE ).getAdminURL( 'googlesitekit-user-input' )
+	);
+
+	const breakpoint = useBreakpoint();
+	const mobileBreakpoint = breakpoint === BREAKPOINT_SMALL;
 
 	if (
 		! userInputEnabled ||
@@ -54,9 +70,60 @@ function KeyMetricsSetupCTAWidget( { Widget, WidgetNull } ) {
 		return <WidgetNull />;
 	}
 
+	const Footer = () => {
+		return (
+			<Row>
+				<Cell size={ 12 }>
+					<Link href="#">
+						{ __( 'Maybe later', 'google-site-kit' ) }
+					</Link>
+				</Cell>
+			</Row>
+		);
+	};
+
 	return (
-		<Widget noPadding>
-			<div>TODO: UI for KeyMetricsSetupCTAWidget</div>
+		<Widget noPadding Footer={ Footer }>
+			<section className="googlesitekit-plugin googlesitekit-setup__wrapper">
+				<Grid>
+					<Row>
+						<Cell smSize={ 6 } mdSize={ 5 } lgSize={ 6 }>
+							<h3 className="googlesitekit-publisher-win__title">
+								{ __(
+									'Get metrics and suggestions tailored to your specific goals',
+									'google-site-kit'
+								) }
+							</h3>
+							{ mobileBreakpoint && <GhostCardsSVG /> }
+							<p>
+								{ __(
+									'Answer 3 questions to show relevant stats for your site',
+									'google-site-kit'
+								) }
+							</p>
+							<div className="googlesitekit-widget-km-actions__wrapper">
+								<Button href={ ctaLink }>
+									{ __(
+										'Get tailored metrics',
+										'google-site-kit'
+									) }
+								</Button>
+								<Link href="#">
+									{ __(
+										'I’ll pick metrics myself',
+										'google-site-kit'
+									) }
+								</Link>
+							</div>
+						</Cell>
+						{ ! mobileBreakpoint && (
+							<Cell smSize={ 6 } mdSize={ 3 } lgSize={ 6 }>
+								<GhostCardsSVG />
+							</Cell>
+						) }
+					</Row>
+				</Grid>
+			</section>
 		</Widget>
 	);
 }
