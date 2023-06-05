a92a405ce24084fe6e58ce1c7719fcd601199714
hussain-t <hussain.thajutheen@10up.com>
2023-06-05 20:13:35 +0530

    Intercept key metrics endpoint.

--- tests/e2e/specs/admin-tracking.test.js
+++ tests/e2e/specs/admin-tracking.test.js
@@ -45,6 +45,21 @@ async function toggleOptIn() {
 }
 
 describe( 'management of tracking opt-in/out via settings page', () => {
+	beforeAll( async () => {
+		await page.setRequestInterception( true );
+		useRequestInterception( ( request ) => {
+			if (
+				request.url.match(
+					'/google-site-kit/v1/core/user/data/key-metrics'
+				)
+			) {
+				request.respond( { status: 200, body: JSON.stringify( [] ) } );
+			} else {
+				request.continue();
+			}
+		} );
+	} );
+
 	beforeEach( async () => {
 		await activatePlugin( 'e2e-tests-proxy-auth-plugin' );
 		await activatePlugin( 'e2e-tests-site-verification-plugin' );
@@ -55,6 +70,7 @@ describe( 'management of tracking opt-in/out via settings page', () => {
 		await expect( page ).toMatchElement( 'a.mdc-tab', {
 			text: 'Admin Settings',
 		} );
+		// /google-site-kit/v1/core/user/data/key-metrics
 
 		await pageWait(); // Delay the next steps.
 
