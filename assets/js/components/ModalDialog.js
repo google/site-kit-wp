e5d8dfa5202a9a3cb88367f990c7ac58b2f2f983
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2023-06-07 11:19:15 +0100

    Refactor Dialog components.

--- assets/js/components/ModalDialog.js
+++ assets/js/components/ModalDialog.js
@@ -1,7 +1,7 @@
 /**
- * Dialog component.
+ * ModalDialog component.
  *
- * Site Kit by Google, Copyright 2021 Google LLC
+ * Site Kit by Google, Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,24 +20,21 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import FocusTrap from 'focus-trap-react';
-import classnames from 'classnames';
 
 /**
  * WordPress dependencies
  */
 import { useInstanceId } from '@wordpress/compose';
-import { createInterpolateElement, useCallback } from '@wordpress/element';
+import { Fragment, createInterpolateElement } from '@wordpress/element';
 import { sprintf, __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import SpinnerButton from './SpinnerButton';
-import Link from '../../components/Link';
-import { MDCDialog } from '../../material-components';
+import { SpinnerButton, Dialog } from 'googlesitekit-components';
+import Link from './Link';
 
-const Dialog = ( {
+const ModalDialog = ( {
 	dialogActive,
 	handleDialog,
 	title,
@@ -49,123 +46,86 @@ const Dialog = ( {
 	danger,
 	inProgress = false,
 } ) => {
-	const dialogRef = useCallback( ( el ) => {
-		if ( el !== null ) {
-			MDCDialog.attachTo( el );
-		}
-	}, [] );
-
-	// eslint-disable-next-line sitekit/acronym-case
-	const instanceID = useInstanceId( Dialog );
-	const labelledByID = `googlesitekit-dialog-label-${ instanceID }`;
+	const instanceID = useInstanceId( ModalDialog );
 	const describedByID = `googlesitekit-dialog-description-${ instanceID }`;
 	const hasProvides = !! ( provides && provides.length );
 
 	return (
-		<div
-			ref={ dialogRef }
-			className={ classnames( 'mdc-dialog', {
-				'mdc-dialog--open': dialogActive,
-			} ) }
-			role="alertdialog"
-			aria-modal="true"
-			aria-labelledby={ title ? labelledByID : undefined }
+		<Dialog
+			open={ dialogActive }
 			aria-describedby={ hasProvides ? describedByID : undefined }
-			aria-hidden={ dialogActive ? 'false' : 'true' }
-			tabIndex="-1"
-		>
-			<div className="mdc-dialog__scrim">&nbsp;</div>
-			<FocusTrap
-				active={ dialogActive }
-				focusTrapOptions={ {
-					tabbableOptions: {
-						displayCheck: 'legacy-full',
-					},
-				} }
-			>
-				<div>
-					<div className="mdc-dialog__container">
-						<div className="mdc-dialog__surface">
-							{ title && (
-								<h2
-									id={ labelledByID }
-									className="mdc-dialog__title"
-								>
-									{ title }
-								</h2>
+			title={ title }
+			content={
+				<Fragment>
+					{ subtitle && (
+						<p className="mdc-dialog__lead">{ subtitle }</p>
+					) }
+					{ hasProvides && (
+						<section
+							id={ describedByID }
+							className="mdc-dialog__content"
+						>
+							<ul className="mdc-list mdc-list--underlined mdc-list--non-interactive">
+								{ provides.map( ( attribute ) => (
+									<li
+										className="mdc-list-item"
+										key={ attribute }
+									>
+										<span className="mdc-list-item__text">
+											{ attribute }
+										</span>
+									</li>
+								) ) }
+							</ul>
+						</section>
+					) }
+					{ dependentModules && (
+						<p className="mdc-dialog__dependecies">
+							{ createInterpolateElement(
+								sprintf(
+									/* translators: %s is replaced with the dependent modules. */
+									__(
+										'<strong>Note:</strong> %s',
+										'google-site-kit'
+									),
+									dependentModules
+								),
+								{
+									strong: <strong />,
+								}
 							) }
-							{ subtitle && (
-								<p className="mdc-dialog__lead">{ subtitle }</p>
-							) }
-							{ hasProvides && (
-								<section
-									id={ describedByID }
-									className="mdc-dialog__content"
-								>
-									<ul className="mdc-list mdc-list--underlined mdc-list--non-interactive">
-										{ provides.map( ( attribute ) => (
-											<li
-												className="mdc-list-item"
-												key={ attribute }
-											>
-												<span className="mdc-list-item__text">
-													{ attribute }
-												</span>
-											</li>
-										) ) }
-									</ul>
-								</section>
-							) }
-							{ dependentModules && (
-								<p className="mdc-dialog__dependecies">
-									{ createInterpolateElement(
-										sprintf(
-											/* translators: %s is replaced with the dependent modules. */
-											__(
-												'<strong>Note:</strong> %s',
-												'google-site-kit'
-											),
-											dependentModules
-										),
-										{
-											strong: <strong />,
-										}
-									) }
-								</p>
-							) }
-							<footer className="mdc-dialog__actions">
-								<SpinnerButton
-									onClick={ handleConfirm }
-									danger={ danger }
-									disabled={ inProgress }
-									isSaving={ inProgress }
-								>
-									{ confirmButton
-										? confirmButton
-										: __(
-												'Disconnect',
-												'google-site-kit'
-										  ) }
-								</SpinnerButton>
-								<Link
-									className="googlesitekit-margin-left-auto mdc-dialog__cancel-button"
-									onClick={ handleDialog }
-									disabled={ inProgress }
-								>
-									{ __( 'Cancel', 'google-site-kit' ) }
-								</Link>
-							</footer>
-						</div>
-					</div>
-				</div>
-			</FocusTrap>
-		</div>
+						</p>
+					) }
+				</Fragment>
+			}
+			footer={
+				<Fragment>
+					<SpinnerButton
+						onClick={ handleConfirm }
+						danger={ danger }
+						disabled={ inProgress }
+						isSaving={ inProgress }
+					>
+						{ confirmButton
+							? confirmButton
+							: __( 'Disconnect', 'google-site-kit' ) }
+					</SpinnerButton>
+					<Link
+						className="googlesitekit-margin-left-auto mdc-dialog__cancel-button"
+						onClick={ handleDialog }
+						disabled={ inProgress }
+					>
+						{ __( 'Cancel', 'google-site-kit' ) }
+					</Link>
+				</Fragment>
+			}
+		/>
 	);
 };
 
-Dialog.displayName = 'Dialog';
+ModalDialog.displayName = 'Dialog';
 
-Dialog.propTypes = {
+ModalDialog.propTypes = {
 	dialogActive: PropTypes.bool,
 	handleDialog: PropTypes.func,
 	handleConfirm: PropTypes.func.isRequired,
@@ -175,7 +135,7 @@ Dialog.propTypes = {
 	danger: PropTypes.bool,
 };
 
-Dialog.defaultProps = {
+ModalDialog.defaultProps = {
 	dialogActive: false,
 	handleDialog: null,
 	title: null,
@@ -184,4 +144,4 @@ Dialog.defaultProps = {
 	danger: false,
 };
 
-export default Dialog;
+export default ModalDialog;
