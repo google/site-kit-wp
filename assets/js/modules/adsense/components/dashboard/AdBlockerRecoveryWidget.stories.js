e2c408d1dae1c5110d26ab2d4c3d1f4f08078145
hussain-t <hussain.thajutheen@10up.com>
2023-06-21 01:39:52 +0530

    Add AdBlockingRecoveryWidget stories.

--- assets/js/modules/adsense/components/dashboard/AdBlockerRecoveryWidget.stories.js
+++ assets/js/modules/adsense/components/dashboard/AdBlockerRecoveryWidget.stories.js
@@ -15,12 +15,26 @@
  */
 
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * Internal dependencies
  */
 import { provideModules } from '../../../../../../tests/js/utils';
 import { withWidgetComponentProps } from '../../../../googlesitekit/widgets/util';
 import WithRegistrySetup from '../../../../../../tests/js/WithRegistrySetup';
+import {
+	useBreakpoint,
+	BREAKPOINT_SMALL,
+} from '../../../../hooks/useBreakpoint';
 import AdBlockingRecoveryWidget from './AdBlockingRecoveryWidget';
+import {
+	AD_BLOCKING_RECOVERY_SETUP_STATUS_SETUP_CONFIRMED,
+	MODULES_ADSENSE,
+} from '../../datastore/constants';
+import { ACCOUNT_STATUS_READY, SITE_STATUS_READY } from '../../util';
 
 const WidgetWithComponentProps = withWidgetComponentProps(
 	'adBlockingRecovery'
@@ -28,13 +42,46 @@ const WidgetWithComponentProps = withWidgetComponentProps(
 
 const Template = () => <WidgetWithComponentProps />;
 
-export const Default = Template.bind( {} );
-Default.storyName = 'Default';
+const validSettings = {
+	accountID: 'pub-12345678',
+	clientID: 'ca-pub-12345678',
+	useSnippet: false,
+	accountStatus: ACCOUNT_STATUS_READY,
+	siteStatus: SITE_STATUS_READY,
+	adBlockingRecoverySetupStatus:
+		AD_BLOCKING_RECOVERY_SETUP_STATUS_SETUP_CONFIRMED,
+};
+
+export const ReadyWithoutCompletionTime = Template.bind( {} );
+ReadyWithoutCompletionTime.storyName = 'Ready Without Completion Time';
+ReadyWithoutCompletionTime.args = {
+	setupRegistry: ( registry ) => {
+		registry
+			.dispatch( MODULES_ADSENSE )
+			.receiveGetSettings( validSettings );
+	},
+};
+ReadyWithoutCompletionTime.scenario = {
+	label: 'Global/AdBlockingRecoveryWidget/ReadyWithoutCompletionTime',
+	delay: 250,
+};
+
+export const ReadyWithCompletionTime = Template.bind( {} );
+ReadyWithCompletionTime.storyName = 'Ready With Completion Time';
+ReadyWithCompletionTime.args = {
+	setupRegistry: ( registry ) => {
+		registry.dispatch( MODULES_ADSENSE ).receiveGetSettings( {
+			...validSettings,
+			// setupCompletedTimestamp: 1689416266,
+			setupCompletedTimestamp: 1684145866,
+		} );
+	},
+};
 
 export default {
 	title: 'Modules/AdSense/Widgets/AdBlockingRecoveryWidget',
 	decorators: [
-		( Story ) => {
+		( Story, { args } ) => {
 			const setupRegistry = ( registry ) => {
 				provideModules( registry, [
 					{
@@ -43,12 +90,39 @@ export default {
 						slug: 'adsense',
 					},
 				] );
+
+				args?.setupRegistry( registry );
 			};
 
+			const breakpoint = useBreakpoint();
+
 			return (
-				<WithRegistrySetup func={ setupRegistry }>
-					<Story />
-				</WithRegistrySetup>
+				<div
+					style={ {
+						minHeight: '200px',
+						display: 'flex',
+						alignItems: 'center',
+					} }
+				>
+					<div id="adminmenu">
+						{ /* eslint-disable-next-line jsx-a11y/anchor-has-content */ }
+						<a href="http://test.test/?page=googlesitekit-settings" />
+					</div>
+					<div
+						style={ { flex: 1 } }
+						className={ classnames( {
+							// Turn off animations for non-mobile breakpoints. The standard VRT behaviour is to set
+							// animation-duration to 0ms, this does not play well with this component as there is a
+							// continual chain of animation at non-mobile breakpoints.
+							'googlesitekit-vrt-animation-none':
+								breakpoint !== BREAKPOINT_SMALL,
+						} ) }
+					>
+						<WithRegistrySetup func={ setupRegistry }>
+							<Story />
+						</WithRegistrySetup>
+					</div>
+				</div>
 			);
 		},
 	],
