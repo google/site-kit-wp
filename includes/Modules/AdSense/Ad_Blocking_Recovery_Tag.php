ab39758d82902666de37a9be461875e75ecf8014
Arafat Zahan <arafat.zahan@10up.com>
2023-06-21 21:58:04 +0600

    Override constructor.

--- includes/Modules/AdSense/Ad_Blocking_Recovery_Tag.php
+++ includes/Modules/AdSense/Ad_Blocking_Recovery_Tag.php
@@ -10,6 +10,9 @@
 
 namespace Google\Site_Kit\Modules\AdSense;
 
+use Google\Site_Kit\Core\Storage\Encrypted_Options;
+use Google\Site_Kit\Core\Storage\Options;
+use Google\Site_Kit\Core\Storage\Options_Interface;
 use Google\Site_Kit\Core\Storage\Setting;
 
 /**
@@ -22,6 +25,17 @@ use Google\Site_Kit\Core\Storage\Setting;
 class Ad_Blocking_Recovery_Tag extends Setting {
 
 	const OPTION = 'googlesitekit_adsense_ad_blocking_recovery_tag';
+
+	/**
+	 * Ad_Blocking_Recovery_Tag constructor.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param Options $options Options_Interface instance.
+	 */
+	public function __construct( Options $options ) {
+		$this->options = new Encrypted_Options( $options );
+	}
 
 	/**
 	 * Gets ad blocking recovery tag.
