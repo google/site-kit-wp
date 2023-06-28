8071bcd07eef0dff52e945089d30ae34de4f7d64
Arafat Zahan <arafat.zahan@10up.com>
2023-06-28 13:17:07 +0600

    Use setters for correct thing.

--- includes/Modules/AdSense.php
+++ includes/Modules/AdSense.php
@@ -804,8 +804,8 @@ final class AdSense extends Module
 
 		if ( Feature_Flags::enabled( 'adBlockerDetection' ) && ! $this->context->is_amp() ) {
 			$ad_blocking_recovery_web_tag = new Ad_Blocking_Recovery_Web_Tag( $settings['accountID'], self::MODULE_SLUG );
-			$this->web_tag->set_ad_blocking_recovery_tag( $this->ad_blocking_recovery_tag );
-			$this->web_tag->set_use_error_snippet( $this->get_settings()->get()['useAdBlockerDetectionErrorSnippet'] );
+			$ad_blocking_recovery_web_tag->set_ad_blocking_recovery_tag( $this->ad_blocking_recovery_tag );
+			$ad_blocking_recovery_web_tag->set_use_error_snippet( $settings['useAdBlockerDetectionErrorSnippet'] );
 
 			if ( $ad_blocking_recovery_web_tag->is_tag_blocked() ) {
 				return;
