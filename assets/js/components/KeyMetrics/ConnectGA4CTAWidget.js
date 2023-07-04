0214cfa7c41151070b8e63dd32fdb95b2e8a6ac7
nfmohit <nahid.mohit@10up.com>
2023-07-05 02:50:16 +0600

    Complete actions.

--- assets/js/components/KeyMetrics/ConnectGA4CTAWidget.js
+++ assets/js/components/KeyMetrics/ConnectGA4CTAWidget.js
@@ -20,22 +20,41 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useEffect, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
-import { Button } from 'googlesitekit-components';
+import { SpinnerButton } from 'googlesitekit-components';
 import KeyMetricsCTAContent from './KeyMetricsCTAContent';
 import KeyMetricsCTAFooter from './KeyMetricsCTAFooter';
 import { CORE_MODULES } from '../../googlesitekit/modules/datastore/constants';
 import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
 import { CORE_WIDGETS } from '../../googlesitekit/widgets/datastore/constants';
 import { AREA_MAIN_DASHBOARD_KEY_METRICS_PRIMARY } from '../../googlesitekit/widgets/default-areas';
-const { useSelect } = Data;
+import { MODULES_ANALYTICS } from '../../modules/analytics/datastore/constants';
+import { CORE_LOCATION } from '../../googlesitekit/datastore/location/constants';
+import useActivateModuleCallback from '../../hooks/useActivateModuleCallback';
+import useCompleteModuleActivationCallback from '../../hooks/useCompleteModuleActivationCallback';
+import { useDebounce } from '../../hooks/useDebounce';
+const { useSelect, useDispatch } = Data;
 
 export default function ConnectGA4CTAWidget( { Widget, WidgetNull } ) {
 	const DISMISSED_ITEM_KEY = 'key-metrics-connect-ga4-cta-widget';
+
+	const activateModuleCallback = useActivateModuleCallback( 'analytics' );
+	const completeModuleActivationCallback =
+		useCompleteModuleActivationCallback( 'analytics' );
+
+	const [ inProgress, setInProgress ] = useState( false );
+
+	/*
+	 * Using debounce here because the spinner has to render across two separate calls.
+	 * Rather than risk it flickering on and off in between the activation call completing and
+	 * the navigate call starting, we will just set a debounce to keep the spinner for 3 seconds.
+	 */
+	const debouncedSetInProgress = useDebounce( setInProgress, 3000 );
 
 	const isCTADismissed = useSelect( ( select ) =>
 		select( CORE_USER ).isItemDismissed( DISMISSED_ITEM_KEY )
@@ -61,6 +80,38 @@ export default function ConnectGA4CTAWidget( { Widget, WidgetNull } ) {
 				keyMetrics.includes( slug ) && modules.includes( 'analytics-4' )
 		);
 	} );
+	const analyticsModuleActive = useSelect( ( select ) =>
+		select( CORE_MODULES ).isModuleActive( 'analytics' )
+	);
+	const isNavigatingToReauthURL = useSelect( ( select ) => {
+		const adminReauthURL = select( MODULES_ANALYTICS ).getAdminReauthURL();
+
+		if ( ! adminReauthURL ) {
+			return false;
+		}
+
+		return select( CORE_LOCATION ).isNavigatingTo( adminReauthURL );
+	} );
+	const isActivating = useSelect( ( select ) =>
+		select( CORE_MODULES ).isFetchingSetModuleActivation(
+			'analytics',
+			true
+		)
+	);
+
+	const { dismissItem } = useDispatch( CORE_USER );
+
+	useEffect( () => {
+		if ( isActivating || isNavigatingToReauthURL ) {
+			setInProgress( true );
+		} else {
+			debouncedSetInProgress( false );
+		}
+	}, [ isActivating, isNavigatingToReauthURL, debouncedSetInProgress ] );
+
+	const onClickCallback = analyticsModuleActive
+		? completeModuleActivationCallback
+		: activateModuleCallback;
 
 	if (
 		isCTADismissed ||
@@ -72,7 +123,14 @@ export default function ConnectGA4CTAWidget( { Widget, WidgetNull } ) {
 	}
 
 	return (
-		<Widget noPadding Footer={ () => <KeyMetricsCTAFooter /> }>
+		<Widget
+			noPadding
+			Footer={ () => (
+				<KeyMetricsCTAFooter
+					onActionClick={ () => dismissItem( DISMISSED_ITEM_KEY ) }
+				/>
+			) }
+		>
 			<KeyMetricsCTAContent
 				className="googlesitekit-km-connect-ga4-cta"
 				title={ __(
@@ -84,9 +142,12 @@ export default function ConnectGA4CTAWidget( { Widget, WidgetNull } ) {
 					'google-site-kit'
 				) }
 				actions={
-					<Button>
+					<SpinnerButton
+						onClick={ onClickCallback }
+						isSaving={ inProgress }
+					>
 						{ __( 'Connect Google Analytics', 'google-site-kit' ) }
-					</Button>
+					</SpinnerButton>
 				}
 			/>
 		</Widget>
