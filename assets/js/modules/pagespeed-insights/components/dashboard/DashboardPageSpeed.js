03f39715fa8016dd1601c30b700f1bf5f77081ef
Arafat Zahan <arafat.zahan@10up.com>
2023-07-07 02:38:47 +0600

    Update `Tab` and `TabBar` imports to use `googlesitekit-components`.

--- assets/js/modules/pagespeed-insights/components/dashboard/DashboardPageSpeed.js
+++ assets/js/modules/pagespeed-insights/components/dashboard/DashboardPageSpeed.js
@@ -20,8 +20,6 @@
  * External dependencies
  */
 import classnames from 'classnames';
-import Tab from '@material/react-tab';
-import TabBar from '@material/react-tab-bar';
 import { useIntersection } from 'react-use';
 
 /**
@@ -33,9 +31,9 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import { Button, ProgressBar, Tab, TabBar } from 'googlesitekit-components';
 import API from 'googlesitekit-api';
 import Data from 'googlesitekit-data';
-import { Button, ProgressBar } from 'googlesitekit-components';
 import DeviceSizeTabBar from '../../../../components/DeviceSizeTabBar';
 import Link from '../../../../components/Link';
 import LabReportMetrics from '../common/LabReportMetrics';
