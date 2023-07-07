daf4fdd15b82b4e5fcbb14ef89074fb63d9a2987
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-07-07 19:06:57 +0300

    Update the widget.

--- assets/js/modules/analytics-4/components/widgets/PopularProductsWidget.js
+++ assets/js/modules/analytics-4/components/widgets/PopularProductsWidget.js
@@ -26,15 +26,61 @@ import PropTypes from 'prop-types';
  */
 import Data from 'googlesitekit-data';
 import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
+import { CORE_SITE } from '../../../../googlesitekit/datastore/site/constants';
+import { CORE_MODULES } from '../../../../googlesitekit/modules/datastore/constants';
+import {
+	DATE_RANGE_OFFSET,
+	MODULES_ANALYTICS_4,
+} from '../../datastore/constants';
+const { useSelect, useInViewSelect } = Data;
 
-const { useSelect } = Data;
+export default function PopularProductsWidget( props ) {
+	const { Widget, WidgetNull } = props;
 
-export default function PopularProductsWidget( { Widget, WidgetNull } ) {
-	const keyMetricsWidgetHidden = useSelect( ( select ) =>
-		select( CORE_USER ).isKeyMetricsWidgetHidden()
+	const isGA4ModuleConnected = useSelect( ( select ) =>
+		select( CORE_MODULES ).isModuleConnected( 'analytics-4' )
+	);
+	const productBasePaths = useSelect( ( select ) =>
+		select( CORE_SITE ).getProductBasePaths()
 	);
 
-	if ( keyMetricsWidgetHidden !== false ) {
+	const dates = useSelect( ( select ) =>
+		select( CORE_USER ).getDateRangeDates( {
+			offsetDays: DATE_RANGE_OFFSET,
+		} )
+	);
+
+	const reportOptions = {
+		...dates,
+		dimensions: [ 'pagePathPlusQueryString' ],
+		metrics: [ { name: 'screenPageViews' } ],
+		orderby: [
+			{
+				metric: { metricName: 'screenPageViews' },
+				desc: true,
+			},
+		],
+		limit: 100,
+	};
+
+	const showWidget = isGA4ModuleConnected && productBasePaths?.length > 0;
+
+	const report = useInViewSelect( ( select ) =>
+		showWidget
+			? select( MODULES_ANALYTICS_4 ).getReport( reportOptions )
+			: undefined
+	);
+
+	const loading = useInViewSelect( ( select ) =>
+		showWidget
+			? ! select( MODULES_ANALYTICS_4 ).hasFinishedResolution(
+					'getReport',
+					[ reportOptions ]
+			  )
+			: undefined
+	);
+
+	if ( ! showWidget ) {
 		return <WidgetNull />;
 	}
 
