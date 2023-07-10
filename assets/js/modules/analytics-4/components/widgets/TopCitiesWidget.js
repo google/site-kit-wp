86d83ff833558219f937d6d6c3f6543a6a773233
hussain-t <hussain.thajutheen@10up.com>
2023-07-10 18:15:45 +0530

    Implement TopCitiesWidget logic.

--- assets/js/modules/analytics-4/components/widgets/TopCitiesWidget.js
+++ assets/js/modules/analytics-4/components/widgets/TopCitiesWidget.js
@@ -22,26 +22,109 @@
 import PropTypes from 'prop-types';
 
 /**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
 import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
+import { CORE_MODULES } from '../../../../googlesitekit/modules/datastore/constants';
+import {
+	DATE_RANGE_OFFSET,
+	MODULES_ANALYTICS_4,
+} from '../../datastore/constants';
+import { ZeroDataMessage } from '../../../analytics/components/common';
+import { numFmt } from '../../../../util';
+import {
+	MetricTileTable,
+	whenKeyMetricsWidgetVisible,
+} from '../../../../components/KeyMetrics';
+const { useSelect, useInViewSelect } = Data;
 
-const { useSelect } = Data;
-
-export default function TopCitiesWidget( { Widget, WidgetNull } ) {
+function TopCitiesWidget( { Widget, WidgetNull } ) {
 	const keyMetricsWidgetHidden = useSelect( ( select ) =>
 		select( CORE_USER ).isKeyMetricsWidgetHidden()
 	);
+	const isGA4ModuleConnected = useSelect( ( select ) =>
+		select( CORE_MODULES ).isModuleConnected( 'analytics-4' )
+	);
 
-	if ( keyMetricsWidgetHidden !== false ) {
+	const dates = useSelect( ( select ) =>
+		select( CORE_USER ).getDateRangeDates( {
+			offsetDays: DATE_RANGE_OFFSET,
+		} )
+	);
+
+	const topcCitiesReportOptions = {
+		...dates,
+		dimensions: [ 'city' ],
+		metrics: [ { name: 'totalUsers' } ],
+		orderby: [
+			{
+				metric: {
+					metricName: 'totalUsers',
+				},
+				desc: true,
+			},
+		],
+		limit: 3,
+	};
+
+	const topCitiesReport = useInViewSelect( ( select ) =>
+		select( MODULES_ANALYTICS_4 ).getReport( topcCitiesReportOptions )
+	);
+
+	const loading = useInViewSelect(
+		( select ) =>
+			! select( MODULES_ANALYTICS_4 ).hasFinishedResolution(
+				'getReport',
+				[ topcCitiesReportOptions ]
+			)
+	);
+
+	if ( ! isGA4ModuleConnected || keyMetricsWidgetHidden !== false ) {
 		return <WidgetNull />;
 	}
 
+	const { rows = [] } = topCitiesReport || {};
+
+	const totalUsers =
+		topCitiesReport?.totals?.[ 0 ]?.metricValues?.[ 0 ]?.value;
+
+	const columns = [
+		{
+			field: 'dimensionValues',
+			Component: ( { fieldValue } ) => {
+				const [ title ] = fieldValue;
+
+				return (
+					<p className="googlesitekit-km-widget-tile__table-item-text">
+						{ title.value }
+					</p>
+				);
+			},
+		},
+		{
+			field: 'metricValues.0.value',
+			Component: ( { fieldValue } ) => {
+				const cityValue = fieldValue / totalUsers;
+				return <strong>{ numFmt( cityValue, '%' ) }</strong>;
+			},
+		},
+	];
+
 	return (
-		<Widget>
-			<div>TODO: UI for TopCitiesWidget</div>
-		</Widget>
+		<MetricTileTable
+			Widget={ Widget }
+			title={ __( 'To cities driving traffic', 'google-site-kit' ) }
+			loading={ loading }
+			rows={ rows }
+			columns={ columns }
+			zeroState={ ZeroDataMessage }
+		/>
 	);
 }
 
@@ -49,3 +132,5 @@ TopCitiesWidget.propTypes = {
 	Widget: PropTypes.elementType.isRequired,
 	WidgetNull: PropTypes.elementType.isRequired,
 };
+
+export default whenKeyMetricsWidgetVisible()( TopCitiesWidget );
