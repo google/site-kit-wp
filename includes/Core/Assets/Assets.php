f1c077109f3f6e2fb561483a8c645fc8a75d6756
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-07-10 11:37:46 +0300

    Update the get_product_base_paths method to find the correct prefix of product URLs.

--- includes/Core/Assets/Assets.php
+++ includes/Core/Assets/Assets.php
@@ -18,6 +18,7 @@ use Google\Site_Kit\Core\Util\BC_Functions;
 use Google\Site_Kit\Core\Util\Feature_Flags;
 use WP_Dependencies;
 use WP_Post_Type;
+use WP_Post;
 
 /**
  * Class managing assets.
@@ -1078,15 +1079,28 @@ final class Assets {
 		}
 
 		$product_base_paths = array();
-		$product            = get_post_type_object( 'product' );
+		$product_type       = get_post_type_object( 'product' );
 
 		// Check whether the product post type is available and public.
-		if (
-			$product instanceof WP_Post_Type &&
-			$product->public &&
-			! empty( $product->rewrite['slug'] )
-		) {
-			$product_base_paths[] = $product->rewrite['slug'];
+		if ( $product_type instanceof WP_Post_Type && $product_type->public ) {
+			// Now we need to create a fake product with a randomly created slug
+			// that we can find in the permalink to determine the product base URL.
+			$post_name = substr( str_shuffle( md5( microtime() ) ), 0, 10 );
+			$product   = new WP_Post( (object) array(
+				'ID'          => rand( 1, 10000 ),
+				'post_name'   => $post_name,
+				'post_type'   => 'product',
+				'post_status' => 'publish',
+				'filter'      => 'raw',
+			) );
+
+			$permalink = get_permalink( $product );
+			if ( ! empty( $permalink ) ) {
+				$product_url_path  = wp_parse_url( $permalink, PHP_URL_PATH );
+				$product_base_path = explode( $post_name, $product_url_path, 2 );
+
+				$product_base_paths[] = $product_base_path[0];
+			}
 		}
 
 		/**
