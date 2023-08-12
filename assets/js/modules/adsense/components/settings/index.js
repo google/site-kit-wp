89121558e72dccea3e46ed252d7e8ba40636fc82
Arafat Zahan <arafat.zahan@10up.com>
2023-08-13 05:48:16 +0600

    Fix test.

--- assets/js/modules/adsense/components/settings/index.js
+++ assets/js/modules/adsense/components/settings/index.js
@@ -16,6 +16,8 @@
  * limitations under the License.
  */
 
+export { default as AdBlockingRecoverySetupCTANotice } from './AdBlockingRecoverySetupCTANotice';
+export { default as AdBlockingRecoveryToggle } from './AdBlockingRecoveryToggle';
 export { default as SettingsEdit } from './SettingsEdit';
 export { default as SettingsForm } from './SettingsForm';
 export { default as SettingsSetupIncomplete } from './SettingsSetupIncomplete';
