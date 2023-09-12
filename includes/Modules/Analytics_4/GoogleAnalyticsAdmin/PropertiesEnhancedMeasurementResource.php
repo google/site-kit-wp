7e37aafee6c358aa0b0ba6b6a32906698995c172
hussain-t <hussain.thajutheen@10up.com>
2023-09-13 00:43:32 +0530

    Use EnhancedMeasurementSettingsModel class.

--- includes/Modules/Analytics_4/GoogleAnalyticsAdmin/PropertiesEnhancedMeasurementResource.php
+++ includes/Modules/Analytics_4/GoogleAnalyticsAdmin/PropertiesEnhancedMeasurementResource.php
@@ -10,7 +10,7 @@
 
 namespace Google\Site_Kit\Modules\Analytics_4\GoogleAnalyticsAdmin;
 
-use Google\Site_Kit_Dependencies\Google\Service\GoogleAnalyticsAdmin\GoogleAnalyticsAdminV1alphaEnhancedMeasurementSettings;
+use Google\Site_Kit\Modules\Analytics_4\GoogleAnalyticsAdmin\EnhancedMeasurementSettingsModel;
 use Google\Site_Kit_Dependencies\Google\Service\Resource;
 
 /**
@@ -28,12 +28,12 @@ class PropertiesEnhancedMeasurementResource extends Resource {
 	 * @param string $name Required. The name of the settings to lookup. Format: properties/{property_id}/webDataStreams/{stream_id}/enhancedMeasurementSettings
 	 *                               Example: "properties/1000/webDataStreams/2000/enhancedMeasurementSettings".
 	 * @param array  $opt_params Optional parameters.
-	 * @return GoogleAnalyticsAdminV1alphaEnhancedMeasurementSettings
+	 * @return EnhancedMeasurementSettingsModel
 	 */
 	public function getEnhancedMeasurementSettings( $name, $opt_params = array() ) {
 		$params = array( 'name' => $name );
 		$params = array_merge( $params, $opt_params );
-		return $this->call( 'getEnhancedMeasurementSettings', array( $params ), GoogleAnalyticsAdminV1alphaEnhancedMeasurementSettings::class );
+		return $this->call( 'getEnhancedMeasurementSettings', array( $params ), EnhancedMeasurementSettingsModel::class );
 	}
 
 	/**
@@ -43,21 +43,21 @@ class PropertiesEnhancedMeasurementResource extends Resource {
 	 *
 	 * @param string                                                 $name Output only. Resource name of this Data Stream. Format: properties/{property_id}/webDataStreams/{stream_id}/enhancedMeasurementSettings
 	 *                                                               Example: "properties/1000/webDataStreams/2000/enhancedMeasurementSettings".
-	 * @param GoogleAnalyticsAdminV1alphaEnhancedMeasurementSettings $post_body The body of the request.
+	 * @param EnhancedMeasurementSettingsModel $post_body The body of the request.
 	 * @param array                                                  $opt_params Optional parameters.
 	 *
 	 * @opt_param string updateMask Required. The list of fields to be updated.
 	 * Field names must be in snake case (e.g., "field_to_update"). Omitted fields
 	 * will not be updated. To replace the entire entity, use one path with the
 	 * string "*" to match all fields.
-	 * @return GoogleAnalyticsAdminV1alphaEnhancedMeasurementSettings
+	 * @return EnhancedMeasurementSettingsModel
 	 */
-	public function updateEnhancedMeasurementSettings( $name, GoogleAnalyticsAdminV1alphaEnhancedMeasurementSettings $post_body, $opt_params = array() ) {
+	public function updateEnhancedMeasurementSettings( $name, EnhancedMeasurementSettingsModel $post_body, $opt_params = array() ) {
 		$params = array(
 			'name'     => $name,
 			'postBody' => $post_body,
 		);
 		$params = array_merge( $params, $opt_params );
-		return $this->call( 'updateEnhancedMeasurementSettings', array( $params ), GoogleAnalyticsAdminV1alphaEnhancedMeasurementSettings::class );
+		return $this->call( 'updateEnhancedMeasurementSettings', array( $params ), EnhancedMeasurementSettingsModel::class );
 	}
 }
