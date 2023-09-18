0cf52ddb8e49ce44dfd9421eaeb25ec1de9a1fd0
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2023-09-18 19:33:58 +0100

    Use environment variable instead of hardcoded variable.

--- bin/check-issue-has-release.js
+++ bin/check-issue-has-release.js
@@ -25,7 +25,7 @@ const checkIssueHasRelease = async () => {
   try {
     const response = await client.request(query, {
       // eslint-disable-next-line sitekit/acronym-case
-      repositoryGhId: 191387173,
+      repositoryGhId: process.env.GITHUB_REPOSITORY_ID,
       issueNumber: process.env.PULL_REQUEST_NUMBER
     }, {
       authorization: `Bearer ${process.env.ZENHUB_GQL_API_TOKEN}`
