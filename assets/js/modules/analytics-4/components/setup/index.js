56033bad70c0102bd9605b1880255d82e0e24b57
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2023-09-20 15:21:29 +0100

    Add missing exports.

--- assets/js/modules/analytics-4/components/setup/index.js
+++ assets/js/modules/analytics-4/components/setup/index.js
@@ -16,4 +16,5 @@
  * limitations under the License.
  */
 
+export { default as SetupEnhancedMeasurementSwitch } from './SetupEnhancedMeasurementSwitch';
 export { default as SetupUseSnippetSwitch } from './SetupUseSnippetSwitch';
