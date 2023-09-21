85b1c0f3e7ba2ff3f8a51196c2d2e7dcd3c4154c
hussain-t <hussain.thajutheen@10up.com>
2023-09-21 22:34:27 +0530

    Register keyMetricsConnectGA4All if 4 tile depend on GA4.

--- assets/js/googlesitekit/widgets/register-defaults.js
+++ assets/js/googlesitekit/widgets/register-defaults.js
@@ -30,8 +30,11 @@ import * as WIDGET_AREAS from './default-areas';
 import {
 	CORE_USER,
 	allKeyMetricsTileWidgets,
+	keyMetricsGA4Widgets,
 } from '../datastore/user/constants';
 import { WIDGET_AREA_STYLES } from './datastore/constants';
+import { CORE_SITE } from '../datastore/site/constants';
+import { CORE_MODULES } from '../modules/datastore/constants';
 import { isFeatureEnabled } from '../../features';
 import {
 	KeyMetricsSetupCTAWidget,
@@ -39,7 +42,7 @@ import {
 } from '../../components/KeyMetrics';
 import AddMetricCTATile from '../../components/KeyMetrics/AddMetricCTATile';
 import Badge from '../../components/Badge';
-import { CORE_SITE } from '../datastore/site/constants';
+import ConnectGA4CTAWidget from '../../modules/analytics-4/components/widgets/ConnectGA4CTAWidget';
 
 const { ...ADDITIONAL_WIDGET_CONTEXTS } = WIDGET_CONTEXTS;
 
@@ -268,6 +271,35 @@ export function registerDefaults( widgetsAPI ) {
 				isActive: ( select ) =>
 					select( CORE_USER ).isAuthenticated() &&
 					select( CORE_SITE ).isKeyMetricsSetupCompleted() === false,
+			},
+			[ AREA_MAIN_DASHBOARD_KEY_METRICS_PRIMARY ]
+		);
+
+		widgetsAPI.registerWidget(
+			'keyMetricsConnectGA4All',
+			{
+				Component: ConnectGA4CTAWidget,
+				width: [ widgetsAPI.WIDGET_WIDTHS.FULL ],
+				priority: 1,
+				wrapWidget: false,
+				modules: [ 'search-console' ],
+				isActive: ( select ) => {
+					const keyMetrics = select( CORE_USER ).getKeyMetrics();
+					const isGA4Connected =
+						select( CORE_MODULES ).isModuleConnected(
+							'analytics-4'
+						);
+
+					if ( isGA4Connected || ! Array.isArray( keyMetrics ) ) {
+						return false;
+					}
+					const kmAnalyticsWidgetCount = keyMetrics.filter(
+						( keyMetric ) =>
+							keyMetricsGA4Widgets.includes( keyMetric )
+					).length;
+
+					return kmAnalyticsWidgetCount > 3;
+				},
 			},
 			[ AREA_MAIN_DASHBOARD_KEY_METRICS_PRIMARY ]
 		);
