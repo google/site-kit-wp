c2bbf95361b6305258cd4743ad1737b2ccdc9164
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2023-09-28 17:10:16 +0100

    Render the EnhancedMeasurementActivationBanner.

--- assets/js/components/notifications/BannerNotifications.js
+++ assets/js/components/notifications/BannerNotifications.js
@@ -38,6 +38,7 @@ import CoreSiteBannerNotifications from './CoreSiteBannerNotifications';
 import ModuleRecoveryAlert from '../dashboard-sharing/ModuleRecoveryAlert';
 import AdSenseAlerts from './AdSenseAlerts';
 import ActivationBanner from '../../modules/analytics-4/components/dashboard/ActivationBanner';
+import EnhancedMeasurementActivationBanner from '../../modules/analytics-4/components/dashboard/EnhancedMeasurementActivationBanner';
 import useViewOnly from '../../hooks/useViewOnly';
 import ZeroDataStateNotifications from './ZeroDataStateNotifications';
 import EnableAutoUpdateBannerNotification from './EnableAutoUpdateBannerNotification';
@@ -50,6 +51,7 @@ import OptimizeRemovalNotification from './OptimizeRemovalNotification';
 const { useSelect } = Data;
 
 export default function BannerNotifications() {
+	const enhancedMeasurementEnabled = useFeature( 'enhancedMeasurement' );
 	const ga4ReportingEnabled = useFeature( 'ga4Reporting' );
 
 	const viewOnly = useViewOnly();
@@ -109,6 +111,9 @@ export default function BannerNotifications() {
 				analyticsModuleConnected &&
 				ga4ModuleConnected && <SwitchedToGA4Banner /> }
 			<ActivationBanner />
+			{ enhancedMeasurementEnabled && (
+				<EnhancedMeasurementActivationBanner />
+			) }
 			{ ga4ModuleConnected && hasGTMScope && isGA4ModuleOwner && (
 				<Fragment>
 					<GoogleTagIDMismatchNotification />
