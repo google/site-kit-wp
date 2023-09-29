44f3159e4759c42e17b34cda16561aa90f49f62f
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2023-09-29 10:31:09 +0100

    Try conditionally removing the `print_emoji_detection_script` script.

--- tests/phpunit/integration/Modules/AnalyticsTest.php
+++ tests/phpunit/integration/Modules/AnalyticsTest.php
@@ -475,13 +475,19 @@ class AnalyticsTest extends TestCase {
 	 * @param bool $is_content_creator
 	 */
 	public function test_tracking_disabled( $settings, $logged_in, $is_tracking_active, $is_content_creator = false ) {
+		global $wp_version;
+
 		wp_scripts()->registered = array();
 		wp_scripts()->queue      = array();
 		wp_scripts()->done       = array();
 		wp_styles(); // Prevent potential ->queue of non-object error.
 		remove_all_actions( 'wp_enqueue_scripts' );
+
 		// Remove irrelevant script from throwing errors in CI from readfile().
-		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		if ( $wp_version < 8 ) {
+			remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		}
+
 		// Set the current user (can be 0 for no user)
 		$role = $is_content_creator ? 'administrator' : 'subscriber';
 		$user = $logged_in ?
