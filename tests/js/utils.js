4581d46185bf9dca9e2e2d74bf220b7edbc90f67
Evan Mattson <emattson@google.com>
2023-10-05 17:02:02 -0400

    Update test for saveKeyMetricsSettings.

--- tests/js/utils.js
+++ tests/js/utils.js
@@ -250,7 +250,7 @@ export const provideSiteInfo = ( registry, extraData = {} ) => {
 			},
 		],
 		productBasePaths: [ '^/product/' ],
-		keyMetricsSetupCompleted: false,
+		keyMetricsSetupCompletedBy: 0,
 	};
 
 	registry.dispatch( CORE_SITE ).receiveSiteInfo( {
