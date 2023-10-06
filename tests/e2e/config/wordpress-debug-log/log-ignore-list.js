6995d728a9a85f396dd8471626a477529931eccd
Evan Mattson <emattson@google.com>
2023-10-06 14:20:10 -0400

    Update ignores for consistency.

--- tests/e2e/config/wordpress-debug-log/log-ignore-list.js
+++ tests/e2e/config/wordpress-debug-log/log-ignore-list.js
@@ -6,15 +6,15 @@
 export const logIgnoreList = {
 	'5.2.16': [
 		// Deprecated syntax or function calls which are fixed in later WP versions.
-		'PHP Deprecated:  Function get_magic_quotes_gpc() is deprecated in /var/www/html/wp-includes/load.php on line',
-		'PHP Deprecated:  Function get_magic_quotes_gpc() is deprecated in /var/www/html/wp-includes/formatting.php on line',
-		'PHP Deprecated:  Array and string offset access syntax with curly braces is deprecated in /var/www/html/wp-includes/class-wp-editor.php on line',
-		'PHP Deprecated:  implode(): Passing glue string after array is deprecated. Swap the parameters in /var/www/html/wp-includes/SimplePie/Parse/Date.php on line',
+		'PHP Deprecated:  Function get_magic_quotes_gpc() is deprecated in /var/www/html/wp-includes/load.php',
+		'PHP Deprecated:  Function get_magic_quotes_gpc() is deprecated in /var/www/html/wp-includes/formatting.php',
+		'PHP Deprecated:  Array and string offset access syntax with curly braces is deprecated in /var/www/html/wp-includes/class-wp-editor.php',
+		'PHP Deprecated:  implode(): Passing glue string after array is deprecated. Swap the parameters in /var/www/html/wp-includes/SimplePie/Parse/Date.php',
 
 		// These are guarded against in later WP versions.
-		'PHP Notice:  Trying to access array offset on value of type bool in /var/www/html/wp-admin/includes/update.php on line',
-		'PHP Notice:  Trying to access array offset on value of type null in /var/www/html/wp-includes/rest-api/class-wp-rest-request.php on line',
-		'PHP Notice:  Trying to access array offset on value of type bool in /var/www/html/wp-includes/theme.php on line',
+		'PHP Notice:  Trying to access array offset on value of type bool in /var/www/html/wp-admin/includes/update.php',
+		'PHP Notice:  Trying to access array offset on value of type null in /var/www/html/wp-includes/rest-api/class-wp-rest-request.php',
+		'PHP Notice:  Trying to access array offset on value of type bool in /var/www/html/wp-includes/theme.php',
 	],
 	nightly: [
 		// Can be removed once WordPress AMP Plugin removes the deprecated function call.
