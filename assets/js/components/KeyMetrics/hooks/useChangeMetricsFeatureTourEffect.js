7e4e26bfabc149cb4675d39ccc41001ba2cc4cf4
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-10-06 15:02:51 +0300

    Update assets/js/components/KeyMetrics/hooks/useChangeMetricsFeatureTourEffect.js

--- assets/js/components/KeyMetrics/hooks/useChangeMetricsFeatureTourEffect.js
+++ assets/js/components/KeyMetrics/hooks/useChangeMetricsFeatureTourEffect.js
@@ -1,7 +1,7 @@
 /**
  * Change metrics feature tour hook.
  *
- * Site Kit by Google, Copyright 2022 Google LLC
+ * Site Kit by Google, Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
