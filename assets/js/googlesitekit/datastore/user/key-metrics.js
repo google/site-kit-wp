72e2dab2beff0261cf693b16037324c4b40c9377
Jimmy Madon <hello@jimmymadon.com>
2023-10-09 14:45:22 +0100

    Update answer based metrics for news publishers.

--- assets/js/googlesitekit/datastore/user/key-metrics.js
+++ assets/js/googlesitekit/datastore/user/key-metrics.js
@@ -30,9 +30,12 @@ import {
 	KM_ANALYTICS_ENGAGED_TRAFFIC_SOURCE,
 	KM_ANALYTICS_LOYAL_VISITORS,
 	KM_ANALYTICS_NEW_VISITORS,
+	KM_ANALYTICS_PAGES_PER_VISIT,
 	KM_ANALYTICS_POPULAR_CONTENT,
 	KM_ANALYTICS_POPULAR_PRODUCTS,
 	KM_ANALYTICS_TOP_TRAFFIC_SOURCE,
+	KM_ANALYTICS_VISITS_PER_VISITOR,
+	KM_ANALYTICS_VISIT_LENGTH,
 	KM_SEARCH_CONSOLE_POPULAR_KEYWORDS,
 } from './constants';
 import { CORE_SITE } from '../../datastore/site/constants';
@@ -41,6 +44,7 @@ import { CORE_WIDGETS } from '../../widgets/datastore/constants';
 
 import { createFetchStore } from '../../data/create-fetch-store';
 import { actions as errorStoreActions } from '../../data/create-error-store';
+import { isFeatureEnabled } from '../../../features';
 
 const { receiveError, clearError } = errorStoreActions;
 const { createRegistrySelector } = Data;
@@ -249,13 +253,30 @@ const baseSelectors = {
 
 		switch ( purpose ) {
 			case 'publish_blog':
-			case 'publish_news':
 				return [
 					KM_ANALYTICS_LOYAL_VISITORS,
 					KM_ANALYTICS_NEW_VISITORS,
 					KM_ANALYTICS_TOP_TRAFFIC_SOURCE,
 					KM_ANALYTICS_ENGAGED_TRAFFIC_SOURCE,
 				];
+
+			case 'publish_news':
+				const newsKeyMetricsEnabled =
+					isFeatureEnabled( 'newsKeyMetrics' );
+
+				return newsKeyMetricsEnabled
+					? [
+							KM_ANALYTICS_PAGES_PER_VISIT,
+							KM_ANALYTICS_VISIT_LENGTH,
+							KM_ANALYTICS_VISITS_PER_VISITOR,
+					  ]
+					: [
+							KM_ANALYTICS_LOYAL_VISITORS,
+							KM_ANALYTICS_NEW_VISITORS,
+							KM_ANALYTICS_TOP_TRAFFIC_SOURCE,
+							KM_ANALYTICS_ENGAGED_TRAFFIC_SOURCE,
+					  ];
+
 			case 'monetize_content':
 				return [
 					KM_ANALYTICS_POPULAR_CONTENT,
@@ -263,6 +284,7 @@ const baseSelectors = {
 					KM_ANALYTICS_NEW_VISITORS,
 					KM_ANALYTICS_TOP_TRAFFIC_SOURCE,
 				];
+
 			case 'sell_products_or_service':
 				return [
 					hasProductPostType()
