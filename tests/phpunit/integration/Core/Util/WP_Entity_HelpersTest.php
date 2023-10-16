b2966ac302f71fbff395d411a15f3fefd96e70c5
Jimmy Madon <hello@jimmymadon.com>
2023-10-16 02:45:16 +0100

    Remove outdated test.

--- tests/phpunit/integration/Core/Util/WP_Entity_HelpersTest.php
+++ tests/phpunit/integration/Core/Util/WP_Entity_HelpersTest.php
@@ -23,23 +23,4 @@ class WP_Entity_HelpersTest extends TestCase {
 		$this->assertEquals( 'test display name', WP_Entity_Helpers::get_user_display_name( $test_user_id ) );
 	}
 
-	public function test_parse_category_names__mixed_values() {
-		$category_with_number = $this->factory()->category->create( array( 'name' => '2' ) );
-		$category_with_commas = $this->factory()->category->create( array( 'name' => 'Category,with,commas' ) );
-		$normal_category      = $this->factory()->category->create( array( 'name' => 'Normal Category' ) );
-		$category_ids_string  = implode( ',', array( $category_with_number, $category_with_commas, 1955, $normal_category ) ); // 1955 would be a non-existent category
-
-		$this->assertEquals(
-			'["2","Category,with,commas",1955,"Normal Category"]',
-			WP_Entity_Helpers::parse_category_names( $category_ids_string )
-		);
-	}
-
-	public function test_parse_category_names__single_value() {
-		$this->assertEquals(
-			'["Uncategorized"]',
-			WP_Entity_Helpers::parse_category_names( '1' )
-		);
-	}
-
 }
