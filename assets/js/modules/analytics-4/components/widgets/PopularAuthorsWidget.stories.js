24f9d65221eb5ef81539203e5ec518e380d2943f
Eugene Manuilov <eugene.manuilov@get10up.com>
2023-10-18 16:15:02 +0300

    Add stories.

--- assets/js/modules/analytics-4/components/widgets/PopularAuthorsWidget.stories.js
+++ assets/js/modules/analytics-4/components/widgets/PopularAuthorsWidget.stories.js
@@ -20,13 +20,21 @@
  * Internal dependencies
  */
 import { ERROR_REASON_INSUFFICIENT_PERMISSIONS } from '../../../../util/errors';
+import {
+	CORE_USER,
+	KM_ANALYTICS_POPULAR_AUTHORS,
+} from '../../../../googlesitekit/datastore/user/constants';
 import { MODULES_ANALYTICS_4 } from '../../datastore/constants';
 import { KEY_METRICS_WIDGETS } from '../../../../components/KeyMetrics/key-metrics-widgets';
-import { KM_ANALYTICS_POPULAR_AUTHORS } from '../../../../googlesitekit/datastore/user/constants';
 import { provideModules } from '../../../../../../tests/js/utils';
 import { provideCustomDimensionError } from '../../utils/custom-dimensions';
 import { withWidgetComponentProps } from '../../../../googlesitekit/widgets/util';
 import WithRegistrySetup from '../../../../../../tests/js/WithRegistrySetup';
+import { replaceValuesInAnalytics4ReportWithZeroData } from '../../../../../../.storybook/utils/zeroReports';
+import {
+	getAnalytics4MockResponse,
+	provideAnalytics4MockReport,
+} from '../../utils/data-mock';
 import PopularAuthorsWidget from './PopularAuthorsWidget';
 
 const WidgetWithComponentProps = withWidgetComponentProps(
@@ -39,10 +47,33 @@ const Template = ( { setupRegistry, ...args } ) => (
 	</WithRegistrySetup>
 );
 
+const reportOptions = {
+	startDate: '2020-08-11',
+	endDate: '2020-09-07',
+	dimensions: [ 'customEvent:googlesitekit_post_author' ],
+	dimensionFilters: {
+		'customEvent:googlesitekit_post_author': {
+			filterType: 'stringFilter',
+			matchType: 'FULL_REGEXP',
+			value: '\\d+',
+		},
+	},
+	metrics: [ { name: 'screenPageViews' } ],
+	orderby: [
+		{
+			metric: { metricName: 'screenPageViews' },
+			desc: true,
+		},
+	],
+	limit: 3,
+};
+
 export const Ready = Template.bind( {} );
 Ready.storyName = 'Ready';
 Ready.args = {
 	setupRegistry: ( registry ) => {
+		provideAnalytics4MockReport( registry, reportOptions );
+
 		registry.dispatch( MODULES_ANALYTICS_4 ).setSettings( {
 			propertyID: '123456789',
 			availableCustomDimensions: [
@@ -54,6 +85,50 @@ Ready.args = {
 };
 Ready.parameters = {
 	features: [ 'newsKeyMetrics' ],
+};
+Ready.scenario = {
+	label: 'KeyMetrics/PopularAuthorsWidget/Ready',
+};
+
+export const Loading = Template.bind( {} );
+Loading.storyName = 'Loading';
+Loading.args = {
+	setupRegistry: ( { dispatch } ) => {
+		dispatch( MODULES_ANALYTICS_4 ).startResolution( 'getReport', [
+			reportOptions,
+		] );
+		dispatch( MODULES_ANALYTICS_4 ).setSettings( {
+			propertyID: '12345',
+			availableCustomDimensions: [
+				KEY_METRICS_WIDGETS[ KM_ANALYTICS_POPULAR_AUTHORS ]
+					.requiredCustomDimensions?.[ 0 ],
+			],
+		} );
+	},
+};
+
+export const ZeroData = Template.bind( {} );
+ZeroData.storyName = 'Zero Data';
+ZeroData.args = {
+	setupRegistry: ( { dispatch } ) => {
+		const report = getAnalytics4MockResponse( reportOptions );
+		const zeroReport =
+			replaceValuesInAnalytics4ReportWithZeroData( report );
+
+		dispatch( MODULES_ANALYTICS_4 ).receiveGetReport( zeroReport, {
+			options: reportOptions,
+		} );
+		dispatch( MODULES_ANALYTICS_4 ).setSettings( {
+			propertyID: '12345',
+			availableCustomDimensions: [
+				KEY_METRICS_WIDGETS[ KM_ANALYTICS_POPULAR_AUTHORS ]
+					.requiredCustomDimensions?.[ 0 ],
+			],
+		} );
+	},
+};
+ZeroData.scenario = {
+	label: 'KeyMetrics/PopularAuthorsWidget/ZeroData',
 };
 
 export const ErrorMissingCustomDimensions = Template.bind( {} );
@@ -132,6 +207,8 @@ export default {
 						connected: true,
 					},
 				] );
+
+				registry.dispatch( CORE_USER ).setReferenceDate( '2020-09-08' );
 
 				// Call story-specific setup.
 				args.setupRegistry( registry );
