0666611d8debe3b045cb7aa4cdb73750be2d959c
Arafat Zahan <arafat.zahan@10up.com>
2023-10-27 07:57:00 +0600

    Add survey trigger test for SettingsCardKeyMetrics.

--- assets/js/components/settings/SettingsCardKeyMetrics.js
+++ assets/js/components/settings/SettingsCardKeyMetrics.js
@@ -1,4 +1,6 @@
 /**
+ * SettingsCardKeyMetrics component.
+ *
  * Site Kit by Google, Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
---
+++ assets/js/components/settings/SettingsCardKeyMetrics.test.js
@@ -0,0 +1,67 @@
+/**
+ * SettingsCardKeyMetrics component tests.
+ *
+ * Site Kit by Google, Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { mockSurveyEndpoints } from '../../../../tests/js/mock-survey-endpoints';
+import { render, waitFor } from '../../../../tests/js/test-utils';
+import {
+	createTestRegistry,
+	muteFetch,
+	provideSiteInfo,
+	provideUserAuthentication,
+} from '../../../../tests/js/utils';
+import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
+import SettingsCardKeyMetrics from './SettingsCardKeyMetrics';
+
+describe( 'SettingsCardKeyMetrics', () => {
+	let registry;
+
+	beforeEach( () => {
+		registry = createTestRegistry();
+		// provideSiteInfo( registry, { homeURL: 'http://example.com' } );
+	} );
+
+	it( 'should trigger a survey when the Key Metrics Setup CTA is in view', async () => {
+		mockSurveyEndpoints( registry );
+		provideUserAuthentication( registry );
+		provideSiteInfo( registry );
+
+		muteFetch(
+			new RegExp( '^/google-site-kit/v1/core/user/data/key-metrics' )
+		);
+
+		await registry
+			.dispatch( CORE_USER )
+			.receiveIsUserInputCompleted( false );
+
+		render( <SettingsCardKeyMetrics />, {
+			registry,
+		} );
+
+		const surveyTriggerEndpoint = new RegExp(
+			'^/google-site-kit/v1/core/user/data/survey-trigger'
+		);
+
+		await waitFor( () =>
+			expect( fetchMock ).toHaveFetched( surveyTriggerEndpoint, {
+				body: {
+					data: { triggerID: 'view_kmw_setup_cta' },
+				},
+			} )
+		);
+	} );
+} );
