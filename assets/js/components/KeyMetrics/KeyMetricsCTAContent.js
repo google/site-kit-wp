49654e5d5977ccdd74973b0f495e254ed6cc1ed9
Arafat Zahan <arafat.zahan@10up.com>
2023-11-01 19:58:58 +0600

    Prevent trigger from firing multiple times.

--- assets/js/components/KeyMetrics/KeyMetricsCTAContent.js
+++ assets/js/components/KeyMetrics/KeyMetricsCTAContent.js
@@ -19,20 +19,28 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { useIntersection } from 'react-use';
+
+/**
+ * WordPress dependencies
+ */
 import { useEffect, useRef, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { Cell, Grid, Row } from '../../material-components';
-import GhostCardsSVG from './GhostCards';
-import SurveyViewTrigger from '../surveys/SurveyViewTrigger';
+import Data from 'googlesitekit-data';
+import { CORE_SITE } from '../../googlesitekit/datastore/site/constants';
+import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
 import { BREAKPOINT_SMALL, useBreakpoint } from '../../hooks/useBreakpoint';
-import { DAY_IN_SECONDS, trackEvent } from '../../util';
 import useViewContext from '../../hooks/useViewContext';
-import { useInView } from '../../hooks/useInView';
+import { Cell, Grid, Row } from '../../material-components';
+import { DAY_IN_SECONDS, trackEvent } from '../../util';
+import GhostCardsSVG from './GhostCards';
+
+const { useDispatch, useSelect } = Data;
 
 export default function KeyMetricsCTAContent( {
 	className,
@@ -44,19 +52,43 @@ export default function KeyMetricsCTAContent( {
 	const trackingRef = useRef();
 	const breakpoint = useBreakpoint();
 	const viewContext = useViewContext();
-	const inView = useInView();
 	const isMobileBreakpoint = breakpoint === BREAKPOINT_SMALL;
+
+	const intersectionEntry = useIntersection( trackingRef, {
+		threshold: 0.25,
+	} );
 	const [ hasBeenInView, setHasBeenInView ] = useState( false );
+	const inView = !! intersectionEntry?.intersectionRatio;
+
+	const { triggerSurvey } = useDispatch( CORE_USER );
+
+	const usingProxy = useSelect( ( select ) =>
+		select( CORE_SITE ).isUsingProxy()
+	);
+
 	useEffect( () => {
-		if ( inView && ! hasBeenInView && ga4Connected ) {
-			trackEvent(
-				`${ viewContext }_kmw-cta-notification`,
-				'view_notification'
-			);
+		if ( inView && ! hasBeenInView ) {
+			if ( ga4Connected ) {
+				trackEvent(
+					`${ viewContext }_kmw-cta-notification`,
+					'view_notification'
+				);
+			}
+
+			if ( usingProxy ) {
+				triggerSurvey( 'view_kmw_setup_cta', { ttl: DAY_IN_SECONDS } );
+			}
 
 			setHasBeenInView( true );
 		}
-	}, [ inView, viewContext, ga4Connected, hasBeenInView ] );
+	}, [
+		inView,
+		viewContext,
+		ga4Connected,
+		hasBeenInView,
+		usingProxy,
+		triggerSurvey,
+	] );
 
 	return (
 		<section
@@ -97,12 +129,6 @@ export default function KeyMetricsCTAContent( {
 					) }
 				</Row>
 			</Grid>
-			{ inView && (
-				<SurveyViewTrigger
-					triggerID="view_kmw_setup_cta"
-					ttl={ DAY_IN_SECONDS }
-				/>
-			) }
 		</section>
 	);
 }
