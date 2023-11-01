f20608b26bbe3224724cea84beed06c0ca31874b
Arafat Zahan <arafat.zahan@10up.com>
2023-11-02 01:31:41 +0600

    Use correct copyright error.

--- assets/js/googlesitekit/widgets/components/WPDashboardReportError.test.js
+++ assets/js/googlesitekit/widgets/components/WPDashboardReportError.test.js
@@ -1,7 +1,7 @@
 /**
  * WPDashboardReportError component tests.
  *
- * Site Kit by Google, Copyright 2021 Google LLC
+ * Site Kit by Google, Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
