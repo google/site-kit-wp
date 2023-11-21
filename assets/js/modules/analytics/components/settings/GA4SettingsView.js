8bc1fc47db7a441261d97371e9a8081405e6e2df
Simon Dowdles <simon.dowdles@get10up.com>
2023-11-21 09:43:26 +0200

    Remove conditional enhancedMeasurement check within GA4 settings view.

--- assets/js/modules/analytics/components/settings/GA4SettingsView.js
+++ assets/js/modules/analytics/components/settings/GA4SettingsView.js
@@ -34,13 +34,10 @@ import {
 } from '../../../analytics-4/datastore/constants';
 import Link from '../../../../components/Link';
 import VisuallyHidden from '../../../../components/VisuallyHidden';
-import { useFeature } from '../../../../hooks/useFeature';
 import SettingsEnhancedMeasurementView from '../../../analytics-4/components/settings/SettingsEnhancedMeasurementView';
 const { useSelect } = Data;
 
 export default function GA4SettingsView() {
-	const enhancedMeasurementEnabled = useFeature( 'enhancedMeasurement' );
-
 	const ga4PropertyID = useSelect( ( select ) =>
 		select( MODULES_ANALYTICS_4 ).getPropertyID()
 	);
@@ -157,10 +154,7 @@ export default function GA4SettingsView() {
 					</p>
 				</div>
 			</div>
-
-			{ enhancedMeasurementEnabled && (
-				<SettingsEnhancedMeasurementView />
-			) }
+			<SettingsEnhancedMeasurementView />
 		</Fragment>
 	);
 }
