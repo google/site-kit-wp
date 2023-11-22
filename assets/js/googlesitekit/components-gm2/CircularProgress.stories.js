d9b5b0cbc3cf51be46e35660d5706cc9e49299f5
nfmohit <nahid.mohit@10up.com>
2023-11-22 13:13:16 +0600

    Add Storybook story for `CircularProgress`.

---
+++ assets/js/googlesitekit/components-gm2/CircularProgress.stories.js
@@ -0,0 +1,56 @@
+/**
+ * Site Kit by Google, Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import CircularProgress from './CircularProgress';
+
+const Template = () => (
+	<div>
+		<h3>Colors</h3>
+		<div style={ { marginBottom: '50px' } }>
+			<h5>Primary Color</h5>
+			<CircularProgress color="primary" />
+		</div>
+		<div style={ { marginBottom: '50px' } }>
+			<h5>Secondary Color</h5>
+			<CircularProgress color="secondary" />
+		</div>
+
+		<h3>Sizes</h3>
+		<div style={ { marginBottom: '50px' } }>
+			<h5>Big (40px)</h5>
+			<CircularProgress size={ 40 } />
+		</div>
+		<div style={ { marginBottom: '50px' } }>
+			<h5>Medium (24px)</h5>
+			<CircularProgress size={ 24 } />
+		</div>
+		<div>
+			<h5>Small (14px)</h5>
+			<CircularProgress size={ 14 } />
+		</div>
+	</div>
+);
+
+export const Default = Template.bind( {} );
+Default.storyName = 'CircularProgress';
+
+export default {
+	title: 'Components/CircularProgress',
+	component: CircularProgress,
+};
