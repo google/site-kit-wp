d4b39e9eeb3ac9b46b3e44146ae9f2ffc82253f2
Hussain Thajutheen <hussain.thajutheen@10up.com>
2023-12-17 10:49:29 +0530

    Remove this comment. Testing for signing commit.

--- assets/js/modules/analytics/components/settings/SettingsForm.stories.js
+++ assets/js/modules/analytics/components/settings/SettingsForm.stories.js
@@ -44,6 +44,8 @@ const measurementID = webDataStreams.find(
 	// eslint-disable-next-line sitekit/acronym-case
 ).webStreamData.measurementId;
 
+// Remove this comment. Testing for signing commit.
+
 function Template( args ) {
 	return (
 		<div className="googlesitekit-layout">
