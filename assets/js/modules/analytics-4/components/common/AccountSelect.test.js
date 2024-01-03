a4f7331d3b562fc8df7c0f9fdae9934fceefcd03
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-01-03 22:10:09 +0530

    Remove the test for resetting property.

--- assets/js/modules/analytics-4/components/common/AccountSelect.test.js
+++ assets/js/modules/analytics-4/components/common/AccountSelect.test.js
@@ -25,16 +25,11 @@ import {
 	freezeFetch,
 	render,
 	waitFor,
-	act,
 } from '../../../../../../tests/js/test-utils';
-import {
-	MODULES_ANALYTICS_4,
-	PROPERTY_CREATE,
-} from '../../datastore/constants';
+import { MODULES_ANALYTICS_4 } from '../../datastore/constants';
 import { MODULES_TAGMANAGER } from '../../../tagmanager/datastore/constants';
 import { provideSiteInfo } from '../../../../../../tests/js/utils';
 import * as fixtures from '../../datastore/__fixtures__';
-import * as uaFixtures from '../../../analytics/datastore/__fixtures__';
 import {
 	ACCOUNT_CREATE,
 	MODULES_ANALYTICS,
@@ -154,87 +149,5 @@ describe( 'AccountSelect', () => {
 			.getAccountID();
 		expect( originalAccountID ).not.toEqual( newAccountID );
 		expect( newAccountID ).toEqual( ACCOUNT_CREATE );
-	} );
-
-	it( 'should reset the property ID when the account is changed', () => {
-		jest.useFakeTimers();
-
-		fetchMock.getOnce(
-			new RegExp( '^/google-site-kit/v1/core/modules/data/list' ),
-			{ body: [] }
-		);
-		fetchMock.getOnce(
-			new RegExp( '^/google-site-kit/v1/core/user/data/authentication' ),
-			{
-				authenticated: true,
-			}
-		);
-
-		const { getByText, container, registry } = render( <AccountSelect />, {
-			setupRegistry,
-		} );
-
-		const accountID = fixtures.accountSummaries[ 1 ]._id;
-		const properties = fixtures.accountSummaries[ 1 ].propertySummaries.map(
-			( property ) => {
-				return {
-					...property,
-					id: `UA-${ property._id }-1`,
-					// eslint-disable-next-line sitekit/acronym-case
-					accountId: accountID,
-				};
-			}
-		);
-		const propertyID = properties[ 0 ].id;
-		const profiles = uaFixtures.profiles.map( ( profile ) => {
-			return {
-				...profile,
-				accountId: accountID, // eslint-disable-line sitekit/acronym-case
-				// eslint-disable-next-line sitekit/acronym-case
-				webPropertyId: propertyID,
-			};
-		} );
-
-		// We still need to mock the UA properties and profiles, as they're used in the `selectAccount` action.
-		registry
-			.dispatch( MODULES_ANALYTICS )
-			.receiveGetProperties( properties, { accountID } );
-		registry.dispatch( MODULES_ANALYTICS ).receiveGetProfiles( profiles, {
-			accountID,
-			propertyID,
-		} );
-
-		registry
-			.dispatch( MODULES_ANALYTICS_4 )
-			.receiveGetWebDataStreamsBatch( fixtures.webDataStreamsBatch, {
-				propertyIDs: [ properties[ 0 ]._id ],
-			} );
-		registry
-			.dispatch( MODULES_ANALYTICS_4 )
-			.finishResolution( 'getWebDataStreamsBatch', [
-				properties[ 0 ]._id,
-			] );
-
-		act( () => {
-			// Click the label to expose the elements in the menu.
-			fireEvent.click( container.querySelector( '.mdc-floating-label' ) );
-			// Click this element to select it and fire the onChange event.
-			const account = fixtures.accountSummaries.find(
-				// eslint-disable-next-line sitekit/acronym-case
-				( acct ) => acct._id === properties[ 0 ]._accountID
-			);
-
-			fireEvent.click( getByText( account.displayName ) );
-		} );
-
-		act( () => {
-			jest.runAllTimers();
-		} );
-
-		const newGA4PropertyID = registry
-			.select( MODULES_ANALYTICS_4 )
-			.getPropertyID();
-
-		expect( newGA4PropertyID ).toBe( PROPERTY_CREATE );
 	} );
 } );
