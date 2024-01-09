57350286d3dc0a3ab25f9690e236e92bb1ee4800
Aleksej <aleksej.vukomanovic@10up.com>
2024-01-09 12:06:29 +0100

    Update tags_placement_test and add response parsing.

--- includes/Core/Util/Debug_Data.php
+++ includes/Core/Util/Debug_Data.php
@@ -10,6 +10,7 @@
 
 namespace Google\Site_Kit\Core\Util;
 
+use Exception;
 use Google\Site_Kit\Context;
 use Google\Site_Kit\Core\Authentication\Authentication;
 use Google\Site_Kit\Core\Authentication\Clients\OAuth_Client;
@@ -21,6 +22,10 @@ use Google\Site_Kit\Core\Storage\User_Options;
 use Google\Site_Kit\Core\Permissions\Permissions;
 use Google\Site_Kit\Core\REST_API\REST_Route;
 use Google\Site_Kit\Core\Util\Feature_Flags;
+use Google\Site_Kit\Modules\AdSense;
+use Google\Site_Kit\Modules\Analytics_4;
+use Google\Site_Kit\Modules\Tag_Manager;
+use WP_Error;
 use WP_REST_Server;
 
 /**
@@ -141,10 +146,10 @@ class Debug_Data {
 			function ( $tests ) {
 				global $wp_version;
 
-				if ( version_compare( $wp_version, '6.5', '<' ) ) {
+				if ( version_compare( $wp_version, '5.6', '<' ) ) {
 					$tests['direct']['tag_placement'] = array(
 						'label' => __( 'Tag Placement', 'google-site-kit' ),
-						'test'  => 'tag_placement',
+						'test'  => $this->get_method_proxy( 'tags_placement_test' ),
 					);
 
 					return $tests;
@@ -198,26 +203,98 @@ class Debug_Data {
 	protected function tags_placement_test() {
 		global $wp_version;
 
-		if ( version_compare( $wp_version, '6.5', '<' ) ) {
-			return ''; // @TODO implement the return array.
-		}
-
 		$result = array(
-			'label'       => __( 'Tags Placement', 'google-site-kit' ),
-			'status'      => 'good',
-			'badge'       => array(
+			'label'   => __( 'Tags Placement', 'google-site-kit' ),
+			'status'  => 'recommended',
+			'badge'   => array(
 				'label' => __( 'Site Kit', 'google-site-kit' ),
 				'color' => 'blue',
 			),
-			'description' => sprintf(
-				'<p>%s</p>',
-				__( 'Description', 'google-site-kit' )
-			),
-			'actions'     => '',
-			'test'        => 'tag_placement',
+			'actions' => '',
+			'test'    => 'tag_placement',
 		);
 
+		if ( version_compare( $wp_version, '5.6', '<' ) ) {
+			$result['description'] = sprintf(
+				'<p>%s</p>',
+				__( 'This feature requires WordPress version 5.6 or higher', 'google-site-kit' )
+			);
+
+			return $result;
+		}
+
+		$response = wp_remote_get( site_url() ); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.wp_remote_get_wp_remote_get
+
+		if ( is_wp_error( $response ) ) {
+			return $response;
+		}
+
+		try {
+			$response = $this->parse_response( $response );
+		} catch ( Exception $e ) {
+			return new WP_Error( 'exception', $e->getMessage() );
+		}
+
+		$active_modules = $this->modules->get_active_modules();
+		$active_modules = array_filter(
+			$active_modules,
+			function( $module ) {
+				return in_array(
+					$module->slug,
+					array(
+						Analytics_4::MODULE_SLUG,
+						AdSense::MODULE_SLUG,
+						Tag_Manager::MODULE_SLUG,
+					),
+					true
+				);
+			}
+		);
+
+		if ( empty( $active_modules ) ) {
+			$result['description'] = sprintf(
+				'<p>%s</p>',
+				__( 'Tag status not available: AdSense, Tag Manager, and Analytics modules are not connected.', 'google-site-kit' )
+			);
+
+			return $result;
+		}
+
+		$description = '';
+
+		foreach ( $active_modules as $module ) {
+			$module_name   = $module->slug; // Correct the slug to name.
+			$search_string = 'Google ' . $module_name . ' snippet added by Site Kit';
+			if ( strpos( $response, $search_string ) !== false ) {
+				$description .= '';
+			}
+		}
+
 		return $result;
+	}
+
+	/**
+	 * Validates and parses the given JSON response into an array.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param array $response HTTP response array.
+	 * @return mixed JSON decoded response.
+	 * @throws Exception Throws exception if response cannot be parsed or if an error is returned.
+	 */
+	private function parse_response( $response ) {
+		$body    = wp_remote_retrieve_body( $response );
+		$decoded = json_decode( $body, true );
+
+		if ( json_last_error() ) {
+			throw new Exception( 'Error while decoding response: ' . json_last_error() );
+		}
+
+		if ( ! empty( $decoded['error'] ) ) {
+			throw new Exception( $decoded['error'] );
+		}
+
+		return $decoded;
 	}
 
 	/**
