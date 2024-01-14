b7c27d63ec96fc17cf086b23900d3e7616647ff7
Arafat Zahan <arafat.zahan@10up.com>
2024-01-15 04:57:29 +0600

    Fix copyright year.

--- assets/js/util/date-range/get-next-date.test.js
+++ assets/js/util/date-range/get-next-date.test.js
@@ -1,7 +1,7 @@
 /**
  * `getNextDate` utility tests.
  *
- * Site Kit by Google, Copyright 2021 Google LLC
+ * Site Kit by Google, Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
