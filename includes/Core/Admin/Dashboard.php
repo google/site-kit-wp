875c39bc9364031ed17b2450818ba3f522325b4f
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-01-22 19:49:19 +0530

    Add the attribute in WP Dashboard.

--- includes/Core/Admin/Dashboard.php
+++ includes/Core/Admin/Dashboard.php
@@ -12,6 +12,7 @@ namespace Google\Site_Kit\Core\Admin;
 
 use Google\Site_Kit\Context;
 use Google\Site_Kit\Core\Assets\Assets;
+use Google\Site_Kit\Core\Authentication\Authentication;
 use Google\Site_Kit\Core\Modules\Modules;
 use Google\Site_Kit\Core\Permissions\Permissions;
 use Google\Site_Kit\Core\Util\Requires_Javascript_Trait;
@@ -51,6 +52,14 @@ final class Dashboard {
 	private $modules;
 
 	/**
+	 * Authentication instance.
+	 *
+	 * @since n.e.x.t
+	 * @var Authentication
+	 */
+	private $authentication;
+
+	/**
 	 * Constructor.
 	 *
 	 * @since 1.0.0
@@ -62,11 +71,13 @@ final class Dashboard {
 	public function __construct(
 		Context $context,
 		Assets $assets = null,
-		Modules $modules = null
+		Modules $modules = null,
+		Authentication $authentication = null
 	) {
 		$this->context = $context;
 		$this->assets  = $assets ?: new Assets( $this->context );
 		$this->modules = $modules ?: new Modules( $this->context );
+		$this->authentication = $authentication ?: new Authentication( $this->context );
 	}
 
 	/**
@@ -113,12 +124,14 @@ final class Dashboard {
 	 * Render the Site Kit WordPress Dashboard widget.
 	 *
 	 * @since 1.0.0
+	 * @since n.e.x.t Added the `data-view-only` attribute.
 	 */
 	private function render_googlesitekit_wp_dashboard() {
 
 		$this->render_noscript_html();
+		$is_view_only = ! $this->authentication->is_authenticated();
 		?>
-		<div id="js-googlesitekit-wp-dashboard" class="googlesitekit-plugin"></div>
+		<div id="js-googlesitekit-wp-dashboard" data-view-only="<?php echo esc_attr( $is_view_only ); ?>" class="googlesitekit-plugin"></div>
 		<?php
 	}
 }
