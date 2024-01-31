bf98fdc155f5718980f8d37edd591c0875115649
Aleksej <aleksej.vukomanovic@10up.com>
2024-01-31 09:58:08 +0100

    Update tests.

--- assets/js/modules/analytics-4/components/setup/SetupForm.test.js
+++ assets/js/modules/analytics-4/components/setup/SetupForm.test.js
@@ -54,40 +54,24 @@ const webDataStreamID = webDataStreamsBatch[ propertyID ][ 0 ]._id;
 
 describe( 'SetupForm', () => {
 	let registry;
-
 	beforeEach( () => {
 		registry = createTestRegistry();
 		provideSiteInfo( registry );
 		provideUserAuthentication( registry );
-		provideModules( registry, [
-			{ slug: 'analytics', active: true },
-			{ slug: 'analytics-4', active: true },
-		] );
+		provideModules( registry, [ { slug: 'analytics-4', active: true } ] );
 	} );
-
 	it( 'renders the form with a progress bar when GTM containers are not resolved', () => {
 		provideModules( registry, [
-			{ slug: 'analytics', active: true, connected: true },
 			{ slug: 'analytics-4', active: true, connected: true },
 			{ slug: 'tagmanager', active: true, connected: true },
 		] );
 		registry.dispatch( MODULES_ANALYTICS_4 ).setSettings( {} );
 		registry.dispatch( MODULES_TAGMANAGER ).setSettings( {} );
-
 		const { container, getByRole } = render( <SetupForm />, { registry } );
-
 		expect( container ).toMatchSnapshot();
-
 		expect( getByRole( 'progressbar' ) ).toBeInTheDocument();
 	} );
-
 	it( 'renders the form correctly', async () => {
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work
-		registry
-			.dispatch( MODULES_ANALYTICS )
-			.receiveGetProperties( [], { accountID } );
-
 		registry.dispatch( MODULES_ANALYTICS_4 ).setSettings( {} );
 		registry.dispatch( MODULES_TAGMANAGER ).setSettings( {} );
 		registry
@@ -115,11 +99,13 @@ describe( 'SetupForm', () => {
 					webDataStreamID,
 				}
 			);
-
-		// @TODO replace to analytics 4 datastore once it is decoupled from analytics module.
-		// Currently needed in order for test to work
-		await registry.dispatch( MODULES_ANALYTICS ).selectAccount( accountID );
-
+		await registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.selectAccount( accountID );
+		// @TODO: This is temporarily needed here as the AccountSelect component
+		// still relies on the `analytics` datastore. This should be removed
+		// once the two Analytics modules are entirely decoupled.
+		registry.dispatch( MODULES_ANALYTICS ).setAccountID( accountID );
 		const { container, getByText, waitForRegistry } = render(
 			<SetupForm />,
 			{
@@ -127,28 +113,17 @@ describe( 'SetupForm', () => {
 			}
 		);
 		await waitForRegistry();
-
 		expect( container ).toMatchSnapshot();
-
 		expect( getByText( 'Account' ) ).toBeInTheDocument();
 		expect( getByText( 'Property' ) ).toBeInTheDocument();
 		expect( getByText( 'Web Data Stream' ) ).toBeInTheDocument();
 	} );
-
 	it( 'submits the form upon pressing the CTA', async () => {
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		registry.dispatch( MODULES_ANALYTICS ).setSettings( {} );
 		registry.dispatch( MODULES_ANALYTICS_4 ).setSettings( {} );
 		registry.dispatch( MODULES_TAGMANAGER ).setSettings( {} );
 		registry
 			.dispatch( MODULES_ANALYTICS_4 )
 			.receiveGetAccountSummaries( accountSummaries );
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		registry
-			.dispatch( MODULES_ANALYTICS )
-			.receiveGetProperties( [], { accountID } );
 		registry
 			.dispatch( MODULES_ANALYTICS_4 )
 			.receiveGetProperty( properties[ 0 ], {
@@ -168,11 +143,13 @@ describe( 'SetupForm', () => {
 					webDataStreamID,
 				}
 			);
-
 		await registry
 			.dispatch( MODULES_ANALYTICS_4 )
 			.selectAccount( accountID );
-
+		// @TODO: This is temporarily needed here as the AccountSelect component
+		// still relies on the `analytics` datastore. This should be removed
+		// once the two Analytics modules are entirely decoupled.
+		registry.dispatch( MODULES_ANALYTICS ).setAccountID( accountID );
 		const finishSetup = jest.fn();
 		const { getByRole, waitForRegistry } = render(
 			<SetupForm finishSetup={ finishSetup } />,
@@ -181,7 +158,6 @@ describe( 'SetupForm', () => {
 			}
 		);
 		await waitForRegistry();
-
 		act( () => {
 			// It doesn't seem possible to show the dropdown menu within the test by clicking on the dropdown in the usual way.
 			// Therefore, we simulate the click on the dropdown menu item directly, despite it being hidden.
@@ -192,65 +168,39 @@ describe( 'SetupForm', () => {
 				} )
 			);
 		} );
-
 		registry
 			.dispatch( CORE_USER )
 			.receiveGetDismissedTours( [ ga4ReportingTour.slug ] );
-
 		const updateAnalyticsSettingsRegexp = new RegExp(
 			'/analytics-4/data/settings'
 		);
-
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		const updateLegacyAnalyticsSettingsRegexp = new RegExp(
-			'/analytics/data/settings'
-		);
-
 		const dismissItemEndpointRegexp = new RegExp(
 			'^/google-site-kit/v1/core/user/data/dismiss-item'
 		);
-
 		muteFetch( updateAnalyticsSettingsRegexp );
-		muteFetch( updateLegacyAnalyticsSettingsRegexp );
-
 		fetchMock.post( dismissItemEndpointRegexp, {
 			status: 200,
 			body: JSON.stringify( [
 				ENHANCED_MEASUREMENT_ACTIVATION_BANNER_DISMISSED_ITEM_KEY,
 			] ),
 		} );
-
 		act( () => {
 			fireEvent.click(
 				getByRole( 'button', { name: /Configure Analytics/i } )
 			);
 		} );
-
 		await waitForRegistry();
-
 		// An additional wait is required in order for all resolvers to finish.
 		await act( waitForDefaultTimeouts );
-
 		expect( fetchMock ).toHaveFetchedTimes(
 			1,
 			updateAnalyticsSettingsRegexp
 		);
-
 		expect( finishSetup ).toHaveBeenCalledTimes( 1 );
 	} );
-
 	it( 'auto-submits the form', async () => {
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		registry.dispatch( MODULES_ANALYTICS ).setSettings( {} );
 		registry.dispatch( MODULES_ANALYTICS_4 ).setSettings( {} );
 		registry.dispatch( MODULES_TAGMANAGER ).setSettings( {} );
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		registry
-			.dispatch( MODULES_ANALYTICS )
-			.receiveGetProperties( [], { accountID } );
 		registry.dispatch( MODULES_ANALYTICS_4 ).receiveGetExistingTag( null );
 		registry.dispatch( MODULES_ANALYTICS_4 ).receiveGetAccountSummaries(
 			accountSummaries.map( ( account ) => ( {
@@ -277,12 +227,13 @@ describe( 'SetupForm', () => {
 					webDataStreamID,
 				}
 			);
-
-		await registry.dispatch( MODULES_ANALYTICS ).selectAccount( accountID );
 		await registry
 			.dispatch( MODULES_ANALYTICS_4 )
 			.selectAccount( accountID );
-
+		// @TODO: This is temporarily needed here as the AccountSelect component
+		// still relies on the `analytics` datastore. This should be removed
+		// once the two Analytics modules are entirely decoupled.
+		registry.dispatch( MODULES_ANALYTICS ).setAccountID( accountID );
 		// Simulate an auto-submit case where the user is returning to the page
 		// after granting extra scopes necessary to submit.
 		// In this situation, the autoSubmit is set before the user goes to oAuth,
@@ -290,55 +241,37 @@ describe( 'SetupForm', () => {
 		registry
 			.dispatch( CORE_FORMS )
 			.setValues( FORM_SETUP, { autoSubmit: true } );
-
 		provideUserAuthentication( registry, {
 			grantedScopes: [ EDIT_SCOPE ],
 		} );
-
 		registry
 			.dispatch( CORE_USER )
 			.receiveGetDismissedTours( [ ga4ReportingTour.slug ] );
-
 		const createPropertyRegexp = new RegExp(
 			'/analytics-4/data/create-property'
 		);
-
 		const createWebDataStreamRegexp = new RegExp(
 			'/analytics-4/data/create-webdatastream'
 		);
-
 		const updateAnalyticsSettingsRegexp = new RegExp(
 			'/analytics-4/data/settings'
 		);
-
 		const accountSummariesRegexp = new RegExp(
 			'/analytics-4/data/account-summaries'
 		);
-
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		const updateLegacyAnalyticsSettingsRegexp = new RegExp(
-			'/analytics/data/settings'
-		);
-
 		fetchMock.post( createPropertyRegexp, {
 			status: 200,
 			body: properties[ 0 ],
 		} );
-
 		fetchMock.post( createWebDataStreamRegexp, {
 			status: 200,
 			body: webDataStreams[ 2 ],
 		} );
-
 		fetchMock.get( accountSummariesRegexp, {
 			status: 200,
 			body: accountSummaries,
 		} );
-
 		muteFetch( updateAnalyticsSettingsRegexp );
-		muteFetch( updateLegacyAnalyticsSettingsRegexp );
-
 		const finishSetup = jest.fn();
 		const { getByRole, waitForRegistry } = render(
 			<SetupForm finishSetup={ finishSetup } />,
@@ -347,44 +280,29 @@ describe( 'SetupForm', () => {
 			}
 		);
 		await waitForRegistry();
-
 		// An additional wait is required in order for all resolvers to finish.
 		await act( async () => {
 			await waitForDefaultTimeouts();
 		} );
-
 		// Ensure the form rendered successfully.
 		expect(
 			getByRole( 'button', { name: /Configure Analytics/i } )
 		).toBeInTheDocument();
-
 		await waitForRegistry();
-
 		// An additional wait is required in order for all resolvers to finish.
 		await act( async () => {
 			await waitForDefaultTimeouts();
 		} );
-
 		expect( fetchMock ).toHaveFetchedTimes( 1, createPropertyRegexp );
 		expect( fetchMock ).toHaveFetchedTimes( 1, createWebDataStreamRegexp );
 		expect( fetchMock ).toHaveFetchedTimes(
 			1,
 			updateAnalyticsSettingsRegexp
 		);
-
 		expect( finishSetup ).toHaveBeenCalledTimes( 1 );
 	} );
-
 	it( 'auto-submits the form only once in the case of an error', async () => {
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		registry.dispatch( MODULES_ANALYTICS ).setSettings( {} );
 		registry.dispatch( MODULES_TAGMANAGER ).setSettings( {} );
-		// @TODO remove after analytics 4 is decoupled from analytics module.
-		// Currently needed in order for test to work.
-		registry
-			.dispatch( MODULES_ANALYTICS )
-			.receiveGetProperties( [], { accountID } );
 		registry.dispatch( MODULES_ANALYTICS_4 ).receiveGetExistingTag( null );
 		registry.dispatch( MODULES_ANALYTICS_4 ).receiveGetAccountSummaries(
 			accountSummaries.map( ( account ) => ( {
@@ -397,11 +315,13 @@ describe( 'SetupForm', () => {
 			.receiveGetWebDataStreamsBatch( webDataStreamsBatch, {
 				propertyIDs: [ propertyID ],
 			} );
-
-		// @TODO replace to analytics 4 datastore once it is decoupled from analytics module.
-		// Currently needed in order for test to work
-		await registry.dispatch( MODULES_ANALYTICS ).selectAccount( accountID );
-
+		await registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.selectAccount( accountID );
+		// @TODO: This is temporarily needed here as the AccountSelect component
+		// still relies on the `analytics` datastore. This should be removed
+		// once the two Analytics modules are entirely decoupled.
+		registry.dispatch( MODULES_ANALYTICS ).setAccountID( accountID );
 		// Simulate an auto-submit case where the user is returning to the page
 		// after granting extra scopes necessary to submit.
 		// In this situation, the autoSubmit is set before the user goes to oAuth,
@@ -412,15 +332,12 @@ describe( 'SetupForm', () => {
 		provideUserAuthentication( registry, {
 			grantedScopes: [ EDIT_SCOPE ],
 		} );
-
 		registry
 			.dispatch( CORE_USER )
 			.receiveGetDismissedTours( [ ga4ReportingTour.slug ] );
-
 		const createPropertyRegexp = new RegExp(
 			'/analytics-4/data/create-property'
 		);
-
 		fetchMock.post( createPropertyRegexp, {
 			status: 403,
 			body: {
@@ -428,7 +345,6 @@ describe( 'SetupForm', () => {
 				error: 'Insufficient permissions',
 			},
 		} );
-
 		const finishSetup = jest.fn();
 		const { getByRole, waitForRegistry } = render(
 			<SetupForm finishSetup={ finishSetup } />,
@@ -437,18 +353,15 @@ describe( 'SetupForm', () => {
 			}
 		);
 		await waitForRegistry();
-
 		// Ensure the form rendered successfully.
 		expect(
 			getByRole( 'button', { name: /Configure Analytics/i } )
 		).toBeInTheDocument();
-
 		// While not strictly needed, add waits to match the successful auto-submit test case to help avoid a false positive result.
 		await waitForRegistry();
 		await act( async () => {
 			await waitForDefaultTimeouts();
 		} );
-
 		// Create property should have only been called once.
 		expect( fetchMock ).toHaveFetchedTimes( 1, createPropertyRegexp );
 		// Setup was not successful, so the finish function should not be called.
