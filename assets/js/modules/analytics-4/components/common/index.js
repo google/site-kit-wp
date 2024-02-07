89cea6e070e2d7cd45c3008513617389e194de8c
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-02-07 18:43:11 +0530

    Move ZeroDataMessage to analytics-4.

--- assets/js/modules/analytics-4/components/common/index.js
+++ assets/js/modules/analytics-4/components/common/index.js
@@ -25,3 +25,4 @@ export { default as PropertySelect } from './PropertySelect';
 export { default as TrackingExclusionSwitches } from './TrackingExclusionSwitches';
 export { default as UseSnippetSwitch } from './UseSnippetSwitch';
 export { default as Notice } from './Notice';
+export { default as ZeroDataMessage } from './ZeroDataMessage';
