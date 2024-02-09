50aa7a7e7f1172a1d0ad98ac07e7a9e9efee3e0e
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-02-09 15:42:40 +0530

    Fix broken UA stories.

--- assets/js/modules/analytics/components/setup/SetupFormGA4.stories.js
+++ assets/js/modules/analytics/components/setup/SetupFormGA4.stories.js
@@ -126,6 +126,10 @@ export default {
 				registry
 					.dispatch( MODULES_ANALYTICS )
 					.selectAccount( accountID );
+
+				registry
+					.dispatch( MODULES_ANALYTICS_4 )
+					.selectAccount( accountID );
 			};
 
 			return (
