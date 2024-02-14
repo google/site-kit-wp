278b93bc91684ee2e8fa4c8f76ca4acff052ae12
Aleksej <aleksej.vukomanovic@10up.com>
2024-02-14 17:24:35 +0100

    Add basic module outline class.

---
+++ includes/Modules/Ads.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Class Google\Site_Kit\Modules\Ads
+ *
+ * @package   Google\Site_Kit
+ * @copyright 2024 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Modules;
+
+use Google\Site_Kit\Core\Modules\Module;
+
+/**
+ * Class representing the Ads module.
+ *
+ * @since n.e.x.t
+ * @access private
+ * @ignore
+ */
+final class Ads extends Module {
+
+	/**
+	 * Module slug name.
+	 */
+	const MODULE_SLUG = 'ads';
+
+	/**
+	 * Registers functionality through WordPress hooks.
+	 *
+	 * @since n.e.x.t
+	 */
+	public function register() {
+
+	}
+
+	/**
+	 * Sets up information about the module.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return array Associative array of module info.
+	 */
+	protected function setup_info() {
+		return array(
+			'slug'        => 'ads',
+			'name'        => _x( 'Ads', 'Service name', 'google-site-kit' ),
+			'description' => __( 'Track conversions for your existing Google Ads campaigns.', 'google-site-kit' ),
+			'order'       => 1,
+			'homepage'    => __( 'https://google.com/ads', 'google-site-kit' ),
+		);
+	}
+
+}
