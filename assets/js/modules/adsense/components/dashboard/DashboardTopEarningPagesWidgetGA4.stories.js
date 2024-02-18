8bab3ec4774e45e38ff44505ea56c6b39fa31313
Arafat Zahan <arafat.zahan@10up.com>
2024-02-19 02:14:34 +0600

    Add stories.

--- assets/js/modules/adsense/components/dashboard/DashboardTopEarningPagesWidgetGA4.stories.js
+++ assets/js/modules/adsense/components/dashboard/DashboardTopEarningPagesWidgetGA4.stories.js
@@ -19,24 +19,93 @@
 /**
  * Internal dependencies
  */
-import { provideModules } from '../../../../../../tests/js/utils';
+import {
+	provideModuleRegistrations,
+	provideModules,
+} from '../../../../../../tests/js/utils';
 import WithRegistrySetup from '../../../../../../tests/js/WithRegistrySetup';
 import { MODULES_ADSENSE } from '../../datastore/constants';
 import { MODULES_ANALYTICS_4 } from '../../../analytics-4/datastore/constants';
 import DashboardTopEarningPagesWidgetGA4 from './DashboardTopEarningPagesWidgetGA4';
-import Widget from '../../../../googlesitekit/widgets/components/Widget';
+import { provideAnalytics4MockReport } from '../../../analytics-4/utils/data-mock';
+import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
+import { withWidgetComponentProps } from '../../../../googlesitekit/widgets/util';
+
+const mockAdsenseAccountID = 'pub-1234567890';
+
+const reportOptions = {
+	startDate: '2020-08-11',
+	endDate: '2020-09-07',
+	dimensions: [ 'pageTitle', 'pagePath', 'adSourceName' ],
+	metrics: [ { name: 'totalAdRevenue' } ],
+	dimensionFilter: {
+		filter: {
+			fieldName: 'adSourceName',
+			stringFilter: {
+				matchType: 'EXACT',
+				value: `Google AdSense account (${ mockAdsenseAccountID })`,
+			},
+		},
+	},
+	orderBys: [ { metric: { metricName: 'totalAdRevenue' } } ],
+	limit: 5,
+};
+
+const WidgetWithComponentProps = withWidgetComponentProps(
+	'adsenseTopEarningPagesGA4'
+)( DashboardTopEarningPagesWidgetGA4 );
 
 function Template() {
-	return <DashboardTopEarningPagesWidgetGA4 Widget={ Widget } />;
+	return <WidgetWithComponentProps />;
 }
 
 export const Default = Template.bind( {} );
 Default.args = {
 	setupRegistry: ( registry ) => {
-		registry.dispatch( MODULES_ANALYTICS_4 ).setAdSenseLinked( true );
+		provideAnalytics4MockReport( registry, reportOptions );
 	},
 };
 Default.storyName = 'Default';
+Default.scenario = {
+	label: 'Modules/AdSense/Widgets/DashboardTopEarningPagesWidgetGA4/Default',
+};
+
+export const Loading = Template.bind( {} );
+Loading.args = {
+	setupRegistry: ( registry ) => {
+		registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.startResolution( 'getReport', [ reportOptions ] );
+	},
+};
+Loading.storyName = 'Loading';
+
+export const DataUnavailable = Template.bind( {} );
+DataUnavailable.args = {
+	setupRegistry: ( registry ) => {
+		registry.dispatch( MODULES_ANALYTICS_4 ).receiveIsGatheringData( true );
+		registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.receiveGetReport( [], { options: reportOptions } );
+	},
+};
+DataUnavailable.storyName = 'Data Unavailable';
+DataUnavailable.scenario = {
+	label: 'Modules/AdSense/Widgets/DashboardTopEarningPagesWidgetGA4/DataUnavailable',
+};
+
+export const ZeroData = Template.bind( {} );
+ZeroData.args = {
+	setupRegistry: ( registry ) => {
+		registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.receiveGetReport( [], { options: reportOptions } );
+	},
+};
+ZeroData.storyName = 'Zero Data';
+ZeroData.scenario = {
+	label: 'Modules/AdSense/Widgets/DashboardTopEarningPagesWidgetGA4/ZeroData',
+};
 
 export const AdSenseNotLinked = Template.bind( {} );
 AdSenseNotLinked.args = {
@@ -45,6 +114,9 @@ AdSenseNotLinked.args = {
 	},
 };
 AdSenseNotLinked.storyName = 'AdSense Not Linked';
+AdSenseNotLinked.scenario = {
+	label: 'Modules/AdSense/Widgets/DashboardTopEarningPagesWidgetGA4/AdSenseNotLinked',
+};
 
 export const AdBlockerActive = Template.bind( {} );
 AdBlockerActive.args = {
@@ -53,6 +125,30 @@ AdBlockerActive.args = {
 	},
 };
 AdBlockerActive.storyName = 'Ad Blocker Active';
+AdBlockerActive.scenario = {
+	label: 'Modules/AdSense/Widgets/DashboardTopEarningPagesWidgetGA4/AdBlockerActive',
+};
+
+export const Error = Template.bind( {} );
+Error.args = {
+	setupRegistry: ( registry ) => {
+		const error = {
+			code: 'missing_required_param',
+			message: 'Request parameter is empty: metrics.',
+		};
+
+		registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.receiveError( error, 'getReport', [ reportOptions ] );
+		registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.finishResolution( 'getReport', [ reportOptions ] );
+	},
+};
+Error.storyName = 'Error';
+Error.scenario = {
+	label: 'Modules/AdSense/Widgets/DashboardTopEarningPagesWidgetGA4/Error',
+};
 
 export default {
 	title: 'Modules/AdSense/Widgets/DashboardTopEarningPagesWidgetGA4',
@@ -71,6 +167,22 @@ export default {
 						slug: 'analytics-4',
 					},
 				] );
+
+				provideModuleRegistrations( registry );
+
+				registry
+					.dispatch( MODULES_ADSENSE )
+					.setAccountID( mockAdsenseAccountID );
+
+				registry.dispatch( CORE_USER ).setReferenceDate( '2020-09-08' );
+
+				registry
+					.dispatch( MODULES_ANALYTICS_4 )
+					.setAdSenseLinked( true );
+
+				registry
+					.dispatch( MODULES_ANALYTICS_4 )
+					.receiveIsGatheringData( false );
 
 				args?.setupRegistry( registry );
 			};
