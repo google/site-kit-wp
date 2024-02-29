e77449142eef9227a35dc9e94fbac55916532966
Aleksej <aleksej.vukomanovic@10up.com>
2024-02-29 12:18:54 +0100

    Remove conditions from rendering subtle notifications.

--- assets/js/components/Header.js
+++ assets/js/components/Header.js
@@ -119,7 +119,7 @@ function Header( { children, subHeader, showNavigation } ) {
 
 			{ showNavigation && <DashboardNavigation /> }
 
-			{ isDashboard && ! isViewOnly && <SubtleNotifications /> }
+			<SubtleNotifications />
 
 			<EntityHeader />
 		</Fragment>
