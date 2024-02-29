87b20c0826465ffd0d2574676e4b4b9a2c0f93e8
Aleksej <aleksej.vukomanovic@10up.com>
2024-02-29 12:23:28 +0100

    Move all the conditions into the one main file.

--- assets/js/components/notifications/GA4AdSenseLinkedNotification.js
+++ assets/js/components/notifications/GA4AdSenseLinkedNotification.js
@@ -27,26 +27,44 @@ import { __ } from '@wordpress/i18n';
  */
 import Data from 'googlesitekit-data';
 import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
-import {
-	MODULES_ANALYTICS_4,
-	GA4_ADSENSE_LINKED_NOTIFICATION_DISMISSED_ITEM_KEY,
-} from '../../modules/analytics-4/datastore/constants';
-import useViewOnly from '../../hooks/useViewOnly';
+import { MODULES_ANALYTICS_4 } from '../../modules/analytics-4/datastore/constants';
+import { DATE_RANGE_OFFSET } from '../../modules/analytics/datastore/constants';
 import CheckFill from '../../../svg/icons/check-fill.svg';
 import { Button } from 'googlesitekit-components';
 import { Grid, Cell, Row } from '../../material-components';
-import { DATE_RANGE_OFFSET } from '../../modules/analytics/datastore/constants';
+import useViewOnly from '../../hooks/useViewOnly';
+import useDashboardType from '../../hooks/useDashboardType';
+import { isZeroReport } from '../../modules/analytics-4/utils';
+import { useFeature } from '../../hooks/useFeature';
+import { CORE_MODULES } from '../../googlesitekit/modules/datastore/constants';
 
 const { useSelect, useInViewSelect, useDispatch } = Data;
 
 export default function GA4AdSenseLinkedNotification() {
 	const viewOnly = useViewOnly();
+	const isDashboard = !! useDashboardType();
 
 	const isDismissed = useSelect( ( select ) =>
 		select( CORE_USER ).isItemDismissed(
 			GA4_ADSENSE_LINKED_NOTIFICATION_DISMISSED_ITEM_KEY
 		)
 	);
+
+	const isGA4AdSenseIntegrationEnabled = useFeature(
+		'ga4AdSenseIntegration'
+	);
+	const adSenseModuleConnected = useSelect( ( select ) =>
+		select( CORE_MODULES ).isModuleConnected( 'adsense' )
+	);
+	const AnalyticsModuleConnected = useSelect( ( select ) =>
+		select( CORE_MODULES ).isModuleConnected( 'analytics-4' )
+	);
+	const isAdSenseLinked = useSelect( ( select ) =>
+		select( MODULES_ANALYTICS_4 ).getAdSenseLinked()
+	);
+
+	const analyticsAndAdsenseConnectedAndLinked =
+		adSenseModuleConnected && AnalyticsModuleConnected && isAdSenseLinked;
 
 	const { dismissItem } = useDispatch( CORE_USER );
 
@@ -55,6 +73,9 @@ export default function GA4AdSenseLinkedNotification() {
 			offsetDays: DATE_RANGE_OFFSET,
 		} )
 	);
+
+	const GA4_ADSENSE_LINKED_NOTIFICATION_DISMISSED_ITEM_KEY =
+		'ga4_adsense_linked_notification_dimissed_item';
 
 	const reportOptions = {
 		startDate,
@@ -71,7 +92,13 @@ export default function GA4AdSenseLinkedNotification() {
 	};
 
 	const report = useInViewSelect( ( select ) => {
-		if ( viewOnly || isDismissed ) {
+		if (
+			viewOnly ||
+			! isDashboard ||
+			isDismissed ||
+			! analyticsAndAdsenseConnectedAndLinked ||
+			! isGA4AdSenseIntegrationEnabled
+		) {
 			return undefined;
 		}
 
@@ -82,34 +109,68 @@ export default function GA4AdSenseLinkedNotification() {
 			reportOptions,
 		] )
 	);
-	const dismissCallback = useCallback( () => {
+	const dismissNotificationForUser = useCallback( () => {
 		dismissItem( GA4_ADSENSE_LINKED_NOTIFICATION_DISMISSED_ITEM_KEY );
 	}, [ dismissItem ] );
 
+	// This notification should only appear when the user has connected their
+	// AdSense and Google Analytics accounts, but has not yet received any data
+	// from linking the accounts. If they have any data from the "linked" report,
+	// we show them a different notification and should not show this one. Check
+	// to see if the user already has data and dismiss this notification without
+	// showing it.
 	useEffect( () => {
 		if (
 			hasFinishedResolution &&
-			report?.rowCount !== null &&
+			! isZeroReport( report ) &&
 			! isDismissed &&
-			! viewOnly
+			! viewOnly &&
+			isDashboard &&
+			analyticsAndAdsenseConnectedAndLinked &&
+			! isGA4AdSenseIntegrationEnabled
 		) {
-			dismissCallback();
+			dismissNotificationForUser();
 		}
 	}, [
 		report,
 		isDismissed,
 		viewOnly,
+		isDashboard,
 		hasFinishedResolution,
-		dismissCallback,
+		analyticsAndAdsenseConnectedAndLinked,
+		isGA4AdSenseIntegrationEnabled,
+		dismissNotificationForUser,
 	] );
 
-	// Prevent flickering with the last condition, by not displaying the notification
-	// until resolution of the report is done.
+	// console.log(
+	// 	viewOnly ||
+	// 		! isDashboard ||
+	// 		isDismissed ||
+	// 		! isZeroReport( report ) ||
+	// 		! hasFinishedResolution ||
+	// 		! analyticsAndAdsenseConnectedAndLinked ||
+	// 		! isGA4AdSenseIntegrationEnabled
+	// );
+	// console.log(
+	// 	viewOnly,
+	// 	! isDashboard,
+	// 	isDismissed,
+	// 	! isZeroReport( report ),
+	// 	! hasFinishedResolution,
+	// 	! analyticsAndAdsenseConnectedAndLinked,
+	// 	! isGA4AdSenseIntegrationEnabled
+	// );
+
+	// Ensure resolution of the report has completed before showing this notification, since
+	// it should only appear when the user has no data in the report.
 	if (
 		viewOnly ||
+		! isDashboard ||
 		isDismissed ||
-		report?.rowCount !== null ||
-		! hasFinishedResolution
+		! isZeroReport( report ) ||
+		! hasFinishedResolution ||
+		! analyticsAndAdsenseConnectedAndLinked ||
+		! isGA4AdSenseIntegrationEnabled
 	) {
 		return null;
 	}
@@ -120,26 +181,26 @@ export default function GA4AdSenseLinkedNotification() {
 				<Cell
 					alignMiddle
 					size={ 12 }
-					className="googlesitekit-dashboard-notifications"
+					className="googlesitekit-subtle-notifications"
 				>
-					<div className="googlesitekit-dashboard-notifications__icon">
+					<div className="googlesitekit-subtle-notifications__icon">
 						<CheckFill width={ 24 } height={ 24 } />
 					</div>
-					<div className="googlesitekit-dashboard-notifications__content">
-						<div>
+					<div className="googlesitekit-subtle-notifications__content">
+						<p>
 							{ __(
 								'Your AdSense and Analytics accounts are linked.',
 								'google-site-kit'
 							) }
-						</div>
-						<div>
+						</p>
+						<p>
 							{ __(
 								'We’ll let you know as soon as there’s enough data available.',
 								'google-site-kit'
 							) }
-						</div>
+						</p>
 					</div>
-					<Button tertiary onClick={ dismissCallback }>
+					<Button tertiary onClick={ dismissNotificationForUser }>
 						{ __( 'Got it', 'google-site-kit' ) }
 					</Button>
 				</Cell>
