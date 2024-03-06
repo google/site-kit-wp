24b91d5b1584bd72628dd916e5b488dc279ff087
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2024-03-06 16:03:49 +0000

    Remove requirement for hasConsentAPI when showing CTA notice.

--- assets/js/components/settings/SettingsCardConsentMode.js
+++ assets/js/components/settings/SettingsCardConsentMode.js
@@ -139,22 +139,20 @@ export default function SettingsCardConsentMode() {
 					</Row>
 					{ ! isLoading && (
 						<Fragment>
-							{ consentAPIInfo?.hasConsentAPI &&
-								isAdsConnected &&
-								! isConsentModeEnabled && (
-									<Row>
-										<Cell size={ 12 }>
-											<SettingsNotice
-												className="googlesitekit-settings-consent-mode__recommendation-notice"
-												type={ TYPE_INFO }
-												notice={ __(
-													'If you have Google Ads campaigns for this site, it’s highly recommended to enable Consent mode - otherwise, you won’t be able to collect any metrics on the effectiveness of your campaigns in regions like the European Economic Area.',
-													'google-site-kit'
-												) }
-											/>
-										</Cell>
-									</Row>
-								) }
+							{ isAdsConnected && ! isConsentModeEnabled && (
+								<Row>
+									<Cell size={ 12 }>
+										<SettingsNotice
+											className="googlesitekit-settings-consent-mode__recommendation-notice"
+											type={ TYPE_INFO }
+											notice={ __(
+												'If you have Google Ads campaigns for this site, it’s highly recommended to enable Consent mode - otherwise, you won’t be able to collect any metrics on the effectiveness of your campaigns in regions like the European Economic Area.',
+												'google-site-kit'
+											) }
+										/>
+									</Cell>
+								</Row>
+							) }
 							{ !! consentAPIInfo && isConsentModeEnabled && (
 								<Row>
 									<Cell size={ 12 }>
