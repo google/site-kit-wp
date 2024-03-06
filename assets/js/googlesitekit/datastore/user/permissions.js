d8e152886dc2bb305066cee13d5a60078e921928
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-03-06 14:21:23 +0530

    Remove the @since tag for filtering analytics in getViewableModules.

--- assets/js/googlesitekit/datastore/user/permissions.js
+++ assets/js/googlesitekit/datastore/user/permissions.js
@@ -221,7 +221,6 @@ const baseSelectors = {
 	 * Gets viewable module slugs of the current user.
 	 *
 	 * @since 1.72.0
-	 * @since 1.101.0 Filters out the duplicate module slugs if the user has both ‘analytics’ and ‘analytics-4’ and shows only one based on whether the user is viewing the GA4 dashboard.
 	 *
 	 * @return {(Array|undefined)} An array of viewable module slugs. `undefined` if `modules` are not loaded yet.
 	 */
