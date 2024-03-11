3bd0230d3c127ce6e2e597543385942221d2d02c
Arafat Zahan <arafat.zahan@10up.com>
2024-03-11 17:00:16 +0600

    Update styles for SpinnerButton to account for new variant.

--- assets/js/googlesitekit/components-gm2/SpinnerButton.js
+++ assets/js/googlesitekit/components-gm2/SpinnerButton.js
@@ -42,6 +42,9 @@ export default function SpinnerButton( props ) {
 				'googlesitekit-button-icon--spinner',
 				{
 					'googlesitekit-button-icon--spinner__running': isSaving,
+					'googlesitekit-button-icon--spinner__left': spinnerOnLeft,
+					'googlesitekit-button-icon--spinner__right':
+						! spinnerOnLeft,
 				}
 			) }
 			icon={
