fb04415e0d8304814f31ce60474608d3c398775f
Arafat Zahan <arafat.zahan@10up.com>
2024-03-15 11:06:20 +0600

    Update story.

--- assets/js/components/OverlayNotification/AnalyticsAndAdSenseAccountsDetectedAsLinkedOverlayNotification.stories.js
+++ assets/js/components/OverlayNotification/AnalyticsAndAdSenseAccountsDetectedAsLinkedOverlayNotification.stories.js
@@ -21,12 +21,34 @@
  */
 import WithRegistrySetup from '../../../../tests/js/WithRegistrySetup';
 import {
+	provideModuleRegistrations,
 	provideModules,
 	provideUserAuthentication,
 } from '../../../../tests/js/utils';
+import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
+import { MODULES_ADSENSE } from '../../modules/adsense/datastore/constants';
 import { MODULES_ANALYTICS_4 } from '../../modules/analytics-4/datastore/constants';
+import { provideAnalytics4MockReport } from '../../modules/analytics-4/utils/data-mock';
 
 import AnalyticsAndAdSenseAccountsDetectedAsLinkedOverlayNotification from './AnalyticsAndAdSenseAccountsDetectedAsLinkedOverlayNotification';
+
+const adSenseAccountID = 'pub-1234567890';
+
+const reportOptions = {
+	startDate: '2020-08-11',
+	endDate: '2020-09-07',
+	dimensions: [ 'pagePath', 'adSourceName' ],
+	metrics: [ { name: 'totalAdRevenue' } ],
+	filter: {
+		fieldName: 'adSourceName',
+		stringFilter: {
+			matchType: 'EXACT',
+			value: `Google AdSense account (${ adSenseAccountID })`,
+		},
+	},
+	orderby: [ { metric: { metricName: 'totalAdRevenue' }, desc: true } ],
+	limit: 1,
+};
 
 function Template() {
 	return <AnalyticsAndAdSenseAccountsDetectedAsLinkedOverlayNotification />;
@@ -60,9 +82,19 @@ export default {
 					},
 				] );
 
+				provideModuleRegistrations( registry );
+
+				registry.dispatch( CORE_USER ).setReferenceDate( '2020-09-08' );
+
 				registry.dispatch( MODULES_ANALYTICS_4 ).setSettings( {
 					adSenseLinked: true,
 				} );
+
+				registry
+					.dispatch( MODULES_ADSENSE )
+					.setAccountID( adSenseAccountID );
+
+				provideAnalytics4MockReport( registry, reportOptions );
 			};
 
 			return (
