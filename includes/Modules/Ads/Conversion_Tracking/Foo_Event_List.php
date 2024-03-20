7d1738df7bc31cbb4e7b42c483a0a5407215ab43
Simon Dowdles <simon.dowdles@get10up.com>
2024-03-20 10:15:53 +0200

    Add Conversion_Tracking folder and classes, POC.

---
+++ includes/Modules/Ads/Conversion_Tracking/Foo_Event_List.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class Google\Site_Kit\Modules\Ads\Conversion_Tracking\Foo_Event_List
+ *
+ * @package   Google\Site_Kit\Modules\Ads\Conversion_Tracking
+ * @copyright 2024 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Modules\Ads\Conversion_Tracking;
+
+/**
+ * Foo event list.
+ *
+ * @since n.e.x.t
+ * @access private
+ * @ignore
+ */
+final class Foo_Event_List extends Event_List {
+	/**
+	 * Registers functionality.
+	 *
+	 * @since n.e.x.t
+	 */
+	public function register() {}
+}
