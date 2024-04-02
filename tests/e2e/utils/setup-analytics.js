7c49851ca0d48ecba882f1982d2a92927519bcb7
Aleksej <aleksej.vukomanovic@10up.com>
2024-04-02 13:50:45 +0200

    Remove old analytics setup e2e utility function.

--- tests/e2e/utils/setup-analytics.js
+++ tests/e2e/utils/setup-analytics.js
@@ -3,14 +3,6 @@
  */
 import { wpApiFetch } from './wp-api-fetch';
 
-const defaultAnalyticsSettings = {
-	accountID: 100,
-	propertyID: 200,
-	profileID: 300,
-	internalWebPropertyID: 400,
-	useSnippet: true,
-};
-
 const defaultAnalytics4Settings = {
 	accountID: '100',
 	propertyID: '500',
@@ -18,36 +10,6 @@ const defaultAnalytics4Settings = {
 	measurementID: 'G-700',
 	useSnippet: true,
 };
-
-/**
- * Activates the Analytics module and completes the UA setup process.
- *
- * @since 1.0.0
- *
- * @param {Object} settingsOverrides Optional settings to override the defaults.
- */
-export async function setupAnalytics( settingsOverrides = {} ) {
-	const settings = {
-		...defaultAnalyticsSettings,
-		...settingsOverrides,
-	};
-	// Activate the module.
-	await wpApiFetch( {
-		method: 'post',
-		path: 'google-site-kit/v1/core/modules/data/activation',
-		data: {
-			data: { slug: 'analytics', active: true },
-		},
-	} );
-	// Set placeholder connection data.
-	await wpApiFetch( {
-		method: 'post',
-		path: 'google-site-kit/v1/modules/analytics/data/settings',
-		data: {
-			data: settings,
-		},
-	} );
-}
 
 /**
  * Activates the Analytics module and completes the GA4 setup process.
