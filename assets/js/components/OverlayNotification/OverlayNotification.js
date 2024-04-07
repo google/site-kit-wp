1432de10f0cf005062432ef01be257ec4a83733d
Arafat Zahan <arafat.zahan@10up.com>
2024-04-07 20:02:39 +0600

    Fix OverlayNotification component.

--- assets/js/components/OverlayNotification/OverlayNotification.js
+++ assets/js/components/OverlayNotification/OverlayNotification.js
@@ -20,21 +20,90 @@
  * External dependencies
  */
 import { Slide } from '@material-ui/core';
+import propTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { useEffect } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import Data from 'googlesitekit-data';
+import { CORE_UI } from '../../googlesitekit/datastore/ui/constants';
+import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
+import {
+	BREAKPOINTS_DESKTOP,
+	BREAKPOINTS_MOBILE,
+	useBreakpoint,
+} from '../../hooks/useBreakpoint';
+
+const { useDispatch, useSelect } = Data;
 
 export default function OverlayNotification( {
-	animateNotification,
-	handleAnimationOnExited,
 	children,
+	GraphicDesktop,
+	GraphicMobile,
+	notificationID,
+	shouldShowNotification,
 } ) {
-	return (
-		<Slide
-			direction="up"
-			in={ animateNotification }
-			onExited={ handleAnimationOnExited }
-		>
+	const breakpoint = useBreakpoint();
+
+	const isDismissed = useSelect( ( select ) =>
+		select( CORE_USER ).isItemDismissed( notificationID )
+	);
+
+	const isShowingNotification = useSelect( ( select ) =>
+		BREAKPOINTS_DESKTOP.includes( breakpoint )
+			? select( CORE_UI ).isShowingOverlayNotification( notificationID )
+			: isDismissed === false
+	);
+
+	const { setOverlayNotificationToShow } = useDispatch( CORE_UI );
+
+	useEffect( () => {
+		if ( shouldShowNotification && ! isShowingNotification ) {
+			// If the conditions to show this notification are met AND no other
+			// notifications are showing, show this notification.
+			setOverlayNotificationToShow( notificationID );
+		}
+	}, [
+		isShowingNotification,
+		notificationID,
+		setOverlayNotificationToShow,
+		shouldShowNotification,
+	] );
+
+	if ( ! shouldShowNotification ) {
+		return null;
+	}
+
+	if ( BREAKPOINTS_MOBILE.includes( breakpoint ) ) {
+		return (
 			<div className="googlesitekit-overlay-notification">
+				{ children }
+
+				{ GraphicMobile && <GraphicMobile /> }
+			</div>
+		);
+	}
+
+	return (
+		<Slide direction="up" in={ isShowingNotification }>
+			<div className="googlesitekit-overlay-notification">
+				{ GraphicDesktop && <GraphicDesktop /> }
+
 				{ children }
 			</div>
 		</Slide>
 	);
 }
+
+OverlayNotification.propTypes = {
+	children: propTypes.node,
+	GraphicDesktop: propTypes.elementType,
+	GraphicMobile: propTypes.elementType,
+	notificationID: propTypes.string.isRequired,
+	shouldShowNotification: propTypes.bool,
+};
