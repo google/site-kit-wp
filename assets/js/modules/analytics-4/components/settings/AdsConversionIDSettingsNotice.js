0c750a6e52269b3b17015c29c5f0ba9732a02d4d
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2024-04-12 12:14:08 +0100

    Revert "Testing the `useIntersectionObserver()` implementation from https://usehooks-ts.com/react-hook/use-intersection-observer#api."
    This reverts commit c5e740601d606411b6a93c643c1c7fccb20b9533.

--- assets/js/modules/analytics-4/components/settings/AdsConversionIDSettingsNotice.js
+++ assets/js/modules/analytics-4/components/settings/AdsConversionIDSettingsNotice.js
@@ -19,7 +19,7 @@
 /**
  * External dependencies
  */
-// import { useIntersection } from 'react-use';
+import { useIntersection } from 'react-use';
 /**
  * WordPress dependencies
  */
@@ -48,154 +48,6 @@ import useViewContext from '../../../../hooks/useViewContext';
 import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
 
 const { useSelect } = Data;
-
-/*
-// This is the `useIntersection()` implementation from our version of `react-use`.
-const useIntersection = ( ref, options ) => {
-	const [ intersectionObserverEntry, setIntersectionObserverEntry ] =
-		useState( null );
-
-	useEffect( () => {
-		if ( ref.current && typeof IntersectionObserver === 'function' ) {
-			const handler = ( entries ) => {
-				setIntersectionObserverEntry( entries[ 0 ] );
-			};
-
-			const observer = new IntersectionObserver( handler, options );
-			observer.observe( ref.current );
-
-			return () => {
-				setIntersectionObserverEntry( null );
-				observer.disconnect();
-			};
-		}
-		return () => {};
-	}, [
-		ref.current,
-		options.threshold,
-		options.root,
-		options.rootMargin,
-	] );
-
-	return intersectionObserverEntry;
-};
-*/
-
-// Testing the `useIntersectionObserver()` implementation from https://usehooks-ts.com/react-hook/use-intersection-observer#api.
-export function useIntersection( {
-	threshold = 0,
-	root = null,
-	rootMargin = '0%',
-	freezeOnceVisible = false,
-	initialIsIntersecting = false,
-	onChange,
-} = {} ) {
-	const [ ref, setRef ] = useState( null );
-
-	const [ state, setState ] = useState( () => ( {
-		isIntersecting: initialIsIntersecting,
-		entry: undefined,
-	} ) );
-
-	const callbackRef = useRef();
-
-	callbackRef.current = onChange;
-
-	const frozen = state.entry?.isIntersecting && freezeOnceVisible;
-
-	useEffect( () => {
-		// Ensure we have a ref to observe
-		if ( ! ref ) {
-			return;
-		}
-
-		// Ensure the browser supports the Intersection Observer API
-		if ( ! ( 'IntersectionObserver' in global ) ) {
-			return;
-		}
-
-		// Skip if frozen
-		if ( frozen ) {
-			return;
-		}
-
-		let unobserve;
-
-		const observer = new IntersectionObserver(
-			( entries ) => {
-				const thresholds = Array.isArray( observer.thresholds )
-					? observer.thresholds
-					: [ observer.thresholds ];
-
-				entries.forEach( ( entry ) => {
-					const isIntersecting =
-						entry.isIntersecting &&
-						thresholds.some(
-							// eslint-disable-next-line no-shadow
-							( threshold ) =>
-								entry.intersectionRatio >= threshold
-						);
-
-					setState( { isIntersecting, entry } );
-
-					if ( callbackRef.current ) {
-						callbackRef.current( isIntersecting, entry );
-					}
-
-					if ( isIntersecting && freezeOnceVisible && unobserve ) {
-						unobserve();
-						unobserve = undefined;
-					}
-				} );
-			},
-			{ threshold, root, rootMargin }
-		);
-
-		observer.observe( ref );
-
-		return () => {
-			observer.disconnect();
-		};
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [
-		ref,
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-		JSON.stringify( threshold ),
-		root,
-		rootMargin,
-		frozen,
-		freezeOnceVisible,
-	] );
-
-	// ensures that if the observed element changes, the intersection observer is reinitialized
-	const prevRef = useRef( null );
-
-	useEffect( () => {
-		if (
-			! ref &&
-			state.entry?.target &&
-			! freezeOnceVisible &&
-			! frozen &&
-			prevRef.current !== state.entry.target
-		) {
-			prevRef.current = state.entry.target;
-			setState( {
-				isIntersecting: initialIsIntersecting,
-				entry: undefined,
-			} );
-		}
-	}, [ ref, state.entry, freezeOnceVisible, frozen, initialIsIntersecting ] );
-
-	const result = [ setRef, !! state.isIntersecting, state.entry ];
-
-	// Support object destructuring, by adding the specific values.
-	result.ref = result[ 0 ];
-	result.isIntersecting = result[ 1 ];
-	result.entry = result[ 2 ];
-
-	return result;
-}
 
 export default function AdsConversionIDSettingsNotice() {
 	const settingsAdminURL = useSelect( ( select ) =>
@@ -226,15 +78,10 @@ export default function AdsConversionIDSettingsNotice() {
 			'confirm_notification'
 		);
 	};
-	// const trackingRef = useRef();
-	// const intersectionEntry = useIntersection( trackingRef, {
-	// 	root: null,
-	// 	threshold: 0.45,
-	// } );
-	// eslint-disable-next-line no-unused-vars
-	const [ trackingRef, _, intersectionEntry ] = useIntersection( {
-		threshold: 0.45,
+	const trackingRef = useRef();
+	const intersectionEntry = useIntersection( trackingRef, {
 		root: null,
+		threshold: 0.45,
 	} );
 	const [ hasBeenInView, setHasBeenInView ] = useState( false );
 	const inView =
@@ -269,9 +116,6 @@ export default function AdsConversionIDSettingsNotice() {
 		shouldShowNotice,
 		viewContext,
 	] );
-
-	// eslint-disable-next-line no-console
-	console.info( { shouldShowNotice } );
 
 	// Do not show the notice if the view conditions have not been met.
 	if ( ! shouldShowNotice ) {
