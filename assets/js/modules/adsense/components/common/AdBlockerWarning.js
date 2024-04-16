99903063bab071952ad33978bbedea4159f00601
Jimmy Madon <hello@jimmymadon.com>
2024-04-16 15:25:06 +0530

    Refactor AdBlockerWarning to use reusable message component.

--- assets/js/modules/adsense/components/common/AdBlockerWarning.js
+++ assets/js/modules/adsense/components/common/AdBlockerWarning.js
@@ -20,23 +20,15 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-
-/**
- * WordPress dependencies
- */
-import { sprintf, __ } from '@wordpress/i18n';
-import { createInterpolateElement } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import Data from 'googlesitekit-data';
-import ErrorIcon from '../../../../../svg/icons/error.svg';
-import Link from '../../../../components/Link';
 
 import { MODULES_ADSENSE } from '../../datastore/constants';
 import { CORE_SITE } from '../../../../googlesitekit/datastore/site/constants';
+import AdBlockerWarningMessage from '../../../../components/AdBlockerWarningMessage';
 const { useSelect } = Data;
 
 export default function AdBlockerWarning( { context } ) {
@@ -47,34 +39,12 @@ export default function AdBlockerWarning( { context } ) {
 		select( CORE_SITE ).getDocumentationLinkURL( 'ad-blocker-detected' )
 	);
 
-	// Return nothing if loading or if everything is fine.
-	if ( ! adBlockerWarningMessage ) {
-		return null;
-	}
-
 	return (
-		<div
-			className={ classnames( 'googlesitekit-settings-module-warning', {
-				[ `googlesitekit-settings-module-warning--${ context }` ]:
-					context,
-			} ) }
-		>
-			{ createInterpolateElement(
-				sprintf(
-					/* translators: 1: The warning message. 2: "Get help" text. */
-					__(
-						'<ErrorIcon /> %1$s. <Link>%2$s</Link>',
-						'google-site-kit'
-					),
-					adBlockerWarningMessage,
-					__( 'Get help', 'google-site-kit' )
-				),
-				{
-					ErrorIcon: <ErrorIcon height="20" width="23" />,
-					Link: <Link href={ getHelpLink } external />,
-				}
-			) }
-		</div>
+		<AdBlockerWarningMessage
+			context={ context }
+			getHelpLink={ getHelpLink }
+			warningMessage={ adBlockerWarningMessage }
+		/>
 	);
 }
 
