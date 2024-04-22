145d971302534ee5865a75f05adfbbef3b8dda25
Arafat Zahan <arafat.zahan@10up.com>
2024-04-22 13:05:44 +0600

    Push WIP.

--- assets/js/modules/analytics-4/datastore/partial-data.js
+++ assets/js/modules/analytics-4/datastore/partial-data.js
@@ -30,7 +30,7 @@ import { createFetchStore } from '../../../googlesitekit/data/create-fetch-store
 import { createReducer } from '../../../googlesitekit/data/create-reducer';
 import { DATE_RANGE_OFFSET, MODULES_ANALYTICS_4 } from './constants';
 import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
-import { stringToDate } from '../../../util';
+import { getDateString } from '../../../util';
 
 const { createRegistrySelector } = Data;
 
@@ -46,7 +46,8 @@ const RESOURCE_TYPES = [
 
 const RECEOVE_RESOURCE_DATA_AVAILABILITY_DATES =
 	'RECEOVE_RESOURCE_DATA_AVAILABILITY_DATES';
-const SET_RESOURCE_PARTIAL_DATA_STATE = 'SET_RESOURCE_PARTIAL_DATA_STATE';
+const SET_RESOURCE_DATA_AVAILABILITY_DATE =
+	'SET_RESOURCE_DATA_AVAILABILITY_DATE';
 
 const fetchSaveResourceDataAvailabilityDateStore = createFetchStore( {
 	baseName: 'saveResourceDataAvailabilityDate',
@@ -102,16 +103,16 @@ const baseActions = {
 	},
 
 	/**
-	 * Sets the partial data state for a specific resource.
+	 * Sets the data availability date for a specific resource.
 	 *
 	 * @since n.e.x.t
 	 *
-	 * @param {string}  resourceSlug  Resource slug.
-	 * @param {string}  resourceType  Resource type.
-	 * @param {boolean} isPartialData TRUE if partial data, otherwise FALSE.
+	 * @param {string} resourceSlug Resource slug.
+	 * @param {string} resourceType Resource type.
+	 * @param {number} date         Data availability date.
 	 * @return {Object} Redux-style action.
 	 */
-	setResourcePartialDataState( resourceSlug, resourceType, isPartialData ) {
+	setResourceDataAvailabilityDate( resourceSlug, resourceType, date ) {
 		invariant(
 			'string' === typeof resourceSlug && resourceSlug.length > 0,
 			'resourceSlug must be a non-empty string.'
@@ -122,15 +123,99 @@ const baseActions = {
 			'resourceType must be a valid resource type.'
 		);
 
-		invariant(
-			'boolean' === typeof isPartialData,
-			'isPartialData must be a boolean.'
-		);
+		invariant( Number.isInteger( date ), 'date must be a integer.' );
 
 		return {
-			payload: { resourceSlug, resourceType, isPartialData },
-			type: SET_RESOURCE_PARTIAL_DATA_STATE,
+			payload: { resourceSlug, resourceType, date },
+			type: SET_RESOURCE_DATA_AVAILABILITY_DATE,
 		};
+	},
+
+	/**
+	 * Determines the data availability date for a specific resource.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {string} resourceSlug Resource slug.
+	 * @param {string} resourceType Resource type.
+	 */
+	*determineResourceDataAvailabilityDate( resourceSlug, resourceType ) {
+		const { select, __experimentalResolveSelect } =
+			yield Data.commonActions.getRegistry();
+
+		// Handle for custom dimensions.
+		yield Data.commonActions.await(
+			__experimentalResolveSelect( MODULES_ANALYTICS_4 ).getSettings()
+		);
+
+		// If resourceType is custom dimension, check if custom dimension is available in settings.
+		if ( resourceType === RESOURCE_TYPE_CUSTOM_DIMENSION ) {
+			// Return early if custom dimension is not available in settings.
+			if (
+				! select( MODULES_ANALYTICS_4 ).hasCustomDimensions(
+					resourceSlug
+				)
+			) {
+				return;
+			}
+		}
+
+		yield Data.commonActions.await(
+			__experimentalResolveSelect( CORE_USER ).getAuthentication()
+		);
+
+		// Return early if user is not authenticated.
+		if ( ! select( CORE_USER ).isAuthenticated() ) {
+			return;
+		}
+
+		const reportArgs = yield Data.commonActions.await(
+			__experimentalResolveSelect(
+				MODULES_ANALYTICS_4
+			).getPartialDataReportOptions( resourceSlug, resourceType )
+		);
+
+		// Return early if reportArgs is not available.
+		if ( ! reportArgs ) {
+			return;
+		}
+
+		const report = yield Data.commonActions.await(
+			__experimentalResolveSelect( MODULES_ANALYTICS_4 ).getReport(
+				reportArgs
+			)
+		);
+
+		const hasReportError = !! select(
+			MODULES_ANALYTICS_4
+		).getErrorForSelector( 'getReport', [ reportArgs ] );
+
+		global.console.log( {
+			report,
+			hasReportError,
+		} );
+
+		const isDataAvailable =
+			! hasReportError &&
+			report?.rows?.[ 0 ]?.dimensionValues?.[ 0 ]?.value?.length;
+
+		if ( isDataAvailable ) {
+			const dataAvailabilityDate = Number(
+				report.rows[ 0 ].dimensionValues[ 0 ].value
+			);
+
+			yield baseActions.setResourceDataAvailabilityDate(
+				resourceSlug,
+				resourceType,
+				dataAvailabilityDate
+			);
+
+			// yield fetchSaveResourceDataAvailabilityDateStore.actions.fetchSaveResourceDataAvailabilityDate(
+			// 	resourceSlug,
+			// 	resourceType,
+			// 	dataAvailabilityDate
+			// );
+		}
 	},
 };
 
@@ -145,6 +230,24 @@ const baseReducer = createReducer( ( state, { type, payload } ) => {
 			break;
 		}
 
+		case SET_RESOURCE_DATA_AVAILABILITY_DATE: {
+			const { resourceSlug, resourceType, date } = payload;
+
+			if ( state.resourceAvailabilityDates === undefined ) {
+				state.resourceAvailabilityDates = {};
+			}
+
+			if (
+				state.resourceAvailabilityDates[ resourceType ] === undefined
+			) {
+				state.resourceAvailabilityDates[ resourceType ] = {};
+			}
+
+			state.resourceAvailabilityDates[ resourceType ][ resourceSlug ] =
+				date;
+			break;
+		}
+
 		default: {
 			break;
 		}
@@ -153,12 +256,11 @@ const baseReducer = createReducer( ( state, { type, payload } ) => {
 
 const baseResolvers = {
 	*getResourceDataAvailabilityDates() {
-		const registry = yield Data.commonActions.getRegistry();
+		const { select } = yield Data.commonActions.getRegistry();
 
 		if (
-			registry
-				.select( MODULES_ANALYTICS_4 )
-				.getResourceDataAvailabilityDates() !== undefined
+			select( MODULES_ANALYTICS_4 ).getResourceDataAvailabilityDates() !==
+			undefined
 		) {
 			return;
 		}
@@ -173,24 +275,83 @@ const baseResolvers = {
 			);
 		}
 	},
+
 	*getResourceDataAvailabilityDate( resourceSlug, resourceType ) {
-		const registry = yield Data.commonActions.getRegistry();
+		const { select, __experimentalResolveSelect } =
+			yield Data.commonActions.getRegistry();
 
 		if (
-			registry
-				.select( MODULES_ANALYTICS_4 )
-				.getResourceDataAvailabilityDate(
-					resourceSlug,
-					resourceType
-				) !== undefined
+			select( MODULES_ANALYTICS_4 ).getResourceDataAvailabilityDate(
+				resourceSlug,
+				resourceType
+			) !== undefined
 		) {
 			return;
 		}
 
+		const resourceAvailabilityDates = yield Data.commonActions.await(
+			__experimentalResolveSelect(
+				MODULES_ANALYTICS_4
+			).getResourceDataAvailabilityDates()
+		);
+
+		if (
+			resourceAvailabilityDates[ resourceType ][ resourceSlug ] ===
+			undefined
+		) {
+			global.console.log(
+				'setting temporary data availability date to 0'
+			);
+			yield baseActions.setResourceDataAvailabilityDate(
+				resourceSlug,
+				resourceType,
+				0
+			);
+			global.console.log( 'determining data availability date' );
+			yield baseActions.determineResourceDataAvailabilityDate(
+				resourceSlug,
+				resourceType
+			);
+		}
+	},
+
+	*isResourcePartialData( resourceSlug, resourceType ) {
+		const { select, __experimentalResolveSelect } =
+			yield Data.commonActions.getRegistry();
+
+		if (
+			select( MODULES_ANALYTICS_4 ).isResourcePartialData(
+				resourceSlug,
+				resourceType
+			) !== undefined
+		) {
+			return;
+		}
+
+		const dataAvailabilityDate = yield Data.commonActions.await(
+			__experimentalResolveSelect(
+				MODULES_ANALYTICS_4
+			).getResourceDataAvailabilityDate( resourceSlug, resourceType )
+		);
+
+		// If the data availability date for the given resource is 0,
+		// We need to determine the date by making a report request.
+		if ( dataAvailabilityDate === 0 ) {
+			yield baseActions.determineResourceDataAvailabilityDate(
+				resourceSlug,
+				resourceType
+			);
+		}
+	},
+
+	*getPartialDataReportOptions() {
+		const { __experimentalResolveSelect } =
+			yield Data.commonActions.getRegistry();
+
 		yield Data.commonActions.await(
-			registry
-				.__experimentalResolveSelect( MODULES_ANALYTICS_4 )
-				.getResourceDataAvailabilityDates()
+			__experimentalResolveSelect(
+				MODULES_ANALYTICS_4
+			).getPropertyCreateTime()
 		);
 	},
 };
@@ -265,11 +426,15 @@ const baseSelectors = {
 				offsetDays: DATE_RANGE_OFFSET,
 			} );
 
-			const startDateinMS = stringToDate( startDate ).getTime();
+			const startDateYYYYMMDD = Number( startDate.replaceAll( '-', '' ) );
 
-			// If the data availability date is same or earlier than the date range start date,
-			// the resource is not in partial data state.
-			return dataAvailabilityDate <= startDateinMS;
+			global.console.log( {
+				dataAvailabilityDate,
+				startDate: startDateYYYYMMDD,
+				isParialData: dataAvailabilityDate > startDateYYYYMMDD,
+			} );
+
+			return dataAvailabilityDate > startDateYYYYMMDD;
 		}
 	),
 
@@ -285,7 +450,6 @@ const baseSelectors = {
 	isAudiencePartialData: createRegistrySelector(
 		( select ) => ( state, audienceSlug ) =>
 			select( MODULES_ANALYTICS_4 ).isResourcePartialData(
-				state,
 				audienceSlug,
 				RESOURCE_TYPE_AUDIENCE
 			)
@@ -303,7 +467,6 @@ const baseSelectors = {
 	isCustomDimensionPartialData: createRegistrySelector(
 		( select ) => ( state, customDimensionSlug ) =>
 			select( MODULES_ANALYTICS_4 ).isResourcePartialData(
-				state,
 				customDimensionSlug,
 				RESOURCE_TYPE_CUSTOM_DIMENSION
 			)
@@ -321,10 +484,81 @@ const baseSelectors = {
 	isPropertyPartialData: createRegistrySelector(
 		( select ) => ( state, propertyID ) =>
 			select( MODULES_ANALYTICS_4 ).isResourcePartialData(
-				state,
 				propertyID,
 				RESOURCE_TYPE_PROPERTY
 			)
+	),
+	/**
+	 * Gets report options for determining data availability date for a given resource.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {Object} state        Data store's state.
+	 * @param {string} resourceSlug Resource slug.
+	 * @param {string} resourceType Resource type.
+	 * @return {Object|undefined} Report options for determining data availability date. Undefined if not available.
+	 */
+	getPartialDataReportOptions: createRegistrySelector(
+		( select ) => ( state, resourceSlug, resourceType ) => {
+			invariant(
+				typeof resourceSlug === 'string',
+				'resourceSlug must be a string.'
+			);
+			invariant(
+				RESOURCE_TYPES.includes( resourceType ),
+				'resourceType must be a valid resource type.'
+			);
+
+			const propertyID = select( MODULES_ANALYTICS_4 ).getPropertyID();
+
+			if ( ! propertyID ) {
+				return undefined;
+			}
+
+			const propertyCreateTime =
+				select( MODULES_ANALYTICS_4 ).getPropertyCreateTime();
+
+			const endDate = select( CORE_USER ).getReferenceDate();
+
+			switch ( resourceType ) {
+				case RESOURCE_TYPE_CUSTOM_DIMENSION:
+					return {
+						startDate: getDateString(
+							new Date( propertyCreateTime )
+						),
+						endDate,
+						dimensions: [ 'date', `customEvent:${ resourceSlug }` ],
+						metrics: [ { name: 'eventCount' } ],
+						limit: 1,
+					};
+
+				case RESOURCE_TYPE_AUDIENCE:
+					return {
+						startDate: getDateString(
+							new Date( propertyCreateTime )
+						),
+						endDate,
+						dimensions: [
+							'date',
+							'audienceResourceName',
+							'audienceName',
+						],
+						metrics: [ { name: 'totalUsers' } ],
+						limit: 1,
+					};
+
+				case RESOURCE_TYPE_PROPERTY:
+					return {
+						startDate: getDateString(
+							new Date( propertyCreateTime )
+						),
+						endDate,
+						dimensions: [ 'date' ],
+						metrics: [ { name: 'totalUsers' } ],
+						limit: 1,
+					};
+			}
+		}
 	),
 };
 
