899d65b8e56a8c29e67a193f594a3234781b331b
Simon Dowdles <simon.dowdles@get10up.com>
2024-04-23 13:56:48 +0200

    Change conditional ads settings view to only show pax fields if ext customer ID set.

--- assets/js/modules/ads/components/settings/SettingsView.js
+++ assets/js/modules/ads/components/settings/SettingsView.js
@@ -52,7 +52,7 @@ export default function SettingsView() {
 	return (
 		<Fragment>
 			<AdBlockerWarning />
-			{ ! isAdBlockerActive && ! extCustomerID && (
+			{ ! isAdBlockerActive && ! paxConversionID && ! extCustomerID && (
 				<div className="googlesitekit-settings-module__meta-item">
 					<h5 className="googlesitekit-settings-module__meta-item-type">
 						{ __( 'Conversion Tracking ID', 'google-site-kit' ) }
@@ -67,7 +67,7 @@ export default function SettingsView() {
 				</div>
 			) }
 
-			{ ! isAdBlockerActive && extCustomerID && (
+			{ ! isAdBlockerActive && paxConversionID && extCustomerID && (
 				<div className="googlesitekit-settings-module__meta-item">
 					<h5 className="googlesitekit-settings-module__meta-item-type">
 						{ __( 'Conversion Tracking ID', 'google-site-kit' ) }
@@ -82,7 +82,7 @@ export default function SettingsView() {
 				</div>
 			) }
 
-			{ ! isAdBlockerActive && extCustomerID && (
+			{ ! isAdBlockerActive && paxConversionID && extCustomerID && (
 				<div className="googlesitekit-settings-module__meta-item">
 					<h5 className="googlesitekit-settings-module__meta-item-type">
 						{ __( 'Customer ID', 'google-site-kit' ) }
