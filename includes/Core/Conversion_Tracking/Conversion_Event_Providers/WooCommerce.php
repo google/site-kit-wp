7a50c95a7b0ee55e57abc7e534cbce00fa30b5c7
Eugene Manuilov <eugene.manuilov@gmail.com>
2024-04-29 18:21:00 +0300

    Fix typo in the class name.

--- includes/Core/Conversion_Tracking/Conversion_Event_Providers/WooCommerce.php
+++ includes/Core/Conversion_Tracking/Conversion_Event_Providers/WooCommerce.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Class Google\Site_Kit\Core\Conversion_Tracking\Conversion_Events_Providers\WooCommerce
+ * Class Google\Site_Kit\Core\Conversion_Tracking\Conversion_Event_Providers\WooCommerce
  *
- * @package   Google\Site_Kit\Core\Conversion_Tracking\Conversion_Events_Providers
+ * @package   Google\Site_Kit\Core\Conversion_Tracking\Conversion_Event_Providers
  * @copyright 2024 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
 
-namespace Google\Site_Kit\Core\Conversion_Tracking\Conversion_Events_Providers;
+namespace Google\Site_Kit\Core\Conversion_Tracking\Conversion_Event_Providers;
 
 use Google\Site_Kit\Core\Assets\Script;
 use Google\Site_Kit\Core\Conversion_Tracking\Conversion_Events_Provider;
@@ -32,7 +32,7 @@ class WooCommerce extends Conversion_Events_Provider {
 	 * @return bool True if WooCommerce is active, false otherwise.
 	 */
 	public function is_active() {
-		return did_action( 'woocommerce_loaded' );
+		return did_action( 'woocommerce_loaded' ) > 0;
 	}
 
 	/**
--- includes/Core/Conversion_Tracking/Conversion_Tracking.php
+++ includes/Core/Conversion_Tracking/Conversion_Tracking.php
@@ -11,7 +11,7 @@
 namespace Google\Site_Kit\Core\Conversion_Tracking;
 
 use Google\Site_Kit\Context;
-use Google\Site_Kit\Core\Conversion_Tracking\Conversion_Events_Providers\WooCommerce;
+use Google\Site_Kit\Core\Conversion_Tracking\Conversion_Event_Providers\WooCommerce;
 use InvalidArgumentException;
 
 /**
