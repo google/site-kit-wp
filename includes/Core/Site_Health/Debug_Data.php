3f8379198286214e4b238754ba8ac05894043f67
Jimmy Madon <hello@jimmymadon.com>
2024-05-06 12:31:20 +0100

    Make naming consistent.

--- includes/Core/Site_Health/Debug_Data.php
+++ includes/Core/Site_Health/Debug_Data.php
@@ -164,33 +164,33 @@ class Debug_Data {
 	 */
 	protected function get_fields() {
 		$fields = array(
-			'version'              => array(
+			'version'                           => array(
 				'label' => __( 'Version', 'google-site-kit' ),
 				'value' => GOOGLESITEKIT_VERSION,
 			),
-			'php_version'          => array(
+			'php_version'                       => array(
 				'label' => __( 'PHP Version', 'google-site-kit' ),
 				'value' => PHP_VERSION,
 			),
-			'wp_version'           => array(
+			'wp_version'                        => array(
 				'label' => __( 'WordPress Version', 'google-site-kit' ),
 				'value' => get_bloginfo( 'version' ),
 			),
-			'reference_url'        => array(
+			'reference_url'                     => array(
 				'label' => __( 'Reference Site URL', 'google-site-kit' ),
 				'value' => $this->context->get_reference_site_url(),
 			),
-			'amp_mode'             => $this->get_amp_mode_field(),
-			'site_status'          => $this->get_site_status_field(),
-			'user_status'          => $this->get_user_status_field(),
-			'verification_status'  => $this->get_verification_status_field(),
-			'connected_user_count' => $this->get_connected_user_count_field(),
-			'active_modules'       => $this->get_active_modules_field(),
-			'recoverable_modules'  => $this->get_recoverable_modules_field(),
-			'required_scopes'      => $this->get_required_scopes_field(),
-			'capabilities'         => $this->get_capabilities_field(),
-			'enabled_features'     => $this->get_feature_fields(),
-			'active_providers'     => $this->get_active_conversion_event_provider_fields(),
+			'amp_mode'                          => $this->get_amp_mode_field(),
+			'site_status'                       => $this->get_site_status_field(),
+			'user_status'                       => $this->get_user_status_field(),
+			'verification_status'               => $this->get_verification_status_field(),
+			'connected_user_count'              => $this->get_connected_user_count_field(),
+			'active_modules'                    => $this->get_active_modules_field(),
+			'recoverable_modules'               => $this->get_recoverable_modules_field(),
+			'required_scopes'                   => $this->get_required_scopes_field(),
+			'capabilities'                      => $this->get_capabilities_field(),
+			'enabled_features'                  => $this->get_feature_fields(),
+			'active_conversion_event_providers' => $this->get_active_conversion_event_provider_fields(),
 		);
 
 		$fields = array_merge( $fields, $this->get_consent_mode_fields() );
