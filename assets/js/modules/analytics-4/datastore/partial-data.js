da2e77f73a16eec05d8ee0b86d21267786834362
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2024-05-08 19:44:40 +0100

    Create audiences and custom dimension in `enableAudienceGroups`.

--- assets/js/modules/analytics-4/datastore/partial-data.js
+++ assets/js/modules/analytics-4/datastore/partial-data.js
@@ -31,7 +31,12 @@ import { createFetchStore } from '../../../googlesitekit/data/create-fetch-store
 import { createReducer } from '../../../googlesitekit/data/create-reducer';
 import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
 import { getDateString, getPreviousDate } from '../../../util';
-import { DATE_RANGE_OFFSET, MODULES_ANALYTICS_4 } from './constants';
+import {
+	CUSTOM_DIMENSION_DEFINITIONS,
+	DATE_RANGE_OFFSET,
+	MODULES_ANALYTICS_4,
+	SITE_KIT_AUDIENCE_DEFINITIONS,
+} from './constants';
 
 const { createRegistrySelector } = Data;
 
@@ -51,6 +56,56 @@ const RECEIVE_RESOURCE_DATA_AVAILABILITY_DATES =
 	'RECEIVE_RESOURCE_DATA_AVAILABILITY_DATES';
 const SET_RESOURCE_DATA_AVAILABILITY_DATE =
 	'SET_RESOURCE_DATA_AVAILABILITY_DATE';
+
+async function getNonZeroDataAudiencesSortedByTotalUsers(
+	registry,
+	audiences,
+	startDate,
+	endDate
+) {
+	const { select, __experimentalResolveSelect } = registry;
+
+	const args = {
+		metrics: [ { name: 'totalUsers' } ],
+		dimensions: [ 'audienceResourceName' ],
+		dimensionFilters: {
+			audienceResourceName: audiences.map( ( { name } ) => name ),
+		},
+		startDate,
+		endDate,
+	};
+
+	const report = await __experimentalResolveSelect(
+		MODULES_ANALYTICS_4
+	).getReport( args );
+
+	const error = select( MODULES_ANALYTICS_4 ).getErrorForSelector(
+		'getReport',
+		[ args ]
+	);
+
+	if ( error ) {
+		// TODO: Full error handling will be implemented via https://github.com/google/site-kit-wp/issues/8134.
+		return { error };
+	}
+
+	const sortedRows = [ ...( report.rows || [] ) ].sort(
+		( rowA, rowB ) =>
+			( rowB.metricValues?.[ 0 ]?.value || 0 ) - // `value` is `totalUsers`.
+			( rowA.metricValues?.[ 0 ]?.value || 0 )
+	);
+
+	// Rows without data shouldn't actually be returned in the report, but to be safe, filter them out if present.
+	const rowsWithData = sortedRows.filter(
+		( { metricValues } ) => metricValues?.[ 0 ]?.value > 0
+	);
+
+	return {
+		audienceResourceNames: rowsWithData.map(
+			( { dimensionValues } ) => dimensionValues?.[ 0 ]?.value // `value` is `audienceResourceName`.
+		),
+	};
+}
 
 const fetchSaveResourceDataAvailabilityDateStore = createFetchStore( {
 	baseName: 'saveResourceDataAvailabilityDate',
@@ -140,15 +195,16 @@ const baseActions = {
 	},
 
 	*enableAudienceGroups() {
-		const { dispatch, select, __experimentalResolveSelect } =
-			yield Data.commonActions.getRegistry();
+		const registry = yield Data.commonActions.getRegistry();
 
-		const { response: availableAudiences, error } =
+		const { dispatch, select, __experimentalResolveSelect } = registry;
+
+		const { response: availableAudiences, error: syncError } =
 			yield Data.commonActions.await(
 				dispatch( MODULES_ANALYTICS_4 ).syncAvailableAudiences()
 			);
 
-		if ( error ) {
+		if ( syncError ) {
 			// TODO: Full error handling will be implemented via https://github.com/google/site-kit-wp/issues/8134.
 			return;
 		}
@@ -170,49 +226,21 @@ const baseActions = {
 				90 + DATE_RANGE_OFFSET // Add offset to ensure we have data for the entirety of the last 90 days.
 			);
 
-			const args = {
-				metrics: [ { name: 'totalUsers' } ],
-				dimensions: [ 'audienceResourceName' ],
-				dimensionFilters: {
-					audienceResourceName: userAudiences.map(
-						( { name } ) => name
-					),
-				},
-				startDate,
-				endDate,
-			};
+			const { audienceResourceNames, error } =
+				yield Data.commonActions.await(
+					getNonZeroDataAudiencesSortedByTotalUsers(
+						registry,
+						userAudiences,
+						startDate,
+						endDate
+					)
+				);
 
-			const report = yield Data.commonActions.await(
-				__experimentalResolveSelect( MODULES_ANALYTICS_4 ).getReport(
-					args
-				)
-			);
-
-			const hasReportError = select(
-				MODULES_ANALYTICS_4
-			).getErrorForSelector( 'getReport', [ args ] );
-
-			if ( ! hasReportError ) {
+			if ( ! error ) {
 				// TODO: Full error handling will be implemented via https://github.com/google/site-kit-wp/issues/8134.
-
-				const sortedRows = [ ...( report.rows || [] ) ].sort(
-					( rowA, rowB ) =>
-						( rowB.metricValues?.[ 0 ]?.value || 0 ) - // `value` is `totalUsers`.
-						( rowA.metricValues?.[ 0 ]?.value || 0 )
+				configuredAudiences.push(
+					...audienceResourceNames.slice( 0, MAX_INITIAL_AUDIENCES )
 				);
-
-				// Rows without data shouldn't actually be returned in the report, but to be safe, filter them out if present.
-				const rowsWithData = sortedRows.filter(
-					( { metricValues } ) => metricValues?.[ 0 ]?.value > 0
-				);
-
-				const audienceResourceNames = rowsWithData
-					.slice( 0, MAX_INITIAL_AUDIENCES )
-					.map(
-						( { dimensionValues } ) => dimensionValues?.[ 0 ]?.value // `value` is `audienceResourceName`.
-					);
-
-				configuredAudiences.push( ...audienceResourceNames );
 			}
 		}
 
@@ -244,17 +272,80 @@ const baseActions = {
 				.slice( 0, MAX_INITIAL_AUDIENCES - configuredAudiences.length )
 				.map( ( { name } ) => name );
 
-			// log( { audienceResourceNames } );
-
 			configuredAudiences.push( ...audienceResourceNames );
 		}
 
 		if ( configuredAudiences.length === 0 ) {
 			// If there are no configured audiences by this points, create the "new-visitors" and "returning-visitors" audiences,
 			// and add them to the configured audiences.
+			const [ newVisitorsResult, returningVisitorsResult ] =
+				yield Data.commonActions.await(
+					Promise.all( [
+						dispatch( MODULES_ANALYTICS_4 ).createAudience(
+							SITE_KIT_AUDIENCE_DEFINITIONS[ 'new-visitors' ]
+						),
+						dispatch( MODULES_ANALYTICS_4 ).createAudience(
+							SITE_KIT_AUDIENCE_DEFINITIONS[
+								'returning-visitors'
+							]
+						),
+					] )
+				);
+
+			// TODO: Full error handling will be implemented via https://github.com/google/site-kit-wp/issues/8134.
+			if ( ! newVisitorsResult.error ) {
+				configuredAudiences.push( newVisitorsResult.response.name );
+			}
+
+			if ( ! returningVisitorsResult.error ) {
+				configuredAudiences.push(
+					returningVisitorsResult.response.name
+				);
+			}
+
+			// Resync available audiences to ensure the newly created audiences are available.
+			yield Data.commonActions.await(
+				dispatch( MODULES_ANALYTICS_4 ).syncAvailableAudiences()
+			);
 		}
 
-		// TODO: Create custom dimension if it doesn't exist.
+		// Create custom dimension if it doesn't exist.
+		yield Data.commonActions.await(
+			__experimentalResolveSelect(
+				MODULES_ANALYTICS_4
+			).getAvailableCustomDimensions()
+		);
+
+		if (
+			! select( MODULES_ANALYTICS_4 ).hasCustomDimensions(
+				'googlesitekit_post_type'
+			)
+		) {
+			const propertyID = select( MODULES_ANALYTICS_4 ).getPropertyID();
+
+			const { error } = yield Data.commonActions.await(
+				dispatch( MODULES_ANALYTICS_4 ).fetchCreateCustomDimension(
+					propertyID,
+					CUSTOM_DIMENSION_DEFINITIONS.googlesitekit_post_type
+				)
+			);
+
+			if ( error ) {
+				// TODO: Full error handling will be implemented via https://github.com/google/site-kit-wp/issues/8134.
+			}
+		}
+
+		dispatch( MODULES_ANALYTICS_4 ).setConfiguredAudiences(
+			configuredAudiences
+		);
+
+		const { error } = yield Data.commonActions.await(
+			dispatch( MODULES_ANALYTICS_4 ).saveAudienceSettings()
+		);
+
+		if ( error ) {
+			// TODO: Full error handling will be implemented via https://github.com/google/site-kit-wp/issues/8134.
+		}
 	},
 };
 
