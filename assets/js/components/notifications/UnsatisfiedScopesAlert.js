8404148f70bd323578a342249e6d451eeb606ec0
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2024-05-10 11:50:08 +0100

    Fix @since version.

--- assets/js/components/notifications/UnsatisfiedScopesAlert.js
+++ assets/js/components/notifications/UnsatisfiedScopesAlert.js
@@ -55,7 +55,7 @@ const MESSAGE_GENERIC = 'generic';
 /**
  * Maps unsatisfied scopes to module names.
  *
- * @since 1.38.0
+ * @since 1.39.0
  *
  * @param {Array}  scopes  Array of unsatisfied scopes.
  * @param {Object} modules Object of all modules.
