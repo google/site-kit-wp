1592f9bbbc185a7c7fd534e00f682b01f950efc9
Ankit Gade <ankit.gade@10up.com>
2024-05-10 17:34:35 +0530

    Register the expirables instance in plugin.

--- includes/Core/Expirables/Expirables.php
+++ includes/Core/Expirables/Expirables.php
@@ -10,6 +10,11 @@
 
 namespace Google\Site_Kit\Core\Expirables;
 
+use Google\Site_Kit\Core\Expirables\Expirable_Items;
+use Google\Site_Kit\Core\Expirables\REST_Expirable_Items_Controller;
+use Google\Site_Kit\Context;
+use Google\Site_Kit\Core\Storage\User_Options;
+
 /**
  * Class for handling expirables.
  *
@@ -19,4 +24,53 @@ namespace Google\Site_Kit\Core\Expirables;
  */
 class Expirables {
 
+	/**
+	 * Expirable_Items instance.
+	 *
+	 * @since n.e.x.t
+	 * @var Expirable_Items
+	 */
+	protected $expirable_items;
+
+	/**
+	 * REST_Expirable_Items_Controller instance.
+	 *
+	 * @since n.e.x.t
+	 * @var REST_Expirable_Items_Controller
+	 */
+	protected $rest_controller;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param Context      $context Plugin context.
+	 * @param User_Options $user_options Optional. User option API. Default is a new instance.
+	 */
+	public function __construct( Context $context, User_Options $user_options = null ) {
+		$this->expirable_items = new Expirable_Items( $user_options ?: new User_Options( $context ) );
+		$this->rest_controller = new REST_Expirable_Items_Controller( $this->expirable_items );
+	}
+
+	/**
+	 * Gets the reference to the Dismissed_Items instance.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return Expirable_Items An instance of the Expirable_Items class.
+	 */
+	public function get_expirable_items() {
+		return $this->expirable_items;
+	}
+
+	/**
+	 * Registers functionality through WordPress hooks.
+	 *
+	 * @since n.e.x.t
+	 */
+	public function register() {
+		$this->expirable_items->register();
+		$this->rest_controller->register();
+	}
 }
