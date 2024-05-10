b6aab06bfa4a2109d4bd8954024167be10fa6b90
Ankit Gade <ankit.gade@10up.com>
2024-05-10 13:44:04 +0530

    Add the datastore for expirable items.

--- assets/js/googlesitekit/datastore/user/expirable-items.js
+++ assets/js/googlesitekit/datastore/user/expirable-items.js
@@ -15,3 +15,175 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * External dependencies
+ */
+import invariant from 'invariant';
+
+/**
+ * Internal dependencies
+ */
+import API from 'googlesitekit-api';
+import Data from 'googlesitekit-data';
+import { CORE_USER } from './constants';
+import { createFetchStore } from '../../data/create-fetch-store';
+import { createValidatedAction } from '../../data/utils';
+const { createRegistrySelector, commonActions } = Data;
+const { getRegistry } = commonActions;
+
+function reducerCallback( state, expirableItems ) {
+	return {
+		...state,
+		expirableItems: Array.isArray( expirableItems ) ? expirableItems : [],
+	};
+}
+
+const fetchGetExpirableItemsStore = createFetchStore( {
+	baseName: 'getExpirableItems',
+	controlCallback: () =>
+		API.get( 'core', 'user', 'expirable-items', {}, { useCache: false } ),
+	reducerCallback,
+} );
+
+const fetchSetExpirableItemTimersStore = createFetchStore( {
+	baseName: 'expirableItems',
+	controlCallback: ( { slug, expiresInSeconds } ) =>
+		API.set( 'core', 'user', 'set-expirable-item-timers', {
+			slug,
+			expiration: expiresInSeconds,
+		} ),
+	reducerCallback,
+	argsToParams: ( items ) => {
+		return items;
+	},
+	validateParams: ( items = [] ) => {
+		items.forEach( ( item ) => {
+			const { slug, expiresInSeconds = 0 } = item;
+			invariant( slug, 'slug is required.' );
+			invariant(
+				Number.isInteger( expiresInSeconds ),
+				'expiresInSeconds must be an integer.'
+			);
+		} );
+	},
+} );
+
+const baseInitialState = {
+	expirableItems: undefined,
+};
+
+const baseActions = {
+	setExpirableItemTimers: createValidatedAction(
+		( items = [] ) => {
+			items.forEach( ( item ) => {
+				const { slug, expiresInSeconds } = item;
+
+				invariant( slug, 'An item slug is required.' );
+				invariant(
+					Number.isInteger( expiresInSeconds ),
+					'expiresInSeconds must be an integer.'
+				);
+			} );
+		},
+		function* ( items ) {
+			return yield fetchSetExpirableItemTimersStore.actions.fetchExpirableItems(
+				items
+			);
+		}
+	),
+};
+
+const baseResolvers = {
+	*getExpirableItems() {
+		const { select } = yield getRegistry();
+		const expirableItems = select( CORE_USER ).getExpirableItems();
+		if ( expirableItems === undefined ) {
+			yield fetchGetExpirableItemsStore.actions.fetchGetExpirableItems();
+		}
+	},
+};
+
+const baseSelectors = {
+	/**
+	 * Returns the expirable items.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {Object} state Data store's state.
+	 * @return {Array|undefined} Items if exists, `undefined` if not resolved yet.
+	 */
+	getExpirableItems( state ) {
+		return state.expirableItems;
+	},
+
+	/**
+	 * Determines whether the item exists in expirable items.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {Object} state Data store's state.
+	 * @param {string} slug  Item slug.
+	 * @return {(boolean|undefined)} TRUE if exists, otherwise FALSE, `undefined` if not resolved yet.
+	 */
+	hasExpirableItem: createRegistrySelector( ( select ) => ( state, slug ) => {
+		return select( CORE_USER ).getExpirableItems()?.includes( slug );
+	} ),
+
+	/**
+	 * Determines whether the item is active and not expired.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {Object} state Data store's state.
+	 * @param {string} slug  Item slug.
+	 * @return {(boolean|undefined)} TRUE if exists, otherwise FALSE, `undefined` if not resolved yet.
+	 */
+	isExpirableItemActive: createRegistrySelector(
+		( select ) => ( state, slug ) => {
+			let active = false;
+			const itemExists = select( CORE_USER ).hasExpirableItem( slug );
+
+			if ( itemExists ) {
+				const expirableItems = select( CORE_USER ).getExpirableItems();
+				expirableItems.forEach( ( item ) => {
+					const { expiresInSeconds = 0 } = item;
+					const timeStamp = Math.floor( Date.now() / 1000 );
+
+					if ( expiresInSeconds < timeStamp ) {
+						active = true;
+					}
+				} );
+			}
+
+			return active;
+		}
+	),
+};
+
+export const {
+	actions,
+	controls,
+	initialState,
+	reducer,
+	resolvers,
+	selectors,
+} = Data.combineStores(
+	{
+		initialState: baseInitialState,
+		actions: baseActions,
+		resolvers: baseResolvers,
+		selectors: baseSelectors,
+	},
+	fetchGetExpirableItemsStore,
+	fetchSetExpirableItemTimersStore
+);
+
+export default {
+	actions,
+	controls,
+	initialState,
+	reducer,
+	resolvers,
+	selectors,
+};
