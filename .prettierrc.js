46890b028e5273b830e31bec9d2a8305c7109ed9
Sayan Mondal <154863207+sayanm085@users.noreply.github.com>
2024-05-15 17:15:32 +0530

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -20,6 +20,4 @@
 // set to automatically format files with Prettier have a config to detect.
 // Many users only run Prettier when a config is present, so this file makes
 // sure one can be detected, even though we aren't doing anything with it.
-module.exports = {
-	...require( '@wordpress/prettier-config' ),
-};
+module.exports = {...require( '@wordpress/prettier-config' ),};
