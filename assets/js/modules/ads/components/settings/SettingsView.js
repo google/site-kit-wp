7178c4a5c8f88e8580a96fecaf54fdb0d24e18a6
Simon Dowdles <simon.dowdles@get10up.com>
2024-05-22 12:52:11 +0200

    Integrate Enhanced conversion tracking settings into Ads read only view.

--- assets/js/modules/ads/components/settings/SettingsView.js
+++ assets/js/modules/ads/components/settings/SettingsView.js
@@ -27,6 +27,7 @@ import { Fragment } from '@wordpress/element';
  */
 import Data from 'googlesitekit-data';
 import { MODULES_ADS } from '../../datastore/constants';
+import { CORE_SITE } from '../../../../googlesitekit/datastore/site/constants';
 import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
 import DisplaySetting from '../../../../components/DisplaySetting';
 import AdBlockerWarning from '../common/AdBlockerWarning';
@@ -35,6 +36,7 @@ const { useSelect } = Data;
 
 export default function SettingsView() {
 	const paxEnabled = useFeature( 'adsPax' );
+	const iceEnabled = useFeature( 'conversionInfra' );
 
 	const conversionID = useSelect( ( select ) =>
 		select( MODULES_ADS ).getConversionID()
@@ -57,9 +59,37 @@ export default function SettingsView() {
 
 	const isPaxView = paxEnabled && ( paxConversionID || extCustomerID );
 
+	const isConversionTrackingEnabled = useSelect( ( select ) =>
+		select( CORE_SITE ).isConversionTrackingEnabled()
+	);
+
+	const conversionTrackingSettingValue = isConversionTrackingEnabled
+		? __( 'Enabled', 'google-site-kit' )
+		: __( 'Disabled', 'google-site-kit' );
+
 	return (
 		<Fragment>
 			<AdBlockerWarning />
+			{ iceEnabled && (
+				<div className="googlesitekit-settings-module__meta-item">
+					{
+						// This is required to add correct spacing if the <AdBlockerWarning> component is present.
+						isAdBlockerActive && <br />
+					}
+					<h5 className="googlesitekit-settings-module__meta-item-type">
+						{ __(
+							'Enhanced conversion tracking',
+							'google-site-kit'
+						) }
+					</h5>
+					<p className="googlesitekit-settings-module__meta-item-data">
+						<DisplaySetting
+							value={ conversionTrackingSettingValue }
+						/>
+					</p>
+				</div>
+			) }
+
 			{ ! isAdBlockerActive && (
 				<div className="googlesitekit-settings-module__meta-item">
 					<h5 className="googlesitekit-settings-module__meta-item-type">
