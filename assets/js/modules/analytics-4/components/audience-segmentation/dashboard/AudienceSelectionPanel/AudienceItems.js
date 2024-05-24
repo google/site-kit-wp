d46f2421f55f95d6e9fadb643f9a26ae15c00750
nfmohit <nahid.mohit@10up.com>
2024-05-25 03:21:35 +0600

    Update `AudienceItems` to compose subtitle and calculate user count.

--- assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceItems.js
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceItems.js
@@ -25,7 +25,11 @@ import { __ } from '@wordpress/i18n';
  * External dependencies
  */
 import Data from 'googlesitekit-data';
-import { MODULES_ANALYTICS_4 } from '../../../../datastore/constants';
+import { CORE_USER } from '../../../../../../googlesitekit/datastore/user/constants';
+import {
+	DATE_RANGE_OFFSET,
+	MODULES_ANALYTICS_4,
+} from '../../../../datastore/constants';
 import AudienceItem from './AudienceItem';
 import { SelectionPanelItems } from '../../../../../../components/SelectionPanel';
 
@@ -35,19 +39,93 @@ export default function AudienceItems() {
 	const savedItemSlugs = useSelect( ( select ) =>
 		select( MODULES_ANALYTICS_4 ).getConfiguredAudiences()
 	);
-	const availableAudiences = useSelect( ( select ) =>
-		select( MODULES_ANALYTICS_4 ).getAvailableAudiences()
-	);
+	const availableAudiences = useSelect( ( select ) => {
+		const { getResourceDataAvailabilityDate, getReport } =
+			select( MODULES_ANALYTICS_4 );
+
+		const audiences = select( MODULES_ANALYTICS_4 )
+			.getAvailableAudiences()
+			// Filter out "Purchasers" audience if it has no data.
+			?.filter( ( { audienceSlug, name } ) => {
+				if ( 'purchasers' !== audienceSlug ) {
+					return true;
+				}
+
+				return !! getResourceDataAvailabilityDate( name, 'audience' );
+			} );
+
+		if ( undefined === audiences ) {
+			return undefined;
+		}
+
+		if ( ! audiences.length ) {
+			return [];
+		}
+
+		const dateRangeDates = select( CORE_USER ).getDateRangeDates( {
+			offsetDays: DATE_RANGE_OFFSET,
+		} );
+
+		// Get the user count for the available audiences.
+		const report = getReport( {
+			...dateRangeDates,
+			metrics: [
+				{
+					name: 'totalUsers',
+				},
+			],
+			dimensions: [ { name: 'audienceResourceName' } ],
+			dimensionFilters: {
+				audienceResourceName: audiences.map( ( { name } ) => name ),
+			},
+		} );
+
+		const { rows = [] } = report || {};
+
+		return audiences?.map( ( audience ) => {
+			const rowIndex = rows?.findIndex(
+				( row ) => row?.dimensionValues?.[ 0 ]?.value === audience.name
+			);
+
+			return {
+				...audience,
+				userCount: rows?.[ rowIndex ]?.metricValues?.[ 0 ]?.value || 0,
+			};
+		} );
+	} );
 
 	const audiencesListReducer = (
 		acc,
-		{ name, displayName, description }
+		{ audienceType, description, displayName, name, userCount }
 	) => {
+		let citation = '';
+
+		switch ( audienceType ) {
+			case 'DEFAULT_AUDIENCE':
+				citation = __(
+					'Created by default by Google Analytics',
+					'google-site-kit'
+				);
+				description = '';
+				break;
+			case 'SITE_KIT_AUDIENCE':
+				citation = __( 'created by Site Kit', 'google-site-kit' );
+				break;
+			case 'USER_AUDIENCE':
+				citation = __(
+					'Already exists in your Analytics property',
+					'google-site-kit'
+				);
+				break;
+		}
+
 		return {
 			...acc,
 			[ name ]: {
 				title: displayName,
-				description,
+				subtitle: description,
+				description: citation,
+				userCount,
 			},
 		};
 	};
