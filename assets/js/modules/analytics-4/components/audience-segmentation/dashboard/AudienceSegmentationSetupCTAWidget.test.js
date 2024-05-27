e12414e94ce3c22006837b6e4eab809b4a1c6457
Ankit Gade <ankit.gade@10up.com>
2024-05-27 19:28:31 +0530

    Test for auto submission of form.

--- assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSegmentationSetupCTAWidget.test.js
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSegmentationSetupCTAWidget.test.js
@@ -27,12 +27,20 @@ import {
 } from '../../../../../../../tests/js/test-utils';
 
 import { getWidgetComponentProps } from '../../../../../googlesitekit/widgets/util';
-import { EDIT_SCOPE, MODULES_ANALYTICS_4 } from '../../../datastore/constants';
+import {
+	EDIT_SCOPE,
+	MODULES_ANALYTICS_4,
+	AUDIENCE_SEGMENTATION_SETUP_FORM,
+} from '../../../datastore/constants';
 import AudienceSegmentationSetupCTAWidget from './AudienceSegmentationSetupCTAWidget';
 import {
 	properties as propertiesFixture,
 	availableAudiences as audiencesFixture,
 } from '../../../datastore/__fixtures__';
+
+import { CORE_USER } from '../../../../../googlesitekit/datastore/user/constants';
+import { CORE_FORMS } from '../../../../../googlesitekit/datastore/forms/constants';
+import fetchMock from 'fetch-mock';
 
 describe( 'AudienceSegmentationSetupCTAWidget', () => {
 	let registry;
@@ -40,6 +48,28 @@ describe( 'AudienceSegmentationSetupCTAWidget', () => {
 	const { Widget } = getWidgetComponentProps(
 		'audienceSegmentationSetupCTA'
 	);
+
+	const syncAvailableAudiencesEndpoint = new RegExp(
+		'^/google-site-kit/v1/modules/analytics-4/data/sync-audiences'
+	);
+
+	const audienceSettingsEndpoint = new RegExp(
+		'^/google-site-kit/v1/modules/analytics-4/data/audience-settings'
+	);
+
+	const reportSettingsEndpoint = new RegExp(
+		'^/google-site-kit/v1/modules/analytics-4/data/report'
+	);
+
+	const referenceDate = '2024-05-01';
+
+	const reportOptions = {
+		options: {
+			endDate: referenceDate,
+			metrics: [ { name: 'totalUsers' } ],
+			startDate: '2024-01-31',
+		},
+	};
 
 	const testPropertyID = propertiesFixture[ 0 ]._id;
 
@@ -172,5 +202,101 @@ describe( 'AudienceSegmentationSetupCTAWidget', () => {
 				'Learn how different types of visitors interact with your site'
 			)
 		).toThrow();
+	} );
+
+	it( 'CTA text changes when button is clicked.', async () => {
+		const { dispatch } = registry;
+		const report = {
+			rows: [
+				{
+					metricValues: [
+						{
+							value: 2,
+						},
+					],
+				},
+				{
+					metricValues: [
+						{
+							value: 2,
+						},
+					],
+				},
+			],
+			totals: [
+				{
+					metricValues: [
+						{
+							value: 2,
+						},
+					],
+				},
+			],
+		};
+
+		fetchMock.get( audienceSettingsEndpoint, {
+			configuredAudiences: [],
+			isAudienceSegmentationWidgetHidden: false,
+		} );
+
+		fetchMock.get( reportSettingsEndpoint, {
+			status: 200,
+			body: report,
+		} );
+
+		fetchMock.post( syncAvailableAudiencesEndpoint, {
+			status: 200,
+			body: audiencesFixture,
+		} );
+
+		fetchMock.postOnce( audienceSettingsEndpoint, {
+			status: 200,
+			body: audiencesFixture,
+		} );
+
+		await dispatch( MODULES_ANALYTICS_4 ).receiveSyncAvailableAudiences(
+			audiencesFixture
+		);
+
+		await dispatch( CORE_USER ).setReferenceDate( referenceDate );
+
+		await dispatch( MODULES_ANALYTICS_4 ).receiveGetReport(
+			report,
+			reportOptions
+		);
+
+		await dispatch( MODULES_ANALYTICS_4 ).finishResolution( 'getReport', [
+			reportOptions.options,
+		] );
+
+		// Set the autoSubmit to true.
+		await dispatch( CORE_FORMS ).setValues(
+			AUDIENCE_SEGMENTATION_SETUP_FORM,
+			{ autoSubmit: true }
+		);
+
+		// Set the data available on page load to true.
+		registry
+			.dispatch( MODULES_ANALYTICS_4 )
+			.receiveIsDataAvailableOnLoad( true );
+
+		const { getByText, waitForRegistry } = render(
+			<AudienceSegmentationSetupCTAWidget Widget={ Widget } />,
+			{
+				registry,
+			}
+		);
+
+		// Wait for resolvers to finish to avoid an unhandled React state update.
+		await waitForRegistry();
+
+		expect(
+			getByText(
+				'Learn how different types of visitors interact with your site'
+			)
+		).toBeInTheDocument();
+
+		// Button text should be `Enabling groups` because we form is getting auto submitted.
+		expect( getByText( 'Enabling groups' ) ).toBeInTheDocument();
 	} );
 } );
