a9004ffe3fcdaf7eb6f4c24197a67f686ae47bb6
Arafat Zahan <arafat.zahan@10up.com>
2024-06-04 08:24:23 +0600

    Add story.

---
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/ConnectAnalyticsCTATileWidget.stories.js
@@ -0,0 +1,67 @@
+/**
+ * ConnectAnalyticsCTATileWidget Component Stories.
+ *
+ * Site Kit by Google, Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import WithRegistrySetup from '../../../../../../../tests/js/WithRegistrySetup';
+import {
+	provideModuleRegistrations,
+	provideModules,
+} from '../../../../../../../tests/js/utils';
+import { withWidgetComponentProps } from '../../../../../googlesitekit/widgets/util';
+import ConnectAnalyticsCTATileWidget from './ConnectAnalyticsCTATileWidget';
+
+const WidgetWithComponentProps = withWidgetComponentProps(
+	'audienceSegmentationConnectAnalytics'
+)( ConnectAnalyticsCTATileWidget );
+
+function Template() {
+	return <WidgetWithComponentProps />;
+}
+
+export const Default = Template.bind( {} );
+Default.storyName = 'ConnectAnalyticsCTATileWidget';
+Default.scenario = {
+	label: 'Modules/Analytics4/Components/AudienceSegmentation/Dashboard/AudienceTilesWidget/ConnectAnalyticsCTATileWidget',
+	delay: 250,
+};
+
+export default {
+	title: 'Modules/Analytics4/Components/AudienceSegmentation/Dashboard/ConnectAnalyticsCTATileWidget',
+	decorators: [
+		( Story ) => {
+			const setupRegistry = ( registry ) => {
+				provideModules( registry, [
+					{
+						slug: 'analytics-4',
+						active: false,
+						connected: false,
+					},
+				] );
+				provideModuleRegistrations( registry );
+			};
+
+			return (
+				<WithRegistrySetup func={ setupRegistry }>
+					<Story />
+				</WithRegistrySetup>
+			);
+		},
+	],
+};
