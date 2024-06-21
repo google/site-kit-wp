1484e1322c709a28b7528295b0f1c577933659bd
Eugene Manuilov <eugene.manuilov@get10up.com>
2024-06-21 15:56:31 +0200

    Update the comment for the getDismissedPrompts selector.

--- assets/js/googlesitekit/datastore/user/prompts.js
+++ assets/js/googlesitekit/datastore/user/prompts.js
@@ -128,7 +128,9 @@ const baseSelectors = {
 			return undefined;
 		}
 
-		// Don't use the getReferenceDate selector here since there is no need in it.
+		// We shouldn't use the getReferenceDate selector here because it returns date only
+		// while we need the current time as well to properly determine whether the prompt is
+		// expired or not.
 		const currentTimeInSeconds = Math.floor( Date.now() / 1000 ); // eslint-disable-line sitekit/no-direct-date
 		return Object.entries( state.dismissedPrompts ).reduce(
 			( acc, [ slug, { expires } ] ) => {
