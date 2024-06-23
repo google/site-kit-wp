bad5dc07b277ed0de4fba92cb94fd32f6c08b44e
Jimmy Madon <hello@jimmymadon.com>
2024-06-23 20:54:38 +0100

    Fix jest test for sorted audiences.

--- assets/js/modules/analytics-4/datastore/audiences.test.js
+++ assets/js/modules/analytics-4/datastore/audiences.test.js
@@ -40,6 +40,7 @@ import {
 	properties as propertiesFixture,
 	audiences as audiencesFixture,
 	availableAudiences as availableAudiencesFixture,
+	availableAudiencesSorted as availableAudiencesSortedFixture,
 } from './__fixtures__';
 import fetchMock from 'fetch-mock';
 
@@ -512,7 +513,7 @@ describe( 'modules/analytics-4 audiences', () => {
 					registry
 						.select( MODULES_ANALYTICS_4 )
 						.getAvailableAudiences()
-				).toEqual( availableAudiencesFixture );
+				).toEqual( availableAudiencesSortedFixture );
 			} );
 
 			it.each( [
