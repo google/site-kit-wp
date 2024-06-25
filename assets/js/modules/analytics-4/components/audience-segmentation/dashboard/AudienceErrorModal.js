64caec5b3df38f33c8792b97115a7908b8d571ac
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-06-25 21:33:31 +0530

    Add AudienceErrorModal component.

---
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceErrorModal.js
@@ -0,0 +1,127 @@
+/**
+ * Audience Segmentation AudienceErrorModal component.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { createInterpolateElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { useSelect } from 'googlesitekit-data';
+import ModalDialog from '../../../../../components/ModalDialog';
+import { isInsufficientPermissionsError } from '../../../../../util/errors';
+import { CORE_SITE } from '../../../../../googlesitekit/datastore/site/constants';
+import Link from '../../../../../components/Link';
+import { MODULES_ANALYTICS_4 } from '../../../datastore/constants';
+
+export default function AudienceErrorModal( {
+	hasOAuthError,
+	apiErrors,
+	onRetry = () => {},
+	inProgress,
+} ) {
+	const errors = Array.isArray( apiErrors ) ? apiErrors : [ apiErrors ];
+	const hasInsufficientPermissionsError = errors.some( ( error ) =>
+		isInsufficientPermissionsError( error )
+	);
+
+	const helpLink = useSelect( ( select ) =>
+		select( CORE_SITE ).getErrorTroubleshootingLinkURL( {
+			code: 'analytics-4_insufficient_permissions',
+		} )
+	);
+
+	const requestAccessURL = useSelect( ( select ) =>
+		select( MODULES_ANALYTICS_4 ).getServiceEntityAccessURL()
+	);
+
+	let title, description, confirmButton, buttonLink;
+
+	if ( hasOAuthError ) {
+		title = __( 'Analytics update failed', 'google-site-kit' );
+		description = createInterpolateElement(
+			__(
+				'Setup was interrupted because you did not grant the necessary permissions. <HelpLink />',
+				'google-site-kit'
+			),
+			{
+				HelpLink: (
+					<Link href={ helpLink } external hideExternalIndicator>
+						{ __( 'Get help', 'google-site-kit' ) }
+					</Link>
+				),
+			}
+		);
+		confirmButton = __( 'Retry', 'google-site-kit' );
+	} else if ( hasInsufficientPermissionsError ) {
+		title = __( 'Insufficient permissions', 'google-site-kit' );
+		description = createInterpolateElement(
+			__(
+				'You’ll need to contact your administrator. Trouble getting access? <HelpLink />',
+				'google-site-kit'
+			),
+			{
+				HelpLink: (
+					<Link href={ helpLink } external hideExternalIndicator>
+						{ __( 'Get help', 'google-site-kit' ) }
+					</Link>
+				),
+			}
+		);
+		confirmButton = __( 'Request access', 'google-site-kit' );
+		buttonLink = requestAccessURL;
+	} else {
+		title = __( 'Failed to set up visitor groups', 'google-site-kit' );
+		description = __(
+			'Oops! Something went wrong. Retry enabling groups.',
+			'google-site-kit'
+		);
+		confirmButton = __( 'Retry', 'google-site-kit' );
+	}
+
+	return (
+		<ModalDialog
+			dialogActive
+			buttonLink={ buttonLink }
+			title={ title }
+			subtitle={ description }
+			handleConfirm={ onRetry }
+			confirmButton={ confirmButton }
+			danger
+			inProgress={ inProgress }
+		/>
+	);
+}
+
+AudienceErrorModal.propTypes = {
+	hasOAuthError: PropTypes.bool,
+	apiErrors: PropTypes.oneOfType( [
+		PropTypes.arrayOf( PropTypes.object ),
+		PropTypes.object,
+	] ),
+	onRetry: PropTypes.func,
+	inProgress: PropTypes.bool,
+};
