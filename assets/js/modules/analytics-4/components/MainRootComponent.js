c0cb8b7c2a8d681bc17b3e1cb30d0320c9656cea
Aleksej <aleksej.vukomanovic@10up.com>
2024-06-28 10:07:35 +0200

    Revert commented out part.

--- assets/js/modules/analytics-4/components/MainRootComponent.js
+++ assets/js/modules/analytics-4/components/MainRootComponent.js
@@ -19,81 +19,81 @@
 /**
  * WordPress dependencies
  */
-// import { useCallback, useEffect } from '@wordpress/element';
+import { useCallback, useEffect } from '@wordpress/element';
 import { useMount } from 'react-use';
 
 /**
  * Internal dependencies
  */
-import { useDispatch } from 'googlesitekit-data';
-// import { CORE_FORMS } from '../../../googlesitekit/datastore/forms/constants';
-// import { CORE_SITE } from '../../../googlesitekit/datastore/site/constants';
-// import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
-// import { CORE_MODULES } from '../../../googlesitekit/modules/datastore/constants';
+import { useSelect, useDispatch } from 'googlesitekit-data';
+import { CORE_FORMS } from '../../../googlesitekit/datastore/forms/constants';
+import { CORE_SITE } from '../../../googlesitekit/datastore/site/constants';
+import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
+import { CORE_MODULES } from '../../../googlesitekit/modules/datastore/constants';
 import {
-	// EDIT_SCOPE,
-	// FORM_CUSTOM_DIMENSIONS_CREATE,
+	EDIT_SCOPE,
+	FORM_CUSTOM_DIMENSIONS_CREATE,
 	MODULES_ANALYTICS_4,
 } from '../datastore/constants';
 
 export default function MainRootComponent() {
 	const ga4Actions = useDispatch( MODULES_ANALYTICS_4 );
 
-	// const isKeyMetricsSetupCompleted = useSelect( ( select ) =>
-	// 	select( CORE_SITE ).isKeyMetricsSetupCompleted()
-	// );
+	const isKeyMetricsSetupCompleted = useSelect( ( select ) =>
+		select( CORE_SITE ).isKeyMetricsSetupCompleted()
+	);
 
-	// const isGA4Connected = useSelect( ( select ) =>
-	// 	select( CORE_MODULES ).isModuleConnected( 'analytics-4' )
-	// );
+	const isGA4Connected = useSelect( ( select ) =>
+		select( CORE_MODULES ).isModuleConnected( 'analytics-4' )
+	);
 
-	// const hasAnalyticsEditScope = useSelect( ( select ) =>
-	// 	select( CORE_USER ).hasScope( EDIT_SCOPE )
-	// );
+	const hasAnalyticsEditScope = useSelect( ( select ) =>
+		select( CORE_USER ).hasScope( EDIT_SCOPE )
+	);
 
-	// const autoSubmit = useSelect( ( select ) =>
-	// 	select( CORE_FORMS ).getValue(
-	// 		FORM_CUSTOM_DIMENSIONS_CREATE,
-	// 		'autoSubmit'
-	// 	)
-	// );
-	// const { setValues } = useDispatch( CORE_FORMS );
+	const autoSubmit = useSelect( ( select ) =>
+		select( CORE_FORMS ).getValue(
+			FORM_CUSTOM_DIMENSIONS_CREATE,
+			'autoSubmit'
+		)
+	);
+	const { setValues } = useDispatch( CORE_FORMS );
 
-	// const { createCustomDimensions } = useDispatch( MODULES_ANALYTICS_4 );
+	const { createCustomDimensions } = useDispatch( MODULES_ANALYTICS_4 );
 
-	// const createDimensionsAndUpdateForm = useCallback( async () => {
-	// 	await createCustomDimensions();
-	// 	setValues( FORM_CUSTOM_DIMENSIONS_CREATE, {
-	// 		isAutoCreatingCustomDimensions: false,
-	// 	} );
-	// }, [ createCustomDimensions, setValues ] );
+	const createDimensionsAndUpdateForm = useCallback( async () => {
+		await createCustomDimensions();
+		setValues( FORM_CUSTOM_DIMENSIONS_CREATE, {
+			isAutoCreatingCustomDimensions: false,
+		} );
+	}, [ createCustomDimensions, setValues ] );
 
 	useMount( () => {
 		ga4Actions?.syncGoogleTagSettings();
 	} );
 
-	// useEffect( () => {
-	// 	if (
-	// 		isKeyMetricsSetupCompleted &&
-	// 		isGA4Connected &&
-	// 		hasAnalyticsEditScope &&
-	// 		autoSubmit
-	// 	) {
-	// 		setValues( FORM_CUSTOM_DIMENSIONS_CREATE, {
-	// 			autoSubmit: false,
-	// 			isAutoCreatingCustomDimensions: true,
-	// 		} );
-	// 		createDimensionsAndUpdateForm();
-	// 	}
-	// }, [
-	// 	autoSubmit,
-	// 	createCustomDimensions,
-	// 	hasAnalyticsEditScope,
-	// 	isKeyMetricsSetupCompleted,
-	// 	isGA4Connected,
-	// 	setValues,
-	// 	createDimensionsAndUpdateForm,
-	// ] );
+	useEffect( () => {
+		if (
+			isKeyMetricsSetupCompleted &&
+			isGA4Connected &&
+			hasAnalyticsEditScope &&
+			autoSubmit
+		) {
+			setValues( FORM_CUSTOM_DIMENSIONS_CREATE, {
+				autoSubmit: false,
+				isAutoCreatingCustomDimensions: true,
+			} );
+			createDimensionsAndUpdateForm();
+		}
+	}, [
+		autoSubmit,
+		createCustomDimensions,
+		hasAnalyticsEditScope,
+		isKeyMetricsSetupCompleted,
+		isGA4Connected,
+		setValues,
+		createDimensionsAndUpdateForm,
+	] );
 
 	return null;
 }
