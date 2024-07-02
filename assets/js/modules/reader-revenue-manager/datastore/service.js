c7fcd458f2fa9481e351f371e85ed06271082ede
Ankit Gade <ankit.gade@10up.com>
2024-07-02 20:23:37 +0530

    Add getServiceURL selector.

---
+++ assets/js/modules/reader-revenue-manager/datastore/service.js
@@ -0,0 +1,86 @@
+/**
+ * `modules/reader-revenue-manager` data store.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import invariant from 'invariant';
+
+/**
+ * WordPress dependencies
+ */
+import { addQueryArgs } from '@wordpress/url';
+
+/**
+ * Internal dependencies
+ */
+import { createRegistrySelector } from 'googlesitekit-data';
+import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
+
+/**
+ * Link to RRM platform.
+ */
+const publicationCenterURL = 'https://publishercenter.google.com';
+
+const selectors = {
+	/**
+	 * Returns a link to the Reader Revenue Manager platform.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {string} publicationID Publication ID.
+	 * @return {string} Link to Reader Revenue Management platform.
+	 */
+	getServiceURL: createRegistrySelector(
+		( select ) =>
+			( state, { publicationID } ) => {
+				// If there is no publication ID, return link to platform.
+				if ( ! publicationID ) {
+					return publicationCenterURL;
+				}
+
+				invariant(
+					'string' === typeof publicationID,
+					'Publication ID must be string.'
+				);
+
+				const linkRRM = addQueryArgs(
+					publicationCenterURL + '/reader-revenue-manager',
+					{
+						publication: publicationID,
+						utm_source: 'sitekit',
+					}
+				);
+
+				const accountChooserBaseURI =
+					select( CORE_USER ).getAccountChooserURL( linkRRM );
+
+				if ( accountChooserBaseURI === undefined ) {
+					return undefined;
+				}
+
+				return accountChooserBaseURI;
+			}
+	),
+};
+
+const store = {
+	selectors,
+};
+
+export default store;
---
+++ assets/js/modules/reader-revenue-manager/datastore/service.test.js
@@ -0,0 +1,94 @@
+/**
+ * `modules/reader-revenue-manager` data store: service tests.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *
+ * Internal dependencies
+ */
+import {
+	createTestRegistry,
+	unsubscribeFromAll,
+} from '../../../../../tests/js/utils';
+import { MODULES_READER_REVENUE_MANAGER } from './constants';
+import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
+import { decodeServiceURL } from '../../../../../tests/js/mock-accountChooserURL-utils';
+
+describe( 'module/reader-revenue-manager service store', () => {
+	const userData = {
+		id: 1,
+		email: 'admin@example.com',
+		name: 'admin',
+		picture: 'https://path/to/image',
+	};
+
+	let registry;
+
+	beforeAll( () => {
+		registry = createTestRegistry();
+	} );
+
+	afterAll( () => {
+		unsubscribeFromAll( registry );
+	} );
+
+	describe( 'selectors', () => {
+		describe( 'getServiceURL', () => {
+			beforeEach( () => {
+				registry.dispatch( CORE_USER ).receiveUserInfo( userData );
+			} );
+
+			it( 'should retrieve the correct URL with no arguments', () => {
+				const publicationID = null;
+				const serviceURL = registry
+					.select( MODULES_READER_REVENUE_MANAGER )
+					.getServiceURL( { publicationID } );
+
+				expect( new URL( serviceURL ) ).toMatchObject( {
+					origin: 'https://publishercenter.google.com',
+				} );
+			} );
+
+			it( 'should append `reader-revenue-manager` path to the URL', () => {
+				const publicationID = 'ABCDEFG';
+				const serviceURL = registry
+					.select( MODULES_READER_REVENUE_MANAGER )
+					.getServiceURL( { publicationID } );
+
+				expect(
+					new URL( decodeServiceURL( serviceURL ) ).pathname
+				).toMatch( '/reader-revenue-manager' );
+			} );
+
+			it( 'should append correct query parameters to the URL', () => {
+				const publicationID = 'ABCDEFG';
+				const serviceURL = registry
+					.select( MODULES_READER_REVENUE_MANAGER )
+					.getServiceURL( { publicationID } );
+
+				const query = {
+					publication: publicationID,
+					utm_source: 'sitekit',
+				};
+
+				expect( decodeServiceURL( serviceURL ) ).toMatchQueryParameters(
+					query
+				);
+			} );
+		} );
+	} );
+} );
