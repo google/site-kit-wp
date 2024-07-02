d02ed76181248bd1cb5f743252352d7f87439777
Ankit Gade <ankit.gade@10up.com>
2024-07-02 11:35:08 +0530

    Add getPublications selector.

---
+++ assets/js/modules/reader-revenue-manager/datastore/publications.js
@@ -0,0 +1,98 @@
+/**
+ * `modules/reader-revenue-manager` data store.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import API from 'googlesitekit-api';
+import { commonActions, combineStores } from 'googlesitekit-data';
+import { createFetchStore } from '../../../googlesitekit/data/create-fetch-store';
+import { MODULES_READER_REVENUE_MANAGER } from './constants';
+
+const getPublicationStore = createFetchStore( {
+	baseName: 'getPublications',
+	controlCallback: () =>
+		API.get(
+			'modules',
+			'reader-revenue-manager',
+			'publications',
+			{},
+			{ useCache: true }
+		),
+	reducerCallback: ( state, publications ) => ( { ...state, publications } ),
+} );
+
+const baseInitialState = {
+	publications: undefined,
+};
+
+const baseActions = {};
+
+const baseControls = {};
+
+const baseReducer = ( state, { type } ) => {
+	switch ( type ) {
+		default:
+			return state;
+	}
+};
+
+const baseResolvers = {
+	*getPublications() {
+		const registry = yield commonActions.getRegistry();
+		// Only fetch publications if there are none in the store for the given account.
+		const publications = registry
+			.select( MODULES_READER_REVENUE_MANAGER )
+			.getPublications();
+		if ( publications === undefined ) {
+			yield getPublicationStore.actions.fetchGetPublications();
+		}
+	},
+};
+
+const baseSelectors = {
+	/**
+	 * Gets list of publications associated with the account.
+	 *
+	 * @since 1.31.0
+	 *
+	 * @param {Object} state Data store's state.
+	 * @return {(Array.<Object>|undefined)} An array publications; `undefined` if not loaded.
+	 */
+	getPublications( state ) {
+		return state.publications;
+	},
+};
+
+const store = combineStores( getPublicationStore, {
+	initialState: baseInitialState,
+	actions: baseActions,
+	controls: baseControls,
+	reducer: baseReducer,
+	resolvers: baseResolvers,
+	selectors: baseSelectors,
+} );
+
+export const initialState = store.initialState;
+export const actions = store.actions;
+export const controls = store.controls;
+export const reducer = store.reducer;
+export const resolvers = store.resolvers;
+export const selectors = store.selectors;
+
+export default store;
