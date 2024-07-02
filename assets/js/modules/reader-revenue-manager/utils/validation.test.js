c556ad5052dcae60196b9bc709888df6dcf8555d
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-07-02 12:48:54 +0530

    Add Validation function tests.

---
+++ assets/js/modules/reader-revenue-manager/utils/validation.test.js
@@ -0,0 +1,57 @@
+/**
+ * Validation function tests.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import { isValidPublicationID, isURLUsingHTTPS } from './validation';
+
+describe( 'utility functions', () => {
+	describe( 'isValidPublicationID', () => {
+		it( 'should return TRUE when a valid publication ID is passed', () => {
+			expect( isValidPublicationID( 'valid-publication_123.ID' ) ).toBe(
+				true
+			);
+		} );
+
+		it.each( [
+			[ 'false', false ],
+			[ 'an integer', 12345 ],
+			[ 'an empty string', '' ],
+			[ 'a string with invalid characters', 'invalid-publication!ID' ],
+		] )( 'should return FALSE when %s is passed', ( _, publicationID ) => {
+			expect( isValidPublicationID( publicationID ) ).toBe( false );
+		} );
+	} );
+
+	describe( 'isURLUsingHTTPS', () => {
+		it( 'should return TRUE when a URL with HTTPS is passed', () => {
+			expect( isURLUsingHTTPS( 'https://example.com' ) ).toBe( true );
+		} );
+
+		it.each( [
+			[ 'an HTTP URL', 'http://example.com' ],
+			[ 'an invalid URL', 'htp://example.com' ],
+			[ 'a string without protocol', 'example.com' ],
+			[ 'an empty string', '' ],
+			[ 'false', false ],
+		] )( 'should return FALSE when %s is passed', ( _, url ) => {
+			expect( isURLUsingHTTPS( url ) ).toBe( false );
+		} );
+	} );
+} );
