2fffd866a2c06cdf1d9a2e9a4248b0a22bd20958
Aleksej <aleksej.vukomanovic@10up.com>
2024-07-03 13:14:54 +0200

    Update AudienceItems component.

--- assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceItems.js
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceItems.js
@@ -25,11 +25,12 @@ import PropTypes from 'prop-types';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useState, useEffect } from '@wordpress/element';
 
 /**
- * External dependencies
+ * Internal dependencies
  */
-import { useSelect } from 'googlesitekit-data';
+import { useSelect, useDispatch } from 'googlesitekit-data';
 import { CORE_USER } from '../../../../../../googlesitekit/datastore/user/constants';
 import {
 	DATE_RANGE_OFFSET,
@@ -37,8 +38,31 @@ import {
 } from '../../../../datastore/constants';
 import AudienceItem from './AudienceItem';
 import { SelectionPanelItems } from '../../../../../../components/SelectionPanel';
+import { CORE_UI } from '../../../../../../googlesitekit/datastore/ui/constants';
+import { AUDIENCE_SELECTION_PANEL_OPENED_KEY } from './constants';
+import AudienceItemPreviewBlock from './AudienceItemPreviewBlock';
 
 export default function AudienceItems( { savedItemSlugs = [] } ) {
+	const [ firstView, setFirstView ] = useState( true );
+	const { syncAvailableAudiences } = useDispatch( MODULES_ANALYTICS_4 );
+
+	const isOpen = useSelect( ( select ) =>
+		select( CORE_UI ).getValue( AUDIENCE_SELECTION_PANEL_OPENED_KEY )
+	);
+
+	const isLoading = useSelect( ( select ) =>
+		select( MODULES_ANALYTICS_4 ).isFetchingSyncAvailableAudiences()
+	);
+
+	useEffect( () => {
+		if ( ! firstView || ! isOpen ) {
+			return;
+		}
+
+		syncAvailableAudiences();
+		setFirstView( false );
+	}, [ firstView, isOpen, syncAvailableAudiences ] );
+
 	const availableAudiences = useSelect( ( select ) => {
 		const {
 			getConfigurableAudiences,
@@ -196,7 +220,9 @@ export default function AudienceItems( { savedItemSlugs = [] } ) {
 			availableItemsTitle={ __( 'Additional groups', 'google-site-kit' ) }
 			availableSavedItems={ availableSavedItems }
 			availableUnsavedItems={ availableUnsavedItems }
-			ItemComponent={ AudienceItem }
+			ItemComponent={
+				isLoading ? AudienceItemPreviewBlock : AudienceItem
+			}
 			savedItemSlugs={ savedItemSlugs }
 		/>
 	);
