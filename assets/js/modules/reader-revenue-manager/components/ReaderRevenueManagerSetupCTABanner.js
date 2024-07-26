2357febb9f38ee703fbf52a4c0320060adef737a
Ankit Gade <ankit.gade@10up.com>
2024-07-26 20:54:16 +0530

    Add Setup CTA Banner.

---
+++ assets/js/modules/reader-revenue-manager/components/ReaderRevenueManagerSetupCTABanner.js
@@ -0,0 +1,179 @@
+/**
+ * Reader Revenue Manager Setup Banner component.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { compose } from '@wordpress/compose';
+import { useCallback } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { useDispatch, useSelect } from 'googlesitekit-data';
+import { Button } from 'googlesitekit-components';
+import {
+	BREAKPOINT_SMALL,
+	BREAKPOINT_TABLET,
+	useBreakpoint,
+} from '../../../hooks/useBreakpoint';
+import useActivateModuleCallback from '../../../hooks/useActivateModuleCallback';
+import whenInactive from '../../../util/when-inactive';
+import { withWidgetComponentProps } from '../../../googlesitekit/widgets/util';
+import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
+import {
+	READER_REVENUE_MANAGER_MODULE_SLUG,
+	READER_REVENUE_MANAGER_SETUP_BANNER_DISMISSED_KEY,
+} from '../datastore/constants';
+import { Cell, Grid, Row } from '../../../material-components';
+import SetupSVG from '../../../../svg/graphics/reader-revenue-manager-setup.svg';
+import SetupTabletSVG from '../../../../svg/graphics/reader-revenue-manager-setup-tablet.svg';
+import SetupMobileSVG from '../../../../svg/graphics/reader-revenue-manager-setup-mobile.svg';
+
+function ReaderRevenueManagerSetupCTABanner( { Widget, WidgetNull } ) {
+	const breakpoint = useBreakpoint();
+	const isMobileBreakpoint = breakpoint === BREAKPOINT_SMALL;
+	const isTabletBreakpoint = breakpoint === BREAKPOINT_TABLET;
+
+	const onSetupCallback = useActivateModuleCallback(
+		READER_REVENUE_MANAGER_MODULE_SLUG
+	);
+
+	const isDismissed = useSelect( ( select ) =>
+		select( CORE_USER ).isItemDismissed(
+			READER_REVENUE_MANAGER_SETUP_BANNER_DISMISSED_KEY
+		)
+	);
+
+	const { dismissItem } = useDispatch( CORE_USER );
+
+	const onDismiss = useCallback( async () => {
+		// For the second dismissal, dismiss permanently.
+		await dismissItem( READER_REVENUE_MANAGER_SETUP_BANNER_DISMISSED_KEY );
+	}, [ dismissItem ] );
+
+	if ( isDismissed || isDismissed === undefined ) {
+		return <WidgetNull />;
+	}
+
+	return (
+		<div className="googlesitekit-widget-context">
+			<Grid className="googlesitekit-widget-area">
+				<Row>
+					<Cell size={ 12 }>
+						<Widget
+							noPadding
+							className="googlesitekit-setup-cta-banner googlesitekit-reader-revenue-manager-setup-cta-widget"
+						>
+							<Grid collapsed>
+								<Row>
+									<Cell
+										smSize={ 12 }
+										mdSize={ 8 }
+										lgSize={ 6 }
+										className="googlesitekit-setup-cta-banner__primary-cell"
+									>
+										<h4 className="googlesitekit-setup-cta-banner__title">
+											{ __(
+												'Grow your revenue and deepen reader engagement',
+												'google-site-kit'
+											) }
+										</h4>
+										<p className="googlesitekit-setup-cta-banner__description">
+											{ __(
+												'Turn casual visitors into loyal readers and earn more from your content with subscriptions, contributions, ad optimization and reader insight tools.',
+												'google-site-kit'
+											) }
+										</p>
+
+										<div className="googlesitekit-setup-cta-banner__actions-wrapper">
+											<Button
+												className="googlesitekit-key-metrics-cta-button"
+												onClick={ onSetupCallback }
+											>
+												{ __(
+													'Set up Reader Revenue Manager',
+													'google-site-kit'
+												) }
+											</Button>
+											<Button
+												tertiary
+												onClick={ onDismiss }
+											>
+												{ __(
+													'Maybe later',
+													'google-site-kit'
+												) }
+											</Button>
+										</div>
+									</Cell>
+									{ ! isMobileBreakpoint &&
+										! isTabletBreakpoint && (
+											<Cell
+												alignBottom
+												className="googlesitekit-setup-cta-banner__svg-wrapper"
+												mdSize={ 8 }
+												lgSize={ 6 }
+											>
+												<SetupSVG />
+											</Cell>
+										) }
+									{ isTabletBreakpoint && (
+										<Cell
+											className="googlesitekit-setup-cta-banner__svg-wrapper"
+											mdSize={ 8 }
+										>
+											<SetupTabletSVG />
+										</Cell>
+									) }
+									{ isMobileBreakpoint && (
+										<Cell
+											alignBottom
+											className="googlesitekit-setup-cta-banner__svg-wrapper"
+											smSize={ 12 }
+										>
+											<SetupMobileSVG />
+										</Cell>
+									) }
+								</Row>
+							</Grid>
+						</Widget>
+					</Cell>
+				</Row>
+			</Grid>
+		</div>
+	);
+}
+
+ReaderRevenueManagerSetupCTABanner.propTypes = {
+	Widget: PropTypes.elementType.isRequired,
+	WidgetNull: PropTypes.elementType,
+};
+
+export default compose(
+	whenInactive( {
+		moduleName: READER_REVENUE_MANAGER_MODULE_SLUG,
+	} ),
+	withWidgetComponentProps( 'readerRevenueManagerSetupCTABanner' )
+)( ReaderRevenueManagerSetupCTABanner );
---
+++ assets/js/modules/reader-revenue-manager/components/ReaderRevenueManagerSetupCTABanner.test.js
@@ -0,0 +1,162 @@
+/**
+ * RReaderRevenueManagerSetupCTABanner tests.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import fetchMock from 'fetch-mock';
+
+/**
+ * Internal dependencies
+ */
+import ReaderRevenueManagerSetupCTABanner from './ReaderRevenueManagerSetupCTABanner';
+import {
+	render,
+	createTestRegistry,
+	fireEvent,
+	provideModules,
+	waitFor,
+} from '../../../../../tests/js/test-utils';
+import { getWidgetComponentProps } from '../../../googlesitekit/widgets/util';
+import { CORE_USER } from '../../../googlesitekit/datastore/user/constants';
+import {
+	READER_REVENUE_MANAGER_MODULE_SLUG,
+	READER_REVENUE_MANAGER_SETUP_BANNER_DISMISSED_KEY,
+} from '../datastore/constants';
+import useActivateModuleCallback from '../../../hooks/useActivateModuleCallback';
+
+jest.mock( '../../../hooks/useActivateModuleCallback' );
+
+describe( 'ReaderRevenueManagerSetupCTABanner', () => {
+	let registry;
+	let activateModuleMock;
+
+	const { Widget, WidgetNull } = getWidgetComponentProps(
+		'audienceSegmentationSetupCTA'
+	);
+
+	beforeEach( () => {
+		registry = createTestRegistry();
+		activateModuleMock = jest.fn();
+
+		provideModules( registry, [
+			{
+				slug: READER_REVENUE_MANAGER_MODULE_SLUG,
+				active: false,
+			},
+		] );
+
+		useActivateModuleCallback.mockImplementation( activateModuleMock );
+	} );
+
+	it( 'should render the banner when not dismissed', async () => {
+		registry.dispatch( CORE_USER ).receiveGetDismissedItems( [] );
+
+		const { getByText, waitForRegistry } = render(
+			<ReaderRevenueManagerSetupCTABanner
+				Widget={ Widget }
+				WidgetNull={ WidgetNull }
+			/>,
+			{
+				registry,
+			}
+		);
+
+		await waitForRegistry();
+
+		expect(
+			getByText( /Grow your revenue and deepen reader engagement/ )
+		).toBeInTheDocument();
+	} );
+
+	it( 'should not render the banner when dismissed', async () => {
+		registry
+			.dispatch( CORE_USER )
+			.receiveGetDismissedItems( [
+				READER_REVENUE_MANAGER_SETUP_BANNER_DISMISSED_KEY,
+			] );
+
+		const { container, waitForRegistry } = render(
+			<ReaderRevenueManagerSetupCTABanner
+				Widget={ Widget }
+				WidgetNull={ WidgetNull }
+			/>,
+			{
+				registry,
+			}
+		);
+
+		await waitForRegistry();
+
+		expect( container.firstChild ).toBeNull();
+	} );
+
+	it( 'should call "useActivateModuleCallback" hook when CTA is clicked', async () => {
+		registry.dispatch( CORE_USER ).receiveGetDismissedItems( [] );
+
+		const { getByRole, waitForRegistry } = render(
+			<ReaderRevenueManagerSetupCTABanner
+				Widget={ Widget }
+				WidgetNull={ WidgetNull }
+			/>,
+			{
+				registry,
+			}
+		);
+
+		await waitForRegistry();
+
+		fireEvent.click(
+			getByRole( 'button', { name: /Set up Reader Revenue Manager/i } )
+		);
+
+		expect( activateModuleMock ).toHaveBeenCalledTimes( 1 );
+	} );
+
+	it( 'should call the dismiss item endpoint when banner is dismissed', async () => {
+		fetchMock.postOnce(
+			RegExp( '^/google-site-kit/v1/core/user/data/dismiss-item' ),
+			{
+				body: JSON.stringify( [
+					READER_REVENUE_MANAGER_SETUP_BANNER_DISMISSED_KEY,
+				] ),
+				status: 200,
+			}
+		);
+
+		registry.dispatch( CORE_USER ).receiveGetDismissedItems( [] );
+
+		const { getByRole, waitForRegistry } = render(
+			<ReaderRevenueManagerSetupCTABanner
+				Widget={ Widget }
+				WidgetNull={ WidgetNull }
+			/>,
+			{
+				registry,
+			}
+		);
+
+		await waitForRegistry();
+
+		fireEvent.click( getByRole( 'button', { name: /Maybe later/i } ) );
+
+		await waitFor( () => {
+			expect( fetchMock ).toHaveFetchedTimes( 1 );
+		} );
+	} );
+} );
