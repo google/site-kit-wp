0f299ed400acef768578f112ca04a65439ff0883
Aleksej <aleksej.vukomanovic@10up.com>
2024-08-05 09:29:17 +0200

    Update syncer to include fallback call to the CRON action.

--- includes/Core/Remote_Features/Remote_Features_Syncer.php
+++ includes/Core/Remote_Features/Remote_Features_Syncer.php
@@ -30,6 +30,13 @@ class Remote_Features_Syncer {
 	private $remote_features;
 
 	/**
+	 * Remote_Features_Last_Sync instance.
+	 *
+	 * @var Remote_Features_Last_Sync
+	 */
+	private $remote_features_last_sync;
+
+	/**
 	 * Function which fetches features.
 	 *
 	 * @var Closure
@@ -48,18 +55,21 @@ class Remote_Features_Syncer {
 	 *
 	 * @since n.e.x.t
 	 *
-	 * @param Remote_Features $remote_features Remote_Features instance.
-	 * @param Closure         $fetch_features  Function which fetches features.
-	 * @param Guard_Interface ...$guards       Guard instances.
+	 * @param Remote_Features           $remote_features           Remote_Features instance.
+	 * @param Remote_Features_Last_Sync $remote_features_last_sync Remote_Features_Last_Sync instance.
+	 * @param Closure                   $fetch_features            Function which fetches features.
+	 * @param Guard_Interface           ...$guards                 Guard instances.
 	 */
 	public function __construct(
 		Remote_Features $remote_features,
+		Remote_Features_Last_Sync $remote_features_last_sync,
 		Closure $fetch_features,
 		Guard_Interface ...$guards
 	) {
-		$this->remote_features = $remote_features;
-		$this->fetch_features  = $fetch_features;
-		$this->guards          = $guards;
+		$this->remote_features           = $remote_features;
+		$this->remote_features_last_sync = $remote_features_last_sync;
+		$this->fetch_features            = $fetch_features;
+		$this->guards                    = $guards;
 	}
 
 	/**
@@ -78,6 +88,20 @@ class Remote_Features_Syncer {
 
 		if ( ! is_wp_error( $features ) && is_array( $features ) ) {
 			$this->remote_features->set( $features );
+			$this->remote_features_last_sync->set( time() );
 		}
+	}
+
+	/**
+	 * Calls the CRON action.
+	 *
+	 * @since n.e.x.t
+	 */
+	public function pull_remote_features_fallback() {
+		check_ajax_referer( 'remote_features_fallback' );
+
+		do_action( Remote_Features_Cron::CRON_ACTION );
+
+		wp_send_json_success();
 	}
 }
