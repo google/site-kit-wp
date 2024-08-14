d51343c58b87ca300389ac31181d944372a435b2
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2024-08-14 13:57:46 +0100

    Add stories for the PlaceholderTile component.

---
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceTilesWidget/PlaceholderTile.stories.js
@@ -0,0 +1,64 @@
+/**
+ * PlaceholderTile Component Stories.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import PlaceholderTile from './PlaceholderTile';
+import { MODULES_ANALYTICS_4 } from '../../../../datastore/constants';
+import WithRegistrySetup from '../../../../../../../../tests/js/WithRegistrySetup';
+import { withWidgetComponentProps } from '../../../../../../googlesitekit/widgets/util';
+import { availableAudiences } from '../../../../datastore/__fixtures__';
+
+const WidgetWithComponentProps =
+	withWidgetComponentProps( 'placeholderTile' )( PlaceholderTile );
+
+function Template( { setupRegistry = () => {}, ...args } ) {
+	return (
+		<WithRegistrySetup func={ setupRegistry }>
+			<WidgetWithComponentProps { ...args } />
+		</WithRegistrySetup>
+	);
+}
+
+export const WithSelectableAudiences = Template.bind( {} );
+WithSelectableAudiences.storyName = 'WithSelectableAudiences';
+WithSelectableAudiences.args = {
+	setupRegistry: ( registry ) => {
+		registry.dispatch( MODULES_ANALYTICS_4 ).receiveGetSettings( {
+			availableAudiences,
+		} );
+	},
+};
+WithSelectableAudiences.scenario = {};
+
+export const WithoutSelectableAudiences = Template.bind( {} );
+WithoutSelectableAudiences.storyName = 'WithoutSelectableAudiences';
+WithoutSelectableAudiences.args = {
+	setupRegistry: ( registry ) => {
+		registry.dispatch( MODULES_ANALYTICS_4 ).receiveGetSettings( {
+			availableAudiences: [],
+		} );
+	},
+};
+WithoutSelectableAudiences.scenario = {};
+
+export default {
+	title: 'Modules/Analytics4/Components/AudienceSegmentation/Dashboard/PlaceholderTile',
+	component: PlaceholderTile,
+};
