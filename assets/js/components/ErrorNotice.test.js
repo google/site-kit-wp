075da085af5d81bd8a616d3c3b9efa991dd1b945
Simon Dowdles <simon.dowdles@get10up.com>
2024-08-15 09:16:44 +0200

    Add new test case for asserting that by default the retry button is not rendered | Default hasButton to false in renderErrorNotice function, update test case to include hasButton whe error is retryable.

--- assets/js/components/ErrorNotice.test.js
+++ assets/js/components/ErrorNotice.test.js
@@ -52,7 +52,11 @@ describe( 'ErrorNotice', () => {
 		invalidateResolutionSpy.mockReset();
 	} );
 
-	async function renderErrorNotice( { error, storeName } ) {
+	async function renderErrorNotice( {
+		error,
+		storeName,
+		hasButton = false,
+	} ) {
 		fetchMock.get(
 			new RegExp(
 				'^/google-site-kit/v1/modules/tagmanager/data/accounts'
@@ -75,7 +79,7 @@ describe( 'ErrorNotice', () => {
 
 		return render(
 			<ErrorNotice
-				hasButton
+				hasButton={ hasButton }
 				error={ selectorError }
 				storeName={ storeName }
 			/>,
@@ -84,6 +88,21 @@ describe( 'ErrorNotice', () => {
 			}
 		);
 	}
+
+	it( 'should not render the `Retry` button by default', async () => {
+		const { queryByText } = await renderErrorNotice( {
+			error: {
+				code: 'test-error-code',
+				message: 'Test error message',
+				data: {
+					reason: '',
+				},
+			},
+			storeName: MODULES_TAGMANAGER,
+		} );
+
+		expect( queryByText( /retry/i ) ).not.toBeInTheDocument();
+	} );
 
 	it( 'should not render the `Retry` button if the error reason is `ERROR_REASON_INSUFFICIENT_PERMISSIONS`', async () => {
 		const { queryByText } = await renderErrorNotice( {
@@ -131,7 +150,7 @@ describe( 'ErrorNotice', () => {
 		expect( queryByText( /retry/i ) ).not.toBeInTheDocument();
 	} );
 
-	it( 'should render the `Retry` button if the error is retryable', async () => {
+	it( 'should render the `Retry` button if the error is retryable and hasButton is passed', async () => {
 		const { queryByText } = await renderErrorNotice( {
 			error: {
 				code: 'test-error-code',
@@ -141,6 +160,7 @@ describe( 'ErrorNotice', () => {
 				},
 			},
 			storeName: MODULES_TAGMANAGER,
+			hasButton: true,
 		} );
 
 		expect( queryByText( /retry/i ) ).toBeInTheDocument();
