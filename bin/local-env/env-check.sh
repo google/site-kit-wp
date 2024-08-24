1ab4db8556de2d83be1098f679f4c63775544a9e
Evan Mattson <evan.mattson@10up.com>
2024-08-23 23:09:42 -0400

    Fix env-check script.

--- bin/local-env/env-check.sh
+++ bin/local-env/env-check.sh
@@ -7,7 +7,7 @@ set -u
 . "$(dirname "$0")/includes.sh"
 
 # If the WordPress container isn't running, start the environment.
-if ! dc ps -q $CONTAINER ; then
+if ! container ls > /dev/null ; then
 	status_message "E2E environment is not running, starting it now..."
 	npm run env:start
 fi
