0ac8d25121b39de18ca5a446238cc581d592a947
Jimmy Madon <hello@jimmymadon.com>
2024-09-01 03:02:13 +0530

    Add check for unsatisfied scopes.

--- assets/js/googlesitekit/notifications/register-defaults.js
+++ assets/js/googlesitekit/notifications/register-defaults.js
@@ -70,7 +70,11 @@ export function registerDefaults( notificationsAPI ) {
 			const showUnsatisfiedScopesAlertGTE =
 				ga4ModuleConnected && ! hasTagManagerReadScope;
 
+			const unsatisfiedScopes =
+				select( CORE_USER ).getUnsatisfiedScopes();
+
 			return (
+				unsatisfiedScopes?.length &&
 				! setupErrorMessage &&
 				isAuthenticated &&
 				! showUnsatisfiedScopesAlertGTE
