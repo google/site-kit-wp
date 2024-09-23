d82ff772f1cc9ed5acb8e393ab9fbef11b93409b
Aleksej <aleksej.vukomanovic@10up.com>
2024-09-23 07:43:03 +0200

    Register TopTrafficSourceDrivingPurchasesWidget to the KMW widget.

--- assets/js/modules/analytics-4/index.js
+++ assets/js/modules/analytics-4/index.js
@@ -36,6 +36,7 @@ import {
 	TopCitiesDrivingLeadsWidget,
 	TopCountriesWidget,
 	TopTrafficSourceWidget,
+	TopTrafficSourceDrivingPurchasesWidget,
 	TopConvertingTrafficSourceWidget,
 	PagesPerVisitWidget,
 	VisitLengthWidget,
@@ -74,6 +75,7 @@ import {
 	KM_ANALYTICS_TOP_RECENT_TRENDING_PAGES,
 	KM_ANALYTICS_TOP_RETURNING_VISITOR_PAGES,
 	KM_ANALYTICS_TOP_TRAFFIC_SOURCE,
+	KM_ANALYTICS_TOP_TRAFFIC_SOURCE_DRIVING_PURCHASES,
 	KM_ANALYTICS_VISIT_LENGTH,
 	KM_ANALYTICS_VISITS_PER_VISITOR,
 } from '../../googlesitekit/datastore/user/constants';
@@ -467,6 +469,22 @@ export const registerWidgets = ( widgets ) => {
 			isActive: ( select ) =>
 				select( CORE_USER ).isKeyMetricActive(
 					KM_ANALYTICS_TOP_TRAFFIC_SOURCE
+				),
+		},
+		[ AREA_MAIN_DASHBOARD_KEY_METRICS_PRIMARY ]
+	);
+
+	widgets.registerWidget(
+		KM_ANALYTICS_TOP_TRAFFIC_SOURCE_DRIVING_PURCHASES,
+		{
+			Component: TopTrafficSourceDrivingPurchasesWidget,
+			width: widgets.WIDGET_WIDTHS.QUARTER,
+			priority: 1,
+			wrapWidget: false,
+			modules: [ 'analytics-4' ],
+			isActive: ( select ) =>
+				select( CORE_USER ).isKeyMetricActive(
+					KM_ANALYTICS_TOP_TRAFFIC_SOURCE_DRIVING_PURCHASES
 				),
 		},
 		[ AREA_MAIN_DASHBOARD_KEY_METRICS_PRIMARY ]
