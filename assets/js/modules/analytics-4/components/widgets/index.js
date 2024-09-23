5aeb6c4f1e09f65d706e9b02462d007378b771b6
Aleksej <aleksej.vukomanovic@10up.com>
2024-09-23 07:43:19 +0200

    Add TopTrafficSourceDrivingPurchasesWidget to the list of exports.

--- assets/js/modules/analytics-4/components/widgets/index.js
+++ assets/js/modules/analytics-4/components/widgets/index.js
@@ -30,6 +30,7 @@ export { default as TopCitiesDrivingLeadsWidget } from './TopCitiesDrivingLeadsW
 export { default as TopCountriesWidget } from './TopCountriesWidget';
 export { default as TopTrafficSourceWidget } from './TopTrafficSourceWidget';
 export { default as TopConvertingTrafficSourceWidget } from './TopConvertingTrafficSourceWidget';
+export { default as TopTrafficSourceDrivingPurchasesWidget } from './TopTrafficSourceDrivingPurchasesWidget';
 export { default as PagesPerVisitWidget } from './PagesPerVisitWidget';
 export { default as VisitLengthWidget } from './VisitLengthWidget';
 export { default as TopReturningVisitorPages } from './TopReturningVisitorPages';
