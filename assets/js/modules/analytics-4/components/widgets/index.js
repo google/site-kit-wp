3381bfdd025c5caefdb42b14ed9d24280fbd450f
Aleksej <aleksej.vukomanovic@10up.com>
2024-09-23 05:56:33 +0200

    Add TopTrafficSourceDrivingAddToCartWidget to the exports.

--- assets/js/modules/analytics-4/components/widgets/index.js
+++ assets/js/modules/analytics-4/components/widgets/index.js
@@ -29,6 +29,7 @@ export { default as TopCitiesWidget } from './TopCitiesWidget';
 export { default as TopCitiesDrivingLeadsWidget } from './TopCitiesDrivingLeadsWidget';
 export { default as TopCountriesWidget } from './TopCountriesWidget';
 export { default as TopTrafficSourceWidget } from './TopTrafficSourceWidget';
+export { default as TopTrafficSourceDrivingAddToCartWidget } from './TopTrafficSourceDrivingAddToCartWidget';
 export { default as TopConvertingTrafficSourceWidget } from './TopConvertingTrafficSourceWidget';
 export { default as PagesPerVisitWidget } from './PagesPerVisitWidget';
 export { default as VisitLengthWidget } from './VisitLengthWidget';
