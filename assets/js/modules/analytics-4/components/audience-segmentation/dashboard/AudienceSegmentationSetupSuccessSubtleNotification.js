0a1fb3031b639d3b21b0fc58fa1da53fc017006b
Jimmy Madon <hello@jimmymadon.com>
2024-09-23 19:44:22 +0530

    Remove all redundant logic from Audiences Setup Success Subtle Notification.

--- assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSegmentationSetupSuccessSubtleNotification.js
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSegmentationSetupSuccessSubtleNotification.js
@@ -24,49 +24,16 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { useDispatch, useSelect } from 'googlesitekit-data';
-import { CORE_USER } from '../../../../../googlesitekit/datastore/user/constants';
 import { Button } from 'googlesitekit-components';
 import SubtleNotification from '../../SubtleNotification';
 import { getNavigationalScrollTop } from '../../../../../util/scroll';
 import { useBreakpoint } from '../../../../../hooks/useBreakpoint';
-import useDashboardType, {
-	DASHBOARD_TYPE_MAIN,
-} from '../../../../../hooks/useDashboardType';
-import useViewOnly from '../../../../../hooks/useViewOnly';
-
-export const AUDIENCE_SEGMENTATION_SETUP_SUCCESS_NOTIFICATION =
-	'audience_segmentation_setup_success_notification';
 
 export default function AudienceSegmentationSetupSuccessSubtleNotification() {
 	const breakpoint = useBreakpoint();
-	const dashboardType = useDashboardType();
-	const viewOnly = useViewOnly();
-
-	const configuredAudiences = useSelect( ( select ) =>
-		select( CORE_USER ).getConfiguredAudiences()
-	);
-
-	const isDismissed = useSelect( ( select ) => {
-		if ( ! dashboardType || viewOnly ) {
-			return null;
-		}
-
-		return select( CORE_USER ).isItemDismissed(
-			AUDIENCE_SEGMENTATION_SETUP_SUCCESS_NOTIFICATION
-		);
-	} );
-
-	const { dismissItem } = useDispatch( CORE_USER );
-
-	function dismissNotificationForUser() {
-		dismissItem( AUDIENCE_SEGMENTATION_SETUP_SUCCESS_NOTIFICATION );
-	}
 
 	const scrollToWidgetArea = ( event ) => {
 		event.preventDefault();
-
-		dismissNotificationForUser();
 
 		setTimeout( () => {
 			const widgetClass =
@@ -79,21 +46,6 @@ export default function AudienceSegmentationSetupSuccessSubtleNotification() {
 		}, 50 );
 	};
 
-	const shouldShowNotification =
-		// Only show this notification on the main dashboard, where the Setup CTA Banner is shown.
-		dashboardType === DASHBOARD_TYPE_MAIN &&
-		// Don't show this notification on the view-only dashboard.
-		! viewOnly &&
-		// Don't show this notification if `isDismissed` call is still loading
-		// or the user has dismissed it.
-		! isDismissed &&
-		// Only show this notification if the user has a set of configured audiences.
-		Array.isArray( configuredAudiences );
-
-	if ( ! shouldShowNotification ) {
-		return null;
-	}
-
 	return (
 		<SubtleNotification
 			title={ __(
@@ -104,7 +56,6 @@ export default function AudienceSegmentationSetupSuccessSubtleNotification() {
 				'Get to know how different types of visitors interact with your site, e.g. which pages they visit and for how long.',
 				'google-site-kit'
 			) }
-			onDismiss={ dismissNotificationForUser }
 			additionalCTA={
 				<Button onClick={ scrollToWidgetArea }>
 					{ __( 'Show me', 'google-site-kit' ) }
