1e72892e81262ccd00b3909cac24f6f1c7b6763e
Eugene Manuilov <eugene.manuilov@gmail.com>
2024-09-24 14:37:04 +0300

    Implement phpunit tests.

---
+++ tests/phpunit/integration/Modules/Sign_In_With_GoogleTest.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * Sign_In_With_GoogleTest
+ *
+ * @package   Google\Site_Kit\Tests\Modules
+ * @copyright 2024 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests\Modules;
+
+use Google\Site_Kit\Context;
+use Google\Site_Kit\Modules\Sign_In_With_Google;
+use Google\Site_Kit\Tests\TestCase;
+
+/**
+ * @group Modules
+ */
+class Sign_In_With_GoogleTest extends TestCase {
+
+	/**
+	 * Sign_In_With_Google object.
+	 *
+	 * @var Sign_In_With_Google
+	 */
+	private $module;
+
+	public function set_up() {
+		parent::set_up();
+		$this->module = new Sign_In_With_Google( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
+	}
+
+	public function test_magic_methods() {
+		$this->assertEquals( Sign_In_With_Google::MODULE_SLUG, $this->module->slug );
+		$this->assertEquals( 'Sign in with Google', $this->module->name );
+		$this->assertEquals( 'https://developers.google.com/identity/gsi/web/guides/overview', $this->module->homepage );
+		$this->assertEquals( 'Improve user engagement, trust, and data privacy, while creating a simple, secure, and personalised experience for your visitors', $this->module->description );
+		$this->assertEquals( 10, $this->module->order );
+	}
+
+}
