0c21724e1aa3158a767f153c8aa63880c30c583a
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-09-24 12:12:05 +0530

    Remove AREA_MODULE_SEARCH_CONSOLE_MAIN constant.

--- assets/js/modules/search-console/constants.js
+++ assets/js/modules/search-console/constants.js
@@ -15,5 +15,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-export const AREA_MODULE_SEARCH_CONSOLE_MAIN = 'moduleSearchConsoleMain';
