e2ca5d4d64293b53120d51e330b3e0b31d742531
Hussain Thajutheen <hussain.thajutheen@10up.com>
2024-09-25 21:13:37 +0530

    Clear error after successful audience creation.

--- assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceCreationNotice.js
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceCreationNotice.js
@@ -155,6 +155,8 @@ export default function AudienceCreationNotice() {
 
 			if ( !! error ) {
 				setApiErrors( [ error ] );
+			} else {
+				setApiErrors( [] );
 			}
 
 			await syncAvailableAudiences();
