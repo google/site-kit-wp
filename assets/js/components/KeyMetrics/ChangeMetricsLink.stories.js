0097bad6f0b3a6ff2920b488973ca94bacaa3cbc
Ben Bowler <ben@benbowler.com>
2024-10-01 10:03:11 +0100

    Fix regression in ChangeMetricsLink story.

--- assets/js/components/KeyMetrics/ChangeMetricsLink.stories.js
+++ assets/js/components/KeyMetrics/ChangeMetricsLink.stories.js
@@ -24,6 +24,7 @@ import {
 	provideUserAuthentication,
 } from '../../../../tests/js/utils';
 import WithRegistrySetup from '../../../../tests/js/WithRegistrySetup';
+import { KM_ANALYTICS_LEAST_ENGAGING_PAGES } from '../../googlesitekit/datastore/user/constants';
 import ChangeMetricsLink from './ChangeMetricsLink';
 
 function Template() {
@@ -43,7 +44,9 @@ export default {
 		( Story ) => {
 			const setupRegistry = ( registry ) => {
 				provideUserAuthentication( registry );
-				provideKeyMetrics( registry, { widgetSlugs: [ 'metricA' ] } );
+				provideKeyMetrics( registry, {
+					widgetSlugs: [ KM_ANALYTICS_LEAST_ENGAGING_PAGES ],
+				} );
 			};
 
 			return (
