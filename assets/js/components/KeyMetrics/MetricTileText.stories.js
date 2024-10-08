7ad1fd0be927f38b8003b59b24b9668696318304
Jimmy Madon <hello@jimmymadon.com>
2024-10-08 20:52:55 +0100

    Add Zero Data scenario to generic MetricTileText component.

--- assets/js/components/KeyMetrics/MetricTileText.stories.js
+++ assets/js/components/KeyMetrics/MetricTileText.stories.js
@@ -71,6 +71,20 @@ ZeroChange.scenario = {
 	delay: 250,
 };
 
+export const ZeroData = Template.bind( {} );
+ZeroData.storyName = 'Zero Data';
+ZeroData.args = {
+	title: 'Most engaged traffic source',
+	metricValue: '-',
+	subText: '0% of engaged sessions',
+	currentValue: 0,
+	previousValue: 0,
+};
+ZeroData.scenario = {
+	label: 'KeyMetrics/MetricTileText/ZeroData',
+	delay: 250,
+};
+
 export const Loading = Template.bind( {} );
 Loading.storyName = 'Loading';
 Loading.args = {
