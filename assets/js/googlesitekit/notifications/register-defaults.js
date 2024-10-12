68bd7e006a7debac3427f6a48fa636e4c57393ee
Jimmy Madon <hello@jimmymadon.com>
2024-10-12 13:05:00 +0200

    Ensure isModuleConnected selector is resolved when registering notifications.

--- assets/js/googlesitekit/notifications/register-defaults.js
+++ assets/js/googlesitekit/notifications/register-defaults.js
@@ -60,14 +60,15 @@ export function registerDefaults( notificationsAPI ) {
 			VIEW_CONTEXT_ENTITY_DASHBOARD_VIEW_ONLY,
 			VIEW_CONTEXT_SETTINGS,
 		],
-		checkRequirements: ( { select } ) => {
+		checkRequirements: async ( { select, resolveSelect } ) => {
 			const setupErrorMessage =
 				select( CORE_SITE ).getSetupErrorMessage();
 
 			const isAuthenticated = select( CORE_USER ).isAuthenticated();
 
-			const ga4ModuleConnected =
-				select( CORE_MODULES ).isModuleConnected( 'analytics-4' );
+			const ga4ModuleConnected = await resolveSelect(
+				CORE_MODULES
+			).isModuleConnected( 'analytics-4' );
 
 			const hasTagManagerReadScope = select( CORE_USER ).hasScope(
 				TAGMANAGER_READ_SCOPE
@@ -100,14 +101,15 @@ export function registerDefaults( notificationsAPI ) {
 			VIEW_CONTEXT_ENTITY_DASHBOARD_VIEW_ONLY,
 			VIEW_CONTEXT_SETTINGS,
 		],
-		checkRequirements: ( { select } ) => {
+		checkRequirements: async ( { select, resolveSelect } ) => {
 			const setupErrorMessage =
 				select( CORE_SITE ).getSetupErrorMessage();
 
 			const isAuthenticated = select( CORE_USER ).isAuthenticated();
 
-			const ga4ModuleConnected =
-				select( CORE_MODULES ).isModuleConnected( 'analytics-4' );
+			const ga4ModuleConnected = await resolveSelect(
+				CORE_MODULES
+			).isModuleConnected( 'analytics-4' );
 
 			const hasTagManagerReadScope = select( CORE_USER ).hasScope(
 				TAGMANAGER_READ_SCOPE
@@ -140,11 +142,13 @@ export function registerDefaults( notificationsAPI ) {
 				resolveSelect,
 				dispatch,
 			} ) => {
-				const adSenseModuleConnected =
-					select( CORE_MODULES ).isModuleConnected( 'adsense' );
+				const adSenseModuleConnected = await resolveSelect(
+					CORE_MODULES
+				).isModuleConnected( 'adsense' );
 
-				const analyticsModuleConnected =
-					select( CORE_MODULES ).isModuleConnected( 'analytics-4' );
+				const analyticsModuleConnected = await resolveSelect(
+					CORE_MODULES
+				).isModuleConnected( 'analytics-4' );
 
 				const isAdSenseLinked =
 					select( MODULES_ANALYTICS_4 ).getAdSenseLinked();
@@ -221,8 +225,9 @@ export function registerDefaults( notificationsAPI ) {
 			const viewOnly =
 				SITE_KIT_VIEW_ONLY_CONTEXTS.includes( viewContext );
 
-			const isAnalyticsConnected =
-				select( CORE_MODULES ).isModuleConnected( 'analytics-4' );
+			const isAnalyticsConnected = await resolveSelect(
+				CORE_MODULES
+			).isModuleConnected( 'analytics-4' );
 
 			const canViewSharedAnalytics = ! viewOnly
 				? true
@@ -295,8 +300,9 @@ export function registerDefaults( notificationsAPI ) {
 
 			const getModuleState = async ( moduleSlug, datastoreSlug ) => {
 				// Check if the module connected and return early if not.
-				const isConnected =
-					select( CORE_MODULES ).isModuleConnected( moduleSlug );
+				const isConnected = await resolveSelect(
+					CORE_MODULES
+				).isModuleConnected( moduleSlug );
 				if ( ! isConnected ) {
 					return 'disconnected';
 				}
