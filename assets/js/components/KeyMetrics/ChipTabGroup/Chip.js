9d554976c783e95a298ba2bac3ca4b4d3acdd0ed
Aleksej <aleksej.vukomanovic@10up.com>
2024-10-23 15:27:42 +0200

    Add Chip component.

---
+++ assets/js/components/KeyMetrics/ChipTabGroup/Chip.js
@@ -0,0 +1,69 @@
+/**
+ * Chip component.
+ *
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import propTypes from 'prop-types';
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import { Button } from 'googlesitekit-components';
+import { KEY_METRICS_CURRENT_SELECTION_GROUP_SLUG } from '../constants';
+import CheckMark from '../../../../svg/icons/check-2.svg';
+
+export default function Chip( {
+	slug,
+	label,
+	isActive = false,
+	onClick,
+	selectedCount,
+} ) {
+	return (
+		<Button
+			className={ classnames( 'googlesitekit-chip-tab-group__chip-item', {
+				'googlesitekit-chip-tab-group__chip-item--active': isActive,
+			} ) }
+			icon={
+				slug === KEY_METRICS_CURRENT_SELECTION_GROUP_SLUG ? (
+					<CheckMark width={ 12 } height={ 12 } />
+				) : null
+			}
+			trailingIcon={
+				selectedCount > 0 ? (
+					<span className="googlesitekit-chip-tab-group__chip-item-count">
+						({ selectedCount })
+					</span>
+				) : null
+			}
+			onClick={ () => onClick( slug ) }
+		>
+			{ label }
+		</Button>
+	);
+}
+
+Chip.propTypes = {
+	slug: propTypes.string.isRequired,
+	label: propTypes.string.isRequired,
+	isActive: propTypes.boolean,
+	selectedCount: propTypes.number.isRequired,
+	onClick: propTypes.func.isRequired,
+};
