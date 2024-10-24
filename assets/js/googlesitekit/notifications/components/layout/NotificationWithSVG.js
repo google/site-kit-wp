2e4a887d690a6d0a68022b94bbfe761f911dfd52
Jimmy Madon <hello@jimmymadon.com>
2024-10-25 00:37:10 +0200

    Add skeleton of new Notification layout with a large SVG.

---
+++ assets/js/googlesitekit/notifications/components/layout/NotificationWithSVG.js
@@ -0,0 +1,100 @@
+/**
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import {
+	BREAKPOINT_SMALL,
+	BREAKPOINT_TABLET,
+	useBreakpoint,
+} from '../../../../hooks/useBreakpoint';
+import { Cell, Grid, Row } from '../../../../material-components';
+
+export default function NotificationWithSVG( {
+	title,
+	description,
+	actions,
+	DesktopSVG,
+	TabletSVG,
+	MobileSVG,
+} ) {
+	const breakpoint = useBreakpoint();
+	const isMobileBreakpoint = breakpoint === BREAKPOINT_SMALL;
+	const isTabletBreakpoint = breakpoint === BREAKPOINT_TABLET;
+
+	return (
+		<div className="googlesitekit-widget-context">
+			<Grid className="googlesitekit-widget-area">
+				<Row>
+					<Cell size={ 12 }>
+						<div className="'googlesitekit-widget googlesitekit-widget--no-padding googlesitekit-setup-cta-banner">
+							<Grid collapsed>
+								<Row>
+									<Cell
+										smSize={ 12 }
+										mdSize={ 8 }
+										lgSize={ 6 }
+										className="googlesitekit-setup-cta-banner__primary-cell"
+									>
+										<h4 className="googlesitekit-setup-cta-banner__title">
+											{ title }
+										</h4>
+										<p className="googlesitekit-setup-cta-banner__description">
+											{ description }
+										</p>
+
+										<div className="googlesitekit-setup-cta-banner__actions-wrapper">
+											{ actions }
+										</div>
+									</Cell>
+									{ ! isMobileBreakpoint &&
+										! isTabletBreakpoint && (
+											<Cell
+												alignBottom
+												className="googlesitekit-setup-cta-banner__svg-wrapper"
+												mdSize={ 8 }
+												lgSize={ 6 }
+											>
+												<DesktopSVG />
+											</Cell>
+										) }
+									{ isTabletBreakpoint && (
+										<Cell
+											className="googlesitekit-setup-cta-banner__svg-wrapper"
+											mdSize={ 8 }
+										>
+											<TabletSVG />
+										</Cell>
+									) }
+									{ isMobileBreakpoint && (
+										<Cell
+											alignBottom
+											className="googlesitekit-setup-cta-banner__svg-wrapper"
+											smSize={ 12 }
+										>
+											<MobileSVG />
+										</Cell>
+									) }
+								</Row>
+							</Grid>
+						</div>
+					</Cell>
+				</Row>
+			</Grid>
+		</div>
+	);
+}
