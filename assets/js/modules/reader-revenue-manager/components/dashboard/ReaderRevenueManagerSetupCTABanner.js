9306ce872423e46adece613e40953a27678168b5
Jimmy Madon <hello@jimmymadon.com>
2024-10-28 17:38:17 +0200

    Standardise description within all Setup CTA banners to be in line with new notification infrastructure.

--- assets/js/modules/reader-revenue-manager/components/dashboard/ReaderRevenueManagerSetupCTABanner.js
+++ assets/js/modules/reader-revenue-manager/components/dashboard/ReaderRevenueManagerSetupCTABanner.js
@@ -194,34 +194,36 @@ function ReaderRevenueManagerSetupCTABanner( { Widget, WidgetNull } ) {
 												'google-site-kit'
 											) }
 										</h4>
-										<p className="googlesitekit-setup-cta-banner__description">
-											{ createInterpolateElement(
-												__(
-													'Turn casual visitors into loyal readers and earn more from your content with voluntary contributions, surveys, newsletter sign-ups and reader insight tools. <a>Learn more</a>',
-													'google-site-kit'
-												),
-												{
-													a: (
-														<Link
-															href={
-																readerRevenueManagerDocumentationURL
-															}
-															external
-															aria-label={ __(
-																'Learn more about reader revenue manager',
-																'google-site-kit'
-															) }
-														/>
+										<div className="googlesitekit-setup-cta-banner__description">
+											<p>
+												{ createInterpolateElement(
+													__(
+														'Turn casual visitors into loyal readers and earn more from your content with voluntary contributions, surveys, newsletter sign-ups and reader insight tools. <a>Learn more</a>',
+														'google-site-kit'
 													),
-												}
-											) }
-											<br />
-											<br />
-											{ __(
-												'* Support for subscriptions coming soon',
-												'google-site-kit'
-											) }
-										</p>
+													{
+														a: (
+															<Link
+																href={
+																	readerRevenueManagerDocumentationURL
+																}
+																external
+																aria-label={ __(
+																	'Learn more about reader revenue manager',
+																	'google-site-kit'
+																) }
+															/>
+														),
+													}
+												) }
+												<br />
+												<br />
+												{ __(
+													'* Support for subscriptions coming soon',
+													'google-site-kit'
+												) }
+											</p>
+										</div>
 
 										<div className="googlesitekit-setup-cta-banner__actions-wrapper">
 											<Button
