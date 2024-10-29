64d48900f750c41a2bb88817608558cc8075c64c
Jimmy Madon <hello@jimmymadon.com>
2024-10-29 21:23:45 +0200

    Remove redundant and incorrect filtering of key metric settings.

--- assets/js/googlesitekit/datastore/user/key-metrics.js
+++ assets/js/googlesitekit/datastore/user/key-metrics.js
@@ -481,70 +481,22 @@ const baseSelectors = {
 	} ),
 
 	/**
-	 * Gets key metrics settings, taking into account whether the user has view-only access.
-	 * If the user has view-only access and the custom dimensions required by the selected widgets are unavailable,
-	 * the widget slugs will be removed from the settings. If only one widget remains after filtering, the widget slugs
-	 * will be an empty array to prevent hiding the widget area.
+	 * Gets key metrics settings.
 	 *
 	 * @since 1.103.0
-	 * @since 1.114.0 Checks for view-only access and adjusts the `widgetSlugs` accordingly.
 	 *
 	 * @param {Object} state Data store's state.
 	 * @return {(Object|undefined)} Key metrics settings. Returns `undefined` if not loaded.
 	 */
-	getKeyMetricsSettings: createRegistrySelector( ( select ) => ( state ) => {
+	getKeyMetricsSettings( state ) {
 		const keyMetricsSettings = state.keyMetricsSettings;
 
 		if ( ! keyMetricsSettings ) {
 			return undefined;
 		}
 
-		const isViewOnly = ! select( CORE_USER ).isAuthenticated();
-
-		// TODO The below logic is specific to unavailable custom dimensions for view only users
-		// who had user picked metrics. So move this to somewhere more appropriate.
-		if ( isViewOnly ) {
-			// Filter out widget slugs that depend on unavailable custom dimensions.
-			const filteredWidgetSlugs = keyMetricsSettings.widgetSlugs.filter(
-				( slug ) => {
-					const widget = KEY_METRICS_WIDGETS[ slug ];
-
-					if ( ! widget ) {
-						return false;
-					}
-
-					if (
-						// The call to displayInList() was added solely for metrics
-						// with custom dimensions, so adding this check till we refactor this
-						// whole isViewOnly section.
-						widget.requiredCustomDimensions?.length &&
-						widget.displayInList
-					) {
-						return widget.displayInList( select, isViewOnly, slug );
-					}
-
-					return true;
-				}
-			);
-
-			// If only one widget remains after filtering, return an empty array.
-			// This prevents hiding the widget area when only one widget is available.
-			// This triggers the `getKeyMetrics` selector to use the default widgets instead.
-			if ( filteredWidgetSlugs.length === 1 ) {
-				return {
-					...keyMetricsSettings,
-					widgetSlugs: [],
-				};
-			}
-
-			return {
-				...keyMetricsSettings,
-				widgetSlugs: filteredWidgetSlugs,
-			};
-		}
-
 		return keyMetricsSettings;
-	} ),
+	},
 
 	/**
 	 * Determines whether the key metrics settings are being saved or not.
