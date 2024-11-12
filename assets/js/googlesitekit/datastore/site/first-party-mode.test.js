6a869c4b0abe070ff368a0457f0e943f7d81ca3a
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2024-11-12 12:03:24 +0000

    Add tests for `saveFirstPartyModeSettings()` action.

---
+++ assets/js/googlesitekit/datastore/site/first-party-mode.test.js
@@ -0,0 +1,132 @@
+/**
+ * Site Kit by Google, Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import API from 'googlesitekit-api';
+import { createTestRegistry } from '../../../../../tests/js/utils';
+import { CORE_SITE } from './constants';
+
+describe( 'core/site First-Party Mode', () => {
+	let registry;
+
+	const firstPartyModeSettingsEndpointRegExp = new RegExp(
+		'^/google-site-kit/v1/core/site/data/fpm-settings'
+	);
+
+	beforeAll( () => {
+		API.setUsingCache( false );
+	} );
+
+	beforeEach( () => {
+		registry = createTestRegistry();
+	} );
+
+	afterAll( () => {
+		API.setUsingCache( true );
+	} );
+
+	describe( 'actions', () => {
+		describe( 'saveFirstPartyModeSettings', () => {
+			it( 'saves the settings and returns the response', async () => {
+				const updatedSettings = {
+					isEnabled: true,
+					isFPMHealthy: false,
+					isScriptAccessEnabled: false,
+				};
+
+				fetchMock.postOnce( firstPartyModeSettingsEndpointRegExp, {
+					body: updatedSettings,
+					status: 200,
+				} );
+
+				registry
+					.dispatch( CORE_SITE )
+					.receiveGetFirstPartyModeSettings( {
+						isEnabled: false,
+						isFPMHealthy: false,
+						isScriptAccessEnabled: false,
+					} );
+
+				registry.dispatch( CORE_SITE ).setFirstPartyModeEnabled( true );
+
+				const { response } = await registry
+					.dispatch( CORE_SITE )
+					.saveFirstPartyModeSettings();
+
+				expect( fetchMock ).toHaveFetched(
+					firstPartyModeSettingsEndpointRegExp,
+					{
+						body: {
+							data: {
+								settings: updatedSettings,
+							},
+						},
+					}
+				);
+
+				expect( response ).toEqual( updatedSettings );
+			} );
+
+			it( 'returns an error if the request fails', async () => {
+				const errorResponse = {
+					code: 'internal_server_error',
+					message: 'Internal server error',
+					data: { status: 500 },
+				};
+
+				fetchMock.postOnce( firstPartyModeSettingsEndpointRegExp, {
+					body: errorResponse,
+					status: 500,
+				} );
+
+				registry
+					.dispatch( CORE_SITE )
+					.receiveGetFirstPartyModeSettings( {
+						isEnabled: false,
+						isFPMHealthy: false,
+						isScriptAccessEnabled: false,
+					} );
+
+				registry.dispatch( CORE_SITE ).setFirstPartyModeEnabled( true );
+
+				const { error } = await registry
+					.dispatch( CORE_SITE )
+					.saveFirstPartyModeSettings();
+
+				expect( fetchMock ).toHaveFetched(
+					firstPartyModeSettingsEndpointRegExp,
+					{
+						body: {
+							data: {
+								settings: {
+									isEnabled: true,
+									isFPMHealthy: false,
+									isScriptAccessEnabled: false,
+								},
+							},
+						},
+					}
+				);
+
+				expect( error ).toEqual( errorResponse );
+
+				expect( console ).toHaveErrored();
+			} );
+		} );
+	} );
+} );
