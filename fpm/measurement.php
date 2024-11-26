56698c5235ea24d39ab44bd21e7216c314510e33
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2024-11-26 16:42:03 +0000

    Update comments to keep diff to original measurement.php at a minimum.

--- fpm/measurement.php
+++ fpm/measurement.php
@@ -8,16 +8,18 @@
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  *
  * NOTICE: This file has been modified from its original version in accordance with the Apache License, Version 2.0.
- */
-
-namespace Google\FirstPartyLibrary;
-
-/**
- * This is a stub for the measurement.php file which is used to proxy requests to the FPFE service.
- * The main body of the file will be merged into this file via issue #9665.
  *
  * PHPCS rules have been disabled at the very top of the file to allow it to be included in Site Kit's codebase mostly unmodified.
  */
+
+// This file should run in isolation from any other PHP file. This means using
+// minimal to no external dependencies, which leads us to supressing the
+// following linting rules:
+//
+// phpcs:disable PSR1.Files.SideEffects.FoundWithSymbols
+// phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
+
+namespace Google\FirstPartyLibrary;
 
 if ( isset( $_GET['healthCheck'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
 	echo 'ok';
