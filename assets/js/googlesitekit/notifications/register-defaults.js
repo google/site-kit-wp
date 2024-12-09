ca7c076ca79e6f02e1b78f03ea88b1cc6d989009
nfmohit <nahid.mohit@10up.com>
2024-12-10 03:16:30 +0600

    Ensure CTA banner is displayed.

--- assets/js/googlesitekit/notifications/register-defaults.js
+++ assets/js/googlesitekit/notifications/register-defaults.js
@@ -452,7 +452,7 @@ export const DEFAULT_NOTIFICATIONS = {
 		areaSlug: NOTIFICATION_AREAS.BANNERS_BELOW_NAV,
 		groupID: NOTIFICATION_GROUPS.SETUP_CTAS,
 		viewContexts: [ VIEW_CONTEXT_MAIN_DASHBOARD ],
-		checkRequirements: async ( { select, resolveSelect } ) => {
+		checkRequirements: async ( { select, resolveSelect, dispatch } ) => {
 			if ( ! isFeatureEnabled( 'firstPartyMode' ) ) {
 				return false;
 			}
@@ -476,11 +476,19 @@ export const DEFAULT_NOTIFICATIONS = {
 				isScriptAccessEnabled,
 			} = select( CORE_SITE );
 
-			return (
-				! isFirstPartyModeEnabled() &&
-				isFPMHealthy() &&
-				isScriptAccessEnabled()
-			);
+			if ( isFirstPartyModeEnabled() ) {
+				return false;
+			}
+
+			if (
+				[ isFPMHealthy(), isScriptAccessEnabled() ].includes( null )
+			) {
+				await dispatch(
+					CORE_SITE
+				).fetchGetFPMServerRequirementStatus();
+			}
+
+			return isFPMHealthy() && isScriptAccessEnabled();
 		},
 		isDismissible: true,
 	},
