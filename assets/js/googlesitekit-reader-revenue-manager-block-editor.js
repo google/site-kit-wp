e9bf010f2944096c8fe8d9b965ad1155b3eb53a8
nfmohit <nahid.mohit@10up.com>
2024-12-21 05:21:36 +0600

    Add toggle.

--- assets/js/googlesitekit-reader-revenue-manager-block-editor.js
+++ assets/js/googlesitekit-reader-revenue-manager-block-editor.js
@@ -20,8 +20,12 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Fragment } from '@wordpress-core/element';
-import { PanelBody, SelectControl } from '@wordpress-core/components';
+import { Fragment, useState } from '@wordpress-core/element';
+import {
+	PanelBody,
+	SelectControl,
+	ToggleControl,
+} from '@wordpress-core/components';
 import {
 	PluginDocumentSettingPanel,
 	PluginSidebar,
@@ -36,23 +40,47 @@ import { select } from 'googlesitekit-data';
 import { CORE_MODULES } from './googlesitekit/modules/datastore/constants';
 import GoogleLogoIcon from '../svg/graphics/logo-g.svg';
 
-function SettingsForm( { label } ) {
+function SettingsForm() {
+	const [ useSnippet, setUseSnippet ] = useState( false );
+
 	return (
-		<SelectControl
-			label={ label }
-			options={ [
-				{ label: __( 'Inherit', 'google-site-kit' ), value: '' },
-				{ label: __( 'None', 'google-site-kit' ), value: 'none' },
-				{
-					label: __( 'Open access', 'google-site-kit' ),
-					value: 'openaccess',
-				},
-			] }
-			help={ __(
-				'The snippet configuration will be inherited from a parent taxonomy term, or Site Kit settings.',
-				'google-site-kit'
+		<Fragment>
+			<ToggleControl
+				label="Place snippet"
+				checked={ useSnippet }
+				onChange={ ( value ) => setUseSnippet( value ) }
+			/>
+			{ useSnippet && (
+				<Fragment>
+					<br />
+					<SelectControl
+						label={ __( 'Product ID', 'google-site-kit' ) }
+						options={ [
+							{
+								label: __( 'Inherit', 'google-site-kit' ),
+								value: '',
+							},
+							{
+								label: __( 'Open access', 'google-site-kit' ),
+								value: 'openaccess',
+							},
+							{
+								label: __( 'Product ID A', 'google-site-kit' ),
+								value: 'openaccess',
+							},
+							{
+								label: __( 'Product ID B', 'google-site-kit' ),
+								value: 'openaccess',
+							},
+						] }
+						help={ __(
+							'The snippet configuration will be inherited from a parent taxonomy term, or Site Kit settings.',
+							'google-site-kit'
+						) }
+					/>
+				</Fragment>
 			) }
-		/>
+		</Fragment>
 	);
 }
 
@@ -93,9 +121,7 @@ function SiteKitSettingPanel() {
 				<PanelBody
 					title={ __( 'Reader Revenue Manager', 'google-site-kit' ) }
 				>
-					<SettingsForm
-						label={ __( 'Choose snippet', 'google-site-kit' ) }
-					/>
+					<SettingsForm />
 				</PanelBody>
 			</PluginSidebar>
 			{ isDocumentSettingPanelAvailable && (
@@ -110,9 +136,7 @@ function SiteKitSettingPanel() {
 							'google-site-kit'
 						) }
 					>
-						<SettingsForm
-							label={ __( 'Choose snippet', 'google-site-kit' ) }
-						/>
+						<SettingsForm />
 					</PanelSection>
 				</PluginDocumentSettingPanel>
 			) }
