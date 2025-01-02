1f9714327baab520feb9a8b0f15d086b4e63a8c0
Ankit Gade <ankit.gade@10up.com>
2025-01-02 16:56:23 +0530

    Fix: php tests.

--- tests/phpunit/integration/Core/Admin/DashboardTest.php
+++ tests/phpunit/integration/Core/Admin/DashboardTest.php
@@ -21,47 +21,8 @@ use Google\Site_Kit\Tests\TestCase;
 class DashboardTest extends TestCase {
 
 	public function test_register() {
-		global $wp_meta_boxes, $current_screen;
-		// Clear out any registered meta boxes
-		$wp_meta_boxes = array();
-		// Set the current screen to the dashboard
-		$current_screen = convert_to_screen( 'dashboard' );
-		$admin_id       = $this->factory()->user->create( array( 'role' => 'administrator' ) );
-		$dashboard      = new Dashboard( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
-		// Clear registered actions on hooks related to our assertions
-		remove_all_actions( 'wp_dashboard_setup' );
-
-		$dashboard->register();
-
-		// Bypass user authentication requirement in map_meta_cap
-		add_filter(
-			'map_meta_cap',
-			function ( $caps, $cap ) {
-				if ( Permissions::VIEW_WP_DASHBOARD_WIDGET === $cap ) {
-					$caps = array_filter(
-						$caps,
-						function ( $cap ) {
-							return 'do_not_allow' !== $cap;
-						}
-					);
-				}
-
-				return $caps;
-			},
-			10,
-			2
-		);
-
-		$this->assertFalse( current_user_can( Permissions::VIEW_WP_DASHBOARD_WIDGET ) );
-		wp_set_current_user( $admin_id );
-		$this->assertTrue( current_user_can( Permissions::VIEW_WP_DASHBOARD_WIDGET ) );
-
-		require_once ABSPATH . 'wp-admin/includes/dashboard.php';
-
-		$this->assertFalse( wp_style_is( 'googlesitekit-wp-dashboard-css', 'enqueued' ) );
-		$this->assertFalse( wp_script_is( 'googlesitekit-wp-dashboard', 'enqueued' ) );
-
-		wp_dashboard_setup();
+		global $wp_meta_boxes;
+		$this->get_widget_markup();
 
 		// Check that the dashboard widget was registered
 		$this->assertArrayHasKey( 'google_dashboard_widget', $wp_meta_boxes['dashboard']['normal']['core'] );
@@ -75,13 +36,7 @@ class DashboardTest extends TestCase {
 	 * @group dashboard_widget
 	 */
 	public function test_render_googlesitekit_wp_dashboard_loading_container() {
-		$dashboard = new Dashboard( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
-		$method    = new \ReflectionMethod( $dashboard, 'render_googlesitekit_wp_dashboard' );
-		$method->setAccessible( true );
-
-		ob_start();
-		$method->invoke( $dashboard );
-		$output = ob_get_clean();
+		$output = $this->get_widget_markup();
 
 		$dom = new \DOMDocument();
 		$dom->loadHTML( $output );
@@ -120,24 +75,7 @@ class DashboardTest extends TestCase {
 				)
 			);
 
-		$admin_id = $this->factory()->user->create( array( 'role' => 'administrator' ) );
-		wp_set_current_user( $admin_id );
-		$dashboard = new Dashboard( new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE ) );
-		$method    = new \ReflectionMethod( $dashboard, 'render_googlesitekit_wp_dashboard' );
-		$method->setAccessible( true );
-
-		$mockAuthentication = $this->getMockBuilder( \stdClass::class )
-			->addMethods( array( 'is_authenticated' ) )
-			->getMock();
-		$mockAuthentication->method( 'is_authenticated' )->willReturn( true );
-
-		$authentication = new \ReflectionProperty( $dashboard, 'authentication' );
-		$authentication->setAccessible( true );
-		$authentication->setValue( $dashboard, $mockAuthentication );
-
-		ob_start();
-		$method->invoke( $dashboard );
-		$output = ob_get_clean();
+		$output = $this->get_widget_markup();
 
 		$dom = new \DOMDocument();
 		$dom->loadHTML( $output );
@@ -153,11 +91,6 @@ class DashboardTest extends TestCase {
 		$query    = '//div[contains(concat(" ", normalize-space(@class), " "), " googlesitekit-wp-dashboard ") and contains(concat(" ", normalize-space(@class), " "), " googlesitekit-wp-dashboard-analytics_active_and_connected ")]';
 		$elements = $xpath->query( $query );
 		$this->assertEquals( 0, $elements->length );
-
-		// Ensure that "googlesitekit-preview-block googlesitekit-wp-dashboard-stats__cta" exists.
-		$query    = '//div[contains(concat(" ", normalize-space(@class), " "), " googlesitekit-preview-block ") and contains(concat(" ", normalize-space(@class), " "), " googlesitekit-wp-dashboard-stats__cta ")]';
-		$elements = $xpath->query( $query );
-		$this->assertEquals( 1, $elements->length );
 	}
 
 	/**
@@ -281,5 +214,49 @@ class DashboardTest extends TestCase {
 		$query    = '//div[contains(concat(" ", normalize-space(@class), " "), " googlesitekit-preview-block ") and contains(concat(" ", normalize-space(@class), " "), " googlesitekit-wp-dashboard-stats__cta ")]';
 		$elements = $xpath->query( $query );
 		$this->assertEquals( 0, $elements->length );
+	}
+
+	public function get_widget_markup() {
+		global $wp_meta_boxes, $current_screen;
+		// Clear out any registered meta boxes
+		$wp_meta_boxes = array();
+		// Set the current screen to the dashboard
+		$current_screen = convert_to_screen( 'dashboard' );
+		$context        = new Context( GOOGLESITEKIT_PLUGIN_MAIN_FILE );
+		$admin_id       = $this->factory()->user->create( array( 'role' => 'administrator' ) );
+		$dashboard      = new Dashboard( $context );
+
+		$dashboard->register();
+
+		// Bypass user authentication requirement in map_meta_cap
+		add_filter(
+			'map_meta_cap',
+			function ( $caps, $cap ) {
+				if ( Permissions::VIEW_WP_DASHBOARD_WIDGET === $cap ) {
+					$caps = array_filter(
+						$caps,
+						function ( $cap ) {
+							return 'do_not_allow' !== $cap;
+						}
+					);
+				}
+
+				return $caps;
+			},
+			10,
+			2
+		);
+
+		wp_set_current_user( $admin_id );
+
+		require_once ABSPATH . 'wp-admin/includes/dashboard.php';
+		wp_dashboard_setup();
+
+		ob_start();
+		$widget_callback = $wp_meta_boxes[ $current_screen->id ]['normal']['core']['google_dashboard_widget']['callback'];
+		$widget_callback();
+		$output = ob_get_clean();
+
+		return $output;
 	}
 }
