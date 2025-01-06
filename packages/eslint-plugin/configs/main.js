3fb6f47ac08c0a6fba97579a03c006e8c5e81bdc
Ben Bowler <ben@benbowler.com>
2025-01-06 08:31:01 +0000

    Update rule naming and message.

--- packages/eslint-plugin/configs/main.js
+++ packages/eslint-plugin/configs/main.js
@@ -28,6 +28,6 @@ module.exports = {
 		'sitekit/jsdoc-tag-order': [ 'error' ],
 		'sitekit/no-yield-dispatch': [ 'error' ],
 		'sitekit/no-direct-date': [ 'error' ],
-		'sitekit/storybook-no-label': [ 'error' ],
+		'sitekit/no-storybook-scenario-label': [ 'error' ],
 	},
 };
--- packages/eslint-plugin/rules/index.js
+++ packages/eslint-plugin/rules/index.js
@@ -27,5 +27,5 @@ module.exports = {
 	'jsdoc-third-person': require( './jsdoc-third-person' ),
 	'no-yield-dispatch': require( './no-yield-dispatch' ),
 	'no-direct-date': require( './no-direct-date' ),
-	'storybook-no-label': require( './storybook-no-label' ),
+	'no-storybook-scenario-label': require( './no-storybook-scenario-label' ),
 };
