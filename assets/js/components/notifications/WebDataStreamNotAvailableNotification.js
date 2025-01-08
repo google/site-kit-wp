80a86e87962a434c163ba974399cc960cc3f510d
Simon Dowdles <simon.dowdles@get10up.com>
2025-01-08 11:26:36 +0200

    Refactor WebDataStreamNotAvailableNotification component (WIP).

--- assets/js/components/notifications/WebDataStreamNotAvailableNotification.js
+++ assets/js/components/notifications/WebDataStreamNotAvailableNotification.js
@@ -24,18 +24,19 @@ import { __, sprintf } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { ProgressBar } from 'googlesitekit-components';
 import { useSelect } from 'googlesitekit-data';
-import BannerNotification from './BannerNotification';
+import SimpleNotification from '../../googlesitekit/notifications/components/layout/SimpleNotification';
 import { MODULES_ANALYTICS_4 } from '../../modules/analytics-4/datastore/constants';
 import { CORE_SITE } from '../../googlesitekit/datastore/site/constants';
 import { MINUTE_IN_SECONDS } from '../../util';
-import { Cell, Grid, Row } from '../../material-components';
+import Description from '../../googlesitekit/notifications/components/common/Description';
+import ActionsCTALinkDismiss from '../../googlesitekit/notifications/components/common/ActionsCTALinkDismiss';
 
-export default function WebDataStreamNotAvailableNotification() {
-	const isWebDataStreamAvailable = useSelect( ( select ) =>
-		select( MODULES_ANALYTICS_4 ).isWebDataStreamAvailable()
-	);
+export default function WebDataStreamNotAvailableNotification( {
+	id,
+	Notification,
+} ) {
+	const isWebDataStreamAvailable = false;
 
 	const measurementID = useSelect( ( select ) =>
 		select( MODULES_ANALYTICS_4 ).getMeasurementID()
@@ -50,7 +51,59 @@ export default function WebDataStreamNotAvailableNotification() {
 		return null;
 	}
 
-	if ( measurementID === undefined ) {
+	return (
+		<Notification className="googlesitekit-publisher-win googlesitekit-publisher-win--win-error">
+			<SimpleNotification
+				title={ __(
+					'Your Analytics configuration has changed',
+					'google-site-kit'
+				) }
+				description={
+					<Description
+						text={ sprintf(
+							/* translators: 1: Google Analytics Measurement ID. */
+							__(
+								'The previously selected web data stream with measurement ID %1$s is no longer available. Please select a new web data stream to continue collecting data with Google Analytics.',
+								'google-site-kit'
+							),
+							measurementID
+						) }
+					/>
+				}
+				ctaLink={ `${ settingsURL }#connected-services/analytics-4/edit` }
+				actions={
+					<div>
+						<ActionsCTALinkDismiss
+							id={ id }
+							ctaLabel={ __(
+								'Update Analytics settings',
+								'google-site-kit'
+							) }
+							ctaLink={ `${ settingsURL }#connected-services/analytics-4/edit` }
+							dismissLabel={ __(
+								'Maybe later',
+								'google-site-kit'
+							) }
+							dismissExpires={ MINUTE_IN_SECONDS * 55 }
+						/>
+					</div>
+				}
+			/>
+		</Notification>
+	);
+}
+
+/*
+ctaLink={ `${ settingsURL }#connected-services/analytics-4/edit` }
+			ctaLabel={ __( 'Update Analytics settings', 'google-site-kit' ) }
+			dismiss={ __( 'Maybe later', 'google-site-kit' ) }
+			// This is arbitrarily set to 55 minutes to ensure that the notification
+			// will become ready to be displayed again in an hour.
+			dismissExpires={ MINUTE_IN_SECONDS * 55 }
+ */
+
+/*
+* if ( measurementID === undefined ) {
 		// Wrap in the googlesitekit-publisher-win class to ensure the output is treated in the same way as BannerNotification,
 		// with only one instance visible on the screen at a time.
 		return (
@@ -64,29 +117,4 @@ export default function WebDataStreamNotAvailableNotification() {
 				</Grid>
 			</div>
 		);
-	}
-
-	return (
-		<BannerNotification
-			id="web-data-stream-not-available"
-			title={ __(
-				'Your Analytics configuration has changed',
-				'google-site-kit'
-			) }
-			description={ sprintf(
-				/* translators: 1: Google Analytics Measurement ID. */
-				__(
-					'The previously selected web data stream with measurement ID %1$s is no longer available. Please select a new web data stream to continue collecting data with Google Analytics.',
-					'google-site-kit'
-				),
-				measurementID
-			) }
-			ctaLink={ `${ settingsURL }#connected-services/analytics-4/edit` }
-			ctaLabel={ __( 'Update Analytics settings', 'google-site-kit' ) }
-			dismiss={ __( 'Maybe later', 'google-site-kit' ) }
-			// This is arbitrarily set to 55 minutes to ensure that the notification
-			// will become ready to be displayed again in an hour.
-			dismissExpires={ MINUTE_IN_SECONDS * 55 }
-		/>
-	);
-}
+	}*/
