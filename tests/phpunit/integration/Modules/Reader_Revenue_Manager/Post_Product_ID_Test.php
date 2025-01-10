492cede30fbc5c7b216f98abc7d01ee5e696d3ff
Ankit Gade <ankit.gade@10up.com>
2025-01-10 11:54:01 +0530

    Add test for Post_Product_ID class.

--- tests/phpunit/integration/Modules/Reader_Revenue_Manager/Post_Product_ID_Test.php
+++ tests/phpunit/integration/Modules/Reader_Revenue_Manager/Post_Product_ID_Test.php
@@ -7,3 +7,38 @@
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
+
+namespace Google\Site_Kit\Tests\Modules\Reader_Revenue_Manager;
+
+use Google\Site_Kit\Core\Storage\Post_Meta;
+use Google\Site_Kit\Modules\Reader_Revenue_Manager\Post_Product_ID;
+use Google\Site_Kit\Tests\TestCase;
+
+class Post_Product_ID_Test extends TestCase {
+
+	/**
+	 * @var Post_Product_ID
+	 */
+	private $setting;
+
+	public function set_up(): void {
+		parent::set_up();
+
+		$post_meta     = new Post_Meta();
+		$this->setting = new Post_Product_ID( $post_meta, 'test_publication_id' );
+		$this->setting->register();
+	}
+
+	public function test_product_id_meta_registered() {
+		$registered = registered_meta_key_exists( 'post', 'googlesitekit_rrm_test_publication_id:productID' );
+
+		$this->assertTrue( $registered );
+	}
+
+	public function test_show_in_rest() {
+		$meta_key     = 'googlesitekit_rrm_test_publication_id:productID';
+		$show_in_rest = get_registered_meta_keys( 'post' )[ $meta_key ]['show_in_rest'];
+
+		$this->assertTrue( $show_in_rest );
+	}
+}
