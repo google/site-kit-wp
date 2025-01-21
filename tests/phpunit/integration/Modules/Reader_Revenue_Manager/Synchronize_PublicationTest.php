3688680a2ccb76aa105c8a3c8da610724c597175
Ankit Gade <ankit.gade@10up.com>
2025-01-21 22:02:51 +0530

    Update doc block.

--- tests/phpunit/integration/Modules/Reader_Revenue_Manager/Synchronize_PublicationTest.php
+++ tests/phpunit/integration/Modules/Reader_Revenue_Manager/Synchronize_PublicationTest.php
@@ -3,7 +3,7 @@
  * Class Google\Site_Kit\Tests\Modules\Reader_Revenue_Manager\Synchronize_OnboardingStateTest
  *
  * @package   Google\Site_Kit\Tests\Modules\Reader_Revenue_Manager
- * @copyright 2024 Google LLC
+ * @copyright 2025 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
