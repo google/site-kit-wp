1f6a2c1d9c96bbfeb14fc9796ea0c664b7e3d1a1
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2025-01-28 11:35:02 +0000

    Add story for ChipMultiSelect.

---
+++ assets/js/components/ChipMultiSelect/ChipMultiSelect.stories.js
@@ -0,0 +1,46 @@
+/**
+ * ChipMultiSelect Component Stories.
+ *
+ * Site Kit by Google, Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import ChipMultiSelect from './ChipMultiSelect';
+import ChipMultiSelectItem from './ChipMultiSelectItem';
+
+function Template() {
+	return (
+		<ChipMultiSelect
+			onToggleChip={ ( id, isSelected ) => {
+				global.console.log( 'onToggleChip', id, isSelected );
+			} }
+		>
+			<ChipMultiSelectItem id="channels">Channels</ChipMultiSelectItem>
+			<ChipMultiSelectItem id="locations">Locations</ChipMultiSelectItem>
+			<ChipMultiSelectItem id="devices">Devices</ChipMultiSelectItem>
+		</ChipMultiSelect>
+	);
+}
+
+export const Default = Template.bind( {} );
+Default.storyName = 'Default';
+Default.scenario = {};
+
+export default {
+	title: 'Components/ChipMultiSelect',
+	component: ChipMultiSelect,
+};
