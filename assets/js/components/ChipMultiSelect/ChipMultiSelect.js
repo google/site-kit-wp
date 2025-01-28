54e0b75cdeccfba56822fc85a6d2f6fa08eccb13
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2025-01-28 11:33:30 +0000

    Add ChipMultiSelect and ChipMultiSelectItem components.

---
+++ assets/js/components/ChipMultiSelect/ChipMultiSelect.js
@@ -0,0 +1,50 @@
+/**
+ * ChipMultiSelect component.
+ *
+ * Site Kit by Google, Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { Provider } from './ChipMultiSelectContext';
+
+export default function ChipMultiSelect( {
+	children,
+	onToggleChip = () => {},
+} ) {
+	const [ selectedChips, setSelectedChips ] = useState( [] );
+
+	return (
+		<Provider value={ { selectedChips, setSelectedChips, onToggleChip } }>
+			{ children }
+		</Provider>
+	);
+}
+
+ChipMultiSelect.propTypes = {
+	children: PropTypes.node.isRequired,
+	onToggleChip: PropTypes.func,
+};
---
+++ assets/js/components/ChipMultiSelect/ChipMultiSelectItem.js
@@ -0,0 +1,64 @@
+/**
+ * ChipMultiSelectItem component.
+ *
+ * Site Kit by Google, Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { omit } from 'lodash';
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { useContext } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { Chip } from 'googlesitekit-components';
+import ChipMultiSelectContext from './ChipMultiSelectContext';
+
+export default function ChipMultiSelectItem( { children, id, ...props } ) {
+	const { selectedChips, setSelectedChips, onToggleChip } = useContext(
+		ChipMultiSelectContext
+	);
+
+	return (
+		<Chip
+			onClick={ () => {
+				const newSelectedChips = selectedChips.includes( id )
+					? selectedChips.filter( ( item ) => item !== id )
+					: [ ...selectedChips, id ];
+
+				setSelectedChips( newSelectedChips );
+
+				onToggleChip( id, newSelectedChips.includes( id ) );
+			} }
+			id={ id }
+			label={ children }
+			selected={ selectedChips.includes( id ) }
+			{ ...props }
+		/>
+	);
+}
+
+ChipMultiSelectItem.propTypes = {
+	children: PropTypes.string.isRequired,
+	id: PropTypes.string.isRequired,
+	...omit( Chip.propTypes, 'label' ),
+};
