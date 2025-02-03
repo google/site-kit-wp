f00d12f3257f8793fdc58d9ee8b10edebc121594
Aleksej <aleksej.vukomanovic@10up.com>
2025-02-03 10:29:39 +0100

    Fix comment header.

--- assets/js/modules/ads/datastore/moduleData.js
+++ assets/js/modules/ads/datastore/moduleData.js
@@ -1,7 +1,7 @@
 /**
- * `modules/adsense` data store: tags.
+ * `modules/ads` data store: module data.
  *
- * Site Kit by Google, Copyright 2021 Google LLC
+ * Site Kit by Google, Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
