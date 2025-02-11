9ab84a7e19c08de5d9c1bf159219eeaa1dae2417
Aleksej <aleksej.vukomanovic@10up.com>
2025-02-11 17:36:00 +0100

    Refactor basic structure.

--- assets/js/components/notifications/AdsModuleSetupCTAWidget.js
+++ assets/js/components/notifications/AdsModuleSetupCTAWidget.js
@@ -24,170 +24,82 @@ import PropTypes from 'prop-types';
 /**
  * WordPress dependencies
  */
-import { compose } from '@wordpress/compose';
-import { useCallback } from '@wordpress/element';
+// import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { useDispatch, useSelect } from 'googlesitekit-data';
-import { Button } from 'googlesitekit-components';
-import {
-	BREAKPOINT_SMALL,
-	BREAKPOINT_TABLET,
-	useBreakpoint,
-} from '../../hooks/useBreakpoint';
-import { ADS_MODULE_SETUP_BANNER_PROMPT_DISMISSED_KEY } from '../../modules/ads/datastore/constants';
-import { CORE_USER } from '../../googlesitekit/datastore/user/constants';
+// import { useDispatch, useSelect } from 'googlesitekit-data';
+// import { useBreakpoint } from '../../hooks/useBreakpoint';
 import { WEEK_IN_SECONDS } from '../../util';
-import { Cell, Grid, Row } from '../../material-components';
-import whenInactive from '../../util/when-inactive';
-import { withWidgetComponentProps } from '../../googlesitekit/widgets/util';
-import useActivateModuleCallback from '../../hooks/useActivateModuleCallback';
 import AdsSetupSVG from '../../../svg/graphics/ads-setup.svg';
-import AdsSetupTabletSVG from '../../../svg/graphics/ads-setup-tablet.svg';
-import AdsSetupMobileSVG from '../../../svg/graphics/ads-setup-mobile.svg';
+import NotificationWithSVG from '../../googlesitekit/notifications/components/layout/NotificationWithSVG';
+import Description from '../../googlesitekit/notifications/components/common/Description';
+import LearnMoreLink from '../../googlesitekit/notifications/components/common/LearnMoreLink';
+import ActionsCTALinkDismiss from '../../googlesitekit/notifications/components/common/ActionsCTALinkDismiss';
+import useActivateModuleCallback from '../../hooks/useActivateModuleCallback';
 
-function AdsModuleSetupCTAWidget( { WidgetNull, Widget } ) {
-	const breakpoint = useBreakpoint();
-	const isMobileBreakpoint = breakpoint === BREAKPOINT_SMALL;
-	const isTabletBreakpoint = breakpoint === BREAKPOINT_TABLET;
+export default function AdsModuleSetupCTAWidget( { id, Notification } ) {
+	// const breakpoint = useBreakpoint();
+	// const isMobileBreakpoint = breakpoint === BREAKPOINT_SMALL;
+	// const isTabletBreakpoint = breakpoint === BREAKPOINT_TABLET;
+
+	const learnMoreURL = undefined;
 
 	const onSetupCallback = useActivateModuleCallback( 'ads' );
 
-	const isDismissed = useSelect( ( select ) =>
-		select( CORE_USER ).isPromptDismissed(
-			ADS_MODULE_SETUP_BANNER_PROMPT_DISMISSED_KEY
-		)
-	);
-	const dismissCount = useSelect( ( select ) =>
-		select( CORE_USER ).getPromptDismissCount(
-			ADS_MODULE_SETUP_BANNER_PROMPT_DISMISSED_KEY
-		)
-	);
-
-	const { dismissPrompt } = useDispatch( CORE_USER );
-
-	const onDismiss = useCallback( async () => {
-		if ( dismissCount < 1 ) {
-			const twoWeeksInSeconds = WEEK_IN_SECONDS * 2;
-
-			await dismissPrompt( ADS_MODULE_SETUP_BANNER_PROMPT_DISMISSED_KEY, {
-				expiresInSeconds: twoWeeksInSeconds,
-			} );
-		} else {
-			// For the second dismissal, dismiss permanently.
-			await dismissPrompt( ADS_MODULE_SETUP_BANNER_PROMPT_DISMISSED_KEY );
-		}
-	}, [ dismissCount, dismissPrompt ] );
-
-	if ( dismissCount > 1 || isDismissed || isDismissed === undefined ) {
-		return <WidgetNull />;
-	}
-
 	return (
-		<div className="googlesitekit-widget-context">
-			<Grid className="googlesitekit-widget-area">
-				<Row>
-					<Cell size={ 12 }>
-						<Widget
-							noPadding
-							className="googlesitekit-setup-cta-banner googlesitekit-ads-setup-cta-widget"
-						>
-							<Grid collapsed>
-								<Row>
-									<Cell
-										smSize={ 12 }
-										mdSize={ 8 }
-										lgSize={ 6 }
-										className="googlesitekit-setup-cta-banner__primary-cell"
-									>
-										<h3 className="googlesitekit-setup-cta-banner__title">
-											{ __(
-												'Get better quality leads and enhance conversions with Ads',
-												'google-site-kit'
-											) }
-										</h3>
-										<div className="googlesitekit-setup-cta-banner__description">
-											<p>
-												{ __(
-													'Help drive sales, leads, or site traffic by getting your business in front of people who are actively searching Google for products or services you offer.',
-													'google-site-kit'
-												) }
-											</p>
-										</div>
-
-										<div className="googlesitekit-setup-cta-banner__actions-wrapper">
-											<Button
-												className="googlesitekit-key-metrics-cta-button"
-												onClick={ onSetupCallback }
-											>
-												{ __(
-													'Set up Ads',
-													'google-site-kit'
-												) }
-											</Button>
-											<Button
-												tertiary
-												onClick={ onDismiss }
-											>
-												{ dismissCount < 1
-													? __(
-															'Maybe later',
-															'google-site-kit'
-													  )
-													: __(
-															'Don’t show again',
-															'google-site-kit'
-													  ) }
-											</Button>
-										</div>
-									</Cell>
-									{ ! isMobileBreakpoint &&
-										! isTabletBreakpoint && (
-											<Cell
-												alignBottom
-												className="googlesitekit-setup-cta-banner__svg-wrapper"
-												mdSize={ 8 }
-												lgSize={ 6 }
-											>
-												<AdsSetupSVG />
-											</Cell>
-										) }
-									{ isTabletBreakpoint && (
-										<Cell
-											className="googlesitekit-setup-cta-banner__svg-wrapper"
-											mdSize={ 8 }
-										>
-											<AdsSetupTabletSVG />
-										</Cell>
-									) }
-									{ isMobileBreakpoint && (
-										<Cell
-											alignBottom
-											className="googlesitekit-setup-cta-banner__svg-wrapper"
-											smSize={ 12 }
-										>
-											<AdsSetupMobileSVG />
-										</Cell>
-									) }
-								</Row>
-							</Grid>
-						</Widget>
-					</Cell>
-				</Row>
-			</Grid>
-		</div>
+		<Notification>
+			<NotificationWithSVG
+				id={ id }
+				title={ __(
+					'Get better quality leads and enhance conversions with Ads',
+					'google-site-kit'
+				) }
+				description={
+					<Description
+						text={ __(
+							'Help drive sales, leads, or site traffic by getting your business in front of people who are actively searching Google for products or services you offer.',
+							'google-site-kit'
+						) }
+						learnMoreLink={
+							<LearnMoreLink
+								id={ id }
+								label={ __( 'Learn more', 'google-site-kit' ) }
+								url={ learnMoreURL }
+							/>
+						}
+					/>
+				}
+				actions={
+					<ActionsCTALinkDismiss
+						id={ id }
+						className="googlesitekit-setup-cta-banner__actions-wrapper"
+						ctaLabel={ __(
+							'Set up Reader Revenue Manager',
+							'google-site-kit'
+						) }
+						onCTAClick={ onSetupCallback }
+						// dismissLabel={
+						// 	isDismissalFinal
+						// 		? __( 'Don’t show again', 'google-site-kit' )
+						// 		: __( 'Maybe later', 'google-site-kit' )
+						// }
+						// onDismiss={ showTooltip }
+						dismissOptions={ {
+							skipHidingFromQueue: true,
+						} }
+						dismissExpires={ 2 * WEEK_IN_SECONDS }
+					/>
+				}
+				SVG={ AdsSetupSVG }
+			/>
+		</Notification>
 	);
 }
 
 AdsModuleSetupCTAWidget.propTypes = {
-	Widget: PropTypes.elementType.isRequired,
-	WidgetNull: PropTypes.elementType.isRequired,
+	id: PropTypes.string,
+	Notification: PropTypes.elementType,
 };
-
-export default compose(
-	whenInactive( { moduleName: 'ads' } ),
-	withWidgetComponentProps( 'ads-setup-cta' )
-)( AdsModuleSetupCTAWidget );
