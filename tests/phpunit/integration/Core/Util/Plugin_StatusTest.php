235742a9e2a34040b10560d771373a277fef8f2b
Simon Dowdles <simon.dowdles@get10up.com>
2025-02-12 15:01:18 +0200

    Add basic test coverage for Plugin_Status class.

---
+++ tests/phpunit/integration/Core/Util/Plugin_StatusTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Plugin_StatusTest
+ *
+ * @package   Google\Site_Kit\Tests\Core\Util
+ * @copyright 2025 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests\Core\Util;
+
+use Google\Site_Kit\Core\Util\Plugin_Status;
+use Google\Site_Kit\Tests\TestCase;
+
+/**
+ * @group Assets
+ */
+class Plugin_StatusTest extends TestCase {
+	/**
+	 * Initial active plugin state array.
+	 */
+	private $initial_active_plugins_state;
+
+	public function set_up() {
+		parent::set_up();
+
+		$this->initial_active_plugins_state = $GLOBALS['wp_tests_options']['active_plugins'];
+	}
+
+	public function tear_down() {
+		parent::tear_down();
+		$this->reset_plugins();
+	}
+
+	public function activate_plugin( $plugin_path = '' ) {
+		if ( empty( $plugin_path ) ) {
+			return;
+		}
+		$GLOBALS['wp_tests_options']['active_plugins'][] = $plugin_path;
+	}
+
+	public function deactivate_plugin( $plugin_path = '' ) {
+		if ( array_key_exists( $plugin_path, $GLOBALS['wp_tests_options']['active_plugins'] ) ) {
+			unset( $GLOBALS['wp_tests_options']['active_plugins'][ $plugin_path ] );
+		}
+	}
+
+	public function reset_plugins() {
+		$GLOBALS['wp_tests_options']['active_plugins'] = $this->initial_active_plugins_state;
+	}
+
+	public function test_default_response() {
+		$plugin_status = Plugin_Status::get_plugin_status();
+
+		$this->assertEquals( Plugin_Status::PLUGIN_STATUS_NOT_INSTALLED, $plugin_status );
+	}
+
+	public function test_response__plugin_active() {
+		$this->activate_plugin( 'foo/foo.php' );
+		$plugin_status = Plugin_Status::get_plugin_status( 'foo/foo.php' );
+		$this->deactivate_plugin( 'foo/foo.php' );
+
+		$this->assertEquals( Plugin_Status::PLUGIN_STATUS_ACTIVE, $plugin_status );
+	}
+}
