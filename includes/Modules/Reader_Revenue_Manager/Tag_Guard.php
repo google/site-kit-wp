c40b92411abca1601b71e2d8832a7cc44bc763e2
nfmohit <hello@nahid.dev>
2025-02-13 08:56:03 +0600

    Update tag placement logic.

--- includes/Modules/Reader_Revenue_Manager/Tag_Guard.php
+++ includes/Modules/Reader_Revenue_Manager/Tag_Guard.php
@@ -10,7 +10,9 @@
 
 namespace Google\Site_Kit\Modules\Reader_Revenue_Manager;
 
+use Google\Site_Kit\Core\Modules\Module_Settings;
 use Google\Site_Kit\Core\Modules\Tags\Module_Tag_Guard;
+use Google\Site_Kit\Core\Util\Feature_Flags;
 
 /**
  * Class for the Reader Revenue Manager tag guard.
@@ -22,6 +24,29 @@ use Google\Site_Kit\Core\Modules\Tags\Module_Tag_Guard;
 class Tag_Guard extends Module_Tag_Guard {
 
 	/**
+	 * Post product ID.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @var string
+	 */
+	private $post_product_id;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param Module_Settings $settings Module settings instance.
+	 * @param string          $post_product_id Post product ID.
+	 */
+	public function __construct( Module_Settings $settings, $post_product_id ) {
+		parent::__construct( $settings );
+
+		$this->post_product_id = $post_product_id;
+	}
+
+	/**
 	 * Determines whether the guarded tag can be activated or not.
 	 *
 	 * @since 1.132.0
@@ -30,6 +55,46 @@ class Tag_Guard extends Module_Tag_Guard {
 	 */
 	public function can_activate() {
 		$settings = $this->settings->get();
-		return ! empty( $settings['publicationID'] );
+
+		if ( empty( $settings['publicationID'] ) ) {
+			return false;
+		}
+
+		if ( Feature_Flags::enabled( 'rrmModuleV2' ) ) {
+			if ( is_singular() ) {
+				if ( 'none' === $this->post_product_id ) {
+					return false;
+				}
+
+				if (
+					empty( $this->post_product_id ) &&
+					'per_post' === $settings['snippetMode']
+				) {
+					return false;
+				}
+
+				/**
+				 * Filters the post types where Reader Revenue Manager CTAs should appear.
+				 *
+				 * @since 1.140.0
+				 *
+				 * @param array $cta_post_types The array of post types.
+				 */
+				$cta_post_types = apply_filters(
+					'googlesitekit_reader_revenue_manager_cta_post_types',
+					$settings['postTypes']
+				);
+
+				if (
+					'post_types' === $settings['snippetMode'] &&
+					! in_array( get_post_type(), $cta_post_types, true ) ) {
+					return false;
+				}
+			} elseif ( 'sitewide' !== $settings['snippetMode'] ) {
+					return false;
+			}
+		}
+
+		return true;
 	}
 }
