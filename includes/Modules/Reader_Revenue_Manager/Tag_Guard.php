b1bb4e0fd35a767783d8777741cefc82c37f8627
nfmohit <hello@nahid.dev>
2025-02-19 00:59:35 +0600

    Simplify logic in `Tag_Guard`.

--- includes/Modules/Reader_Revenue_Manager/Tag_Guard.php
+++ includes/Modules/Reader_Revenue_Manager/Tag_Guard.php
@@ -60,40 +60,60 @@ class Tag_Guard extends Module_Tag_Guard {
 			return false;
 		}
 
-		if ( Feature_Flags::enabled( 'rrmModuleV2' ) ) {
-			if ( is_singular() ) {
-				if ( 'none' === $this->post_product_id ) {
-					return false;
-				}
-
-				if ( empty( $this->post_product_id ) ) {
-					if ( 'per_post' === $settings['snippetMode'] ) {
-						return false;
-					}
-
-					/**
-					 * Filters the post types where Reader Revenue Manager CTAs should appear.
-					 *
-					 * @since 1.140.0
-					 *
-					 * @param array $cta_post_types The array of post types.
-					 */
-					$cta_post_types = apply_filters(
-						'googlesitekit_reader_revenue_manager_cta_post_types',
-						$settings['postTypes']
-					);
-
-					if (
-						'post_types' === $settings['snippetMode'] &&
-						! in_array( get_post_type(), $cta_post_types, true ) ) {
-						return false;
-					}
-				}
-			} elseif ( 'sitewide' !== $settings['snippetMode'] ) {
-					return false;
-			}
+		if ( ! Feature_Flags::enabled( 'rrmModuleV2' ) ) {
+			return true;
 		}
 
+		if ( is_singular() ) {
+			return $this->can_activate_for_singular_post();
+		}
+
+		return 'sitewide' === $settings['snippetMode'];
+	}
+
+	/**
+	 * Determines whether the guarded tag can be activated for a singular post or not.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return bool TRUE if guarded tag can be activated for a singular post, otherwise FALSE.
+	 */
+	private function can_activate_for_singular_post() {
+		if ( 'none' === $this->post_product_id ) {
+			return false;
+		}
+
+		if ( ! empty( $this->post_product_id ) ) {
+			return true;
+		}
+
+		$settings = $this->settings->get();
+
+		// If the snippet mode is `per_post` and there is no post product ID,
+		// we don't want to render the tag.
+		if ( 'per_post' === $settings['snippetMode'] ) {
+			return false;
+		}
+
+		// If the snippet mode is `post_types`, we only want to render the tag
+		// if the current post type is in the list of allowed post types.
+		if ( 'post_types' === $settings['snippetMode'] ) {
+			/**
+			 * Filters the post types where Reader Revenue Manager CTAs should appear.
+			 *
+			 * @since 1.140.0
+			 *
+			 * @param array $cta_post_types The array of post types.
+			 */
+			$cta_post_types = apply_filters(
+				'googlesitekit_reader_revenue_manager_cta_post_types',
+				$settings['postTypes']
+			);
+
+			return in_array( get_post_type(), $cta_post_types, true );
+		}
+
+		// Snippet mode is `sitewide` at this point, so we want to render the tag.
 		return true;
 	}
 }
