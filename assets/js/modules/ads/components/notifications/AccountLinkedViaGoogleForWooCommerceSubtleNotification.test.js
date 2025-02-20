f397fac80ee1fc24e9e404c1ae19686a578750fa
Aleksej <aleksej.vukomanovic@10up.com>
2025-02-20 21:36:13 +0100

    Expand tests for AccountLinkedViaGoogleForWooCommerceSubtleNotification.

--- assets/js/modules/ads/components/notifications/AccountLinkedViaGoogleForWooCommerceSubtleNotification.test.js
+++ assets/js/modules/ads/components/notifications/AccountLinkedViaGoogleForWooCommerceSubtleNotification.test.js
@@ -20,22 +20,110 @@
  * Internal dependencies
  */
 import { ADS_NOTIFICATIONS } from '../..';
+import { mockLocation } from '../../../../../../tests/js/mock-browser-utils';
+import { fireEvent, render } from '../../../../../../tests/js/test-utils';
 import {
 	createTestRegistry,
+	provideModuleRegistrations,
+	provideModules,
 	provideSiteInfo,
+	provideUserCapabilities,
 } from '../../../../../../tests/js/utils';
+import { enabledFeatures } from '../../../../features';
 import { VIEW_CONTEXT_MAIN_DASHBOARD } from '../../../../googlesitekit/constants';
+import { CORE_MODULES } from '../../../../googlesitekit/modules/datastore/constants';
+import { CORE_NOTIFICATIONS } from '../../../../googlesitekit/notifications/datastore/constants';
+import { withNotificationComponentProps } from '../../../../googlesitekit/notifications/util/component-props';
+import AccountLinkedViaGoogleForWooCommerceSubtleNotification from './AccountLinkedViaGoogleForWooCommerceSubtleNotification';
+
+const NOTIFICATION_ID = 'account-linked-via-google-for-woocommerce';
 
 describe( 'AccountLinkedViaGoogleForWooCommerceSubtleNotification.test', () => {
+	mockLocation();
+
 	let registry;
 
-	const notification =
-		ADS_NOTIFICATIONS[ 'account-linked-via-google-for-woocommerce' ];
+	const dismissItemEndpoint = RegExp(
+		'^/google-site-kit/v1/core/user/data/dismiss-item'
+	);
+
+	const notification = ADS_NOTIFICATIONS[ NOTIFICATION_ID ];
+
+	const AccountLinkedViaGoogleForWooCommerceSubtleNotificationComponent =
+		withNotificationComponentProps( NOTIFICATION_ID )(
+			AccountLinkedViaGoogleForWooCommerceSubtleNotification
+		);
 
 	beforeEach( () => {
 		registry = createTestRegistry();
 
+		enabledFeatures.add( 'adsPax' );
+
+		registry
+			.dispatch( CORE_NOTIFICATIONS )
+			.registerNotification( NOTIFICATION_ID, notification );
+
 		provideSiteInfo( registry );
+	} );
+
+	it( 'should start Ads module setup when tertiary CTA is clicked', async () => {
+		provideSiteInfo( registry );
+		provideModuleRegistrations( registry );
+		provideModules( registry );
+		provideUserCapabilities( registry );
+
+		fetchMock.postOnce(
+			RegExp( 'google-site-kit/v1/core/modules/data/activation' ),
+			{
+				body: { success: true },
+			}
+		);
+		fetchMock.getOnce(
+			RegExp( '^/google-site-kit/v1/core/user/data/authentication' ),
+			{
+				body: { needsReauthentication: false },
+			}
+		);
+		fetchMock.postOnce( dismissItemEndpoint, {
+			body: JSON.stringify( [ NOTIFICATION_ID ] ),
+		} );
+
+		const { getByText, waitForRegistry } = render(
+			<AccountLinkedViaGoogleForWooCommerceSubtleNotificationComponent />,
+			{ registry, viewContext: VIEW_CONTEXT_MAIN_DASHBOARD }
+		);
+
+		fireEvent.click( getByText( 'Create new account' ) );
+
+		await waitForRegistry();
+
+		expect(
+			registry.select( CORE_MODULES ).isDoingSetModuleActivation( 'ads' )
+		).toBe( true );
+
+		// Modal should be dismissed.
+		expect( fetchMock ).toHaveFetched( dismissItemEndpoint );
+	} );
+
+	it( 'should dismiss subtle notification when primary CTA is clicked', async () => {
+		provideModules( registry );
+		provideUserCapabilities( registry );
+
+		fetchMock.postOnce( dismissItemEndpoint, {
+			body: JSON.stringify( [ NOTIFICATION_ID ] ),
+		} );
+
+		const { getByText, waitForRegistry } = render(
+			<AccountLinkedViaGoogleForWooCommerceSubtleNotificationComponent />,
+			{ registry, viewContext: VIEW_CONTEXT_MAIN_DASHBOARD }
+		);
+
+		fireEvent.click( getByText( 'Keep existing account' ) );
+
+		await waitForRegistry();
+
+		// Modal should be dismissed.
+		expect( fetchMock ).toHaveFetched( dismissItemEndpoint );
 	} );
 
 	describe( 'checkRequirements', () => {
