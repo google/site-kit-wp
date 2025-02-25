c80432d92f0c37b822345f6126e9bbf47e83a197
Jimmy Madon <hello@jimmymadon.com>
2025-02-26 01:24:04 +0530

    Remove local storage cache dismissal for first setup use case.

--- assets/js/components/notifications/EnableAutoUpdateBannerNotification.js
+++ assets/js/components/notifications/EnableAutoUpdateBannerNotification.js
@@ -29,54 +29,30 @@ import { useSelect, useDispatch } from 'googlesitekit-data';
 import { CORE_SITE } from '../../googlesitekit/datastore/site/constants';
 import { MINUTE_IN_SECONDS } from '../../util';
 import useQueryArg from '../../hooks/useQueryArg';
-import {
-	CORE_USER,
-	PERMISSION_UPDATE_PLUGINS,
-} from '../../googlesitekit/datastore/user/constants';
-import { getItem, setItem } from '../../googlesitekit/api/cache';
 import SimpleNotification from '../../googlesitekit/notifications/components/layout/SimpleNotification';
 import Description from '../../googlesitekit/notifications/components/common/Description';
 import ActionsCTALinkDismiss from '../../googlesitekit/notifications/components/common/ActionsCTALinkDismiss';
 import Dismiss from '../../googlesitekit/notifications/components/common/Dismiss';
-
-const CACHE_KEY_HIDE_NOTIFICATION_ON_FIRST_SETUP =
-	'auto-update-banner-hide-notification-on-first-setup';
+import { useMount } from 'react-use';
+import { CORE_NOTIFICATIONS } from '../../googlesitekit/notifications/datastore/constants';
 
 export default function EnableAutoUpdateBannerNotification( {
 	id,
 	Notification,
 } ) {
-	const hasUpdatePluginCapacity = useSelect( ( select ) =>
-		select( CORE_USER ).hasCapability( PERMISSION_UPDATE_PLUGINS )
-	);
-	const hasChangePluginAutoUpdatesCapacity = useSelect( ( select ) =>
-		select( CORE_SITE ).hasChangePluginAutoUpdatesCapacity()
-	);
 	const siteKitAutoUpdatesEnabled = useSelect( ( select ) =>
 		select( CORE_SITE ).getSiteKitAutoUpdatesEnabled()
 	);
-
 	const enableAutoUpdateError = useSelect( ( select ) =>
 		select( CORE_SITE ).getErrorForAction( 'enableAutoUpdate', [] )
 	);
 
+	const { dismissNotification } = useDispatch( CORE_NOTIFICATIONS );
 	const { enableAutoUpdate } = useDispatch( CORE_SITE );
 
 	const [ notification ] = useQueryArg( 'notification' );
 
-	const [ isInitialPluginSetup, setIsFirstPluginSetup ] = useState( true );
-	const [ enabledViaCTA, setEnabledViaCTA ] = useState( undefined );
-
-	// Set the `enabledViaCTA` state to the value of `siteKitAutoUpdatesEnabled`,
-	// once `siteKitAutoUpdatesEnabled` is loaded.
-	useEffect( () => {
-		if (
-			enabledViaCTA === undefined &&
-			siteKitAutoUpdatesEnabled !== undefined
-		) {
-			setEnabledViaCTA( siteKitAutoUpdatesEnabled );
-		}
-	}, [ enabledViaCTA, siteKitAutoUpdatesEnabled ] );
+	const [ enabledViaCTA, setEnabledViaCTA ] = useState( false );
 
 	const ctaActivate = useCallback( async () => {
 		await enableAutoUpdate();
@@ -88,27 +64,6 @@ export default function EnableAutoUpdateBannerNotification( {
 		}
 	}, [ enabledViaCTA, siteKitAutoUpdatesEnabled ] );
 
-	const setFirstPluginSetup = useCallback(
-		async ( isFirstSetup ) => {
-			if ( isFirstSetup ) {
-				await setItem(
-					CACHE_KEY_HIDE_NOTIFICATION_ON_FIRST_SETUP,
-					true,
-					{
-						ttl: MINUTE_IN_SECONDS * 10,
-					}
-				);
-				setIsFirstPluginSetup( isFirstSetup );
-			} else {
-				const { value } = await getItem(
-					CACHE_KEY_HIDE_NOTIFICATION_ON_FIRST_SETUP
-				);
-				setIsFirstPluginSetup( !! value );
-			}
-		},
-		[ setIsFirstPluginSetup ]
-	);
-
 	/**
 	 * If the user just set up Site Kit (eg. just returned from the
 	 * initial OAuth sign-in flow) and is seeing the dashboard
@@ -116,39 +71,13 @@ export default function EnableAutoUpdateBannerNotification( {
 	 * minutes so they aren't immediately bothered by
 	 * CTA notifications.
 	 */
-	useEffect( () => {
-		if (
-			! hasUpdatePluginCapacity ||
-			! hasChangePluginAutoUpdatesCapacity ||
-			siteKitAutoUpdatesEnabled
-		) {
-			return;
+	useMount( () => {
+		if ( notification === 'authentication_success' ) {
+			dismissNotification( id, {
+				expiresInSeconds: MINUTE_IN_SECONDS * 10,
+			} );
 		}
-		setFirstPluginSetup( notification === 'authentication_success' );
-	}, [
-		notification,
-		hasUpdatePluginCapacity,
-		hasChangePluginAutoUpdatesCapacity,
-		setFirstPluginSetup,
-		siteKitAutoUpdatesEnabled,
-	] );
-
-	// Don't render anything if the user has no permission to update plugin,
-	// auto-updates can not be enabled for Site Kit, or auto update are already
-	// enabled for Site Kit.
-	if (
-		! hasUpdatePluginCapacity ||
-		! hasChangePluginAutoUpdatesCapacity ||
-		( siteKitAutoUpdatesEnabled && ! enabledViaCTA )
-	) {
-		return null;
-	}
-
-	// Don't show this banner if the user just came from the initial Site Kit
-	// setup flow less than 10 minutes ago.
-	if ( isInitialPluginSetup ) {
-		return null;
-	}
+	} );
 
 	// Render the Auto Updates enabled successfully banner.
 	if ( enabledViaCTA ) {
