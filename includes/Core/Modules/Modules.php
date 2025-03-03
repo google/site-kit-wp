8923c90abbf13ab522f7ea7bc3fc2bf1c9481b9d
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2025-03-03 16:04:44 +0000

    Add SiwG to core modules list.

--- includes/Core/Modules/Modules.php
+++ includes/Core/Modules/Modules.php
@@ -145,13 +145,14 @@ final class Modules {
 	 * @var string[] Core module class names.
 	 */
 	private $core_modules = array(
-		Site_Verification::MODULE_SLUG  => Site_Verification::class,
-		Search_Console::MODULE_SLUG     => Search_Console::class,
-		Ads::MODULE_SLUG                => Ads::class,
-		Analytics_4::MODULE_SLUG        => Analytics_4::class,
-		Tag_Manager::MODULE_SLUG        => Tag_Manager::class,
-		AdSense::MODULE_SLUG            => AdSense::class,
-		PageSpeed_Insights::MODULE_SLUG => PageSpeed_Insights::class,
+		Site_Verification::MODULE_SLUG   => Site_Verification::class,
+		Search_Console::MODULE_SLUG      => Search_Console::class,
+		Ads::MODULE_SLUG                 => Ads::class,
+		Analytics_4::MODULE_SLUG         => Analytics_4::class,
+		Tag_Manager::MODULE_SLUG         => Tag_Manager::class,
+		AdSense::MODULE_SLUG             => AdSense::class,
+		PageSpeed_Insights::MODULE_SLUG  => PageSpeed_Insights::class,
+		Sign_In_With_Google::MODULE_SLUG => Sign_In_With_Google::class,
 	);
 
 	/**
@@ -182,8 +183,6 @@ final class Modules {
 		if ( Feature_Flags::enabled( 'rrmModule' ) ) {
 			$this->core_modules[ Reader_Revenue_Manager::MODULE_SLUG ] = Reader_Revenue_Manager::class;
 		}
-
-		$this->core_modules[ Sign_In_With_Google::MODULE_SLUG ] = Sign_In_With_Google::class;
 
 		$this->rest_controller              = new REST_Modules_Controller( $this );
 		$this->dashboard_sharing_controller = new REST_Dashboard_Sharing_Controller( $this );
