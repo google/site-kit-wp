9e7ee164d8785ad74fdb2006fe7f20feb04956ca
Aleksej <aleksej.vukomanovic@10up.com>
2025-03-03 21:14:37 +0100

    Revert resourceAvailabilityDates.

--- assets/js/modules/analytics-4/datastore/module-data.js
+++ assets/js/modules/analytics-4/datastore/module-data.js
@@ -42,7 +42,6 @@ export const initialState = {
 		lostEvents: undefined,
 		newBadgeEvents: undefined,
 		hasMismatchedTag: undefined,
-		resourceAvailabilityDates: undefined,
 	},
 };
 
@@ -69,24 +68,11 @@ export const actions = {
 export const reducer = createReducer( ( state, { payload, type } ) => {
 	switch ( type ) {
 		case RECEIVE_MODULE_DATA: {
-			const {
-				tagIDMismatch,
-				resourceAvailabilityDates,
-				newEvents,
-				lostEvents,
-				newBadgeEvents,
-			} = payload;
-
-			// Replace empty array value with empty object in resourceAvailabilityDates object.
-			Object.keys( resourceAvailabilityDates ).forEach( ( key ) => {
-				if ( Array.isArray( resourceAvailabilityDates[ key ] ) ) {
-					resourceAvailabilityDates[ key ] = {};
-				}
-			} );
+			const { tagIDMismatch, newEvents, lostEvents, newBadgeEvents } =
+				payload;
 
 			const moduleData = {
 				hasMismatchedTag: !! tagIDMismatch,
-				resourceAvailabilityDates,
 				newEvents,
 				lostEvents,
 				newBadgeEvents,
@@ -141,19 +127,6 @@ export const selectors = {
 	 * @return {boolean} If GA4 has mismatched Google Tag ID.
 	 */
 	hasMismatchedGoogleTagID: getModuleDataProperty( 'hasMismatchedTag' ),
-
-	/**
-	 * Gets the data availability date for all resources.
-	 *
-	 * @since 1.127.0
-	 * @since n.e.x.t Moved over from partial-data store partial.
-	 *
-	 * @param {Object} state Data store's state.
-	 * @return {Object} Resource data availability dates. Undefined if not loaded.
-	 */
-	getResourceDataAvailabilityDates: getModuleDataProperty(
-		'resourceAvailabilityDates'
-	),
 
 	/**
 	 * Gets new events data.
