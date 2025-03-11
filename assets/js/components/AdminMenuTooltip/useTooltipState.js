4ff48b963db3ed994ef384dc83324428c07880e7
Jimmy Madon <hello@jimmymadon.com>
2025-03-11 20:10:15 +0530

    Update fetching tooltip state using fixed tooltip key.

--- assets/js/components/AdminMenuTooltip/useTooltipState.js
+++ assets/js/components/AdminMenuTooltip/useTooltipState.js
@@ -1,10 +1,10 @@
 import { useSelect } from 'googlesitekit-data';
 import { CORE_UI } from '../../googlesitekit/datastore/ui/constants';
 
-export function useTooltipState( tooltipStateKey ) {
+export function useTooltipState() {
 	return useSelect(
 		( select ) =>
-			select( CORE_UI ).getValue( tooltipStateKey ) || {
+			select( CORE_UI ).getValue( 'admin-menu-tooltip' ) || {
 				isTooltipVisible: false,
 				rehideAdminMenu: false,
 				rehideAdminSubMenu: false,
