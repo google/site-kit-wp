1473b0162210c5ef311c2bd79802aa9f18e31644
Simon Dowdles <simon.dowdles@get10up.com>
2025-03-18 11:16:28 +0200

    Define new disabled prop on CTALink and forward to SpinnerButton component disabled prop.

--- assets/js/googlesitekit/notifications/components/common/CTALink.js
+++ assets/js/googlesitekit/notifications/components/common/CTALink.js
@@ -45,6 +45,7 @@ export default function CTALink( {
 	dismissExpires = 0,
 	dismissOptions = { skipHidingFromQueue: true },
 	gaTrackingEventArgs,
+	disabled = false,
 } ) {
 	const [ isAwaitingCTAResponse, setIsAwaitingCTAResponse ] =
 		useState( false );
@@ -110,7 +111,9 @@ export default function CTALink( {
 			className="googlesitekit-notification__cta"
 			href={ ctaLink }
 			onClick={ handleCTAClick }
-			disabled={ isAwaitingCTAResponse || isNavigatingToCTALink }
+			disabled={
+				isAwaitingCTAResponse || isNavigatingToCTALink || disabled
+			}
 			isSaving={
 				isAwaitingCTAResponse || isNavigatingToCTALink || isSaving
 			}
