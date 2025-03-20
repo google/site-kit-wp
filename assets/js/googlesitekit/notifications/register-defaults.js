fdfa61bea2c01c2cd3e95c97a6b7d07ec46cd1a7
Junio Vitorino <jgvitorino@gmail.com>
2025-03-20 07:54:19 -0300

    Replace isAdsConnected by isAdsConnectedUncached.

--- assets/js/googlesitekit/notifications/register-defaults.js
+++ assets/js/googlesitekit/notifications/register-defaults.js
@@ -609,7 +609,7 @@ export const DEFAULT_NOTIFICATIONS = {
 				return false;
 			}
 
-			return resolveSelect( CORE_SITE ).isAdsConnected();
+			return resolveSelect( CORE_SITE ).isAdsConnectedUncached();
 		},
 		dismissRetries: 2,
 	},
