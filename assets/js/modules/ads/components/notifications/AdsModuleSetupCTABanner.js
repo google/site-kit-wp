50cab19f2a6919e24aa42c2b2ba0265191474b27
Simon Dowdles <simon.dowdles@get10up.com>
2025-03-24 13:49:19 +0200

    Add state and useEffect implementation to render correct maybe later or final dismissal text on secondary cta.

--- assets/js/modules/ads/components/notifications/AdsModuleSetupCTABanner.js
+++ assets/js/modules/ads/components/notifications/AdsModuleSetupCTABanner.js
@@ -24,7 +24,7 @@ import PropTypes from 'prop-types';
 /**
  * WordPress dependencies
  */
-import { useCallback, useState } from '@wordpress/element';
+import { useCallback, useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -154,6 +154,16 @@ export default function AdsModuleSetupCTABanner( { id, Notification } ) {
 	const showTooltip = useShowTooltip( id );
 	const { isTooltipVisible } = useTooltipState( id );
 
+	const [ dismissLabel, setDismissLabel ] = useState(
+		__( 'Maybe later', 'google-site-kit' )
+	);
+
+	useEffect( () => {
+		if ( true === isDismissalFinal ) {
+			setDismissLabel( __( 'Don’t show again', 'google-site-kit' ) );
+		}
+	}, [ isDismissalFinal ] );
+
 	if ( isTooltipVisible ) {
 		return (
 			<AdminMenuTooltip
@@ -213,11 +223,7 @@ export default function AdsModuleSetupCTABanner( { id, Notification } ) {
 						onCTAClick={ onSetupCallback }
 						dismissOnCTAClick={ false }
 						isSaving={ isSaving }
-						dismissLabel={
-							isDismissalFinal
-								? __( 'Don’t show again', 'google-site-kit' )
-								: __( 'Maybe later', 'google-site-kit' )
-						}
+						dismissLabel={ dismissLabel }
 						dismissOptions={ {
 							skipHidingFromQueue: true,
 						} }
