2e6d1057d974953b85fe6f138a06bbd1898b494b
Jimmy Madon <hello@jimmymadon.com>
2025-04-02 13:20:24 +0100

    Ensure PSI Success Notice is dismissed when clicking on scroll to link.

--- assets/js/modules/pagespeed-insights/components/notifications/SetupSuccessNotification.js
+++ assets/js/modules/pagespeed-insights/components/notifications/SetupSuccessNotification.js
@@ -29,6 +29,8 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import { useDispatch } from 'googlesitekit-data';
+import { CORE_NOTIFICATIONS } from '../../../../googlesitekit/notifications/datastore/constants';
 import SubtleNotification from '../../../../googlesitekit/notifications/components/layout/SubtleNotification';
 import CTALinkSubtle from '../../../../googlesitekit/notifications/components/common/CTALinkSubtle';
 import Dismiss from '../../../../googlesitekit/notifications/components/common/Dismiss';
@@ -43,9 +45,14 @@ export default function SetupSuccessNotification( { id, Notification } ) {
 	const [ , setNotification ] = useQueryArg( 'notification' );
 	const [ , setSlug ] = useQueryArg( 'slug' );
 
+	const { dismissNotification } = useDispatch( CORE_NOTIFICATIONS );
+
 	const anchorLink = `#${ ANCHOR_ID_SPEED }`;
 	const onJumpLinkClick = ( event ) => {
 		event.preventDefault();
+
+		dismissNotification( id );
+		onDismiss();
 
 		global.history.replaceState( {}, '', anchorLink );
 		global.scrollTo( {
