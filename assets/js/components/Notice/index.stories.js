b21a7499202323810b8e690ae6b9413284be69a5
Aleksej <aleksej.vukomanovic@10up.com>
2025-04-04 11:26:59 +0200

    Add stories file with 1st notice.

---
+++ assets/js/components/Notice/index.stories.js
@@ -0,0 +1,66 @@
+/**
+ * Notice stories.
+ *
+ * Site Kit by Google, Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import { Cell, Grid, Row } from '../../material-components';
+import Notice from '.';
+
+function Template() {
+	return (
+		<Grid className="googlesitekit-widget-area googlesitekit-widget-area--boxes">
+			<Row className="googlesitekit-widget-area-widgets">
+				<Cell size={ 12 }>
+					<h1>Notice</h1>
+				</Cell>
+				<Cell size={ 12 }>
+					<Notice
+						title="Notice Title"
+						description="Notice Description"
+						dismissButton={ {
+							label: 'Got it',
+							onClick: () => {},
+						} }
+						ctaButton={ {
+							label: 'Take action',
+							onClick: () => {},
+						} }
+						type="success"
+					/>
+				</Cell>
+			</Row>
+		</Grid>
+	);
+}
+
+export const DefaultNotice = Template.bind( {} );
+DefaultNotice.scenario = {};
+
+export default {
+	title: 'Notice',
+	decorators: [
+		( Story ) => {
+			return (
+				<div className="googlesitekit-story--notice">
+					<Story />
+				</div>
+			);
+		},
+	],
+};
