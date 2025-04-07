2ddcce4d03392ae6fc92937175dc4d82be42903d
Hussain Thajutheen <hussain.thajutheen@10up.com>
2025-04-07 19:57:04 +0300

    Add render widget test for EngagedTrafficSourceWidget.

---
+++ assets/js/modules/analytics-4/components/widgets/EngagedTrafficSourceWidget.test.js
@@ -0,0 +1,70 @@
+/**
+ * EngagedTrafficSourceWidget component tests.
+ *
+ * Site Kit by Google, Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import { render } from '../../../../../../tests/js/test-utils';
+import {
+	createTestRegistry,
+	provideKeyMetrics,
+	provideModules,
+} from '../../../../../../tests/js/utils';
+import { provideAnalytics4MockReport } from '../../utils/data-mock';
+import { getWidgetComponentProps } from '../../../../googlesitekit/widgets/util';
+import {
+	CORE_USER,
+	KM_ANALYTICS_ENGAGED_TRAFFIC_SOURCE,
+} from '../../../../googlesitekit/datastore/user/constants';
+import EngagedTrafficSourceWidget from './EngagedTrafficSourceWidget';
+import { withConnected } from '../../../../googlesitekit/modules/datastore/__fixtures__';
+import { DATE_RANGE_OFFSET } from '../../datastore/constants';
+
+describe( 'EngagedTrafficSourceWidget', () => {
+	const { Widget, WidgetNull } = getWidgetComponentProps(
+		KM_ANALYTICS_ENGAGED_TRAFFIC_SOURCE
+	);
+
+	it( 'should render correctly with the expected metrics', async () => {
+		const registry = createTestRegistry();
+		registry.dispatch( CORE_USER ).setReferenceDate( '2020-09-08' );
+		provideKeyMetrics( registry );
+		provideModules( registry, withConnected( 'analytics-4' ) );
+		provideAnalytics4MockReport( registry, {
+			...registry.select( CORE_USER ).getDateRangeDates( {
+				offsetDays: DATE_RANGE_OFFSET,
+				compare: true,
+			} ),
+			dimensions: [ 'sessionDefaultChannelGroup' ],
+			metrics: [ { name: 'engagedSessions' } ],
+			orderBy: 'engagedSessions',
+			limit: 1,
+		} );
+
+		const { container, waitForRegistry } = render(
+			<EngagedTrafficSourceWidget
+				Widget={ Widget }
+				WidgetNull={ WidgetNull }
+			/>,
+			{ registry }
+		);
+		await waitForRegistry();
+
+		expect( container ).toMatchSnapshot();
+	} );
+} );
