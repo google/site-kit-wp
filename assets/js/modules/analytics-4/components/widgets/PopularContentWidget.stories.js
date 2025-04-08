404c9a915f174013fb7901ac451ee5e585c037b4
Simon Dowdles <simon.dowdles@get10up.com>
2025-04-08 15:46:34 +0200

    Remove no longer required scenario objects or their label properties.

--- assets/js/modules/analytics-4/components/widgets/PopularContentWidget.stories.js
+++ assets/js/modules/analytics-4/components/widgets/PopularContentWidget.stories.js
@@ -107,8 +107,6 @@ Ready.args = {
 	},
 };
 Ready.scenario = {
-	// eslint-disable-next-line sitekit/no-storybook-scenario-label
-	label: 'KeyMetrics/PopularContentWidget/Ready',
 	delay: 250,
 };
 
@@ -132,8 +130,6 @@ ReadyViewOnly.args = {
 	viewContext: VIEW_CONTEXT_MAIN_DASHBOARD_VIEW_ONLY,
 };
 ReadyViewOnly.scenario = {
-	// eslint-disable-next-line sitekit/no-storybook-scenario-label
-	label: 'KeyMetrics/PopularContentWidget/ReadyViewOnly',
 	delay: 250,
 };
 
