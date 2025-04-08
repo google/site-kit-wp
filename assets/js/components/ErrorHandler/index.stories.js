24c73da8ecf78f827a2dd425d6215ad555c07c6d
Simon Dowdles <simon.dowdles@get10up.com>
2025-04-08 12:11:10 +0200

    Remove scenario label.

--- assets/js/components/ErrorHandler/index.stories.js
+++ assets/js/components/ErrorHandler/index.stories.js
@@ -32,9 +32,6 @@ function Template() {
 
 export const Default = Template.bind( {} );
 Default.storyName = 'Default';
-Default.scenario = {
-	label: 'Global/ErrorHandler',
-};
 
 export default {
 	title: 'Components/ErrorHandler',
