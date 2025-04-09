d25e5b7d4c142f418420966b7066eeb6f2f37120
Simon Dowdles <simon.dowdles@get10up.com>
2025-04-09 14:25:28 +0200

    Remove no longer required scenario objects or their label properties.

--- assets/js/components/notifications/BannerNotification/index.stories.js
+++ assets/js/components/notifications/BannerNotification/index.stories.js
@@ -36,9 +36,6 @@ WithImage.args = {
 	SmallImageSVG: AwardSVG,
 	type: 'win-success',
 };
-WithImage.scenario = {
-	label: 'Components/BannerNotification',
-};
 
 export const WithNoImage = Template.bind( {} );
 WithNoImage.storyName = 'With No Image';
