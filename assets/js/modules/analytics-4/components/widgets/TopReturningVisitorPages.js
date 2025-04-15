7ef9f3f903c4d8c80ae1bf5539a88f68275f7cee
Simon Dowdles <simon.dowdles@get10up.com>
2025-04-15 08:58:05 +0200

    Decode ampersands in row output of TopReturningVisitorPages component.

--- assets/js/modules/analytics-4/components/widgets/TopReturningVisitorPages.js
+++ assets/js/modules/analytics-4/components/widgets/TopReturningVisitorPages.js
@@ -43,6 +43,7 @@ import { numFmt } from '../../../../util';
 import whenActive from '../../../../util/when-active';
 import ConnectGA4CTATileWidget from './ConnectGA4CTATileWidget';
 import useViewOnly from '../../../../hooks/useViewOnly';
+import { decodeAmpersand } from '../../utils';
 
 function TopReturningVisitorPages( props ) {
 	const { Widget } = props;
@@ -109,7 +110,7 @@ function TopReturningVisitorPages( props ) {
 			field: 'dimensionValues.0.value',
 			Component( { fieldValue } ) {
 				const url = fieldValue;
-				const title = titles[ url ].replace( /&amp;/gi, '&' );
+				const title = decodeAmpersand( titles[ url ] );
 				// Utilizing `useSelect` inside the component rather than
 				// returning its direct value to the `columns` array.
 				// This pattern ensures that the component re-renders correctly based on changes in state,
