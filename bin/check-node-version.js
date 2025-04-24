6af3723756a22e000000f1f90d89e68931f0d63c
nfmohit <hello@nahid.dev>
2025-04-24 12:19:06 +0200

    Add utility function.

--- bin/check-node-version.js
+++ bin/check-node-version.js
@@ -5,19 +5,29 @@ const { execSync } = require( 'child_process' );
 const fs = require( 'fs' );
 const path = require( 'path' );
 
+const getMajorVersion = ( version ) => {
+	const majorVersion = version.split( '.' )[ 0 ];
+
+	if ( majorVersion === undefined ) {
+		throw new Error( `Unable to parse version: ${version}` );
+	}
+
+	return parseInt( majorVersion );
+}
+
 const currentNodeVersion = process.versions.node;
-const currentMajorNodeVersion = parseInt( currentNodeVersion.split( '.' )[ 0 ] );
+const currentMajorNodeVersion = getMajorVersion( currentNodeVersion );
 
 const nvmrcPath = path.join( __dirname, '../.nvmrc' );
 const expectedNodeVersion = fs.readFileSync( nvmrcPath, 'utf8' ).trim();
-const expectedMajorNodeVersion = parseInt( expectedNodeVersion.split( '.' )[ 0 ] );
+const expectedMajorNodeVersion = getMajorVersion( expectedNodeVersion );
 
 const currentNPMVersion = execSync( 'npm -v' ).toString().trim();
-const currentMajorNPMVersion = parseInt( currentNPMVersion.split( '.' )[ 0 ] );
+const currentMajorNPMVersion = getMajorVersion( currentNPMVersion );
 
 const packageFile = require( '../package.json' );
 const expectedNPMVersion = packageFile.scripts[ 'install-global-npm' ].split( '@' )[ 1 ];
-const expectedMajorNPMVersion = parseInt( expectedNPMVersion.split( '.' )[ 0 ] );
+const expectedMajorNPMVersion = getMajorVersion( expectedNPMVersion );
 
 if ( currentMajorNodeVersion !== expectedMajorNodeVersion ) {
 	console.error(
