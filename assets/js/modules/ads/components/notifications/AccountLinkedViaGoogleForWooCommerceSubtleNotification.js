3792cf092fb31e1d0c709c7495a726aa9123f1c2
Simon Dowdles <simon.dowdles@get10up.com>
2025-04-28 09:57:15 +0200

    Remove reverseCtas prop.

--- assets/js/modules/ads/components/notifications/AccountLinkedViaGoogleForWooCommerceSubtleNotification.js
+++ assets/js/modules/ads/components/notifications/AccountLinkedViaGoogleForWooCommerceSubtleNotification.js
@@ -87,7 +87,6 @@ export default function AccountLinkedViaGoogleForWooCommerceSubtleNotification( 
 					onClick: dismissWooCommerceRedirectModal,
 					disabled: isSaving,
 				} }
-				reverseCTAs
 			/>
 		</Notification>
 	);
