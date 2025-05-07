3c0c236591445c3a314686501b497c5060c50b98
Simon Dowdles <simon.dowdles@get10up.com>
2025-05-07 15:53:50 +0200

    Refactor AudienceCreationNotice so that styling is more inline with prior implementation of SubtleNotification.

--- assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceCreationNotice.js
+++ assets/js/modules/analytics-4/components/audience-segmentation/dashboard/AudienceSelectionPanel/AudienceCreationNotice.js
@@ -330,20 +330,18 @@ export default function AudienceCreationNotice() {
 					) ) }
 			</div>
 			{ ! hasAnalytics4EditScope && ! isEditScopeNoticeDismissed && (
-				<div className="googlesitekit-audience-selection-panel__audience-creation-notice-info">
-					<Notice
-						type="warning"
-						title={ __(
-							'Creating these groups require more data tracking. You will be directed to update your Analytics property.',
-							'google-site-kit'
-						) }
-						dismissButton={ {
-							label: __( 'Got it', 'google-site-kit' ),
-							onClick: handleDismissEditScopeNotice,
-						} }
-						hideIcon
-					/>
-				</div>
+				<Notice
+					type="warning"
+					description={ __(
+						'Creating these groups require more data tracking. You will be directed to update your Analytics property.',
+						'google-site-kit'
+					) }
+					dismissButton={ {
+						label: __( 'Got it', 'google-site-kit' ),
+						onClick: handleDismissEditScopeNotice,
+					} }
+					hideIcon
+				/>
 			) }
 			{ ( apiErrors.length > 0 || hasOAuthError ) && (
 				<AudienceCreationErrorNotice
