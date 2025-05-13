e0b79bb1287225590c493f428ec7c85e6bb6526b
Hussain Thajutheen <hussain.thajutheen@10up.com>
2025-05-13 13:25:26 +0300

    Retain comments.

--- packages/eslint-plugin/rules/acronym-case.js
+++ packages/eslint-plugin/rules/acronym-case.js
@@ -126,6 +126,7 @@ module.exports = {
 			Identifier( node ) {
 				const name = node.name;
 
+				// Ignore imports, because they may not respect our rules.
 				if ( isImported( node ) ) {
 					importedNames.push( name );
 					return;
@@ -137,6 +138,8 @@ module.exports = {
 					return;
 				}
 
+				// Ignore known, JS globals like `document` and `window`.
+				// `document.getElementById` should not set off this rule.
 				if (
 					node.parent &&
 					node.parent.object &&
