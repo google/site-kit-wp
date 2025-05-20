fc40bfeb40fc8dddc016b93de6458a3a048dddcb
Aleksej <aleksej.vukomanovic@10up.com>
2025-05-20 10:04:57 +0200

    Add assertion fail messages in the Woo tests.

--- tests/phpunit/integration/Core/Conversion_Tracking/Conversion_Event_Providers/WooCommerceTest.php
+++ tests/phpunit/integration/Core/Conversion_Tracking/Conversion_Event_Providers/WooCommerceTest.php
@@ -72,27 +72,27 @@ class WooCommerceTest extends TestCase {
 	public function test_get_event_names() {
 		$events = $this->woocommerce->get_event_names();
 		$this->assertCount( 2, $events );
-		$this->assertEquals( 'add_to_cart', $events[0] );
-		$this->assertEquals( 'purchase', $events[1] );
+		$this->assertEquals( 'add_to_cart', $events[0], 'Expected add_to_cart event, got ' . $events[0] );
+		$this->assertEquals( 'purchase', $events[1], 'Expected purchase event, got ' . $events[1] );
 	}
 
 	public function test_register_script() {
 		$handle = 'googlesitekit-events-provider-' . WooCommerce::CONVERSION_EVENT_PROVIDER_SLUG;
-		$this->assertFalse( wp_script_is( $handle, 'registered' ) );
+		$this->assertFalse( wp_script_is( $handle, 'registered' ), 'Expected script to not be registered.' );
 
 		$script = $this->woocommerce->register_script();
-		$this->assertInstanceOf( Script::class, $script );
-		$this->assertTrue( wp_script_is( $handle, 'registered' ) );
+		$this->assertInstanceOf( Script::class, $script, 'Expected script to be an instance of Script.' );
+		$this->assertTrue( wp_script_is( $handle, 'registered' ), 'Expected script to be registered.' );
 	}
 
 	public function test_register_hooks() {
-		$this->assertFalse( has_action( 'woocommerce_thankyou' ) );
-		$this->assertFalse( has_action( 'woocommerce_add_to_cart' ) );
-		$this->assertFalse( has_filter( 'woocommerce_loop_add_to_cart_link' ) );
+		$this->assertFalse( has_action( 'woocommerce_thankyou' ), 'Expected woocommerce_thankyou action to not be registered.' );
+		$this->assertFalse( has_action( 'woocommerce_add_to_cart' ), 'Expected woocommerce_add_to_cart action to not be registered.' );
+		$this->assertFalse( has_filter( 'woocommerce_loop_add_to_cart_link' ), 'Expected woocommerce_loop_add_to_cart_link filter to not be registered.' );
 
 		$this->woocommerce->register_hooks();
-		$this->assertTrue( has_action( 'woocommerce_thankyou' ) );
-		$this->assertTrue( has_action( 'woocommerce_add_to_cart' ) );
-		$this->assertTrue( has_filter( 'woocommerce_loop_add_to_cart_link' ) );
+		$this->assertTrue( has_action( 'woocommerce_thankyou' ), 'Expected woocommerce_thankyou action to be registered.' );
+		$this->assertTrue( has_action( 'woocommerce_add_to_cart' ), 'Expected woocommerce_add_to_cart action to be registered.' );
+		$this->assertTrue( has_filter( 'woocommerce_loop_add_to_cart_link' ), 'Expected woocommerce_loop_add_to_cart_link filter to be registered.' );
 	}
 }
