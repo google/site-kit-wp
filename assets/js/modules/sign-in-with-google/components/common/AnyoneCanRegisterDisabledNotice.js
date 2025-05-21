f684d6f9a3fe2be9dcf9f5cf3e618d721b85fe17
Simon Dowdles <simon.dowdles@get10up.com>
2025-05-21 12:30:44 +0200

    Refactor AnyoneCanRegisterDisabledNotice and remove useCallback for dismissal, instead dismiss on click callback.

--- assets/js/modules/sign-in-with-google/components/common/AnyoneCanRegisterDisabledNotice.js
+++ assets/js/modules/sign-in-with-google/components/common/AnyoneCanRegisterDisabledNotice.js
@@ -17,11 +17,7 @@
 /**
  * WordPress dependencies
  */
-import {
-	createInterpolateElement,
-	Fragment,
-	useCallback,
-} from '@wordpress/element';
+import { createInterpolateElement, Fragment } from '@wordpress/element';
 import { __, _x, sprintf } from '@wordpress/i18n';
 
 /**
@@ -58,10 +54,6 @@ export default function AnyoneCanRegisterDisabledNotice() {
 	);
 
 	const { dismissItem } = useDispatch( CORE_USER );
-
-	const dismissNotice = useCallback( () => {
-		dismissItem( ANYONE_CAN_REGISTER_DISABLED_NOTICE );
-	} );
 
 	const isDismissed = useSelect( ( select ) =>
 		select( CORE_USER ).isItemDismissed(
@@ -114,7 +106,8 @@ export default function AnyoneCanRegisterDisabledNotice() {
 				) }
 				dismissButton={ {
 					label: __( 'Got it', 'google-site-kit' ),
-					onClick: dismissNotice,
+					onClick: () =>
+						dismissItem( ANYONE_CAN_REGISTER_DISABLED_NOTICE ),
 				} }
 			/>
 		</div>
