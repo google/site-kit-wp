e3191bd949bc68ff7858ab00b5379d77dfc2f95b
Aleksej <aleksej.vukomanovic@10up.com>
2025-05-22 18:01:05 +0200

    Update TopTrafficSourceDrivingAddToCartWidget stories.

--- assets/js/modules/analytics-4/components/widgets/TopTrafficSourceDrivingAddToCartWidget.stories.js
+++ assets/js/modules/analytics-4/components/widgets/TopTrafficSourceDrivingAddToCartWidget.stories.js
@@ -34,6 +34,7 @@ import {
 import {
 	getAnalytics4MockResponse,
 	provideAnalytics4MockReport,
+	provideAnalyticsReportWithoutDateRangeData,
 } from '../../utils/data-mock';
 import { replaceValuesInAnalytics4ReportWithZeroData } from '../../../../../../tests/js/utils/zeroReports';
 import { CORE_USER } from '../../../../googlesitekit/datastore/user/constants';
@@ -169,6 +170,23 @@ InsufficientPermissions.args = {
 		] );
 	},
 };
+
+export const NoDataInComparisonDateRange = Template.bind( {} );
+NoDataInComparisonDateRange.storyName = 'NoDataInComparisonDateRange';
+NoDataInComparisonDateRange.args = {
+	setupRegistry: ( registry ) => {
+		provideAnalyticsReportWithoutDateRangeData(
+			registry,
+			reportOptions[ 0 ]
+		);
+		provideAnalyticsReportWithoutDateRangeData(
+			registry,
+			reportOptions[ 1 ],
+			{ emptyRowBehavior: 'remove' }
+		);
+	},
+};
+NoDataInComparisonDateRange.scenario = {};
 
 export default {
 	title: 'Key Metrics/TopTrafficSourceDrivingAddToCart',
