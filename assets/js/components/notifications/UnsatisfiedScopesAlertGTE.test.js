36b313c240a8a48e3d20a83f348b69a99d107d98
Jimmy Madon <hello@jimmymadon.com>
2025-06-08 14:36:29 +0100

    Test unsatisfied scopes GTE alert when there is a setup error.

--- assets/js/components/notifications/UnsatisfiedScopesAlertGTE.test.js
+++ assets/js/components/notifications/UnsatisfiedScopesAlertGTE.test.js
@@ -22,6 +22,7 @@
 import {
 	createTestRegistry,
 	provideModules,
+	provideSiteInfo,
 	provideUserAuthentication,
 	render,
 } from '../../../../tests/js/test-utils';
@@ -82,6 +83,21 @@ describe( 'UnsatisfiedScopesAlertGTE', () => {
 			);
 
 			expect( isActive ).toBe( true );
+		} );
+
+		it( 'is not active when there is a setup error', async () => {
+			provideSiteInfo( registry, {
+				proxySupportLinkURL: 'https://test.com',
+				setupErrorCode: 'error_code',
+				setupErrorMessage: 'An error occurred',
+			} );
+
+			const isActive = await notification.checkRequirements(
+				registry,
+				VIEW_CONTEXT_MAIN_DASHBOARD
+			);
+
+			expect( isActive ).toBe( false );
 		} );
 	} );
 } );
