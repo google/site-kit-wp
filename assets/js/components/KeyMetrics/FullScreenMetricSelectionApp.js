b9d32c3aa5b85f257b99036992fec82b4cb3971f
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2025-06-11 13:04:20 -0400

    Ensure key metrics setup appears.

--- assets/js/components/KeyMetrics/FullScreenMetricSelectionApp.js
+++ assets/js/components/KeyMetrics/FullScreenMetricSelectionApp.js
@@ -78,7 +78,7 @@ export default function FullScreenMetricSelectionApp() {
 	);
 
 	const showSelectionPanel =
-		( hasFinishedGettingInputSettings && isKeyMetricsSetupCompleted ) ||
+		( hasFinishedGettingInputSettings && ! isKeyMetricsSetupCompleted ) ||
 		( ! previousIsKeyMetricsCompleted && isKeyMetricsSetupCompleted );
 
 	useEffect( () => {
