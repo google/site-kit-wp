24208ad4b88fd784525dcf996e4d06542766dbb1
Ben Bowler <ben@benbowler.com>
2025-06-11 16:07:56 +0100

    Debug working debounce of first font resize call.

--- tests/backstop/engine_scripts/puppet/onReady.js
+++ tests/backstop/engine_scripts/puppet/onReady.js
@@ -1,1 +1,114 @@
-module.exports = async () => {};
+/**
+ * Custom onReady script for BackstopJS.
+ *
+ * Site Kit by Google, Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-console */
+
+/**
+ * Executes custom waiting logic before taking screenshots.
+ *
+ * This script applies specific waiting logic based on the scenario label.
+ * Scenarios don't support passing function props therefore waiting logic
+ * is implemented here and triggered via text props.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Object} page     The Puppeteer page object.
+ * @param {Object} scenario The scenario configuration.
+ * @param {Object} viewport The viewport configuration.
+ */
+module.exports = async ( page, scenario, viewport ) => {
+	// NOTE: We can implement waitForRegistry or other not arbitrary time based delays here to improve test stability.
+
+	await new Promise( ( resolve ) => {
+		setTimeout( resolve, 2000 );
+	} );
+
+	// // Wait font size in selectors to match the expected size for the current viewport.
+	// // Currently used for the DashboardOverallPageMetricsWidgetGA4 story which uses the DataBlockGroup component.
+	// if ( scenario.waitForFontSizeToMatch && scenario.fontSizeSelector ) {
+	// 	const expectedFontSizes = {
+	// 		large: scenario.fontSizeLarge || false,
+	// 		medium: scenario.fontSizeMedium || false,
+	// 		small: scenario.fontSizeSmall || false,
+	// 	};
+	// 	const expectedFontSize = expectedFontSizes[ viewport.label ];
+
+	// 	if ( expectedFontSize === false ) {
+	// 		return;
+	// 	}
+
+	// 	await page.evaluate(
+	// 		( selector, targetFontSize, viewportLabel ) => {
+	// 			return new Promise( ( resolve ) => {
+	// 				const checkFontSizes = () => {
+	// 					const elements = document.querySelectorAll( selector );
+
+	// 					if ( ! elements.length ) {
+	// 						console.log(
+	// 							'No data block elements found, retrying...'
+	// 						);
+	// 						setTimeout( checkFontSizes, 100 );
+	// 						return;
+	// 					}
+
+	// 					// Get current font sizes of all data blocks
+	// 					const currentFontSizes = Array.from( elements ).map(
+	// 						( el ) => {
+	// 							const computedSize =
+	// 								window.getComputedStyle( el ).fontSize;
+	// 							// Convert "43px" to 43
+	// 							return parseInt( computedSize, 10 );
+	// 						}
+	// 					);
+
+	// 					console.log(
+	// 						`Current font sizes for ${ viewportLabel }:`,
+	// 						JSON.stringify( currentFontSizes )
+	// 					);
+	// 					console.log(
+	// 						`Expected font size: ${ targetFontSize }px`
+	// 					);
+
+	// 					// Check if all font sizes match the expected size for this viewport
+	// 					const allMatchExpected = currentFontSizes.every(
+	// 						( size ) => size === targetFontSize
+	// 					);
+
+	// 					if ( allMatchExpected ) {
+	// 						console.log(
+	// 							`✅ All font sizes match expected ${ targetFontSize }px for ${ viewportLabel } viewport`
+	// 						);
+	// 						resolve();
+	// 					} else {
+	// 						console.log(
+	// 							"❌ Font sizes don't match expected size yet, retrying..."
+	// 						);
+	// 						setTimeout( checkFontSizes, 100 );
+	// 					}
+	// 				};
+
+	// 				checkFontSizes();
+	// 			} );
+	// 		},
+	// 		scenario.fontSizeSelector,
+	// 		expectedFontSize,
+	// 		viewport.label
+	// 	);
+	// }
+};
