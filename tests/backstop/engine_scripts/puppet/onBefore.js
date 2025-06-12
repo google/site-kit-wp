fc045e782b4c1ceaf5f22fba4e74ed3933dc5209
Ben Bowler <ben@benbowler.com>
2025-06-12 16:25:06 +0100

    Implement component level font size reset before continuig.

--- tests/backstop/engine_scripts/puppet/onBefore.js
+++ tests/backstop/engine_scripts/puppet/onBefore.js
@@ -1,14 +1,1 @@
-module.exports = async ( page, scenario, viewport ) => {
-	// for debug, loop through all .googlesitekit-data-block__datapoint and set font size to ''
-	await page.evaluate( () => {
-		const dataBlocks = document.querySelectorAll(
-			'.googlesitekit-data-block__datapoint'
-		);
-		dataBlocks.forEach( ( block ) => {
-			console.log( '🚀 ~ onBefore.js ~ block resetting.' );
-			block.style.fontSize = '';
-		} );
-	} );
-	// Log the viewport label for debugging
-	console.log( `🚀 ${ viewport.label } ~ onBefore.js ~ scenario:`, scenario );
-};
+module.exports = async () => {};
