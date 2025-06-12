0a6fd439dac067098cef58de40d9af569b9fb3eb
Ben Bowler <ben@benbowler.com>
2025-06-12 15:19:50 +0100

    Clean up DataBlockGroup.

--- assets/js/components/DataBlockGroup.js
+++ assets/js/components/DataBlockGroup.js
@@ -26,29 +26,9 @@ import { useMount, useUnmount } from 'react-use';
  */
 import { useRef } from '@wordpress/element';
 import { useDebounce } from '../hooks/useDebounce';
-import { useEffect } from 'react';
 
 export default function DataBlockGroup( { className, children } ) {
 	const ref = useRef();
-	// const [ fontSizeOnLoad, setFontSizeOnLoad ] = useRef( null );
-
-	// useEffect( () => {
-	// 	// Set the initial font size on load.
-	// 	const blocks = ref?.current?.querySelectorAll(
-	// 		'.googlesitekit-data-block'
-	// 	);
-	// 	if ( blocks?.length ) {
-	// 		const firstBlockDataPoint = blocks[ 0 ]?.querySelector(
-	// 			'.googlesitekit-data-block__datapoint'
-	// 		);
-	// 		if ( firstBlockDataPoint ) {
-	// 			setFontSizeOnLoad( firstBlockDataPoint.style.fontSize );
-	// 		}
-	// 	}
-	// }, [] );
-
-	// Get body width:
-	const bodyWidth = global?.document?.body?.offsetWidth || 0;
 
 	const adjustFontSize = () => {
 		const blocks = ref?.current?.querySelectorAll(
@@ -58,13 +38,6 @@ export default function DataBlockGroup( { className, children } ) {
 		if ( ! blocks?.length ) {
 			return;
 		}
-		// console.log(
-		// 	`🚀 ${ bodyWidth } ~ adjustFontSize ~ blocks?.length:`,
-		// 	blocks?.length
-		// );
-
-		// Reset font sizes first to get accurate measurement, specifically on resize.
-		// setFontSizes( blocks, '' );
 
 		// Find the smallest font size needed across all blocks to fit without overflow.
 		let smallestScaleFactor = 1;
@@ -73,20 +46,10 @@ export default function DataBlockGroup( { className, children } ) {
 			const dataPoint = block.querySelector(
 				'.googlesitekit-data-block__datapoint'
 			);
-			// console.log(
-			// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ dataPoint:`,
-			// 	dataPoint
-			// );
 
 			if ( ! dataPoint ) {
 				return;
 			}
-
-			// // Console log innerText for debugging purposes.
-			// console.log(
-			// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ dataPoint.innerText:`,
-			// 	dataPoint.innerText
-			// );
 
 			const parentWidth = dataPoint?.parentElement?.offsetWidth;
 
@@ -94,37 +57,8 @@ export default function DataBlockGroup( { className, children } ) {
 				// Calculate the exact scale factor needed to resize the content to the parent.
 				const scaleFactor = parentWidth / dataPoint.scrollWidth;
 
-				// Round scaling factor **down* to the nearest .1 to account for variations in font rendering.
-				// const roundedScaleFactor = scaleFactor;
+				// Round scaling factor down 1D.P to account for variations in font rendering causing inconsistent resizing for VRTs.
 				const roundedScaleFactor = Math.floor( scaleFactor * 10 ) / 10;
-				// console.log(
-				// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ roundedScaleFactor:`,
-				// 	roundedScaleFactor
-				// );
-
-				//console log the parent for debugging purposes.
-				// console.log(
-				// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ parent:`,
-				// 	dataPoint.parentElement
-				// );
-
-				// console.log(
-				// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ scaleFactor:`,
-				// 	scaleFactor
-				// );
-				// console.log(
-				// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ dataPoint.scrollWidth:`,
-				// 	dataPoint.scrollWidth
-				// );
-				// console.log(
-				// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ parentWidth:`,
-				// 	parentWidth
-				// );
-				// // Console log the full computed styles as json string for debugging purposes.
-				// console.log(
-				// 	`🚀 ${ bodyWidth } ~ blocks.forEach ~ dataPoint.computedStyle:`,
-				// 	JSON.stringify( global?.getComputedStyle( dataPoint ) )
-				// );
 
 				if ( roundedScaleFactor < smallestScaleFactor ) {
 					smallestScaleFactor = roundedScaleFactor;
@@ -133,10 +67,6 @@ export default function DataBlockGroup( { className, children } ) {
 		} );
 
 		// Apply the smallest font size to all blocks if adjustment is needed.
-		console.log(
-			'🚀 ~ adjustFontSize ~ smallestScaleFactor < 1 :',
-			smallestScaleFactor < 1
-		);
 		if ( smallestScaleFactor < 1 && smallestScaleFactor > 0 ) {
 			const fontSize = parseInt(
 				global?.getComputedStyle(
@@ -148,20 +78,12 @@ export default function DataBlockGroup( { className, children } ) {
 			);
 
 			const newSize = Math.floor( fontSize * smallestScaleFactor );
-			// console.log(
-			// 	`🚀 ${ bodyWidth } ~ adjustFontSize ~ newSize:`,
-			// 	newSize
-			// );
 			const clampedNewSize = Math.max( newSize, 14 ); // Don't allow the font size to go below 14px.
 			setFontSizes( blocks, `${ clampedNewSize }px` );
 		}
 	};
 
 	const setFontSizes = ( blocks, adjustedSize ) => {
-		console.log(
-			`🚀 ${ bodyWidth } ~ setFontSizes ~ adjustedSize:`,
-			adjustedSize
-		);
 		blocks.forEach( ( block ) => {
 			const dataPoint = block?.querySelector(
 				'.googlesitekit-data-block__datapoint'
