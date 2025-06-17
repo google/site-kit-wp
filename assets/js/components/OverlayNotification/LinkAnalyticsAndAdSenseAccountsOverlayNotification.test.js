c88c6b732141ab85784b9836664b86c3c5e9fa9f
Jimmy Madon <hello@jimmymadon.com>
2025-06-17 18:10:13 +0100

    Remove legacy feature flag test.

--- assets/js/components/OverlayNotification/LinkAnalyticsAndAdSenseAccountsOverlayNotification.test.js
+++ assets/js/components/OverlayNotification/LinkAnalyticsAndAdSenseAccountsOverlayNotification.test.js
@@ -298,25 +298,6 @@ describe( 'LinkAnalyticsAndAdSenseAccountsOverlayNotification', () => {
 		);
 	} );
 
-	it( 'does not render without the feature flag', () => {
-		registry
-			.dispatch( CORE_USER )
-			.receiveGetDismissedItems( [
-				LINK_ANALYTICS_ADSENSE_OVERLAY_NOTIFICATION,
-			] );
-
-		const { container } = render(
-			<LinkAnalyticsAndAdSenseAccountsOverlayNotification />,
-			{
-				registry,
-				viewContext: VIEW_CONTEXT_MAIN_DASHBOARD,
-			}
-		);
-		expect( container ).not.toHaveTextContent(
-			'Link your Analytics and AdSense accounts to find out'
-		);
-	} );
-
 	it( 'renders `Learn how` and `Maybe later` buttons`', () => {
 		const supportURL = registry.select( CORE_SITE ).getGoogleSupportURL( {
 			path: '/adsense/answer/6084409',
