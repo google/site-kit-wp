3e71835b0bd12e7d50e04741d6881f13c9491262
Jimmy Madon <hello@jimmymadon.com>
2025-06-17 18:19:30 +0100

    Refactor the CTA button click test.

--- assets/js/components/OverlayNotification/LinkAnalyticsAndAdSenseAccountsOverlayNotification.test.js
+++ assets/js/components/OverlayNotification/LinkAnalyticsAndAdSenseAccountsOverlayNotification.test.js
@@ -283,7 +283,10 @@ describe( 'LinkAnalyticsAndAdSenseAccountsOverlayNotification', () => {
 		} );
 
 		const { container, getByRole, rerender, waitForRegistry } = render(
-			<LinkAnalyticsAndAdSenseAccountsOverlayNotification />,
+			<Notifications
+				areaSlug={ NOTIFICATION_AREAS.OVERLAYS }
+				groupID={ NOTIFICATION_GROUPS.SETUP_CTAS }
+			/>,
 			{
 				registry,
 				viewContext: VIEW_CONTEXT_MAIN_DASHBOARD,
