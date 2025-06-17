ad654649a65d7f8467b87449d1cc597f0c1bcb19
Jimmy Madon <hello@jimmymadon.com>
2025-06-17 18:52:12 +0100

    Reorganise test suite correctly.

--- assets/js/googlesitekit/datastore/ui/ui.test.js
+++ assets/js/googlesitekit/datastore/ui/ui.test.js
@@ -189,214 +189,182 @@ describe( 'core/ui store', () => {
 					expect( updatedIsDimissed ).toBe( true );
 				} );
 			} );
+		} );
 
-			describe( 'setValues', () => {
-				it( 'requires the values param', () => {
-					expect( () => {
-						registry.dispatch( CORE_UI ).setValues();
-					} ).toThrow( 'values must be an object.' );
-				} );
+		describe( 'setValues', () => {
+			it( 'requires the values param', () => {
+				expect( () => {
+					registry.dispatch( CORE_UI ).setValues();
+				} ).toThrow( 'values must be an object.' );
+			} );
 
-				it( 'requires the values param to be an object not an array', () => {
-					expect( () => {
-						registry.dispatch( CORE_UI ).setValues( [] );
-					} ).toThrow( 'values must be an object.' );
-				} );
+			it( 'requires the values param to be an object not an array', () => {
+				expect( () => {
+					registry.dispatch( CORE_UI ).setValues( [] );
+				} ).toThrow( 'values must be an object.' );
+			} );
 
-				it( 'does not throw if values is an object', () => {
-					expect( () => {
-						registry.dispatch( CORE_UI ).setValues( {} );
-					} ).not.toThrow();
-				} );
-
-				it( 'requires the values param to be an object not a string', () => {
-					expect( () => {
-						registry.dispatch( CORE_UI ).setValues( 'values' );
-					} ).toThrow( 'values must be an object.' );
-				} );
-
-				it( 'does not overwrite unrelated keys', () => {
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: 'value1', key2: 'value2' } );
-
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: 'value3' } );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key2' );
-					expect( uiValue ).toEqual( 'value2' );
-				} );
-
-				it( 'returns a newly-set value if a new value for an existing key is set', () => {
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: 'value1', key2: 'value2' } );
-
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: 'value3' } );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( 'value3' );
-				} );
-
-				it( 'does not overwrite unrelated keys when an empty object is supplied to values', () => {
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: 'value1', key2: 'value2' } );
-
+			it( 'does not throw if values is an object', () => {
+				expect( () => {
 					registry.dispatch( CORE_UI ).setValues( {} );
+				} ).not.toThrow();
+			} );
 
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key2' );
-					expect( uiValue ).toEqual( 'value2' );
+			it( 'requires the values param to be an object not a string', () => {
+				expect( () => {
+					registry.dispatch( CORE_UI ).setValues( 'values' );
+				} ).toThrow( 'values must be an object.' );
+			} );
+
+			it( 'does not overwrite unrelated keys', () => {
+				registry
+					.dispatch( CORE_UI )
+					.setValues( { key1: 'value1', key2: 'value2' } );
+
+				registry.dispatch( CORE_UI ).setValues( { key1: 'value3' } );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key2' );
+				expect( uiValue ).toEqual( 'value2' );
+			} );
+
+			it( 'returns a newly-set value if a new value for an existing key is set', () => {
+				registry
+					.dispatch( CORE_UI )
+					.setValues( { key1: 'value1', key2: 'value2' } );
+
+				registry.dispatch( CORE_UI ).setValues( { key1: 'value3' } );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( 'value3' );
+			} );
+
+			it( 'does not overwrite unrelated keys when an empty object is supplied to values', () => {
+				registry
+					.dispatch( CORE_UI )
+					.setValues( { key1: 'value1', key2: 'value2' } );
+
+				registry.dispatch( CORE_UI ).setValues( {} );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key2' );
+				expect( uiValue ).toEqual( 'value2' );
+			} );
+
+			it( 'works with empty values where the key value is updated', () => {
+				registry.dispatch( CORE_UI ).setValues( {} );
+
+				registry
+					.dispatch( CORE_UI )
+					.setValues( { key1: 'value1', key2: 'value2' } );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key2' );
+				expect( uiValue ).toEqual( 'value2' );
+			} );
+
+			it( 'preserves data from state when new data is assigned', () => {
+				registry
+					.dispatch( CORE_UI )
+					.setValues( { key1: 'value1', key2: 'value2' } );
+
+				registry
+					.dispatch( CORE_UI )
+					.setValues( { key3: 'value3', key4: 'value4' } );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key2' );
+				expect( uiValue ).toEqual( 'value2' );
+			} );
+
+			it( 'sets object values', () => {
+				registry
+					.dispatch( CORE_UI )
+					.setValues( { key1: { childKey1: 'childValue1' } } );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( { childKey1: 'childValue1' } );
+			} );
+
+			it( 'sets boolean values', () => {
+				registry.dispatch( CORE_UI ).setValues( { key1: false } );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( false );
+			} );
+
+			it( 'sets array values', () => {
+				registry.dispatch( CORE_UI ).setValues( {
+					key1: [ 'childKey1', 'childKey2', 'childKey3' ],
 				} );
 
-				it( 'works with empty values where the key value is updated', () => {
-					registry.dispatch( CORE_UI ).setValues( {} );
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( [
+					'childKey1',
+					'childKey2',
+					'childKey3',
+				] );
+			} );
+		} );
 
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: 'value1', key2: 'value2' } );
+		describe( 'setValue', () => {
+			it( 'requires the key param', () => {
+				expect( () => {
+					registry.dispatch( CORE_UI ).setValue();
+				} ).toThrow( 'key is required.' );
+			} );
 
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key2' );
-					expect( uiValue ).toEqual( 'value2' );
+			it( 'does not throw with a key', () => {
+				expect( () => {
+					registry.dispatch( CORE_UI ).setValue( 'key1', 'value1' );
+				} ).not.toThrow();
+			} );
+
+			it( 'works with key and value', () => {
+				registry.dispatch( CORE_UI ).setValue( 'key1', 'value1' );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( 'value1' );
+			} );
+
+			it( 'works with a boolean value', () => {
+				registry.dispatch( CORE_UI ).setValue( 'key1', false );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( false );
+			} );
+
+			it( 'works with the value as an object', () => {
+				registry.dispatch( CORE_UI ).setValue( 'key1', {
+					childKey1: 'childValue1',
+					childKey2: 'childValue2',
 				} );
 
-				it( 'preserves data from state when new data is assigned', () => {
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: 'value1', key2: 'value2' } );
-
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key3: 'value3', key4: 'value4' } );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key2' );
-					expect( uiValue ).toEqual( 'value2' );
-				} );
-
-				it( 'sets object values', () => {
-					registry
-						.dispatch( CORE_UI )
-						.setValues( { key1: { childKey1: 'childValue1' } } );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( { childKey1: 'childValue1' } );
-				} );
-
-				it( 'sets boolean values', () => {
-					registry.dispatch( CORE_UI ).setValues( { key1: false } );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( false );
-				} );
-
-				it( 'sets array values', () => {
-					registry.dispatch( CORE_UI ).setValues( {
-						key1: [ 'childKey1', 'childKey2', 'childKey3' ],
-					} );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( [
-						'childKey1',
-						'childKey2',
-						'childKey3',
-					] );
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( {
+					childKey1: 'childValue1',
+					childKey2: 'childValue2',
 				} );
 			} );
 
-			describe( 'setValue', () => {
-				it( 'requires the key param', () => {
-					expect( () => {
-						registry.dispatch( CORE_UI ).setValue();
-					} ).toThrow( 'key is required.' );
-				} );
-
-				it( 'does not throw with a key', () => {
-					expect( () => {
-						registry
-							.dispatch( CORE_UI )
-							.setValue( 'key1', 'value1' );
-					} ).not.toThrow();
-				} );
-
-				it( 'works with key and value', () => {
-					registry.dispatch( CORE_UI ).setValue( 'key1', 'value1' );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( 'value1' );
-				} );
-
-				it( 'works with a boolean value', () => {
-					registry.dispatch( CORE_UI ).setValue( 'key1', false );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( false );
-				} );
-
-				it( 'works with the value as an object', () => {
-					registry.dispatch( CORE_UI ).setValue( 'key1', {
-						childKey1: 'childValue1',
-						childKey2: 'childValue2',
-					} );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( {
-						childKey1: 'childValue1',
-						childKey2: 'childValue2',
-					} );
-				} );
-
-				it( 'works with the value as an array', () => {
-					registry
-						.dispatch( CORE_UI )
-						.setValue( 'key1', [
-							'childKey1',
-							'childKey2',
-							'childKey3',
-						] );
-
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( [
+			it( 'works with the value as an array', () => {
+				registry
+					.dispatch( CORE_UI )
+					.setValue( 'key1', [
 						'childKey1',
 						'childKey2',
 						'childKey3',
 					] );
-				} );
 
-				it( 'works with the value undefined', () => {
-					registry.dispatch( CORE_UI ).setValue( 'key1' );
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( [
+					'childKey1',
+					'childKey2',
+					'childKey3',
+				] );
+			} );
 
-					const uiValue = registry
-						.select( CORE_UI )
-						.getValue( 'key1' );
-					expect( uiValue ).toEqual( undefined );
-				} );
+			it( 'works with the value undefined', () => {
+				registry.dispatch( CORE_UI ).setValue( 'key1' );
+
+				const uiValue = registry.select( CORE_UI ).getValue( 'key1' );
+				expect( uiValue ).toEqual( undefined );
 			} );
 		} );
 
