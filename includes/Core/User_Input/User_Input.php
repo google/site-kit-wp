74b741ddbd4040c30e65abd827b4fb9bcf2549eb
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2025-06-21 00:55:29 +0100

    Add progress bar to splash screen.

--- includes/Core/User_Input/User_Input.php
+++ includes/Core/User_Input/User_Input.php
@@ -165,6 +165,7 @@ class User_Input {
 
 			$answered_by = intval( $setting['answeredBy'] );
 
+			// HERE, why do we want to skip if question has been answered by the current user?
 			if ( ! $answered_by || $answered_by === $this->user_options->get_user_id() ) {
 				continue;
 			}
