14052cf0fa54c0cd26e8728e09dc09e6d0fedf97
Aleksej <aleksej.vukomanovic@10up.com>
2025-06-30 10:01:09 +0200

    Add default SVG for warning notifications.

--- assets/js/googlesitekit/notifications/components/layout/BannerNotification.js
+++ assets/js/googlesitekit/notifications/components/layout/BannerNotification.js
@@ -33,6 +33,7 @@ import LearnMoreLink from '../../../../components/Banner/LearnMoreLink';
 import CTAButton from '../../../../components/Banner/CTAButton';
 import DismissButton from '../../../../components/Banner/DismissButton';
 import { Cell, Grid, Row } from '../../../../material-components';
+import warningDesktopSVG from '@/svg/graphics/banner-warning.svg?url';
 
 export const TYPES = {
 	INFO: 'info',
@@ -89,7 +90,19 @@ export default function BannerNotification( {
 		await learnMoreLink?.onClick?.( event );
 	};
 
-	const SVGData = props?.svg;
+	let SVGData = props?.svg;
+
+	if ( ! SVGData && type !== TYPES.INFO ) {
+		SVGData = {
+			desktop: undefined,
+			mobile: undefined,
+			verticalPosition: 'center',
+		};
+
+		if ( type === TYPES.WARNING ) {
+			SVGData.desktop = warningDesktopSVG;
+		}
+	}
 
 	return (
 		<div
