b106d4761dd77fc2c1ee6f3e23aaa4f96d332d2e
Ankit Gade <ankit.gade@10up.com>
2025-07-02 15:33:52 +0530

    Use waitForSelector for DOM stability.

--- tests/e2e/specs/modules/analytics/write-scope-requests.test.js
+++ tests/e2e/specs/modules/analytics/write-scope-requests.test.js
@@ -435,7 +435,9 @@ describe( 'Analytics write scope requests', () => {
 
 		// They should end up on the dashboard.
 		await page.waitForNavigation();
-		await page.waitForNetworkIdle();
+		await page.waitForSelector( '.googlesitekit-notice__title', {
+			timeout: 5000,
+		} );
 
 		await expect( page ).toMatchElement( '.googlesitekit-notice__title', {
 			text: /Congrats on completing the setup for Analytics!/i,
