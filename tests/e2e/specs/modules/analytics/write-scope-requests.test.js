8307fc527160bc28230ba31ad456c49429083865
Ankit Gade <ankit.gade@10up.com>
2025-07-02 14:20:12 +0530

    Remove unnecessary code.

--- tests/e2e/specs/modules/analytics/write-scope-requests.test.js
+++ tests/e2e/specs/modules/analytics/write-scope-requests.test.js
@@ -435,7 +435,6 @@ describe( 'Analytics write scope requests', () => {
 
 		// They should end up on the dashboard.
 		await page.waitForNavigation();
-		await page.waitForNetworkIdle();
 		await expect( page ).toMatchElement( '.googlesitekit-notice__title', {
 			text: /Congrats on completing the setup for Analytics!/i,
 		} );
