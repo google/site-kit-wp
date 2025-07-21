7fa68941c5e09b0d62de9eb6e5b339cce867bfbd
Ben Bowler <ben@benbowler.com>
2025-07-21 11:47:00 +0100

    Update test createWaitForFetchRequestsWithDebounce implementation.

--- tests/e2e/utils/create-wait-for-fetch-requests.js
+++ tests/e2e/utils/create-wait-for-fetch-requests.js
@@ -50,55 +50,55 @@ export function createWaitForFetchRequests() {
 	};
 }
 
-export function createWaitForFetchRequestsWithDebounce( debounceTime = 250 ) {
-	const responsePromises = [];
+// export function createWaitForFetchRequestsWithDebounce( debounceTime = 250 ) {
+// 	const responsePromises = [];
 
-	let timeout;
-	let resolvePromise;
+// 	let timeout;
+// 	let resolvePromise;
 
-	const listener = ( req ) => {
-		if ( req.resourceType() === 'fetch' ) {
-			const promise = page.waitForResponse(
-				// eslint-disable-next-line sitekit/acronym-case
-				( res ) => res.request()._requestId === req._requestId
-			);
-			// A promise may be rejected if the execution context it was
-			// captured in no longer exists (e.g. previous page) which
-			// is necessary in some cases, and can be ignored since
-			// there is nothing to wait for any more.
-			responsePromises.push( promise.catch( () => {} ) );
+// 	const listener = ( req ) => {
+// 		if ( req.resourceType() === 'fetch' ) {
+// 			const promise = page.waitForResponse(
+// 				// eslint-disable-next-line sitekit/acronym-case
+// 				( res ) => res.request()._requestId === req._requestId
+// 			);
+// 			// A promise may be rejected if the execution context it was
+// 			// captured in no longer exists (e.g. previous page) which
+// 			// is necessary in some cases, and can be ignored since
+// 			// there is nothing to wait for any more.
+// 			responsePromises.push( promise.catch( () => {} ) );
 
-			if ( timeout ) {
-				clearTimeout( timeout );
+// 			if ( timeout ) {
+// 				clearTimeout( timeout );
 
-				timeout = setTimeout( () => {
-					if ( resolvePromise ) {
-						resolvePromise( Promise.all( responsePromises ) );
-						resolvePromise = null;
-						timeout = null;
-					}
-				}, debounceTime );
-			}
-		}
-	};
+// 				timeout = setTimeout( () => {
+// 					if ( resolvePromise ) {
+// 						resolvePromise( Promise.all( responsePromises ) );
+// 						resolvePromise = null;
+// 						timeout = null;
+// 					}
+// 				}, debounceTime );
+// 			}
+// 		}
+// 	};
 
-	page.on( 'request', listener );
+// 	page.on( 'request', listener );
 
-	return () => {
-		page.off( 'request', listener );
+// 	return () => {
+// 		page.off( 'request', listener );
 
-		return new Promise( ( resolve ) => {
-			resolvePromise = resolve;
+// 		return new Promise( ( resolve ) => {
+// 			resolvePromise = resolve;
 
-			// TODO: Could DRY this with the setTimeout() above:
-			timeout = setTimeout( () => {
-				resolve( Promise.all( responsePromises ) );
-				resolvePromise = null;
-				timeout = null;
-			}, debounceTime );
-		} );
-	};
-}
+// 			// TODO: Could DRY this with the setTimeout() above:
+// 			timeout = setTimeout( () => {
+// 				resolve( Promise.all( responsePromises ) );
+// 				resolvePromise = null;
+// 				timeout = null;
+// 			}, debounceTime );
+// 		} );
+// 	};
+// }
 
 // export function createWaitForFetchRequestsWithDebounce( debounceTime = 250 ) {
 // 	const responsePromises = [];
@@ -181,3 +181,53 @@ export function createWaitForFetchRequestsWithDebounce( debounceTime = 250 ) {
 // 		} );
 // 	};
 // }
+
+export function createWaitForFetchRequestsWithDebounce( debounceTime = 250 ) {
+	const responsePromises = [];
+
+	let timeout;
+	let resolvePromise;
+
+	const listener = ( req ) => {
+		if ( req.resourceType() === 'fetch' ) {
+			const promise = page.waitForResponse(
+				// eslint-disable-next-line sitekit/acronym-case
+				( res ) => res.request()._requestId === req._requestId
+			);
+			// A promise may be rejected if the execution context it was
+			// captured in no longer exists (e.g. previous page) which
+			// is necessary in some cases, and can be ignored since
+			// there is nothing to wait for any more.
+			responsePromises.push( promise.catch( () => {} ) );
+
+			if ( timeout ) {
+				clearTimeout( timeout );
+			}
+
+			timeout = setTimeout( () => {
+				if ( resolvePromise ) {
+					resolvePromise( Promise.all( responsePromises ) );
+					resolvePromise = null;
+					timeout = null;
+				}
+			}, debounceTime );
+		}
+	};
+
+	page.on( 'request', listener );
+
+	return () => {
+		page.off( 'request', listener );
+
+		return new Promise( ( resolve ) => {
+			resolvePromise = resolve;
+
+			// TODO: Could DRY this with the setTimeout() above:
+			timeout = setTimeout( () => {
+				resolve( Promise.all( responsePromises ) );
+				resolvePromise = null;
+				timeout = null;
+			}, debounceTime );
+		} );
+	};
+}
