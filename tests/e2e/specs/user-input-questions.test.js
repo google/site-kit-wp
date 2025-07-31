26d44e284df3712a3c20fbab78beb6437b033883
Ben Bowler <ben@benbowler.com>
2025-07-31 12:58:16 +0100

    Extend timeout for user input E2E test.

--- tests/e2e/specs/user-input-questions.test.js
+++ tests/e2e/specs/user-input-questions.test.js
@@ -47,7 +47,7 @@ import getMultiDimensionalObjectFromParams from '../utils/get-multi-dimensional-
 describe( 'User Input Settings', () => {
 	async function fillInInputSettings() {
 		// The UserInputApp needs more time to load to pass consistently.
-		const extendedTimeout = 20000;
+		const extendedTimeout = 30000;
 
 		await page.waitForNetworkIdle( { timeout: extendedTimeout } );
 		await page.waitForSelector( '.googlesitekit-user-input__question', {
