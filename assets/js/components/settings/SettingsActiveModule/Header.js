fab35dd6b4c307395b8b454b24c0a679bd3efc17
Eugene Manuilov <eugene.manuilov@fueled.com>
2025-08-12 16:49:30 +0300

    Update styles.

--- assets/js/components/settings/SettingsActiveModule/Header.js
+++ assets/js/components/settings/SettingsActiveModule/Header.js
@@ -52,6 +52,7 @@ import IconWrapper from '../../IconWrapper';
 import { useDispatch } from '../../../googlesitekit-data';
 import Typography from '../../Typography';
 import P from '../../Typography/P';
+import { SIZE_MEDIUM } from '../../Typography/constants';
 
 export default function Header( { slug } ) {
 	const viewContext = useViewContext();
@@ -132,7 +133,9 @@ export default function Header( { slug } ) {
 	let moduleStatus = null;
 
 	if ( connected ) {
-		moduleStatus = <P>{ __( 'Connected', 'google-site-kit' ) }</P>;
+		moduleStatus = (
+			<P size={ SIZE_MEDIUM }>{ __( 'Connected', 'google-site-kit' ) }</P>
+		);
 	} else {
 		moduleStatus = (
 			<Button
