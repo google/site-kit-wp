f98aa819533bf2d801f2c572e0886b1a312f3efb
Simon Dowdles <72872375+10upsimon@users.noreply.github.com>
2025-08-14 13:54:08 +0200

    Update tests/phpunit/integration/Core/Modules/REST_Modules_ControllerTest.php
    Co-authored-by: Aleksej Vukomanovic <aleksej.vukomanovic@10up.com>

--- tests/phpunit/integration/Core/Modules/REST_Modules_ControllerTest.php
+++ tests/phpunit/integration/Core/Modules/REST_Modules_ControllerTest.php
@@ -992,7 +992,7 @@ class REST_Modules_ControllerTest extends TestCase {
 		$response = rest_get_server()->dispatch( $request );
 
 		$this->assertEquals( 403, $response->get_status(), 'GET on module data should return 403 when module is inactive.' );
-		$this->assertEquals( 'module_not_active', $response->get_data()['code'], 'Error code should indicate module not active.' );
+		$this->assertEquals( 'module_not_active', $response->get_data()['code'], 'Error code should indicate the module is not active.' );
 		$this->assertEquals( 'Module must be active to request data.', $response->get_data()['message'], 'Error message should indicate active module required.' );
 
 		$request = new WP_REST_Request( 'POST', '/' . REST_Routes::REST_ROOT . '/modules/fake-module/data/test-request' );
