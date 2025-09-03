17a2139178a358facdf5d3301bf9f750ad07ef50
Hussain Thajutheen <hussain.thajutheen@10up.com>
2025-09-03 14:38:08 +0300

    Capture and send user data from CF7 for EC.

--- assets/js/event-providers/contact-form-7.js
+++ assets/js/event-providers/contact-form-7.js
@@ -14,10 +14,125 @@
  * limitations under the License.
  */
 
+/**
+ * Internal dependencies
+ */
+import { classifyPII, normalizeValue, PII_TYPE } from './utils';
+
 global.document.addEventListener( 'wpcf7mailsent', ( event ) => {
+	const gtagUserDataEnabled = global._googlesitekit?.gtagUserData;
+
+	const userData = gtagUserDataEnabled ? getUserData( event.target ) : null;
+
 	global._googlesitekit?.gtagEvent?.( 'contact', {
 		// eslint-disable-next-line sitekit/acronym-case
 		event_category: event.detail.contactFormId,
 		event_label: event.detail.unitTag,
+		...( userData ? { user_data: userData } : {} ),
 	} );
 } );
+
+/**
+ * Extracts and formats name fields for Google Tag's user_data address object.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Array<Object>} fields An array of detected PII fields.
+ * @return {Object|undefined} An object containing normalized first_name and optionally last_name, or undefined if no names found.
+ */
+function getAddress( fields ) {
+	const names = fields
+		.filter( ( { type } ) => type === PII_TYPE.NAME )
+		.map( ( { value } ) => value );
+
+	if ( ! names.length ) {
+		return undefined;
+	}
+
+	const [ firstName, ...lastNames ] =
+		names.length === 1 ? names[ 0 ].split( ' ' ) : names;
+
+	return {
+		first_name: normalizeValue( firstName ),
+		...( lastNames?.length > 0
+			? { last_name: normalizeValue( lastNames.join( ' ' ) ) }
+			: {} ),
+	};
+}
+
+/**
+ * Extracts the email address from detected PII fields.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Array<Object>} fields An array of detected PII fields.
+ * @return {string|undefined} The email address if found, undefined otherwise.
+ */
+function getEmail( fields ) {
+	return fields.find( ( { type } ) => type === PII_TYPE.EMAIL )?.value;
+}
+
+/**
+ * Extracts the phone number from detected PII fields.
+ *
+ * @since n.e.x.t
+ *
+ * @param {Array<Object>} fields An array of detected PII fields.
+ * @return {string|undefined} The phone number if found, undefined otherwise.
+ */
+function getPhoneNumber( fields ) {
+	return fields.find( ( { type } ) => type === PII_TYPE.PHONE )?.value;
+}
+
+/**
+ * Extracts and classifies user data from a Contact Form 7 form submission.
+ *
+ * @since n.e.x.t
+ *
+ * @param {HTMLFormElement} form The submitted form element.
+ * @return {Object|undefined} A user_data object containing detected PII (address, email, phone_number), or undefined if no PII found.
+ */
+function getUserData( form ) {
+	// eslint-disable-next-line sitekit/acronym-case
+	if ( ! form || ! ( form instanceof HTMLFormElement ) ) {
+		return undefined;
+	}
+
+	// Get all interactive form controls (input, textarea, select)
+	const formControls = form.querySelectorAll( 'input, textarea, select' );
+	const detectedFields = Array.from( formControls )
+		.map( ( input ) => {
+			const { type, name, value } = input;
+
+			// Skip empty values and hidden fields
+			if ( ! value || type === 'hidden' || type === 'submit' ) {
+				return null;
+			}
+
+			// Get label text for better PII classification
+			const label = input.id
+				? form.querySelector( `label[for='${ input.id }']` )
+						?.textContent
+				: undefined;
+
+			return classifyPII( {
+				type,
+				label,
+				name,
+				value,
+			} );
+		} )
+		.filter( Boolean );
+
+	const userDataFields = [
+		[ 'address', getAddress( detectedFields ) ],
+		[ 'email', getEmail( detectedFields ) ],
+		[ 'phone_number', getPhoneNumber( detectedFields ) ],
+	].filter( ( [ , value ] ) => value );
+
+	if ( userDataFields.length === 0 ) {
+		return undefined;
+	}
+
+	return Object.fromEntries( userDataFields );
+}
