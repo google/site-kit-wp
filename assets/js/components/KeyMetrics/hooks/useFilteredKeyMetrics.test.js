208c65825f75322de46b1e66f4e7432d00496ee5
Aleksej <aleksej.vukomanovic@10up.com>
2025-09-18 13:28:51 +0200

    Add file header and inline comment to the useFilteredKeyMetrics test.

--- assets/js/components/KeyMetrics/hooks/useFilteredKeyMetrics.test.js
+++ assets/js/components/KeyMetrics/hooks/useFilteredKeyMetrics.test.js
@@ -16,6 +16,9 @@
  * limitations under the License.
  */
 
+/**
+ * Internal dependencies
+ */
 import { renderHook } from '../../../../../tests/js/test-utils';
 import useFilteredKeyMetrics from './useFilteredKeyMetrics';
 import {
