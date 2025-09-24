8a0c2c00c9ff43f63884c110082108b1f2508b82
Eugene Manuilov <eugene.manuilov@fueled.com>
2025-09-24 18:41:13 +0300

    Add the wp login checkers.

---
+++ includes/Modules/Sign_In_With_Google/Compatibility_Checks/WP_Login_Accessible_Check.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * WordPress login accessibility compatibility check.
+ *
+ * @package   Google\Site_Kit\Modules\Sign_In_With_Google
+ * @copyright 2025 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Modules\Sign_In_With_Google\Compatibility_Checks;
+
+/**
+ * Compatibility check for WordPress login accessibility.
+ *
+ * @since n.e.x.t
+ */
+class WP_Login_Accessible_Check extends Compatibility_Checker {
+
+	/**
+	 * Gets the unique slug for this compatibility check.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return string The unique slug for this compatibility check.
+	 */
+	public function get_slug() {
+		return 'wp_login_inaccessible';
+	}
+
+	/**
+	 * Runs the compatibility check.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return bool True if login is inaccessible (404), false otherwise.
+	 */
+	public function run() {
+		$login_url = wp_login_url();
+		$response  = wp_remote_head( $login_url );
+
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+
+		$status_code = wp_remote_retrieve_response_code( $response );
+
+		return 404 === $status_code;
+	}
+}
