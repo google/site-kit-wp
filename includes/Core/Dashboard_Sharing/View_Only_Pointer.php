fd0f9248b7c935bdc3a0f62d4d33fef80dd04fe4
Aleksej <aleksej.vukomanovic@10up.com>
2025-10-01 21:52:06 +0200

    Switch to button.

--- includes/Core/Dashboard_Sharing/View_Only_Pointer.php
+++ includes/Core/Dashboard_Sharing/View_Only_Pointer.php
@@ -98,7 +98,7 @@ final class View_Only_Pointer {
 						};
 						jQuery("body").on("click", ".googlesitekit-pointer-cta", onClick);
 
-						return jQuery(\'<a class="googlesitekit-pointer-cta button-primary">Setup</a>\');
+						return jQuery(\'<button class="googlesitekit-pointer-cta button-primary">Setup</button>\');
 					}
 					', self::SLUG),
 			)
