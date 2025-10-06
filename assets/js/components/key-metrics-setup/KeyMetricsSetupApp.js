863b4e039485987b3c3a024b796572839ce58a8d
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2025-10-06 12:11:51 +0100

    Remove unneeded logic from the `KeyMetricsSetupApp` component.

--- assets/js/components/key-metrics-setup/KeyMetricsSetupApp.js
+++ assets/js/components/key-metrics-setup/KeyMetricsSetupApp.js
@@ -17,6 +17,11 @@
  */
 
 /**
+ * External dependencies
+ */
+import { omit } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import {
@@ -30,16 +35,12 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { useDispatch, useSelect } from 'googlesitekit-data';
-import { ProgressBar, SpinnerButton } from 'googlesitekit-components';
+import { SpinnerButton } from 'googlesitekit-components';
 import { CORE_LOCATION } from '@/js/googlesitekit/datastore/location/constants';
-import { CORE_MODULES } from '@/js/googlesitekit/modules/datastore/constants';
 import { CORE_SITE } from '@/js/googlesitekit/datastore/site/constants';
 import { CORE_USER } from '@/js/googlesitekit/datastore/user/constants';
-import { MODULE_SLUG_ANALYTICS_4 } from '@/js/modules/analytics-4/constants';
-import { MODULES_ANALYTICS_4 } from '@/js/modules/analytics-4/datastore/constants';
 import { Grid, Row, Cell } from '@/js/material-components';
 import Header from '@/js/components/Header';
-import HelpMenu from '@/js/components/help/HelpMenu';
 import Layout from '@/js/components/layout/Layout';
 import ErrorNotice from '@/js/components/ErrorNotice';
 import Typography from '@/js/components/Typography';
@@ -69,11 +70,6 @@ export default function KeyMetricsSetupApp() {
 		select( CORE_LOCATION ).isNavigating()
 	);
 
-	// User input settings are not cleared when Analytics is disconnected, so we may have an existing answer.
-	const hasFinishedGettingInputSettings = useSelect( ( select ) =>
-		select( CORE_USER ).hasFinishedResolution( 'getUserInputSettings' )
-	);
-
 	const error = useSelect( ( select ) =>
 		select( CORE_USER ).getErrorForAction( 'saveUserInputSettings', [] )
 	);
@@ -85,48 +81,16 @@ export default function KeyMetricsSetupApp() {
 			) || []
 	);
 
-	const userInputPurposeConversionEvents = useSelect( ( select ) => {
-		const isGA4Connected = select( CORE_MODULES ).isModuleConnected(
-			MODULE_SLUG_ANALYTICS_4
-		);
-
-		if ( ! isGA4Connected ) {
-			return [];
-		}
-
-		return select(
-			MODULES_ANALYTICS_4
-		).getUserInputPurposeConversionEvents();
-	} );
-
-	const { setUserInputSetting, saveUserInputSettings } =
-		useDispatch( CORE_USER );
+	const { saveUserInputSettings } = useDispatch( CORE_USER );
 	const { navigateTo } = useDispatch( CORE_LOCATION );
 
 	const submitChanges = useCallback( async () => {
-		// trackEvent( gaEventCategory, 'summary_submit' );
-
-		// Update 'includeConversionEvents' setting with included conversion events,
-		// to mark that their respective metrics should be included in the
-		// list of tailored metrics and persist on the dashboard in case events are lost.
-		setUserInputSetting(
-			'includeConversionEvents',
-			userInputPurposeConversionEvents
-		);
-
 		const response = await saveUserInputSettings();
 		if ( ! response.error ) {
 			const url = new URL( dashboardURL );
 			navigateTo( url.toString() );
 		}
-	}, [
-		// gaEventCategory,
-		saveUserInputSettings,
-		userInputPurposeConversionEvents,
-		dashboardURL,
-		setUserInputSetting,
-		navigateTo,
-	] );
+	}, [ saveUserInputSettings, dashboardURL, navigateTo ] );
 
 	const isScreenLoading = isSavingSettings || isNavigating;
 
@@ -146,100 +110,81 @@ export default function KeyMetricsSetupApp() {
 
 	return (
 		<Fragment>
-			<Header>
-				<HelpMenu />
-			</Header>
+			<Header />
 			<div className="googlesitekit-key-metrics-setup">
-				{ ! hasFinishedGettingInputSettings && (
-					<Grid>
-						<Row>
-							<Cell size={ 12 }>
-								<ProgressBar />
-							</Cell>
-						</Row>
-					</Grid>
-				) }
-				{ hasFinishedGettingInputSettings && (
-					<Grid>
-						<Row>
-							<Cell size={ 12 }>
-								<Layout rounded>
-									<Typography
-										as="h1"
-										type="headline"
-										size="medium"
-									>
-										{ __(
-											'Tell us your main goal to get tailored metrics',
-											'google-site-kit'
-										) }
-									</Typography>
-									<Typography
-										as="h2"
-										type="body"
-										size="large"
-									>
-										{ __(
-											'Which option most closely matches the purpose of your site?',
-											'google-site-kit'
-										) }
-									</Typography>
-									<P
-										className="googlesitekit-key-metrics-setup__description"
-										type="body"
-										size="small"
-									>
-										{ __(
-											'Even if multiple options apply to your site, select the one that applies the most. You can also answer or edit your response later in Settings.',
-											'google-site-kit'
-										) }
-									</P>
-
-									<UserInputSelectOptions
-										slug={ USER_INPUT_QUESTIONS_PURPOSE }
-										max={
-											USER_INPUT_MAX_ANSWERS[
-												USER_INPUT_QUESTIONS_PURPOSE
-											]
-										}
-										options={ USER_INPUT_ANSWERS_PURPOSE }
-										descriptions={
-											USER_INPUT_ANSWERS_PURPOSE_DESCRIPTIONS
-										}
-									/>
-
-									{ error && (
-										<div className="googlesitekit-user-input__error">
-											<ErrorNotice
-												error={ error }
-												Icon={ WarningSVG }
-											/>
-										</div>
+				<Grid>
+					<Row>
+						<Cell size={ 12 }>
+							<Layout rounded>
+								<Typography
+									as="h1"
+									type="headline"
+									size="medium"
+								>
+									{ __(
+										'Tell us your main goal to get tailored metrics',
+										'google-site-kit'
 									) }
+								</Typography>
+								<Typography as="h2" type="body" size="large">
+									{ __(
+										'Which option most closely matches the purpose of your site?',
+										'google-site-kit'
+									) }
+								</Typography>
+								<P
+									className="googlesitekit-key-metrics-setup__description"
+									type="body"
+									size="small"
+								>
+									{ createInterpolateElement(
+										__(
+											'Even if multiple options apply to your site, select the one that applies the most.<br />You can also answer or edit your response later in Settings.',
+											'google-site-kit'
+										),
+										{
+											br: <br />,
+										}
+									) }
+								</P>
 
-									<SpinnerButton
-										className="googlesitekit-user-input__buttons--complete"
-										onClick={ onSaveClick }
-										isSaving={ isScreenLoading }
-										disabled={ hasErrorForAnswer( values ) }
-									>
-										{ createInterpolateElement(
-											__(
-												'Complete<span> setup</span>',
-												'google-site-kit'
-											),
-											{
-												span: (
-													<span className="googlesitekit-user-input__responsive-text" />
-												),
-											}
-										) }
-									</SpinnerButton>
-								</Layout>
-							</Cell>
-						</Row>
-					</Grid>
-				) }
+								<UserInputSelectOptions
+									slug={ USER_INPUT_QUESTIONS_PURPOSE }
+									max={
+										USER_INPUT_MAX_ANSWERS[
+											USER_INPUT_QUESTIONS_PURPOSE
+										]
+									}
+									options={ omit(
+										USER_INPUT_ANSWERS_PURPOSE,
+										'other'
+									) }
+									descriptions={
+										USER_INPUT_ANSWERS_PURPOSE_DESCRIPTIONS
+									}
+								/>
+
+								{ error && (
+									<ErrorNotice
+										error={ error }
+										Icon={ WarningSVG }
+									/>
+								) }
+
+								<SpinnerButton
+									onClick={ onSaveClick }
+									isSaving={ isScreenLoading }
+									disabled={ hasErrorForAnswer( values ) }
+								>
+									{ __(
+										'Complete setup',
+										'google-site-kit'
+									) }
+								</SpinnerButton>
+							</Layout>
+						</Cell>
+					</Row>
+				</Grid>
 			</div>
 		</Fragment>
 	);
