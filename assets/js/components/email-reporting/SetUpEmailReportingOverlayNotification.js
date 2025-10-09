a13ecccb0f74d06eb198c7bf266882c8a7975d07
Aleksej <aleksej.vukomanovic@10up.com>
2025-10-09 20:29:27 +0200

    Add timeout for showing the tooltip.

--- assets/js/components/email-reporting/SetUpEmailReportingOverlayNotification.js
+++ assets/js/components/email-reporting/SetUpEmailReportingOverlayNotification.js
@@ -86,7 +86,9 @@ export default function SetUpEmailReportingOverlayNotification( {
 			dismissNotification( id );
 
 			if ( ! isUserSubscribed ) {
-				showTooltip();
+				setTimeout( () => {
+					showTooltip();
+				}, 310 ); // Wait until after the panel close animation.
 			}
 		}
 	}, [
