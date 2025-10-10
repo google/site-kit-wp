65dc0b17bb8db22700890cc764ecc7f7b50a91d3
Aleksej <aleksej.vukomanovic@10up.com>
2025-10-10 16:44:02 +0200

    Move utils out of CompatibilityErrorNotice.

--- assets/js/modules/sign-in-with-google/components/setup/CompatibilityChecks/CompatibilityErrorNotice.js
+++ assets/js/modules/sign-in-with-google/components/setup/CompatibilityChecks/CompatibilityErrorNotice.js
@@ -24,103 +24,17 @@ import PropTypes from 'prop-types';
 /**
  * WordPress dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import Notice from '@/js/components/Notice';
 import Typography from '@/js/components/Typography';
-
-export const ERROR_WP_LOGIN_INACCESSIBLE = 'ERROR_WP_LOGIN_INACCESSIBLE';
-export const ERROR_WPCOM = 'ERROR_WPCOM';
-export const ERROR_CONFLICTING_PLUGINS = 'ERROR_CONFLICTING_PLUGINS';
-
-const SLUG_TO_ERROR = {
-	wp_login_inaccessible: ERROR_WP_LOGIN_INACCESSIBLE,
-	host_wordpress_dot_com: ERROR_WPCOM,
-	conflicting_plugins: ERROR_CONFLICTING_PLUGINS,
-};
-
-/**
- * Normalizes the conflicting plugins data to an array of plugin info objects.
- *
- * @since n.e.x.t
- *
- * @param {Object|Array} conflictingPlugins Plugins data keyed by slug or array.
- * @return {Array} Array of plugin info objects.
- */
-function normalizeConflictingPlugins( conflictingPlugins ) {
-	if ( Array.isArray( conflictingPlugins ) ) {
-		return conflictingPlugins;
-	}
-
-	if (
-		conflictingPlugins &&
-		typeof conflictingPlugins === 'object' &&
-		Object.keys( conflictingPlugins ).length > 0
-	) {
-		return Object.values( conflictingPlugins );
-	}
-
-	return [];
-}
+import { getErrorMessage } from './utils';
 
 export default function CompatibilityErrorNotice( { errors } ) {
-	const errorMessages = [];
-
-	for ( const [ slug, value ] of Object.entries( errors ) ) {
-		const errorCode = SLUG_TO_ERROR[ slug ];
-
-		switch ( errorCode ) {
-			case ERROR_WP_LOGIN_INACCESSIBLE:
-				errorMessages.push(
-					__(
-						'Your login page (wp-login.php) is not accessible at the expected location. This can prevent Sign in with Google from functioning correctly.',
-						'google-site-kit'
-					)
-				);
-				break;
-
-			case ERROR_WPCOM:
-				errorMessages.push(
-					__(
-						'Sign in with Google does not function on sites hosted on wordpress.com.',
-						'google-site-kit'
-					)
-				);
-				break;
-
-			case ERROR_CONFLICTING_PLUGINS: {
-				const plugins = normalizeConflictingPlugins( value );
-
-				for ( const plugin of plugins ) {
-					const { pluginName, conflictMessage } = plugin || {};
-
-					if ( conflictMessage ) {
-						errorMessages.push( conflictMessage );
-						continue;
-					}
-
-					if ( pluginName ) {
-						errorMessages.push(
-							sprintf(
-								/* translators: %s: plugin name */
-								__(
-									'%s can interfere with Sign in with Google. When this plugin is active, Sign in with Google may not function properly',
-									'google-site-kit'
-								),
-								pluginName
-							)
-						);
-					}
-				}
-				break;
-			}
-			default:
-				break;
-		}
-	}
+	const errorMessages = getErrorMessage( errors );
 
 	if ( errorMessages.length === 0 ) {
 		return null;
