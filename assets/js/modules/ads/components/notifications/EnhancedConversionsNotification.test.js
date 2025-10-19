b4ed62f51810e1c83eb76d0fa724537688520afc
nfmohit <hello@nahid.dev>
2025-10-19 18:02:47 +0200

    Add test coverage for Ads `EnhancedConversionsNotification` component.

---
+++ assets/js/modules/ads/components/notifications/EnhancedConversionsNotification.test.js
@@ -0,0 +1,59 @@
+/**
+ * Ads EnhancedConversionsNotification component tests.
+ *
+ * Site Kit by Google, Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import {
+	createTestRegistry,
+	provideSiteInfo,
+	render,
+} from '../../../../../../tests/js/test-utils';
+import { withNotificationComponentProps } from '@/js/googlesitekit/notifications/util/component-props';
+import EnhancedConversionsNotification, {
+	ENHANCED_CONVERSIONS_NOTIFICATION_ADS,
+} from './EnhancedConversionsNotification';
+
+describe( 'Ads EnhancedConversionsNotification', () => {
+	let registry;
+
+	const EnhancedConversionsNotificationComponent =
+		withNotificationComponentProps( ENHANCED_CONVERSIONS_NOTIFICATION_ADS )(
+			EnhancedConversionsNotification
+		);
+
+	beforeEach( () => {
+		registry = createTestRegistry();
+
+		provideSiteInfo( registry );
+	} );
+
+	it( 'should render the notification', async () => {
+		const { container, waitForRegistry } = render(
+			<EnhancedConversionsNotificationComponent />,
+			{
+				registry,
+				features: [ 'gtagUserData' ],
+			}
+		);
+
+		await waitForRegistry();
+
+		expect( container ).toMatchSnapshot();
+	} );
+} );
