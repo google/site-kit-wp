7e0a92af1e785b68deab83e24a66b6af9e3cc665
Aleksej <aleksej.vukomanovic@10up.com>
2025-10-23 23:38:20 +0200

    Add attributes to the block.

--- includes/Modules/Sign_In_With_Google/Sign_In_With_Google_Block.php
+++ includes/Modules/Sign_In_With_Google/Sign_In_With_Google_Block.php
@@ -77,13 +77,45 @@ class Sign_In_With_Google_Block {
 	 * Render callback for the Sign in with Google block.
 	 *
 	 * @since 1.147.0
+	 * @since n.e.x.t Added the `$attributes` parameter.
+	 *
+	 * @param array $attributes Block attributes.
 	 * @return string Rendered block.
 	 */
-	public function render_callback() {
+	public function render_callback( $attributes = array() ) {
 		// If the user is already signed in, do not render a Sign in
 		// with Google button.
 		if ( is_user_logged_in() ) {
 			return '';
+		}
+
+		$attributes = is_array( $attributes ) ? $attributes : array();
+
+		$button_args = array();
+
+		$text_values = wp_list_pluck( Settings::TEXTS, 'value' );
+		if ( ! empty( $attributes['text'] ) && in_array( $attributes['text'], $text_values, true ) ) {
+			$button_args['text'] = $attributes['text'];
+		}
+
+		$theme_values = wp_list_pluck( Settings::THEMES, 'value' );
+		if ( ! empty( $attributes['theme'] ) && in_array( $attributes['theme'], $theme_values, true ) ) {
+			$button_args['theme'] = $attributes['theme'];
+		}
+
+		$shape_values = wp_list_pluck( Settings::SHAPES, 'value' );
+		if ( ! empty( $attributes['shape'] ) && in_array( $attributes['shape'], $shape_values, true ) ) {
+			$button_args['shape'] = $attributes['shape'];
+		}
+
+		if ( ! empty( $attributes['buttonClassName'] ) && is_string( $attributes['buttonClassName'] ) ) {
+			$classes = array_filter(
+				preg_split( '/\s+/', trim( $attributes['buttonClassName'] ) )
+			);
+
+			if ( ! empty( $classes ) ) {
+				$button_args['class'] = $classes;
+			}
 		}
 
 		ob_start();
@@ -94,7 +126,7 @@ class Sign_In_With_Google_Block {
 		 *
 		 * @param array $args Optional arguments to customize button attributes.
 		 */
-		do_action( 'googlesitekit_render_sign_in_with_google_button' );
+		do_action( 'googlesitekit_render_sign_in_with_google_button', $button_args );
 		return ob_get_clean();
 	}
 }
