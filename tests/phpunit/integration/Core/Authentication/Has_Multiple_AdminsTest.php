c6ec33d37152727db89da7cd0160ec5f8f6313be
Jimmy Madon <hello@jimmymadon.com>
2025-11-11 15:45:32 +0000

    Add tests for creating and deleting users affecting the has_multiple_admins transient.

--- tests/phpunit/integration/Core/Authentication/Has_Multiple_AdminsTest.php
+++ tests/phpunit/integration/Core/Authentication/Has_Multiple_AdminsTest.php
@@ -46,7 +46,42 @@ class Has_Multiple_AdminsTest extends TestCase {
 		$this->factory()->user->create( array( 'role' => 'administrator' ) );
 
 		// Delete the transient and check that we have multiple admins now.
-		$this->transients->delete( Has_Multiple_Admins::OPTION );
 		$this->assertTrue( $has_multiple_admins->get(), 'Should return true when there are multiple admin users' );
+	}
+
+	public function test_register__user_register_hook() {
+		$has_multiple_admins = new Has_Multiple_Admins( $this->transients );
+		$has_multiple_admins->register();
+
+		// Initially, there is only one admin (ID=1).
+		$this->assertFalse( $has_multiple_admins->get(), 'Should return false when there is only one admin user' );
+
+		// Create a new user with non-admin role, should not affect the transient.
+		$this->factory()->user->create( array( 'role' => 'editor' ) );
+		$this->assertFalse( $has_multiple_admins->get(), 'Should still return false when a non-admin user is created' );
+
+		// Create a new admin user, should clear the transient and reflect the change.
+		$this->factory()->user->create( array( 'role' => 'administrator' ) );
+		$this->assertTrue( $has_multiple_admins->get(), 'Should return true when a second admin user is created' );
+	}
+
+	public function test_register__deleted_user_hook() {
+		$has_multiple_admins = new Has_Multiple_Admins( $this->transients );
+		$has_multiple_admins->register();
+
+		// Create a second admin user and one non-admin user.
+		$admin_id_2   = $this->factory()->user->create( array( 'role' => 'administrator' ) );
+		$non_admin_id = $this->factory()->user->create( array( 'role' => 'editor' ) );
+
+		// Now we have multiple admins.
+		$this->assertTrue( $has_multiple_admins->get(), 'Should return true when there are multiple admin users' );
+
+		// Delete a non-admin user, should not affect the transient.
+		wp_delete_user( $non_admin_id );
+		$this->assertTrue( $has_multiple_admins->get(), 'Should still return true when a non-admin user is deleted' );
+
+		// Delete one of the admin users, should clear the transient and reflect the change.
+		wp_delete_user( $admin_id_2 );
+		$this->assertFalse( $has_multiple_admins->get(), 'Should return false when there is only one admin user left' );
 	}
 }
