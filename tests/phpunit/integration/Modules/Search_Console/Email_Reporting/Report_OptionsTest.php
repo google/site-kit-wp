4db6451cc8fd6487c789033c44b0a0818fde37d8
Aleksej <aleksej.vukomanovic@10up.com>
2025-11-13 11:00:17 +0100

    Move helper tests.

--- tests/phpunit/integration/Modules/Search_Console/Email_Reporting/Report_OptionsTest.php
+++ tests/phpunit/integration/Modules/Search_Console/Email_Reporting/Report_OptionsTest.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Class Google\Site_Kit\Tests\Core\Email_Reporting\Report_Options\Search_Console_Report_OptionsTest
+ * Class Google\Site_Kit\Tests\Modules\Search_Console\Email_Reporting\Report_OptionsTest
  *
- * @package   Google\Site_Kit\Tests\Core\Email_Reporting
+ * @package   Google\Site_Kit\Tests\Modules\Search_Console\Email_Reporting
  * @copyright 2025 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://sitekit.withgoogle.com
  */
 
-namespace Google\Site_Kit\Tests\Core\Email_Reporting\Report_Options;
+namespace Google\Site_Kit\Tests\Modules\Search_Console\Email_Reporting;
 
-use Google\Site_Kit\Core\Email_Reporting\Report_Options\Search_Console_Report_Options;
+use Google\Site_Kit\Modules\Search_Console\Email_Reporting\Search_Console_Report_Options;
 use Google\Site_Kit\Tests\TestCase;
 
 /**
