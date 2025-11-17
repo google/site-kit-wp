0fe7f2df82599e70428817a2e2157173554d7e33
Hussain Thajutheen <hussain.thajutheen@10up.com>
2025-11-17 12:13:03 +0300

    Add WC_Countries_Fake class for testing phone normalization.

---
+++ tests/phpunit/includes/WC_Countries_Fake.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * WC_Countries_Fake
+ *
+ * @package   Google\Site_Kit
+ * @copyright 2024 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+namespace Google\Site_Kit\Tests;
+
+/**
+ * Fake WC_Countries class for testing phone normalization.
+ */
+class WC_Countries_Fake {
+
+	/**
+	 * The calling code to return from get_country_calling_code.
+	 *
+	 * @var string
+	 */
+	public static $test_calling_code = '';
+
+	/**
+	 * Mock method to get country calling code.
+	 *
+	 * @param string $country Country code.
+	 * @return string Calling code.
+	 */
+	// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found -- Fake method signature must match WC_Countries.
+	public function get_country_calling_code( $country ) {
+		return self::$test_calling_code;
+	}
+}
