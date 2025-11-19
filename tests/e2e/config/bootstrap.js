c86b536d57ce0942eea1c3a42df223f3a18d4278
Ankit Gade <ankit.gade@10up.com>
2025-11-19 20:15:14 +0530

    Revert bootstrap changes.

--- tests/e2e/config/bootstrap.js
+++ tests/e2e/config/bootstrap.js
@@ -191,7 +191,6 @@ const pageEvents = [];
 
 // The Jest timeout is increased because these tests are a bit slow
 jest.setTimeout( PUPPETEER_TIMEOUT || 100000 );
-
 // Set default timeout for Puppeteer waits. (Default: 30 sec)
 page.setDefaultTimeout( 5_000 );
 // Set default timeout for individual expect-puppeteer assertions. (Default: 1000)
