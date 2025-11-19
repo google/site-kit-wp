0e97f0b73cabf1244ea3c9cf05fef9f13f3ea778
Ankit Gade <ankit.gade@10up.com>
2025-11-19 15:50:22 +0530

    Add CDP protocol.

--- tests/e2e/jest-puppeteer.config.js
+++ tests/e2e/jest-puppeteer.config.js
@@ -18,6 +18,7 @@
 
 module.exports = {
 	launch: {
+		protocol: 'cdp',
 		// devtools, headless and slowmo properties inlined from @wordpress/script config/puppeteer.config.js v12.0.0.
 		// https://github.com/WordPress/gutenberg/blob/8e06f0d212f89adba9099106497117819adefc5a/packages/scripts/config/puppeteer.config.js
 		devtools: process.env.PUPPETEER_DEVTOOLS === 'true',
@@ -28,4 +29,5 @@ module.exports = {
 			'--disable-web-security', // Fixes SSL issues that may happen on when you run e2e localy.
 		],
 	},
+	browserContext: 'default',
 };
