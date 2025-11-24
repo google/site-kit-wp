f7218ad44e70119df532514055008411bd7b6902
Jimmy Madon <hello@jimmymadon.com>
2025-11-24 23:40:45 +0000

    Add REST route test.

--- tests/phpunit/integration/Core/Email_Reporting/REST_Email_Reporting_ControllerTest.php
+++ tests/phpunit/integration/Core/Email_Reporting/REST_Email_Reporting_ControllerTest.php
@@ -70,6 +70,7 @@ class REST_Email_Reporting_ControllerTest extends TestCase {
 		$server     = rest_get_server();
 		$routes     = array(
 			'/' . REST_Routes::REST_ROOT . '/core/site/data/email-reporting',
+			'/' . REST_Routes::REST_ROOT . '/core/site/data/was-analytics-4-connected',
 		);
 		$get_routes = array_intersect( $routes, array_keys( $server->get_routes() ) );
 
