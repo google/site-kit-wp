d2d6e1c911621856273f4e21a73cf21109f31070
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2025-12-11 00:50:09 +0000

    Use full import path.

--- includes/Core/Conversion_Tracking/Conversion_Event_Providers/WooCommerce.php
+++ includes/Core/Conversion_Tracking/Conversion_Event_Providers/WooCommerce.php
@@ -195,7 +195,7 @@ class WooCommerce extends Conversion_Events_Provider {
 			function ( $button, $product ) {
 				// If the product is not a valid WC_Product instance, return
 				// early.
-				if ( false === $product instanceof \WC_Product ) {
+				if ( false === $product instanceof \WooCommerce\Abstracts\WC_Product ) {
 					return $button;
 				}
 
@@ -212,7 +212,7 @@ class WooCommerce extends Conversion_Events_Provider {
 			function ( $cart_item_key, $product_id, $quantity, $variation_id, $variation ) {
 				$product = wc_get_product( $product_id );
 
-				if ( false === $product instanceof \WC_Product ) {
+				if ( false === $product instanceof \WooCommerce\Abstracts\WC_Product ) {
 					return;
 				}
 
@@ -277,7 +277,7 @@ class WooCommerce extends Conversion_Events_Provider {
 	 *
 	 * @return array
 	 */
-	protected function get_formatted_product( $product, $variation_id = 0, $variation = false, $quantity = false ) {
+	protected function get_formatted_product( WC_Product $product, $variation_id = 0, $variation = false, $quantity = false ) {
 		$product_id = $product->is_type( 'variation' ) ? $product->get_parent_id() : $product->get_id();
 		$price      = $product->get_price();
 
@@ -358,7 +358,7 @@ class WooCommerce extends Conversion_Events_Provider {
 				function ( $item ) {
 					// If the product is not a valid WC_Product instance, return
 					// early.
-					if ( $item->get_product() instanceof \WC_Product === false ) {
+					if ( $item->get_product() instanceof \WooCommerce\Abstracts\WC_Product === false ) {
 						return $item;
 					}
 
