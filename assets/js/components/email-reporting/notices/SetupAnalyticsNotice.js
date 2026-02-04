d2a98dad316e1038f852f0e6e6f502206ca0c345
Ben Bowler <ben@benbowler.com>
2026-02-04 10:50:00 +0000

    Correctly track SetupAnalyticsNotice learn more link event.

--- assets/js/components/email-reporting/notices/SetupAnalyticsNotice.js
+++ assets/js/components/email-reporting/notices/SetupAnalyticsNotice.js
@@ -38,9 +38,9 @@ import { CORE_MODULES } from '@/js/googlesitekit/modules/datastore/constants';
 import { MODULE_SLUG_ANALYTICS_4 } from '@/js/modules/analytics-4/constants';
 import useActivateModuleCallback from '@/js/hooks/useActivateModuleCallback';
 import useCompleteModuleActivationCallback from '@/js/hooks/useCompleteModuleActivationCallback';
-import Link from '@/js/components/Link';
 import useViewOnly from '@/js/hooks/useViewOnly';
 import useNotificationEvents from '@/js/googlesitekit/notifications/hooks/useNotificationEvents';
+import LearnMoreLink from '@/js/googlesitekit/notifications/components/common/LearnMoreLink';
 import withIntersectionObserver from '@/js/util/withIntersectionObserver';
 
 export const EMAIL_REPORTING_SETUP_ANALYTICS_NOTICE_DISMISSED_ITEM =
@@ -136,7 +136,7 @@ export default function SetupAnalyticsNotice() {
 		<NoticeWithIntersectionObserver
 			type={ TYPES.NEW }
 			title={ __(
-				'Understand how visitors interact with your content',
+				'HERE Understand how visitors interact with your content',
 				'google-site-kit'
 			) }
 			description={ createInterpolateElement(
@@ -145,7 +145,13 @@ export default function SetupAnalyticsNotice() {
 					'google-site-kit'
 				),
 				{
-					a: <Link href={ learnMoreLink } external />,
+					a: (
+						<LearnMoreLink
+							id={ EMAIL_REPORTS_SETUP_ANALYTICS_NOTICE_SLUG }
+							label={ __( 'Learn more', 'google-site-kit' ) }
+							url={ learnMoreLink }
+						/>
+					),
 				}
 			) }
 			ctaButton={ {
