7b3d502fb96171f26a3349a428802dfc568ec111
Hussain Thajutheen <hussain.thajutheen@10up.com>
2026-02-04 20:37:26 +0530

    Add className to PolicyViolationSettingsNotice for styling.

--- assets/js/modules/reader-revenue-manager/components/common/PolicyViolationSettingsNotice.tsx
+++ assets/js/modules/reader-revenue-manager/components/common/PolicyViolationSettingsNotice.tsx
@@ -56,6 +56,7 @@ const PolicyViolationSettingsNotice: FC = () => {
 	return (
 		<Notice
 			// @ts-expect-error - The `Notice` component is not typed yet.
+			className="googlesitekit-policy-violation-settings-notice"
 			type={ type }
 			title={ title }
 			description={ description }
