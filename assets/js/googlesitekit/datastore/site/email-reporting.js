88eba7a167bb04eadcdc0f4033ae6b43aba82da4
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2026-02-05 12:22:57 +0000

    Add `null` to return type.

--- assets/js/googlesitekit/datastore/site/email-reporting.js
+++ assets/js/googlesitekit/datastore/site/email-reporting.js
@@ -276,7 +276,7 @@ const baseSelectors = {
 	 * @since n.e.x.t
 	 *
 	 * @param {Object} state Data store's state.
-	 * @return {(string|undefined)} Category ID of the latest email reporting error; `undefined` if not loaded; null if no errors or category ID is not present for the latest error.
+	 * @return {(string|null|undefined)} Category ID of the latest email reporting error; `undefined` if not loaded; null if no errors or category ID is not present for the latest error.
 	 */
 	getLatestEmailReportingErrorCategoryID: createRegistrySelector(
 		( select ) => () => {
