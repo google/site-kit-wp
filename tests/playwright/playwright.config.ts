05f99bd885e19457cdda2dce1ba9eedd3ab35735
Eugene Manuilov <eugene.manuilov@gmail.com>
2026-02-18 17:20:38 +0200

    Add the initial configuration for the playwright infrastructure.

---
+++ tests/playwright/playwright.config.ts
@@ -0,0 +1,55 @@
+/**
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig( {
+	testDir: './specs',
+	outputDir: 'artifacts/playwright-output',
+	fullyParallel: true,
+	forbidOnly: !! process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: [
+		[ 'list' ],
+		[
+			'html',
+			{
+				open: 'never',
+				outputFolder: 'artifacts/playwright-html',
+			},
+		],
+	],
+	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+	use: {
+		/* Base URL to use in actions like `await page.goto('')`. */
+		// baseURL: 'http://localhost:3000',
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: 'on-first-retry',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices[ 'Desktop Chrome' ] },
+		},
+	],
+} );
