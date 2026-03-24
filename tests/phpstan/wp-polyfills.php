2baf166e219d57bb6bb2269bc8ebc595e830af3f
Ben Bowler <ben@benbowler.com>
2026-03-24 09:24:49 +0000

    Address additional code review comments.

---
+++ tests/phpstan/wp-polyfills.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Stubs for PHP 8 functions polyfilled by WordPress.
+ *
+ * These functions are available in WordPress since 5.2.20+
+ * via wp-includes/compat.php.
+ *
+ * @package   Google\Site_Kit
+ * @copyright 2026 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://sitekit.withgoogle.com
+ */
+
+if ( ! function_exists( 'str_starts_with' ) ) {
+	/**
+	 * Polyfill for `str_starts_with()` function added in PHP 8.0.
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle The substring to search for in the haystack.
+	 * @return bool True if haystack begins with needle, false otherwise.
+	 */
+	function str_starts_with( string $haystack, string $needle ): bool {
+		return 0 === strncmp( $haystack, $needle, strlen( $needle ) );
+	}
+}
+
+if ( ! function_exists( 'str_contains' ) ) {
+	/**
+	 * Polyfill for `str_contains()` function added in PHP 8.0.
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle The substring to search for in the haystack.
+	 * @return bool True if needle is in haystack, false otherwise.
+	 */
+	function str_contains( string $haystack, string $needle ): bool {
+		return '' === $needle || false !== strpos( $haystack, $needle );
+	}
+}
