c3da5b201fa6014f1fe9e2625487571ef662d0cb
Aleksej <aleksej.vukomanovic@10up.com>
2026-03-26 22:30:40 +0100

    Export SelectionPanelContent.

--- assets/js/components/SelectionPanel/index.js
+++ assets/js/components/SelectionPanel/index.js
@@ -19,11 +19,11 @@
 /**
  * Internal dependencies
  */
-import SelectionPanel, { SelectionPanelContent } from './SelectionPanel';
-export { SelectionPanelContent };
+import SelectionPanel from './SelectionPanel';
 export { default as SelectionPanelHeader } from './SelectionPanelHeader';
 export { default as SelectionPanelItem } from './SelectionPanelItem';
 export { default as SelectionPanelItems } from './SelectionPanelItems';
 export { default as SelectionPanelFooter } from './SelectionPanelFooter';
+export { default as SelectionPanelContent } from './SelectionPanelContent';
 
 export default SelectionPanel;
