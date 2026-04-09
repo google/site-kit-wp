bb8c34fdcb9a1d672d3062be19d6765f7beef573
nfmohit <hello@nahid.dev>
2026-04-09 14:17:50 +0600

    Add `AnalyticsAccountCreationErrorNotice` component for handling account creation errors.

---
+++ assets/js/modules/analytics-4/components/common/AccountCreate/AnalyticsAccountCreationErrorNotice.tsx
@@ -0,0 +1,134 @@
+/**
+ * AnalyticsAccountCreationErrorNotice component.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import type { FC, ReactNode } from 'react';
+
+/**
+ * WordPress dependencies
+ */
+import { createInterpolateElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { useSelect, type Select } from 'googlesitekit-data';
+import Link from '@/js/components/Link';
+import Notice from '@/js/components/Notice';
+import { TYPES } from '@/js/components/Notice/constants';
+import { CORE_SITE } from '@/js/googlesitekit/datastore/site/constants';
+
+export interface AnalyticsAccountCreationErrorNoticeProps {
+	errorCode: string;
+	onRetry: () => void;
+}
+
+const AnalyticsAccountCreationErrorNotice: FC<
+	AnalyticsAccountCreationErrorNoticeProps
+> = ( { errorCode, onRetry } ) => {
+	const analyticsAccountLimitHelpURL = useSelect( ( select: Select ) =>
+		select( CORE_SITE ).getGoogleSupportURL( {
+			path: '/analytics/',
+			hash: 'topic=14090456',
+		} )
+	);
+
+	const additionalAnalyticsSupportURL = useSelect( ( select: Select ) =>
+		select( CORE_SITE ).getDocumentationLinkURL(
+			'analytics-additional-support'
+		)
+	);
+
+	const title = __( 'Analytics account creation failed', 'google-site-kit' );
+
+	let description: ReactNode;
+	let ctaButton: {
+		label: string;
+		onClick?: () => void;
+		href?: string;
+		external?: boolean;
+	};
+
+	if ( errorCode === 'user_cancel' ) {
+		description = __(
+			'Creating a new Analytics account failed because the Terms of Service were not accepted. Go to Analytics to accept the Terms of Service.',
+			'google-site-kit'
+		);
+		ctaButton = {
+			label: __( 'Go to Analytics', 'google-site-kit' ),
+			onClick: () => {
+				global.history.back();
+			},
+		};
+	} else if ( errorCode === 'max_accounts_reached' ) {
+		description = createInterpolateElement(
+			__(
+				'Creating a new Analytics account failed because the Analytics account limit has been reached. Try again or <a>get help</a>',
+				'google-site-kit'
+			),
+			{
+				a: (
+					<Link
+						// @ts-expect-error - The `Link` component is not currently typed.
+						href={ analyticsAccountLimitHelpURL || '' }
+						external
+					/>
+				),
+			}
+		);
+		ctaButton = {
+			label: __( 'Retry', 'google-site-kit' ),
+			onClick: onRetry,
+		};
+	} else {
+		description = createInterpolateElement(
+			__(
+				'Something went wrong. Try again or <a>get help</a>',
+				'google-site-kit'
+			),
+			{
+				a: (
+					<Link
+						// @ts-expect-error - The `Link` component is not currently typed.
+						href={ additionalAnalyticsSupportURL }
+						external
+					/>
+				),
+			}
+		);
+		ctaButton = {
+			label: __( 'Retry', 'google-site-kit' ),
+			onClick: onRetry,
+		};
+	}
+
+	return (
+		<Notice
+			// @ts-expect-error - The `Notice` component is not typed yet.
+			type={ TYPES.ERROR }
+			title={ title }
+			description={ description }
+			ctaButton={ ctaButton }
+		/>
+	);
+};
+
+export default AnalyticsAccountCreationErrorNotice;
