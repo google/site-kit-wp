3b2e87b36cde21a81b54ba7fab7deb98c316736d
nfmohit <hello@nahid.dev>
2026-04-09 14:18:08 +0600

    Add stories for `AnalyticsAccountCreationErrorNotice` component.

---
+++ assets/js/modules/analytics-4/components/common/AccountCreate/AnalyticsAccountCreationErrorNotice.stories.tsx
@@ -0,0 +1,82 @@
+/**
+ * AnalyticsAccountCreationErrorNotice Component Stories.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import type { ElementType, ReactNode } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import AnalyticsAccountCreationErrorNotice from './AnalyticsAccountCreationErrorNotice';
+import {
+	createTestRegistry,
+	provideSiteInfo,
+} from '../../../../../../../tests/js/utils';
+import WithRegistrySetup from '../../../../../../../tests/js/WithRegistrySetup';
+
+type Registry = ReturnType< typeof createTestRegistry >;
+
+function Template( args: { errorCode: string; onRetry: () => void } ) {
+	return (
+		<AnalyticsAccountCreationErrorNotice
+			errorCode={ args.errorCode }
+			onRetry={ args.onRetry }
+		/>
+	);
+}
+
+export const TermsOfServiceNotAccepted = Template.bind( {} );
+TermsOfServiceNotAccepted.storyName = 'Terms of service not accepted';
+TermsOfServiceNotAccepted.args = {
+	errorCode: 'user_cancel',
+	onRetry: () => {},
+};
+
+export const AccountLimitReached = Template.bind( {} );
+AccountLimitReached.storyName = 'Account limit reached';
+AccountLimitReached.args = {
+	errorCode: 'max_accounts_reached',
+	onRetry: () => {},
+};
+
+export const GenericError = Template.bind( {} );
+GenericError.storyName = 'Generic error';
+GenericError.args = {
+	errorCode: 'backend_error',
+	onRetry: () => {},
+};
+
+export default {
+	title: 'Modules/Analytics4/Components/AccountCreate/AnalyticsAccountCreationErrorNotice',
+	component: AnalyticsAccountCreationErrorNotice,
+	decorators: [
+		( Story: ElementType ): ReactNode => {
+			return (
+				<WithRegistrySetup
+					func={ ( registry: Registry ) => {
+						provideSiteInfo( registry );
+					} }
+				>
+					<Story />
+				</WithRegistrySetup>
+			);
+		},
+	],
+};
