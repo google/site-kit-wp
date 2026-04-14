791be14de8cc288bf2d932b9ae021a7a0ee079a0
Ankit Gade <ankit.gade@10up.com>
2026-04-14 10:45:12 +0530

    Revert story update and VRT images.

--- assets/js/components/Banner/index.stories.js
+++ assets/js/components/Banner/index.stories.js
@@ -179,7 +179,15 @@ function Template() {
 					<div className="googlesitekit-banner-notification googlesitekit-banner-notification--info">
 						<Banner
 							title="Analytics is gathering data"
-							description="It may take up to 72 hours before stats show up for your site."
+							description="It can take up to 72 hours before stats show up for your site. While you’re waiting, connect more services to get more stats."
+							ctaButton={ {
+								label: 'Connect more services',
+								onClick: () => {},
+							} }
+							dismissButton={ {
+								label: 'Got it',
+								onClick: () => {},
+							} }
 							svg={ {
 								desktop: gatheringDataSVG,
 								verticalPosition: 'center',
