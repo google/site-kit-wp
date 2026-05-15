5e61bfaf814c0af04e65f10596f7692b1d4169f2
Aleksej <aleksej.vukomanovic@10up.com>
2026-05-15 17:01:42 +0200

    Add device type goal driver stories.

---
+++ assets/js/modules/analytics-4/components/site-goals/goal-drivers/DeviceTypeGoalDriver.stories.tsx
@@ -0,0 +1,101 @@
+/**
+ * DeviceTypeGoalDriver component stories.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { ReactElement } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { Story } from '@/js/types/Story';
+import DeviceTypeGoalDriver from './DeviceTypeGoalDriver';
+import { GoalDriverComponentProps } from './types';
+
+type DeviceTypeGoalDriverStoryProps = GoalDriverComponentProps;
+
+export default {
+	title: 'Modules/Analytics4/Components/Site Goals/GoalDriverTiles/DeviceType',
+	component: DeviceTypeGoalDriver,
+	decorators: [
+		( StoryComponent: () => ReactElement ) => (
+			<div className="googlesitekit-widget">
+				<div className="googlesitekit-widget__body">
+					<StoryComponent />
+				</div>
+			</div>
+		),
+	],
+};
+
+function Template( props: DeviceTypeGoalDriverStoryProps ) {
+	return <DeviceTypeGoalDriver { ...props } />;
+}
+
+const RETRYABLE_ERROR = {
+	code: 400,
+	message: 'Data loading failed',
+	data: {
+		status: 400,
+		reason: 'badRequest',
+	},
+};
+
+export const Ready = Template.bind(
+	{}
+) as Story< DeviceTypeGoalDriverStoryProps >;
+Ready.args = {
+	goalType: 'ecommerce',
+	rows: [
+		{ label: 'Mobile', value: '56.5%' },
+		{ label: 'Tablet', value: '41.3%' },
+		{ label: 'Desktop', value: '2.2%' },
+	],
+	loading: false,
+	limit: 3,
+};
+Ready.scenario = {};
+
+export const Loading = Template.bind(
+	{}
+) as Story< DeviceTypeGoalDriverStoryProps >;
+Loading.args = {
+	...Ready.args,
+	rows: [],
+	loading: true,
+};
+
+export const NoData = Template.bind(
+	{}
+) as Story< DeviceTypeGoalDriverStoryProps >;
+NoData.args = {
+	...Ready.args,
+	rows: [],
+	loading: false,
+};
+
+export const Error = Template.bind(
+	{}
+) as Story< DeviceTypeGoalDriverStoryProps >;
+Error.args = {
+	...Ready.args,
+	rows: [],
+	loading: false,
+	error: RETRYABLE_ERROR,
+};
