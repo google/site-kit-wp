ab9ef9fc740555e14dccf20c30874910bfcdd06c
nfmohit <nahid.mohit@10up.com>
2026-05-27 09:35:49 +0600

    Change scroll approach to use DOM id.

--- assets/js/components/notifications/InternalServerError.js
+++ assets/js/components/notifications/InternalServerError.js
@@ -19,7 +19,7 @@
 /**
  * WordPress dependencies
  */
-import { useEffect, useRef } from '@wordpress/element';
+import { useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -30,32 +30,40 @@ import BannerNotification, {
 	TYPES,
 } from '@/js/googlesitekit/notifications/components/layout/BannerNotification';
 import Notification from '@/js/googlesitekit/notifications/components/Notification';
+import { useBreakpoint } from '@/js/hooks/useBreakpoint';
+import { getNavigationalScrollTop } from '@/js/util/scroll';
+
+const NOTIFICATION_ID = 'internal-server-error';
 
 export default function InternalServerError() {
-	const notificationRef = useRef();
+	const breakpoint = useBreakpoint();
 	const error = useSelect( ( select ) =>
 		select( CORE_SITE ).getInternalServerError()
 	);
 
 	// Scroll to the notification when the error is present.
 	useEffect( () => {
-		if ( error ) {
-			notificationRef.current?.scrollIntoView( {
-				behavior: 'smooth',
-				block: 'nearest',
-				inline: 'nearest',
-			} );
+		if ( ! error ) {
+			return;
 		}
-	}, [ error ] );
+
+		global.scrollTo( {
+			top: getNavigationalScrollTop(
+				`#${ NOTIFICATION_ID }`,
+				breakpoint
+			),
+			behavior: 'smooth',
+		} );
+	}, [ error, breakpoint ] );
 
 	if ( ! error ) {
 		return null;
 	}
 
 	return (
-		<Notification id="internal-server-error" ref={ notificationRef }>
+		<Notification id={ NOTIFICATION_ID }>
 			<BannerNotification
-				notificationID="internal-server-error"
+				notificationID={ NOTIFICATION_ID }
 				type={ TYPES.ERROR }
 				title={ error.title }
 				description={ error.description }
