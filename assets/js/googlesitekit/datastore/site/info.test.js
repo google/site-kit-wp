3696b739303a2f5c242e8482ff0c45b608f139af
Aleksej <aleksej.vukomanovic@10up.com>
2026-05-28 20:18:01 +0200

    Update info store tests.

--- assets/js/googlesitekit/datastore/site/info.test.js
+++ assets/js/googlesitekit/datastore/site/info.test.js
@@ -51,6 +51,8 @@ describe( 'core/site site info', () => {
 		],
 		productPostType: [ 'product' ],
 		isMultisite: false,
+		hasActiveLeadEventProviders: false,
+		hasActiveEcommerceEventProviders: false,
 	};
 	const entityInfoVar = '_googlesitekitEntityData';
 	const entityInfo = {
@@ -483,6 +485,11 @@ describe( 'core/site site info', () => {
 			[ 'isKeyMetricsSetupCompleted', 'keyMetricsSetupCompletedBy' ],
 			[ 'getConsentModeRegions', 'consentModeRegions' ],
 			[ 'isMultisite', 'isMultisite' ],
+			[ 'hasActiveLeadEventProviders', 'hasActiveLeadEventProviders' ],
+			[
+				'hasActiveEcommerceEventProviders',
+				'hasActiveEcommerceEventProviders',
+			],
 		] )( '%s', ( selector, infoKey ) => {
 			it( 'uses a resolver to load site info then returns the info when this specific selector is used', async () => {
 				global[ baseInfoVar ] = baseInfo;
