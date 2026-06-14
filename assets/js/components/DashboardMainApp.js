0f2cc86742f6cd938be119273dd4ca0f61bc0bd5
Eugene Manuilov <eugene.manuilov@fueled.com>
2026-06-14 14:17:00 +0300

    Remove proactive user engagement feature from DashboardMainApp component.

--- assets/js/components/DashboardMainApp.js
+++ assets/js/components/DashboardMainApp.js
@@ -298,7 +298,6 @@ export default function DashboardMainApp() {
 		);
 	} );
 
-	const emailReportingEnabled = useFeature( 'proactiveUserEngagement' );
 	const setupFlowRefreshEnabled = useFeature( 'setupFlowRefresh' );
 	const pdfGenerationEnabled = useFeature( 'pdfGeneration' );
 
@@ -472,12 +471,8 @@ export default function DashboardMainApp() {
 				</Fragment>
 			) }
 
-			{ emailReportingEnabled && (
-				<Fragment>
-					<UserSettingsSelectionPanel />
-					<PUESurveyTriggers />
-				</Fragment>
-			) }
+			<UserSettingsSelectionPanel />
+			<PUESurveyTriggers />
 
 			{ configuredAudiences && <AudienceSelectionPanel /> }
 
