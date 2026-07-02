974bc4313b656d6b291afcf413d0d2e2adfcf12d
nfmohit <nahid.mohit@10up.com>
2026-07-02 10:18:22 +0200

    Add necessary hook types.

---
+++ assets/js/components/setup/hooks/types.ts
@@ -0,0 +1,60 @@
+/**
+ * Module setup hooks types.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import { GATrackingEventArgs } from '@/js/types/GATrackingEventArgs';
+
+/**
+ * Options for the `useFinishSetup` hook.
+ *
+ * @since n.e.x.t
+ */
+export interface UseFinishSetupOptions {
+	gaTrackingEventArgs?: GATrackingEventArgs;
+}
+
+/**
+ * Options for the `useModuleSetupTracking` hook.
+ *
+ * @since n.e.x.t
+ */
+export interface UseModuleSetupTrackingOptions {
+	viewGATrackingEventArgs?: GATrackingEventArgs;
+	cancelGATrackingEventArgs?: GATrackingEventArgs;
+}
+
+/**
+ * Callback returned by `useFinishSetup`.
+ *
+ * Clears the module setup cache, tracks a completion event, and redirects
+ * the user to the Site Kit dashboard or an optional custom URL.
+ *
+ * @since n.e.x.t
+ */
+export type FinishSetupCallback = ( redirectURL?: string ) => Promise< void >;
+
+/**
+ * Return value of the `useModuleSetupTracking` hook.
+ *
+ * @since n.e.x.t
+ */
+export interface UseModuleSetupTrackingReturn {
+	trackCancel: () => Promise< void >;
+}
