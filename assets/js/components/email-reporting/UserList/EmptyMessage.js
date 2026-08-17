ec36fdd848874c983ab97c73f64da97ca08e0f01
Aleksej <aleksej.vukomanovic@10up.com>
2026-08-17 17:01:22 +0800

    Add shared empty-list message component.

---
+++ assets/js/components/email-reporting/UserList/EmptyMessage.js
@@ -0,0 +1,50 @@
+/**
+ * EmptyMessage component.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Gets the message shown when a search matches no users.
+ *
+ * Shared by the invite and subscribed-users lists so their empty-search
+ * copy can't drift apart. Called at render time, since `__()` needs
+ * translations to already be loaded.
+ *
+ * @since n.e.x.t
+ *
+ * @return {string} The empty-search message.
+ */
+export function getNoSearchResultsText() {
+	return __( 'No users match your search.', 'google-site-kit' );
+}
+
+export default function EmptyMessage( { text } ) {
+	return <div className="googlesitekit-user-list__empty">{ text }</div>;
+}
+
+EmptyMessage.propTypes = {
+	text: PropTypes.string.isRequired,
+};
