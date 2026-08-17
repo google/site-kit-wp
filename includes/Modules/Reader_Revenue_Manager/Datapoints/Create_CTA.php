0d0f99c300cc4a4e1bfcc574e0739d1b74d26c17
Hussain Thajutheen <hussain.thajutheen@10up.com>
2026-08-17 19:04:33 +0530

    Accept a generic CTA type and config, routed through the CTA type handler registry.

--- includes/Modules/Reader_Revenue_Manager/Datapoints/Create_CTA.php
+++ includes/Modules/Reader_Revenue_Manager/Datapoints/Create_CTA.php
@@ -13,9 +13,11 @@ namespace Google\Site_Kit\Modules\Reader_Revenue_Manager\Datapoints;
 use Google\Site_Kit\Core\Modules\Datapoint;
 use Google\Site_Kit\Core\Modules\Executable_Datapoint;
 use Google\Site_Kit\Core\REST_API\Data_Request;
+use Google\Site_Kit\Core\REST_API\Exception\Invalid_Param_Exception;
 use Google\Site_Kit\Core\REST_API\Exception\Missing_Required_Param_Exception;
+use Google\Site_Kit\Modules\Reader_Revenue_Manager\Datapoints\CTA\CTA_Type_Handler_Interface;
+use Google\Site_Kit\Modules\Reader_Revenue_Manager\Datapoints\CTA\Newsletter_Signup_CTA_Type_Handler;
 use Google\Site_Kit_Dependencies\Google\Service\Webcontentpublisher\Cta;
-use Google\Site_Kit_Dependencies\Google\Service\Webcontentpublisher\NewsletterConfig;
 
 /**
  * Class for the CTA creation datapoint.
@@ -27,11 +29,6 @@ use Google\Site_Kit_Dependencies\Google\Service\Webcontentpublisher\NewsletterCo
 class Create_CTA extends Datapoint implements Executable_Datapoint {
 
 	/**
-	 * Newsletter sign-up CTA type. This is the only type the API currently supports.
-	 */
-	const TYPE_NEWSLETTER_SIGNUP = 'NEWSLETTER_SIGNUP';
-
-	/**
 	 * Creates a request object.
 	 *
 	 * @since n.e.x.t
@@ -39,12 +36,36 @@ class Create_CTA extends Datapoint implements Executable_Datapoint {
 	 * @param Data_Request $data_request Data request object.
 	 * @return mixed Request object.
 	 * @throws Missing_Required_Param_Exception Thrown if a required parameter is missing.
+	 * @throws Invalid_Param_Exception          Thrown if a parameter is invalid.
 	 */
 	public function create_request( Data_Request $data_request ) {
-		foreach ( array( 'organizationID', 'publicationID', 'newsletterConfig' ) as $required_param ) {
+		foreach ( array( 'organizationID', 'publicationID', 'type', 'config' ) as $required_param ) {
 			if ( empty( $data_request[ $required_param ] ) ) {
 				throw new Missing_Required_Param_Exception( $required_param );
 			}
+		}
+
+		$handlers = $this->get_cta_type_handlers();
+		$type     = $data_request['type'];
+
+		if ( ! isset( $handlers[ $type ] ) ) {
+			throw new Invalid_Param_Exception( 'type' );
+		}
+
+		if ( ! is_array( $data_request['config'] ) ) {
+			throw new Invalid_Param_Exception( 'config' );
+		}
+
+		if ( isset( $data_request['displayName'] ) && ! is_string( $data_request['displayName'] ) ) {
+			throw new Invalid_Param_Exception( 'displayName' );
+		}
+
+		$cta = new Cta();
+
+		$handlers[ $type ]->configure_cta( $cta, $data_request['config'] );
+
+		if ( ! empty( $data_request['displayName'] ) ) {
+			$cta->setDisplayName( $data_request['displayName'] );
 		}
 
 		$parent = sprintf(
@@ -53,15 +74,29 @@ class Create_CTA extends Datapoint implements Executable_Datapoint {
 			$data_request['publicationID']
 		);
 
-		$cta = new Cta();
-		$cta->setType( self::TYPE_NEWSLETTER_SIGNUP );
-		$cta->setNewsletterConfig( new NewsletterConfig( $data_request['newsletterConfig'] ) );
+		return $this->get_service()->organizations_publications_ctas->create( $parent, $cta );
+	}
 
-		if ( ! empty( $data_request['displayName'] ) ) {
-			$cta->setDisplayName( $data_request['displayName'] );
+	/**
+	 * Gets the supported CTA type handlers, keyed by CTA type.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return CTA_Type_Handler_Interface[] CTA type handlers.
+	 */
+	private function get_cta_type_handlers() {
+		$handlers = array();
+
+		$type_handlers = array(
+			new Newsletter_Signup_CTA_Type_Handler(),
+			// Register additional CTA type handlers here as they are added.
+		);
+
+		foreach ( $type_handlers as $handler ) {
+			$handlers[ $handler->get_type() ] = $handler;
 		}
 
-		return $this->get_service()->organizations_publications_ctas->create( $parent, $cta );
+		return $handlers;
 	}
 
 	/**
