207e8fc4e7c4c902dc14eac131eb9620a2492ffe
Jacob Peattie <jacobpeattie@me.com>
2026-09-08 18:20:54 +1000

    Build the FeatureDiscoveryApp shell.

---
+++ assets/js/components/feature-discovery/FeatureDiscoveryContent.test.tsx
@@ -0,0 +1,61 @@
+/**
+ * Feature Discovery routed content tests.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import { render } from '@tests/js/test-utils';
+import FeatureDiscoveryContent from './FeatureDiscoveryContent';
+
+describe( 'FeatureDiscoveryContent', () => {
+	it.each( [
+		[
+			'/all-services',
+			'Feature Discovery Hub tab panel placeholder: All services and features',
+		],
+		[
+			'/whats-new',
+			'Feature Discovery Hub tab panel placeholder: What’s new?',
+		],
+	] )( 'should render the tab panel content for %s', ( route, text ) => {
+		const { getByText } = render( <FeatureDiscoveryContent />, { route } );
+
+		expect( getByText( text ) ).toBeInTheDocument();
+	} );
+
+	it.each( [ '/', '/unknown' ] )(
+		'should redirect %s to whats-new',
+		( route ) => {
+			const { getByText, history } = render(
+				<FeatureDiscoveryContent />,
+				{
+					route,
+				}
+			);
+
+			expect( history.location.pathname ).toBe( '/whats-new' );
+			expect( history.action ).toBe( 'REPLACE' );
+
+			expect(
+				getByText(
+					'Feature Discovery Hub tab panel placeholder: What’s new?'
+				)
+			).toBeInTheDocument();
+		}
+	);
+} );
