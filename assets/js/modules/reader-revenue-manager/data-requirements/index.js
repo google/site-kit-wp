1e038b97370e64e084c89c6101d7a0edc3cc77b1
Jimmy Madon <hello@jimmymadon.com>
2026-09-10 03:08:27 +0530

    Add publication onboarding state data requirement.

---
+++ assets/js/modules/reader-revenue-manager/data-requirements/index.js
@@ -0,0 +1,44 @@
+/**
+ * Reader Revenue Manager module data requirements.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import { WPDataRegistry } from 'googlesitekit-data';
+import { MODULES_READER_REVENUE_MANAGER } from '@/js/modules/reader-revenue-manager/datastore/constants';
+
+/**
+ * Returns a function that checks if the publication onboarding state matches the given state.
+ *
+ * @since n.e.x.t
+ *
+ * @param {string|undefined} state Publication onboarding state to match.
+ * @return {function(WPDataRegistry): Promise<boolean>} Whether the publication onboarding state matches or not.
+ */
+export function requirePublicationOnboardingState( state ) {
+	return async ( { select, resolveSelect } ) => {
+		await resolveSelect( MODULES_READER_REVENUE_MANAGER ).getSettings();
+
+		return (
+			state ===
+			select(
+				MODULES_READER_REVENUE_MANAGER
+			).getPublicationOnboardingState()
+		);
+	};
+}
---
+++ assets/js/modules/reader-revenue-manager/data-requirements/index.test.js
@@ -0,0 +1,77 @@
+/**
+ * Reader Revenue Manager module data requirements tests.
+ *
+ * Site Kit by Google, Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import {
+	MODULES_READER_REVENUE_MANAGER,
+	PUBLICATION_ONBOARDING_STATES,
+} from '@/js/modules/reader-revenue-manager/datastore/constants';
+import { createTestRegistry } from '@tests/js/test-utils';
+import { requirePublicationOnboardingState } from './index';
+
+describe( 'Reader Revenue Manager data requirements', () => {
+	let registry;
+
+	beforeEach( () => {
+		registry = createTestRegistry();
+	} );
+
+	describe( 'requirePublicationOnboardingState', () => {
+		it( 'should return true when the onboarding state matches', async () => {
+			registry
+				.dispatch( MODULES_READER_REVENUE_MANAGER )
+				.receiveGetSettings( {
+					publicationOnboardingState:
+						PUBLICATION_ONBOARDING_STATES.ONBOARDING_COMPLETE,
+				} );
+
+			expect(
+				await requirePublicationOnboardingState(
+					PUBLICATION_ONBOARDING_STATES.ONBOARDING_COMPLETE
+				)( registry )
+			).toBe( true );
+		} );
+
+		it( 'should return false when the onboarding state does not match', async () => {
+			registry
+				.dispatch( MODULES_READER_REVENUE_MANAGER )
+				.receiveGetSettings( {
+					publicationOnboardingState:
+						PUBLICATION_ONBOARDING_STATES.PENDING_VERIFICATION,
+				} );
+
+			expect(
+				await requirePublicationOnboardingState(
+					PUBLICATION_ONBOARDING_STATES.ONBOARDING_COMPLETE
+				)( registry )
+			).toBe( false );
+		} );
+
+		it( 'should return true when matching against an undefined onboarding state', async () => {
+			registry
+				.dispatch( MODULES_READER_REVENUE_MANAGER )
+				.receiveGetSettings( {} );
+
+			expect(
+				await requirePublicationOnboardingState( undefined )( registry )
+			).toBe( true );
+		} );
+	} );
+} );
