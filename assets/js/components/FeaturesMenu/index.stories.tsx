0afcd88b6796b13917769c4f42a15a259bb8d793
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2026-09-14 19:01:29 +0100

    Add `FeaturesMenu` story with the "Add features" button present.

--- assets/js/components/FeaturesMenu/index.stories.tsx
+++ assets/js/components/FeaturesMenu/index.stories.tsx
@@ -102,6 +102,20 @@ Default.scenario = {
 	onReadyScript: 'mouse.js',
 };
 
+export const WithAddFeaturesButton = Template.bind( {} ) as Story;
+WithAddFeaturesButton.storyName = 'With Add Features Button';
+WithAddFeaturesButton.args = {
+	setupRegistry: ( registry: WPDataRegistry ) => {
+		provideUserAuthentication( registry );
+	},
+};
+WithAddFeaturesButton.parameters = {
+	features: [ 'featureDiscoveryHub' ],
+};
+WithAddFeaturesButton.scenario = {
+	...Default.scenario,
+};
+
 export const WithoutPDFGeneration = Template.bind( {} ) as Story;
 WithoutPDFGeneration.storyName = 'Without PDF Generation';
 WithoutPDFGeneration.args = {
@@ -110,10 +124,7 @@ WithoutPDFGeneration.args = {
 	},
 };
 WithoutPDFGeneration.scenario = {
-	delay: 3000,
-	clickSelector: '.googlesitekit-features-menu__button',
-	postInteractionWait: 3000,
-	onReadyScript: 'mouse.js',
+	...Default.scenario,
 };
 
 export const ViewOnly = Template.bind( {} ) as Story & {
@@ -159,10 +170,7 @@ ViewOnly.parameters = {
 	features: [ 'pdfGeneration' ],
 };
 ViewOnly.scenario = {
-	delay: 3000,
-	clickSelector: '.googlesitekit-features-menu__button',
-	postInteractionWait: 3000,
-	onReadyScript: 'mouse.js',
+	...Default.scenario,
 };
 
 export default {
