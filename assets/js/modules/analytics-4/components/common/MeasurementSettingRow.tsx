4f4841c683cd8fa41f68b57cbc55fe21ebf70a9f
Rupesh Kumar <rsah0123456@gmail.com>
2026-09-16 11:31:13 +0545

    Update MeasurementSettingRow to use P component (#13388)

--- assets/js/modules/analytics-4/components/common/MeasurementSettingRow.tsx
+++ assets/js/modules/analytics-4/components/common/MeasurementSettingRow.tsx
@@ -28,6 +28,7 @@ import { FC, ReactNode } from 'react';
 import { ProgressBar } from 'googlesitekit-components';
 import CheckMark from '@/svg/icons/check-2.svg';
 import StarFill from '@/svg/icons/star-fill.svg';
+import P from '@/js/components/Typography/P';
 
 interface MeasurementSettingRowProps {
 	isEnabled?: boolean;
@@ -68,12 +69,12 @@ const MeasurementSettingRow: FC< MeasurementSettingRowProps > = ( {
 
 			<div className="googlesitekit-settings-measurement-row__content">
 				<div className="googlesitekit-settings-measurement-row__details">
-					<p className="googlesitekit-settings-measurement-row__title">
+					<P className="googlesitekit-settings-measurement-row__title">
 						{ title }
-					</p>
-					<p className="googlesitekit-module-settings-group__helper-text">
+					</P>
+					<P className="googlesitekit-module-settings-group__helper-text">
 						{ description }
-					</p>
+					</P>
 				</div>
 
 				{ ! isEnabled && action && (
