46120a9e421ae060eb10acb3bd16244d03d011f9
Jacob Peattie <4100733+JakePT@users.noreply.github.com>
2026-09-23 20:43:03 +1000

    Update assets/js/components/feature-discovery/FeatureCard.tsx
    Co-authored-by: Ankit Gade <ankit.gade@10up.com>

--- assets/js/components/feature-discovery/FeatureCard.tsx
+++ assets/js/components/feature-discovery/FeatureCard.tsx
@@ -94,7 +94,7 @@ const FeatureCard: FC< FeatureCardProps > = ( {
 			{
 				id: FEATURE_RELEVANCY_REASONS.NOT_RELEVANT,
 				label: __(
-					"It's not relevant to my site goals",
+					"It’s not relevant to my site goals",
 					'google-site-kit'
 				),
 				value: getFeatureRelevancyTriggerID(
